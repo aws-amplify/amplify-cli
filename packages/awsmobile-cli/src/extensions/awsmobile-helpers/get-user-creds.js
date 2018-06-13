1605e4db46bc03f4ae78b34918ec56caa73aaff0
Ghosh <kaustavg@amazon.com>
2018-06-13 09:36:42 -0700

    Remove creds

--- packages/awsmobile-cli/src/extensions/awsmobile-helpers/get-user-creds.js
+++ packages/awsmobile-cli/src/extensions/awsmobile-helpers/get-user-creds.js
@@ -3,9 +3,9 @@ function getUserCreds() {
   // Logic here should be modififed as  a part of init/config command
 
   return {
-    accessKey: 'AKIAI5LD6XA6YBWCTR2Q',
-    secretKey: '5qyNG2i69KgqrLBzr0i7Muc3JjuNoYqURSiwmGRx',
-    region: 'us-east-1',
+    accessKey: '<your accessKey>',
+    secretKey: '<your secretKey',
+    region: '<your region',
   };
 }
 
