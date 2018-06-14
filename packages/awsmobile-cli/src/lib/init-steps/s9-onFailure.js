ae361a014d71c95e48b8693d03f0d844833ad1c4
UnleashedMind <michaelzhou.net@gmail.com>
2018-06-14 14:37:33 -0700

    update based on the comments of Kaustav

--- packages/awsmobile-cli/src/lib/init-steps/s9-onFailure.js
+++ packages/awsmobile-cli/src/lib/init-steps/s9-onFailure.js
@@ -1,9 +1,9 @@
-const chalk = require('chalk');
+const {print} = require('gluegun/print')
 const util = require('util');
 
 function run(e) {
-  console.log(chalk.red('init failed'));
-  console.log(util.inspect(e));
+  print.error('init failed');
+  print.info(util.inspect(e));
 }
 
 module.exports = {
