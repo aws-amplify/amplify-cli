c2ab2be52995da709234cf9717c17623792a4ebb
UnleashedMind <michaelzhou.net@gmail.com>
2018-06-27 16:11:36 -0700

    add logic to scan different types of project

--- packages/amplify-frontend-javascript/lib/project-scanner.js
+++ packages/amplify-frontend-javascript/lib/project-scanner.js
@@ -4,9 +4,8 @@ const constants = require('./constants');
 
 function run(projectPath){
     let score = constants.ProjectScanBaseScore; 
-    let settingsFilePath = path.join(projectPath, 'settings.gradle')
-    let buildFilePath = path.join(projectPath, 'build.gradle')
-    if(fs.existsSync(settingsFilePath) && fs.existsSync(buildFilePath)){
+    const packageJsonFilePath = path.join(projectPath, 'package.json');
+    if (fs.existsSync(packageJsonFilePath)) {
         score = constants.ProjectScanMaxScore; 
     }
     return score;
