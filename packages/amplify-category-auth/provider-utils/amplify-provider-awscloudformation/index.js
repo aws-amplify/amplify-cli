9468c3c657de40bdd9db2573e488fa7018f0654b
Dustin Noyes <dnnoyes@amazon.com>
2018-06-29 10:37:18 -0700

    multiple question set refactor

--- packages/amplify-category-auth/provider-utils/amplify-provider-awscloudformation/index.js
+++ packages/amplify-category-auth/provider-utils/amplify-provider-awscloudformation/index.js
@@ -66,44 +66,4 @@ function addResource(context, category, service, configure) {
     .then(() => props.resourceName);
 }
 
-// async function addResource(context, category, service, configure) {
-//   let props = {};
-
-//   serviceMetadata = JSON.parse(fs.readFileSync(`${__dirname}/../supported-services${configure}.json`))[service];
-//   const { cfnFilename, defaultValuesFilename } = serviceMetadata;
-
-//   const serviceWalkthroughSrc = `${__dirname}/service-walkthroughs/core-questions`;
-//   const { serviceWalkthrough } = require(serviceWalkthroughSrc);
-
-//   let result = await serviceWalkthrough(context, defaultValuesFilename, serviceMetadata)
-//     .then((result) => {
-//       /* for each auth selection made by user,
-//        * populate defaults associated with the choice into props object */
-//       const defaultValuesSrc = `${__dirname}/default-values/${defaultValuesFilename}`;
-//       const { functionMap } = require(defaultValuesSrc);
-
-//       result.authSelections.forEach((i) => {
-//         props = Object.assign(props, functionMap[i](result.resourceName));
-//       });
-
-//       /* merge actual answers object into props object of defaults answers,
-//        * ensuring that manual entries override defaults */
-//       props = Object.assign(props, result);
-
-//       /* make sure that resource name populates '<label'>
-//        * placeholder from default if it hasn't already */
-//       // TODO: improve this
-//       Object.keys(props).forEach((el) => {
-//         if (typeof props[el] === 'string') {
-//           props[el] = props[el].replace(/<label>/g, props.resourceName);
-//         }
-//       });
-
-//       console.log('cfnFileName', cfnFilename)
-
-//       copyCfnTemplate(context, category, props, cfnFilename);
-//     })
-//     .then(() => props.resourceName);
-// }
-
 module.exports = { addResource };
