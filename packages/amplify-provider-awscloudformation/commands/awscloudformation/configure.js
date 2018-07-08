770fbcc59be233410bff97144663cb0ffe4c1e65
UnleashedMind <michaelzhou.net@gmail.com>
2018-07-08 15:14:25 -0700

    remove configure command from plugin level

--- packages/amplify-provider-awscloudformation/commands/awscloudformation/configure.js
+++
@@ -1,8 +0,0 @@
-const configManager = require('../../lib/configuration-manager');
-
-module.exports = {
-  name: 'configure',
-  run: async (context) => {
-    configManager.configure(context);
-  },
-};
