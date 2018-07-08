47af4dd1c30adf4ce0e85fc51f0abf979fd3d49e
UnleashedMind <michaelzhou.net@gmail.com>
2018-07-08 15:05:19 -0700

    tested new user setup end-to-end

--- packages/amplify-provider-awscloudformation/commands/awscloudformation/setupNewUser.js
+++ packages/amplify-provider-awscloudformation/commands/awscloudformation/setupNewUser.js
@@ -1,8 +1,9 @@
-const {setupNewUser} = require('../../lib/setup-new-user');
+const setupNewUser = require('../../lib/setup-new-user');
 
 module.exports = {
   name: 'setupNewUser',
+  alias: ['setup-new-user', 'newUser', 'new-user'],
   run: async (context) => {
-    setupNewUser(context);
+    setupNewUser.run(context);
   },
 };
