550449bbbb4a884bdf984b8693ae71e204acbc6f
UnleashedMind <michaelzhou.net@gmail.com>
2018-07-15 01:41:45 -0700

    add child process to execute js frontend build and start commands

--- packages/amplify-category-hosting/lib/S3AndCloudFront/index.js
+++ packages/amplify-category-hosting/lib/S3AndCloudFront/index.js
@@ -1,28 +1,67 @@
 const fs = require('fs-extra'); 
 const path = require('path');
+const inquirer = require('inquirer');
+const moment = require('moment'); 
 const fileUPloader = require('./helpers/file-uploader'); 
 const constants = require('../constants'); 
 const serviceName = 'S3AndCloudFront';
 const providerPlugin = "amplify-provider-awscloudformation";
 
+const templateFileName = 'hosting-s3andcloudfront-template.json'; 
+const parametersFileName = 'parameters.json'; 
+
 function enable(context) {
   const projectBackendDirPath = context.amplify.pathManager.getBackendDirPath(); 
   const serviceDirPath = path.join(projectBackendDirPath, constants.CategoryName, serviceName);
   fs.ensureDirSync(serviceDirPath); 
+  
+  fs.copySync(path.join(__dirname, 'template.json'), path.join(serviceDirPath, templateFileName));
 
-  const copyFilter = (src) => path.basename(src).indexof('template.json') !== -1 ||
-                              path.basename(src).indexof('parameters.json') !== -1;
-  fs.copySync(__dirname, serviceDirPath, {filter: copyFilter});
+  const parameters = JSON.parse(fs.readFileSync(path.join(__dirname, 'parameters.json'))); 
 
-  const metaData = {
-    "service": serviceName,
-    "providerPlugin": providerPlugin
-  }; 
-  return context.amplify.updateamplifyMetaAfterResourceAdd(
-    constants.CategoryName,
-    serviceName,
-    metaData,
-  );
+  const { projectConfig } = context.exeInfo; 
+  const timeStamp = `-${moment().format('YYYYMMDDHHmmss')}-`;
+  const bucketName = projectConfig.projectName + timeStamp + parameters['HostingBucketName'];
+  parameters['HostingBucketName'] = bucketName.replace(/[^-a-z0-9]/g, '');
+
+  const parameterQuestions = [
+    {
+      name: 'HostingBucketName',
+      type: 'input',
+      message: 'hosting bucket name',
+      default: parameters['HostingBucketName']
+    },
+    {
+      name: 'IndexDocument',
+      type: 'input',
+      message: 'index doc for the website',
+      default: parameters['IndexDocument']
+    },
+    {
+      name: 'ErrorDocument',
+      type: 'input',
+      message: 'error doc for the website',
+      default: parameters['ErrorDocument']
+    },
+  ];
+
+  return inquirer.prompt(parameterQuestions)
+  .then((answers) => {
+    const jsonString = JSON.stringify(answers, null, 4);
+    const parametersFilePath = path.join(serviceDirPath, parametersFileName);
+    fs.writeFileSync(parametersFilePath, jsonString, 'utf8');
+    return answers;
+  }).then(()=>{
+    const metaData = {
+      "service": serviceName,
+      "providerPlugin": providerPlugin
+    }; 
+    return context.amplify.updateamplifyMetaAfterResourceAdd(
+      constants.CategoryName,
+      serviceName,
+      metaData,
+    );
+  });
 }
 
 async function disable(context) {
--- packages/amplify-frontend-javascript/lib/server.js
+++ packages/amplify-frontend-javascript/lib/server.js
@@ -1,14 +1,37 @@
-const builder = require('./builder'); 
+const { spawn } = require('child_process')
 
 function run(context) {
-    return builder.run(context)
-    .then(runAppLocally);
+    return new Promise((resolve, reject)=>{
+        const {projectConfig} = context.exeInfo;
+        const startCommand = projectConfig[constants.Label]['config']['StartCommand'];
+        let args = startCommand.split(/\s+/);
+        const command = args[0];
+        args = args.slice(1);
+        
+        const serveExecution = spawn(command, args, {cwd: process.cwd(), env: process.env, stdio: 'inherit'});
+        
+        let rejectFlag = false; 
+        serveExecution.on('exit', function(code) {
+            context.print.info('frontend start command exited with code ' + code.toString());
+            if(code == 0){
+               resolve(context); 
+            }else if(!rejectFlag){
+                rejectFlag = true; 
+                reject(code)
+            }
+        })
+        serveExecution.on('error', (err) => {
+            context.print.error(chalk.red('frontend start command execution error'));
+            context.print.info(err);
+            if(!rejectFlag){
+                rejectFlag = true; 
+                reject(err);
+            }
+        })
+    })
 }
-
-function runAppLocally(context){
-    return context;
-}
-
+  
 module.exports = {
     run
 };
+  
