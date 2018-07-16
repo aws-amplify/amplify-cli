0786b1476b94e5d971fbce86075d9d9c4f981448
Lisa Shon <lisashon@amazon.com>
2018-07-16 15:37:47 -0700

    Add unit test for the filter inputs

--- packages/appsync-dynamodb-transformer/src/__tests__/AppSyncDynamoDBTransformer.test.ts
+++ packages/appsync-dynamodb-transformer/src/__tests__/AppSyncDynamoDBTransformer.test.ts
@@ -1,6 +1,6 @@
 import {
     ObjectTypeDefinitionNode, parse, FieldDefinitionNode, DocumentNode,
-    DefinitionNode, Kind
+    DefinitionNode, Kind, InputObjectTypeDefinitionNode
 } from 'graphql'
 import GraphQLTransform from 'graphql-transform'
 import { ResourceConstants } from 'appsync-transformer-common'
@@ -100,6 +100,56 @@ test('Test AppSyncDynamoDBTransformer with only create mutations', () => {
     doNotExpectFields(mutationType, ['updatePost'])
 });
 
+test('Test AppSyncDynamoDBTransformer with multiple model directives', () => {
+    const validSchema = `
+    type Post @model {
+        id: ID!
+        title: String!
+        createdAt: String
+        updatedAt: String
+    }
+
+    type User @model {
+        id: ID!
+        name: String!
+    }
+    `
+    const transformer = new GraphQLTransform({
+        transformers: [
+            new AppSyncDynamoDBTransformer()
+        ]
+    })
+    const out = transformer.transform(validSchema);
+    expect(out).toBeDefined()
+
+    const schema = out.Resources[ResourceConstants.RESOURCES.GraphQLSchemaLogicalID]
+    expect(schema).toBeDefined()
+    const definition = schema.Properties.Definition
+    expect(definition).toBeDefined()
+    const parsed = parse(definition);
+    const queryType = getObjectType(parsed, 'Query')
+    expect(queryType).toBeDefined()
+    expectFields(queryType, ['listPost'])
+    expectFields(queryType, ['listUser'])
+    
+    const stringInputType = getInputType(parsed, 'TableStringFilterInput')
+    expect(stringInputType).toBeDefined()
+    const booleanInputType = getInputType(parsed, 'TableBooleanFilterInput')
+    expect(booleanInputType).toBeDefined()
+    const intInputType = getInputType(parsed, 'TableIntFilterInput')
+    expect(intInputType).toBeDefined()
+    const floatInputType = getInputType(parsed, 'TableFloatFilterInput')
+    expect(floatInputType).toBeDefined()
+    const idInputType = getInputType(parsed, 'TableIDFilterInput')
+    expect(idInputType).toBeDefined()
+
+    verifyInputCount(parsed, 'TableStringFilterInput', 1);
+    verifyInputCount(parsed, 'TableBooleanFilterInput', 1);
+    verifyInputCount(parsed, 'TableIntFilterInput', 1);
+    verifyInputCount(parsed, 'TableFloatilterInput', 1);
+    verifyInputCount(parsed, 'TableIDFilterInput', 1);
+});
+
 function expectFields(type: ObjectTypeDefinitionNode, fields: string[]) {
     for (const fieldName of fields) {
         const foundField = type.fields.find((f: FieldDefinitionNode) => f.name.value === fieldName)
@@ -119,4 +169,14 @@ function getObjectType(doc: DocumentNode, type: string): ObjectTypeDefinitionNod
     return doc.definitions.find(
         (def: DefinitionNode) => def.kind === Kind.OBJECT_TYPE_DEFINITION && def.name.value === type
     ) as ObjectTypeDefinitionNode | undefined
+}
+
+function getInputType(doc: DocumentNode, type: string): InputObjectTypeDefinitionNode | undefined {
+    return doc.definitions.find(
+        (def: DefinitionNode) => def.kind === Kind.INPUT_OBJECT_TYPE_DEFINITION && def.name.value === type
+    ) as InputObjectTypeDefinitionNode | undefined
+}
+
+function verifyInputCount(doc: DocumentNode, type: string, count: number): number {
+    return doc.definitions.filter(def => def.kind === Kind.INPUT_OBJECT_TYPE_DEFINITION && def.name.value === type).length;
 }
