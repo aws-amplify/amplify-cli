80aad6504d9f7e0e72aa41c1664376913fb02e91
Dustin Noyes <dnnoyes@amazon.com>
2018-07-19 08:30:56 -0700

    bug fixes

--- packages/amplify-category-auth/provider-utils/amplify-provider-awscloudformation/service-walkthroughs/auth-questions.js
+++ packages/amplify-category-auth/provider-utils/amplify-provider-awscloudformation/service-walkthroughs/auth-questions.js
@@ -63,7 +63,6 @@ async function serviceWalkthrough(
     },
   );
 
-  let roles;
   if (defaultConfigAnswer.useDefault === false) {
     coreAnswers = await inquirer.prompt(coreQuestions);
 
@@ -80,9 +79,9 @@ async function serviceWalkthrough(
       );
       appClientAnswers = await inquirer.prompt(appClientQuestions);
     }
-
-    roles = await context.amplify.executeProviderUtils(context, 'amplify-provider-awscloudformation', 'staticRoles');
   }
+
+  const roles = await context.amplify.executeProviderUtils(context, 'amplify-provider-awscloudformation', 'staticRoles');
 
   return {
     ...defaultConfigAnswer,
