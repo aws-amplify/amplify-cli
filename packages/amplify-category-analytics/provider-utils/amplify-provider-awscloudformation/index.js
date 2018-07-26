b01a023cf52a52a54a39cb87265b4c1cf1c746ca
Ghosh <kaustavg@amazon.com>
2018-07-26 00:28:33 -0700

    Attach appopriate IAM policies for pinpoint + s3 resources & kickoff auth flow if auth not enabled when adding these resources

--- packages/amplify-category-analytics/provider-utils/amplify-provider-awscloudformation/index.js
+++ packages/amplify-category-analytics/provider-utils/amplify-provider-awscloudformation/index.js
@@ -10,14 +10,4 @@ function addResource(context, category, service) {
   return addWalkthrough(context, defaultValuesFilename, serviceMetadata);
 }
 
-function updateResource(context, category, service) {
-  const serviceMetadata = JSON.parse(fs.readFileSync(`${__dirname}/../supported-services.json`))[service];
-  const { defaultValuesFilename, serviceWalkthroughFilename } = serviceMetadata;
-
-  const serviceWalkthroughSrc = `${__dirname}/service-walkthroughs/${serviceWalkthroughFilename}`;
-  const { updateWalkthrough } = require(serviceWalkthroughSrc);
-
-  return updateWalkthrough(context, defaultValuesFilename, serviceMetadata);
-}
-
-module.exports = { addResource, updateResource };
+module.exports = { addResource };
