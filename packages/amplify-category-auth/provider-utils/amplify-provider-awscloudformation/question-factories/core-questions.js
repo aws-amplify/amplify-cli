eac1debfda140d00ad8e7c0bc0d19e1e5f127ca7
Dustin Noyes <dnnoyes@amazon.com>
2018-07-31 09:32:59 -0700

    learn more functionality

---
+++ packages/amplify-category-auth/provider-utils/amplify-provider-awscloudformation/question-factories/core-questions.js
@@ -0,0 +1,83 @@
+const inquirer = require('inquirer');
+
+function parseInputs(input, amplify, defaultValuesFilename, stringMapsFilename, currentAnswers, context) {
+  const defaultValuesSrc = `${__dirname}/../assets/${defaultValuesFilename}`;
+  const stringMapsSrc = `${__dirname}/../assets/${stringMapsFilename}`;
+  const { getAllDefaults } = require(defaultValuesSrc);
+  const { getAllMaps } = require(stringMapsSrc);
+
+    // Can have a cool question builder function here based on input json - will iterate on this
+    // Can also have some validations here based on the input json
+    // Uncool implementation here
+
+  let question = {
+    name: input.key,
+    message: input.question,
+    prefix: input.prefix,
+    suffix: input.suffix,
+    when: amplify.getWhen(input, currentAnswers),
+    validate: amplify.inputValidation(input),
+    default: (answers) => { // eslint-disable-line no-unused-vars
+      if (currentAnswers) {
+        answers = Object.assign(answers, currentAnswers);
+      }
+      const defaultValue = getAllDefaults(amplify.getProjectDetails(amplify))[input.key];
+
+      if (defaultValue) {
+        return defaultValue;
+      }
+
+      if (context.api.privacy === 'protected' && input.key === 'allowUnauthenticatedIdentities') {
+        return true;
+      }
+
+      return undefined;
+    },
+  };
+
+  if (input.type && ['list', 'multiselect'].includes(input.type)) {
+    if (!input.requiredOptions || !currentAnswers[input.requiredOptions]) {
+      question = Object.assign({
+        choices: input.map ? getAllMaps()[input.map] : input.options,
+      }, question);
+    } else {
+      const requiredOptions = getAllMaps()[input.map]
+        .filter(x => currentAnswers[input.requiredOptions]
+          .includes(x.value));
+      const trueOptions = getAllMaps()[input.map]
+        .filter(x => !currentAnswers[input.requiredOptions]
+          .includes(x.value));
+
+      question = Object.assign(question, {
+        choices: [new inquirer.Separator(`--- You have already selected the following attributes as required for this User Pool.  They are writeable by default: ${requiredOptions.map(t => t.name).join(', ')}   ---`), ...trueOptions],
+        filter: ((input) => {
+          input = input.concat(...requiredOptions.map(z => z.value));
+          return input;
+        }),
+      });
+    }
+  }
+
+  if (input.type && input.type === 'list') {
+    question = Object.assign({
+      type: 'list',
+    }, question);
+  } else if (input.type && input.type === 'multiselect') {
+    question = Object.assign({
+      type: 'checkbox',
+    }, question);
+  } else if (input.type && input.type === 'confirm') {
+    question = Object.assign({
+      type: 'confirm',
+    }, question);
+  } else {
+    question = Object.assign({
+      type: 'input',
+    }, question);
+  }
+  
+
+  return question;
+}
+
+module.exports = { parseInputs };
---
+++ packages/amplify-category-auth/provider-utils/amplify-provider-awscloudformation/service-walkthroughs/auth-questions.js
@@ -0,0 +1,96 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const chalkpipe = require('chalk-pipe')
+const thirdPartyMap = require('../assets/string-maps').authProviders;
+const inputTest = require('../assets/input-tests.js').inputTest;
+
+async function serviceWalkthrough(
+  context,
+  defaultValuesFilename,
+  stringMapsFilename,
+  serviceMetadata,
+) {
+  const { inputs } = serviceMetadata;
+  const { amplify } = context;
+  const { parseInputs } = require(`${__dirname}/../question-factories/core-questions.js`);
+
+  let coreAnswers = {};
+  const coreQuestionInputs = inputs;
+
+
+  /* Begin looping through questions */
+  for (let i=0; i < coreQuestionInputs.length; i ++) {
+
+    /* 
+      If we are on the second or higher question, we first check to see if the user selected 'learn more',
+      and if they did we append the learn more text as a chalk-styled suffix to the last question
+      and decrement the loop iterator by one to 'rewind' to the last question with the suffix displayed.
+      */
+    if (i > 0 && new RegExp(/learn/i).test(coreAnswers[coreQuestionInputs[i-1].key]) && coreQuestionInputs[i-1].learnMore) {
+     
+// !IMPORTANT! Do no change indentation in template string.
+      const helpTextArray = coreQuestionInputs[i-1].learnMore.split("\n");
+      const helpText = `
+${helpTextArray.join(
+`
+        
+`
+      )}
+      
+`;
+      
+      coreQuestionInputs[i-1].prefix = chalkpipe(null, chalk.magenta)(helpText);
+      i--
+
+    };
+
+    if (coreAnswers['useDefault'] === 'default'){
+      break;
+    }
+
+    let q = parseInputs(
+        coreQuestionInputs[i],
+        amplify,
+        defaultValuesFilename,
+        stringMapsFilename,
+        coreAnswers,
+        context
+    );
+
+    coreAnswers = { ...coreAnswers, ...(await inquirer.prompt(q)) }
+  };
+
+  /*
+    create key/value pairs of third party auth providers,
+    where key = name accepted by updateIdentityPool API call and value = id entered by user
+    TODO: evalutate need for abstracted version of this operation
+  */
+  if (coreAnswers.thirdPartyAuth) {
+    coreAnswers.selectedParties = {};
+    thirdPartyMap.forEach((e) => {
+      if (coreAnswers[e.answerHashKey]) {
+        coreAnswers.selectedParties[e.value] = coreAnswers[e.answerHashKey];
+      }
+      /*
+        certain third party providers (such as Twitter) require multiple values,
+        which Cognito requires to be a concatenated string -
+        so here we build the string using 'concatKeys' defined in the thirdPartyMap
+      */
+      if (coreAnswers[e.answerHashKey] && e.concatKeys) {
+        e.concatKeys.forEach((i) => {
+          coreAnswers.selectedParties[e.value] = coreAnswers.selectedParties[e.value].concat(';', coreAnswers[i]);
+        });
+      }
+    });
+  }
+
+  const roles = await context.amplify.executeProviderUtils(context, 'amplify-provider-awscloudformation', 'staticRoles');
+
+
+  return {
+    ...coreAnswers,
+    ...roles,
+  };
+}
+
+module.exports = { serviceWalkthrough };
