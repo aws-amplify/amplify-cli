9bff85bc95ad6eb6b1fd120a1a950db351f7a04a
kateglee <kateglee@amazon.com>
2018-08-15 01:12:11 -0700

    Edit pass complete
    Made changes based on AWS style. Note that "backend and front-end resources" is correct, even though it looks odd.

--- packages/amplify-category-analytics/commands/analytics/add.js
+++ packages/amplify-category-analytics/commands/analytics/add.js
@@ -34,8 +34,8 @@ module.exports = {
         print.success(`Successfully added resource ${resourceName} locally`);
         print.info('');
         print.success('Some next steps:');
-        print.info('"amplify push" will build all your local backend resources and provision it in the cloud');
-        print.info('"amplify publish" will build all your local backend and frontend resources (if you have hosting category added) and provision it in the cloud');
+        print.info('"amplify push" builds all of your local backend resources and provisions them in the cloud');
+        print.info('"amplify publish" builds all your local backend and front-end resources (if you have hosting category added) and provisions them in the cloud');
         print.info('');
       })
       .catch((err) => {
