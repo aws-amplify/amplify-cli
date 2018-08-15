4107f4f6a52ad3446ecd79800c147409a9339b6f
Dustin Noyes <dnnoyes@amazon.com>
2018-08-15 08:45:41 -0700

    fixed issue with defaults, added android config vals

--- packages/amplify-frontend-android/lib/frontend-config-creator.js
+++ packages/amplify-frontend-android/lib/frontend-config-creator.js
@@ -20,7 +20,7 @@ function createAmplifyConfig(context, amplifyResources) {
 function createAWSConfig(context, amplifyResources) {
   const { serviceResourceMapping } = amplifyResources;
   const configOutput = {
-    UserAgent: 'aws-amplify-cli',
+    UserAgent: 'aws-amplify-cli/0.1.0',
     Version: '1.0',
     IdentityManager: {
       Default: {},
@@ -66,40 +66,48 @@ function getCognitoConfig(cognitoResources, projectRegion) {
 
   const cognitoResource = cognitoResources[0];
 
-  const resourceValue = {
-    CredentialsProvider: {
-      CognitoIdentity: {
+  const cognitoConfig = {};
+
+  if (cognitoResource.output.IdentityPoolId) {
+    Object.assign(cognitoConfig, {
+      CredentialsProvider: {
+        CognitoIdentity: {
+          Default: {
+            PoolId: cognitoResource.output.IdentityPoolId,
+            Region: projectRegion,
+          },
+        },
+      },
+    });
+  }
+
+  if (cognitoResource.output.UserPoolId) {
+    Object.assign(cognitoConfig, {
+      CognitoUserPool: {
         Default: {
-          PoolId: cognitoResource.output.IdentityPoolId,
+          PoolId: cognitoResource.output.UserPoolId,
+          AppClientId: cognitoResource.output.AppClientID,
+          AppClientSecret: cognitoResource.output.AppClientSecret,
           Region: projectRegion,
         },
       },
-    },
-    CognitoUserPool: {
-      Default: {
-        PoolId: cognitoResource.output.UserPoolId,
-        AppClientId: cognitoResource.output.AppClientID,
-        AppClientSecret: cognitoResource.output.AppClientSecret,
-        Region: projectRegion,
-      },
-    },
-  };
+    });
+  }
 
   if (cognitoResource.output.GoogleWebClient) {
-    resourceValue.GoogleSignIn = {
+    cognitoResource.GoogleSignIn = {
       Permissions: 'email,profile,openid',
       'ClientId-WebApp': cognitoResource.output.GoogleWebClient,
     };
   }
   if (cognitoResource.output.FacebookWebClient) {
-    resourceValue.FacebookSignIn = {
+    cognitoResource.FacebookSignIn = {
       AppId: cognitoResource.output.FacebookWebClient,
       Permissions: 'public_profile',
     };
   }
 
-
-  return resourceValue;
+  return cognitoConfig;
 }
 
 
