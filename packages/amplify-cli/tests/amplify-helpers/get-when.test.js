5549fceff7cab8baba1905f25bdb057f3c42bb51
Dustin Noyes <dnnoyes@amazon.com>
2018-08-28 16:21:30 -0700

    rebased off master

---
+++ packages/amplify-cli/tests/amplify-helpers/get-when.test.js
@@ -0,0 +1,12 @@
+const getWhen = require('../../src/extensions/amplify-helpers/get-when-function');
+
+describe('get-when helper: ', () => {
+  
+  it('the get-when function should be exported', () => {
+    expect(getWhen).toBeDefined();
+  });
+
+
+
+});
+
