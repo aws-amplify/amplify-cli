62d28c0f37ba3adb1e1e53a394267f570d14b8a3
Dustin Noyes <dnnoyes@amazon.com>
2018-08-28 16:14:01 -0700

    initial test for get-when helper

---
+++ packages/amplify-cli/tests/amplify-helpers/get-when.test.js
@@ -0,0 +1,10 @@
+const getWhen = require('../../src/extensions/amplify-helpers/get-when-function');
+
+describe('auth enable: ', () => {
+
+  it('enable run method should exist', () => {
+    expect(getWhen).toBeDefined();
+  });
+
+});
+
