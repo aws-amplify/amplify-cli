a9c00940698e11614ebb9f42ca5de870ac6b4d86
UnleashedMind <michaelzhou.net@gmail.com>
2018-09-12 11:47:12 -0700

    minor fix

--- packages/amplify-category-notifications/__tests__/lib/channel-APNS.test.js
+++ packages/amplify-category-notifications/__tests__/lib/channel-APNS.test.js
@@ -37,6 +37,7 @@ describe('channel-APNS', () => {
     }; 
 
     beforeAll(() => {
+        global.console = {log: jest.fn()}
         configureKey.run = jest.fn(()=>{
             return mockKeyConfig; 
         });
