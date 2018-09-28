4a40bb9b4488ace11f9798a9aa332db8a222227e
Shankar Raju <shanraju@amazon.com>
2018-09-28 12:37:17 -0700

    Ensures we don't add multiple non-model input types

--- packages/graphql-dynamodb-transformer/src/DynamoDBModelTransformer.ts
+++ packages/graphql-dynamodb-transformer/src/DynamoDBModelTransformer.ts
@@ -105,6 +105,9 @@ export class DynamoDBModelTransformer extends Transformer {
         nonModelArray.forEach(
             (value: ObjectTypeDefinitionNode) => {
                 let nonModelObject = makeNonModelInputObject(value, nonModelArray, ctx)
+                if (this.typeExist(nonModelObject.name.value, ctx)) {
+                    return
+                }
                 ctx.addInput(nonModelObject)
             }
         )
