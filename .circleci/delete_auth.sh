7b492e0465bcb064a49f76c74cc6b41e04d16e51
Yathi <511386+yuth@users.noreply.github.com>
2018-10-05 15:14:14 -0700

    fix: remove deployment bucket after integ test

--- .circleci/delete_auth.sh
+++ .circleci/delete_auth.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -xv
 cd aws-amplify-cypress-api
+DEPLOYMENT_BUCKET="s3://$(jq -r '.providers.awscloudformation.DeploymentBucketName' amplify/backend/amplify-meta.json)"
 amplify delete
 echo "delete auth executed"
+echo "deleting the deployment buckets"
+aws s3 rb "$DEPLOYMENT_BUCKET" --force
