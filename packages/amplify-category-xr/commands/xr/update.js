bf750669b2660a6521856ce7ecd33a47cd0a26e3
UnleashedMind <michaelzhou.net@gmail.com>
2018-10-17 17:47:13 -0700

    work in progress

--- packages/amplify-category-xr/commands/xr/update.js
+++ packages/amplify-category-xr/commands/xr/update.js
@@ -3,6 +3,7 @@ const xrManager = require('../../lib/xr-manager')
 
 module.exports = {
   name: subcommand,
+  alias: ['configure'],
   run: async (context) => {
     context.exeInfo = context.amplify.getProjectDetails();
     xrManager.configure(context);
