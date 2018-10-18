2ca49d44c67ea3f391a4458f61ea4f3d294397d7
Sharma <nidsharm@amazon.com>
2018-10-18 14:29:57 -0700

    updating cypress test for auth1

--- cypress/integration/auth_spec.js
+++ cypress/integration/auth_spec.js
@@ -8,7 +8,7 @@ describe('withAuthenticator Sign In', function() {
     // Check for user not signed up
     cy.get('input[name=username]').type('testuser')
     cy.get('input[name=password]').type('testPassword')
-    cy.get(button).contains('Sign In').click()
+    cy.get('button').contains('Sign In').click()
     cy.get('.amplify-error-section').contains('User does not exist')
   })
 })
