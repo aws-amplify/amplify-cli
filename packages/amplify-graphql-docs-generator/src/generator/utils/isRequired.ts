1618ff66bed24968e6f9bbe7320e4b245c8af6cc
Yathi <511386+yuth@users.noreply.github.com>
2018-10-23 09:36:47 -0700

    style(amplify-graphql-docs-generator): remove console.log

--- packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
+++ packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
@@ -1,7 +1,6 @@
 import { GraphQLType, isNonNullType, isListType } from "graphql";
 export default function isRequired(typeObj: GraphQLType): boolean {
   if (isNonNullType(typeObj) && isListType(typeObj.ofType)) {
-    console.log('inside condition');
     return isRequired(typeObj.ofType.ofType)
   }
   return isNonNullType(typeObj);
