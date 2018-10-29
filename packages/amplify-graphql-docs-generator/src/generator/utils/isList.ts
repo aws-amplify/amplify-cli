bd515e6a387474d070b30befc864c434cb1c5404
Yathiraj <511386+yuth@users.noreply.github.com>
2018-10-29 11:13:23 -0700

    fix(amplify-graphql-docs-generator): support list types in input (#336)
    * fix(amplify-graphql-docs-generator): support list types in input
    
    statement generator did not honor the list types in input. It used to coerce a list into a non list
    type and the check for non null type was a shallow check. Updated code to support list in input
    types
    
    fix #295

---
+++ packages/amplify-graphql-docs-generator/src/generator/utils/isList.ts
@@ -0,0 +1,8 @@
+import { GraphQLType, isNonNullType, isListType } from "graphql";
+
+export default function isList(typeObj: GraphQLType): boolean {
+  if (isNonNullType(typeObj)) {
+    return isList(typeObj.ofType);
+  }
+  return isListType(typeObj);
+}
