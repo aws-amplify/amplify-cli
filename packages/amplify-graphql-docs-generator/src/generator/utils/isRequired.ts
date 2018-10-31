0b9f9ab2071f30fda59657c7e5739b40d9a6c3b1
Nate <43688026+OtterFlip@users.noreply.github.com>
2018-10-31 17:36:07 -0600

    Handle Nullable Lists of Non-null Types
    There are four permutations of list arguments in GQL.  We we weren't handling nullable lists of non-null types.  This change corrects that.  For future reference these four permutations are:
    
    Nullable List Type: [] e.g. [String]
    Nullable List of Non-null Types: [!] e.g . [String!]
    Non-null List Type: []! e.g. [String]!
    Non-null List of Non-null Types: [!]! e.g [String!]!

--- packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
+++ packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
@@ -1,7 +1,12 @@
 import { GraphQLType, isNonNullType, isListType } from "graphql";
 export default function isRequired(typeObj: GraphQLType): boolean {
+  // See if it's a Non-null List of Non-null Types
   if (isNonNullType(typeObj) && isListType(typeObj.ofType)) {
     return isRequired(typeObj.ofType.ofType)
+  }
+  // See if it's a Nullable List of Non-null Types
+  else if (isListType(typeObj)) {
+    return isNonNullType(typeObj.ofType);
   }
   return isNonNullType(typeObj);
 }
