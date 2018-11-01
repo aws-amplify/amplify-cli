c21b5fa0dbc306fe777ae714e30c4c03a5ef5fbf
Nate <43688026+OtterFlip@users.noreply.github.com>
2018-10-31 18:06:04 -0600

    Update isRequired.ts

--- packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
+++ packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
@@ -1,11 +1,11 @@
 import { GraphQLType, isNonNullType, isListType } from "graphql";
 export default function isRequired(typeObj: GraphQLType): boolean {
-  // See if it's a Non-null List of Non-null Types
   if (isNonNullType(typeObj) && isListType(typeObj.ofType)) {
+    // See if it's a Non-null List of Non-null Types  
     return isRequired(typeObj.ofType.ofType)
   }
-  // See if it's a Nullable List of Non-null Types
   else if (isListType(typeObj)) {
+    // See if it's a Nullable List of Non-null Types
     return isNonNullType(typeObj.ofType);
   }
   return isNonNullType(typeObj);
