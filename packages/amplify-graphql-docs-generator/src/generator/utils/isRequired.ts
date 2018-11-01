3b16632f94762a8001653b9c98de6d4d348c3a17
Nate <43688026+OtterFlip@users.noreply.github.com>
2018-11-01 10:49:33 -0600

    Fix: (amplify-graphql-docs-generator) codegen to handle all permutations of LIST args (#380)
    There are four permutations of list arguments in GQL.  We we weren't handling nullable lists of non-null types.  This change fixes that
    
    For future reference these four permutations are:
    - Nullable List Type: [] e.g. [String]
    - Nullable List of Non-null Types: [!] e.g . [String!]
    - Non-null List Type: []! e.g. [String]!
    - Non-null List of Non-null Types: [!]! e.g [String!]!

--- packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
+++ packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
@@ -1,7 +1,11 @@
 import { GraphQLType, isNonNullType, isListType } from "graphql";
 export default function isRequired(typeObj: GraphQLType): boolean {
   if (isNonNullType(typeObj) && isListType(typeObj.ofType)) {
+    // See if it's a Non-null List of Non-null Types
     return isRequired(typeObj.ofType.ofType)
+  } else if (isListType(typeObj)) {
+    // See if it's a Nullable List of Non-null Types
+    return isNonNullType(typeObj.ofType);
   }
   return isNonNullType(typeObj);
 }
