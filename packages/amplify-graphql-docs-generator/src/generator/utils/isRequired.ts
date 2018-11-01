c3ab2effc410bddbda06585d73e94d5037a1952a
Nate <43688026+OtterFlip@users.noreply.github.com>
2018-10-31 18:11:33 -0600

    Update isRequired.ts
    Trying to puzzle out the style of spacing/braces that'll make tslint happy...  Yea!

--- packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
+++ packages/amplify-graphql-docs-generator/src/generator/utils/isRequired.ts
@@ -3,8 +3,7 @@ export default function isRequired(typeObj: GraphQLType): boolean {
   if (isNonNullType(typeObj) && isListType(typeObj.ofType)) {
     // See if it's a Non-null List of Non-null Types  
     return isRequired(typeObj.ofType.ofType)
-  }
-  else if (isListType(typeObj)) {
+  } else if (isListType(typeObj)) {
     // See if it's a Nullable List of Non-null Types
     return isNonNullType(typeObj.ofType);
   }
