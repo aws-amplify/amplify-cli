d5a473fdb9e0ce88ee582dcb0ff7e76a5d850c79
UnleashedMind <michaelzhou.net@gmail.com>
2018-11-05 16:23:46 -0800

    minor fix

--- packages/amplify-cli/src/extensions/amplify-helpers/construct-exeInfo.js
+++ packages/amplify-cli/src/extensions/amplify-helpers/construct-exeInfo.js
@@ -1,7 +1,9 @@
 const { getProjectDetails } = require('./get-project-details');
+const { getEnvInfo } = require('./get-env-info');
 
 function constructExeInfo(context) {
   context.exeInfo = getProjectDetails();
+  context.exeInfo.localEnvInfo = getEnvInfo();
   context.exeInfo.inputParams = {};
   Object.keys(context.parameters.options).forEach((key) => {
     const normalizedKey = normalizeKey(key);
