a24ffb95ad4ceff6da5d00751ba6ba90ab32283c
UnleashedMind <michaelzhou.net@gmail.com>
2018-11-09 16:11:22 -0800

    work in progress

--- packages/amplify-category-notifications/lib/multi-env-manager.js
+++ packages/amplify-category-notifications/lib/multi-env-manager.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const sequential = require('promise-sequential');
 const pinpointHelper = require('./pinpoint-helper'); 
 const constants = require('./constants'); 
 const notificationManager = require('./notifications-manager'); 
@@ -18,16 +19,19 @@ async function pullCurrentAmplifyMeta(context){
     const teamProviderInfoFilepath = context.amplify.pathManger.getProviderInfoFilePath();
     if(fs.existsSync(teamProviderInfoFilepath)){
         const teamProviderInfo = require(teamProviderInfoFilepath);
-        if(teamProviderInfo[envName] && teamProviderInfo[envName][constants.CategoryName]){
-            pinpointApp = teamProviderInfo[envName][constants.CategoryName]; 
+        if(teamProviderInfo[envName] && 
+            teamProviderInfo[envName]['categories'] && 
+            teamProviderInfo[envName]['categories'][constants.CategoryName]){
+            pinpointApp = teamProviderInfo[envName]['categories'][constants.CategoryName]; 
         }
     }
 
     const currentMetaFilePath = context.amplify.pathManger.getCurentAmplifyMetaFilePath(); 
     const currentBackendAmplifyMeta =  JSON.parse(fs.readFileSync(currentMetaFilePath));
     if(!pinpointApp){
-        pinpointApp = 
-        pinpointHelper.scanCategoryMetaForPinpoint(currentBackendAmplifyMeta[constants.AnalyticsCategoryName]);
+        pinpointApp = pinpointHelper.scanCategoryMetaForPinpoint(
+            currentBackendAmplifyMeta[constants.AnalyticsCategoryName]
+        );
     }
 
     if(pinpointApp){
@@ -38,6 +42,9 @@ async function pullCurrentAmplifyMeta(context){
             "output": pinpointApp,
             "lastPushTimeStamp": new Date(),
         };
+        
+        const jsonString = JSON.stringify(currentBackendAmplifyMeta, null, 4);
+        fs.writeFileSync(currentMetaFilePath, jsonString, 'utf8');
     }
 }
 
@@ -45,13 +52,105 @@ async function constructAmplifyMeta(context){
     const backendConfigFilePath = context.amplify.pathManger.getBackendConfigFilePath(); 
     const backendConfig =  JSON.parse(fs.readFileSync(backendConfigFilePath));
     if(backendConfig[constants.CategoryName]){
-        
+        const metaFilePath = context.amplify.pathManger.getAmplifyMetaFilePath(); 
+        const amplifyMeta =  JSON.parse(fs.readFileSync(metaFilePath));
+        amplifyMeta[constants.CategoryName] = backendConfig[constants.CategoryName];
+        const jsonString = JSON.stringify(amplifyMeta, null, 4);
+        fs.writeFileSync(metaFilePath, jsonString, 'utf8');
     }
 }
 
 async function pushChanges(context){
+    const currentMetaFilePath = context.amplify.pathManger.getCurentAmplifyMetaFilePath(); 
+    const currentBackendAmplifyMeta =  JSON.parse(fs.readFileSync(currentMetaFilePath));
 
+    const metaFilePath = context.amplify.pathManger.getAmplifyMetaFilePath(); 
+    const amplifyMeta =  JSON.parse(fs.readFileSync(metaFilePath));
+    
+    const availableChannels = notificationManager.getAvailableChannels(); 
+
+    const currentEnabledChannels = []
+    const newEnabledChannels = []; 
+
+    if(currentBackendAmplifyMeta && currentBackendAmplifyMeta[constants.CategoryName]){
+        const categoryMeta = currentBackendAmplifyMeta[constants.CategoryName];
+        const services = Object.keys(categoryMeta);
+        for (let i = 0; i < services.length; i++) {
+          const serviceMeta = categoryMeta[services[i]];
+          if (serviceMeta.service === 'Pinpoint' &&
+                                    serviceMeta.output &&
+                                    serviceMeta.output.Id) {
+            availableChannels.forEach((channel) => {
+              if (serviceMeta.output[channel] && serviceMeta.output[channel].Enabled) {
+                currentEnabledChannels.push(channel);
+              }
+            });
+            break;
+          }
+        }
+    }
+
+
+    if(amplifyMeta && amplifyMeta[constants.CategoryName]){
+        const categoryMeta = amplifyMeta[constants.CategoryName];
+        const services = Object.keys(categoryMeta);
+        for (let i = 0; i < services.length; i++) {
+          const serviceMeta = categoryMeta[services[i]];
+          if ( serviceMeta.service === 'Pinpoint' && serviceMeta.channels ) {
+            availableChannels.forEach((channel) => {
+              if (serviceMeta.channels.includes(channel)) {
+                newEnabledChannels.push(channel);
+              }
+            });
+            break;
+          }
+        }
+    }
+
+    const channelsToEnable = []; 
+    const channelsToDisable = []; 
+    const channelsToUpdate = []; 
+
+    availableChannels.forEach((channel)=>{
+        let isCurrentlyEnabled = currentEnabledChannels.includes(channel); 
+        let needToBeEnabled = newEnabledChannels.includes(channel); 
+
+        if(isCurrentlyEnabled && needToBeEnabled){
+            channelsToUpdate.push(channel);
+        }
+        if(isCurrentlyEnabled && !needToBeEnabled){
+            channelsToDisable.push(channel); 
+        }
+        if(!isCurrentlyEnabled && needToBeEnabled){
+            channelsToEnable.push(channel);
+        }
+    });
+
+    const tasks = []; 
+
+    channelsToEnable.map((channel)=>{
+        tasks.push(()=>{
+            notificationManager.enableChannel(context, channel); 
+        })
+    });
+
+
+    channelsToDisable.map((channel)=>{
+        tasks.push(()=>{
+            notificationManager.disableChannel(context, channel); 
+        })
+    });
+
+
+    channelsToUpdate.map((channel)=>{
+        tasks.push(()=>{
+            notificationManager.configureChannel(context, channel); 
+        })
+    });
+
+    await sequential(tasks);
 }
+
 
 async function initEnvPush(context){
     await pushChanges(context); 
