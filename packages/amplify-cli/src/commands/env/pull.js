cc5f5d08db636c217fd294f71e3cce6c417a8d9a
Ghosh <kaustavg@amazon.com>
2018-11-12 12:10:49 -0800

    fix: rename amplify env sync to amplify env pull

--- packages/amplify-cli/src/commands/env/pull.js
+++ packages/amplify-cli/src/commands/env/pull.js
@@ -1,7 +1,7 @@
 const { initializeEnv } = require('../../lib/initialize-env');
 
 module.exports = {
-  name: 'sync',
+  name: 'pull',
   run: async (context) => {
     context.amplify.constructExeInfo(context);
     context.exeInfo.forcePush = false;
