aabc487b78ce2cf190bb392edee4309c291767d9
UnleashedMind <michaelzhou.net@gmail.com>
2018-12-05 11:28:30 -0800

    feat(amplify-category-hosting): validate hosting bucket name

--- packages/amplify-category-hosting/lib/S3AndCloudFront/configuration-manager.js
+++ packages/amplify-category-hosting/lib/S3AndCloudFront/configuration-manager.js
@@ -10,7 +10,7 @@ const configurables = {
 async function init(context) {
   const { projectConfig } = context.exeInfo;
   const timeStamp = `-${moment().format('YYYYMMDDHHmmss')}-`;
-  let bucketName = `${projectConfig.projectName + timeStamp}-hostingbucket`;
+  let bucketName = `${projectConfig.projectName + timeStamp}hostingbucket`;
   bucketName = bucketName.replace(/[^-a-z0-9]/g, '');
 
   const questions = [{
@@ -18,6 +18,44 @@ async function init(context) {
     type: 'input',
     message: 'hosting bucket name',
     default: bucketName,
+    validate: (value) => {
+      let isValid =
+      typeof value === 'string' &&
+      value.length >= 3 &&
+      value.length <= 63 &&
+      /^[a-z0-9.-]*$/.test(value);
+
+      if (!isValid) {
+        return 'The bucket name must be a string between 3 and 63 characters long, and can contain only lower-case characters, numbers, periods, and dashes.';
+      }
+
+      isValid = /^[a-z0-9]/.test(value);
+      if (!isValid) {
+        return 'The bucket name must start with a lowercase letter or number.';
+      }
+
+      isValid = !/-$/.test(value);
+      if (!isValid) {
+        return 'The bucket name cannot end with a dash.';
+      }
+
+      isValid = !/\.{2,}/.test(value);
+      if (!isValid) {
+        return 'The bucket name cannot have consecutive periods.';
+      }
+
+      isValid = !/\.-|-\./.test(value);
+      if (!isValid) {
+        return 'The bucket name cannot have dashes adjacent to periods.';
+      }
+
+      isValid = !(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/.test(value));
+      if (!isValid) {
+        return 'The bucket name cannot be formatted as an IP address.';
+      }
+
+      return true;
+    },
   }];
 
   const answers = await inquirer.prompt(questions);
