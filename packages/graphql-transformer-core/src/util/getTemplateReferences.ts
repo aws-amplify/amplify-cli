8a7a55b9e9c1ba5e2fb7e1264122f9981dbd5808
Paris <parismic@amazon.com>
2018-12-07 15:57:07 -0800

    Forwarding root parameters and adding nested stack resources to root

--- packages/graphql-transformer-core/src/util/getTemplateReferences.ts
+++ packages/graphql-transformer-core/src/util/getTemplateReferences.ts
@@ -1,13 +1,15 @@
-import Template from "cloudform/types/template";
-import { Condition } from "cloudform/types/dataTypes";
-import { Fn, Refs, IntrinsicFunction } from 'cloudform'
+import Template from "cloudform-types/types/template";
 
+/**
+ * A reference map maps a resource id to all locations that reference that
+ * resource via a Fn.Ref or Fn.GetAtt intrinsic function.
+ */
 export interface ReferenceMap {
     [referenceId: string]: string[][]
 }
 /**
- * Returns a map where the key is the logical id of the reference and the
- * value is a list locations where that reference is used.
+ * Returns a map where the key is the logical id of the resource and the
+ * value is a list locations where that resource is referenced.
  * @param template The template
  */
 export function getTemplateReferences(template: Template): ReferenceMap {
