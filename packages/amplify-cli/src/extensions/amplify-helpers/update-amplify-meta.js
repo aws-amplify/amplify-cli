d845bca06225863cf7282adc388c08c27bbe688c
Sam Patzer <wizagesmax@gmail.com>
2019-01-02 11:11:30 -0800

    fix(update-amplify-meta): allow update value to be other types
    fix(update-amplify-meta): Allow update value to be other types other then dictionary or an array.

--- packages/amplify-cli/src/extensions/amplify-helpers/update-amplify-meta.js
+++ packages/amplify-cli/src/extensions/amplify-helpers/update-amplify-meta.js
@@ -13,14 +13,16 @@ function updateAwsMetaFile(filePath, category, resourceName, attribute, value, t
   } else if (!amplifyMeta[category][resourceName]) {
     amplifyMeta[category][resourceName] = {};
   }
-  if (!amplifyMeta[category][resourceName][attribute]) {
-    amplifyMeta[category][resourceName][attribute] = {};
-  }
-  if (Array.isArray(amplifyMeta[category][resourceName][attribute])) {
-    amplifyMeta[category][resourceName][attribute] = value;
-  } else {
+  
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    if (!amplifyMeta[category][resourceName][attribute]) {
+      amplifyMeta[category][resourceName][attribute] = {};
+    }
     Object.assign(amplifyMeta[category][resourceName][attribute], value);
+  } else {
+    amplifyMeta[category][resourceName][attribute] = value;
   }
+  
   if (timeStamp) {
     amplifyMeta[category][resourceName].lastPushTimeStamp = timeStamp;
   }
