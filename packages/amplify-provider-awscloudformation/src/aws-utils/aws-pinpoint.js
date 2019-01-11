9ce1d083da188cef48d3e5829fe2d3b663878236
franklin <franklin@weareinteractive.com>
2019-01-11 22:28:14 +0100

    feat(amplify-provider-awscloudformation): add pinpoint eu-central-1 region (#695)

--- packages/amplify-provider-awscloudformation/src/aws-utils/aws-pinpoint.js
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws-pinpoint.js
@@ -18,7 +18,7 @@ const serviceRegionMap = {
   'ap-southeast-1': 'us-west-2',
   'ap-southeast-2': 'us-west-2',
   'ap-northeast-1': 'us-west-2',
-  'eu-central-1': 'eu-west-1',
+  'eu-central-1': 'eu-central-1',
   'eu-west-1': 'eu-west-1',
   'eu-west-2': 'eu-west-1',
   'eu-west-3': 'eu-west-1',
