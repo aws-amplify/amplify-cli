848bbeaf1ac659734aa5de7ccc0ad2af2265209a
Paris <parismic@amazon.com>
2019-01-28 23:20:53 -0800

    Adding support for rollback when failing in the second stack update for the API migration

--- packages/amplify-provider-awscloudformation/lib/upload-appsync-files.js
+++ packages/amplify-provider-awscloudformation/lib/upload-appsync-files.js
@@ -6,6 +6,10 @@ const S3 = require('../src/aws-utils/aws-s3');
 
 const ROOT_APPSYNC_S3_KEY = 'amplify-appsync-files';
 const providerName = require('./constants').ProviderName;
+const { hashElement } = require('folder-hash');
+
+const CF_TEMPLATE_FILE_NAME = 'cloudformation-template.json';
+const PARAM_FILE_NAME = 'parameters.json';
 
 function getProjectBucket(context) {
   const projectDetails = context.amplify.getProjectDetails();
@@ -13,9 +17,27 @@ function getProjectBucket(context) {
   return projectBucket;
 }
 
-async function uploadAppSyncFiles(context, resources, defaultParams = {}) {
+/**
+ * 
+ * @param {*} template 
+ * @param {*} params 
+ */
+function getParameters(template, params) {
+  const cloudFormationTemplate = path.join(backEndDir, category, resourceName, 'build', CF_TEMPLATE_FILE_NAME);
+  const parametersOutputFilePath = path.join(backEndDir, category, resourceName, 'build', PARAM_FILE_NAME);
+}
+
+/**
+ * Updates build/parameters.json with new timestamps and then uploads the contents of the build/ directory to S3.
+ * @param {*} context 
+ * @param {*} resources 
+ * @param {*} defaultParams 
+ */
+async function uploadAppSyncFiles(context, resources, options = {}) {
   resources = resources.filter(resource => resource.service === 'AppSync');
-  const buildTimeStamp = new Date().getTime().toString();
+  const defaultParams = options.defaultParams;
+  const useDeprecatedParameters = options.useDeprecatedParameters || false;
+  // new Date().getTime().toString();
   // There can only be one appsync resource
   if (resources.length > 0) {
     const resource = resources[0];
@@ -23,10 +45,32 @@ async function uploadAppSyncFiles(context, resources, defaultParams = {}) {
     const backEndDir = context.amplify.pathManager.getBackendDirPath();
     const resourceBuildDir = path.normalize(path.join(backEndDir, category, resourceName, 'build'));
     const projectBucket = getProjectBucket(context);
-    const deploymentRootKey = `${ROOT_APPSYNC_S3_KEY}/${buildTimeStamp}`;
+    const buildDirectoryHash = await hashDirectory(resourceBuildDir);
+    const deploymentRootKey = `${ROOT_APPSYNC_S3_KEY}/${buildDirectoryHash}`;
 
+    // Read parameters.json, add timestamps, and write to build/parameters.json
+    const parametersFilePath = path.join(backEndDir, category, resourceName, PARAM_FILE_NAME);
+    let currentParameters = defaultParams;
+    if (fs.existsSync(parametersFilePath)) {
+      try {
+        const personalParams = JSON.parse(fs.readFileSync(parametersFilePath));
+        Object.assign(currentParameters, personalParams);
+      } catch (e) {
+        context.print.error(`Could not parse parameters file at "${parametersFilePath}"`);
+      }
+    }
+    if (!useDeprecatedParameters) {
+      Object.assign(currentParameters, {
+        S3DeploymentBucket: projectBucket,
+        S3DeploymentRootKey: deploymentRootKey,
+      });
+    }
+    const jsonString = JSON.stringify(currentParameters, null, 4);
+    const parametersOutputFilePath = path.join(backEndDir, category, resourceName, 'build', PARAM_FILE_NAME);
+    fs.writeFileSync(parametersOutputFilePath, jsonString, 'utf8');
+
+    // Upload build/* to S3.
     const s3Client = await new S3(context);
-
     if (!fs.existsSync(resourceBuildDir)) {
       return;
     }
@@ -41,27 +85,16 @@ async function uploadAppSyncFiles(context, resources, defaultParams = {}) {
         });
       },
     });
-
-    const parametersFilePath = path.join(backEndDir, category, resourceName, 'parameters.json');
-    let currentParameters;
-
-    if (fs.existsSync(parametersFilePath)) {
-      try {
-        currentParameters = JSON.parse(fs.readFileSync(parametersFilePath));
-      } catch (e) {
-        currentParameters = defaultParams;
-      }
-    }
-
-    Object.assign(currentParameters, {
-      S3DeploymentBucket: projectBucket,
-      S3DeploymentRootKey: deploymentRootKey,
-    });
-    Object.assign(currentParameters, defaultParams);
-    const jsonString = JSON.stringify(currentParameters, null, 4);
-    const parametersOutputFilePath = path.join(backEndDir, category, resourceName, 'build', 'parameters.json');
-    fs.writeFileSync(parametersOutputFilePath, jsonString, 'utf8');
   }
+}
+
+// Hash a directory into a unique value.
+async function hashDirectory(directory) {
+  const options = {
+      folders: { exclude: ['.*', 'node_modules', 'test_coverage'] },
+  };
+
+  return hashElement(directory, options).then(result => result.hash);
 }
 
 module.exports = {
