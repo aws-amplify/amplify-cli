06b86b7a9d63d877775b3460d78ad82136ac4fad
Yathi <511386+yuth@users.noreply.github.com>
2019-03-04 11:53:45 -0800

    fix(amplify-graphql-types-generator): cannot redefine property: flatMap
    Using the polyfill from core-js for flatMap

--- packages/amplify-graphql-types-generator/src/utilities/array.ts
+++ packages/amplify-graphql-types-generator/src/utilities/array.ts
@@ -5,10 +5,3 @@ declare global {
     flatMap<U>(callbackfn: (value: T, index: number, array: T[]) => U[] | undefined, thisArg?: any): U[];
   }
 }
-
-Object.defineProperty(Array.prototype, 'flatMap', {
-  value: function<T,U>(this: Array<T>, callbackfn: (value: T, index: number, array: T[]) => U[], thisArg?: any): U[] {
-    return [].concat.apply([], this.map(callbackfn, thisArg));
-  },
-  enumerable: false
-});
