7d11023423c034d53f070d2252df0e2e6b47e8b8
Dustin Noyes <dnnoyes@amazon.com>
2019-03-12 13:26:51 -0700

    handles legacy permission values as well as update defaults

--- packages/amplify-category-api/provider-utils/awscloudformation/service-walkthroughs/apigw-walkthrough.js
+++ packages/amplify-category-api/provider-utils/awscloudformation/service-walkthroughs/apigw-walkthrough.js
@@ -72,6 +72,7 @@ async function updateWalkthrough(context, defaultValuesFilename) {
     parameters = {};
   }
   parameters.resourceName = updateApi.resourceName;
+
   Object.assign(allDefaultValues, parameters);
   answers = { ...answers, ...parameters };
   [answers.uuid] = uuid().split('-');
@@ -214,8 +215,16 @@ async function askPrivacy(context, answers, currentPath) {
       privacy: answer.privacy,
     };
 
-    const { privacy: { auth: authPrivacy } } = currentPath || { privacy: {} };
-    const { privacy: { unauth: unauthPrivacy } } = currentPath || { privacy: {} };
+    let { privacy: { auth: authPrivacy } } = currentPath || { privacy: {} };
+    let { privacy: { unauth: unauthPrivacy } } = currentPath || { privacy: {} };
+
+    // convert legacy permissions to CRUD structure
+    if (authPrivacy && ['r', 'rw'].includes(authPrivacy)) {
+      authPrivacy = convertToCRUD(authPrivacy);
+    }
+    if (unauthPrivacy && ['r', 'rw'].includes(unauthPrivacy)) {
+      unauthPrivacy = convertToCRUD(unauthPrivacy);
+    }
 
     if (answer.privacy === 'private') {
       privacy.auth = await askReadWrite('Authenticated', context, authPrivacy);
@@ -266,20 +275,28 @@ async function askPrivacy(context, answers, currentPath) {
   }
 }
 
-async function askReadWrite(userType, context) {
-  while (true) {
-    const permissionMap = {
-      create: ['/POST'],
-      read: ['/GET'],
-      update: ['/PUT', '/PATCH'],
-      delete: ['/DELETE'],
-    };
+async function askReadWrite(userType, context, privacy) {
+  const permissionMap = {
+    create: ['/POST'],
+    read: ['/GET'],
+    update: ['/PUT', '/PATCH'],
+    delete: ['/DELETE'],
+  };
 
-    return await context.amplify.crudFlow(
-      userType,
-      permissionMap,
-    );
+  const defaults = [];
+  if (privacy) {
+    Object.values(permissionMap).forEach((el, index) => {
+      if (el.every(i => privacy.includes(i))) {
+        defaults.push(Object.keys(permissionMap)[index]);
+      }
+    });
   }
+
+  return await context.amplify.crudFlow(
+    userType,
+    permissionMap,
+    defaults,
+  );
 }
 
 async function askPaths(context, answers, currentPath) {
@@ -608,6 +625,14 @@ async function migrate(context, projectPath, resourceName) {
 //   }
 //   return authExists;
 // }
+function convertToCRUD(privacy) {
+  if (privacy === 'r') {
+    privacy = ['/GET'];
+  } else if (privacy === 'rw') {
+    privacy = ['/POST', '/GET', '/PUT', '/PATCH', '/DELETE'];
+  }
 
+  return privacy;
+}
 
 module.exports = { serviceWalkthrough, updateWalkthrough, migrate };
