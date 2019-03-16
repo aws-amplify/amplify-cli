ef842980bb57a3e108b4a1d4564086f7bd4eb573
Castro Roy <ycastro.roy@gmail.com>
2019-03-15 22:41:11 -0400

    feat(graphql-elasticsearch-transformer): export domain arn and endpoint
    #1047

--- packages/graphql-transformer-common/src/ResourceConstants.ts
+++ packages/graphql-transformer-common/src/ResourceConstants.ts
@@ -75,6 +75,8 @@ export class ResourceConstants {
         // Elasticsearch
         ElasticsearchStreamingLambdaIAMRoleArn: 'ElasticsearchStreamingLambdaIAMRoleArn',
         ElasticsearchAccessIAMRoleArn: 'ElasticsearchAccessIAMRoleArn',
+        ElasticsearchDomainArn: 'ElasticsearchDomainArn',
+        ElasticsearchDomainEndpoint: 'ElasticsearchDomainEndpoint',
 
         // Auth
         AuthCognitoUserPoolIdOutput: 'AuthCognitoUserPoolIdOutput',
