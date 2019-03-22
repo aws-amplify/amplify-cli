afeb8606972cb145a8a660f3d16c19006c189d0d
Dustin Noyes <dnnoyes@amazon.com>
2019-03-21 20:54:15 -0700

    removing unneeded defaults

--- packages/amplify-category-auth/provider-utils/awscloudformation/assets/cognito-defaults.js
+++ packages/amplify-category-auth/provider-utils/awscloudformation/assets/cognito-defaults.js
@@ -73,8 +73,6 @@ const userPoolDefaults = projectName => ({
   userpoolClientLogPolicy: `${projectName}_userpoolclient_lambda_log_policy`,
   userpoolClientLambdaPolicy: `${projectName}_userpoolclient_lambda_iam_policy`,
   userpoolClientSetAttributes: false,
-  AllowedOAuthFlows: oAuthFlows.map(i => i.value),
-  AllowedOAuthScopes: oAuthScopes.map(i => i.value),
 });
 
 const withSocialDefaults = projectName => ({
