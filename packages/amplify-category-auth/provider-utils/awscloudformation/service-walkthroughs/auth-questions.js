bdcdf19d7d99cd5091f7a375857bca0753839b9a
Dustin Noyes <dnnoyes@amazon.com>
2019-04-01 08:57:17 -0700

    unit tests

--- packages/amplify-category-auth/provider-utils/awscloudformation/service-walkthroughs/auth-questions.js
+++ packages/amplify-category-auth/provider-utils/awscloudformation/service-walkthroughs/auth-questions.js
@@ -26,7 +26,7 @@ async function serviceWalkthrough(
     const { resourceName, authProvidersUserPool, hostedUIProviderMeta } = context.updatingAuth;
     const { hostedUIProviderCreds } = context.amplify.loadEnvResourceParameters(context, 'auth', resourceName);
     /* eslint-disable */
-    const oAuthCreds = parseOAuthCreds(context, authProvidersUserPool, hostedUIProviderMeta, resourceName, hostedUIProviderCreds);
+    const oAuthCreds = parseOAuthCreds(authProvidersUserPool, hostedUIProviderMeta, hostedUIProviderCreds);
     /* eslint-enable */
     context.updatingAuth = Object.assign(context.updatingAuth, oAuthCreds);
   }
@@ -315,6 +315,8 @@ function structureoAuthMetaData(coreAnswers, context, defaults, amplify) {
       LogoutURLs,
     });
   }
+
+  return coreAnswers;
 }
 
 /*
@@ -330,7 +332,7 @@ function parseOAuthMetaData(previousAnswers) {
 /*
   Deserialize oAuthCredentials for CLI update flow
 */
-function parseOAuthCreds(context, providers, metadata, resourceName, envCreds) {
+function parseOAuthCreds(providers, metadata, envCreds) {
   const providerKeys = {};
   try {
     const parsedMetaData = JSON.parse(metadata);
@@ -363,4 +365,9 @@ function filterInput(input, updateFlow) {
   return false;
 }
 
-module.exports = { serviceWalkthrough, userPoolProviders, parseOAuthCreds };
+module.exports = {
+  serviceWalkthrough,
+  userPoolProviders,
+  parseOAuthCreds,
+  structureoAuthMetaData,
+};
