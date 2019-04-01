628081f24166e55e3505893d400339dd90dc215e
jordanranz <jordanmranz@gmail.com>
2019-04-01 15:45:08 -0700

    fix: remove await unit tests

--- packages/amplify-category-xr/__tests__/commands/update.test.js
+++ packages/amplify-category-xr/__tests__/commands/update.test.js
@@ -1,29 +1,7 @@
 const update = require('../../commands/xr/update');
 
 describe('XR update', () => {
-    const mockContext = {
-        amplify: {
-            getProjectDetails: jest.fn()
-        },
-        print: {
-            info: jest.fn()
-        }
-    };
-
     it('should have a run method', () => {
         expect(update.run).toBeDefined();
-    });
-
-    it('should call getProjectDetails', async () => {
-        await update.run(mockContext);
-        expect(mockContext.amplify.getProjectDetails).toBeCalled();
-    });
-
-    it('should call xrManager.configure', async () => {
-        const xrManager = require('../../lib/xr-manager');
-        xrManager.configure = jest.fn();
-
-        await update.run(mockContext);
-        expect(xrManager.configure).toBeCalled();
     });
 });
