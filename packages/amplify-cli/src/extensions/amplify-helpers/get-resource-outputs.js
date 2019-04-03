b193136fc9cbcc4e572dd9c21eae2d98b7e816e5
Ghosh <kaustavg@amazon.com>
2019-04-03 13:11:57 -0700

    fix: fix #1201
    fix #1201

--- packages/amplify-cli/src/extensions/amplify-helpers/get-resource-outputs.js
+++ packages/amplify-cli/src/extensions/amplify-helpers/get-resource-outputs.js
@@ -15,48 +15,52 @@ function getResourceOutputs(amplifyMeta) {
     serviceResourceMapping: {},
   };
 
-  Object.keys(amplifyMeta.providers).forEach((provider) => {
-    outputsByProvider[provider] = {};
-    outputsByProvider[provider].metadata = amplifyMeta.providers[provider] || {};
-    outputsByProvider[provider].serviceResourceMapping = {};
-  });
-
-  Object.keys(amplifyMeta).forEach((category) => {
-    const categoryMeta = amplifyMeta[category];
-    Object.keys(categoryMeta).forEach((resourceName) => {
-      const resourceMeta = categoryMeta[resourceName];
-      if (resourceMeta.output && resourceMeta.lastPushTimeStamp) {
-        const { providerPlugin } = resourceMeta;
-        if (!outputsByProvider[providerPlugin]) {
-          outputsByProvider[providerPlugin] = {
-            metadata: {},
-            serviceResourceMapping: {},
-          };
-        }
-        if (!outputsByProvider[providerPlugin].serviceResourceMapping[resourceMeta.service]) {
-          outputsByProvider[providerPlugin].serviceResourceMapping[resourceMeta.service] = [];
-        }
-        /*eslint-disable*/
-        outputsByProvider[providerPlugin].serviceResourceMapping[resourceMeta.service].push(resourceMeta);
-         /* eslint-enable */
-        if (!outputsByCategory[category]) {
-          outputsByCategory[category] = {};
-        }
-        if (resourceMeta.service) {
-          resourceMeta.output.service = resourceMeta.service;
-        }
-        outputsByCategory[category][resourceName] = resourceMeta.output;
-
-        // for frontend configuration file generation
-        if (!outputsForFrontend.serviceResourceMapping[resourceMeta.service]) {
-          outputsForFrontend.serviceResourceMapping[resourceMeta.service] = [];
-        }
-
-        resourceMeta.resourceName = resourceName;
-        outputsForFrontend.serviceResourceMapping[resourceMeta.service].push(resourceMeta);
-      }
+  if (amplifyMeta.providers) {
+    Object.keys(amplifyMeta.providers).forEach((provider) => {
+      outputsByProvider[provider] = {};
+      outputsByProvider[provider].metadata = amplifyMeta.providers[provider] || {};
+      outputsByProvider[provider].serviceResourceMapping = {};
     });
-  });
+  }
+
+  if (amplifyMeta) {
+    Object.keys(amplifyMeta).forEach((category) => {
+      const categoryMeta = amplifyMeta[category];
+      Object.keys(categoryMeta).forEach((resourceName) => {
+        const resourceMeta = categoryMeta[resourceName];
+        if (resourceMeta.output && resourceMeta.lastPushTimeStamp) {
+          const { providerPlugin } = resourceMeta;
+          if (!outputsByProvider[providerPlugin]) {
+            outputsByProvider[providerPlugin] = {
+              metadata: {},
+              serviceResourceMapping: {},
+            };
+          }
+          if (!outputsByProvider[providerPlugin].serviceResourceMapping[resourceMeta.service]) {
+            outputsByProvider[providerPlugin].serviceResourceMapping[resourceMeta.service] = [];
+          }
+          /*eslint-disable*/
+          outputsByProvider[providerPlugin].serviceResourceMapping[resourceMeta.service].push(resourceMeta);
+           /* eslint-enable */
+          if (!outputsByCategory[category]) {
+            outputsByCategory[category] = {};
+          }
+          if (resourceMeta.service) {
+            resourceMeta.output.service = resourceMeta.service;
+          }
+          outputsByCategory[category][resourceName] = resourceMeta.output;
+
+          // for frontend configuration file generation
+          if (!outputsForFrontend.serviceResourceMapping[resourceMeta.service]) {
+            outputsForFrontend.serviceResourceMapping[resourceMeta.service] = [];
+          }
+
+          resourceMeta.resourceName = resourceName;
+          outputsForFrontend.serviceResourceMapping[resourceMeta.service].push(resourceMeta);
+        }
+      });
+    });
+  }
 
   if (outputsByProvider.awscloudformation) {
     outputsForFrontend.metadata = outputsByProvider.awscloudformation.metadata;
