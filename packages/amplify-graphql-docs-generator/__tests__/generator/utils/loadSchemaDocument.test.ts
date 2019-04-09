029c388ac0242428a47908fecad4025158b83165
Yathi <511386+yuth@users.noreply.github.com>
2019-04-09 10:04:02 -0700

    Add more unit tests

--- packages/amplify-graphql-docs-generator/__tests__/generator/utils/loadSchemaDocument.test.ts
+++ packages/amplify-graphql-docs-generator/__tests__/generator/utils/loadSchemaDocument.test.ts
@@ -15,33 +15,73 @@ jest.mock('fs')
 jest.mock('graphql')
 
 describe('loadSchmeaDocument', () => {
-  const mockIntrospectionSchema = {
-    data: {
-      'foo': 'bar'
-    },
-  }
   describe('introspection schema', () => {
     beforeEach(() => {
       jest.resetAllMocks()
-      readFileSync.mockReturnValue(JSON.stringify(mockIntrospectionSchema))
     })
     it('should build client schmea if the schema file ends with .json', () => {
+      const mockIntrospectionSchema = {
+        data: {
+          foo: 'bar',
+        },
+      }
+      readFileSync.mockReturnValue(JSON.stringify(mockIntrospectionSchema))
       loadSchemaDocument('foo.json')
       expect(buildClientSchema).toHaveBeenCalledWith(mockIntrospectionSchema.data)
     })
     it('should build schema client if the there is __schema', () => {
       const mockIntrospectionSchema = {
         __schema: {
-          'foo': 'bar'
+          foo: 'bar',
         },
       }
       readFileSync.mockReturnValue(JSON.stringify(mockIntrospectionSchema))
       loadSchemaDocument('foo.json')
       expect(buildClientSchema).toHaveBeenCalledWith(mockIntrospectionSchema)
     })
+
+    it("should throw error if the introspection schema doesn't have data", () => {
+      const mockIntrospectionSchema = {}
+      readFileSync.mockReturnValue(JSON.stringify(mockIntrospectionSchema))
+      expect(() => loadSchemaDocument('foo.json')).toThrowError(
+        /GraphQL schema file should contain a valid GraphQL introspection query result/
+      )
+    })
   })
 
   describe('SDL Schema', () => {
-    
+    const mockSDLSchema = `type Foo {
+      bar: String
+    }`
+    const mockAuthDirective = `MOCK_AUTH_DIRECTIVES`
+
+    const mockAst = 'MOCK_AST'
+    const mockConcatenateAst = 'MOCK_CONCATENATE_AST'
+
+
+    beforeEach(() => {
+      jest.resetAllMocks()
+      readFileSync.mockImplementation((path) => {
+        if (path.endsWith('aws_auth_directives.graphql')) {
+          return mockAuthDirective
+        }
+        return mockSDLSchema
+      })
+  
+    })
+
+    it('should load SDL Schema', () => {
+      parse.mockReturnValue(mockAst)
+      concatAST.mockReturnValue(mockConcatenateAst)
+      loadSchemaDocument('foo.graphql')
+
+      expect(readFileSync).toHaveBeenCalledTimes(2)
+      expect(readFileSync.mock.calls[0][0]).toEqual('foo.graphql')
+
+      expect(parse).toHaveBeenCalledTimes(2)
+      
+      expect(concatAST).toHaveBeenCalled()
+      expect(buildASTSchema).toHaveBeenCalledWith(mockConcatenateAst)
+    })
   })
 })
