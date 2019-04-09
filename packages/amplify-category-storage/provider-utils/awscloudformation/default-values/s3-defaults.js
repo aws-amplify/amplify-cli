60a986070ccbb04b194627ccbe40069b18557db3
Dustin Noyes <dnnoyes@amazon.com>
2019-04-08 20:05:42 -0700

    PR suggestions - moving defaults from defaults.js file to parameter defaults in cf template

--- packages/amplify-category-storage/provider-utils/awscloudformation/default-values/s3-defaults.js
+++ packages/amplify-category-storage/provider-utils/awscloudformation/default-values/s3-defaults.js
@@ -24,10 +24,6 @@ const getAllDefaults = (project) => {
     storageAccess: 'auth',
     selectedGuestPermissions: ['s3:GetObject', 's3:ListBucket'],
     selectedAuthenticatedPermissions: ['s3:GetObject', 's3:ListBucket'],
-
-    s3PermissionsGuestPublic: 'DISALLOW',
-    s3PermissionsGuestUploads: 'DISALLOW',
-    GuestAllowList: 'DISALLOW',
   };
 
   return defaults;
