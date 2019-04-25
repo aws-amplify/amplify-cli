31242d5aa47d0b67f417bb681a3a2537f490e306
UnleashedMind <michaelzhou.net@gmail.com>
2019-04-25 16:57:18 -0700

    updates

--- packages/amplify-category-hosting/__tests__/index.test.js
+++ packages/amplify-category-hosting/__tests__/index.test.js
@@ -1,4 +1,5 @@
 jest.mock('promise-sequential');
+jest.mock('../lib/category-manager');
 
 const inquirer = require('inquirer');
 const mockirer = require('mockirer');
@@ -9,9 +10,16 @@ const categoryManager = require('../lib/category-manager');
 const indexModule = require('../index');
 
 describe('index', () => {
-    const mockContext = {}; 
-    const S3AndCloudFront = 'S3AndCloudFront'; 
-    const AnotherHostingService = 'AnotherHostingService';
+    const mockContext = {
+        print: {
+            info: jest.fn(),
+            warning: jest.fn(),
+            error: jest.fn(),
+            success: jest.fn()
+        }
+    }; 
+    const S3ANDCLOUDFRONT = 'S3AndCloudFront'; 
+    const ANOTHERSERVICE = 'AnotherHostingService';
 
     const mockAvailableServices = []; 
     let mockDisabledServices = []; 
@@ -29,8 +37,6 @@ describe('index', () => {
                 disabledServices: mockDisabledServices
             }
         }); 
-        categoryManager.runServiceAction = jest.fn(); 
-        categoryManager.migrate = jest.fn(); 
     }); 
 
     beforeEach(() => {
@@ -44,128 +50,108 @@ describe('index', () => {
     });
 
     test('add', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockAvailableServices.push(AnotherHostingService);
-        mockDisabledServices.push(S3AndCloudFront);
-        mockDisabledServices.push(AnotherHostingService);
-        mockAnswers.selectedServices.push(S3AndCloudFront);
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockAvailableServices.push(ANOTHERSERVICE);
+        mockDisabledServices.push(S3ANDCLOUDFRONT);
+        mockDisabledServices.push(ANOTHERSERVICE);
+        mockAnswers.selectedServices.push(S3ANDCLOUDFRONT);
         mockirer(inquirer, mockAnswers); 
         await indexModule.add(mockContext); 
         expect(sequential).toBeCalled(); 
     });
 
     test('add, only one disabled serivce', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockDisabledServices.push(S3AndCloudFront);
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockDisabledServices.push(S3ANDCLOUDFRONT);
         await indexModule.add(mockContext); 
         expect(categoryManager.runServiceAction).toBeCalled(); 
         expect(categoryManager.runServiceAction.mock.calls[0][0]).toBe(mockContext); 
-        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3AndCloudFront); 
+        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3ANDCLOUDFRONT); 
         expect(categoryManager.runServiceAction.mock.calls[0][2]).toBe('enable'); 
     });
 
     test('add, no disabled service', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockEnabledServices.push(S3AndCloudFront);
-        try{
-            await indexModule.add(mockContext); 
-        }catch(err){
-            expect(sequential).not.toBeCalled(); 
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockEnabledServices.push(S3ANDCLOUDFRONT);
+
+        await expect(indexModule.add(mockContext)).rejects.toThrow();  
+        expect(sequential).not.toBeCalled(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('add, no available service', async () => {
-        try{
-            await indexModule.add(mockContext); 
-        }catch(err){
-            expect(sequential).not.toBeCalled(); 
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+        await expect(indexModule.add(mockContext)).rejects.toThrow();
+        expect(sequential).not.toBeCalled(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('configure', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockAvailableServices.push(AnotherHostingService);
-        mockEnabledServices.push(S3AndCloudFront);
-        mockEnabledServices.push(AnotherHostingService);
-        mockAnswers.selectedServices.push(S3AndCloudFront);
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockAvailableServices.push(ANOTHERSERVICE);
+        mockEnabledServices.push(S3ANDCLOUDFRONT);
+        mockEnabledServices.push(ANOTHERSERVICE);
+        mockAnswers.selectedServices.push(S3ANDCLOUDFRONT);
         mockirer(inquirer, mockAnswers); 
         await indexModule.configure(mockContext); 
         expect(sequential).toBeCalled(); 
     });
 
     test('configure, only one enabled serivce', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockEnabledServices.push(S3AndCloudFront);
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockEnabledServices.push(S3ANDCLOUDFRONT);
         await indexModule.configure(mockContext); 
         expect(categoryManager.runServiceAction).toBeCalled(); 
         expect(categoryManager.runServiceAction.mock.calls[0][0]).toBe(mockContext); 
-        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3AndCloudFront); 
+        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3ANDCLOUDFRONT); 
         expect(categoryManager.runServiceAction.mock.calls[0][2]).toBe('configure'); 
     });
 
     test('configure, no enabled service', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        try{
-            await indexModule.configure(mockContext); 
-        }catch(err){
-            expect(sequential).not.toBeCalled(); 
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        
+        await expect(indexModule.configure(mockContext)).rejects.toThrow();
+        expect(sequential).not.toBeCalled(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('configure, no available service', async () => {
-        try{
-            await indexModule.configure(mockContext); 
-        }catch(err){
-            expect(sequential).not.toBeCalled(); 
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+        await expect(indexModule.configure(mockContext)).rejects.toThrow(); 
+        expect(sequential).not.toBeCalled(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('publish', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockEnabledServices.push(S3AndCloudFront);
-        await indexModule.publish(mockContext, S3AndCloudFront); 
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockEnabledServices.push(S3ANDCLOUDFRONT);
+        await indexModule.publish(mockContext, S3ANDCLOUDFRONT); 
         expect(categoryManager.runServiceAction).toBeCalled(); 
         expect(categoryManager.runServiceAction.mock.calls[0][0]).toBe(mockContext); 
-        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3AndCloudFront); 
+        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3ANDCLOUDFRONT); 
         expect(categoryManager.runServiceAction.mock.calls[0][2]).toBe('publish'); 
     });
 
-    test('publish, expected service no enabled', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockAvailableServices.push(AnotherHostingService);
-        mockEnabledServices.push(AnotherHostingService);
-        mockDisabledServices.push(S3AndCloudFront);
-        try{
-            await indexModule.publish(mockContext, S3AndCloudFront); 
-        }catch(err){
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+    test('publish, expected service no enabled', () => {
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockAvailableServices.push(ANOTHERSERVICE);
+        mockEnabledServices.push(ANOTHERSERVICE);
+        mockDisabledServices.push(S3ANDCLOUDFRONT);
+       
+        expect(()=>{indexModule.publish(mockContext, S3ANDCLOUDFRONT)}).toThrow();  
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
-    test('publish, no enabled service', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        try{
-            await indexModule.publish(mockContext, S3AndCloudFront); 
-        }catch(err){
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+    test('publish, no enabled service', () => {
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+
+        expect(()=>{indexModule.publish(mockContext, S3ANDCLOUDFRONT)}).toThrow(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('console', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockAvailableServices.push(AnotherHostingService);
-        mockEnabledServices.push(S3AndCloudFront);
-        mockEnabledServices.push(AnotherHostingService);
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockAvailableServices.push(ANOTHERSERVICE);
+        mockEnabledServices.push(S3ANDCLOUDFRONT);
+        mockEnabledServices.push(ANOTHERSERVICE);
         mockAnswers.selectedService = mockEnabledServices[0];
         mockirer(inquirer, mockAnswers); 
         await indexModule.console(mockContext); 
@@ -176,35 +162,28 @@ describe('index', () => {
     });
 
     test('console, only one enabled serivce', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockEnabledServices.push(S3AndCloudFront);
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockEnabledServices.push(S3ANDCLOUDFRONT);
         await indexModule.console(mockContext); 
         expect(categoryManager.runServiceAction).toBeCalled(); 
         expect(categoryManager.runServiceAction.mock.calls[0][0]).toBe(mockContext); 
-        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3AndCloudFront); 
+        expect(categoryManager.runServiceAction.mock.calls[0][1]).toBe(S3ANDCLOUDFRONT); 
         expect(categoryManager.runServiceAction.mock.calls[0][2]).toBe('console'); 
     });
 
     test('console, no enabled service', async () => {
-        mockAvailableServices.push(S3AndCloudFront);
-        mockDisabledServices.push(S3AndCloudFront);
-        try{
-            await indexModule.console(mockContext); 
-        }catch(err){
-            expect(sequential).not.toBeCalled(); 
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+        mockAvailableServices.push(S3ANDCLOUDFRONT);
+        mockDisabledServices.push(S3ANDCLOUDFRONT);
+        
+        await expect(indexModule.console(mockContext)).rejects.toThrow();
+        expect(sequential).not.toBeCalled(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('console, no available service', async () => {
-        try{
-            await indexModule.console(mockContext); 
-        }catch(err){
-            expect(sequential).not.toBeCalled(); 
-            expect(categoryManager.runServiceAction).not.toBeCalled(); 
-            expect(err).toBeDefined();
-        }
+        await expect(indexModule.console(mockContext)).rejects.toThrow();  
+        expect(sequential).not.toBeCalled(); 
+        expect(categoryManager.runServiceAction).not.toBeCalled(); 
     });
 
     test('migrate', async () => {
