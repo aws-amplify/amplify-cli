9c5ae266da487b5e5d1d90838c045d46caca4b6a
Dustin Noyes <dnnoyes@amazon.com>
2019-04-29 18:55:41 -0700

    fix(amplify-category-auth): more linting fixes

--- packages/amplify-category-function/provider-utils/awscloudformation/function-template-dir/trigger-module.js
+++ packages/amplify-category-function/provider-utils/awscloudformation/function-template-dir/trigger-module.js
@@ -1,3 +1,4 @@
 exports.handler = async (event, context, callback) => {
- // insert code to be executed by your lambda trigger
+  // insert code to be executed by your lambda trigger
+  callback(null, 'hello world');
 };
