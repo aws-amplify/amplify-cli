aws-amplify/amplify-cli

commit df1712b00427792bcce34adf7027698afd8e6841

feat(graphql-dynamodb-transformer): always output stream arn
solve #987 and fix #980

packages/graphql-elasticsearch-transformer/src/resources.ts
@@ -165,7 +165,12 @@ export class ResourceFactory {
         return new Lambda.EventSourceMapping({
             BatchSize: 1,
             Enabled: true,
-            EventSourceArn: Fn.GetAtt(ModelResourceIDs.ModelTableResourceID(typeName), 'StreamArn'),
+            EventSourceArn: Fn.ImportValue(
+                Fn.Join(
+                    ':',
+                    [Fn.Ref(ResourceConstants.PARAMETERS.AppSyncApiId), "GetAtt", ModelResourceIDs.ModelTableResourceID(typeName), "StreamArn"]
+                )
+            ),
             FunctionName: Fn.GetAtt(ResourceConstants.RESOURCES.ElasticsearchStreamingLambdaFunctionLogicalID, 'Arn'),
             StartingPosition: 'LATEST'
         }).dependsOn([

packages/graphql-dynamodb-transformer/src/resources.ts
@@ -115,6 +115,16 @@ export class ResourceFactory {
         }
     }
 
+    public makeTableStreamArnOutput(resourceId: string): Output {
+        return {
+            Description: "Your DynamoDB table StreamArn.",
+            Value: Fn.GetAtt(resourceId, "StreamArn"),
+            Export: {
+                Name: Fn.Join(':', [Fn.Ref(ResourceConstants.PARAMETERS.AppSyncApiId), "GetAtt", resourceId, "StreamArn"])
+            }
+        }
+    }
+
     public makeDataSourceOutput(resourceId: string): Output {
         return {
             Description: "Your model DataSource name.",

packages/graphql-dynamodb-transformer/src/DynamoDBModelTransformer.ts
@@ -143,6 +143,10 @@ export class DynamoDBModelTransformer extends Transformer {
             this.resources.makeDynamoDBDataSource(tableLogicalID, iamRoleLogicalID, typeName)
         )
         ctx.setOutput(
+            ModelResourceIDs.ModelTableStreamArn(typeName),
+            this.resources.makeTableStreamArnOutput(tableLogicalID)
+        )
+        ctx.setOutput(
             `GetAtt${dataSourceRoleLogicalID}Name`,
             this.resources.makeDataSourceOutput(dataSourceRoleLogicalID)
         )

packages/graphql-transformer-common/src/ModelResourceIDs.ts
@@ -6,6 +6,9 @@ export class ModelResourceIDs {
     static ModelTableResourceID(typeName: string): string {
         return `${typeName}Table`
     }
+    static ModelTableStreamArn(typeName: string): string {
+        return `${typeName}TableStreamArn`
+    }
     static ModelTableDataSourceID(typeName: string): string {
         return `${typeName}DataSource`
     }

commit 8c8046214d1abbfd2f3752fec8ff6c1d3dd104ce

fix(graphql-elasticsearch-transformer): use Fn::GetAtt for StreamArn (#1494)
Fixing bug related to DynamoDB Stream Arn Outputs introduced in commit https://github.com/aws-amplify/amplify-cli/commit/df1712b00427792bcce34adf7027698afd8e6841#diff-22e2a5351fb3f897025bc1e45811acb5R168

packages/graphql-elasticsearch-transformer/src/resources.ts
@@ -165,12 +165,7 @@ export class ResourceFactory {
         return new Lambda.EventSourceMapping({
             BatchSize: 1,
             Enabled: true,
-            EventSourceArn: Fn.ImportValue(
-                Fn.Join(
-                    ':',
-                    [Fn.Ref(ResourceConstants.PARAMETERS.AppSyncApiId), "GetAtt", ModelResourceIDs.ModelTableResourceID(typeName), "StreamArn"]
-                )
-            ),
+            EventSourceArn: Fn.GetAtt(ModelResourceIDs.ModelTableResourceID(typeName), 'StreamArn'),
             FunctionName: Fn.GetAtt(ResourceConstants.RESOURCES.ElasticsearchStreamingLambdaFunctionLogicalID, 'Arn'),
             StartingPosition: 'LATEST'
         }).dependsOn([