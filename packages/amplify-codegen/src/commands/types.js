e2ea11e21ea7bbc5415933aab2ec26bae689278b
Yathiraj <511386+yuth@users.noreply.github.com>
2019-06-10 15:02:29 -0700

    refactor(amplify-codegen): skip types gen early (#1640)
    Add checks before running types generator. If the codegen config doesn't require generation of types, bail before doing any other operation
    
    partial fix #1585

--- packages/amplify-codegen/src/commands/types.js
+++ packages/amplify-codegen/src/commands/types.js
@@ -22,19 +22,20 @@ async function generateTypes(context, forceDownloadSchema) {
     const { projectPath } = context.amplify.getEnvInfo();
 
     projects.forEach(async (cfg) => {
-      const excludes = cfg.excludes.map(pattern => `!${pattern}`);
+      const { generatedFileName } = cfg.amplifyExtension || {};
       const includeFiles = cfg.includes;
+      if (!generatedFileName || (generatedFileName === '') || (includeFiles.length === 0)) {
+        return;
+      }
+
+      const excludes = cfg.excludes.map(pattern => `!${pattern}`);
       const queries = glob.sync([...includeFiles, ...excludes], {
         cwd: projectPath,
         absolute: true,
       });
       const schemaPath = path.join(projectPath, cfg.schema);
-      const { generatedFileName } = cfg.amplifyExtension;
       const target = cfg.amplifyExtension.codeGenTarget;
 
-      if (!generatedFileName || generatedFileName === '') {
-        return;
-      }
       const outputPath = path.join(projectPath, generatedFileName);
       if (forceDownloadSchema || jetpack.exists(schemaPath) !== 'file') {
         await downloadIntrospectionSchemaWithProgress(
