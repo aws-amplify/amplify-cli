d41b495c77e6dda6101bd4f99e2bdfbc0f42fd96
Yathi <511386+yuth@users.noreply.github.com>
2019-06-12 10:52:56 -0700

    fix(amplify-category-interactions): call updateMetaAfterAdd only once
    When adding interaction category, updateMateAfterAdd got called multiple times and if a resource
    already exists with the name, no data is added to amplify-meta. Update interaction category to call
    the method only once. Update the updateMetaAfterAdd to throw error when called multiple times for
    the same resource. Also added integration test to ensure interaction add works
    
    fix #1621

--- packages/amplify-e2e-tests/src/utils/sdk-calls.ts
+++ packages/amplify-e2e-tests/src/utils/sdk-calls.ts
@@ -1,17 +1,18 @@
 import * as AWS from 'aws-sdk';
 
-
 const getUserPool = async (userpoolId, region) => {
   AWS.config.update({ region });
   const CognitoIdentityServiceProvider = AWS.CognitoIdentityServiceProvider;
   let res;
   try {
-    res = await new CognitoIdentityServiceProvider().describeUserPool({UserPoolId: userpoolId}).promise();
+    res = await new CognitoIdentityServiceProvider()
+      .describeUserPool({ UserPoolId: userpoolId })
+      .promise();
   } catch (e) {
     console.log(e);
   }
   return res;
-}
+};
 
 const getUserPoolClients = async (userpoolId, region) => {
   AWS.config.update({ region });
@@ -19,18 +20,25 @@ const getUserPoolClients = async (userpoolId, region) => {
   const provider = new CognitoIdentityServiceProvider();
   let res = [];
   try {
-    let clients = await provider.listUserPoolClients({UserPoolId: userpoolId}).promise();
+    let clients = await provider.listUserPoolClients({ UserPoolId: userpoolId }).promise();
     for (let i = 0; i < clients.UserPoolClients.length; i++) {
-      let clientData = await provider.describeUserPoolClient({
-        UserPoolId: userpoolId,
-        ClientId: clients.UserPoolClients[i].ClientId
-      }).promise();
+      let clientData = await provider
+        .describeUserPoolClient({
+          UserPoolId: userpoolId,
+          ClientId: clients.UserPoolClients[i].ClientId
+        })
+        .promise();
       res.push(clientData);
     }
   } catch (e) {
     console.log(e);
   }
   return res;
-}
+};
 
-export { getUserPool, getUserPoolClients }
+const getBot = async (botName: string, region: string) => {
+  const service = new AWS.LexModelBuildingService({ region });
+  return await service.getBot({ name: botName, versionOrAlias: '$LATEST' }).promise();
+};
+
+export { getUserPool, getUserPoolClients, getBot };
