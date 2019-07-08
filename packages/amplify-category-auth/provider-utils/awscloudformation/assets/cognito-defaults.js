40758f626b519e9de147e498d9eb4ace87930dc4
Dustin Noyes <dnnoyes@amazon.com>
2019-07-08 08:40:41 -0700

    fix: removing bad dependency changes

--- packages/amplify-category-auth/provider-utils/awscloudformation/assets/cognito-defaults.js
+++ packages/amplify-category-auth/provider-utils/awscloudformation/assets/cognito-defaults.js
@@ -42,9 +42,6 @@ const userPoolDefaults = (projectName) => {
     autoVerifiedAttributes: ['email'],
     mfaConfiguration: 'OFF',
     mfaTypes: ['SMS Text Message'],
-    // roleName: `${projectNameTruncated}_sns-role`,
-    // roleExternalId: `${projectNameTruncated}_role_external_id`,
-    // policyName: `${projectNameTruncated}-sns-policy`,
     smsAuthenticationMessage: 'Your authentication code is {####}',
     smsVerificationMessage: 'Your verification code is {####}',
     emailVerificationSubject: 'Your verification code',
@@ -60,12 +57,7 @@ const userPoolDefaults = (projectName) => {
     userpoolClientWriteAttributes: ['email'],
     userpoolClientReadAttributes: ['email'],
     mfaLambdaRole: `${projectNameTruncated}_totp_lambda_role`,
-    // mfaLambdaLogPolicy: `${projectNameTruncated}_totp_lambda_log_policy`,
-    // mfaPassRolePolicy: `${projectNameTruncated}_totp_pass_role_policy`,
-    // mfaLambdaIAMPolicy: `${projectNameTruncated}_totp_lambda_iam_policy`,
     userpoolClientLambdaRole: `${projectNameTruncated}_userpoolclient_lambda_role`,
-    // userpoolClientLogPolicy: `${projectNameTruncated}_userpoolclient_lambda_log_policy`,
-    // userpoolClientLambdaPolicy: `${projectNameTruncated}_userpoolclient_lambda_iam_policy`,
     userpoolClientSetAttributes: false,
   });
 };
@@ -82,11 +74,7 @@ const identityPoolDefaults = (projectName) => {
   return ({
     identityPoolName: `${projectName}_identitypool_${sharedId}`,
     allowUnauthenticatedIdentities: booleanOptions.find(b => b.value === false).value,
-    // lambdaLogPolicy: `${projectNameTruncated}_lambda_log_policy`,
     openIdLambdaRoleName: `${projectNameTruncated}_openid_lambda_role`,
-    // openIdRolePolicy: `${projectNameTruncated}_openid_pass_role_policy`,
-    // openIdLambdaIAMPolicy: `${projectNameTruncated}_openid_lambda_iam_policy`,
-    // openIdLogPolicy: `${projectNameTruncated}_openid_lambda_log_policy`,
   });
 };
 
