522bc060674a6dfc753f2eabe24bc2359800b2c0
Dustin Noyes <dnnoyes@amazon.com>
2019-07-31 21:08:09 -0700

    updated env init

--- packages/amplify-category-function/provider-utils/awscloudformation/index.js
+++ packages/amplify-category-function/provider-utils/awscloudformation/index.js
@@ -442,10 +442,7 @@ async function initTriggerEnvs(context, resourceParams, providerPlugin, envParam
     const parentResourceParams = providerPlugin
       .loadResourceParameters(context, resourceParams.parentStack, resourceParams.parentResource);
     const triggers = typeof parentResourceParams.triggers === 'string' ? JSON.parse(parentResourceParams.triggers) : parentResourceParams.triggers;
-    const parentKeys = Object.keys(parentResourceParams);
-    const parentValues = Object.values(parentResourceParams);
-    const index = parentValues.indexOf(resourceParams.resourceName);
-    const currentTrigger = parentKeys[index];
+    const currentTrigger = resourceParams.resourceName.replace(parentResourceParams.resourceName, '');
     const currentEnvVariables = context.amplify.loadEnvResourceParameters(context, 'function', resourceParams.resourceName);
     const triggerPath = `${__dirname}/../../../amplify-category-${resourceParams.parentStack}/provider-utils/${srvcMetaData.provider}/triggers/${currentTrigger}`;
     if (context.commandName !== 'checkout') {
