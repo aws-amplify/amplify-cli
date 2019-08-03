e594aadb59c6dbd4fba4c65acfdb8958336e9415
Yathi <511386+yuth@users.noreply.github.com>
2019-08-03 11:28:45 -0700

    Run codegen add workflow while push only if codegen is not configured

--- packages/amplify-codegen/src/utils/isCodegenConfigured.js
+++ packages/amplify-codegen/src/utils/isCodegenConfigured.js
@@ -1,8 +1,13 @@
 const loadConfig = require('../codegen-config');
 
-function isCodegenConfigured(context) {
+function isCodegenConfigured(context, apiName) {
   const config = loadConfig(context);
   const projects = config.getProjects();
+  if (apiName) {
+    const isConfigured = projects.find(p => p.projectName === apiName);
+    return isConfigured !== undefined;
+  }
+
   return projects.length > 0;
 }
 
