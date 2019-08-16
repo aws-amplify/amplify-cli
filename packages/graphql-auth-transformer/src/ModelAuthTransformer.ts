8b6e89c4383ae4a7224a2fcb0e7b1999379f8730
SwaySway <josurui@amazon.com>
2019-08-16 11:07:10 -0700

    feat(graphql-auth-transformer): protecting subscriptions
    adding protection on subscriptions
    
    BREAKING CHANGE: If an owner is used in the auth directive it will either be a requirement if it's
    the only rule or an optional input if used with other rules
    
    re #1766 #1043

--- packages/graphql-auth-transformer/src/ModelAuthTransformer.ts
+++ packages/graphql-auth-transformer/src/ModelAuthTransformer.ts
@@ -1093,7 +1093,7 @@ All @auth directives used on field definitions are performed when the field is r
         if ( rules.length > 1 && hasOwner ) {
             this.addSubscriptionOwnerArgument(ctx, resolver, false)
         }
-        }
+    }
 
     private addSubscriptionOwnerArgument(ctx: TransformerContext, resolver: Resolver, makeNonNull: boolean = false) {
         let subscription = ctx.getSubscription();
