b82fbb11320d424ac73a03fc13f295604ed0ad16
Attila Hajdrik <attila@eyedea.hu>
2019-08-19 17:49:21 +0200

    fix: #429 - Editor hanging bug

--- packages/amplify-cli/src/extensions/amplify-helpers/open-editor.js
+++ packages/amplify-cli/src/extensions/amplify-helpers/open-editor.js
@@ -1,5 +1,6 @@
+const childProcess = require('child_process');
 const inquirer = require('inquirer');
-const openInEditor = require('open-in-editor');
+const envEditor = require('env-editor');
 const { editorSelection } = require('./editor-selection');
 const { getEnvInfo } = require('./get-env-info');
 
@@ -21,24 +22,38 @@ async function openEditor(context, filePath) {
   }
 
   if (editorSelected !== 'none') {
-    const editorOption = {
-      editor: editorSelected,
-    };
+    const editorArguments = [];
+    const editor = envEditor.getEditor(editorSelected);
 
-    const editor = openInEditor.configure(editorOption, (err) => {
-      console.error(`Selected editor not found in your machine. Please open your favorite editor and modify the file if needed: ${err}`);
-    });
+    if (!editor) {
+      console.error(`Selected editor '${editorSelected}' was not found in your machine. Please open your favorite editor and modify the file if needed.`);
+    }
 
-    return editor.open(filePath)
-      .then(async () => {
-        context.print.info(`Please edit the file in your editor: ${filePath}`);
-      }, async () => {
-        context.print.error(`Selected default editor not found in your machine. Please manually edit the file created at ${filePath}`);
-      })
-      .then(async () => await inquirer.prompt(continueQuestion));
+    if (editorSelected === 'vscode') {
+      editorArguments.push('--goto');
+    }
+
+    editorArguments.push(filePath);
+
+    const stdio = editor.isTerminalEditor ? 'inherit' : 'ignore';
+
+    try {
+      const subProcess = childProcess.spawn(editor.binary, editorArguments, {
+        detached: true,
+        stdio,
+      });
+
+      if (!editor.isTerminalEditor) {
+        subProcess.unref();
+      }
+
+      context.print.info(`Please edit the file in your editor: ${filePath}`);
+
+      await inquirer.prompt(continueQuestion);
+    } catch (e) {
+      context.print.error(`Selected default editor not found in your machine. Please manually edit the file created at ${filePath}`);
+    }
   }
-  context.print.error(`Please manually edit the file created at ${filePath}`);
-  await inquirer.prompt(continueQuestion);
 }
 
 module.exports = {
