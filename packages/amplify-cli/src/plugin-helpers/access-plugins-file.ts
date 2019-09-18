91eb2c9c930ae485e32abe796764a30a0b598ab7
UnleashedMind <michaelzhou.net@gmail.com>
2019-09-17 19:05:24 -0700

    refactor(cli): bump versions manually (#2325)
    * fix(cli): manually bump versions
    
    * graphql package version bumpg
    
    * storage related version bumps
    
    * appsync simulator version bump

--- packages/amplify-cli/src/plugin-helpers/access-plugins-file.ts
+++ packages/amplify-cli/src/plugin-helpers/access-plugins-file.ts
@@ -9,18 +9,18 @@ const JSON_SPACE = 4;
 
 export function readPluginsJsonFileSync(): PluginPlatform | undefined {
   let result: PluginPlatform | undefined;
-  const pluginsFilePath = path.join(os.homedir(),
-    constants.DotAmplifyDirName, constants.PLUGINS_FILE_NAME);
+  const pluginsFilePath = getPluginsJsonFilePath();
+
   if (fs.existsSync(pluginsFilePath)) {
     result = readJsonFileSync(pluginsFilePath)
   }
+
   return result;
 }
 
 export async function readPluginsJsonFile(): Promise<PluginPlatform | undefined> {
   let result: PluginPlatform | undefined;
-  const pluginsFilePath = path.join(os.homedir(),
-    constants.DotAmplifyDirName, constants.PLUGINS_FILE_NAME);
+  const pluginsFilePath = getPluginsJsonFilePath();
 
   const exists = await fs.pathExists(pluginsFilePath);
 
@@ -32,8 +32,8 @@ export async function readPluginsJsonFile(): Promise<PluginPlatform | undefined>
 }
 
 export function writePluginsJsonFileSync(pluginsJson: PluginPlatform): void {
-  const systemDotAmplifyDirPath = path.join(os.homedir(), constants.DotAmplifyDirName);
-  const pluginsJsonFilePath = path.join(systemDotAmplifyDirPath, constants.PLUGINS_FILE_NAME);
+  const systemDotAmplifyDirPath = getSystemDotAmplifyDirPath();
+  const pluginsJsonFilePath = path.join(systemDotAmplifyDirPath, getPluginsJsonFileName());
 
   fs.ensureDirSync(systemDotAmplifyDirPath);
 
@@ -42,11 +42,31 @@ export function writePluginsJsonFileSync(pluginsJson: PluginPlatform): void {
 }
 
 export async function writePluginsJsonFile(pluginsJson: PluginPlatform): Promise<void> {
-  const systemDotAmplifyDirPath = path.join(os.homedir(), constants.DotAmplifyDirName);
-  const pluginsJsonFilePath = path.join(systemDotAmplifyDirPath, constants.PLUGINS_FILE_NAME);
+  const systemDotAmplifyDirPath = getSystemDotAmplifyDirPath();
+  const pluginsJsonFilePath = path.join(systemDotAmplifyDirPath, getPluginsJsonFileName());
 
   await fs.ensureDir(systemDotAmplifyDirPath);
 
   const jsonString = JSON.stringify(pluginsJson, null, JSON_SPACE);
   await fs.writeFile(pluginsJsonFilePath, jsonString, 'utf8');
+}
+
+
+function getPluginsJsonFilePath(): string {
+  return path.join(getSystemDotAmplifyDirPath(), getPluginsJsonFileName());
+}
+
+function getSystemDotAmplifyDirPath(): string {
+  return path.join(os.homedir(), constants.DotAmplifyDirName);
+}
+
+function getPluginsJsonFileName(): string {
+  let result = constants.PLUGINS_FILE_NAME;
+  const amplifyExecutableName = path.basename(process.argv[1]);
+
+  if (amplifyExecutableName !== constants.Amplify) {
+    result = amplifyExecutableName + '-' + constants.PLUGINS_FILE_NAME;
+  }
+
+  return result;
 }
