73e09ff41ee49b637826563c6611c729e11b0b2d
Nikhil Lingireddy <nikling@amazon.com>
2019-09-30 16:14:29 -0700

    refactor(cli): normalize for yarn/npm start
    moved package manager helpers to a separate file, added normalization to post init setup

--- packages/amplify-cli/src/lib/init-steps/postInitSetup.js
+++ packages/amplify-cli/src/lib/init-steps/postInitSetup.js
@@ -1,6 +1,8 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const { getPackageManager } = require('../packageManagerHelpers')
+const { normalizePackageManagerForOS } = require('../packageManagerHelpers')
 
 async function run(context) {
   if (context.parameters.options.app) {
@@ -23,34 +25,17 @@ async function run(context) {
 }
 
 /**
- * Determine the package manager of the current project
- *
- * @return {string} 'yarn' if yarn.lock exists, 'npm' if package.json exists, undefined otherwise
-*/
-async function getPackageManager() {
-  const yarnLock = './yarn.lock';
-  const yarnLockDir = path.join(process.cwd(), yarnLock);
-  const packageJson = './package.json';
-  const packageJsonDir = path.join(process.cwd(), packageJson);
-  if (fs.existsSync(yarnLockDir)) {
-    return 'yarn';
-  } else if (fs.existsSync(packageJsonDir)) {
-    return 'npm';
-  }
-  return undefined;
-}
-
-/**
  * Install package using the correct package manager if package handling file exists
  *
  * @param packageManager either npm or yarn
  */
 async function runPackage() {
   const packageManager = getPackageManager();
+  const normalizedPackageManager = normalizePackageManagerForOS(packageManager);
   if (packageManager === 'yarn') {
-    await execSync('yarn start', { stdio: 'inherit' });
+    await execSync(`${normalizedPackageManager} start`, { stdio: 'inherit' });
   } else if (packageManager === 'npm') {
-    await execSync('npm start', { stdio: 'inherit' });
+    await execSync(`${normalizedPackageManager} start`, { stdio: 'inherit' });
   }
 }
 
--- packages/amplify-cli/src/lib/init-steps/preInitSetup.js
+++ packages/amplify-cli/src/lib/init-steps/preInitSetup.js
@@ -1,6 +1,8 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const { getPackageManager }= require('../packageManagerHelpers')
+const { normalizePackageManagerForOS } = require('../packageManagerHelpers')
 
 async function run(context) {
   if (context.parameters.options.app) {
@@ -56,43 +58,6 @@ async function validateGithubRepo(repoUrl) {
  */
 async function cloneRepo(repoUrl) {
   execSync(`git clone ${repoUrl} .`, { stdio: 'inherit' });
-}
-
-/**
- * Determine the package manager of the current project
- *
- * @return {string} 'yarn' if yarn.lock exists, 'npm' if package.json exists, undefined otherwise
-*/
-async function getPackageManager() {
-  const yarnLock = './yarn.lock';
-  const yarnLockDir = path.join(process.cwd(), yarnLock);
-  const packageJson = './package.json';
-  const packageJsonDir = path.join(process.cwd(), packageJson);
-  if (fs.existsSync(yarnLockDir)) {
-    return 'yarn';
-  } else if (fs.existsSync(packageJsonDir)) {
-    return 'npm';
-  }
-  return undefined;
-}
-
-/**
- * Determines the OS and returns the corresponding command given a package manager
- *
- * @param {string} packageManager the type of package manager detected
- * @return {string} the package manager command for the correct OS
- */
-async function normalizePackageManagerForOS(packageManager) {
-  const isOnWindows = /^win/.test(process.platform);
-  if (isOnWindows) {
-    if (packageManager === 'yarn') {
-      return 'npm.cmd';
-    } else if (packageManager === 'npm') {
-      return 'yarn.cmd';
-    }
-    return undefined;
-  }
-  return packageManager;
 }
 
 /**
