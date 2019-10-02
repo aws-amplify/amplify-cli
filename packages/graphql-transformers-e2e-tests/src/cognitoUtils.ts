40915b489f17320e107e09a0726db94698bb2799
SwaySway <josurui@amazon.com>
2019-10-02 15:35:37 -0700

    test(graphql-transformers-e2e-tests): add e2e for auth on searchable
    added e2e to test auth on searchable
    
    re #1043

--- packages/graphql-transformers-e2e-tests/src/cognitoUtils.ts
+++ packages/graphql-transformers-e2e-tests/src/cognitoUtils.ts
@@ -5,11 +5,24 @@ import {
     CreateUserPoolRequest, CreateUserPoolClientRequest,
     CreateUserPoolClientResponse, DeleteUserPoolRequest, DeleteUserRequest,
 } from 'aws-sdk/clients/cognitoidentityserviceprovider'
+import { ResourceConstants } from 'graphql-transformer-common'
+import { IAM as cfnIAM, Cognito as cfnCognito } from 'cloudform-types';
 import {
     AuthenticationDetails,
 } from 'amazon-cognito-identity-js';
 import * as CognitoClient from 'aws-sdk/clients/cognitoidentityserviceprovider'
 import TestStorage from './TestStorage'
+import DeploymentResources from 'graphql-transformer-core/lib/DeploymentResources';
+
+interface E2Econfiguration {
+    STACK_NAME?: string,
+    AUTH_ROLE_NAME?: string,
+    UNAUTH_ROLE_NAME?: string,
+    IDENTITY_POOL_NAME?: string,
+    USER_POOL_CLIENTWEB_NAME?: string,
+    USER_POOL_CLIENT_NAME?: string,
+    USER_POOL_ID?: string,
+}
 
 const cognitoClient = new CognitoClient({ apiVersion: '2016-04-19', region: 'us-west-2' })
 
@@ -162,4 +175,213 @@ export async function createUserPoolClient(client: CognitoClient, userPoolId: st
         };
         client.createUserPoolClient(params, (err, data) => err ? rej(err) : res(data))
     })
+}
+
+export function addIAMRolesToCFNStack(out: DeploymentResources, e2eConfig: E2Econfiguration) {
+    const {
+        AUTH_ROLE_NAME,
+        UNAUTH_ROLE_NAME,
+        IDENTITY_POOL_NAME,
+        USER_POOL_CLIENTWEB_NAME,
+        USER_POOL_CLIENT_NAME,
+        USER_POOL_ID,
+    } = e2eConfig;
+
+    // logic to add IAM roles to cfn
+    const authRole = new cfnIAM.Role({
+        RoleName: AUTH_ROLE_NAME,
+        AssumeRolePolicyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Sid: '',
+                    Effect: 'Allow',
+                    Principal: {
+                        Federated: 'cognito-identity.amazonaws.com'
+                    },
+                    Action: 'sts:AssumeRoleWithWebIdentity',
+                    Condition: {
+                        'ForAnyValue:StringLike': {
+                            'cognito-identity.amazonaws.com:amr': 'authenticated'
+                        }
+                    }
+                }
+            ]
+        }
+    });
+
+    const unauthRole = new cfnIAM.Role({
+        RoleName: UNAUTH_ROLE_NAME,
+        AssumeRolePolicyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Sid: '',
+                    Effect: 'Allow',
+                    Principal: {
+                        Federated: 'cognito-identity.amazonaws.com'
+                    },
+                    Action: 'sts:AssumeRoleWithWebIdentity',
+                    Condition: {
+                        'ForAnyValue:StringLike': {
+                            'cognito-identity.amazonaws.com:amr': 'unauthenticated'
+                        }
+                    }
+                }
+            ]
+        },
+        Policies: [
+            new cfnIAM.Role.Policy({
+                PolicyName: 'appsync-unauthrole-policy',
+                PolicyDocument: {
+                    Version: '2012-10-17',
+                    Statement: [
+                    {
+                        Effect: 'Allow',
+                        Action: [
+                            'appsync:GraphQL'
+                        ],
+                        Resource: [{
+                            'Fn::Join': [
+                                '',
+                                [
+                                    'arn:aws:appsync:',
+                                    { Ref: 'AWS::Region' },
+                                    ':',
+                                    { Ref: 'AWS::AccountId' },
+                                    ':apis/',
+                                    {
+                                        'Fn::GetAtt': [
+                                            'GraphQLAPI',
+                                            'ApiId'
+                                        ]
+                                    },
+                                    '/*',
+                                ],
+                            ],
+                        }],
+                    }],
+                },
+            }),
+        ]
+    });
+
+    const identityPool = new cfnCognito.IdentityPool({
+        IdentityPoolName: IDENTITY_POOL_NAME,
+        CognitoIdentityProviders: [
+            {
+                ClientId: {
+                    Ref: 'UserPoolClient'
+                },
+                ProviderName: {
+                    'Fn::Sub': [
+                        'cognito-idp.${region}.amazonaws.com/${client}',
+                        {
+                            'region': {
+                                Ref: 'AWS::Region'
+                            },
+                            'client': USER_POOL_ID
+                        }
+                    ]
+                }
+            } as unknown,
+            {
+                ClientId: {
+                    Ref: 'UserPoolClientWeb'
+                },
+                ProviderName: {
+                    'Fn::Sub': [
+                        'cognito-idp.${region}.amazonaws.com/${client}',
+                        {
+                            'region': {
+                                Ref: 'AWS::Region'
+                            },
+                            'client': USER_POOL_ID
+                        }
+                    ]
+                }
+            } as unknown,
+        ],
+        AllowUnauthenticatedIdentities: true
+    });
+
+    const identityPoolRoleMap = new cfnCognito.IdentityPoolRoleAttachment({
+        IdentityPoolId: { Ref: 'IdentityPool' } as unknown as string,
+        Roles: {
+            'unauthenticated': { 'Fn::GetAtt': [ 'UnauthRole', 'Arn' ] },
+            'authenticated': { 'Fn::GetAtt': [ 'AuthRole', 'Arn' ] }
+        }
+    });
+
+    const userPoolClientWeb = new cfnCognito.UserPoolClient({
+        ClientName: USER_POOL_CLIENTWEB_NAME,
+        RefreshTokenValidity: 30,
+        UserPoolId: USER_POOL_ID
+    });
+
+    const userPoolClient = new cfnCognito.UserPoolClient({
+        ClientName: USER_POOL_CLIENT_NAME,
+        GenerateSecret: true,
+        RefreshTokenValidity: 30,
+        UserPoolId: USER_POOL_ID
+    });
+
+    out.rootStack.Resources.IdentityPool = identityPool;
+    out.rootStack.Resources.IdentityPoolRoleMap = identityPoolRoleMap;
+    out.rootStack.Resources.UserPoolClientWeb = userPoolClientWeb;
+    out.rootStack.Resources.UserPoolClient = userPoolClient;
+    out.rootStack.Outputs.IdentityPoolId = { Value: { Ref: 'IdentityPool' } };
+    out.rootStack.Outputs.IdentityPoolName = { Value: { 'Fn::GetAtt': [ 'IdentityPool', 'Name' ] } };
+
+    out.rootStack.Resources.AuthRole = authRole;
+    out.rootStack.Outputs.AuthRoleArn = { Value: { 'Fn::GetAtt': [ 'AuthRole', 'Arn' ] } };
+    out.rootStack.Resources.UnauthRole = unauthRole;
+    out.rootStack.Outputs.UnauthRoleArn = { Value: { 'Fn::GetAtt': [ 'UnauthRole', 'Arn' ] } };
+
+    // Since we're doing the policy here we've to remove the transformer generated artifacts from
+    // the generated stack.
+    delete out.rootStack.Resources[ResourceConstants.RESOURCES.UnauthRolePolicy];
+    delete out.rootStack.Parameters.unauthRoleName;
+    delete out.rootStack.Resources[ResourceConstants.RESOURCES.AuthRolePolicy];
+    delete out.rootStack.Parameters.authRoleName;
+
+    for (const key of Object.keys(out.rootStack.Resources)) {
+        if (out.rootStack.Resources[key].Properties &&
+            out.rootStack.Resources[key].Properties.Parameters &&
+            out.rootStack.Resources[key].Properties.Parameters.unauthRoleName) {
+            delete out.rootStack.Resources[key].Properties.Parameters.unauthRoleName;
+        }
+
+        if (out.rootStack.Resources[key].Properties &&
+            out.rootStack.Resources[key].Properties.Parameters &&
+            out.rootStack.Resources[key].Properties.Parameters.authRoleName) {
+            delete out.rootStack.Resources[key].Properties.Parameters.authRoleName;
+        }
+    }
+
+    for (const stackKey of Object.keys(out.stacks)) {
+        const stack = out.stacks[stackKey];
+
+        for (const key of Object.keys(stack.Resources)) {
+            if (stack.Parameters &&
+                stack.Parameters.unauthRoleName) {
+                delete stack.Parameters.unauthRoleName;
+            }
+            if (stack.Parameters &&
+                stack.Parameters.authRoleName) {
+                delete stack.Parameters.authRoleName;
+            }
+            if (stack.Resources[key].Properties &&
+                stack.Resources[key].Properties.Parameters &&
+                stack.Resources[key].Properties.Parameters.unauthRoleName) {
+                delete stack.Resources[key].Properties.Parameters.unauthRoleName;
+            }
+            if (stack.Resources[key].Properties &&
+                stack.Resources[key].Properties.Parameters &&
+                stack.Resources[key].Properties.Parameters.authRoleName) {
+                delete stack.Resources[key].Properties.Parameters.authRoleName;
+            }
+        }
+    }
+    return out;
 }
