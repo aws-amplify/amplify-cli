4a1c9532bb26bcff4770a393acffadcd1eb25271
Eric Betts <eric.b@sightbox.com>
2019-10-09 15:50:29 -0700

    Pass object, not json, to generateFilterExpression

--- packages/amplify-appsync-simulator/src/velocity/util/transform/index.ts
+++ packages/amplify-appsync-simulator/src/velocity/util/transform/index.ts
@@ -1,7 +1,7 @@
 import { generateFilterExpression } from './dynamodb-filter';
 export const transformUtils = {
   toDynamoDBFilterExpression: filter => {
-    const result = generateFilterExpression(filter.toJSON());
+    const result = generateFilterExpression(filter);
     return JSON.stringify({
       expression: result.expressions.join(' ').trim(),
       expressionNames: result.expressionNames,
