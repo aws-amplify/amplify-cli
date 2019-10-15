5da3d8b36901d2608e5a961780c24537211d2979
Harry Solovay <harrysolovay@gmail.com>
2019-10-15 11:28:02 -0700

    refactor(cli): remove banner
    banner clogged terminal and was not additive––removed figlet dep as well

--- packages/amplify-cli/src/extensions/amplify-helpers/show-help.js
+++ packages/amplify-cli/src/extensions/amplify-helpers/show-help.js
@@ -1,30 +1,17 @@
 const print = require('./print');
-const figlet = require('figlet');
-const cliConstants = require('./constants');
 
 function showHelp(header, commands) {
-  figlet.text(
-    cliConstants.BrandName,
-    {
-      font: 'ANSI Shadow',
-    },
-    (err, data) => {
-      if (!err) {
-        print.info(data);
-      }
-      print.info(header);
-      print.info('');
-      const tableOptions = [];
+  print.info(header);
+  print.info('');
+  const tableOptions = [];
 
-      for (let i = 0; i < commands.length; i += 1) {
-        tableOptions.push([commands[i].name, commands[i].description]);
-      }
+  for (let i = 0; i < commands.length; i += 1) {
+    tableOptions.push([commands[i].name, commands[i].description]);
+  }
 
-      const { table } = print;
+  const { table } = print;
 
-      table(tableOptions, { format: 'default' });
-    }
-  );
+  table(tableOptions, { format: 'default' });
 }
 
 module.exports = {
