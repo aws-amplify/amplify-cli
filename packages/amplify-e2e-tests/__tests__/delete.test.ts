5106fb0b047ae3a754f552fea3366481946a6716
AmmarKarachi <ammkara@amazon.com>
2019-10-17 14:02:59 -0700

    test: test for deleting a project

--- packages/amplify-e2e-tests/__tests__/delete.test.ts
+++ packages/amplify-e2e-tests/__tests__/delete.test.ts
@@ -1,5 +1,6 @@
 require('../src/aws-matchers/'); // custom matcher for assertion
 
+import * as AWS from 'aws-sdk';
 import { initProjectWithProfile, deleteProject } from '../src/init';
 import { createNewProjectDir, deleteProjectDir, getProjectMeta } from '../src/utils';
 
@@ -16,12 +17,30 @@ describe('amplify delete', () => {
   });
 
   it('should delete resources', async () => {
-    const meta = getProjectMeta(projRoot).providers.awscloudformation;
+    const amplifyMeta = getProjectMeta(projRoot);
+    const meta = amplifyMeta.providers.awscloudformation;
     expect(meta.Region).toBeDefined();
     const { AuthRoleName, UnauthRoleName } = meta;
 
     await deleteProject(projRoot, true);
+    await expect(await bucketExists(meta.DeploymentBucketName)).toBe(false);
     await expect(AuthRoleName).not.toBeIAMRoleWithArn();
     await expect(UnauthRoleName).not.toBeIAMRoleWithArn();
   });
 });
+
+async function bucketExists(bucket: string) {
+  const s3 = new AWS.S3();
+  const options = {
+    Bucket: bucket,
+  };
+  try {
+    await s3.headBucket(options).promise();
+    return true;
+  } catch (error) {
+    if (error.statusCode === 404) {
+      return false;
+    }
+    throw error;
+  }
+}
--- packages/amplify-ui-tests/src/init/deleteProject.ts
+++ packages/amplify-ui-tests/src/init/deleteProject.ts
@@ -1,35 +1,27 @@
-import * as AWS from 'aws-sdk';
 import * as nexpect from 'nexpect';
-
 import { getCLIPath, isCI } from '../utils';
-import { getProjectMeta } from '../utils';
+
 export default function deleteProject(cwd: string, deleteDeploymentBucket: Boolean = true, verbose: Boolean = isCI() ? false : true) {
   return new Promise((resolve, reject) => {
-    const meta = getProjectMeta(cwd).providers.awscloudformation;
-    nexpect
-      .spawn(getCLIPath(), ['delete'], { cwd, stripColors: true, verbose })
+    const nrun = nexpect
+      .spawn(getCLIPath(), deleteDeploymentBucket ? ['delete', '--all'] : ['delete'], { cwd, stripColors: true, verbose })
       .wait('Are you sure you want to continue?')
-      .sendline('y\r')
-      .wait('Project deleted locally.')
-      .run(async function(err: Error) {
-        if (!err) {
-          const { DeploymentBucketName } = meta;
-          if (deleteDeploymentBucket) {
-            const s3 = new AWS.S3();
-            const { Contents: items } = await s3.listObjects({ Bucket: DeploymentBucketName }).promise();
-            const promises = [];
-            items.forEach(item => {
-              promises.push(s3.deleteObject({ Bucket: DeploymentBucketName, Key: item.Key }).promise());
-            });
-            await Promise.all(promises);
-            await s3.deleteBucket({ Bucket: DeploymentBucketName }).promise();
-            resolve();
-          } else {
-            resolve();
-          }
-        } else {
-          reject(err);
-        }
-      });
+      .sendline('y')
+      .sendline('');
+
+    if (deleteDeploymentBucket) {
+      nrun
+        .wait('Are you sure you want to continue to delete')
+        .sendline('y')
+        .sendline('');
+    }
+
+    nrun.wait('Project deleted locally.').run(async function(err: Error) {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
   });
 }
