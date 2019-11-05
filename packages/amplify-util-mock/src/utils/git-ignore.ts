eae4b9c09ea0d93cbb48e20df68b6a8621c06495
AmmarKarachi <ammkara@amazon.com>
2019-11-05 10:42:21 -0800

    fix(amplify-util-mock): fixed the content check for gitignore
    check for amplify/mock-data in windows was being generated with '\' as the directory separator.
    GitIgnore only uses / more info at https://git-scm.com/docs/gitignore#_pattern_format

--- packages/amplify-util-mock/src/utils/git-ignore.ts
+++ packages/amplify-util-mock/src/utils/git-ignore.ts
@@ -1,16 +1,21 @@
 import * as fs from 'fs-extra';
-import * as path from 'path';
-import { getMockDataDirectory } from './mock-data-directory';
+import * as os from 'os';
 
 export function addMockDataToGitIgnore(context) {
   const gitIgnoreFilePath = context.amplify.pathManager.getGitIgnoreFilePath();
   if (fs.existsSync(gitIgnoreFilePath)) {
-    const gitRoot = path.dirname(gitIgnoreFilePath);
-    const mockDataDirectory = path.relative(gitRoot, getMockDataDirectory(context));
-    let gitIgnoreContent = fs.readFileSync(gitIgnoreFilePath).toString();
-    if (gitIgnoreContent.search(RegExp(`^\s*${mockDataDirectory}\w*$`, 'gm')) === -1) {
-      gitIgnoreContent += '\n' + mockDataDirectory;
-      fs.writeFileSync(gitIgnoreFilePath, gitIgnoreContent);
+    const gitIgnoreContent = fs
+      .readFileSync(gitIgnoreFilePath)
+      .toString()
+      .split(os.EOL);
+    const gitIgnoreMap = gitIgnoreContent.reduce((map, val) => {
+      map.set(val, true);
+      return map;
+    }, new Map<string, boolean>());
+    const amplifyMockData = 'amplify/mock-data';
+    if (!gitIgnoreMap.has(amplifyMockData)) {
+      gitIgnoreMap.set(amplifyMockData, true);
+      fs.writeFileSync(gitIgnoreFilePath, [...gitIgnoreMap.keys()].join(os.EOL));
     }
   }
 }
