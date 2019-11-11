0581da7add6270b50a1b8fe4e8ddb0558cb603ac
SwaySway <josurui@amazon.com>
2019-11-10 17:03:44 -0800

    added api id and env on delta sync table and iam role

--- packages/graphql-transformer-common/src/SyncResourceIDs.ts
+++ packages/graphql-transformer-common/src/SyncResourceIDs.ts
@@ -5,9 +5,9 @@ export class SyncResourceIDs {
   public static syncTableName: string = 'DeltaSyncTable';
   public static syncPrimaryKey: string = 'ds_pk';
   public static syncRangeKey: string = 'ds_sk';
-  public static syncIAMRoleID: string = 'DynamoDBDeltaSyncIAMRole';
-  public static syncIAMRoleName: string = 'DeltaSyncIAMRoleName';
-  public static syncFunctionRoleName: string = 'deltaSyncLambdaRole';
+  public static syncIAMRoleID: string = 'DynamoDBDeltaSyncIAMRole'
+  public static syncIAMRoleName: string = 'DeltaSyncIAMRole';
+  public static syncFunctionRoleName: string = 'DeltaSyncLambdaRole';
   public static syncFunctionID(name: string, region?: string): string {
     return `${simplifyName(name)}${simplifyName(region || '')}Role`;
   }
