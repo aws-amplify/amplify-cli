aad02f4c983ebd31892ae1956e3a4b405d2de2a0
AmmarKarachi <ammkara@amazon.com>
2019-11-10 18:05:45 -0800

    Revert "feat(cli): collecting crash reports"
    This reverts commit 00cf4960cb2410ef5eca02073a7fbad1641bf914.

--- packages/amplify-cli/src/metrics/prompt-collect-metrics.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from '../domain/context';
-
-export async function promptMetrics(context: Context): Promise<boolean> {
-  const { amplify } = context;
-  return await amplify.confirmPrompt.run(
-    'Crash reporting and metrics collection helps us improve AWS Amplify. Do you consent to collection of usage?'
-  );
-}
