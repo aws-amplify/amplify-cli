1230480d220028e4224c6b00b1a68896f9871ef9
UnleashedMind <michaelzhou.net@gmail.com>
2019-11-21 10:59:59 -0800

    region map

--- packages/amplify-provider-awscloudformation/src/aws-utils/aws-amplify.js
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws-amplify.js
@@ -2,8 +2,34 @@ const aws = require('aws-sdk');
 const proxyAgent = require('proxy-agent');
 const configurationManager = require('../../lib/configuration-manager');
 
-// const betaEndpoint = 'https://ntb76nklh1.execute-api.us-west-2.amazonaws.com/beta';
-const gammaEndpoint = 'https://e3alza85jk.execute-api.us-west-2.amazonaws.com/gamma';
+const defaultAmplifyRegion = 'us-west-2';
+const serviceRegionMap = {
+  'us-east-1': 'us-east-1',
+  'us-east-2': 'us-east-2',
+  'sa-east-1': 'us-east-1',
+  'ca-central-1': 'us-east-1',
+  'us-west-1': 'us-west-2',
+  'us-west-2': 'us-west-2',
+  'cn-north-1': 'us-west-2',
+  'cn-northwest-1': 'us-west-2',
+  'ap-south-1': 'ap-south-1',
+  'ap-northeast-3': 'us-west-2',
+  'ap-northeast-2': 'ap-northeast-2',
+  'ap-southeast-1': 'ap-southeast-1',
+  'ap-southeast-2': 'ap-southeast-2',
+  'ap-northeast-1': 'ap-northeast-1',
+  'eu-central-1': 'eu-central-1',
+  'eu-west-1': 'eu-west-1',
+  'eu-west-2': 'eu-west-2',
+  'eu-west-3': 'eu-west-1',
+};
+
+function mapServiceRegion(region) {
+  if (serviceRegionMap[region]) {
+    return serviceRegionMap[region];
+  }
+  return defaultAmplifyRegion;
+}
 
 async function getConfiguredAmplifyClient(context, options = {}) {
   let cred = {};
@@ -15,14 +41,9 @@ async function getConfiguredAmplifyClient(context, options = {}) {
     // ignore missing config
   }
 
-  let endpoint = gammaEndpoint;
-  if (process.env.AWS_AMPLIFY_ENDPOINT) {
-    endpoint = process.env.AWS_AMPLIFY_ENDPOINT;
-  }
-
   const defaultOptions = {
-    region: 'us-west-2',
-    endpoint,
+    region: mapServiceRegion(cred.region || configurationManager.resolveRegion()),
+    endpoint: process.env.AWS_AMPLIFY_ENDPOINT,
   };
 
   if (httpProxy) {
