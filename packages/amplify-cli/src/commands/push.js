760901856f78e92ffcf8705cb1794fb957b9e4ed
UnleashedMind <michaelzhou.net@gmail.com>
2019-11-24 22:18:03 -0800

    fix(cli): remove extra prompt in the new push workflow (#2824)

--- packages/amplify-cli/src/commands/push.js
+++ packages/amplify-cli/src/commands/push.js
@@ -1,17 +1,17 @@
-const { initializeEnv } = require('../lib/initialize-env');
+const sequential = require('promise-sequential');
+const ora = require('ora');
+const { readJsonFile } = require('../extensions/amplify-helpers/read-json-file');
+
+const spinner = ora('');
+
+const { getProviderPlugins } = require('../extensions/amplify-helpers/get-provider-plugins');
 
 module.exports = {
   name: 'push',
   run: async context => {
     try {
       context.amplify.constructExeInfo(context);
-      context.exeInfo.forcePush = false;
-      context.exeInfo.restoreBackend = false;
-      // The following line of code pulls the latest backend to #current-cloud-backend
-      // so the amplify status is correctly shown to the user before the user confirms
-      // to push his local developments
-      await initializeEnv(context);
-
+      await syncCurrentCloudBackend(context);
       await context.amplify.pushResources(context);
     } catch (e) {
       if (e.name !== 'InvalidDirectiveError') {
@@ -21,3 +21,35 @@ module.exports = {
     }
   },
 };
+
+// The following code pulls the latest backend to #current-cloud-backend
+// so the amplify status is correctly shown to the user before the user confirms
+// to push his local developments
+async function syncCurrentCloudBackend(context) {
+  context.exeInfo.restoreBackend = false;
+
+  const currentEnv = context.exeInfo.localEnvInfo.envName;
+
+  try {
+    const { projectPath } = context.exeInfo.localEnvInfo;
+    const providerInfoFilePath = context.amplify.pathManager.getProviderInfoFilePath(projectPath);
+    const amplifyMeta = {};
+    amplifyMeta.providers = readJsonFile(providerInfoFilePath)[currentEnv];
+
+    const providerPlugins = getProviderPlugins(context);
+
+    const pullCurrentCloudTasks = [];
+
+    context.exeInfo.projectConfig.providers.forEach(provider => {
+      const providerModule = require(providerPlugins[provider]);
+      pullCurrentCloudTasks.push(() => providerModule.initEnv(context, amplifyMeta.providers[provider]));
+    });
+
+    spinner.start(`Fetching updates to backend environment: ${currentEnv} from the cloud.`);
+    await sequential(pullCurrentCloudTasks);
+    spinner.succeed(`Successfully pulled backend environment ${currentEnv} from the cloud.`);
+  } catch (e) {
+    spinner.fail(`There was an error pulling the backend environment ${currentEnv}.`);
+    throw e;
+  }
+}
