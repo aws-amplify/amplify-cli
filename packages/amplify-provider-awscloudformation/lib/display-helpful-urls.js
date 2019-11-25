886c9ee6e8bac6bc30553ab959c1d5a659ac649f
Attila Hajdrik <hajdrik@amazon.com>
2019-11-25 15:09:24 -0800

    Custom Transformer loading enhancement and fixes (#2814)
    * feat: enhance custom transformer loading
    
    * fix: lint fixes
    
    * fix: update snapshots
    
    * feat: enhance custom transformer loading
    
    * fix: lint fixes
    
    * fix: update snapshots
    
    * fix: yarn.lock update

--- packages/amplify-provider-awscloudformation/lib/display-helpful-urls.js
+++ packages/amplify-provider-awscloudformation/lib/display-helpful-urls.js
@@ -106,9 +106,7 @@ function showHostedUIURLs(context, resourcesToBeCreated) {
       context.print.info(chalk`Hosted UI Endpoint: {blue.underline ${hostedUIEndpoint}}`);
       const redirectURIs = oAuthMetadata.CallbackURLs.concat(oAuthMetadata.LogoutURLs);
       if (redirectURIs.length > 0) {
-        const testHostedUIEndpoint = `https://${HostedUIDomain}.auth.${Region}.amazoncognito.com/login?response_type=code&client_id=${AppClientIDWeb}&redirect_uri=${
-          redirectURIs[0]
-        }\n`;
+        const testHostedUIEndpoint = `https://${HostedUIDomain}.auth.${Region}.amazoncognito.com/login?response_type=code&client_id=${AppClientIDWeb}&redirect_uri=${redirectURIs[0]}\n`;
         context.print.info(chalk`Test Your Hosted UI Endpoint: {blue.underline ${testHostedUIEndpoint}}`);
       }
     }
