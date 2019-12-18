92a9ae33026112d228c98f278bd674d74decf23f
SwaySway <josurui@amazon.com>
2019-12-18 13:42:05 -0800

    fix(graphql-function-transformer): add short id to function role name
    generated a short uuid along with the role name to keep them unique
    
    re #2468

--- packages/graphql-transformer-common/src/FunctionResourceIDs.ts
+++ packages/graphql-transformer-common/src/FunctionResourceIDs.ts
@@ -1,4 +1,5 @@
 import { simplifyName } from './util';
+import * as shortid from 'shortid';
 
 export class FunctionResourceIDs {
   static FunctionDataSourceID(name: string, region?: string): string {
@@ -7,6 +8,13 @@ export class FunctionResourceIDs {
 
   static FunctionIAMRoleID(name: string, region?: string): string {
     return `${FunctionResourceIDs.FunctionDataSourceID(name, region)}Role`;
+  }
+
+  static FunctionIAMRoleName(name: string, withEnv: boolean = false): string {
+    if (withEnv) {
+      return `${simplifyName(name).slice(0, 18)}${shortid.generate()}`;
+    }
+    return `${simplifyName(name).slice(0, 28)}${shortid.generate()}`;
   }
 
   static FunctionAppSyncFunctionConfigurationID(name: string, region?: string): string {
