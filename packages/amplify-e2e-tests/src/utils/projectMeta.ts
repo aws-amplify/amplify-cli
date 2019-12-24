6890af6afee07fc18bd908b6780509cfee0ea368
Dustin Noyes <dnnoyes@amazon.com>
2019-12-24 10:49:11 -0800

    test(amplify-e2e-tests): adding tests for customAuth flag w/ native

--- packages/amplify-e2e-tests/src/utils/projectMeta.ts
+++ packages/amplify-e2e-tests/src/utils/projectMeta.ts
@@ -1,7 +1,19 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
-export function getProjectMeta(projectRoot: string) {
+function getProjectMeta(projectRoot: string) {
   const metaFilePath = path.join(projectRoot, 'amplify', '#current-cloud-backend', 'amplify-meta.json');
   return JSON.parse(fs.readFileSync(metaFilePath, 'utf8'));
 }
+
+function getAwsAndroidConfig(projectRoot: string) {
+  const configPath = path.join(projectRoot, 'app', 'src', 'main', 'res', 'raw', 'awsconfiguration.json');
+  return JSON.parse(fs.readFileSync(configPath, 'utf8'));
+}
+
+function getAwsIOSConfig(projectRoot: string) {
+  const configPath = path.join(projectRoot, 'awsconfiguration.json');
+  return JSON.parse(fs.readFileSync(configPath, 'utf8'));
+}
+
+export { getProjectMeta, getAwsAndroidConfig, getAwsIOSConfig };
