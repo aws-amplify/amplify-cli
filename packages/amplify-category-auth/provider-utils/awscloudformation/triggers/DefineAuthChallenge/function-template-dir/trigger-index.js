847b3f447f22754a6b819807f086facfc9e70f1e
Dustin Noyes <dnnoyes@amazon.com>
2019-12-27 11:07:47 -0800

    fix(amplify-category-auth): updating path in trigger indexes for node10
    fix #3083

--- packages/amplify-category-auth/provider-utils/awscloudformation/triggers/DefineAuthChallenge/function-template-dir/trigger-index.js
+++ packages/amplify-category-auth/provider-utils/awscloudformation/triggers/DefineAuthChallenge/function-template-dir/trigger-index.js
@@ -7,7 +7,7 @@
 exports.handler = (event, context, callback) => {
   const modules = process.env.MODULES.split(',');
   for (let i = 0; i < modules.length; i += 1) {
-    const { handler } = require(modules[i]);
+    const { handler } = require(`./${modules[i]}`);
     handler(event, context, callback);
   }
 };
