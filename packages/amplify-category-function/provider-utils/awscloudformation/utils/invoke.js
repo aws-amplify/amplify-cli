a56f2dee5899a708a4344985b11590bf32254d94
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2020-01-13 16:00:26 +0300

    change function invocation for new arrow function

--- packages/amplify-category-function/provider-utils/awscloudformation/utils/invoke.js
+++ packages/amplify-category-function/provider-utils/awscloudformation/utils/invoke.js
@@ -1,18 +1,17 @@
 const path = require('path');
 
-function loadFunction(fileName) {
+const loadFunction = fileName => {
   return require(path.resolve(fileName));
-}
+};
 /*
 var options = this.options({
     'packageFolder': './', //required
     'handler': 'handler', // required
     'fileName': 'index.js', // required
-    'event': 'event.json', // required
-    'context': 'context.json' // optional
+    'event': 'event.json' // required
 });
 */
-function invokeFunction(options) {
+const invokeFunction = async options => {
   console.log('Testing function locally');
   let cwd;
   if (options.packageFolder) {
@@ -20,50 +19,29 @@ function invokeFunction(options) {
     process.chdir(path.resolve(options.packageFolder));
   }
 
-  const context = {
-    done(error, result) {
-      if (error === null || typeof error === 'undefined') {
-        context.succeed(result);
-      } else {
-        context.fail(error);
-      }
-    },
-    succeed(result) {
-      if (cwd) {
-        process.chdir(cwd);
-      }
-      console.log('');
-      console.log('Success!  Message:');
-      console.log('------------------');
-      const msg = typeof result === 'object' ? JSON.stringify(result) : result;
-      console.log(typeof result !== 'undefined' ? msg : 'Successful!');
-    },
-    fail(error) {
-      if (cwd) {
-        process.chdir(cwd);
-      }
-      console.log('');
-      console.log('Failure!  Message:');
-      console.log('------------------');
-      const msg = typeof error === 'object' ? JSON.stringify(error) : error;
-      console.log(typeof error !== 'undefined' ? msg : 'Error not provided.');
-    },
-    awsRequestId: 'LAMBDA_INVOKE',
-    logStreamName: 'LAMBDA_INVOKE',
-  };
-
-  if (options.context) {
-    Object.assign(context, options.context);
-  }
-
-  const callback = (error, object) => {
-    context.done(error, object);
-  };
-
   const lambda = loadFunction(options.fileName);
   const { event } = options;
-  lambda[options.handler](event, context, callback);
-}
+
+  try {
+    const result = await lambda[options.handler](event);
+
+    console.log('');
+    console.log('Success!  Message:');
+    console.log('------------------');
+    const msg = typeof result === 'object' ? JSON.stringify(result) : result;
+    console.log(typeof result !== 'undefined' ? msg : 'Successful!');
+  } catch (error) {
+    console.log('');
+    console.log('Failure!  Message:');
+    console.log('------------------');
+    const msg = typeof error === 'object' ? JSON.stringify(error) : error;
+    console.log(typeof error !== 'undefined' ? msg : 'Error not provided.');
+  } finally {
+    if (cwd) {
+      process.chdir(cwd);
+    }
+  }
+};
 
 module.exports = {
   invokeFunction,
