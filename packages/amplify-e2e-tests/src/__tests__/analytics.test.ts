1a1efcfe9ba11242316ebed3bca3bf5fe78761f7
ammarkarachi <56042290+ammarkarachi@users.noreply.github.com>
2020-01-14 11:50:37 -0800

    fix(amplify-category-analytics): reverted the hyphen and updated tests (#3181)
    * fix(amplify-category-analytics): reverted the hyphen and updated tests
    
    The hyphens are not allowed when pushing and had to revert it back
    
    fix #3163
    
    * test(amplify-e2e-tests): fixing the misplaced boolean

--- packages/amplify-e2e-tests/src/__tests__/analytics.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/analytics.test.ts
@@ -1,4 +1,4 @@
-import { initJSProjectWithProfile } from '../init';
+import { initJSProjectWithProfile, amplifyPushUpdate, deleteProject } from '../init';
 import { addAnalytics, removeAnalytics } from '../categories/analytics';
 import { createNewProjectDir, deleteProjectDir } from '../utils';
 import * as fs from 'fs-extra';
@@ -12,13 +12,15 @@ describe('amplify add analytics', () => {
 
   afterEach(async () => {
     await removeAnalytics(projRoot, {});
+    await deleteProject(projRoot, true);
     deleteProjectDir(projRoot);
   });
 
   it('add analytics', async () => {
     await initJSProjectWithProfile(projRoot, {});
-    const rightName = 'my-app';
-    await addAnalytics(projRoot, { rightName, wrongName: 'my=app' });
+    const rightName = 'myapp';
+    await addAnalytics(projRoot, { rightName, wrongName: '$' });
+    await amplifyPushUpdate(projRoot);
     expect(fs.existsSync(path.join(projRoot, 'amplify', 'backend', 'analytics', rightName))).toBe(true);
   });
 });
