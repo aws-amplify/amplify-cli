9462bb5b3d364b23415105daf6245fd63a446b1f
Josue Ruiz <josurui@amazon.com>
2020-02-07 12:42:40 -0800

    fix(amplify-graphql-types-generator): fix passing typename on fields … (#3328)
    * fix(amplify-graphql-types-generator): fix passing typename on fields
    
    fix passing typenames when evaluating fields and their selection sets
    
    fix #1325

--- packages/amplify-graphql-types-generator/test/angular/index.js
+++ packages/amplify-graphql-types-generator/test/angular/index.js
@@ -317,5 +317,29 @@ describe('Angular code generation', function() {
       const source = generateSource(context);
       expect(source).toMatchSnapshot();
     });
+
+    test('should have the correct __typename(s) for nested fragments', () => {
+      const { compileFromSource } = setup(starWarsSchema);
+      const context = compileFromSource(`
+        query FindHuman($id: ID!) {
+          human {
+            ...humanDetails
+          }
+        }
+        fragment humanDetails on Human {
+          id
+          name
+          starships {
+            ...starshipDetails
+          }
+        }
+        fragment starshipDetails on Starship {
+          id
+          name
+        }
+      `);
+      const source = generateSource(context);
+      expect(source).toMatchSnapshot();
+    });
   });
 });
