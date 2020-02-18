a08bd4c2a330130a6f3d77ed7380b27c6c3c80fd
Nikhil Lingireddy <nikling@amazon.com>
2020-02-18 13:49:04 -0800

    ci: address comments

--- .circleci/local_publish_helpers.sh
+++ .circleci/local_publish_helpers.sh
@@ -12,12 +12,14 @@ function startLocalRegistry {
   (cd && nohup npx ${VERDACCIO_PACKAGE:-$default_verdaccio_package} -c $1 &>$tmp_registry_log &)
   # Wait for Verdaccio to boot
   grep -q 'http address' <(tail -f $tmp_registry_log)
+}
 
+function loginToLocalRegistry {
   # Login so we can publish packages
   (cd && npx npm-auth-to-token@1.0.0 -u user -p password -e user@example.com -r "$custom_registry_url")
 }
 
-function unsetNpmRegistryPath {
+function unsetNpmRegistryUrl {
   # Restore the original NPM and Yarn registry URLs and stop Verdaccio
   npm set registry "https://registry.npmjs.org/"
   yarn config set registry "https://registry.npmjs.org/"
@@ -29,7 +31,7 @@ function changeNpmGlobalPath {
   export PATH=~/.npm-global/bin:$PATH
 }
 
-function setNpmRegistryPathToLocal {
+function setNpmRegistryUrlToLocal {
   # Set registry to local registry
   npm set registry "$custom_registry_url"
   yarn config set registry "$custom_registry_url"
