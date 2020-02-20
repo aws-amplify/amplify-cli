fa019915ce8f636b968757a3d52015c1296620ad
Nikhil Lingireddy <nikling@amazon.com>
2020-02-19 20:45:20 -0800

    Restart verdaccio server and registry path before e2e  (#3433)
    * ci: restart verdaccio server for e2e tests

--- .circleci/local_publish_helpers.sh
+++ .circleci/local_publish_helpers.sh
@@ -12,16 +12,14 @@ function startLocalRegistry {
   (cd && nohup npx ${VERDACCIO_PACKAGE:-$default_verdaccio_package} -c $1 &>$tmp_registry_log &)
   # Wait for Verdaccio to boot
   grep -q 'http address' <(tail -f $tmp_registry_log)
+}
 
-  # Set registry to local registry
-  npm set registry "$custom_registry_url"
-  yarn config set registry "$custom_registry_url"
-
+function loginToLocalRegistry {
   # Login so we can publish packages
   (cd && npx npm-auth-to-token@1.0.0 -u user -p password -e user@example.com -r "$custom_registry_url")
 }
 
-function stopLocalRegistry {
+function unsetNpmRegistryUrl {
   # Restore the original NPM and Yarn registry URLs and stop Verdaccio
   npm set registry "https://registry.npmjs.org/"
   yarn config set registry "https://registry.npmjs.org/"
@@ -31,4 +29,10 @@ function changeNpmGlobalPath {
   mkdir ~/.npm-global
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
+}
+
+function setNpmRegistryUrlToLocal {
+  # Set registry to local registry
+  npm set registry "$custom_registry_url"
+  yarn config set registry "$custom_registry_url"
 }
