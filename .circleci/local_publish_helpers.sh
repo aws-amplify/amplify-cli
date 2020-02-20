eb5b24d35c9311b04ae16c246ebce0332ee61424
Attila Hajdrik <attila@eyedea.hu>
2020-02-19 21:24:42 -0800

    wip

--- .circleci/local_publish_helpers.sh
+++ .circleci/local_publish_helpers.sh
@@ -1,8 +1,6 @@
   #!/bin/bash
 
 custom_registry_url=http://localhost:4873
-original_npm_registry_url=`npm get registry`
-original_yarn_registry_url=`yarn config get registry`
 default_verdaccio_package=verdaccio@4.4.3
 
 function startLocalRegistry {
