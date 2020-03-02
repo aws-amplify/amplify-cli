2b2d52f05edc1190953965ca0f3ecd880ec66a63
Edward Foyle <foyleef@amazon.com>
2020-03-02 11:44:55 -0800

    fix(amplify-category-api): plumb api id to resources that require it (#3464)
    fix #3431, fix #3386

--- packages/amplify-category-function/provider-utils/awscloudformation/index.js
+++ packages/amplify-category-function/provider-utils/awscloudformation/index.js
@@ -80,7 +80,7 @@ function copyCfnTemplate(context, category, options, cfnFilename) {
           template: triggerPackagePath,
           target: `${targetDir}/${category}/${options.resourceName}/src/package.json`,
         },
-      ]
+      ],
     );
   } else {
     switch (options.functionTemplate) {
@@ -102,7 +102,7 @@ function copyCfnTemplate(context, category, options, cfnFilename) {
               template: 'function-template-dir/package.json.ejs',
               target: `${targetDir}/${category}/${options.resourceName}/src/package.json`,
             },
-          ]
+          ],
         );
         break;
       case 'serverless':
@@ -128,7 +128,7 @@ function copyCfnTemplate(context, category, options, cfnFilename) {
               template: 'function-template-dir/serverless-event.json',
               target: `${targetDir}/${category}/${options.resourceName}/src/event.json`,
             },
-          ]
+          ],
         );
         break;
       case 'lambdaTrigger':
@@ -149,7 +149,7 @@ function copyCfnTemplate(context, category, options, cfnFilename) {
               template: 'function-template-dir/package.json.ejs',
               target: `${targetDir}/${category}/${options.resourceName}/src/package.json`,
             },
-          ]
+          ],
         );
         break;
       default:
@@ -175,7 +175,7 @@ function copyCfnTemplate(context, category, options, cfnFilename) {
               template: 'function-template-dir/crud-event.json',
               target: `${targetDir}/${category}/${options.resourceName}/src/event.json`,
             },
-          ]
+          ],
         );
         break;
     }
@@ -456,7 +456,7 @@ async function initTriggerEnvs(context, resourceParams, providerPlugin, envParam
           triggerPath,
           currentTrigger,
           triggers[currentTrigger],
-          currentEnvVariables
+          currentEnvVariables,
         );
       } else {
         envParams = currentEnvVariables;
