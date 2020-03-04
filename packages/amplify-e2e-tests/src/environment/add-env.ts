c24af2fbcc2bcb45a248af6ad2852a9b74896980
jhockett <hockett.john.m@gmail.com>
2020-03-04 12:14:39 -0800

    test: new helper methods for E2E tests
    Checking code in before pulling down new E2E PR, WIP

--- packages/amplify-e2e-tests/src/environment/add-env.ts
+++ packages/amplify-e2e-tests/src/environment/add-env.ts
@@ -21,6 +21,7 @@ export function addEnvironment(cwd: string, settings: any) {
       });
   });
 }
+
 export function checkoutEnvironment(cwd: string, settings: any) {
   return new Promise((resolve, reject) => {
     spawn(getCLIPath(), ['env', 'checkout', settings.envName], { cwd, stripColors: true })
@@ -29,6 +30,105 @@ export function checkoutEnvironment(cwd: string, settings: any) {
         if (!err) {
           resolve();
         } else {
+          reject(err);
+        }
+      });
+  });
+}
+
+// Test multiple Environments by passing settings.numEnv
+export function listEnvironment(cwd: string, settings: any) {
+  return new Promise((resolve, reject) => {
+    let numEnv = settings.numEnv || 1;
+    let regex = /\|\s\*?[a-z]{2,10}\s+\|/;
+    let chain = spawn(getCLIPath(), ['env', 'list'], { cwd, stripColors: true })
+      .wait('| Environments |')
+      .wait('| ------------ |');
+
+    for (let i = 0; i < numEnv; ++i) {
+      chain = chain.wait(regex);
+    }
+
+    chain = chain.sendEof().run((err: Error) => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+// Helper function for getEnvironment()
+function _addQuotes(s: string, prefix: string): string {
+  return s.replace(prefix, '"' + prefix + '"').replace(': ', ':"') + '"';
+}
+
+// Get environment details and return them as JSON
+export function getEnvironment(cwd: string, settings: any) {
+  return new Promise((resolve, reject) => {
+    var envData = '';
+    spawn(getCLIPath(), ['env', 'get', '--name', settings.envName], { cwd, stripColors: true })
+      .wait(settings.envName)
+      .wait('--------------')
+      .wait('Provider', output => (envData = envData.concat('', output.replace('Provider: ', '{"') + '":{')))
+      .wait('AuthRoleName', output => (envData = envData.concat('', _addQuotes(output, 'AuthRoleName'))))
+      .wait('UnauthRoleArn', output => (envData = envData.concat(',', _addQuotes(output, 'UnauthRoleArn'))))
+      .wait('Region', output => (envData = envData.concat(',', _addQuotes(output, 'Region'))))
+      .wait('DeploymentBucketName', output => (envData = envData.concat(',', _addQuotes(output, 'DeploymentBucketName'))))
+      .wait('UnauthRoleName', output => (envData = envData.concat(',', _addQuotes(output, 'UnauthRoleName'))))
+      .wait('StackName', output => (envData = envData.concat(',', _addQuotes(output, 'StackName'))))
+      .wait('StackId', output => (envData = envData.concat(',', _addQuotes(output, 'StackId') + '}}')))
+      .wait('--------------')
+      .sendEof()
+      .run((err: Error) => {
+        if (!err) {
+          resolve(envData);
+          return envData;
+        } else {
+          reject(err);
+        }
+      });
+  });
+}
+
+/*
+  `amplify env pull` only outputs via ora.spinner,
+  but nexpect can't wait() on the spinner output
+  See amplify-cli/src/initialize-env.js
+*/
+export function pullEnvironment(cwd: string, settings: any) {
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), ['env', 'pull'], { cwd, stripColors: true }).run((err: Error) => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+export function importEnvironment(cwd: string, settings: any) {
+  const cmd_array = [
+    'env',
+    'import',
+    '--name',
+    settings.envName,
+    '--config',
+    settings.providerConfig,
+    '--yes', // If env with same name already exists, overwrite it
+  ];
+
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), cmd_array, { cwd, stripColors: true })
+      .wait('Successfully added environment from your project')
+      .sendEof()
+      .run((err: Error) => {
+        if (!err) {
+          resolve();
+        } else {
+          console.error(err);
           reject(err);
         }
       });
