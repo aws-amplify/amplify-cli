e156691258eb1cd686fa24c81ee42fc7ab82b68d
Yathi <511386+yuth@users.noreply.github.com>
2020-03-13 19:41:11 -0700

    test(amplify-e2e-tests): fix auth test (#3670)
    Updated the auth test to use the right config from amplify-meta.json

--- packages/amplify-e2e-tests/src/__tests__/auth.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/auth.test.ts
@@ -151,7 +151,7 @@ describe('amplify add auth...', () => {
     const createFunctionName = `${Object.keys(meta.auth)[1]}CreateAuthChallenge-integtest`;
     const defineFunctionName = `${Object.keys(meta.auth)[1]}DefineAuthChallenge-integtest`;
 
-    const authMeta = Object.keys(meta.auth).map(key => meta.auth[key])[0];
+    const authMeta = Object.keys(meta.auth).map(key => meta.auth[key])[1];
     const id = authMeta.output.UserPoolId;
     const userPool = await getUserPool(id, meta.providers.awscloudformation.Region);
     const clientIds = [authMeta.output.AppClientIDWeb, authMeta.output.AppClientID];
