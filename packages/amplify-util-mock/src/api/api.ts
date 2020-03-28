783c3b8d290b337d5ead1b1740def3f2c610e3d0
Yathi <511386+yuth@users.noreply.github.com>
2020-03-27 19:56:56 -0700

    fix(amplify-util-mock): pass env vars to lambda when invoked with mock
    Updated the mock lambda invoker to pass the env vars related to resources suck as API Key and
    GraphQL endpoint
    
    fix #2453 and #2690

--- packages/amplify-util-mock/src/api/api.ts
+++ packages/amplify-util-mock/src/api/api.ts
@@ -190,10 +190,11 @@ export class APITest {
             if (!lambdaConfig) {
               throw new Error(`Lambda function ${functionName} does not exist in your project. \nPlease run amplify add function`);
             }
+            const envVars = await this.hydrateLambdaEnvVars(context, lambdaConfig.environment, config);
             const invoker = await getInvoker(context, {
               resourceName: functionName,
               handler: lambdaConfig.handler,
-              envVars: lambdaConfig.environment,
+              envVars,
             });
             return {
               ...d,
@@ -332,5 +333,43 @@ export class APITest {
 
     this.configOverrideManager.addOverride('api', override);
     await this.configOverrideManager.generateOverriddenFrontendExports(context);
+  }
+
+  private async hydrateLambdaEnvVars(context, sourceEnvVars: Record<string, any>, config): Promise<Record<string, any>> {
+    const resources = await context.amplify.getResourceStatus();
+    const allResources = resources.allResources;
+    const outputMap = allResources.reduce((acc, r) => {
+      const category = r.category;
+      const resourceName = r.resourceName.toLowerCase();
+      const outputs = Object.entries(r.output || {}).reduce((sum, [name, value]) => {
+        return { ...sum, [name.toLowerCase()]: value };
+      }, {});
+
+      return { ...acc, [category]: { ...acc.category, [resourceName]: outputs } };
+    }, {});
+    outputMap.api = {
+      ...outputMap.api,
+      [this.apiName]: {
+        graphqlapiendpointoutput: this.appSyncSimulator.url,
+        graphqlapikeyoutput: config.appSync.apiKey,
+        graphqlapiidoutput: 'fake-api-id',
+        ...config.dataSources
+          .filter(ds => ds.type === 'AMAZON_DYNAMODB')
+          .reduce((acc, ds) => {
+            return { ...acc, [`${ds.name}_NAME`.toLowerCase()]: ds.name, [`${ds.name}_ARN`.toLowerCase()]: ds.Arn };
+          }, {}),
+      },
+    };
+    return Object.entries(sourceEnvVars).reduce((acc, [name, value]) => {
+      const [category, resourceName, ...outputName] = name.split('_').map(f => f.toLowerCase());
+      let computedValue = value;
+      if (category && resourceName && outputName) {
+        if (outputMap[category] && outputMap[category][resourceName] && outputMap[category][resourceName][outputName.join('_')]) {
+          computedValue = outputMap[category][resourceName][outputName.join('_')];
+        }
+      }
+
+      return { ...acc, [name]: computedValue };
+    }, {});
   }
 }
