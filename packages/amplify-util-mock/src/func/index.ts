cc9c8a92a37d8e47c1acaadb5d6caab79e5f0e9e
Yathi <511386+yuth@users.noreply.github.com>
2020-03-30 15:42:08 -0700

    fix(amplify-util-mock): pass env vars to lambda when invoked with mock (#3790)
    Updated the mock lambda invoker to pass the environment variables related to resources such as API Key and GraphQL endpoint. 
    
    fix #2453 and #2690

--- packages/amplify-util-mock/src/func/index.ts
+++ packages/amplify-util-mock/src/func/index.ts
@@ -2,9 +2,10 @@ import { getInvoker } from 'amplify-category-function';
 import * as path from 'path';
 import * as inquirer from 'inquirer';
 import { loadMinimalLambdaConfig } from '../utils/lambda/loadMinimal';
+import { hydrateAllEnvVars } from '../utils';
 
 export async function start(context) {
-  if (context.input.subCommands.length < 1) {
+  if (!context.input.subCommands || context.input.subCommands.length < 1) {
     throw new Error('Specify the function name to invoke with "amplify mock function <function name>"');
   }
 
@@ -31,8 +32,10 @@ export async function start(context) {
   if (!lambdaConfig || !lambdaConfig.handler) {
     throw new Error(`Could not parse handler for ${resourceName} from cloudformation file`);
   }
+  const { allResources } = await context.amplify.getResourceStatus();
 
-  const invoker = await getInvoker(context, { resourceName, handler: lambdaConfig.handler, envVars: lambdaConfig.environment });
+  const envVars = hydrateAllEnvVars(allResources, lambdaConfig.environment);
+  const invoker = await getInvoker(context, { resourceName, handler: lambdaConfig.handler, envVars });
   context.print.success('Starting execution...');
   await invoker({ event })
     .then(result => {
