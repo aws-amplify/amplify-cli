6878dbf552c59bd4c689f425ee531ab244426be9
Attila Hajdrik <hajdrik@amazon.com>
2020-04-01 12:48:34 -0700

    chore: enable python for e2e provider selection (#3824)
    * chore: enable python for e2e provider selection
    
    * chore: increase circleci machine size

--- packages/amplify-e2e-tests/src/categories/function.ts
+++ packages/amplify-e2e-tests/src/categories/function.ts
@@ -8,7 +8,7 @@ type FunctionRuntimes = 'netCore21' | 'netCore31' | 'go' | 'java' | 'nodejs' | '
 type FunctionCallback = (chain: any, cwd: string, settings: any) => any;
 
 // runtimeChoices are shared between tests
-export const runtimeChoices = [/*'.NET Core 2.1', '.NET Core 3.1',*/ 'Go' /*, 'Java'*/, 'NodeJS' /*, 'Python'*/];
+export const runtimeChoices = [/*'.NET Core 2.1', '.NET Core 3.1',*/ 'Go' /*, 'Java'*/, 'NodeJS', 'Python'];
 
 // templateChoices is per runtime
 const dotNetCore21TemplateChoices = ['Hello World'];
