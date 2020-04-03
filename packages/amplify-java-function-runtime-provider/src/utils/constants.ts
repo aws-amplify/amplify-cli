02375bc910aa12fb2d17151867735d600a4ab966
Edward Foyle <foyleef@amazon.com>
2020-04-03 07:06:34 -0700

    fix: fix java local invoker and api e2e tests (#3855)
    Co-authored-by: Edward Foyle <foyleef@.amazon.com>

--- packages/amplify-java-function-runtime-provider/src/utils/constants.ts
+++ packages/amplify-java-function-runtime-provider/src/utils/constants.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 
-export const shimSrcPath = path.resolve(`${__dirname}/../../resources/localinvoke`);
-export const shimBinaryName = 'latest_build.jar';
-export const shimBinPath = path.join(shimSrcPath, 'build', 'libs');
+export const shimPath = path.resolve(path.join(__dirname, '..', '..', 'resources', 'localinvoke'));
+export const shimJarPath = path.join(shimPath, 'lib', 'localinvoke.jar');
 export const minJavaVersion = '>=11';
 export const minGradleVersion = '>=5';
