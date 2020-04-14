98e47645d10c0b50f1ef440a23f63bf88de83bb5
Edward Foyle <foyleef@.amazon.com>
2020-04-14 13:57:33 -0700

    fix(amplify-e2e-tests): add .NET template and remove ddb uuid

--- packages/amplify-e2e-tests/src/__tests__/function.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/function.test.ts
@@ -381,7 +381,7 @@ describe('nodejs', () => {
       await initJSProjectWithProfile(projRoot, {});
       const random = Math.floor(Math.random() * 10000);
       const funcName = `nodetestfn${random}`;
-      const ddbName = `nodetestddb${random}`;
+      const ddbName = `nodetestddb`;
 
       await addFunction(
         projRoot,
