ea1e13d0b8c78c8bc53a7f5d17e2e42a6d0545ff
Zhou <zhoweimi@186590ce137f.ant.amazon.com>
2020-05-06 15:00:51 -0700

    remove duplicate file from merge

--- packages/amplify-e2e-core/src/utils/readJsonFile.ts
+++ packages/amplify-e2e-core/src/utils/readJsonFile.ts
@@ -1,19 +1,12 @@
-import fs from 'fs-extra';
-import parseJson from 'parse-json';
+import * as fs from 'fs-extra';
 
-function stripBOM(content: string) {
+function stripBOM(content) {
   if (content.charCodeAt(0) === 0xfeff) {
     content = content.slice(1);
   }
   return content;
 }
 
-export function readJsonFileSync(jsonFilePath: string, encoding: string = 'utf8'): any {
-  const content = fs.readFileSync(jsonFilePath, encoding);
-  return parseJson(stripBOM(content), jsonFilePath);
-}
-
-export async function readJsonFile(jsonFilePath: string, encoding: string = 'utf8'): Promise<any> {
-  const content = await fs.readFile(jsonFilePath, encoding);
-  return parseJson(stripBOM(content), jsonFilePath);
+export function readJsonFile(jsonFilePath, encoding = 'utf8') {
+  return JSON.parse(stripBOM(fs.readFileSync(jsonFilePath, encoding)));
 }
