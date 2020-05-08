2ea6a42829086d0c6ab10acd77cbbd0fc9320938
Attila Hajdrik <hajdrik@amazon.com>
2020-05-07 17:21:51 -0700

    fix: e2e init and prediction test failures (#4195)
    * fix: e2e init test failures
    
    * fix: awsexport require for e2e tests

--- packages/amplify-e2e-tests/src/aws-exports/awsExports.ts
+++ packages/amplify-e2e-tests/src/aws-exports/awsExports.ts
@@ -1,4 +1,4 @@
-require = require('esm')(module);
+import * as fs from 'fs-extra';
 import * as path from 'path';
 
 export function getAWSExportsPath(projRoot: string): string {
@@ -6,6 +6,11 @@ export function getAWSExportsPath(projRoot: string): string {
 }
 
 export function getAWSExports(projectRoot: string) {
-  const metaFilePath = getAWSExportsPath(projectRoot);
-  return require(metaFilePath);
+  const awsExportsPath = getAWSExportsPath(projectRoot);
+  // From Jest 25, ESM modules can only be loaded with mjs extension and Jest takes over
+  // require, that's why we need to copy the file.
+  const awsExportsMJSPath = awsExportsPath.replace('.js', '.mjs');
+  fs.copySync(awsExportsPath, awsExportsMJSPath, { overwrite: true });
+  const localRequire = require('esm')(module);
+  return localRequire(awsExportsMJSPath);
 }
