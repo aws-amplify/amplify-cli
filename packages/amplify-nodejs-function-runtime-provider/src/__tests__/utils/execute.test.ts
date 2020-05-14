5638b0eda8c03b17ce2da5697f6d1f3bb0b62082
Benoît Bouré <benoit.boure@gmail.com>
2020-05-14 19:21:16 +0200

    fix(amplify-util-mock): non-promise lambda failing (#4203)

--- packages/amplify-nodejs-function-runtime-provider/src/__tests__/utils/execute.test.ts
+++ packages/amplify-nodejs-function-runtime-provider/src/__tests__/utils/execute.test.ts
@@ -13,17 +13,6 @@ describe('execute', () => {
       ).resolves.toMatchSnapshot();
     });
 
-    it('should succeed with a returned large payload', async () => {
-      await expect(
-        invokeFunction({
-          packageFolder: __dirname,
-          handler: 'handlers.asyncHandlerLargeData',
-          event: '{"succeed": true}',
-          environment: {},
-        }),
-      ).resolves.toMatchSnapshot();
-    });
-
     it('should fail when throwing an error', async () => {
       await expect(
         invokeFunction({
