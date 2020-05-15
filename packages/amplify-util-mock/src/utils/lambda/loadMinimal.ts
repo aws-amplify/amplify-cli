eaa9bcf46c21d4ac4879919d1cadf264ecbb8eef
Ammar <56042290+ammarkarachi@users.noreply.github.com>
2020-05-15 12:35:30 -0700

    fix(amplify-util-mock): loading resources from amplify-meta (#4194)
    Loading resources from Amplify Meta if not found from CFN
    
    #4085 #4012

--- packages/amplify-util-mock/src/utils/lambda/loadMinimal.ts
+++ packages/amplify-util-mock/src/utils/lambda/loadMinimal.ts
@@ -5,6 +5,16 @@ import * as path from 'path';
 export function loadMinimalLambdaConfig(context: any, resourceName: string, params: { [key: string]: string } = {}): LambdaFunctionConfig {
   const resourcePath = path.join(context.amplify.pathManager.getBackendDirPath(), 'function', resourceName);
   const cfn = context.amplify.readJsonFile(path.join(resourcePath, `${resourceName}-cloudformation-template.json`));
-
-  return processResources(cfn.Resources, {}, params);
+  const projectMeta = context.amplify.getProjectMeta();
+  let extendedParams: any = {};
+  if (projectMeta.function[resourceName].dependsOn) {
+    extendedParams = projectMeta.function[resourceName].dependsOn.reduce((ini, depend) => {
+      depend.attributes.forEach(attribute => {
+        const val = projectMeta[depend.category][depend.resourceName].output[attribute];
+        ini[depend.category + depend.resourceName + attribute] = val;
+      });
+      return ini;
+    }, {});
+  }
+  return processResources(cfn.Resources, {}, { ...params, ...extendedParams });
 }
