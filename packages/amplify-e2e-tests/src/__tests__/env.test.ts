afe97422de764e9b11c607f85bf5530678012148
jhockett <hockett.john.m@gmail.com>
2020-05-19 22:52:45 -0700

    test: uncomment import test, validate proj before env is deleted

--- packages/amplify-e2e-tests/src/__tests__/env.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/env.test.ts
@@ -66,39 +66,35 @@ describe('environment commands', () => {
 });
 
 /* Disabling test for now */
-// describe('cross project environment commands', () => {
-//   let projRoot: string;
-//   beforeEach(async () => {
-//     projRoot = await createNewProjectDir('import-env-test');
-//   });
+describe('cross project environment commands', () => {
+  let projRoot: string;
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('import-env-test');
+  });
 
-//   afterEach(async () => {
-//     await deleteProject(projRoot);
-//     deleteProjectDir(projRoot);
-//   });
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
 
-//   it('init two projects, get and import environment from one to the other', async () => {
-//     await initJSProjectWithProfile(projRoot, { envName: 'env' });
-//     await addAuthWithDefault(projRoot, {});
-//     const providerConfig: string = await getEnvironment(projRoot, { envName: 'env' });
-//     expect(providerConfig === JSON.stringify(JSON.parse(providerConfig))).toBeTruthy();
-//     await amplifyPushUpdate(projRoot);
-//     let projRoot2: string;
-//     try {
-//       projRoot2 = await createNewProjectDir('import-env-test2');
-//       await initJSProjectWithProfile(projRoot2, {});
-//       await importEnvironment(projRoot2, { providerConfig, envName: 'env' });
-
-//       const meta2 = getProjectMeta(projRoot2);
-//       await validate(meta2);
-//     } catch (e) {
-//       console.error(e);
-//     } finally {
-//       await deleteProject(projRoot2);
-//       deleteProjectDir(projRoot2);
-//     }
-
-//     const meta1 = getProjectMeta(projRoot);
-//     await validate(meta1);
-//   });
-// });
+  it('init two projects, get and import environment from one to the other', async () => {
+    await initJSProjectWithProfile(projRoot, { envName: 'env' });
+    await addAuthWithDefault(projRoot, {});
+    const providerConfig: string = await getEnvironment(projRoot, { envName: 'env' });
+    expect(providerConfig === JSON.stringify(JSON.parse(providerConfig))).toBeTruthy();
+    await amplifyPushUpdate(projRoot);
+    let projRoot2: string;
+    try {
+      projRoot2 = await createNewProjectDir('import-env-test2');
+      await initJSProjectWithProfile(projRoot2, {});
+      await importEnvironment(projRoot2, { providerConfig, envName: 'env' });
+      await validate(getProjectMeta(projRoot));
+      await validate(getProjectMeta(projRoot2));
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await deleteProject(projRoot2);
+      deleteProjectDir(projRoot2);
+    }
+  });
+});
