1c54da1baa3eb0fb48c8f6150935188b9c629803
Edward Foyle <foyleef@.amazon.com>
2020-05-20 15:22:49 -0700

    chore: add unit tests for open console and mock check

--- packages/amplify-category-function/src/commands/function/console.ts
+++ packages/amplify-category-function/src/commands/function/console.ts
@@ -1,19 +1,16 @@
 import { supportedServices as servicesMetadata } from '../../provider-utils/supported-services';
 import { category } from '../../constants';
 
-const subcommand = 'console';
+export const name = 'console';
 
-module.exports = {
-  name: subcommand,
-  run: async context => {
-    const { amplify } = context;
-    return amplify.serviceSelectionPrompt(context, category, servicesMetadata).then(result => {
-      const providerController = servicesMetadata[result.service].providerController;
-      if (!providerController) {
-        context.print.error('Provider not configured for this category');
-        return;
-      }
-      providerController.openConsole(context, result.service);
-    });
-  },
+export const run = async context => {
+  const { amplify } = context;
+  return amplify.serviceSelectionPrompt(context, category, servicesMetadata).then(result => {
+    const providerController = servicesMetadata[result.service].providerController;
+    if (!providerController) {
+      context.print.error('Provider not configured for this category');
+      return;
+    }
+    providerController.openConsole(context, result.service);
+  });
 };
