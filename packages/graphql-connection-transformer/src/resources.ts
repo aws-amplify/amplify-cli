9800384efff53a57973105508482cad945523727
Andrei Alecu <andreialecu@users.noreply.github.com>
2020-05-21 22:22:02 +0300

    fix(graphql-connection-transformer): limit was not respected (#4021)
    * fix(graphql-connection-transformer): limit was not respected
    
    When using `@connection(fields: [...], limit: 999)` the limit wouldn't be respected. This PR fixes
    it.
    
    * test: use constant
    
    * added limit test in new connection e2e
    
    Co-authored-by: Josue Ruiz <josurui@amazon.com>

--- packages/graphql-connection-transformer/src/resources.ts
+++ packages/graphql-connection-transformer/src/resources.ts
@@ -309,9 +309,11 @@ export class ResourceFactory {
     connectionAttributes: string[],
     keySchema: KeySchema[],
     indexName: string,
+    limit?: number,
   ) {
+    const pageLimit = limit || ResourceConstants.DEFAULT_PAGE_LIMIT;
     const setup: Expression[] = [
-      set(ref('limit'), ref(`util.defaultIfNull($context.args.limit, ${ResourceConstants.DEFAULT_PAGE_LIMIT})`)),
+      set(ref('limit'), ref(`util.defaultIfNull($context.args.limit, ${pageLimit})`)),
       set(ref('query'), this.makeExpression(keySchema, connectionAttributes)),
     ];
 
