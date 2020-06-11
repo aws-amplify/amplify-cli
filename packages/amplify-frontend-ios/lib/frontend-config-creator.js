3413b90eccce090a4c36925825282c828754b568
Nikhil Lingireddy <nikling@amazon.com>
2020-06-10 18:14:04 -0700

    fix: do not remove if generated filename is empty

--- packages/amplify-frontend-ios/lib/frontend-config-creator.js
+++ packages/amplify-frontend-ios/lib/frontend-config-creator.js
@@ -35,8 +35,9 @@ function deleteAmplifyConfig(context) {
         const file = path.join(srcDirPath, docsFilePath, `${filename}.${FILE_EXTENSION_MAP[codeGenTarget]}`);
         if (fs.existsSync(file)) fs.removeSync(file);
       });
-
-      fs.removeSync(path.join(srcDirPath, generatedFileName));
+      if (generatedFileName !== '') {
+        fs.removeSync(path.join(srcDirPath, generatedFileName));
+      }
     });
   }
 }
