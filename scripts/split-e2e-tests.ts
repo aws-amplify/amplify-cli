56326cfc5a8c7cf493c09c72f7af367020420790
Yathi <511386+yuth@users.noreply.github.com>
2020-06-25 18:22:13 -0700

    chore: test concurrency

--- scripts/split-e2e-tests.ts
+++ scripts/split-e2e-tests.ts
@@ -3,8 +3,8 @@ import * as glob from 'glob';
 import { join } from 'path';
 import * as fs from 'fs-extra';
 
-const CONCURRENCY = 2;
-const AWS_REGIONS_TO_RUN_TESTS = ['us-east-2', 'eu-west-1']; // 'ap-southeast-1', 'ap-south-1', 'ap-southeast-2'];
+const CONCURRENCY = 4;
+const AWS_REGIONS_TO_RUN_TESTS = ['us-east-2', 'eu-west-1', 'ap-southeast-1', 'ap-south-1', 'ap-southeast-2'];
 
 // This array needs to be update periodically when new tests suites get added
 // or when a test suite changes drastically
