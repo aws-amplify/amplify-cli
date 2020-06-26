e0c1222509d59dd3779ea3d2cc9f3ac7305d8a02
Ammar <56042290+ammarkarachi@users.noreply.github.com>
2020-06-26 12:09:44 -0700

    test(cli): fix test check against url (#4681)

--- packages/amplify-cli/src/__tests__/version-manager.test.ts
+++ packages/amplify-cli/src/__tests__/version-manager.test.ts
@@ -1,12 +1,13 @@
-import { getUrl } from '../domain/amplify-telemetry/getTelemetryUrl';
+import { prodUrl } from '../domain/amplify-telemetry/getTelemetryUrl';
 import { TelemetryPayload } from '../domain/amplify-telemetry/TelemetryPayload';
 import { getLatestApiVersion, getLatestPayloadVersion } from '../domain/amplify-telemetry/VersionManager';
 import { Input } from '../domain/input';
+import url from 'url';
 describe('test version manager', () => {
   it('url version should be the latest URL', () => {
-    const url = getUrl();
+    const prodURL = url.parse(prodUrl);
     const apiVersion = getLatestApiVersion();
-    expect(url.pathname).toContain(apiVersion);
+    expect(prodURL.pathname).toContain(apiVersion);
   });
 
   it('payload version should be the latest', () => {
