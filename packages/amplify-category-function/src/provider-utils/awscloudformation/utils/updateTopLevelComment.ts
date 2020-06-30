8f030694ea02ebee92b136bd15d3a7b24689b444
Edward Foyle <foyleef@amazon.com>
2020-06-29 17:32:17 -0700

    fix: env vars comment on func update (#4712)

--- packages/amplify-category-function/src/provider-utils/awscloudformation/utils/updateTopLevelComment.ts
+++ packages/amplify-category-function/src/provider-utils/awscloudformation/utils/updateTopLevelComment.ts
@@ -6,7 +6,8 @@ import _ from 'lodash';
  * This is legacy code that has been copied here.
  * In the future we either need to get rid of the top level comment entirely, or create a template hook to modify it
  */
-export const tryUpdateTopLevelComment = (resourceDirPath: string, newComment: string) => {
+export const tryUpdateTopLevelComment = (resourceDirPath: string, envVars: string[]) => {
+  const newComment = createTopLevelComment(envVars);
   const appJSFilePath = path.join(resourceDirPath, 'src', 'app.js');
   const indexJSFilePath = path.join(resourceDirPath, 'src', 'index.js');
   if (fs.existsSync(appJSFilePath)) {
@@ -15,6 +16,8 @@ export const tryUpdateTopLevelComment = (resourceDirPath: string, newComment: st
     updateTopLevelComment(indexJSFilePath, newComment);
   }
 };
+
+const createTopLevelComment = (envVars: string[]) => `${topLevelCommentPrefix}${envVars.sort().join('\n\t')}${topLevelCommentSuffix}`;
 
 const updateTopLevelComment = (filePath, newComment) => {
   const commentRegex = new RegExp(`${_.escapeRegExp(topLevelCommentPrefix)}[a-zA-Z0-9\\-\\s._=]+${_.escapeRegExp(topLevelCommentSuffix)}`);
