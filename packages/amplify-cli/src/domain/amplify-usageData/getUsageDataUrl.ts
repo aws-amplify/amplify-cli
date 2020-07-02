b1131444ef33ad476ee3710e4e69ce3d95fa220e
AmmarKarachi <ammkara@amazon.com>
2020-07-02 08:48:02 -0700

    chore(cli): updated url

--- packages/amplify-cli/src/domain/amplify-usageData/getUsageDataUrl.ts
+++ packages/amplify-cli/src/domain/amplify-usageData/getUsageDataUrl.ts
@@ -2,7 +2,7 @@ import url, { UrlWithStringQuery } from 'url';
 import { getLatestApiVersion } from './VersionManager';
 
 const version = getLatestApiVersion();
-export const prodUrl = `https://aws-amplify-cli-usageData.us-east-1.amazonaws.com/${version}/metrics`;
+export const prodUrl = `https://api.cli.amplify.aws/${version}/metrics`;
 export function getUrl(): UrlWithStringQuery {
   if (process.env.AMPLIFY_CLI_BETA_USAGE_TRACKING_URL && typeof process.env.AMPLIFY_CLI_BETA_USAGE_TRACKING_URL === 'string')
     return url.parse(process.env.AMPLIFY_CLI_BETA_USAGE_TRACKING_URL || '');
