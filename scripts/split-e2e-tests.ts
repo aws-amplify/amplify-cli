a62c476a8d6dfd9817710e0bd11ab62c05d81a88
UnleashedMind <zhoweimi@186590ce137f.ant.amazon.com>
2020-07-08 21:03:59 -0700

    reduce concurrency

--- scripts/split-e2e-tests.ts
+++ scripts/split-e2e-tests.ts
@@ -10,12 +10,7 @@ const AWS_REGIONS_TO_RUN_TESTS = [
   'us-west-2',
   'eu-west-1',
   'eu-west-2',
-  'eu-central-1',
-  'ap-northeast-1',
-  'ap-northeast-2',
-  'ap-southeast-1',
-  'ap-southeast-2',
-  'ap-south-1'
+  'eu-central-1'
 ];
 
 // This array needs to be update periodically when new tests suites get added
