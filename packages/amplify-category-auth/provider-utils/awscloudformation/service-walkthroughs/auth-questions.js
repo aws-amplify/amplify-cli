d555674e4968cceab6fddd34f1b016dd57d506ea
akshbhu <39866697+akshbhu@users.noreply.github.com>
2020-07-09 18:51:55 -0700

    fix: warning added for identity pool deletion (#4731)
    * fix: warning added for identity pool deletion
    
    * chore: revert config.yml file
    
    * Reverted Config.yml file

--- packages/amplify-category-auth/provider-utils/awscloudformation/service-walkthroughs/auth-questions.js
+++ packages/amplify-category-auth/provider-utils/awscloudformation/service-walkthroughs/auth-questions.js
@@ -150,6 +150,9 @@ async function serviceWalkthrough(context, defaultValuesFilename, stringMapsFile
 
   // if user selects user pool only, ensure that we clean id pool options
   if (coreAnswers.authSelections === 'userPoolOnly' && context.updatingAuth) {
+    context.print.warning(
+      `Warning! Your existing IdentityPool: ${context.updatingAuth.identityPoolName} will be deleted upon the next “amplify push”!`,
+    );
     delete context.updatingAuth.identityPoolName;
     delete context.updatingAuth.allowUnauthenticatedIdentities;
     delete context.updatingAuth.thirdPartyAuth;
@@ -206,7 +209,7 @@ async function updateUserPoolGroups(context) {
     context.amplify.pathManager.getBackendDirPath(),
     'auth',
     'userPoolGroups',
-    'user-pool-group-precedence.json'
+    'user-pool-group-precedence.json',
   );
 
   try {
@@ -433,14 +436,14 @@ function userPoolProviders(oAuthProviders, coreAnswers, prevAnswers) {
           authorize_scopes: scopes.join(delimmiter),
           AttributeMapping: maps,
         };
-      })
+      }),
     );
     res.hostedUIProviderCreds = JSON.stringify(
       oAuthProviders.map(el => ({
         ProviderName: el,
         client_id: coreAnswers[`${el.toLowerCase()}AppIdUserPool`],
         client_secret: coreAnswers[`${el.toLowerCase()}AppSecretUserPool`],
-      }))
+      })),
     );
   }
   return res;
@@ -615,7 +618,7 @@ function getIAMPolicies(resourceName, crudOptions) {
           'cognito-idp:ChangePassword',
           'cognito-idp:ConfirmDevice',
           'cognito-idp:AdminResetUserPassword',
-          'cognito-idp:ResendConfirmationCode'
+          'cognito-idp:ResendConfirmationCode',
         );
         break;
       case 'update':
@@ -631,7 +634,7 @@ function getIAMPolicies(resourceName, crudOptions) {
           'cognito-idp:UpdateGroup',
           'cognito-idp:AdminUpdateAuthEventFeedback',
           'cognito-idp:UpdateDeviceStatus',
-          'cognito-idp:UpdateUserPool'
+          'cognito-idp:UpdateUserPool',
         );
         break;
       case 'read':
@@ -649,7 +652,7 @@ function getIAMPolicies(resourceName, crudOptions) {
           'cognito-sync:List*',
           'iam:ListOpenIdConnectProviders',
           'iam:ListRoles',
-          'sns:ListPlatformApplications'
+          'sns:ListPlatformApplications',
         );
         break;
       case 'delete':
@@ -663,7 +666,7 @@ function getIAMPolicies(resourceName, crudOptions) {
           'cognito-idp:DeleteUserPool',
           'cognito-idp:AdminDeleteUser',
           'cognito-idp:DeleteIdentityProvider',
-          'cognito-idp:DeleteUser'
+          'cognito-idp:DeleteUser',
         );
         break;
       default:
