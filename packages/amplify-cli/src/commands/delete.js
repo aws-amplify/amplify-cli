f21a0f449a23c0c80a6f3280eef76bcbf3e9cb7c
UnleashedMind <michaelzhou.net@gmail.com>
2020-07-10 14:05:09 -0700

    Revert problematic PRs (#4803)
    * feat(amplify-provider-awscloudformation): add region ca-central-1
    
    * fix test
    
    * Revert "Add region ca-central-1 (#4796)"
    
    This reverts commit af29f359f14989fdc89112e96ebed08644296914.
    
    * Revert "feat: add check for extra command line args provided with amplify delete (#4576)"
    
    This reverts commit 48b27b79698b33ee62a01df2ad00b701f79029d8.
    
    * Revert "perf: fulfill promises to upload files to S3 concurrently (#4575)"
    
    This reverts commit 9fbee8a71b4bf941dbda9d2f76fbedd73ab754ef.
    
    * Revert "fix(cli): remove unnecessary stack trace log when adding services (#4610)"
    
    This reverts commit 56efb32b79c47839cb9506a9300d40a01875a9fc.
    
    * Revert "fix test"
    
    This reverts commit 50a8073fbbdb56931191912938fb50f542197832.
    
    Co-authored-by: UnleashedMind <zhoweimi@186590ce137f.ant.amazon.com>

--- packages/amplify-cli/src/commands/delete.js
+++ packages/amplify-cli/src/commands/delete.js
@@ -1,12 +1,6 @@
 module.exports = {
   name: 'delete',
   run: async context => {
-    if (Array.isArray(context.parameters.array) && context.parameters.array.length > 0) {
-      context.print.error('"delete" command does not expect additional arguments.');
-      context.print.error('Perhaps you meant to use the "remove" command instead of "delete"?');
-      process.exit(1);
-    }
-
     await context.amplify.deleteProject(context);
   },
 };
