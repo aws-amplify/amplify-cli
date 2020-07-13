2303b0897fa599aa9c0a1b6cdbd4d9012b2fa2db
Ammar <56042290+ammarkarachi@users.noreply.github.com>
2020-07-13 15:10:21 -0700

    fix(cli): change in usage data ux and revert usage data disable (#4791)

--- packages/amplify-cli/scripts/post-install.js
+++ packages/amplify-cli/scripts/post-install.js
@@ -26,6 +26,18 @@ console.log(EOL);
 console.log(chalk.green('iOS Getting Started - https://docs.amplify.aws/start/q/integration/ios'));
 console.log(EOL);
 
+console.log(
+  chalk.blue(
+    `Amplify CLI collects anonymized usage data, which is used to help understand${EOL}\
+how to improve the product. If you don't wish to send anonymized Amplify CLI${EOL}\
+usage data to AWS, run `,
+  ) +
+    chalk.blue.italic.bgWhite('amplify configure --usage-data-off ') +
+    chalk.blue(` to opt-out.${EOL}${EOL}\
+Learn more - https://docs.amplify.aws/cli/reference/usage-data`),
+);
+console.log(EOL);
+
 if (fs.existsSync('../lib/plugin-manager.js')) {
   require('../lib/plugin-manager').scan();
 }
--- packages/amplify-cli/src/context-manager.ts
+++ packages/amplify-cli/src/context-manager.ts
@@ -14,7 +14,9 @@ export function constructContext(pluginPlatform: PluginPlatform, input: Input): 
 export function attachUsageData(context: Context) {
   const { AMPLIFY_CLI_ENABLE_USAGE_DATA } = process.env;
   const config = init(context);
-  const usageTrackingEnabled = !AMPLIFY_CLI_ENABLE_USAGE_DATA ? false : config.usageDataConfig.isUsageTrackingEnabled;
+  const usageTrackingEnabled = AMPLIFY_CLI_ENABLE_USAGE_DATA
+    ? AMPLIFY_CLI_ENABLE_USAGE_DATA === 'true'
+    : config.usageDataConfig.isUsageTrackingEnabled;
   if (usageTrackingEnabled) {
     context.usageData = UsageData.Instance;
   } else {
