20a8dec9b4c51239760846ec94a71dc528009498
John Hockett <jhockett@users.noreply.github.com>
2020-07-13 12:38:29 -0700

    fix: update function not loading previous selections (#4823)

--- packages/amplify-category-function/src/provider-utils/awscloudformation/utils/permissionMapUtils.ts
+++ packages/amplify-category-function/src/provider-utils/awscloudformation/utils/permissionMapUtils.ts
@@ -1,13 +1,13 @@
-import _ from "lodash"
+import _ from 'lodash';
 
-export const fetchPermissionCategories = (permissionMap) => {
+export const fetchPermissionCategories = permissionMap => {
   return _.keys(permissionMap);
-}
+};
 
 export const fetchPermissionResourcesForCategory = (permissionMap, category: string) => {
   return _.keys(_.get(permissionMap, [category]));
-}
+};
 
 export const fetchPermissionsForResourceInCategory = (permissionMap, category: string, resourceName: string) => {
   return _.get(permissionMap, [category, resourceName]);
-}
+};
