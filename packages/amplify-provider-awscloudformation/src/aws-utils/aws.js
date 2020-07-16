93979e54e1c460889460e8a105971097e0244376
UnleashedMind <michaelzhou.net@gmail.com>
2020-07-16 13:36:51 -0700

    fix(amplify-provider-awscloudformation): fix bug for no credential file (#4310)
    * fix(cli): fix a bug in init workflow
    
    fix a bug in init workflow when there is no credentials file and the customer select not to set up
    new user and directly type in credentials
    
    fix #4284
    
    * address PR comments
    
    * run split test
    
    * dev
    
    * back to the original tests
    
    * add warning message on running  init-special-case test
    
    Co-authored-by: Zhou <zhoweimi@186590ce137f.ant.amazon.com>

--- packages/amplify-provider-awscloudformation/src/aws-utils/aws.js
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws.js
@@ -1,8 +1,20 @@
+const path = require('path');
+const fs = require('fs-extra');
+const os = require('os');
+
+const dotAWSDirPath = path.normalize(path.join(os.homedir(), '.aws'));
+const credentialsFilePath = path.join(dotAWSDirPath, 'credentials');
+const configFilePath = path.join(dotAWSDirPath, 'config');
+
 let aws;
 
 try {
   delete require.cache[require.resolve('aws-sdk')];
-  process.env.AWS_SDK_LOAD_CONFIG = true;
+  if (fs.existsSync(credentialsFilePath) && fs.existsSync(configFilePath)) {
+    process.env.AWS_SDK_LOAD_CONFIG = true;
+  } else {
+    delete process.env.AWS_SDK_LOAD_CONFIG;
+  }
   aws = require('aws-sdk');
 } catch (e) {
   delete require.cache[require.resolve('aws-sdk')];
