80fa4e0111d74aad3f1c3ee7efc6606065919822
UnleashedMind <zhoweimi@186590ce137f.ant.amazon.com>
2020-07-16 10:45:07 -0700

    back to the original tests

--- packages/amplify-e2e-tests/src/init-special-cases/index.ts
+++ packages/amplify-e2e-tests/src/init-special-cases/index.ts
@@ -1,17 +1,49 @@
 import path from 'path';
-import { nspawn as spawn, singleSelect, amplifyRegions } from 'amplify-e2e-core';
+import { nspawn as spawn, getCLIPath, singleSelect, amplifyRegions } from 'amplify-e2e-core';
+import fs from 'fs-extra';
+import os from 'os';
 
-export async function initWithoutCredentialFileAndNoNewUserSetup(cwd) {
+export async function initWithoutCredentialFileAndNoNewUserSetup(projRoot) {
   const settings = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.CLI_REGION || process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || 'us-west-2',
+    region: process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || 'us-west-2',
   };
-  
-  const exePath = path.join(__dirname, './setupAndInit');
-  
+
+  delete process.env.AWS_ACCESS_KEY_ID;
+  delete process.env.AWS_SECRET_ACCESS_KEY;
+  delete process.env.AWS_DEFAULT_REGION;
+  delete process.env.AWS_REGION;
+
+  const dotAWSDirPath = path.normalize(path.join(os.homedir(), '.aws'));
+  const credentialsFilePath = path.join(dotAWSDirPath, 'credentials');
+  const configFilePath = path.join(dotAWSDirPath, 'config');
+  const credentialsFilePathHide = path.join(dotAWSDirPath, 'credentials.hide');
+  const configFilePathHide = path.join(dotAWSDirPath, 'config.hide');
+
+  try {
+    if (fs.existsSync(configFilePath)) {
+      fs.renameSync(configFilePath, configFilePathHide);
+    }
+    if (fs.existsSync(credentialsFilePath)) {
+      fs.renameSync(credentialsFilePath, credentialsFilePathHide);
+    }
+    await initWorkflow(projRoot, settings);
+  } catch (e) {
+    throw e;
+  } finally {
+    if (fs.existsSync(configFilePathHide)) {
+      fs.renameSync(configFilePathHide, configFilePath);
+    }
+    if (fs.existsSync(credentialsFilePathHide)) {
+      fs.renameSync(credentialsFilePathHide, credentialsFilePath);
+    }
+  }
+}
+
+async function initWorkflow(cwd: string, settings: { accessKeyId: string; secretAccessKey: string; region: string }) {
   return new Promise((resolve, reject) => {
-    let chain = spawn(exePath, [], { cwd, stripColors: true })
+    let chain = spawn(getCLIPath(), ['init'], { cwd, stripColors: true })
       .wait('Enter a name for the project')
       .sendCarriageReturn()
       .wait('Enter a name for the environment')
