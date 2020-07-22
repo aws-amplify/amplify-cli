a9346cd1f11a6594302b0655a9df947182727538
Edward Foyle <foyleef@amazon.com>
2020-07-21 17:49:57 -0700

    fix: remove mutableParametersState from stored function-params (#4897)

--- packages/amplify-category-api/src/index.ts
+++ packages/amplify-category-api/src/index.ts
@@ -143,26 +143,28 @@ export async function getPermissionPolicies(context, resourceOpsMapping) {
   const permissionPolicies = [];
   const resourceAttributes = [];
 
-  Object.keys(resourceOpsMapping).forEach(resourceName => {
-    try {
-      const providerController = require(`./provider-utils/${amplifyMeta[category][resourceName].providerPlugin}/index`);
-      if (providerController) {
-        const { policy, attributes } = providerController.getPermissionPolicies(
-          context,
-          amplifyMeta[category][resourceName].service,
-          resourceName,
-          resourceOpsMapping[resourceName],
-        );
-        permissionPolicies.push(policy);
-        resourceAttributes.push({ resourceName, attributes, category });
-      } else {
-        context.print.error(`Provider not configured for ${category}: ${resourceName}`);
+  await Promise.all(
+    Object.keys(resourceOpsMapping).map(async resourceName => {
+      try {
+        const providerController = require(`./provider-utils/${amplifyMeta[category][resourceName].providerPlugin}/index`);
+        if (providerController) {
+          const { policy, attributes } = await providerController.getPermissionPolicies(
+            context,
+            amplifyMeta[category][resourceName].service,
+            resourceName,
+            resourceOpsMapping[resourceName],
+          );
+          permissionPolicies.push(policy);
+          resourceAttributes.push({ resourceName, attributes, category });
+        } else {
+          context.print.error(`Provider not configured for ${category}: ${resourceName}`);
+        }
+      } catch (e) {
+        context.print.warning(`Could not get policies for ${category}: ${resourceName}`);
+        throw e;
       }
-    } catch (e) {
-      context.print.warning(`Could not get policies for ${category}: ${resourceName}`);
-      throw e;
-    }
-  });
+    }),
+  );
   return { permissionPolicies, resourceAttributes };
 }
 
