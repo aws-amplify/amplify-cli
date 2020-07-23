ef0d6072b496aa10c3e90a209cf1e4b321cb5ac7
akshbhu <39866697+akshbhu@users.noreply.github.com>
2020-07-23 10:44:37 -0700

    fix: support large payload in Node.js functions (#4906)
    * fix: change flow to stream for size limits
    
    * fix: added tests for large payload
    
    * fix: using execa instead of fork
    
    * chore: removing files
    
    * Revert Config.yml

--- packages/amplify-nodejs-function-runtime-provider/src/utils/execute.ts
+++ packages/amplify-nodejs-function-runtime-provider/src/utils/execute.ts
@@ -91,7 +91,8 @@ export function invokeFunction(options: InvokeOptions) {
 process.on('message', async options => {
   try {
     const result = await invokeFunction(JSON.parse(options));
-    process.send!(JSON.stringify({ result, error: null }));
+    process.stdout.write('\n');
+    process.stdout.write(JSON.stringify({ result, error: null }));
   } catch (error) {
     process.send!(
       JSON.stringify({
