018bbabab02389f28b9c8e2ea83faacce47c5eb4
arseneoaa <arseneoaa@users.noreply.github.com>
2020-07-23 04:05:09 +0200

    fix(amplify-provider-awscloudformation): Stack delete condition (#4465)
    * fix(amplify-provider-awscloudformation): Stack delete condition
    
    Currently when updating/deleting the stack both authParamsJson and unauthParamsJson keep their previous condition statement.
    When the new stack is created the previous values for 'Condition': {'StringEquals': {'cognito-identity.amazonaws.com:aud': idpId},' for example do not get updated.
    
    The fix is just about deleting the condition on the object inside the Statement array instead of on the Statement array itself.
    
    * add test
    
    * address PR comments
    
    Co-authored-by: UnleashedMind <zhoweimi@186590ce137f.ant.amazon.com>

--- packages/amplify-e2e-tests/src/__tests__/auth_2.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/auth_2.test.ts
@@ -21,6 +21,7 @@ import {
   addAuthViaAPIWithTrigger,
   addAuthWithMaxOptions,
   addAuthUserPoolOnly,
+  getBackendAmplifyMeta,
 } from 'amplify-e2e-core';
 import {
   createNewProjectDir,
@@ -46,6 +47,28 @@ describe('amplify add auth...', () => {
   afterEach(async () => {
     await deleteProject(projRoot);
     deleteProjectDir(projRoot);
+  });
+
+  it('...should init a project and add auth with defaults and push, then remove auth and push should clean up trust relationship conditions', async () => {
+    await initJSProjectWithProfile(projRoot, defaultsSettings);
+    await addAuthWithDefault(projRoot, {});
+    await amplifyPushAuth(projRoot);
+
+    const amplifyMeta = getBackendAmplifyMeta(projRoot);
+    const { AuthRoleName, UnauthRoleName } = amplifyMeta.providers.awscloudformation;
+    const cognitoResource = Object.values(amplifyMeta.auth).find((res: any) => {
+      return res.service === 'Cognito';
+    }) as any;
+    const idpId = cognitoResource.output.IdentityPoolId;
+
+    expect(AuthRoleName).toHaveValidPolicyConditionMatchingIdpId(idpId);
+    expect(UnauthRoleName).toHaveValidPolicyConditionMatchingIdpId(idpId);
+
+    await removeAuthWithDefault(projRoot);
+    await amplifyPushAuth(projRoot);
+
+    expect(AuthRoleName).not.toHaveValidPolicyConditionMatchingIdpId(idpId);
+    expect(UnauthRoleName).not.toHaveValidPolicyConditionMatchingIdpId(idpId);
   });
 
   it('...should init a project with only user pool and no identity pool', async () => {
