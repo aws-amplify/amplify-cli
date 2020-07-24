913dd67e143515ba580d08871d8210ef636e2748
Edward Foyle <foyleef@amazon.com>
2020-07-24 14:02:31 -0700

    Add warning emoji to messages
    Co-authored-by: Rene Brandel <4989523+renebrandel@users.noreply.github.com>

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/print-api-key-warnings.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/print-api-key-warnings.ts
@@ -5,13 +5,13 @@ export const printApiKeyWarnings = (context, oldConfigHadApiKey: boolean, newCon
     context.print.warning(
       'If other resources depend on this API, run "amplify update <category>" and reselect this API to remove the dependency on the API key.',
     );
-    context.print.warning('This must be done before running "amplify push" to prevent a push failure');
+    context.print.warning('⚠️ This must be done before running "amplify push" to prevent a push failure');
   }
 
   if (!oldConfigHadApiKey && newConfigHasApiKey) {
     context.print.warning('The API_KEY auth type has been added to the API.');
     context.print.warning(
-      'If other resources depend on this API and need access to the API key, run "amplify update <category>" and reselect this API as a dependency to add the API key dependency.',
+      '⚠️ If other resources depend on this API and need access to the API key, run "amplify update <category>" and reselect this API as a dependency to add the API key dependency.',
     );
   }
 };
