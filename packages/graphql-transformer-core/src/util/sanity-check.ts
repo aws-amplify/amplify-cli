7d962b681137c36cb27c6855a9e80ba317c1507c
Akshay <akshayupadhyay3@gmail.com>
2020-07-27 18:01:59 -0700

    fix: validation check for multiple GSI update

--- packages/graphql-transformer-core/src/util/sanity-check.ts
+++ packages/graphql-transformer-core/src/util/sanity-check.ts
@@ -36,7 +36,7 @@ export async function check(
     cantAddAndRemoveGSIAtSameTime,
   ];
   // Project rules run on the full set of diffs, the current build, and the next build.
-  const projectRules: ProjectRule[] = [cantHaveMoreThan200Resources];
+  const projectRules: ProjectRule[] = [cantHaveMoreThan200Resources, cantAddMultipleGSIAtUpdateTime];
   if (cloudBackendDirectoryExists && buildDirectoryExists) {
     const current = await loadDiffableProject(currentCloudBackendDir, rootStackName);
     const next = await loadDiffableProject(buildDirectory, rootStackName);
@@ -267,6 +267,46 @@ export function cantHaveMoreThan200Resources(diffs: Diff[], currentBuild: Diffab
           'If the stack was generated, you have hit a limit and can use the StackMapping argument in ' +
           `${TRANSFORM_CONFIG_FILE_NAME} to fine tune how resources are assigned to stacks.`,
       );
+    }
+  }
+}
+
+/**
+ * Throws a helpful error when a customer is trying to complete an invalid migration.
+ * Users are unable to add multiple GSIs at the same time in update stage.
+ * @param diffs The set of diffs between currentBuild and nextBuild.
+ * @param currentBuild The last deployed build.
+ * @param nextBuild The next build.
+ */
+export function cantAddMultipleGSIAtUpdateTime(diffs: Diff[], currentBuild: DiffableProject, nextBuild: DiffableProject) {
+  function throwError(stackName: string, tableName: string) {
+    throw new InvalidMigrationError(
+      `Attempting to more than 1 a global secondary index at the same time on the ${tableName} table in the ${stackName} stack. `,
+      'You may only add one global secondary index in a single CloudFormation stack update. ',
+      'If using @key, include one @key at a time. ' +
+        'If using @connection, just add one new @connection which is using @key, run `amplify push`, ',
+    );
+  }
+
+  if (diffs) {
+    let gsiCount: number = 0; // max gsiCount = 1 // for update flow
+    for (const diff of diffs) {
+      if (
+        // implies a field was changed in a GSI after it was created.
+        // Path like:["stacks","Todo.json","Resources","TodoTable","Properties","GlobalSecondaryIndexes", ... ]
+        diff.kind === 'A' &&
+        diff.path.length >= 6 &&
+        diff.path[5] === 'GlobalSecondaryIndexes'
+      ) {
+        if (diff.item.kind === 'N') {
+          gsiCount += 1;
+          if (gsiCount > 1) {
+            const stackName = basename(diff.path[1], '.json');
+            const tableName = diff.path[3];
+            throwError(stackName, tableName);
+          }
+        }
+      }
     }
   }
 }
