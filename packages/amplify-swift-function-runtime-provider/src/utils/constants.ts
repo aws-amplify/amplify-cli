2ff2689c2e24ffdb04847de2c5dc1d132ff2ee00
Nick Chaloult <npchaloult@gmail.com>
2020-08-20 14:50:22 -0400

    feat: write functionality to check swift-lambda tool as a dependency

--- packages/amplify-swift-function-runtime-provider/src/utils/constants.ts
+++ packages/amplify-swift-function-runtime-provider/src/utils/constants.ts
@@ -1,3 +1,4 @@
 import { SemVer, coerce } from 'semver';
 
 export const SWIFT_MIN_VERSION = <SemVer>coerce('5.3');
+export const BUILD_TOOL_INFO_MSG = 'Scripts to build and deploy Swift Package Manager projects on AWS Lambda';
--- packages/amplify-swift-function-runtime-provider/src/utils/deps.ts
+++ packages/amplify-swift-function-runtime-provider/src/utils/deps.ts
@@ -1,6 +1,6 @@
 import { CheckDependenciesResult } from 'amplify-function-plugin-interface';
 import { executeCommand } from './helpers';
-import { SWIFT_MIN_VERSION } from './constants';
+import { SWIFT_MIN_VERSION, BUILD_TOOL_INFO_MSG } from './constants';
 import { SemVer, coerce, lt } from 'semver';
 import * as which from 'which';
 
@@ -14,6 +14,11 @@ export const checkDependencies = async (runtimeValue: string): Promise<CheckDepe
   if (swiftResult.errorMessage !== undefined) {
     overallResult.hasRequiredDependencies = false;
     errArray.push(swiftResult.errorMessage);
+  }
+  const buildToolResult = await checkSwiftLambda();
+  if (buildToolResult.errorMessage !== undefined) {
+    overallResult.hasRequiredDependencies = false;
+    errArray.push(buildToolResult.errorMessage);
   }
 
   if (!overallResult.hasRequiredDependencies) {
@@ -62,6 +67,40 @@ const checkSwift = async (): Promise<CheckDependenciesResult> => {
     return {
       hasRequiredDependencies: false,
       errorMessage: `The "${executableName}" version found was: ${versionNum}, but must be greater than or equal to ${SWIFT_MIN_VERSION}}`,
+    };
+  }
+
+  return { hasRequiredDependencies: true };
+};
+
+// checkSwiftLambda validates that the swift-lambda cross-compilation tool is installed.
+//
+// The swift-lambda tool doesn't have a "--version" flag, or any equivalent, as of August 2020. Since the official instructions for
+// installing swift-lambda (https://github.com/swiftxcode/swift-lambda#installation) recommend installing the tool via homebrew, we use
+// homebrew to make sure that the swift-lambda tool is installed correctly.
+const checkSwiftLambda = async (): Promise<CheckDependenciesResult> => {
+  const homebrewCaskName = 'swift-lambda';
+
+  // Validate that the swift-lambda build tool that's installed on a user's machine is the tool that we think it is.
+  // versionOutput should look something like:
+  //
+  // spmdestinations/tap/swift-lambda: stable 0.1.0
+  // Scripts to build and deploy Swift Package Manager projects on AWS Lambda
+  // ...
+  const versionOutput = executeCommand('brew', ['info', homebrewCaskName]);
+  if (!versionOutput) {
+    return {
+      hasRequiredDependencies: false,
+      errorMessage: `Failed to verify ${homebrewCaskName} installation with: "brew info ${homebrewCaskName}"`,
+    };
+  }
+  const parts = versionOutput.split('\n');
+  const infoMsg = parts[1];
+  if (infoMsg !== BUILD_TOOL_INFO_MSG) {
+    // TODO: replace rick roll yt link to a documentation link. Something like this, maybe: https://docs.amplify.aws/cli/function#supported-lambda-runtimes
+    return {
+      hasRequiredDependencies: false,
+      errorMessage: `The "${homebrewCaskName}" tool that you have installed is not the tool that the Amplify CLI needs. Please install "${homebrewCaskName}" by following these instructions: https://www.youtube.com/watch?v=oHg5SJYRHA0`,
     };
   }
 
