d702387ec9f29b690fb229978dca8f3dfed81024
Edward Foyle <foyleef@.amazon.com>
2020-08-20 12:14:05 -0700

    test: add tests

--- packages/amplify-util-mock/src/api/api.ts
+++ packages/amplify-util-mock/src/api/api.ts
@@ -17,6 +17,7 @@ import { getAllLambdaFunctions } from '../utils/lambda/load';
 import { getInvoker } from 'amplify-category-function';
 import { keys } from 'lodash';
 import { LambdaFunctionConfig } from '../CFNParser/lambda-resource-processor';
+import { lambdaArnToConfig } from './lambda-arn-to-config';
 
 export class APITest {
   private apiName: string;
@@ -195,7 +196,7 @@ export class APITest {
           if (d.type !== 'AWS_LAMBDA') {
             return d;
           }
-          const lambdaConfig = this.lambdaArnToConfig(d.LambdaFunctionArn, provisionedLambdas);
+          const lambdaConfig = lambdaArnToConfig(d.LambdaFunctionArn, provisionedLambdas);
           const envVars = await this.hydrateLambdaEnvVars(context, lambdaConfig.environment, config);
           const invoker = await getInvoker(context, {
             resourceName: lambdaConfig.name,
@@ -347,34 +348,4 @@ export class APITest {
       dataSources: config.dataSources,
     });
   }
-
-  /**
-   * Attempts to match an arn object against the array of lambdas configured in the project
-   */
-  private lambdaArnToConfig = (arn: any, provisionedLambdas: LambdaFunctionConfig[]): LambdaFunctionConfig => {
-    const errorSuffix =
-      '\nSee https://docs.amplify.aws/cli/graphql-transformer/directives#function for information on how to configure Lambda resolvers.';
-    let searchString = '';
-    if (typeof arn === 'string') {
-      searchString = arn;
-    } else if (typeof arn === 'object' && keys(arn).length === 1) {
-      const funcArr = arn['Fn::GetAtt'] || arn['Fn::Sub'];
-      if (Array.isArray(funcArr) && funcArr.length > 0) {
-        searchString = funcArr[0];
-      } else {
-        throw new Error(`Malformed Lambda ARN [${JSON.stringify(arn)}]${errorSuffix}`);
-      }
-    } else {
-      throw new Error(`Cannot interpret Lambda ARN [${JSON.stringify(arn)}]${errorSuffix}`);
-    }
-    const lambdaConfig = provisionedLambdas.find(funcConfig => searchString.includes(funcConfig.name));
-    if (!lambdaConfig) {
-      throw new Error(
-        `Did not find a Lambda matching ARN [${JSON.stringify(
-          arn,
-        )}] in the project. Local mocking only supports Lambdas that are configured in the project.${errorSuffix}`,
-      );
-    }
-    return lambdaConfig;
-  };
 }
