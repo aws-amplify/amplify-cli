8db5d44c317fb625053795510e393ba5d16a807d
Nick Chaloult <npchaloult@gmail.com>
2020-08-20 12:06:04 -0400

    feat: write boilerplate for new Swift func runtime provider pkg

---
+++ packages/amplify-swift-function-runtime-provider/src/index.ts
@@ -0,0 +1,23 @@
+import { FunctionRuntimeContributorFactory } from 'amplify-function-plugin-interface';
+
+export const functionRuntimeContributorFactory: FunctionRuntimeContributorFactory = context => {
+  return {
+    contribute: async request => {
+      if (request.selection !== 'swift') {
+        throw new Error(`Unknown selection ${request.selection}`);
+      }
+      return {
+        runtime: {
+          name: 'Swift',
+          value: 'swift',
+          cloudTemplateValue: 'custom', // Stubbed. Not sure if this is correct?
+          defaultHandler: 'index.handler', // Stubbed. Not sure if this is correct?
+        },
+      };
+    },
+    checkDependencies: async runtimeValue => ({ hasRequiredDependencies: false }), // Stubbed.
+    package: async request => ({}), // Stubbed.
+    build: async request => ({ rebuilt: false }), // Stubbed.
+    invoke: async request => ({}), // Stubbed.
+  };
+};
