b21fb2d2eb0570655264c3db9826521c14cf9dd0
Yathi <511386+yuth@users.noreply.github.com>
2020-08-28 15:38:52 -0700

    fix(amplify-provider-awscloudformation): include region configure url
    Updated the user addition URL to include selected region instead of undefined. Updated code to use async/await
    instead of promises
    
    fix #4735

--- packages/amplify-provider-awscloudformation/lib/setup-new-user.js
+++ packages/amplify-provider-awscloudformation/lib/setup-new-user.js
@@ -7,7 +7,7 @@ const constants = require('./constants.js');
 const systemConfigManager = require('./system-config-manager');
 const obfuscationUtil = require('./utility-obfuscate');
 
-function run(context) {
+async function run(context) {
   const awsConfig = {
     accessKeyId: constants.DefaultAWSAccessKeyId,
     secretAccessKey: constants.DefaultAWSSecretAccessKey,
@@ -20,93 +20,84 @@ function run(context) {
   context.print.info(chalk.green(constants.AWSAmazonConsoleUrl));
   open(constants.AWSAmazonConsoleUrl, { wait: false }).catch(() => {});
 
-  return context.amplify.pressEnterToContinue
-    .run({ message: 'Press Enter to continue' })
-    .then(() => {
-      context.print.info('Specify the AWS Region');
-      return inquirer.prompt([
-        {
-          type: 'list',
-          name: 'region',
-          message: 'region: ',
-          choices: awsRegions,
-          default: awsConfig.region,
-        },
-      ]);
-    })
-    .then(answers => {
-      awsConfig.region = answers.region;
-      context.print.info('Specify the username of the new IAM user:');
-      return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'userName',
-          message: 'user name: ',
-          default: `amplify-${context.amplify.makeId()}`,
-        },
-      ]);
-    })
-    .then(answers => {
-      const deepLinkURL = constants.AWSCreateIAMUsersUrl.replace('{userName}', answers.userName).replace('{region}', answers.region);
-      context.print.info('Complete the user creation using the AWS console');
-      context.print.info(chalk.green(deepLinkURL));
-      open(deepLinkURL, { wait: false }).catch(() => {});
-      return context.amplify.pressEnterToContinue.run({ message: 'Press Enter to continue' });
-    })
-    .then(() => {
-      context.print.info('Enter the access key of the newly created user:');
-      return inquirer.prompt([
-        {
-          type: 'password',
-          mask: '*',
-          name: 'accessKeyId',
-          message: 'accessKeyId: ',
-          default: awsConfig.accessKeyId,
-          transformer: obfuscationUtil.transform,
-        },
-        {
-          type: 'password',
-          mask: '*',
-          name: 'secretAccessKey',
-          message: 'secretAccessKey: ',
-          default: awsConfig.secretAccessKey,
-          transformer: obfuscationUtil.transform,
-        },
-      ]);
-    })
-    .then(answers => {
-      if (answers.accessKeyId) {
-        awsConfig.accessKeyId = answers.accessKeyId.trim();
-      }
-      if (answers.secretAccessKey) {
-        awsConfig.secretAccessKey = answers.secretAccessKey.trim();
-      }
-      return awsConfig;
-    })
-    .then(async awsConfig => {
-      if (validateAWSConfig(awsConfig)) {
-        let profileName = 'default';
-        context.print.warning('This would update/create the AWS Profile in your local machine');
-        const answer = await inquirer.prompt([
-          {
-            type: 'input',
-            name: 'pn',
-            message: 'Profile Name: ',
-            default: 'default',
-          },
-        ]);
+  await context.amplify.pressEnterToContinue.run({ message: 'Press Enter to continue' });
 
-        profileName = answer.pn.trim();
+  context.print.info('Specify the AWS Region');
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'region',
+      message: 'region: ',
+      choices: awsRegions,
+      default: awsConfig.region,
+    },
+  ]);
+  awsConfig.region = answers.region;
+  context.print.info('Specify the username of the new IAM user:');
+  const { userName } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'userName',
+      message: 'user name: ',
+      default: `amplify-${context.amplify.makeId()}`,
+    },
+  ]);
 
-        systemConfigManager.setProfile(awsConfig, profileName);
-        context.print.info('');
-        context.print.success('Successfully set up the new user.');
-        return profileName;
-      }
-      context.print.info('');
-      context.print.info('You did NOT enter valid keys.');
-      throw new Error('New user setup failed.');
-    });
+  const deepLinkURL = constants.AWSCreateIAMUsersUrl.replace('{userName}', userName).replace('{region}', awsConfig.region);
+  context.print.info('Complete the user creation using the AWS console');
+  context.print.info(chalk.green(deepLinkURL));
+  open(deepLinkURL, { wait: false }).catch(() => {});
+  await context.amplify.pressEnterToContinue.run({ message: 'Press Enter to continue' });
+
+  context.print.info('Enter the access key of the newly created user:');
+  const accountDetails = await inquirer.prompt([
+    {
+      type: 'password',
+      mask: '*',
+      name: 'accessKeyId',
+      message: 'accessKeyId: ',
+      default: awsConfig.accessKeyId,
+      transformer: obfuscationUtil.transform,
+    },
+    {
+      type: 'password',
+      mask: '*',
+      name: 'secretAccessKey',
+      message: 'secretAccessKey: ',
+      default: awsConfig.secretAccessKey,
+      transformer: obfuscationUtil.transform,
+    },
+  ]);
+
+  if (accountDetails.accessKeyId) {
+    awsConfig.accessKeyId = accountDetails.accessKeyId.trim();
+  }
+  if (accountDetails.secretAccessKey) {
+    awsConfig.secretAccessKey = accountDetails.secretAccessKey.trim();
+  }
+
+  if (validateAWSConfig(awsConfig)) {
+    let profileName = 'default';
+    context.print.warning('This would update/create the AWS Profile in your local machine');
+    const profileDetails = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'pn',
+        message: 'Profile Name: ',
+        default: 'default',
+      },
+    ]);
+
+    profileName = profileDetails.pn.trim();
+
+    systemConfigManager.setProfile(awsConfig, profileName);
+    context.print.info('');
+    context.print.success('Successfully set up the new user.');
+    return profileName;
+  }
+  context.print.info('');
+  context.print.info('You did NOT enter valid keys.');
+  throw new Error('New user setup failed.');
 }
 
 function validateAWSConfig(awsConfig) {
