6d8b8a8dca9fd20feccb9d638a669a5029912a05
user32-2 <60904938+user32-2@users.noreply.github.com>
2020-08-28 10:21:11 +0800

    Added supported regions interactions service
    Updating interactions service to include additional regions supported by lex.
    
    As mentioned in https://github.com/aws-amplify/amplify-cli/issues/5186

--- packages/amplify-provider-awscloudformation/src/aws-utils/aws-lex.js
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws-lex.js
@@ -13,12 +13,12 @@ const serviceRegionMap = {
   'ap-south-1': 'us-west-2',
   'ap-northeast-3': 'us-west-2',
   'ap-northeast-2': 'us-west-2',
-  'ap-southeast-1': 'ap-southeast-2',
+  'ap-southeast-1': 'ap-southeast-1',
   'ap-southeast-2': 'ap-southeast-2',
-  'ap-northeast-1': 'ap-southeast-2',
-  'eu-central-1': 'eu-west-1',
+  'ap-northeast-1': 'ap-northeast-1',
+  'eu-central-1': 'eu-central-1',
   'eu-west-1': 'eu-west-1',
-  'eu-west-2': 'eu-west-1',
+  'eu-west-2': 'eu-west-2',
   'eu-west-3': 'eu-west-1',
 };
 
