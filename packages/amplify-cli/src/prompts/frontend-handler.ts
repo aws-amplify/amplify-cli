7610595ecc68de71d61b36c98ccd832019b54dce
flywheelms <flywheelms@gmail.com>
2020-09-04 09:43:40 -0400

    Update frontend-handler.ts

--- packages/amplify-cli/src/prompts/frontend-handler.ts
+++ packages/amplify-cli/src/prompts/frontend-handler.ts
@@ -2,7 +2,6 @@ import { InputPrompt, SelectPrompt, Choice } from 'amplify-cli-core';
 
 const PROMPT_NAME = 'select';
 const PROMPT_MESSAGE = "Choose the type of app that you're building";
-// const PROMPT_MESSAGE = "Choose your frontend"
 
 function constructFrontendPrompt(frontendChoices: string[] | Choice[], defaultFrontend?: string): SelectPrompt {
   const frontendQuestion = new SelectPrompt(PROMPT_NAME, PROMPT_MESSAGE, frontendChoices, defaultFrontend);
