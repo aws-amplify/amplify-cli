b64f1bd784b0595f49241fdd89686c5f8a421ae3
John Hockett <jhockett@users.noreply.github.com>
2020-09-09 10:28:13 -0700

    fix: update default file permissions for credentials/config (#5246)
    * fix: check credentials/config file permissions and update if needed
    
    * chore: add eslint ignore comment
    
    * fix: remove unused import
    
    * refactor: address PR feedback
    
    * refactor: fix naming

--- packages/amplify-cli-core/src/state-manager/pathManager.ts
+++ packages/amplify-cli-core/src/state-manager/pathManager.ts
@@ -3,6 +3,11 @@ import * as fs from 'fs-extra';
 import { homedir } from 'os';
 
 const PathConstants = {
+  // in home directory
+  DotAWSDir: '.aws',
+  AWSCredentials: 'credentials',
+  AWSConfig: 'config',
+
   // in project root
   AmplifyDirName: 'amplify',
   DotAmplifyDirName: '.amplify',
@@ -88,6 +93,12 @@ export class PathManager {
       PathConstants.CurrentCloudBackendDirName,
       PathConstants.BackendConfigFileName,
     ]);
+
+  getDotAWSDirPath = (): string => path.normalize(path.join(homedir(), PathConstants.DotAWSDir));
+
+  getAWSCredentialsFilePath = (): string => path.normalize(path.join(this.getDotAWSDirPath(), PathConstants.AWSCredentials));
+
+  getAWSConfigFilePath = (): string => path.normalize(path.join(this.getDotAWSDirPath(), PathConstants.AWSConfig));
 
   private constructPath = (projectPath?: string, segments: string[] = []): string => {
     if (!projectPath) {
