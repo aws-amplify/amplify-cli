a924a481ea3abbc09870b106b1c43756f8084e07
Edward Foyle <foyleef@.amazon.com>
2020-09-22 17:22:55 -0700

    test: add headless auth e2e tests

--- packages/amplify-e2e-tests/src/__tests__/auth_2.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/auth_2.test.ts
@@ -5,20 +5,19 @@ import {
   initIosProjectWithProfile,
   deleteProject,
   amplifyPushAuth,
-  amplifyPush,
+  addHeadlessAuth,
+  updateHeadlessAuth,
+  removeHeadlessAuth,
 } from 'amplify-e2e-core';
 import {
   addAuthWithDefault,
   removeAuthWithDefault,
-  addAuthWithDefaultSocial,
-  addAuthWithGroupTrigger,
   addAuthWithRecaptchaTrigger,
   addAuthWithCustomTrigger,
   addAuthWithSignInSignOutUrl,
   updateAuthWithoutCustomTrigger,
   updateAuthRemoveRecaptchaTrigger,
   updateAuthSignInSignOutUrl,
-  addAuthViaAPIWithTrigger,
   addAuthWithMaxOptions,
   addAuthUserPoolOnly,
   getBackendAmplifyMeta,
@@ -33,6 +32,8 @@ import {
   getUserPoolClients,
   getLambdaFunction,
 } from 'amplify-e2e-core';
+import { AddAuthRequest, CognitoUserPoolSigninMethod, CognitoUserProperty, UpdateAuthRequest } from 'amplify-headless-interface';
+import _ from 'lodash';
 
 const defaultsSettings = {
   name: 'authTest',
@@ -190,5 +191,82 @@ describe('amplify updating auth...', () => {
     meta = getAwsIOSConfig(projRoot);
     expect(meta.Auth.Default.authenticationFlowType).toBeDefined();
     expect(meta.Auth.Default.authenticationFlowType).toEqual('USER_SRP_AUTH');
+  });
+});
+
+describe('headless auth', () => {
+  let projRoot: string;
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('auth-update');
+  });
+
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
+  it('adds auth resource', async () => {
+    const addAuthRequest: AddAuthRequest = {
+      version: 1,
+      resourceName: 'myAuthResource',
+      serviceConfiguration: {
+        serviceName: 'Cognito',
+        includeIdentityPool: false,
+        userPoolConfiguration: {
+          requiredSignupAttributes: [CognitoUserProperty.EMAIL, CognitoUserProperty.PHONE_NUMBER],
+          signinMethod: CognitoUserPoolSigninMethod.USERNAME,
+        },
+      },
+    };
+
+    await initJSProjectWithProfile(projRoot, defaultsSettings);
+    await addHeadlessAuth(projRoot, addAuthRequest);
+    await amplifyPushAuth(projRoot);
+    const meta = getProjectMeta(projRoot);
+    const id = Object.keys(meta.auth).map(key => meta.auth[key])[0].output.UserPoolId;
+    const userPool = await getUserPool(id, meta.providers.awscloudformation.Region);
+    expect(userPool.UserPool).toBeDefined();
+  });
+
+  it('updates existing auth resource', async () => {
+    const updateAuthRequest: UpdateAuthRequest = {
+      version: 1,
+      serviceModification: {
+        serviceName: 'Cognito',
+        userPoolModification: {
+          userPoolGroups: [
+            {
+              groupName: 'group1',
+            },
+            {
+              groupName: 'group2',
+            },
+          ],
+        },
+        includeIdentityPool: true,
+        identityPoolModification: {
+          unauthenticatedLogin: true,
+        },
+      },
+    };
+
+    await initJSProjectWithProfile(projRoot, defaultsSettings);
+    await addAuthWithDefault(projRoot, {});
+    await updateHeadlessAuth(projRoot, updateAuthRequest);
+    await amplifyPushAuth(projRoot);
+    const meta = getProjectMeta(projRoot);
+    const id = Object.keys(meta.auth).map(key => meta.auth[key])[0].output.UserPoolId;
+    const userPool = await getUserPool(id, meta.providers.awscloudformation.Region);
+    expect(userPool.UserPool).toBeDefined();
+    expect(_.get(meta, ['auth', 'userPoolGroups'])).toBeDefined();
+  });
+
+  it('removes auth resource', async () => {
+    await initJSProjectWithProfile(projRoot, defaultsSettings);
+    await addAuthWithDefault(projRoot, {});
+    const { auth: authBefore } = getBackendAmplifyMeta(projRoot);
+    const authResourceName = _.keys(authBefore).find(() => true); // first element or undefined
+    await removeHeadlessAuth(projRoot, authResourceName);
+    const { auth: authAfter } = getBackendAmplifyMeta(projRoot);
+    expect(_.isEmpty(authAfter)).toBe(true);
   });
 });
