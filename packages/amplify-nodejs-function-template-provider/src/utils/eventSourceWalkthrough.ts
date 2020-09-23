0d2f632029bc3c78434fe502899780649f3f74e3
Ammar <56042290+ammarkarachi@users.noreply.github.com>
2020-09-23 14:37:26 -0700

    fix: fixed typos and bad import (#5390)

--- packages/amplify-nodejs-function-template-provider/src/utils/eventSourceWalkthrough.ts
+++ packages/amplify-nodejs-function-template-provider/src/utils/eventSourceWalkthrough.ts
@@ -153,7 +153,7 @@ export async function askEventSourceQuestions(context: any) {
           if (!storageResources) {
             const errMessage = 'There are no DynamoDB resources configured in your project currently';
             context.print.error(errMessage);
-            context.usagedate.emitError(new ResourceDoesNotExistError(errMessage));
+            context.usageData.emitError(new ResourceDoesNotExistError(errMessage));
             process.exit(0);
           }
 
