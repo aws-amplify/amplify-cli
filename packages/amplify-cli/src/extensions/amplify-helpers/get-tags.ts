74abca91a79dd34b53266861f47b9e7350cc6009
AmmarKarachi <ammkara@amazon.com>
2020-09-24 17:30:51 -0700

    fix(cli): fixed projName and envName
    #5399

--- packages/amplify-cli/src/extensions/amplify-helpers/get-tags.ts
+++ packages/amplify-cli/src/extensions/amplify-helpers/get-tags.ts
@@ -4,7 +4,7 @@ export function getTags() {
   const projectDetails = getProjectDetails();
   const { projectName } = projectDetails.projectConfig;
   const { envName } = projectDetails.localEnvInfo;
-  return HydrateTags(stateManager.getProjectTags(), projectName, envName);
+  return HydrateTags(stateManager.getProjectTags(), envName, projectName);
 }
 
 function HydrateTags(tags: any[], envName: string, projectName: string) {
