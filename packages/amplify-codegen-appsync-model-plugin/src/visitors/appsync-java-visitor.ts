d7f8179f859b57be91febe440632352f4fe5b2a3
Zeyu Li <lizeyu@amazon.com>
2020-10-07 14:59:58 -0400

    Revert "remove camel and use first uppercase instead of pascal case for methods and fields"
    This reverts commit 0d9d397a514dfb061c6528c7199a736130d17f3d.

--- packages/amplify-codegen-appsync-model-plugin/src/visitors/appsync-java-visitor.ts
+++ packages/amplify-codegen-appsync-model-plugin/src/visitors/appsync-java-visitor.ts
@@ -1,5 +1,5 @@
 import { indent, indentMultiline, transformComment } from '@graphql-codegen/visitor-plugin-common';
-import { camelCase, constantCase } from 'change-case';
+import { camelCase, constantCase, pascalCase } from 'change-case';
 import dedent from 'ts-dedent';
 import {
   MODEL_CLASS_IMPORT_PACKAGES,
@@ -590,8 +590,7 @@ export class AppSyncModelJavaVisitor<
    * @param field codegen field
    */
   protected getFieldGetterName(field: CodeGenField): string {
-    const fieldName = field.name;
-    return `get${fieldName[0].toUpperCase()}${fieldName.slice(1)}`;
+    return `get${pascalCase(field.name)}`;
   }
 
   /**
@@ -599,7 +598,7 @@ export class AppSyncModelJavaVisitor<
    * @param field
    */
   protected getStepFunctionName(field: CodeGenField): string {
-    return field.name;
+    return camelCase(field.name);
   }
 
   /**
@@ -607,7 +606,7 @@ export class AppSyncModelJavaVisitor<
    * @param field
    */
   protected getStepFunctionArgumentName(field: CodeGenField): string {
-    return field.name;
+    return camelCase(field.name);
   }
 
   /**
@@ -746,7 +745,7 @@ export class AppSyncModelJavaVisitor<
    * @returns string
    */
   private getStepInterfaceName(nextFieldName: string): string {
-    return `${nextFieldName[0].toUpperCase()}${nextFieldName.slice(1)}Step`;
+    return `${pascalCase(nextFieldName)}Step`;
   }
 
   protected generateModelAnnotations(model: CodeGenModel): string[] {
