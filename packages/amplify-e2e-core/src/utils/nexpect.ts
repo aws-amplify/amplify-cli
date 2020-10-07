a800fcefe77ba06867e6f1d1ae055c4fb30af1e5
Attila Hajdrik <attila@eyedea.hu>
2020-10-07 11:05:28 -0700

    chore: fix lgtm warning

--- packages/amplify-e2e-core/src/utils/nexpect.ts
+++ packages/amplify-e2e-core/src/utils/nexpect.ts
@@ -19,7 +19,6 @@ import { AssertionError } from 'assert';
 import strip = require('strip-ansi');
 import { EOL } from 'os';
 import retimer = require('retimer');
-import { max } from 'lodash';
 
 const DEFAULT_NO_OUTPUT_TIMEOUT = 5 * 60 * 1000; // 5 Minutes
 const EXIT_CODE_TIMEOUT = 2;
