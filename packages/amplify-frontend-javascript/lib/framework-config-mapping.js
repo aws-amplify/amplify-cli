c58805df40205f3f38fa13778464a23388722b7a
Wei He <git@weispot.com>
2020-10-21 18:17:02 -0400

    fix(amplify-frontend-javascript): update source and distribution dir
    aws-amplify/amplify-cli#5483

--- packages/amplify-frontend-javascript/lib/framework-config-mapping.js
+++ packages/amplify-frontend-javascript/lib/framework-config-mapping.js
@@ -12,8 +12,8 @@ const reactConfig = {
 };
 
 const reactNativeConfig = {
-  SourceDir: '/',
-  DistributionDir: '/',
+  SourceDir: './',
+  DistributionDir: './',
   BuildCommand: `${npm} run-script build`,
   StartCommand: `${npm} run-script start`,
 };
@@ -40,7 +40,7 @@ const vueConfig = {
 };
 
 const emberConfig = {
-  SourceDir: '/',
+  SourceDir: './',
   DistributionDir: 'dist',
   BuildCommand: `${npm} run-script build -- -e production`,
   StartCommand: `${npm} run-script start`,
