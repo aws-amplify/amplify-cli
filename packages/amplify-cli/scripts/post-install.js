9ccda46fb3ea545f838939b2b13d1555d95437dd
Edward Foyle <foyleef@amazon.com>
2020-10-22 09:31:12 -0700

    fix: check lib before running post-install (#5669)

--- packages/amplify-cli/scripts/post-install.js
+++ packages/amplify-cli/scripts/post-install.js
@@ -15,9 +15,14 @@ try {
   // could not delete the cache directory but don't want to fail the installation
 }
 
-// execute post-install amplify command
-const ampPath = path.resolve(path.join(__dirname, '..', 'bin', 'amplify'));
-execa.commandSync(`${ampPath} post-install`);
+// execute post-install amplify command if cli has been built
+// this check is necessary because yarn workspace builds don't execute lifecycle scripts properly
+// https://github.com/yarnpkg/yarn/issues/6175
+if (fs.pathExistsSync(path.join(__dirname, '..', 'lib'))) {
+  const ampPath = path.resolve(path.join(__dirname, '..', 'bin', 'amplify'));
+  execa.commandSync(`${ampPath} post-install`);
+  console.log(chalk.green('Ran amplify post-install'));
+}
 
 console.log(EOL);
 console.log(chalk.green('----------------------------------------'));
