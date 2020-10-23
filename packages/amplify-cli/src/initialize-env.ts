83c9c85eae6bb0d6e9ea0208e111814e361e725d
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-10-23 12:30:22 -0500

    Update error message.

--- packages/amplify-cli/src/initialize-env.ts
+++ packages/amplify-cli/src/initialize-env.ts
@@ -65,7 +65,10 @@ export async function initializeEnv(context: $TSContext, currentAmplifyMeta?: $T
     try {
       await sequential(initializationTasks);
     } catch (e) {
-      throw Error(`${currentEnv} environment not found. Try running "amplify env add" to add a new environment.`);
+      context.print.error(`Environment '${currentEnv}' not found.`);
+      context.print.info(`Try running "amplify env add" to add a new environment.`);
+      context.print.info(`If this backend already exists, try restoring it's definition in your team-provider-info.json file.`);
+      process.exit(1);
     }
 
     spinner.succeed(
