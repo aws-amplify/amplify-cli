6143e4bf6f27a6bfc62196be9a85dffe1f23ef50
Attila Hajdrik <hajdrik@amazon.com>
2020-11-02 09:40:52 -0800

    fix: #5662 - python fallback from python3 (#5744)
    * fix: #5662 - python fallback from python3

--- packages/amplify-python-function-runtime-provider/src/util/invokeUtil.ts
+++ packages/amplify-python-function-runtime-provider/src/util/invokeUtil.ts
@@ -5,6 +5,7 @@ import execa from 'execa';
 import path from 'path';
 import { pathManager } from 'amplify-cli-core';
 import { packageName, relativeShimPath } from '../constants';
+import { getPythonBinaryName } from './pyUtils';
 
 const shimPath = path.join(pathManager.getAmplifyPackageLibDirPath(packageName), relativeShimPath);
 
@@ -12,17 +13,29 @@ export async function pythonInvoke(context: any, request: InvocationRequest): Pr
   const handlerParts = path.parse(request.handler);
   const handlerFile = path.join(request.srcRoot, 'src', handlerParts.dir, handlerParts.name);
   const handlerName = handlerParts.ext.replace('.', '');
-  const childProcess = execa('pipenv', ['run', 'python3', shimPath, handlerFile + '.py', handlerName]);
+
+  const pyBinary = getPythonBinaryName();
+
+  if (!pyBinary) {
+    throw new Error(`Could not find 'python3' or 'python' executable in the PATH.`);
+  }
+
+  const childProcess = execa('pipenv', ['run', pyBinary, shimPath, handlerFile + '.py', handlerName]);
+
   childProcess.stdout.pipe(process.stdout);
   childProcess.stdin.write(JSON.stringify({ event: request.event, context: {} }) + '\n');
+
   const { stdout } = await childProcess;
   const lines = stdout.split('\n');
   const lastLine = lines[lines.length - 1];
+
   let result = lastLine;
+
   try {
     result = JSON.parse(lastLine);
   } catch (err) {
     context.print.warning('Could not parse function output as JSON. Using raw output.');
   }
+
   return result;
 }
