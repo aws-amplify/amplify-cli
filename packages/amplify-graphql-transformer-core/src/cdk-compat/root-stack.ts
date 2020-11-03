c08c020d16b8bab5573be2625c9771da49bbf2dc
Yathi <511386+yuth@users.noreply.github.com>
2020-11-03 07:44:00 -0800

    fix build error

--- packages/amplify-graphql-transformer-core/src/cdk-compat/root-stack.ts
+++ packages/amplify-graphql-transformer-core/src/cdk-compat/root-stack.ts
@@ -1,4 +1,4 @@
-import { CfnElement, Stack, CfnResource } from '@aws-cdk/core';
+import { CfnElement, Stack, CfnResource, ISynthesisSession } from '@aws-cdk/core';
 
 export class TransformerRootStack extends Stack {
   private readonly resourceTypeToPreserveLogicalName: string[] = [
@@ -36,7 +36,8 @@ export class TransformerRootStack extends Stack {
    * of stack sythesize to support that.
    * @param session
    */
-  public renderCloudFormationTemplate = (): string => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public renderCloudFormationTemplate = (_:ISynthesisSession): string => {
     return JSON.stringify((this as any)._toCloudFormation(), undefined, 2);
   };
 }
