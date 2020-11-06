6df9e819cb2462d5ddd4c531b3599a5780e11457
Attila Hajdrik <attila@eyedea.hu>
2020-11-06 09:24:32 -0800

    chore: address feedback

--- packages/amplify-cli/src/extensions/amplify-helpers/invoke-plugin-method.ts
+++ packages/amplify-cli/src/extensions/amplify-helpers/invoke-plugin-method.ts
@@ -17,19 +17,9 @@ export const invokePluginMethod = async <T>(
 
   const pluginMethod: any = plugin[method];
 
-  if (!pluginMethod) {
-    throw new Error(`Method ${method} does not exist in category plugin: ${category}.`);
+  if (!pluginMethod || typeof pluginMethod !== 'function') {
+    throw new Error(`Method ${method} does not exist or not a function in category plugin: ${category}.`);
   }
 
-  let methodResult = pluginMethod(...args);
-
-  // Check if result is a Promise and await it, return the unwrapped result
-  if (methodResult.then && typeof methodResult.then === 'function') {
-    return methodResult;
-  }
-
-  // Wrap the result in a promise
-  return new Promise((resolve, _) => {
-    resolve(methodResult);
-  });
+  return pluginMethod(...args);
 };
