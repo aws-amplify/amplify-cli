1229728492cdab7844ef1f2a98375446c47211a7
Edward Foyle <foyleef@amazon.com>
2020-11-13 15:35:20 -0500

    feat: modify upgrade nag for amplify upgrade command

--- packages/amplify-cli/src/version-notifier.ts
+++ packages/amplify-cli/src/version-notifier.ts
@@ -1,10 +1,15 @@
 import { JSONUtilities, $TSAny } from 'amplify-cli-core';
 import { default as updateNotifier } from 'update-notifier';
 import path from 'path';
+import chalk from 'chalk';
 
 const pkg = JSONUtilities.readJson<$TSAny>(path.join(__dirname, '..', 'package.json'));
 const notifier = updateNotifier({ pkg }); // defaults to 1 day interval
 
-export function notify(customMessage?: updateNotifier.NotifyOptions): void {
-  notifier.notify(customMessage);
+const defaultOpts: updateNotifier.NotifyOptions = {
+  message: `Update available:\nRun ${chalk.blueBright('amplify upgrade')} for the latest features and fixes!`,
+};
+export function notify(notifyOpts?: updateNotifier.NotifyOptions): void {
+  notifyOpts = { ...defaultOpts, ...notifyOpts };
+  notifier.notify(notifyOpts);
 }
