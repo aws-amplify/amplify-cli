f779a3e2d91c3f555f7b9e2627ab74be89f3a2ae
Edward Foyle <foyleef@amazon.com>
2020-11-12 23:02:20 -0500

    fix: more windows fixes

--- packages/amplify-cli/src/commands/uninstall.ts
+++ packages/amplify-cli/src/commands/uninstall.ts
@@ -1,6 +1,8 @@
 import { isPackaged, pathManager } from 'amplify-cli-core';
 import execa from 'execa';
 import * as fs from 'fs-extra';
+import * as path from 'path';
+import { pendingDeletePath, setRegPendingDelete } from '../utils/win-utils';
 
 export const run = async context => {
   if (
@@ -12,6 +14,17 @@ export const run = async context => {
   }
   if (!isPackaged) {
     await uninstallNodeCli();
+  } else if (process.platform.startsWith('win')) {
+    const binPath = path.join(pathManager.getHomeDotAmplifyDirPath(), 'bin', 'amplify.exe');
+    fs.move(binPath, pendingDeletePath);
+    try {
+      await setRegPendingDelete();
+    } catch (err) {
+      context.print.warning(err);
+      context.print.warning(
+        `Unable to set registry value marking Amplify binary for deletion. You can manually delete ${pendingDeletePath}.`,
+      );
+    }
   }
   await removeHomeDotAmplifyDir();
   context.print.success('Uninstalled the Amplify CLI');
