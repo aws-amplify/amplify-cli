d73f5e16a71f36a23c33667a8287029fccb8cedc
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2020-11-14 14:09:08 -0800

    feat: change cloudmap namespace name to consider vpc id

--- packages/amplify-provider-awscloudformation/src/network/stack.ts
+++ packages/amplify-provider-awscloudformation/src/network/stack.ts
@@ -1,178 +1,187 @@
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 import * as ecs from '@aws-cdk/aws-ecs';
-import * as discovery from "@aws-cdk/aws-servicediscovery";
+import * as discovery from '@aws-cdk/aws-servicediscovery';
 import * as apigw2 from '@aws-cdk/aws-apigatewayv2';
 
 type NetworkStackProps = {
-    stackName: string;
-    vpcName: string;
-    vpcId: string;
-    internetGatewayId: string;
-    subnetCidrs: ReadonlyMap<string, string>
+  stackName: string;
+  vpcName: string;
+  vpcId: string;
+  internetGatewayId: string;
+  subnetCidrs: ReadonlyMap<string, string>;
 };
 
 export const NETWORK_STACK_LOGICAL_ID = 'NetworkStack';
 
 export class NetworkStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props: NetworkStackProps) {
-        super(scope, id);
+  constructor(scope: cdk.Construct, id: string, props: NetworkStackProps) {
+    super(scope, id);
 
-        const { stackName, vpcId = '', vpcName, internetGatewayId = '', subnetCidrs = new Map<string, string>() } = props;
+    const { stackName, vpcId = '', vpcName, internetGatewayId = '', subnetCidrs = new Map<string, string>() } = props;
 
-        const { outputVpc, outputIgw, vpcCidrBlock } = createVpc(this, vpcId, vpcName, internetGatewayId);
+    const { outputVpc, outputIgw, vpcCidrBlock } = createVpc(this, vpcId, vpcName, internetGatewayId);
 
-        createAmplifyEnv(this, stackName, outputVpc as any, vpcCidrBlock, outputIgw as any, subnetCidrs);
-    }
+    createAmplifyEnv(this, stackName, outputVpc as any, vpcCidrBlock, outputIgw as any, subnetCidrs);
+  }
 }
 
 function createVpc(scope: cdk.Construct, vpcId: string, vpcName: string, internetGatewayId: string) {
-    const vpcCidrBlock = '10.0.0.0/16';
-    const condition = new cdk.CfnCondition(scope, 'UseNewVpcCondition', {
-        expression: cdk.Fn.conditionAnd(cdk.Fn.conditionEquals(vpcId, ''), cdk.Fn.conditionEquals(internetGatewayId, '')),
-    });
+  const vpcCidrBlock = '10.0.0.0/16';
+  const condition = new cdk.CfnCondition(scope, 'UseNewVpcCondition', {
+    expression: cdk.Fn.conditionAnd(cdk.Fn.conditionEquals(vpcId, ''), cdk.Fn.conditionEquals(internetGatewayId, '')),
+  });
 
-    const vpc = new ec2.Vpc(scope, vpcName, {
-        cidr: vpcCidrBlock,
-        subnetConfiguration: [],
-    });
-    (vpc.node.defaultChild as ec2.CfnVPC).tags.setTag('Name', vpcName);
+  const vpc = new ec2.Vpc(scope, vpcName, {
+    cidr: vpcCidrBlock,
+    subnetConfiguration: [],
+  });
+  (vpc.node.defaultChild as ec2.CfnVPC).tags.setTag('Name', vpcName);
 
-    const cfnVpc = <ec2.CfnVPC>vpc.node.defaultChild;
-    cfnVpc.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
-    cfnVpc.cfnOptions.condition = condition;
+  const cfnVpc = <ec2.CfnVPC>vpc.node.defaultChild;
+  cfnVpc.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
+  cfnVpc.cfnOptions.condition = condition;
 
-    const outputVpc = cdk.Fn.conditionIf(condition.logicalId, vpc.vpcId, vpcId);
-    new cdk.CfnOutput(scope, 'VpcId', {
-        value: outputVpc as any,
-    });
-    new cdk.CfnOutput(scope, 'VpcCidrBlock', {
-        value: vpcCidrBlock as any,
-    });
+  const outputVpc = cdk.Fn.conditionIf(condition.logicalId, vpc.vpcId, vpcId);
+  new cdk.CfnOutput(scope, 'VpcId', {
+    value: outputVpc as any,
+  });
+  new cdk.CfnOutput(scope, 'VpcCidrBlock', {
+    value: vpcCidrBlock as any,
+  });
 
-    const igw = new ec2.CfnInternetGateway(scope, 'InternetGateway');
-    igw.tags.setTag('Name', `${scope.node.id}/${igw.logicalId}`);
-    igw.cfnOptions.condition = condition;
-    igw.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
+  const igw = new ec2.CfnInternetGateway(scope, 'InternetGateway');
+  igw.tags.setTag('Name', `${scope.node.id}/${igw.logicalId}`);
+  igw.cfnOptions.condition = condition;
+  igw.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
 
-    const outputIgw = cdk.Fn.conditionIf(condition.logicalId, igw.ref, internetGatewayId);
-    new cdk.CfnOutput(scope, 'Igw', {
-        value: outputIgw as any,
-    });
+  const outputIgw = cdk.Fn.conditionIf(condition.logicalId, igw.ref, internetGatewayId);
+  new cdk.CfnOutput(scope, 'Igw', {
+    value: outputIgw as any,
+  });
 
-    const att = new ec2.CfnVPCGatewayAttachment(scope, 'VPCGatewayAttachment', {
-        vpcId: vpc.vpcId,
-        internetGatewayId: igw.ref,
-    });
-    att.cfnOptions.condition = condition;
-    att.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
+  const att = new ec2.CfnVPCGatewayAttachment(scope, 'VPCGatewayAttachment', {
+    vpcId: vpc.vpcId,
+    internetGatewayId: igw.ref,
+  });
+  att.cfnOptions.condition = condition;
+  att.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
 
-    return {
-        outputVpc,
-        outputIgw,
-        vpcCidrBlock,
-    };
+  return {
+    outputVpc,
+    outputIgw,
+    vpcCidrBlock,
+  };
 }
 
-function createAmplifyEnv(scope: cdk.Construct, envName: string, vpcId: string, vpcCidrBlock: string, igwId: string, subnetCidrs: ReadonlyMap<string, string>) {
-    const availabilityZones = [];
+function createAmplifyEnv(
+  scope: cdk.Construct,
+  envName: string,
+  vpcId: string,
+  vpcCidrBlock: string,
+  igwId: string,
+  subnetCidrs: ReadonlyMap<string, string>,
+) {
+  const availabilityZones = [];
 
-    const azSubnetMap = new cdk.CfnMapping(scope, "AzsMap");
-    subnetCidrs.forEach((cidr, az) => {
-        availabilityZones.push(az);
-        azSubnetMap.setValue(az, 'SubnetCidrBlock', cidr);
-    });
+  const azSubnetMap = new cdk.CfnMapping(scope, 'AzsMap');
+  subnetCidrs.forEach((cidr, az) => {
+    availabilityZones.push(az);
+    azSubnetMap.setValue(az, 'SubnetCidrBlock', cidr);
+  });
 
-    const vpc = ec2.Vpc.fromVpcAttributes(scope, 'vpc', { vpcId, availabilityZones });
+  const vpc = ec2.Vpc.fromVpcAttributes(scope, 'vpc', { vpcId, availabilityZones });
 
-    const cluster = new ecs.CfnCluster(scope, 'Cluster');
+  const cluster = new ecs.CfnCluster(scope, 'Cluster');
 
-    new cdk.CfnOutput(scope, "ClusterName", {
-        value: cdk.Fn.ref(cluster.logicalId)
-    });
+  new cdk.CfnOutput(scope, 'ClusterName', {
+    value: cdk.Fn.ref(cluster.logicalId),
+  });
 
-    const subnets: ec2.ISubnet[] = [];
+  const subnets: ec2.ISubnet[] = [];
 
-    const pubNacl = new ec2.NetworkAcl(scope, 'Nacl', {
-        vpc,
-    });
-    pubNacl.addEntry('Egress', {
-        cidr: ec2.AclCidr.anyIpv4(),
-        ruleNumber: 100,
-        traffic: ec2.AclTraffic.allTraffic(),
-        direction: ec2.TrafficDirection.EGRESS,
-        ruleAction: ec2.Action.ALLOW,
-    });
+  const pubNacl = new ec2.NetworkAcl(scope, 'Nacl', {
+    vpc,
+  });
+  pubNacl.addEntry('Egress', {
+    cidr: ec2.AclCidr.anyIpv4(),
+    ruleNumber: 100,
+    traffic: ec2.AclTraffic.allTraffic(),
+    direction: ec2.TrafficDirection.EGRESS,
+    ruleAction: ec2.Action.ALLOW,
+  });
 
-    pubNacl.addEntry(`IngressVPC`, {
-        cidr: ec2.AclCidr.ipv4(vpcCidrBlock),
-        ruleNumber: 200,
+  pubNacl.addEntry(`IngressVPC`, {
+    cidr: ec2.AclCidr.ipv4(vpcCidrBlock),
+    ruleNumber: 200,
+    traffic: ec2.AclTraffic.allTraffic(),
+    direction: ec2.TrafficDirection.INGRESS,
+    ruleAction: ec2.Action.DENY,
+  });
+
+  pubNacl.addEntry(`IngressInternet`, {
+    cidr: ec2.AclCidr.anyIpv4(),
+    ruleNumber: 300,
+    traffic: ec2.AclTraffic.allTraffic(),
+    direction: ec2.TrafficDirection.INGRESS,
+    ruleAction: ec2.Action.ALLOW,
+  });
+
+  Array.from(subnetCidrs.keys())
+    .sort()
+    .forEach((az, i) => {
+      const [, , azId] = az.split('-');
+
+      const cidr = azSubnetMap.findInMap(az, 'SubnetCidrBlock');
+
+      pubNacl.addEntry(`IngressEnv${azId}`, {
+        cidr: ec2.AclCidr.ipv4(cidr),
+        ruleNumber: 100 + i,
         traffic: ec2.AclTraffic.allTraffic(),
         direction: ec2.TrafficDirection.INGRESS,
-        ruleAction: ec2.Action.DENY,
-    });
-
-    pubNacl.addEntry(`IngressInternet`, {
-        cidr: ec2.AclCidr.anyIpv4(),
-        ruleNumber: 300,
-        traffic: ec2.AclTraffic.allTraffic(),
-        direction: ec2.TrafficDirection.INGRESS,
         ruleAction: ec2.Action.ALLOW,
+      });
+
+      const publicSubnetName = `PublicSubnet${azId}`;
+      const publicSubnet = new ec2.PublicSubnet(scope, publicSubnetName, {
+        vpcId,
+        cidrBlock: cidr,
+        availabilityZone: az,
+        mapPublicIpOnLaunch: true,
+      });
+
+      const cdfnPublicSubnet = publicSubnet.node.defaultChild as ec2.CfnSubnet;
+      cdfnPublicSubnet.tags.setTag('amplify-env', envName);
+
+      new ec2.CfnRoute(scope, `DefaultRoute${azId}`, {
+        routeTableId: publicSubnet.routeTable.routeTableId,
+        destinationCidrBlock: '0.0.0.0/0',
+        gatewayId: igwId,
+      });
+
+      // TODO: CDK doesn't return a CfnSubnet to set a custom logicalName
+      publicSubnet.associateNetworkAcl('', pubNacl);
+
+      subnets.push(publicSubnet);
     });
 
-    Array.from(subnetCidrs.keys()).sort().forEach((az, i) => {
-        const [, , azId] = az.split('-');
+  const subnetIds = subnets.map(({ subnetId }) => subnetId);
 
-        const cidr = azSubnetMap.findInMap(az, 'SubnetCidrBlock');
+  new cdk.CfnOutput(scope, 'SubnetIds', {
+    value: subnetIds.join(','),
+  });
 
-        pubNacl.addEntry(`IngressEnv${azId}`, {
-            cidr: ec2.AclCidr.ipv4(cidr),
-            ruleNumber: 100 + i,
-            traffic: ec2.AclTraffic.allTraffic(),
-            direction: ec2.TrafficDirection.INGRESS,
-            ruleAction: ec2.Action.ALLOW,
-        });
+  const vpcLink = new apigw2.CfnVpcLink(scope, 'VpcLink', {
+    name: `${envName}VpcLink`,
+    subnetIds,
+  });
 
-        const publicSubnetName = `PublicSubnet${azId}`;
-        const publicSubnet = new ec2.PublicSubnet(scope, publicSubnetName, {
-            vpcId,
-            cidrBlock: cidr,
-            availabilityZone: az,
-            mapPublicIpOnLaunch: true,
-        });
+  new cdk.CfnOutput(scope, 'VpcLinkId', { value: cdk.Fn.ref(vpcLink.logicalId) });
 
-        const cdfnPublicSubnet = (publicSubnet.node.defaultChild as ec2.CfnSubnet);
-        cdfnPublicSubnet.tags.setTag('amplify-env', envName);
+  const namespace = new discovery.PrivateDnsNamespace(scope, 'Namespace', {
+    vpc,
+    name: cdk.Fn.join('-', [envName, vpc.vpcId]),
+  });
 
-        new ec2.CfnRoute(scope, `DefaultRoute${azId}`, {
-            routeTableId: publicSubnet.routeTable.routeTableId,
-            destinationCidrBlock: '0.0.0.0/0',
-            gatewayId: igwId,
-        });
-
-        // TODO: CDK doesn't return a CfnSubnet to set a custom logicalName
-        publicSubnet.associateNetworkAcl('', pubNacl);
-
-        subnets.push(publicSubnet);
-    });
-
-    const subnetIds = subnets.map(({ subnetId }) => subnetId);
-
-    new cdk.CfnOutput(scope, 'SubnetIds', {
-        value: subnetIds.join(','),
-    });
-
-    const vpcLink = new apigw2.CfnVpcLink(scope, "VpcLink", {
-        name: `${envName}VpcLink`,
-        subnetIds
-    });
-
-    new cdk.CfnOutput(scope, "VpcLinkId", { value: cdk.Fn.ref(vpcLink.logicalId) });
-
-    const namespace = new discovery.PrivateDnsNamespace(scope, "Namespace", {
-        vpc,
-        name: envName,
-    });
-
-    new cdk.CfnOutput(scope, "CloudMapNamespaceId", { value: namespace.namespaceId });
+  new cdk.CfnOutput(scope, 'CloudMapNamespaceId', { value: namespace.namespaceId });
 }
