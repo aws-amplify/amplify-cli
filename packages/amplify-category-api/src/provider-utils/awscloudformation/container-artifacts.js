27a1e4416bb7c8fde471166baeb81ca046a376c3
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2020-11-15 17:39:10 -0800

    fix: template format

--- packages/amplify-category-api/src/provider-utils/awscloudformation/container-artifacts.js
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/container-artifacts.js
@@ -1,6 +1,5 @@
 export const containerFiles = {
-  Dockerfile: `
-FROM node:alpine
+  Dockerfile: `FROM node:alpine
 
 ENV PORT=8080
 EXPOSE 8080
@@ -14,51 +13,48 @@ RUN npm i
 
 CMD [ "node", "index.js" ]
     `,
-  'package.json': `
-    {
-      "name": "express-lasagna",
-      "version": "1.0.0",
-      "description": "",
-      "main": "index.js",
-      "scripts": {
-        "test": "echo \\"Error: no test specified\\" && exit 1"
-      },
-      "keywords": [],
-      "author": "",
-      "license": "ISC",
-      "dependencies": {
-        "express": "^4.17.1"
-      }
-    }  
-    `,
-  'index.js': `
-    const express = require("express");
-    const app = express();
-    const port = process.env.PORT;
-  
-    // Enable CORS for all methods
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*")
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-      next()
-    });
-    
-    app.get("/", (req, res) => {
-      const jwt = req.header("Authorization") || "";
-    
-      const [, jwtBody] = jwt.split(".");
-    
-      const obj = JSON.parse(
-        jwtBody ? Buffer.from(jwtBody, "base64").toString("utf-8") : "{}"
-      );
-    
-      const result = JSON.stringify(obj, null, 2);
-    
-      res.contentType("application/json").send(result);
-    });
-    
-    app.listen(port, () => {
-      console.log(\`Example app listening at http://localhost:\${port}\`);
-    });  
-    `,
+  'package.json': `{
+  "name": "express-lasagna",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \\"Error: no test specified\\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}`,
+  'index.js': `const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+app.get("/", (req, res) => {
+  const jwt = req.header("Authorization") || "";
+
+  const [, jwtBody] = jwt.split(".");
+
+  const obj = JSON.parse(
+    jwtBody ? Buffer.from(jwtBody, "base64").toString("utf-8") : "{}"
+  );
+
+  const result = JSON.stringify(obj, null, 2);
+
+  res.contentType("application/json").send(result);
+});
+
+app.listen(port, () => {
+  console.log(\`Example app listening at http://localhost:\${port}\`);
+});  
+`,
 };
