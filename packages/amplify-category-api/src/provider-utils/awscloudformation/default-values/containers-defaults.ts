75299d22aeed09aa085b8d3eb5c4784c33f36f3d
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2020-11-17 17:52:13 -0800

    fix: rename to .ts and set default name

--- packages/amplify-category-api/src/provider-utils/awscloudformation/default-values/containers-defaults.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/default-values/containers-defaults.ts
@@ -1,10 +1,9 @@
 const uuid = require('uuid');
 
 const getAllDefaults = project => {
-  const name = project.projectConfig.projectName.toLowerCase().replace(/[^0-9a-zA-Z]/gi, '');
   const [shortId] = uuid().split('-');
   const defaults = {
-    
+    resourceName: `container${shortId}`,
   };
 
   return defaults;
