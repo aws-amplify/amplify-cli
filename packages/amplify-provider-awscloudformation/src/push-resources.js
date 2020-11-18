b6ee1158c980bdea32b75cb357ee0b84e698e699
Ghosh <kaustavg@amazon.com>
2020-11-18 00:11:10 -0800

    change from -> by in the description

--- packages/amplify-provider-awscloudformation/src/push-resources.js
+++ packages/amplify-provider-awscloudformation/src/push-resources.js
@@ -514,7 +514,7 @@ function formNestedStack(context, projectDetails, categoryName, resourceName, se
 
   // Track Amplify Console generated stacks
   if (process.env.CLI_DEV_INTERNAL_DISABLE_AMPLIFY_APP_DELETION) {
-    nestedStack.Description = 'Root stack created from AWS Amplify Console';
+    nestedStack.Description = 'Root stack created by AWS Amplify Console';
   }
 
   const { amplifyMeta } = projectDetails;
