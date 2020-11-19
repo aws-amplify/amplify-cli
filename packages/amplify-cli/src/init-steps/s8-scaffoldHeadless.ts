26624220d96e3dfef1e67937f1c2c92941b968d8
Diego Costantino <diegoco@amazon.com>
2020-11-18 17:48:40 -0800

    feat(cli): scaffold headless project

--- packages/amplify-cli/src/init-steps/s8-scaffoldHeadless.ts
+++ packages/amplify-cli/src/init-steps/s8-scaffoldHeadless.ts
@@ -3,10 +3,9 @@ import * as path from 'path';
 import {
   pathManager,
   $TSContext,
-  NonEmptyDirectoryError,
-  exitOnNextTick,
   CLIContextEnvironmentProvider,
   FeatureFlags,
+  PathConstants,
 } from 'amplify-cli-core';
 import { insertAmplifyIgnore } from '../extensions/amplify-helpers/git-manager';
 
@@ -14,12 +13,29 @@ import { insertAmplifyIgnore } from '../extensions/amplify-helpers/git-manager';
  * Extract amplify project structure with backend-config and project-config
  */
 export async function scaffoldProjectHeadless(context: $TSContext) {
-  insertAmplifyIgnore(pathManager.getGitIgnoreFilePath(process.cwd()));
+  const projectPath = process.cwd();
+  const { projectName, frontend } = context.exeInfo.projectConfig;
 
   const skeletonLocalDir = path.join(__dirname, '..', '..', 'templates', 'amplify-skeleton');
-  const skeletonProjectDir = path.join(pathManager.getAmplifyDirPath(process.cwd()));
 
-  await fs.copy(skeletonLocalDir, skeletonProjectDir);
+  // create amplify folder
+  const destFolder = pathManager.getAmplifyDirPath(projectPath);
+  await fs.ensureDir(destFolder);
+
+  // create .config folder
+  await fs.ensureDir(pathManager.getDotConfigDirPath(projectPath));
+
+  // copy project-config.json file
+  const projectConfigFile = await fs.readJSON(
+    path.join(skeletonLocalDir, PathConstants.DotConfigDirName, `project-config__${frontend}.json`)
+  );
+  projectConfigFile['projectName'] = projectName;
+  await fs.writeJSON(pathManager.getProjectConfigFilePath(projectPath), projectConfigFile, { spaces: 4 });
+
+  // copy backend folder
+  await fs.copy(path.join(skeletonLocalDir, PathConstants.BackendDirName), pathManager.getBackendDirPath(projectPath))
+
+  insertAmplifyIgnore(pathManager.getGitIgnoreFilePath(projectPath));
 
   // Initialize feature flags
   const contextEnvironmentProvider = new CLIContextEnvironmentProvider({
