e4dc866c068ec08faa657171dc4764eb1f31ce68
Akshay <akshayupadhyay3@gmail.com>
2020-12-08 09:33:43 -0800

    chore: addressing comments

--- packages/amplify-codegen/src/utils/getCodegenPackageName.js
+++ packages/amplify-codegen/src/utils/getCodegenPackageName.js
@@ -1,5 +1,5 @@
 const CODEGEN_OLD_PACKAGE = 'amplify-codegen-appsync-model-plugin';
-const CODEGEN_NEW_PACKAGE = '@aws-amplify/app-sync-model-gen-plugin';
+const CODEGEN_NEW_PACKAGE = '@aws-amplify/appsync-modelgen-plugin';
 
 function getCodegenPackageName(isNewCodegenPackage) {
   return isNewCodegenPackage ? CODEGEN_NEW_PACKAGE : CODEGEN_OLD_PACKAGE;
