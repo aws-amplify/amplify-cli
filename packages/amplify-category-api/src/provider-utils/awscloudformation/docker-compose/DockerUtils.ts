af4800c6920b431f3571a657a92cde9d769fd433
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2020-12-08 15:11:32 -0800

    fix: create hash from source version to avoid invalid characters

--- packages/amplify-category-api/src/provider-utils/awscloudformation/docker-compose/DockerUtils.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/docker-compose/DockerUtils.ts
@@ -43,7 +43,7 @@ phases:
       - echo Logging in to Amazon ECR...
       - aws --version
       - aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-      - COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
+      - COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | md5sum | cut -c 1-7)
       - IMAGE_TAG=\${COMMIT_HASH:=latest}
   build:
     commands:
