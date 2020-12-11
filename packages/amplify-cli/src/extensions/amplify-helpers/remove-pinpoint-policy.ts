70061663a1a585392f779d48d0523f1838f22fe7
Nikhil Lingireddy <nikling@amazon.com>
2020-12-11 12:20:22 -0800

    refactor: addressing comments and using lodash

--- packages/amplify-cli/src/extensions/amplify-helpers/remove-pinpoint-policy.ts
+++ packages/amplify-cli/src/extensions/amplify-helpers/remove-pinpoint-policy.ts
@@ -3,21 +3,17 @@ import * as fs from 'fs-extra';
 import { pathManager, JSONUtilities } from 'amplify-cli-core';
 import _ from 'lodash';
 
-const projectBackendDirPath = pathManager.getBackendDirPath();
 const pinpointTemplateFileName = 'pinpoint-cloudformation-template.json';
 
 export function removeGetUserEndpoints(resourceName) {
-  const resourceDirPath = path.join(projectBackendDirPath, 'analytics', resourceName);
-  const pinpointTemplateFilePath = path.join(resourceDirPath, pinpointTemplateFileName);
+  const projectBackendDirPath = pathManager.getBackendDirPath();
+  const pinpointTemplateFilePath = path.join(projectBackendDirPath, 'analytics', resourceName, pinpointTemplateFileName);
   let pinpointTemplateFile: any = JSONUtilities.readJson(pinpointTemplateFilePath);
-  pinpointTemplateFile.Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action = [
-    'mobiletargeting:PutEvents',
-    'mobiletargeting:UpdateEndpoint',
-  ];
-  pinpointTemplateFile.Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action = [
-    'mobiletargeting:PutEvents',
-    'mobiletargeting:UpdateEndpoint',
-  ];
-  const pinpointTemplateFileJSON = JSONUtilities.stringify(pinpointTemplateFile);
-  fs.writeFileSync(pinpointTemplateFilePath, pinpointTemplateFileJSON);
+  const unAuthAction = _.get(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action');
+  const authAction = _.get(pinpointTemplateFile, 'Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action');
+  _.remove(unAuthAction, action => action === 'mobiletargeting:GetUserEndpoints');
+  _.remove(authAction, action => action === 'mobiletargeting:GetUserEndpoints');
+  _.set(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action', unAuthAction);
+  _.set(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action', authAction);
+  JSONUtilities.writeJson(pinpointTemplateFilePath, pinpointTemplateFile);
 }
