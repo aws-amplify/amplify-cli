24b5523a2513a2cf6561825270eb63da61d346d4
Nikhil Lingireddy <nikling@amazon.com>
2020-12-11 17:23:52 -0800

    refactor: remove getUserEndpoints on push (#6152)
    * refactor: remove getUserEndpoints on push
    
    * refactor: addressing comments and using lodash
    
    * fix: set auth policy
    
    * test: e2e for removal on push

--- packages/amplify-e2e-tests/src/__tests__/analytics.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/analytics.test.ts
@@ -1,8 +1,10 @@
-import { initJSProjectWithProfile, initFlutterProjectWithProfile, amplifyPushUpdate, deleteProject } from 'amplify-e2e-core';
+import { initJSProjectWithProfile, initFlutterProjectWithProfile, amplifyPushUpdate, amplifyStatus, deleteProject } from 'amplify-e2e-core';
 import { addPinpoint, addKinesis, removeAnalytics } from 'amplify-e2e-core';
 import { createNewProjectDir, deleteProjectDir } from 'amplify-e2e-core';
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import _ from 'lodash';
+import { JSONUtilities } from 'amplify-cli-core';
 
 describe('amplify add analytics', () => {
   let projRoot: string;
@@ -21,8 +23,46 @@ describe('amplify add analytics', () => {
     await initJSProjectWithProfile(projRoot, {});
     const rightName = 'myapp';
     await addPinpoint(projRoot, { rightName, wrongName: '$' });
-    await amplifyPushUpdate(projRoot);
-    expect(fs.existsSync(path.join(projRoot, 'amplify', 'backend', 'analytics', rightName))).toBe(true);
+    const pinpointTemplateFilePath = path.join(
+      projRoot,
+      'amplify',
+      'backend',
+      'analytics',
+      rightName,
+      'pinpoint-cloudformation-template.json',
+    );
+    expect(fs.existsSync(pinpointTemplateFilePath)).toBe(true);
+    let pinpointTemplateFile: any = JSONUtilities.readJson(pinpointTemplateFilePath);
+    let unAuthAction = _.get(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action');
+    let authAction = _.get(pinpointTemplateFile, 'Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action');
+    expect(_.includes(unAuthAction, 'mobiletargeting:GetUserEndpoints')).toBe(false);
+    expect(_.includes(authAction, 'mobiletargeting:GetUserEndpoints')).toBe(false);
+
+    // manually add getUserEndpoints policy to verify that it is removed on push
+    unAuthAction.push('mobiletargeting:GetUserEndpoints');
+    authAction.push('mobiletargeting:GetUserEndpoints');
+    _.set(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action', unAuthAction);
+    _.set(pinpointTemplateFile, 'Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action', authAction);
+    JSONUtilities.writeJson(pinpointTemplateFilePath, pinpointTemplateFile);
+    await amplifyPushUpdate(projRoot, /Pinpoint URL to track events.*/);
+    pinpointTemplateFile = JSONUtilities.readJson(pinpointTemplateFilePath);
+    unAuthAction = _.get(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action');
+    authAction = _.get(pinpointTemplateFile, 'Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action');
+    expect(_.includes(unAuthAction, 'mobiletargeting:GetUserEndpoints')).toBe(false);
+    expect(_.includes(authAction, 'mobiletargeting:GetUserEndpoints')).toBe(false);
+
+    // manually add getUserEndpoints policy to verify that it is removed on status
+    unAuthAction.push('mobiletargeting:GetUserEndpoints');
+    authAction.push('mobiletargeting:GetUserEndpoints');
+    _.set(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action', unAuthAction);
+    _.set(pinpointTemplateFile, 'Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action', authAction);
+    JSONUtilities.writeJson(pinpointTemplateFilePath, pinpointTemplateFile);
+    await amplifyStatus(projRoot, 'No Change');
+    pinpointTemplateFile = JSONUtilities.readJson(pinpointTemplateFilePath);
+    unAuthAction = _.get(pinpointTemplateFile, 'Resources.CognitoUnauthPolicy.Properties.PolicyDocument.Statement[0].Action');
+    authAction = _.get(pinpointTemplateFile, 'Resources.CognitoAuthPolicy.Properties.PolicyDocument.Statement[0].Action');
+    expect(_.includes(unAuthAction, 'mobiletargeting:GetUserEndpoints')).toBe(false);
+    expect(_.includes(authAction, 'mobiletargeting:GetUserEndpoints')).toBe(false);
   });
 
   it('add pinpoint for flutter', async () => {
