499077f863b41b0140be699598229fb90e527224
jhockett <hockett.john.m@gmail.com>
2021-01-22 14:19:24 -0800

    refactor: update types

--- packages/amplify-util-mock/src/CFNParser/lambda-resource-processor.ts
+++ packages/amplify-util-mock/src/CFNParser/lambda-resource-processor.ts
@@ -40,8 +40,8 @@ export function lambdaFunctionHandler(resourceName, resource, cfnContext: CloudF
   };
 }
 
-export function processResources(resources: { [key: string]: any }, params = {}): LambdaFunctionConfig | undefined {
-  const definition = Object.entries(resources).find((entry: [string, any]) => entry[1].Type === 'AWS::Lambda::Function');
+export function processResources(resources: $TSObject, params = {}): LambdaFunctionConfig | undefined {
+  const definition = Object.entries(resources).find((entry: [string, $TSAny]) => entry[1].Type === 'AWS::Lambda::Function');
   if (definition) {
     return lambdaFunctionHandler(definition[0], definition[1], {
       conditions: CFN_DEFAULT_CONDITIONS,
