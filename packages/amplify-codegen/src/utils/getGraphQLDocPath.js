96e16f7c4f57719a23a69035c359c16a3faafd44
Zeyu Li <lizeyu@amazon.com>
2021-02-01 15:50:39 -0800

    chore: push to see circleci tests

--- packages/amplify-codegen/src/utils/getGraphQLDocPath.js
+++ packages/amplify-codegen/src/utils/getGraphQLDocPath.js
@@ -5,6 +5,7 @@ function getGraphQLDocPath(frontend, graphQLDirectory, includePathGlob) {
   if (frontend === 'android') {
     return join(graphQLDirectory, 'com/amazonaws/amplify/generated/graphql');
   }
+  //TODO
   return includePathGlob ? globParent(includePathGlob) : graphQLDirectory;
 }
 
