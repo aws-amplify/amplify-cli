06495d01c31cdac825c77af430f9b8681c6dcd4e
jhockett <hockett.john.m@gmail.com>
2021-02-03 13:28:53 -0800

    refactor: simplify yes flag code to match rest of file

--- packages/amplify-provider-awscloudformation/src/configuration-manager.ts
+++ packages/amplify-provider-awscloudformation/src/configuration-manager.ts
@@ -727,9 +727,8 @@ async function determineAuthFlow(context: $TSContext, projectConfig?: ProjectCon
     ['exeInfo', 'inputParams', 'awscloudformation'],
     {},
   );
-  const yesFlag = !!_.get(context, ['exeInfo', 'inputParams', 'yes'], false);
 
-  if (yesFlag) {
+  if (context.exeInfo.inputParams.yes) {
     if (process.env.AWS_SDK_LOAD_CONFIG) {
       useProfile = useProfile === undefined ? true : useProfile;
       profileName = profileName || process.env.AWS_PROFILE || 'default';
@@ -757,7 +756,7 @@ async function determineAuthFlow(context: $TSContext, projectConfig?: ProjectCon
     return { ...awsConfig, type: 'accessKeys' };
   }
 
-  if (yesFlag) {
+  if (context.exeInfo.inputParams.yes) {
     const errorMessage = 'Failed to resolve AWS access keys with --yes flag.';
     const docsUrl = 'https://docs.amplify.aws/cli/usage/headless';
     context.print.error(errorMessage);
