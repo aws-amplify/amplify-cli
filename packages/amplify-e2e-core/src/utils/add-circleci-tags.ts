a6b158ca5ecfe206d9e30de5268a5de56a9578d9
Yathi <511386+yuth@users.noreply.github.com>
2021-02-08 23:27:11 -0800

    test: update tags for circleci tests
    updated circle ci tags in tests, delete hosting bucket and pass down env from current process if no
    env is passed

--- packages/amplify-e2e-core/src/utils/add-circleci-tags.ts
+++ packages/amplify-e2e-core/src/utils/add-circleci-tags.ts
@@ -1,7 +1,17 @@
 import { stateManager } from 'amplify-cli-core';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      getTestName?: () => string;
+      getHookName?: () => string;
+      getDescibeBlocks?: () => string[];
+    }
+  }
+}
+
 export const addCircleCITags = (projectPath: string): void => {
-  if (process.env && process.env['CIRCLE_CI']) {
+  if (process.env && process.env['CIRCLECI']) {
     const tags = stateManager.getProjectTags(projectPath);
 
     const addTagIfNotExist = (key: string, value: string): void => {
@@ -13,10 +23,25 @@ export const addCircleCITags = (projectPath: string): void => {
       }
     };
 
-    addTagIfNotExist('circleci', process.env['CIRCLE_CI'] || 'N/A');
+    addTagIfNotExist('circleci', process.env['CIRCLECI'] || 'N/A');
     addTagIfNotExist('circleci:branch', process.env['CIRCLE_BRANCH'] || 'N/A');
     addTagIfNotExist('circleci:sha1', process.env['CIRCLE_SHA1'] || 'N/A');
     addTagIfNotExist('circleci:workflow_id', process.env['CIRCLE_WORKFLOW_ID'] || 'N/A');
+    addTagIfNotExist('circleci:build_id', process.env['CIRCLE_BUILD_NUM'] || 'N/A');
+    addTagIfNotExist('circleci:build_url', process.env['CIRCLE_BUILD_URL'] || 'N/A');
+    addTagIfNotExist('circleci:job', process.env['CIRCLE_JOB'] || 'N/A');
+    // exposed by custom CLI test environment
+    if (global.getTestName) {
+      addTagIfNotExist('jest:test_name', global.getTestName().substr(0, 255) || 'N/A');
+    }
+    if (global.getHookName) {
+      addTagIfNotExist('jest:hook_name', global.getHookName().substr(0, 255) || 'N/A');
+    }
+    if (global.getDescibeBlocks) {
+      global.getDescibeBlocks().forEach((blockName, i) => {
+        addTagIfNotExist(`jest:describe_${i + 1}`, blockName.substr(0, 255) || 'N/A');
+      });
+    }
 
     stateManager.setProjectFileTags(projectPath, tags);
   }
