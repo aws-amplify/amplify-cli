7b953aaafc926370b7c3be7eacf8307763305679
Yathi <511386+yuth@users.noreply.github.com>
2021-02-12 18:00:29 -0800

    chore: remove unmaintained e2e tests

--- packages/amplify-ui-tests/src/init/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as initProjectWithProfile, initAndroidProject, initIosProject } from './initProjectHelper';
-export { amplifyPushApi, amplifyPush } from './amplifyPush';
-export { default as deleteProject } from './deleteProject';
