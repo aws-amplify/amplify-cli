0010f0fc2632743a4c5a53e72417a67cb0170866
Zeyu Li <lizeyu@amazon.com>
2021-02-23 16:12:06 -0800

    address CRs

--- packages/amplify-codegen/src/utils/getGraphQLDocPath.js
+++ packages/amplify-codegen/src/utils/getGraphQLDocPath.js
@@ -5,7 +5,7 @@ function getGraphQLDocPath(frontend, graphQLDirectory, includePathGlob) {
   if (frontend === 'android') {
     return join(graphQLDirectory, 'com/amazonaws/amplify/generated/graphql');
   }
-  return includePathGlob && includePathGlob.length
+  return Array.isArray(includePathGlob) && includePathGlob.length > 0
     ? globParent(includePathGlob[0])
     : graphQLDirectory;
 }
