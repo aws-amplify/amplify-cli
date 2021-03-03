28a1c773eb6a6f4c3040578b8d608465072744f9
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-03-03 12:05:09 -0800

    fix: address PR comments

--- packages/amplify-provider-awscloudformation/src/aws-utils/aws-amplify-backend.ts
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws-amplify-backend.ts
@@ -4,22 +4,25 @@ import { loadConfiguration } from '../configuration-manager';
 import { $TSContext } from 'amplify-cli-core';
 
 export class AmplifyBackend {
+  private static instance: AmplifyBackend;
+  private readonly context: $TSContext;
   public amplifyBackend: AWS.AmplifyBackend;
 
-  constructor(private readonly context: $TSContext, options = {}) {
-    const instancePromise = (async () => {
+  static async getInstance(context: $TSContext, options = {}): Promise<AmplifyBackend> {
+    if (!AmplifyBackend.instance) {
       let cred = {};
       try {
         cred = await loadConfiguration(context);
       } catch (e) {
         // ignore missing config
       }
+      AmplifyBackend.instance = new AmplifyBackend(context, cred, options);
+    }
+    return AmplifyBackend.instance;
+  }
 
-      this.amplifyBackend = new (aws as typeof AWS).AmplifyBackend({ ...cred, ...options });
-
-      return this;
-    })();
-
-    return <AmplifyBackend>(<unknown>instancePromise);
+  constructor(context: $TSContext, creds, options = {}) {
+    this.context = context;
+    this.amplifyBackend = new aws.AmplifyBackend({ ...creds, ...options });
   }
 }
