16b0bc2ce464ef10093c4d0f0686dcca6a806f4e
Akshay <akshayupadhyay3@gmail.com>
2021-03-05 14:21:07 -0800

    fix: replace length check with snapshot test

--- packages/graphql-transformer-core/src/__tests__/util/amplifyUtils.test.ts
+++ packages/graphql-transformer-core/src/__tests__/util/amplifyUtils.test.ts
@@ -13,20 +13,23 @@ describe('get sanity check rules', () => {
     expect(sanityCheckRules.projectRules.length).toBe(0);
   });
 
-  test.only('sanitycheck rule list when api is in update status and ff enabled', () => {
+  test('sanitycheck rule list when api is in update status and ff enabled', () => {
     const ff_mock = new AmplifyCLIFeatureFlagAdapter();
     (<any>FeatureFlags.getBoolean).mockReturnValue(true);
     const sanityCheckRules: SanityCheckRules = getSanityCheckRules(false, ff_mock);
-    console.log(sanityCheckRules.projectRules[0][0].Function);
-    expect(sanityCheckRules.diffRules.length).toBe(4);
-    expect(sanityCheckRules.projectRules.length).toBe(1);
+    const diffRulesFn = sanityCheckRules.diffRules.map(func => func.name);
+    const projectRulesFn = sanityCheckRules.projectRules.map(func => func.name);
+    expect(diffRulesFn).toMatchSnapshot();
+    expect(projectRulesFn).toMatchSnapshot();
   });
 
   test('sanitycheck rule list when api is in update status and no ff enabled', () => {
     const ff_mock = new AmplifyCLIFeatureFlagAdapter();
     (<any>FeatureFlags.getBoolean).mockReturnValue(false);
     const sanityCheckRules: SanityCheckRules = getSanityCheckRules(false, ff_mock);
-    expect(sanityCheckRules.diffRules.length).toBe(6);
-    expect(sanityCheckRules.projectRules.length).toBe(2);
+    const diffRulesFn = sanityCheckRules.diffRules.map(func => func.name);
+    const projectRulesFn = sanityCheckRules.projectRules.map(func => func.name);
+    expect(diffRulesFn).toMatchSnapshot();
+    expect(projectRulesFn).toMatchSnapshot();
   });
 });
