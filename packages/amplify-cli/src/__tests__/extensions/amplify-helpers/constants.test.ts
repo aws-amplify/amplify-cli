3477f2aa77e61558e830b088637db3c3e1651ec4
Daijiro Wachi <daijiro.wachi@gmail.com>
2021-03-16 13:58:54 +0900

    test(cli): use "as any" to enable overwriting const object

--- packages/amplify-cli/src/__tests__/extensions/amplify-helpers/constants.test.ts
+++ packages/amplify-cli/src/__tests__/extensions/amplify-helpers/constants.test.ts
@@ -4,8 +4,7 @@ describe('constants', () => {
   it('should freeze values', () => {
     expect(amplifyCLIConstants.AmplifyCLIDirName).toBe('amplify');
     expect(() => {
-      // @ts-ignore
-      amplifyCLIConstants.AmplifyCLIDirName = 'test';
+      (amplifyCLIConstants as any).AmplifyCLIDirName = 'test';
     }).toThrow(new TypeError("Cannot assign to read only property 'AmplifyCLIDirName' of object '#<Object>'"));
     expect(amplifyCLIConstants.AmplifyCLIDirName).toBe('amplify');
   });
