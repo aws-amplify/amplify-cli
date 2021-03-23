97b7de9dcc6e5ce05714dea78186253b8dcb5738
Nikhil Lingireddy <nikling@amazon.com>
2021-03-22 17:41:58 -0700

    Init and Configure DX changes (#6745)
    * refactor: initial commit
    
    * refactor: display/set all init defaults
    
    set project configuration defaults in init steps and frontend defaults in the frontend provider
    plugins
    
    * refactor: display information in configure
    
    * refactor: configure settings question
    
    * fix: fix functionality of envName and yes flags
    
    * refactor: addressing Q/A
    
    * refactor: addressing comments and test fixes
    
    * refactor: use print function
    
    * Init configure dx changes (#6907)
    
    * refactor: initial commit
    
    * Update config.yml
    
    * Update config.yml
    
    * fix: fix init test bug
    
    * test: test fixes
    
    * test: e2e test fixes
    
    * fix: remove unused imports
    
    * test: init migration fix
    
    * fix: address comments

--- packages/amplify-provider-awscloudformation/src/configuration-manager.ts
+++ packages/amplify-provider-awscloudformation/src/configuration-manager.ts
@@ -77,12 +77,16 @@ export async function configure(context: $TSContext) {
   context.exeInfo = context.exeInfo || context.amplify.getProjectDetails();
   normalizeInputParams(context);
   context.exeInfo.awsConfigInfo = getCurrentConfig(context);
-  await enableServerlessContainers(context);
+  if (context.exeInfo.inputParams.containerSetting) {
+    await enableServerlessContainers(context);
+  }
 
-  await newUserCheck(context);
-  printProfileInfo(context);
-  await setProjectConfigAction(context);
-  return await carryOutConfigAction(context);
+  if (context.exeInfo.inputParams.profileSetting) {
+    await newUserCheck(context);
+    printProfileInfo(context);
+    await setProjectConfigAction(context);
+    return await carryOutConfigAction(context);
+  }
 }
 
 async function enableServerlessContainers(context: $TSContext) {
