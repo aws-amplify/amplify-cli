bf0b768be1a791d0263a620435028f5e59fb09d2
SwaySway <josurui@amazon.com>
2021-03-30 12:51:20 -0700

    feat(cli): validate iterative rollback input

--- packages/amplify-cli/src/extensions/amplify-helpers/push-resources.ts
+++ packages/amplify-cli/src/extensions/amplify-helpers/push-resources.ts
@@ -10,8 +10,14 @@ export async function pushResources(
   context: $TSContext,
   category?: string,
   resourceName?: string,
-  filteredResources?: { category: string, resourceName: string }[],
+  filteredResources?: { category: string; resourceName: string }[],
 ) {
+  if (context.parameters.options['iterative-rollback']) {
+    // validate --iterative-rollback with --force
+    if (context.parameters.options.force)
+      throw new Error("Consider runnning 'amplify push --force' to iteratively rollback and deploy new changes.");
+    context.exeInfo.iterativeRollback = true;
+  }
   if (context.parameters.options.env) {
     const envName: string = context.parameters.options.env;
     const allEnvs = context.amplify.getAllEnvs();
@@ -23,15 +29,11 @@ export async function pushResources(
       context.exeInfo.projectConfig = stateManager.getProjectConfig(undefined, {
         throwIfNotExist: false,
       });
-
       context.exeInfo.localEnvInfo = getEnvInfo();
-
       if (context.exeInfo.localEnvInfo.envName !== envName) {
         context.exeInfo.localEnvInfo.envName = envName;
-
         stateManager.setLocalEnvInfo(context.exeInfo.localEnvInfo.projectPath, context.exeInfo.localEnvInfo);
       }
-
       await initializeEnv(context);
     } else {
       const errMessage = "Environment doesn't exist. Please use 'amplify init' to create a new environment";
@@ -55,6 +57,9 @@ export async function pushResources(
   let continueToPush = context.exeInfo && context.exeInfo.inputParams && context.exeInfo.inputParams.yes;
 
   if (!continueToPush) {
+    if (context.exeInfo.iterativeRollback) {
+      context.print.info('The CLI will rollback an the last known iterative deployment.');
+    }
     continueToPush = await context.amplify.confirmPrompt('Are you sure you want to continue?');
   }
 
