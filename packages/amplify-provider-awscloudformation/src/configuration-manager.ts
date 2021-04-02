9077aa5cfd92a949c89308b1a925fe0f2b6c36ef
jhockett <johocke@amazon.com>
2021-04-02 14:42:34 -0700

    fix(amplify-provider-awscloudformation): throw err profile missing key

--- packages/amplify-provider-awscloudformation/src/configuration-manager.ts
+++ packages/amplify-provider-awscloudformation/src/configuration-manager.ts
@@ -579,10 +579,16 @@ export async function loadConfigurationForEnv(context: $TSContext, env: string, 
       exitOnNextTick(1);
     }
   } else if (authType.type === 'profile') {
-    if (authType?.profileName) {
-      awsConfig = await systemConfigManager.getProfiledAwsConfig(context, authType.profileName);
-    } else {
-      throw Error('Project configuration invalid. Missing profile name.');
+    try {
+      if (authType?.profileName) {
+        awsConfig = await systemConfigManager.getProfiledAwsConfig(context, authType.profileName);
+      } else {
+        throw Error('Project configuration invalid. Missing profile name.');
+      }
+    } catch (e) {
+      context.print.error(`Failed to get profile: ${e.message || e}`);
+      await context.usageData.emitError(e);
+      exitOnNextTick(1);
     }
   } else if (authType.type === 'accessKeys') {
     awsConfig = loadConfigFromPath(projectConfigInfo.config.awsConfigFilePath);
@@ -703,6 +709,17 @@ export async function getAwsConfig(context: $TSContext): Promise<AwsConfig> {
   let awsConfig: AwsSdkConfig;
   if (awsConfigInfo.configLevel === 'project') {
     if (awsConfigInfo.config.useProfile) {
+      try {
+        if (awsConfigInfo.config.profileName) {
+          awsConfig = await systemConfigManager.getProfiledAwsConfig(context, awsConfigInfo.config.profileName);
+        } else {
+          throw Error('Project configuration invalid. Missing profile name.');
+        }
+      } catch (e) {
+        context.print.error(`Failed to get profile: ${e.message || e}`);
+        await context.usageData.emitError(e);
+        exitOnNextTick(1);
+      }
       awsConfig = await systemConfigManager.getProfiledAwsConfig(context, awsConfigInfo.config.profileName);
     } else {
       awsConfig = {
