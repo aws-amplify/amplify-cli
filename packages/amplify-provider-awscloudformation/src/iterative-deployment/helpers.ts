1ac80f949d7158d9277f62a3172a0c6412e5537b
Josue Ruiz <josurui@amazon.com>
2021-04-06 13:49:45 -0700

    Update helpers.ts
    remove unnecessary import

--- packages/amplify-provider-awscloudformation/src/iterative-deployment/helpers.ts
+++ packages/amplify-provider-awscloudformation/src/iterative-deployment/helpers.ts
@@ -1,5 +1,3 @@
-import * as path from 'path';
-
 import { DeployMachineContext, DeploymentMachineOp } from './state-machine';
 
 export const collectError = (context: DeployMachineContext, err: any, meta: any) => {
