abba2a2578de74ee218fbe58ea5d7e05bb347d10
Edward Foyle <foyleef@amazon.com>
2021-04-14 13:11:06 -0700

    fix: update iam role modifier

--- packages/amplify-cli-core/src/permissionBoundaryState.ts
+++ packages/amplify-cli-core/src/permissionBoundaryState.ts
@@ -5,8 +5,13 @@ import _ from 'lodash';
 const backendConfigObjectPath = ['providers', 'awscloudformation', 'PermissionBoundaryPolicyArn'];
 
 export const getPermissionBoundaryArn: () => string | undefined = () => {
-  const backendConfig = stateManager.getBackendConfig();
-  return _.get(backendConfig, backendConfigObjectPath) as string | undefined;
+  try {
+    const backendConfig = stateManager.getBackendConfig();
+    return _.get(backendConfig, backendConfigObjectPath) as string | undefined;
+  } catch (err) {
+    // uninitialized project
+    return undefined;
+  }
 };
 
 export const setPermissionBoundaryArn: (arn?: string) => void = arn => {
--- packages/amplify-provider-awscloudformation/src/pre-push-cfn-processor/modifiers/iam-role-permission-boundary-modifier.ts
+++ packages/amplify-provider-awscloudformation/src/pre-push-cfn-processor/modifiers/iam-role-permission-boundary-modifier.ts
@@ -2,13 +2,14 @@ import { getPermissionBoundaryArn } from 'amplify-cli-core';
 import Role from 'cloudform-types/types/iam/role';
 import { ResourceModifier } from '../pre-push-cfn-modifier';
 
-export const iamRolePermissionBoundaryModifier: ResourceModifier = async (resource: Role) => {
+export const iamRolePermissionBoundaryModifier: ResourceModifier<Role> = async resource => {
   if (resource?.Properties?.PermissionsBoundary) {
-    return; // don't modify an existing permission boundary
+    return resource; // don't modify an existing permission boundary
   }
   const policyArn = getPermissionBoundaryArn();
   if (!policyArn) {
-    return; // exit if no permission boundary specified
+    return resource; // exit if no permission boundary specified
   }
   resource.Properties.PermissionsBoundary = policyArn;
+  return resource;
 };
