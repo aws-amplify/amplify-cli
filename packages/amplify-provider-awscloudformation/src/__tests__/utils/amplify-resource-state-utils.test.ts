f585a8a3508e4fadc0282b4e94664dc50c2dcf70
RajRajhans <me@rajrajhans.com>
2021-04-17 18:02:55 +0530

    test(amplify-provider-awscloudformation): update tests for amplify-resource-state-utils.ts

--- packages/amplify-provider-awscloudformation/src/__tests__/utils/amplify-resource-state-utils.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/utils/amplify-resource-state-utils.test.ts
@@ -3,7 +3,7 @@ import { CloudFormation } from 'aws-sdk';
 
 const cfnClientStub = ({
   describeStackResources: ({ StackName }) => ({
-    promise: () => ({
+    promise: () => Promise.resolve({
       StackResources: [{
         LogicalResourceId: 'LogicalResourceIdTest1',
         PhysicalResourceId: 'PhysicalResourceIdTest',
@@ -11,7 +11,7 @@ const cfnClientStub = ({
     }),
   }),
   describeStacks: ({ StackName }) => ({
-    promise: () => ({
+    promise: () => Promise.resolve({
       Stacks: [{
         Outputs: [
           {
