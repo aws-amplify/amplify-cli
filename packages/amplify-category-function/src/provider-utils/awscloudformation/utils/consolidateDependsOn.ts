435d8062f19e1c98716137383b0058d987a42a02
Akshay <akshayupadhyay3@gmail.com>
2021-04-19 16:20:32 -0700

    fix: remove lodash get

--- packages/amplify-category-function/src/provider-utils/awscloudformation/utils/consolidateDependsOn.ts
+++ packages/amplify-category-function/src/provider-utils/awscloudformation/utils/consolidateDependsOn.ts
@@ -1,7 +1,5 @@
 import { $TSMeta } from 'amplify-cli-core';
 import { FunctionDependency } from 'amplify-function-plugin-interface';
-import _ from 'lodash';
-import { category } from '../../../constants';
 import { lambdaLayerSetting, resourceAccessSetting, ServiceName } from './constants';
 
 export const consolidateDependsOnForLambda = (
@@ -11,18 +9,18 @@ export const consolidateDependsOnForLambda = (
   selectedSettings: string[],
 ): FunctionDependency[] => {
   let updatedDependsOn: FunctionDependency[];
-  const prevFunctionParametersDependsOn: FunctionDependency[] = _.get(projectMeta, ['function', lambdaToUpdate, 'dependsOn'], []);
+  const prevFunctionParametersDependsOn: FunctionDependency[] = projectMeta?.function?.[`${lambdaToUpdate}`]?.dependsOn ?? [];
   if (selectedSettings.includes(resourceAccessSetting)) {
     // insert old lambdaLayer dependsOn if present
     const prevLayersDependsOn: FunctionDependency[] = prevFunctionParametersDependsOn.filter(
-      resource => _.get(projectMeta, [category, resource.resourceName, 'service'], []) === ServiceName.LambdaLayer,
+      resource => projectMeta?.function?.[`${resource.resourceName}`]?.service === ServiceName.LambdaLayer,
     );
     updatedDependsOn = currentDependsOn.concat(prevLayersDependsOn);
   }
   if (selectedSettings.includes(lambdaLayerSetting)) {
     //  insert resource dependsOn
     const prevDependsOnExcludingLayers: FunctionDependency[] = prevFunctionParametersDependsOn.filter(
-      resource => _.get(projectMeta, [category, resource.resourceName, 'service'], []) !== ServiceName.LambdaLayer,
+      resource => projectMeta?.function?.[`${resource.resourceName}`]?.service !== ServiceName.LambdaLayer,
     );
     updatedDependsOn = currentDependsOn.concat(prevDependsOnExcludingLayers);
   }
