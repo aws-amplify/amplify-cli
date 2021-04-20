bded9bd16cef8b0c1580aeffe28c1f071655015b
Raj Rajhans <me@rajrajhans.com>
2021-04-20 05:40:42 +0530

    test: add unit tests for amplify-resource-state-utils.ts (#7062)

---
+++ packages/amplify-provider-awscloudformation/src/__tests__/utils/amplify-resource-state-utils.test.ts
@@ -0,0 +1,61 @@
+import { getTableNames, getPreviousDeploymentRecord } from '../../utils/amplify-resource-state-utils';
+import { CloudFormation } from 'aws-sdk';
+
+const cfnClientStub = ({
+  describeStackResources: ({ StackName }) => ({
+    promise: () => Promise.resolve({
+      StackResources: [{
+        LogicalResourceId: 'LogicalResourceIdTest1',
+        PhysicalResourceId: 'PhysicalResourceIdTest',
+      }],
+    }),
+  }),
+  describeStacks: ({ StackName }) => ({
+    promise: () => Promise.resolve({
+      Stacks: [{
+        Outputs: [
+          {
+            OutputKey: 'GetAttLogicalResourceIdTest1TableName',
+            OutputValue: 'TestStackOutputValue1',
+          },
+          {
+            OutputKey: 'InvalidLogicalResourceIdTableName',
+            OutputValue: 'TestStackOutputValue2',
+          },
+        ],
+        Parameters: [
+          {
+            ParameterKey: 'TestParameterKey1',
+            ParameterValue: 'TestParameterValue1',
+          },
+        ],
+        Capabilities: [
+          'CAPABILITY_IAM',
+        ],
+      }],
+    }),
+  }),
+} as unknown) as CloudFormation;
+
+describe('amplify-resource-state-utils', () => {
+  const StackID = 'TestSTackID';
+
+  test('test getTableNames', async () => {
+    const tables = ['LogicalResourceIdTest1', 'LogicalResourceIdTest2'];
+    const expectedTableNameMap: Map<string, string> = new Map();
+    expectedTableNameMap.set('LogicalResourceIdTest1', 'TestStackOutputValue1');
+
+    const tableNames = await getTableNames(cfnClientStub, tables, StackID);
+    expect(tableNames).toEqual(expectedTableNameMap);
+  });
+
+  test('test getPreviousDeploymentRecord', async () => {
+    const expectedPrevDeploymentRecord = {
+      capabilities: ['CAPABILITY_IAM'],
+      parameters: { TestParameterKey1: 'TestParameterValue1' },
+    };
+    const prevDeploymentRecord = await getPreviousDeploymentRecord(cfnClientStub, StackID);
+
+    expect(prevDeploymentRecord).toEqual(expectedPrevDeploymentRecord);
+  });
+});
