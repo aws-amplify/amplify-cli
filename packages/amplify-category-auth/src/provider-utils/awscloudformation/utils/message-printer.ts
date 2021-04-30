a4af0b4eb399238800553599244a5e1822e7f109
Yathi <511386+yuth@users.noreply.github.com>
2021-04-30 11:53:17 -0700

    feat: display SNS sandbox status

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/message-printer.ts
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/message-printer.ts
@@ -1,3 +1,5 @@
+import { ServiceQuestionsResult } from '../service-walkthrough-types';
+
 /**
  * A factory function that returns a function that prints the "success message" after adding auth
  * @param print The amplify print object
@@ -24,4 +26,21 @@ const printCommonText = (print: any) => {
     '"amplify publish" will build all your local backend and frontend resources (if you have hosting category added) and provision it in the cloud',
   );
   print.info('');
+};
+
+const printSMSSandboxWarning = (print: any) => {
+  print.info('');
+  print.info('TODO: Add message about SMS Sandbox and graduation');
+};
+
+export const doesConfigurationIncludeSMS = (request: ServiceQuestionsResult): boolean => {
+  if ((request.mfaConfiguration === 'OPTIONAL' || request.mfaConfiguration === 'ON') && request.mfaTypes?.includes('SMS Text Message')) {
+    return true;
+  }
+
+  if (request.usernameAttributes?.includes('phone_number')) {
+    return true;
+  }
+
+  return false;
 };
