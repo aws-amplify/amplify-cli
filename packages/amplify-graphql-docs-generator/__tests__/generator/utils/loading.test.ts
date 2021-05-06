9a73d5b3d4f9495a28c32149fb8aa9a2641078aa
Kihara, Takuya <gray@tacck.net>
2021-05-06 21:30:44 +0900

    test: use path.join for concatenate paths

--- packages/amplify-graphql-docs-generator/__tests__/generator/utils/loading.test.ts
+++ packages/amplify-graphql-docs-generator/__tests__/generator/utils/loading.test.ts
@@ -1,13 +1,14 @@
 import { loadSchema } from '../../../src/generator/utils/loading';
+import { join } from 'path';
 
 describe('loading', () => {
   it('JSON schema', () => {
-    const graphqlSchema = loadSchema(__dirname + '/../../../fixtures/schema.json');
+    const graphqlSchema = loadSchema(join(__dirname, '/../../../fixtures/schema.json'));
     expect(graphqlSchema.constructor.name).toBe('GraphQLSchema');
   });
 
   it('JSON schema without data property', () => {
-    const graphqlSchema = loadSchema(__dirname + '/../../../fixtures/schemaWithoutDataProperty.json');
+    const graphqlSchema = loadSchema(join(__dirname, '/../../../fixtures/schemaWithoutDataProperty.json'));
     expect(graphqlSchema.constructor.name).toBe('GraphQLSchema');
   });
   it('JSON schema file is not exits', () => {
@@ -15,12 +16,12 @@ describe('loading', () => {
   });
 
   it('JSON schema file is invalid', () => {
-    expect(() => loadSchema(__dirname + '/../../../fixtures/invalidSchema.json')).toThrow(
+    expect(() => loadSchema(join(__dirname, '/../../../fixtures/invalidSchema.json'))).toThrow(
       'GraphQL schema file should contain a valid GraphQL introspection query result',
     );
   });
   it('empty GraphQL schema', () => {
-    const graphqlSchema = loadSchema(__dirname + '/../../../fixtures/empty.graphql');
+    const graphqlSchema = loadSchema(join(__dirname, '/../../../fixtures/empty.graphql'));
     expect(graphqlSchema.constructor.name).toBe('GraphQLSchema');
   });
 });
