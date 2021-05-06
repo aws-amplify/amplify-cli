2d337765e0c884b71e5ee1fc7a70b38d2e749421
Yoshiaki Togami <tuabobo123@gmail.com>
2021-05-07 01:39:30 +0900

    test(cli): validate the args passed to appendFileSync and writeFileSync

--- packages/amplify-cli/src/__tests__/extensions/amplify-helpers/git-manager.test.ts
+++ packages/amplify-cli/src/__tests__/extensions/amplify-helpers/git-manager.test.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs-extra';
+import * as os from 'os';
+import { LocalLogDirectory } from 'amplify-cli-logger';
 import { insertAmplifyIgnore } from '../../../extensions/amplify-helpers/git-manager';
 jest.mock('fs-extra');
 
@@ -7,16 +9,46 @@ const fsMock = fs as jest.Mocked<typeof fs>;
 fsMock.readFileSync.mockImplementation(() => 'test');
 fsMock.writeFileSync.mockImplementation();
 
+const amplifyMark = '#amplify';
+
+const ignoreList = [
+  'amplify/\\#current-cloud-backend',
+  'amplify/.config/local-*',
+  `amplify/${LocalLogDirectory}`,
+  'amplify/mock-data',
+  'amplify/backend/amplify-meta.json',
+  'amplify/backend/awscloudformation',
+  'amplify/backend/.temp',
+  'build/',
+  'dist/',
+  'node_modules/',
+  'aws-exports.js',
+  'awsconfiguration.json',
+  'amplifyconfiguration.json',
+  'amplifyconfiguration.dart',
+  'amplify-build-config.json',
+  'amplify-gradle-config.json',
+  'amplifytools.xcconfig',
+  '.secret-*',
+];
+
+const toAppend = `${os.EOL + os.EOL + amplifyMark + os.EOL}${ignoreList.join(os.EOL)}`;
+
 describe('git-manager', () => {
-  const gitIgnoreFilePath = '';
+  beforeEach(() => {
+    fsMock.writeFileSync.mockClear();
+  });
+  const gitIgnoreFilePath = 'testPath';
   test('append files should be excluded to .gitignore', () => {
     fsMock.existsSync.mockImplementation(() => true);
     insertAmplifyIgnore(gitIgnoreFilePath);
-    expect(fsMock.appendFileSync).toHaveBeenCalled();
+    expect(fsMock.appendFileSync.mock.calls[0][0]).toEqual('testPath');
+    expect(fsMock.appendFileSync.mock.calls[0][1]).toEqual(toAppend);
   });
   test('create a new .gitignore', () => {
     fsMock.existsSync.mockImplementation(() => false);
     insertAmplifyIgnore(gitIgnoreFilePath);
-    expect(fsMock.writeFileSync).toHaveBeenCalled();
+    expect(fsMock.writeFileSync.mock.calls[0][0]).toEqual('testPath');
+    expect(fsMock.writeFileSync.mock.calls[0][1]).toEqual(toAppend.trim());
   });
 });
