83196aa060731c52f0a086ad72369428c64e5bc4
Christian Lewis <1657236+ctjlewis@users.noreply.github.com>
2021-05-13 21:49:26 -0500

    chore: clarify CAPTCHA logic

--- packages/amplify-category-auth/provider-utils/awscloudformation/triggers/VerifyAuthChallengeResponse/captcha-verify.js
+++ packages/amplify-category-auth/provider-utils/awscloudformation/triggers/VerifyAuthChallengeResponse/captcha-verify.js
@@ -2,16 +2,21 @@
 const axios = require('axios');
 /* eslint-enable */
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event) => {
   const response = await axios.post(
     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHASECRET}&response=${event.request.challengeAnswer}`,
     {},
   );
-
-  if (!(response && response.data && response.data.success)) {
+  /**
+   * If the CAPTCHA challenge succeeded, set the `answerCorrect` field to
+   * `true`; otherwise, set it to `false` and throw an error.
+   */
+  const challengeSucceeded = response && response.data && response.data.success;
+  if (challengeSucceeded) {
+    event.response.answerCorrect = true;
+  } else {
     event.response.answerCorrect = false;
-    throw new Error('captcha verification error');
+    throw new Error('CAPTCHA verification error');
   }
-
   return event;
 };
