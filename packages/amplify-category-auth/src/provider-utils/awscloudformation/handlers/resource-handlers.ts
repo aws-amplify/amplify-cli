3dbb3bfc199fdd7faac68cdee236d2625d6fb1ea
Yathi <511386+yuth@users.noreply.github.com>
2021-05-20 16:38:38 -0700

    feat: prep work for Cognito SMS Sandbox #2 (#7338)
    * feat: display SNS sandbox status
    
    * chore: SNS API error handling
    
    * chore: add support for SMS Sandbox check
    
    * chore: auth add show sandbox warning
    
    * chore: add some tests
    
    * chore: address review comments and remove unused code

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/handlers/resource-handlers.ts
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/handlers/resource-handlers.ts
@@ -2,8 +2,9 @@ import { ServiceQuestionsResult } from '../service-walkthrough-types';
 import { getAddAuthDefaultsApplier, getUpdateAuthDefaultsApplier } from '../utils/auth-defaults-appliers';
 import { getResourceSynthesizer, getResourceUpdater } from '../utils/synthesize-resources';
 import { getPostAddAuthMetaUpdater, getPostUpdateAuthMetaUpdater } from '../utils/amplify-meta-updaters';
-import { getPostAddAuthMessagePrinter, getPostUpdateAuthMessagePrinter } from '../utils/message-printer';
+import { getPostAddAuthMessagePrinter, getPostUpdateAuthMessagePrinter, printSMSSandboxWarning } from '../utils/message-printer';
 import { supportedServices } from '../../supported-services';
+import { doesConfigurationIncludeSMS } from '../utils/auth-sms-workflow-helper';
 
 /**
  * Factory function that returns a ServiceQuestionsResult consumer that handles all of the resource generation logic.
@@ -13,43 +14,48 @@ import { supportedServices } from '../../supported-services';
 export const getAddAuthHandler = (context: any) => async (request: ServiceQuestionsResult) => {
   const serviceMetadata = supportedServices[request.serviceName];
   const { cfnFilename, defaultValuesFilename, provider } = serviceMetadata;
+
   let projectName = context.amplify.getProjectConfig().projectName.toLowerCase();
   const disallowedChars = /[^A-Za-z0-9]+/g;
   projectName = projectName.replace(disallowedChars, '');
+
   const requestWithDefaults = await getAddAuthDefaultsApplier(context, defaultValuesFilename, projectName)(request);
-  await getResourceSynthesizer(
-    context,
-    cfnFilename,
-    provider,
-  )(requestWithDefaults)
-    .then(req => req.resourceName!)
-    .then(getPostAddAuthMetaUpdater(context, { service: requestWithDefaults.serviceName, providerName: provider }))
-    .then(getPostAddAuthMessagePrinter(context.print))
-    .catch(err => {
-      context.print.info(err.stack);
-      context.print.error('There was an error adding the auth resource');
-      context.usageData.emitError(err);
-      process.exitCode = 1;
-    });
+
+  try {
+    await getResourceSynthesizer(context, cfnFilename, provider)(requestWithDefaults);
+    await getPostAddAuthMetaUpdater(context, { service: requestWithDefaults.serviceName, providerName: provider })(
+      requestWithDefaults.resourceName!,
+    );
+    await getPostAddAuthMessagePrinter(context.print)(requestWithDefaults.resourceName!);
+
+    if (doesConfigurationIncludeSMS(request)) {
+      await printSMSSandboxWarning(context.print);
+    }
+  } catch (err) {
+    context.print.info(err.stack);
+    context.print.error('There was an error adding the auth resource');
+    context.usageData.emitError(err);
+    process.exitCode = 1;
+  }
   return requestWithDefaults.resourceName!;
 };
 
 export const getUpdateAuthHandler = (context: any) => async (request: ServiceQuestionsResult) => {
   const { cfnFilename, defaultValuesFilename, provider } = supportedServices[request.serviceName];
   const requestWithDefaults = await getUpdateAuthDefaultsApplier(context, defaultValuesFilename, context.updatingAuth)(request);
-  await getResourceUpdater(
-    context,
-    cfnFilename,
-    provider,
-  )(requestWithDefaults)
-    .then(req => req.resourceName!)
-    .then(getPostUpdateAuthMetaUpdater(context))
-    .then(getPostUpdateAuthMessagePrinter(context.print))
-    .catch(err => {
-      context.print.info(err.stack);
-      context.print.error('There was an error updating the auth resource');
-      context.usageData.emitError(err);
-      process.exitCode = 1;
-    });
+  try {
+    await getResourceUpdater(context, cfnFilename, provider)(requestWithDefaults);
+    await getPostUpdateAuthMetaUpdater(context)(requestWithDefaults.resourceName!);
+    await getPostUpdateAuthMessagePrinter(context.print)(requestWithDefaults.resourceName!);
+
+    if (doesConfigurationIncludeSMS(requestWithDefaults)) {
+      await printSMSSandboxWarning(context.print);
+    }
+  } catch (err) {
+    context.print.info(err.stack);
+    context.print.error('There was an error updating the auth resource');
+    context.usageData.emitError(err);
+    process.exitCode = 1;
+  }
   return requestWithDefaults.resourceName!;
 };
---
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws-sns.ts
@@ -0,0 +1,34 @@
+import { $TSAny, $TSContext } from 'amplify-cli-core';
+import { loadConfiguration } from '../configuration-manager';
+import aws from './aws.js';
+
+export class SNS {
+  private static instance: SNS;
+  private readonly sns: AWS.SNS;
+
+  static async getInstance(context: $TSContext, options = {}): Promise<SNS> {
+    if (!SNS.instance) {
+      let cred = {};
+      try {
+        cred = await loadConfiguration(context);
+      } catch (e) {
+        // ignore missing config
+      }
+
+      SNS.instance = new SNS(context, cred, options);
+    }
+    return SNS.instance;
+  }
+
+  private constructor(context: $TSContext, cred: $TSAny, options = {}) {
+    this.sns = new aws.SNS({ ...cred, ...options });
+  }
+
+  public async isInSandboxMode(): Promise<boolean> {
+    // AWS SDK still does not have getSMSSandboxAccountStatus. Casting sns to any to avoid compile error
+    // Todo: remove any casting once aws-sdk is updated
+    const snsClient = this.sns as any;
+    const result = await snsClient.getSMSSandboxAccountStatus().promise();
+    return result.IsInSandbox;
+  }
+}
