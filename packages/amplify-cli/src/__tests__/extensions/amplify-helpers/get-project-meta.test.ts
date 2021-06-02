714228d1efb8fe380ebf2da16fde585494945cd8
Yoshiaki Togami <tuabobo123@gmail.com>
2021-06-02 10:50:50 +0900

    test(cli): update the test to check for the error message

--- packages/amplify-cli/src/__tests__/extensions/amplify-helpers/get-project-meta.test.ts
+++ packages/amplify-cli/src/__tests__/extensions/amplify-helpers/get-project-meta.test.ts
@@ -1,15 +1,20 @@
 import { getProjectMeta } from '../../../extensions/amplify-helpers/get-project-meta';
 import { stateManager } from 'amplify-cli-core';
-jest.mock('amplify-cli-core', () => ({
-  stateManager: {
-    metaFileExists: jest.fn(),
-    getMeta: jest.fn().mockImplementation(() => ({
-      auth: {
-        amplifyAuth: 'test',
-      },
-    })),
-  },
-}));
+
+jest.mock('amplify-cli-core', () => {
+  const original = jest.requireActual('amplify-cli-core');
+  return {
+    ...original,
+    stateManager: {
+      metaFileExists: jest.fn(),
+      getMeta: jest.fn().mockImplementation(() => ({
+        auth: {
+          amplifyAuth: 'test',
+        },
+      })),
+    },
+  };
+});
 
 const stateManager_mock = stateManager as jest.Mocked<typeof stateManager>;
 
@@ -25,6 +30,10 @@ describe('getProjectMeta', () => {
   });
   it('should throw NotInitializedError when metaFile does not exists', () => {
     stateManager_mock.metaFileExists.mockImplementation(() => false);
-    expect(() => getProjectMeta()).toThrow();
+    expect(() => getProjectMeta()).toThrow(`
+      No Amplify backend project files detected within this folder. Either initialize a new Amplify project or pull an existing project.
+      - "amplify init" to initialize a new Amplify project
+      - "amplify pull <app-id>" to pull your existing Amplify project. Find the <app-id> in the AWS Console or Amplify Admin UI.
+      `);
   });
 });
