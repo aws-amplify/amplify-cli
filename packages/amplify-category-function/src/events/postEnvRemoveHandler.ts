0d3f9bf6ba8703a5ecf2c1fa5e6edbf6a92d9e2c
Edward Foyle <foyleef@amazon.com>
2021-06-10 14:32:00 -0700

    feat: add post env remove hook

--- packages/amplify-category-function/src/events/postEnvRemoveHandler.ts
+++ packages/amplify-category-function/src/events/postEnvRemoveHandler.ts
@@ -1,4 +1,9 @@
-export const postEnvRemoveHandler = async () => {
-  // loop over all functions in removed env
-  // if any have secrets, remove all secrets
+import { $TSContext } from 'amplify-cli-core';
+import { FunctionSecretsStateManager } from '../provider-utils/awscloudformation/secrets/functionSecretsStateManager';
+
+export const postEnvRemoveHandler = async (context: $TSContext, envName: string) => {
+  await removeAllEnvSecrets(context, envName);
 };
+
+const removeAllEnvSecrets = async (context: $TSContext, envName: string) =>
+  (await FunctionSecretsStateManager.getInstance(context)).deleteAllEnvironmentSecrets(envName);
