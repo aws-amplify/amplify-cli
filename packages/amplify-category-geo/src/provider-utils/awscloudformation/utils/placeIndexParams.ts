4b45ea912fce503f5ca6f6f12ba3d4b57dda6cb0
Zeyu Li <lizeyu@amazon.com>
2021-06-22 16:50:53 -0700

    feat(amplify-category-geo): add walkthrough for place index

---
+++ packages/amplify-category-geo/src/provider-utils/awscloudformation/utils/placeIndexParams.ts
@@ -0,0 +1,24 @@
+import { DataProvider, ResourceParameters } from './resourceParams';
+
+/**
+ * Data structure that represents a Map Resource
+ */
+export type PlaceIndexParameters = ResourceParameters & {
+    providerContext: ProviderContext; // higher level context around the function
+    placeIndexName: string; // name of the place index
+    indexPolicyName: string;
+    dataProvider: DataProvider;
+    dataSourceIntendedUse: DataSourceIntendedUse;
+    isDefaultPlaceIndex: boolean;
+};
+
+export enum DataSourceIntendedUse {
+  SingleUse = "SingleUse",
+  Storage = "Storage"
+}
+
+export interface ProviderContext {
+  provider: string;
+  service: string;
+  projectName: string;
+}
