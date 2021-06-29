ac11ffc12ea31ee9925fc3aebc3c080b32a9c498
David Lopez <lopezbnd@amazon.com>
2021-06-29 01:40:39 +0000

    fix(amplify-category-auth): rename function for amplify version 4.30
    Changes the name of the function to be more clear that it's used for Amplify 4.30

--- packages/amplify-e2e-core/src/categories/auth.ts
+++ packages/amplify-e2e-core/src/categories/auth.ts
@@ -545,7 +545,7 @@ export function addAuthWithSignInSignOutUrl(cwd: string, settings: any): Promise
   });
 }
 
-export function addAuthWithDefaultSocial_v30(cwd: string, settings: any): Promise<void> {
+export function addAuthWithDefaultSocial_v4_30(cwd: string, settings: any): Promise<void> {
   return new Promise((resolve, reject) => {
     const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, GOOGLE_APP_ID, GOOGLE_APP_SECRET, AMAZON_APP_ID, AMAZON_APP_SECRET } = getSocialProviders(
       true,
