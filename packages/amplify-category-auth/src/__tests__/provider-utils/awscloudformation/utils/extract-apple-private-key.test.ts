6b5aa7c0c2c95affc6a2b06a9048986b7914bbb2
David Lopez <lopezbnd@amazon.com>
2021-07-01 23:25:48 +0000

    fix(amplify-category-auth): remove un-needed duplicate unit test

--- packages/amplify-category-auth/src/__tests__/provider-utils/awscloudformation/utils/extract-apple-private-key.test.ts
+++ packages/amplify-category-auth/src/__tests__/provider-utils/awscloudformation/utils/extract-apple-private-key.test.ts
@@ -10,16 +10,6 @@ describe('When extracting apple private key...', () => {
     expect(extractApplePrivateKey(input)).toEqual(expectedOutput);
   });
 
-  it('it should remove new lines and space', () => {
-    const input = `-----BEGIN PRIVATE KEY-----
-        MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgIltgNsTgTfSzUadY
-        iCS0VYtDDMFln/J8i1yJsSIw5g+gCgYIKoZIzj0DAQehRANCAASI8E0L/DhR/mIf
-        TT07v3VwQu6q8I76lgn7kFhT0HvWoLuHKGQFcFkXXCgztgBrprzd419mUChAnKE6
-        y89bWcNw
-        -----END PRIVATE KEY-----`;
-    expect(extractApplePrivateKey(input)).toEqual(expectedOutput);
-  });
-
   it('it should not alter a pre extracted key', () => {
     const input = expectedOutput;
     expect(extractApplePrivateKey(input)).toEqual(expectedOutput);
