94431827d733dd33159e246c8ea6980860a3c066
Zeyu Li <lizeyu@amazon.com>
2021-07-09 13:12:40 -0700

    test: add e2e test for geo

--- packages/amplify-e2e-core/src/utils/sdk-calls.ts
+++ packages/amplify-e2e-core/src/utils/sdk-calls.ts
@@ -13,7 +13,11 @@ import {
   CloudFormation,
   AmplifyBackend,
   IAM,
+<<<<<<< HEAD
   SSM,
+=======
+  Location,
+>>>>>>> bab6d1bdc... test: add e2e test for geo
 } from 'aws-sdk';
 import * as path from 'path';
 import _ from 'lodash';
@@ -311,6 +315,7 @@ export const listAttachedRolePolicies = async (roleName: string, region: string)
   return (await service.listAttachedRolePolicies({ RoleName: roleName }).promise()).AttachedPolicies;
 };
 
+<<<<<<< HEAD
 export const getPermissionsBoundary = async (roleName: string, region) => {
   const iamClient = new IAM({ region });
   return (await iamClient.getRole({ RoleName: roleName }).promise())?.Role?.PermissionsBoundary?.PermissionsBoundaryArn;
@@ -328,3 +333,19 @@ export const getSSMParameters = async (region: string, appId: string, envName: s
     })
     .promise();
 };
+=======
+//Amazon location service calls
+export const getMap = async (mapName: string, region: string) => {
+  const service = new Location({region});
+  return await service.describeMap({
+    MapName: mapName
+  }).promise()
+}
+
+export const getPlaceIndex = async (placeIndexName: string, region: string) => {
+  const service = new Location({region});
+  return await service.describePlaceIndex({
+    IndexName: placeIndexName
+  }).promise()
+}
+>>>>>>> bab6d1bdc... test: add e2e test for geo
