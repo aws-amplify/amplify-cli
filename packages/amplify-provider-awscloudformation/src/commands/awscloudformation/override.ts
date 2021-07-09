9487f26628273d1ea41d79080c973e8105aafc8d
Kaustav Ghosh <kaustav.ghosh19@gmail.com>
2021-07-08 17:06:46 -0700

    feat: amplify add override root command and skeleton generation (#7684)
    * feat: amplify add override root command and skeleton generation
    
    * feat: build and package overrides directory
    
    * test: add unit tests
    
    * fix: updates with PR comments
    
    * test: fix unit tests
    
    Co-authored-by: Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>

---
+++ packages/amplify-provider-awscloudformation/src/commands/awscloudformation/override.ts
@@ -0,0 +1,14 @@
+/*
+    entry code for amplify override root
+*/
+
+import { generateOverrideSkeleton } from '../../utils/override-skeleton-generator';
+
+const subcommand = 'override';
+
+module.exports = {
+  name: subcommand,
+  run: async context => {
+    await generateOverrideSkeleton(context);
+  },
+};
