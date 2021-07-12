6947a050cf4b7e2421d30c3d6b6c5abed24ab3e1
Zeyu Li <lizeyu@amazon.com>
2021-07-12 16:56:46 -0700

    test: add cases for update and remove

--- packages/amplify-e2e-core/src/categories/geo.ts
+++ packages/amplify-e2e-core/src/categories/geo.ts
@@ -50,3 +50,99 @@ export function addPlaceIndexWithDefault(cwd: string): Promise<void> {
       })
   });
 }
+
+/**
+ * Update an existing map with default values. Assume auth is already configured
+ * @param cwd command directory
+ */
+ export function updateMapWithDefault(cwd: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), ['geo', 'update'], { cwd, stripColors: true })
+      .wait('Select which capability you want to update:')
+      .sendCarriageReturn()
+      .wait('Select the Map you want to update')
+      .sendCarriageReturn()
+      .wait('Who can access this Map?')
+      .send(KEY_DOWN_ARROW)
+      .sendCarriageReturn()
+      .run((err: Error) => {
+        if (!err) {
+          resolve();
+        } else {
+          reject();
+        }
+      })
+  });
+}
+
+/**
+ * Update an existing place index with default values. Assume auth is already configured
+ * @param cwd command directory
+ */
+ export function updatePlaceIndexWithDefault(cwd: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), ['geo', 'update'], { cwd, stripColors: true })
+      .wait('Select which capability you want to update:')
+      .send(KEY_DOWN_ARROW)
+      .sendCarriageReturn()
+      .wait('Select the search index you want to update')
+      .sendCarriageReturn()
+      .wait('Who can access this Search Index?')
+      .send(KEY_DOWN_ARROW)
+      .sendCarriageReturn()
+      .run((err: Error) => {
+        if (!err) {
+          resolve();
+        } else {
+          reject();
+        }
+      })
+  });
+}
+
+/**
+ * Remove an existing map. Assume auth is already configured
+ * @param cwd command directory
+ */
+ export function removeMap(cwd: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), ['geo', 'remove'], { cwd, stripColors: true })
+      .wait('Select which capability you want to remove:')
+      .sendCarriageReturn()
+      .wait('Select the Map you want to remove')
+      .sendCarriageReturn()
+      .wait('Are you sure you want to delete the resource?')
+      .sendConfirmYes()
+      .run((err: Error) => {
+        if (!err) {
+          resolve();
+        } else {
+          reject();
+        }
+      })
+  });
+}
+
+/**
+ * Remove an existing place index. Assume auth is already configured
+ * @param cwd command directory
+ */
+ export function removePlaceIndex(cwd: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), ['geo', 'remove'], { cwd, stripColors: true })
+      .wait('Select which capability you want to remove:')
+      .send(KEY_DOWN_ARROW)
+      .sendCarriageReturn()
+      .wait('Select the PlaceIndex you want to remove')
+      .sendCarriageReturn()
+      .wait('Are you sure you want to delete the resource?')
+      .sendConfirmYes()
+      .run((err: Error) => {
+        if (!err) {
+          resolve();
+        } else {
+          reject();
+        }
+      })
+  });
+}
