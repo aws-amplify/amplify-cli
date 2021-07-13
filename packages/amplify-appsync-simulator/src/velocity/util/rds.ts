6ac34eba923f29186e443ea880125f7f6e8cefd3
Mac <mohammed.ali.chherawalla@gmail.com>
2021-07-13 16:30:50 +0530

    feat(amplify-appsync-simulator): cleanup

--- packages/amplify-appsync-simulator/src/velocity/util/rds.ts
+++ packages/amplify-appsync-simulator/src/velocity/util/rds.ts
@@ -2,16 +2,15 @@ export const rds = {
   toJsonString: rdsObject => {
     try {
       rdsObject = JSON.parse(rdsObject);
-      const rdsJson = (rdsObject?.sqlStatementResults || []).map(statement => {
-        return (statement?.records || []).map(record => {
+      const rdsJson = (rdsObject?.sqlStatementResults ?? []).map(statement =>
+        (statement?.records ?? []).map(record => {
           const result = {};
           record.forEach((row, index) => {
-            result[statement.columnMetadata[index].name] = Object.values(row)[0];
+            result[statement?.columnMetadata?.[index]?.name] = Object.values(row)?.[0];
           });
-
           return result;
-        });
-      });
+        }),
+      );
       return JSON.stringify(rdsJson);
     } catch {
       return '';
