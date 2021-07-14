f424c5b14d2876819a29cfd1dad483839c20d172
phani-srikar <phanisrikar93ume@gmail.com>
2021-07-14 13:14:50 -0700

    fix(amplify-category-geo): add preview banner

--- packages/amplify-category-geo/src/commands/geo/console.ts
+++ packages/amplify-category-geo/src/commands/geo/console.ts
@@ -2,12 +2,13 @@ import { $TSContext } from 'amplify-cli-core';
 import { openConsole } from '../../provider-controllers';
 import { category } from '../../constants';
 import { supportedServices } from '../../supportedServices';
-import { provider } from '../../service-utils/constants';
+import { provider, previewBanner } from '../../service-utils/constants';
 
 export const name = 'console';
 
 export const run = async (context: $TSContext) => {
   const { amplify } = context;
+  context.print.warning(previewBanner);
   const result: {service: string, providerName: string} = await amplify.serviceSelectionPrompt(context, category, supportedServices);
 
   if (result.providerName !== provider) {
