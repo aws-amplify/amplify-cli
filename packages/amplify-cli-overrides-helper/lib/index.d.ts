3a63830b7bfdcfb0d0ac041c5652ea8ba84f2e01
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-07-14 17:16:35 -0700

    feat: add helper util package for overrides

---
+++ packages/amplify-cli-overrides-helper/lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare const getProjectInfo: () => void;
+export declare const addDependency: () => void;
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/amplify-cli-overrides-helper/src/index.ts
@@ -0,0 +1,7 @@
+export const getProjectInfo = () => {
+  console.log('Hello from the skeleton of get project info');
+};
+
+export const addDependency = () => {
+  console.log('Hello from the skeleton of add dependency');
+};
