01ed20d45da0995a3f730aa06078cc84db7fe7e1
Jainam Shah <jainam.2000@yahoo.com>
2021-07-15 08:10:41 -0700

    feat: hooks - amplify pull - attachBackend

--- packages/amplify-provider-awscloudformation/src/utils/hooks-manager.ts
+++ packages/amplify-provider-awscloudformation/src/utils/hooks-manager.ts
@@ -1,9 +1,12 @@
-import { $TSContext, FeatureFlags, JSONUtilities, pathManager, stateManager } from 'amplify-cli-core';
+import { $TSAny, $TSContext, pathManager, stateManager } from 'amplify-cli-core';
 import * as path from 'path';
 import _ from 'lodash';
 import ignore from 'ignore';
 import { S3 } from '../aws-utils/aws-s3';
+import * as aws from 'aws-sdk';
 import fs from 'fs-extra';
+
+const S3_HOOKS_DIRECTORY = 'hooks/';
 
 export async function uploadHooksDirectory(context: $TSContext): Promise<string[]> {
   // resolves with list of successfully uploaded files
@@ -73,5 +76,59 @@ function getNonIgnoredFileList(context: $TSContext): string[] {
 }
 
 function getS3UploadPath(relativePath: string): string {
-  return 'hooks/' + relativePath.split(path.sep).join(path.posix.sep);
+  return S3_HOOKS_DIRECTORY + relativePath.split(path.sep).join(path.posix.sep);
+}
+
+export async function downloadHooks(context: $TSContext, backendEnv: $TSAny, awsConfigInfo: $TSAny) {
+  if (!backendEnv) {
+    return;
+  }
+  const projectPath = process.cwd();
+  const hooksDirPath = pathManager.getHooksDirPath(projectPath);
+
+  const s3Client = new aws.S3(awsConfigInfo);
+  const deploymentBucketName = backendEnv.deploymentArtifacts;
+
+  const params = {
+    Prefix: S3_HOOKS_DIRECTORY,
+    Bucket: deploymentBucketName,
+  };
+
+  // TODO: logs
+  //   const log = logger("downloadHooks.s3.listObjects", [params]);
+  let listHookObjects;
+  try {
+    // log();
+    listHookObjects = await s3Client.listObjects(params).promise();
+  } catch (ex) {
+    // log(ex);
+    return;
+  }
+
+  // loop over each object in S3 hooks directory and download the file
+  for (let listHookObject of listHookObjects.Contents) {
+    const params = {
+      Key: listHookObject.Key,
+      Bucket: deploymentBucketName,
+    };
+    // TODO: logs
+    // const log = logger('downloadHooks.s3.getObject', [params]);
+    let hooksFileObject = null;
+    try {
+      // log();
+      hooksFileObject = await s3Client.getObject(params).promise();
+    } catch (ex) {
+      // log(ex);
+    }
+    try {
+      let relativeFilePath: string = listHookObject.Key;
+      if (relativeFilePath.substring(0, S3_HOOKS_DIRECTORY.length).localeCompare(S3_HOOKS_DIRECTORY) === 0) {
+        relativeFilePath = relativeFilePath.substring(S3_HOOKS_DIRECTORY.length);
+      }
+      // relativeFilePath has POSIX seperation, split and join to get OS specific path
+      const hooksFilePath = path.join(hooksDirPath, ...relativeFilePath.split('/'));
+      fs.ensureFileSync(hooksFilePath);
+      fs.writeFileSync(hooksFilePath, hooksFileObject.Body);
+    } catch (ex) {}
+  }
 }
