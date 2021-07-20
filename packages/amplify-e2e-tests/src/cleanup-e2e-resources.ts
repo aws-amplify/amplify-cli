d2b228ef99df23777d0b8353bf73a48dfb99f4b6
John Corser <johnpc@umich.edu>
2021-07-20 12:09:11 -0400

    ci: add support for e2e token rotation (#7665)
    * ci: add support for e2e token rotation
    
    Currently we use long-lived, manually generated access tokens to set up AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables allowing our tests to run. This is a bad practice - authorization tokens should be automatically rotated to minimize risk of the tokens landing in the wrong hands.
    
    This PR sets up the Amplify CLI codebase to use temporary credentials during CI. Temporary STS credentials use an additional parameter, AWS_SESSION_TOKEN, to authorize the requests.
    
    This has been tested in CI on my fork and all tests pass:
    
    https://app.circleci.com/pipelines/github/johnpc/amplify-cli/144/workflows/fb12f075-d881-4c92-8cf7-f8647afb909e
    
    The token rotator itself is a lambda that runs every hour:
    
    https://github.com/johnpc/amplify-e2e-token-rotation-lambda
    
    * chore: use ini lib instead of manually updating file
    
    * chore: update injectSessionToken calls

--- packages/amplify-e2e-tests/src/cleanup-e2e-resources.ts
+++ packages/amplify-e2e-tests/src/cleanup-e2e-resources.ts
@@ -79,6 +79,7 @@ const configureAws = (): void => {
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN,
       ...(process.env.AWS_SESSION_TOKEN ? { sessionToken: process.env.AWS_SESSION_TOKEN } : {}),
     },
     maxRetries: 10,
