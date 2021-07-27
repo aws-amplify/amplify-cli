2680ac81c4900dcc98af1a9c62caa887483036fa
Pavel Lazar <lazpavel@amazon.com>
2021-07-26 22:19:23 -0400

    fix(amplify-category-auth): added e2e test for add auth user selections to config files

---
+++ packages/amplify-e2e-tests/src/__tests__/auth_6.test.ts
@@ -0,0 +1,44 @@
+import {
+  initJSProjectWithProfile,
+  getProjectMeta,
+  deleteProject,
+  amplifyPushAuth,
+  createNewProjectDir,
+  deleteProjectDir,
+  addAuthWithMaxOptions,
+} from 'amplify-e2e-core';
+import _ from 'lodash';
+
+const PROJECT_NAME = 'authTest';
+const defaultsSettings = {
+  name: PROJECT_NAME,
+};
+describe('zero config auth ', () => {
+  let projRoot: string;
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('zero-config-auth');
+  });
+
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
+
+  it.only('...should init a javascript project and add auth with a all uptions and update front end config', async () => {
+    await initJSProjectWithProfile(projRoot, defaultsSettings);
+    await addAuthWithMaxOptions(projRoot, { useInclusiveTerminology: false });
+    await amplifyPushAuth(projRoot);
+    let meta = getProjectMeta(projRoot);
+
+    let id = Object.keys(meta.auth)[0];
+    let authMeta = meta.auth[id];
+
+    expect(authMeta.frontEndConfig).toBeDefined();
+    expect(authMeta.frontEndConfig.loginMechanism).toBeDefined();
+    expect(authMeta.frontEndConfig.signupAttributes).toBeDefined();
+    expect(authMeta.frontEndConfig.mfaConfiguration).toBeDefined();
+    expect(authMeta.frontEndConfig.mfaConfiguration).toEqual('ON');
+    expect(authMeta.frontEndConfig.mfaTypes).toBeDefined();
+    expect(authMeta.frontEndConfig.passwordProtectionSettings).toBeDefined();
+  });
+});
