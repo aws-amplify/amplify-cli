e0834dc3cc1596129a82f61428c6cdf4b92fdc10
Ben <lolitsaburrito@gmail.com>
2021-07-27 19:50:38 -0700

    test(amplify-util-mock): add initial tests for AWS::NoValue support
    Add additional test for cfnIf, and new test file for testing Fn::If evaluation and handling of
    AWS::NoValue

---
+++ packages/amplify-util-mock/src/__tests__/CFNParser/nested-aws-no-value.test.ts
@@ -0,0 +1,57 @@
+import { CloudFormationTemplate } from '../../CFNParser/stack/types';
+import { CloudFormationParseContext } from '../../CFNParser/types';
+import { parseValue } from '../../CFNParser/field-parser';
+
+describe('cloudformation templates', () => {
+  const nestedNoValue: CloudFormationTemplate = {
+    Conditions: {
+      AlwaysTrue: {
+        'Fn::Equals': ['true', 'true'],
+      },
+    },
+    Resources: {
+      DummyResource: {
+        Type: 'Resource::Dummy',
+        Properties: {
+          ConditionalValue: {
+            'Fn::If': ['AlwaysTrue', 'ConditionIsTrue', { Ref: 'AWS::NoValue' }],
+          },
+          ConditionalRef: {
+            'Fn::If': ['AlwaysTrue', { Ref: 'SomeRef' }, { Ref: 'AWS::NoValue' }],
+          },
+          ConditionalNoValue: {
+            'Fn::If': ['AlwaysTrue', { Ref: 'AWS::NoValue' }, { Ref: 'SomeRef' }],
+          },
+        },
+      },
+    },
+  };
+  const cfnContext: CloudFormationParseContext = {
+    params: {},
+    conditions: {
+      AlwaysTrue: true,
+    },
+    resources: {
+      SomeRef: {
+        Type: 'String',
+        result: { ref: 'resolvedRefValue' },
+      },
+    },
+    exports: {},
+  };
+
+  it('should resolve nested `Fn::If`', () => {
+    const value = parseValue(nestedNoValue.Resources.DummyResource.Properties.ConditionalValue, cfnContext);
+    expect(value).toEqual('ConditionIsTrue');
+  });
+
+  it('should resolve nested `Fn::If` and evaluate a resulting Ref', () => {
+    const value = parseValue(nestedNoValue.Resources.DummyResource.Properties.ConditionalRef, cfnContext);
+    expect(value).toEqual('resolvedRefValue');
+  });
+
+  it('should resolve nested `Fn::If` and evaluate a resulting `AWS::NoValue` Ref', () => {
+    const value = parseValue(nestedNoValue.Resources.DummyResource.Properties.ConditionalNoValue, cfnContext);
+    expect(value).toBeUndefined();
+  });
+});
