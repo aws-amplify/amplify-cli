899a95d2a6581a0b572176b8a5323afad4a1ee36
lazpavel <85319655+lazpavel@users.noreply.github.com>
2021-08-04 17:03:34 -0400

    fix(graphql-model-transformer): fixed @model scalar types model filter input generation

--- packages/amplify-graphql-model-transformer/src/__tests__/model-transformer.test.ts
+++ packages/amplify-graphql-model-transformer/src/__tests__/model-transformer.test.ts
@@ -938,12 +938,10 @@ describe('ModelTransformer: ', () => {
           name: String!
       }
     `;
-
     const transformer = new GraphQLTransform({
       transformers: [new ModelTransformer()],
       featureFlags,
     });
-
     const out = transformer.transform(validSchema);
     expect(out).toBeDefined();
 
