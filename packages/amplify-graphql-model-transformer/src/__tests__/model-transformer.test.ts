4899b0d4d993537544775d1bf60a34f9deed6bd9
lazpavel <85319655+lazpavel@users.noreply.github.com>
2021-08-04 00:41:02 -0400

    fix(graphql-model-transformer): fixed input type field generation for enum types

--- packages/amplify-graphql-model-transformer/src/__tests__/model-transformer.test.ts
+++ packages/amplify-graphql-model-transformer/src/__tests__/model-transformer.test.ts
@@ -791,4 +791,31 @@ describe('ModelTransformer: ', () => {
     const updateTestInput = getInputType(schema, 'UpdateTestInput');
     expect(getFieldOnInputType(updateTestInput!, 'email')).toBeUndefined();
   });
+
+  it('should support enum as a field', () => {
+    const validSchema = `
+      enum Status { DELIVERED IN_TRANSIT PENDING UNKNOWN }
+      type Test @model {
+        status: Status!
+        lastStatus: Status!
+      }
+    `;
+    const transformer = new GraphQLTransform({
+      transformers: [new ModelTransformer()],
+      featureFlags,
+    });
+
+    const out = transformer.transform(validSchema);
+    expect(out).toBeDefined();
+    const definition = out.schema;
+    expect(definition).toBeDefined();
+    const parsed = parse(definition);
+    validateModelSchema(parsed);
+
+    const createTestInput = getInputType(parsed, 'CreateTestInput');
+    expectFieldsOnInputType(createTestInput!, ['status', 'lastStatus']);
+
+    const updateTestInput = getInputType(parsed, 'CreateTestInput');
+    expectFieldsOnInputType(updateTestInput!, ['status', 'lastStatus']);
+  });
 });
