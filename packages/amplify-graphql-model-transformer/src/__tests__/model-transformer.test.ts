aed6076c889ace0a3ca2d9321c5d45b645db4f3b
lazpavel <85319655+lazpavel@users.noreply.github.com>
2021-08-04 17:03:34 -0400

    fix(graphql-model-transformer): fixed @model scalar types model filter input generation

--- packages/amplify-graphql-model-transformer/src/__tests__/model-transformer.test.ts
+++ packages/amplify-graphql-model-transformer/src/__tests__/model-transformer.test.ts
@@ -665,12 +665,10 @@ describe('ModelTransformer: ', () => {
           name: String!
       }
     `;
-
     const transformer = new GraphQLTransform({
       transformers: [new ModelTransformer()],
       featureFlags,
     });
-
     const out = transformer.transform(validSchema);
     expect(out).toBeDefined();
 
@@ -704,32 +702,5 @@ describe('ModelTransformer: ', () => {
     expect(verifyInputCount(parsed, 'ModelIDFilterInput', 1)).toBeTruthy();
     expect(verifyInputCount(parsed, 'ModelPostFilterInput', 1)).toBeTruthy();
     expect(verifyInputCount(parsed, 'ModelUserFilterInput', 1)).toBeTruthy();
-  });
-
-  it('should support enum as a field', () => {
-    const validSchema = `
-      enum Status { DELIVERED IN_TRANSIT PENDING UNKNOWN }
-      type Test @model {
-        status: Status!
-        lastStatus: Status!
-      }
-    `;
-    const transformer = new GraphQLTransform({
-      transformers: [new ModelTransformer()],
-      featureFlags,
-    });
-
-    const out = transformer.transform(validSchema);
-    expect(out).toBeDefined();
-    const definition = out.schema;
-    expect(definition).toBeDefined();
-    const parsed = parse(definition);
-    validateModelSchema(parsed);
-
-    const createTestInput = getInputType(parsed, 'CreateTestInput');
-    expectFieldsOnInputType(createTestInput!, ['status', 'lastStatus']);
-
-    const updateTestInput = getInputType(parsed, 'CreateTestInput');
-    expectFieldsOnInputType(updateTestInput!, ['status', 'lastStatus']);
   });
 });
