073060bb9a01fc49a2c1d25ad623da5291b00636
Phani Srikar Edupuganti <55896475+phani-srikar@users.noreply.github.com>
2021-08-09 11:49:03 -0700

    Amplify geo preview feedback items (#7891)
    * fix(amplify-category-geo): update aws-exports upon default geo resource change, implement getPermissionPolicies

--- packages/amplify-category-geo/src/commands/geo/remove.ts
+++ packages/amplify-category-geo/src/commands/geo/remove.ts
@@ -20,8 +20,13 @@ export const run = async(context: $TSContext) => {
     return await removeResource(context, result.service);
 
   } catch (error) {
+    if (error.message) {
+      context.print.error(error.message);
+    }
     context.print.info('');
-    context.print.info(error.stack);
+    if (error.stack) {
+      context.print.info(error.stack);
+    }
     context.print.error('There was an error removing the geo resource');
     context.usageData.emitError(error);
     process.exitCode = 1;
