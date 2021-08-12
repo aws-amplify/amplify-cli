054256bc7f6e56e76fe55baed86e5f875c4c2592
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-08-11 17:16:24 -0700

    feat: add override command for auth

--- packages/amplify-provider-awscloudformation/src/commands/awscloudformation/override.ts
+++ packages/amplify-provider-awscloudformation/src/commands/awscloudformation/override.ts
@@ -2,13 +2,20 @@
     entry code for amplify override root
 */
 
-import { generateOverrideSkeleton } from '../../utils/override-skeleton-generator';
+//import { generateOverrideSkeleton } from '../../utils/override-skeleton-generator';
+import path from 'path';
+import { generateOverrideSkeleton } from 'amplify-cli-core';
 
 const subcommand = 'override';
 
 module.exports = {
   name: subcommand,
   run: async context => {
-    await generateOverrideSkeleton(context);
+    const backendDir = context.amplify.pathManager.getBackendDirPath();
+
+    const destPath = path.normalize(path.join(backendDir, 'awscloudformation', 'overrides'));
+    const srcPath = path.normalize(path.join(__dirname, '..', '..', '..', 'resources', 'overrides-resource'));
+
+    await generateOverrideSkeleton(context, srcPath, destPath);
   },
 };
