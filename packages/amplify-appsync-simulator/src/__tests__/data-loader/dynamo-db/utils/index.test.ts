62bf7041186ee7c5c0d3cd26a3d1666470e03ddf
behrmans <44505263+behrmans@users.noreply.github.com>
2021-08-13 16:11:45 -0400

    Update packages/amplify-appsync-simulator/src/__tests__/data-loader/dynamo-db/utils/index.test.ts

--- packages/amplify-appsync-simulator/src/__tests__/data-loader/dynamo-db/utils/index.test.ts
+++ packages/amplify-appsync-simulator/src/__tests__/data-loader/dynamo-db/utils/index.test.ts
@@ -1,7 +1,7 @@
 import { mapTableObject, unmarshall } from '../../../../data-loader/dynamo-db/utils';
 
 describe('mapTableObject', () => {
-  it(`will returns mapped TableName key object`, () => {
+  it(`will return mapped TableName key object`, () => {
     expect(mapTableObject({ TestTable: { id: { S: '1' } }, OtherTable: { id: { S: '2' } } }, item => unmarshall(item))).toEqual({
       TestTable: { id: '1' },
       OtherTable: { id: '2' },
