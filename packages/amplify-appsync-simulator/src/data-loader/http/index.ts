c84ec69bfa37581eb14181b8a8250a13af1b8082
Ben <lolitsaburrito@gmail.com>
2021-08-18 08:04:12 -0700

    feat(amplify-appsync-simulator): flatten result.data before returning it in HttpDataLoader

--- packages/amplify-appsync-simulator/src/data-loader/http/index.ts
+++ packages/amplify-appsync-simulator/src/data-loader/http/index.ts
@@ -1,13 +1,10 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { AmplifyAppSyncSimulatorDataLoader } from '..';
 import { AppSyncSimulatorDataSourceHttpConfig } from '../../type-definition';
 import { print } from 'graphql';
 
 export class HttpDataLoader implements AmplifyAppSyncSimulatorDataLoader {
   private endpoint: string;
-  // private method: 'PUT' | 'POST' | 'GET' | 'DELETE' | 'PATCH';
-  // private headers: Array<{key: string, value: string}>;
-  // private body: object;
 
   constructor(config: AppSyncSimulatorDataSourceHttpConfig) {
     this.endpoint = config?.httpConfig?.endpoint;
@@ -18,20 +15,44 @@ export class HttpDataLoader implements AmplifyAppSyncSimulatorDataLoader {
     // console.log((await import('util')).inspect(extraData, {depth: null}));
     console.log('payload');
     console.log(payload);
+    console.log('print');
+    console.log(print(extraData.info.operation.selectionSet));
+
     try {
-      const result = await axios({
+      const result: AxiosResponse = await axios({
         method: payload.method,
         url: this.endpoint,
         headers: payload.params.headers,
         data: payload.params.query,
         params: { query: print(extraData.info.operation.selectionSet) },
       });
+
       console.log(result.data);
-      return result.data;
+
+      const transformedResult = JSON.stringify(this.flattenObject(result.data.data));
+      const cfxResult = { body: transformedResult, statusCode: result.status, headers: result.headers };
+
+      console.log(cfxResult);
+      return cfxResult;
     } catch (error) {
       console.log('HTTP Data source failed with the following error:');
       console.error(error);
       throw error;
     }
   }
+
+  // Source: https://stackoverflow.com/a/55251598/16010441
+  private flattenObject = obj => {
+    const flattened = {};
+
+    Object.keys(obj).forEach(key => {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        Object.assign(flattened, this.flattenObject(obj[key]));
+      } else {
+        flattened[key] = obj[key];
+      }
+    });
+
+    return flattened;
+  };
 }
