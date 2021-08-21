27d9a7a4894dac3ca5279ce426c7418f5bad1457
John Hockett <jhockett@users.noreply.github.com>
2021-08-21 00:36:57 -0700

    test: update dependencies for schema e2e tests (#8000)
    * test: update dependencies for schema e2e tests
    * chore: keep test  dependencies in sync
    * fix: remove no longer needed workaround
    * test: update graphql e2e tests

--- packages/graphql-transformers-e2e-tests/src/__tests__/SubscriptionsWithAuthTest.e2e.test.ts
+++ packages/graphql-transformers-e2e-tests/src/__tests__/SubscriptionsWithAuthTest.e2e.test.ts
@@ -19,10 +19,11 @@ import { IAM as cfnIAM, Cognito as cfnCognito } from 'cloudform-types';
 import {
   createUserPool,
   createUserPoolClient,
-  signupAndAuthenticateUser,
   createGroup,
   addUserToGroup,
   configureAmplify,
+  signupUser,
+  authenticateUser,
 } from '../cognitoUtils';
 import 'isomorphic-fetch';
 import { API } from 'aws-amplify';
@@ -48,6 +49,7 @@ const featureFlags = {
   getObject: jest.fn(),
   getString: jest.fn(),
 };
+
 // to deal with bug in cognito-identity-js
 (global as any).fetch = require('node-fetch');
 // to deal with subscriptions in node env
@@ -398,7 +400,7 @@ beforeAll(async () => {
     });
 
     const identityPoolRoleMap = new cfnCognito.IdentityPoolRoleAttachment({
-      IdentityPoolId: ({ Ref: 'IdentityPool' } as unknown) as string,
+      IdentityPoolId: { Ref: 'IdentityPool' } as unknown as string,
       Roles: {
         unauthenticated: { 'Fn::GetAtt': ['UnauthRole', 'Arn'] },
         authenticated: { 'Fn::GetAtt': ['AuthRole', 'Arn'] },
@@ -532,8 +534,9 @@ beforeAll(async () => {
     // Configure Amplify, create users, and sign in.
     configureAmplify(USER_POOL_ID, userPoolClientId, identityPoolId);
 
-    await signupAndAuthenticateUser(USER_POOL_ID, USERNAME1, TMP_PASSWORD, REAL_PASSWORD);
-    await signupAndAuthenticateUser(USER_POOL_ID, USERNAME2, TMP_PASSWORD, REAL_PASSWORD);
+    await signupUser(USER_POOL_ID, USERNAME1, TMP_PASSWORD);
+    await signupUser(USER_POOL_ID, USERNAME2, TMP_PASSWORD);
+    await signupUser(USER_POOL_ID, USERNAME3, TMP_PASSWORD);
     await createGroup(USER_POOL_ID, INSTRUCTOR_GROUP_NAME);
     await createGroup(USER_POOL_ID, MEMBER_GROUP_NAME);
     await createGroup(USER_POOL_ID, ADMIN_GROUP_NAME);
@@ -542,7 +545,7 @@ beforeAll(async () => {
     await addUserToGroup(INSTRUCTOR_GROUP_NAME, USERNAME1, USER_POOL_ID);
     await addUserToGroup(INSTRUCTOR_GROUP_NAME, USERNAME2, USER_POOL_ID);
 
-    const authResAfterGroup: any = await signupAndAuthenticateUser(USER_POOL_ID, USERNAME1, TMP_PASSWORD, REAL_PASSWORD);
+    const authResAfterGroup: any = await authenticateUser(USERNAME1, TMP_PASSWORD, REAL_PASSWORD);
     const idToken = authResAfterGroup.getIdToken().getJwtToken();
     GRAPHQL_CLIENT_1 = new AWSAppSyncClient({
       url: GRAPHQL_ENDPOINT,
@@ -557,7 +560,7 @@ beforeAll(async () => {
       },
     });
 
-    const authRes2AfterGroup: any = await signupAndAuthenticateUser(USER_POOL_ID, USERNAME2, TMP_PASSWORD, REAL_PASSWORD);
+    const authRes2AfterGroup: any = await authenticateUser(USERNAME2, TMP_PASSWORD, REAL_PASSWORD);
     const idToken2 = authRes2AfterGroup.getIdToken().getJwtToken();
     GRAPHQL_CLIENT_2 = new AWSAppSyncClient({
       url: GRAPHQL_ENDPOINT,
@@ -572,7 +575,7 @@ beforeAll(async () => {
       },
     });
 
-    const authRes3: any = await signupAndAuthenticateUser(USER_POOL_ID, USERNAME3, TMP_PASSWORD, REAL_PASSWORD);
+    const authRes3: any = await authenticateUser(USERNAME3, TMP_PASSWORD, REAL_PASSWORD);
     const idToken3 = authRes3.getIdToken().getJwtToken();
     GRAPHQL_CLIENT_3 = new AWSAppSyncClient({
       url: GRAPHQL_ENDPOINT,
@@ -624,6 +627,8 @@ beforeAll(async () => {
     // "The security token included in the request is invalid" errors
     await new Promise(res => setTimeout(res, PROPAGATION_DELAY));
   } catch (e) {
+    console.log(e);
+
     expect(true).toEqual(false);
   }
 });
@@ -654,7 +659,7 @@ test('Test that only authorized members are allowed to view subscriptions', asyn
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = observer.subscribe((event: any) => {
@@ -698,7 +703,7 @@ test('Test that an user not in the group is not allowed to view the subscription
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = observer.subscribe({
@@ -741,7 +746,7 @@ test('Test a subscription on update', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = observer.subscribe((event: any) => {
@@ -794,7 +799,7 @@ test('Test a subscription on delete', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, reject) => {
     subscription = observer.subscribe({
@@ -857,7 +862,7 @@ test('test that group is only allowed to listen to subscriptions and listen to o
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = observer.subscribe((event: any) => {
@@ -899,7 +904,7 @@ test('authorized group is allowed to listen to onUpdate', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
 
   const subscriptionPromise = new Promise((resolve, reject) => {
@@ -949,7 +954,7 @@ test('authorized group is allowed to listen to onDelete', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
 
   const subscriptionPromise = new Promise((resolve, reject) => {
@@ -996,7 +1001,7 @@ test('Test subscription onCreatePost with ownerField', async () => {
         }
     }`,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = observer.subscribe((event: any) => {
@@ -1035,7 +1040,7 @@ test('Test onCreatePost with optional argument', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = failedObserver.subscribe(
@@ -1072,7 +1077,7 @@ test('Test that IAM can listen and read to onCreatePost', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.AWS_IAM,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
 
   const subscriptionPromise = new Promise((resolve, reject) => {
@@ -1115,7 +1120,7 @@ test('test that subcsription with apiKey', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.API_KEY,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
 
   const subscriptionPromise = new Promise((resolve, _) => {
@@ -1156,7 +1161,7 @@ test('test that subscription with apiKey onUpdate', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.API_KEY,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, reject) => {
     subscription = observer.subscribe(
@@ -1211,7 +1216,7 @@ test('test that subscription with apiKey onDelete', async () => {
       }
     `,
     authMode: GRAPHQL_AUTH_MODE.API_KEY,
-  }) as Observable<any>;
+  }) as unknown as Observable<any>;
   let subscription: ZenObservable.Subscription;
   const subscriptionPromise = new Promise((resolve, _) => {
     subscription = observer.subscribe((event: any) => {
