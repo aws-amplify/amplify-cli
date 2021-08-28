2f608a3bdef0d4eb465f812c0f520be42ac25ef9
Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>
2021-08-27 18:07:15 -0700

    fix: add schema json for cli-inputs

--- packages/amplify-category-storage/resources/genInputSchema.ts
+++ packages/amplify-category-storage/resources/genInputSchema.ts
@@ -1,20 +1,17 @@
-import {TypeDef, CLIInputSchemaGenerator} from "amplify-cli-core";
+import { TypeDef, CLIInputSchemaGenerator } from 'amplify-cli-core';
 
 //ResourceProvider TypeDefs
-const DDBStorageTypeDef : TypeDef = {
+const DDBStorageTypeDef: TypeDef = {
   typeName: 'DynamoDBCLIInputs',
-  service: 'dynamoDB'
-}
-const S3StorageTypeDef : TypeDef = {
+  service: 'dynamoDB',
+};
+const S3StorageTypeDef: TypeDef = {
   typeName: 'S3UserInputs',
-  service: 's3'
-}
+  service: 's3',
+};
 
 // Defines the type names and the paths to the TS files that define them
-const storageCategoryTypeDefs: TypeDef[] = [
-  S3StorageTypeDef,
-  DDBStorageTypeDef
-];
+const storageCategoryTypeDefs: TypeDef[] = [DDBStorageTypeDef];
 
 const schemaGenerator = new CLIInputSchemaGenerator(storageCategoryTypeDefs);
 schemaGenerator.generateJSONSchemas(); //convert CLI input data into json schemas.
