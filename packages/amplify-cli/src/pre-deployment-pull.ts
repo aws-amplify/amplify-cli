500a8db94ac795f0cde978365af1ed9c14fb1bab
John Corser <johnpc@umich.edu>
2021-09-07 20:46:13 -0400

    chore(amplify-provider-awscloudformation): add env vars for configuring development against beta/gamma

--- packages/amplify-cli/src/pre-deployment-pull.ts
+++ packages/amplify-cli/src/pre-deployment-pull.ts
@@ -7,8 +7,9 @@ import * as path from 'path';
 
 export async function preDeployPullBackend(context: $TSContext, sandboxId: string) {
   const providerPlugin = await import(context.amplify.getProviderPlugins(context).awscloudformation);
-
-  const url = `${providerPlugin.adminBackendMap['us-east-1'].appStateUrl}/AppState/${sandboxId}`;
+  // environment variable APPSTATE_BASE_URL useful for development against beta/gamma appstate endpoints
+  const appStateBaseUrl = process.env.APPSTATE_BASE_URL ?? providerPlugin.adminBackendMap['us-east-1'].appStateUrl;
+  const url = `${appStateBaseUrl}/AppState/${sandboxId}`;
 
   // Fetch schema
   const res = await fetch(`${url}`);
