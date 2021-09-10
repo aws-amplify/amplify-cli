15deba837e06ef077922e4bebd0275276d8585f2
Colin Ihrig <cjihrig@gmail.com>
2021-09-10 15:04:18 -0400

    feat: support five new regions in CLI (#8094)
    * feat: support five new regions in CLI
    
    This commit updates the CLI to support five new regions:
    
    - us-west-1
    - eu-west-3
    - eu-north-1
    - me-south-1
    - sa-east-1

--- packages/amplify-provider-awscloudformation/src/aws-regions.js
+++ packages/amplify-provider-awscloudformation/src/aws-regions.js
@@ -4,9 +4,12 @@
 const regionMappings = {
   'us-east-1': 'US East (N. Virginia)',
   'us-east-2': 'US East (Ohio)',
+  'us-west-1': 'US West (N. California)',
   'us-west-2': 'US West (Oregon)',
+  'eu-north-1': 'EU (Stockholm)',
   'eu-west-1': 'EU (Ireland)',
   'eu-west-2': 'EU (London)',
+  'eu-west-3': 'EU (Paris)',
   'eu-central-1': 'EU (Frankfurt)',
   'ap-northeast-1': 'Asia Pacific (Tokyo)',
   'ap-northeast-2': 'Asia Pacific (Seoul)',
@@ -14,6 +17,8 @@ const regionMappings = {
   'ap-southeast-2': 'Asia Pacific (Sydney)',
   'ap-south-1': 'Asia Pacific (Mumbai)',
   'ca-central-1': 'Canada (Central)',
+  'me-south-1': 'Middle East (Bahrain)',
+  'sa-east-1': 'South America (São Paulo)',
 };
 
 module.exports = {
--- packages/amplify-provider-awscloudformation/src/aws-utils/aws-pinpoint.js
+++ packages/amplify-provider-awscloudformation/src/aws-utils/aws-pinpoint.js
@@ -20,9 +20,11 @@ const serviceRegionMap = {
   'ap-southeast-2': 'us-west-2',
   'ap-northeast-1': 'us-west-2',
   'eu-central-1': 'eu-central-1',
+  'eu-north-1': 'eu-central-1',
   'eu-west-1': 'eu-west-1',
   'eu-west-2': 'eu-west-1',
   'eu-west-3': 'eu-west-1',
+  'me-south-1': 'ap-south-1',
 };
 
 async function getConfiguredPinpointClient(context, category, action, envName) {
