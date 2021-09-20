aeeb91b59667f96bbcb7812e8cda21c2277f500b
John Corser <johnpc@umich.edu>
2021-09-20 11:07:17 -0400

    feat(cli): add post env add plugin event

--- packages/amplify-cli/src/commands/env/add.ts
+++ packages/amplify-cli/src/commands/env/add.ts
@@ -1,6 +1,8 @@
 import { $TSContext } from 'amplify-cli-core';
+import { raisePostEnvAddEvent } from '../../execution-manager';
 import { run as init } from '../init';
 
 export const run = async (context: $TSContext) => {
   await init(context);
+  await raisePostEnvAddEvent(context as any, context.exeInfo.sourceEnvName, context.exeInfo.localEnvInfo.envName);
 };
