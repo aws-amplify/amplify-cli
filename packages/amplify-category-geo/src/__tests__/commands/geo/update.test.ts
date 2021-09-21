83b3040814cd61895814aaafc49c9c44cc4458e9
Phani Srikar Edupuganti <55896475+phani-srikar@users.noreply.github.com>
2021-09-21 13:29:54 -0700

    Add geo CLI category plugin with Maps and Search capabilities (#8212)
    * Amplify geo maps and search (#7566)
    
    * feat(amplify-category-geo): add geo category workflows for maps and search
    
    * fix(amplify-category-geo): update dependency on cli-core
    
    * fix(amplify-category-geo): add preview banner
    
    * fix(amplify-category-geo): fix failing command unit test
    
    * test: add e2e test for geo
    
    * test: add cases for update and remove
    
    * test: add multiple maps case
    
    * test: add multiple resource cases for geo
    
    * fix(amplify-category-geo): modify the pricing plan walkthrough
    
    * fix(amplify-category-geo): disable pricing plan from advanced walkthrough
    
    * fix(amplify-category-geo): remove YesOrNo enum and use booleans
    
    * chore(amplify-category-geo): increment patch version
    
    * fix(amplify-category-geo): make resource names env specific
    
    * fix(amplify-category-geo): minor changes to place index walkthrough
    
    * chore: update dependency version
    
    * fix(amplify-category-geo): enable unauth access for identity pool (#7858)
    
    * Amplify geo preview feedback items (#7891)
    
    * fix(amplify-category-geo): update aws-exports upon default geo resource change, implement getPermissionPolicies
    
    * Generate Geo Maps, Search configuration for iOS, Android frontends (#8031)
    
    * feat(amplify-frontend-ios, amplify-frontend-android): add geo config in amplifyconfiguration.json
    
    * fix(amplify-category-geo): rebase with master and fix invokePlugin method calls
    
    * chore(frontend): moved frontend native geo changes to different branch
    
    * Update Amplify geo e2e tests (#8122)
    
    * chore(amplify-e2e-core): fix geo category e2e tests
    
    * fix(amplify-category-geo): pricing plan, data provider,  JS plugin name updates (#8155)
    
    * fix(amplify-category-geo): pricing plan walkthrough changes, HERE data provider rename
    
    * fix(amplify-category-geo): update dependency on cli-core
    
    * fix(amplify-category-geo): use amplify-prompts in place of inquirer for prompts (#8182)
    
    * fix(amplify-category-geo): use amplify-prompts in place of inquirer for prompts
    
    * chore(amplify-category-geo): update readme and package version
    
    * fix(amplify-codegen-geo): update cli-core dependency
    
    * fix(amplify-category-geo): nits and minor walkthrough updates
    
    * fix(amplify-category-geo): e2e test changes; remove dev preview banner
    
    * fix(amplify-category-geo): move setting default resource walktrough question to after developer confirms deletion of resource
    
    * fix(amplify-category-geo): nit - use search index term in place of place index
    
    Co-authored-by: Zeyu Li <lizeyu@amazon.com>

---
+++ packages/amplify-category-geo/src/__tests__/commands/geo/update.test.ts
@@ -0,0 +1,49 @@
+import { $TSContext } from 'amplify-cli-core';
+import { updateResource } from '../../../provider-controllers/index';
+import { ServiceName } from '../../../service-utils/constants';
+import { run } from '../../../commands/geo/update';
+
+const mockUpdateResource = updateResource as jest.MockedFunction< typeof updateResource >;
+mockUpdateResource.mockImplementation(async (_, service: string) => service);
+
+jest.mock('amplify-cli-core');
+jest.mock('../../../provider-controllers/index');
+
+
+describe('update command tests', () => {
+    const provider = 'awscloudformation';
+    let mockContext: $TSContext;
+    
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockContext = ({
+            print: {
+                info: jest.fn(),
+                warning: jest.fn()
+            },
+            amplify: {}
+        } as unknown) as $TSContext;
+    });
+
+    it('update resource workflow is invoked for map service', async() => {
+        const service = ServiceName.Map;
+        mockContext.amplify.serviceSelectionPrompt = jest.fn().mockImplementation( async () => {
+            return { service: service, providerName: provider};
+        });
+
+        await run(mockContext);
+
+        expect(mockUpdateResource).toHaveBeenCalledWith(mockContext, service);
+    });
+
+    it('update resource workflow is invoked for place index service', async() => {
+        const service = ServiceName.PlaceIndex;
+        mockContext.amplify.serviceSelectionPrompt = jest.fn().mockImplementation( async () => {
+            return { service: service, providerName: provider};
+        });
+
+        await run(mockContext);
+
+        expect(mockUpdateResource).toHaveBeenCalledWith(mockContext, service);
+    });
+});
---
+++ packages/amplify-category-geo/src/service-walkthroughs/mapWalkthrough.ts
@@ -0,0 +1,199 @@
+import _ from 'lodash';
+import uuid from 'uuid';
+import { merge } from '../service-utils/resourceUtils';
+import { MapParameters, getGeoMapStyle, MapStyle, getMapStyleComponents, EsriMapStyleType } from '../service-utils/mapParams';
+import { apiDocs, ServiceName } from '../service-utils/constants';
+import { $TSContext } from 'amplify-cli-core';
+import { getCurrentMapParameters, getMapFriendlyNames } from '../service-utils/mapUtils';
+import { getGeoServiceMeta, updateDefaultResource, geoServiceExists, getGeoPricingPlan, checkGeoResourceExists} from '../service-utils/resourceUtils';
+import { resourceAccessWalkthrough, pricingPlanWalkthrough, defaultResourceQuestion } from './resourceWalkthrough';
+import { DataProvider } from '../service-utils/resourceParams';
+import { printer, formatter, prompter, alphanumeric } from 'amplify-prompts';
+
+/**
+ * Starting point for CLI walkthrough that creates a map resource
+ * @param context The Amplify Context object
+ * @param parameters The configurations of the Map Resource
+ */
+export const createMapWalkthrough = async (
+  context: $TSContext,
+  parameters: Partial<MapParameters>
+): Promise<Partial<MapParameters>> => {
+  // get the map name
+  parameters = merge(parameters, await mapNameWalkthrough(context));
+
+  // get the access
+  parameters = merge(parameters, await resourceAccessWalkthrough(parameters, ServiceName.Map));
+
+  // initiate pricing plan walkthrough if this is the first Map/Place Index added
+  if (!(await geoServiceExists(ServiceName.Map)) && !(await geoServiceExists(ServiceName.PlaceIndex))) {
+    parameters = merge(parameters, await pricingPlanWalkthrough(context, parameters));
+  }
+
+  // optional advanced walkthrough
+  parameters = merge(parameters, await mapAdvancedWalkthrough(context, parameters));
+
+  // ask if the map should be set as a default. Default to true if it's the only map
+  const currentMapResources = await getGeoServiceMeta(ServiceName.Map);
+  if (currentMapResources && Object.keys(currentMapResources).length > 0) {
+    parameters.isDefault = await prompter.yesOrNo(
+        defaultResourceQuestion(ServiceName.Map),
+        true
+    );
+  }
+  else {
+      parameters.isDefault = true;
+  }
+
+  return parameters;
+};
+
+export const mapNameWalkthrough = async (context: any): Promise<Partial<MapParameters>> => {
+    let mapName;
+    while(!mapName) {
+        const [shortId] = uuid().split('-');
+        const mapNameInput = await prompter.input(
+            'Provide a name for the Map:',
+            { validate: alphanumeric(), initial: `map${shortId}` }
+        );
+        if (await checkGeoResourceExists(mapNameInput)) {
+            printer.info(`Map ${mapNameInput} already exists. Choose another name.`);
+        }
+        else mapName = mapNameInput;
+    }
+    return { name: mapName };
+};
+
+export const mapAdvancedWalkthrough = async (context: $TSContext, parameters: Partial<MapParameters>): Promise<Partial<MapParameters>> => {
+    // const includePricingPlan = await geoServiceExists(ServiceName.Map) || await geoServiceExists(ServiceName.PlaceIndex);
+    const includePricingPlan = false;
+    const currentPricingPlan = parameters.pricingPlan ? parameters.pricingPlan : await getGeoPricingPlan();
+    const advancedSettingOptions: string[] = ['Map style & Map data provider (default: Streets provided by Esri)'];
+    if (includePricingPlan) {
+        advancedSettingOptions.push(`Map pricing plan (current: ${currentPricingPlan})`);
+    }
+    printer.info('Available advanced settings:');
+    formatter.list(advancedSettingOptions);
+    printer.blankLine();
+
+    if(await prompter.yesOrNo('Do you want to configure advanced settings?', false)) {
+        // get the map style parameters
+        parameters = merge(parameters, await mapStyleWalkthrough(parameters));
+
+        if (includePricingPlan) {
+            // get the pricing plan
+            parameters = merge(parameters, await pricingPlanWalkthrough(context, parameters));
+        }
+        else {
+            parameters.pricingPlan = currentPricingPlan;
+        }
+    }
+    else {
+        parameters.dataProvider = DataProvider.Esri;
+        parameters.mapStyleType = EsriMapStyleType.Streets;
+        parameters.pricingPlan = currentPricingPlan;
+    }
+
+    return parameters;
+};
+
+export const mapStyleWalkthrough = async (parameters: Partial<MapParameters>): Promise<Partial<MapParameters>> => {
+    const mapStyleChoices = [
+        { name: 'Streets (data provided by Esri)', value: MapStyle.VectorEsriStreets },
+        { name: 'Berlin (data provided by HERE)', value: MapStyle.VectorHereBerlin },
+        { name: 'Topographic (data provided by Esri)', value: MapStyle.VectorEsriTopographic },
+        { name: 'Navigation (data provided by Esri)', value: MapStyle.VectorEsriNavigation },
+        { name: 'LightGrayCanvas (data provided by Esri)', value: MapStyle.VectorEsriLightGrayCanvas },
+        { name: 'DarkGrayCanvas (data provided by Esri)', value: MapStyle.VectorEsriDarkGrayCanvas }
+    ];
+    const mapStyleDefault = parameters.dataProvider && parameters.mapStyleType ?
+        getGeoMapStyle(parameters.dataProvider, parameters.mapStyleType) : 'VectorEsriStreets';
+
+    const mapStyleDefaultIndex = mapStyleChoices.findIndex(item => item.value === mapStyleDefault);
+    const mapStyleInput = await prompter.pick<'one', string>(
+        `Specify the map style. Refer ${apiDocs.mapStyles}`,
+        mapStyleChoices,
+        { initial: mapStyleDefaultIndex }
+    );
+    return getMapStyleComponents(mapStyleInput as MapStyle);
+};
+
+/**
+ * Starting point for CLI walkthrough that updates an existing map resource
+ * @param context The Amplify Context object
+ * @param parameters The configurations of the Map resource
+ * @param resourceToUpdate Name of the Map resource to update
+ */
+export const updateMapWalkthrough = async (
+    context: $TSContext,
+    parameters: Partial<MapParameters>,
+    resourceToUpdate?: string
+): Promise<Partial<MapParameters>> => {
+    const mapResources = ((await context.amplify.getResourceStatus()).allResources as any[])
+    .filter(resource => resource.service === ServiceName.Map)
+
+    if (mapResources.length === 0) {
+        printer.error('No Map resource to update. Use "amplify add geo" to create a new Map.');
+        return parameters;
+    }
+
+    const mapResourceNames = mapResources.map(resource => resource.resourceName);
+
+    if (resourceToUpdate) {
+        if (!mapResourceNames.includes(resourceToUpdate)) {
+            printer.error(`No Map named ${resourceToUpdate} exists in the project.`);
+            return parameters;
+        }
+    }
+    else {
+        resourceToUpdate = await prompter.pick<'one', string>('Select the Map you want to update', mapResourceNames);
+    }
+
+    parameters.name = resourceToUpdate;
+    parameters = merge(parameters, await getCurrentMapParameters(resourceToUpdate));
+
+    // overwrite the parameters based on user input
+    parameters.accessType = (await resourceAccessWalkthrough(parameters, ServiceName.Map)).accessType;
+
+    const otherMapResources = mapResourceNames.filter(mapResourceName => mapResourceName != resourceToUpdate);
+    // if this is the only map, default cannot be removed
+    if (otherMapResources.length > 0) {
+        const isDefault = await prompter.yesOrNo(defaultResourceQuestion(ServiceName.Map), true);
+        // If a default map is updated, ask for new default
+        if (parameters.isDefault && !isDefault) {
+            await updateDefaultMapWalkthrough(context, resourceToUpdate, otherMapResources);
+        }
+        parameters.isDefault = isDefault;
+    }
+    else {
+        parameters.isDefault = true; // only map is always the default
+    }
+    return parameters;
+};
+
+/**
+ * Walkthrough to choose a different default map
+ * @param context The Amplify Context object
+ * @param currentDefault The current default map name
+ * @param availableMaps The names of available maps
+ * @returns name of the new default map choosen
+ */
+export const updateDefaultMapWalkthrough = async (
+    context: $TSContext,
+    currentDefault: string,
+    availableMaps?: string[]
+): Promise<string> => {
+    if (!availableMaps) {
+        availableMaps = ((await context.amplify.getResourceStatus()).allResources as any[])
+        .filter(resource => resource.service === ServiceName.Map)
+        .map(resource => resource.resourceName);
+    }
+    const otherMapResources = availableMaps.filter(mapResourceName => mapResourceName != currentDefault);
+    if (otherMapResources?.length > 0) {
+        const mapFriendlyNames = await getMapFriendlyNames(otherMapResources);
+        const mapChoices = mapFriendlyNames.map((friendlyName, index) => ({ name: friendlyName, value: otherMapResources[index] }));
+        const defaultMapName = await prompter.pick('Select the Map you want to set as default:', mapChoices);
+        await updateDefaultResource(context, ServiceName.Map, defaultMapName);
+    }
+    return currentDefault;
+}
