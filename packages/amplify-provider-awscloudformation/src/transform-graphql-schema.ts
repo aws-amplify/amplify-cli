1dcaafae8d456929bb4e2ebd6f2cf4189f3d5552
Marc VandenBerg <marc.vandenberg1@gmail.com>
2021-09-23 14:41:47 -0400

    Change: Warn when the opensearch instance size is set small (#8226)
    * Change: Warn when the opensearch instance size is left at or set to default small
    
    * Change: move searchable warning next to the one for auth
    
    * Change: check to make sure searchable is in use
    
    * Fix: remove unused file

--- packages/amplify-provider-awscloudformation/src/transform-graphql-schema.ts
+++ packages/amplify-provider-awscloudformation/src/transform-graphql-schema.ts
@@ -17,7 +17,8 @@ import { ProviderName as providerName } from './constants';
 import { AmplifyCLIFeatureFlagAdapter } from './utils/amplify-cli-feature-flag-adapter';
 import { isAmplifyAdminApp } from './utils/admin-helpers';
 import { JSONUtilities, stateManager } from 'amplify-cli-core';
-import { prompter } from 'amplify-prompts';
+import { ResourceConstants } from 'graphql-transformer-common';
+import { printer, prompter } from 'amplify-prompts';
 
 import {
   collectDirectivesByTypeNames,
@@ -52,6 +53,19 @@ const schemaFileName = 'schema.graphql';
 const schemaDirName = 'schema';
 const ROOT_APPSYNC_S3_KEY = 'amplify-appsync-files';
 const s3ServiceName = 'S3';
+
+export function searchablePushChecks(context, map): void {
+  const searchableModelTypes = Object.keys(map).filter(type => !map[type].includes('searchable') && map[type].includes('model'));
+  if (searchableModelTypes.length) {
+    const currEnv = context.amplify.getEnvInfo().envName;
+    const teamProviderInfo = stateManager.getTeamProviderInfo();
+    const apiCategory = teamProviderInfo[currEnv]?.categories?.api;
+    const instanceType = apiCategory ? apiCategory[ResourceConstants.PARAMETERS.ElasticsearchInstanceType] : null;
+    if (!instanceType || instanceType === 't2.small.elasticsearch') {
+      printer.warn("Your instance type for OpenSearch is t2.small, you may experience performance issues or data loss. Consider reconfiguring with the instructions here https://docs.amplify.aws/cli/graphql-transformer/searchable/")
+    }
+  }
+}
 
 function warnOnAuth(context, map) {
   const unAuthModelTypes = Object.keys(map).filter(type => !map[type].includes('auth') && map[type].includes('model'));
@@ -472,6 +486,7 @@ export async function transformGraphQLSchema(context, options) {
   // Check for common errors
   const directiveMap = collectDirectivesByTypeNames(project.schema);
   warnOnAuth(context, directiveMap.types);
+  searchablePushChecks(context, directiveMap.types);
 
   await transformerVersionCheck(context, resourceDir, previouslyDeployedBackendDir, resourcesToBeUpdated, directiveMap.directives);
 
