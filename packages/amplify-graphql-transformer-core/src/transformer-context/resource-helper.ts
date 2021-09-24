812a67163d6dd33160bf7ace9afd538c83a7af1a
Pavel Lazar <85319655+lazpavel@users.noreply.github.com>
2021-09-24 12:39:48 -0400

    fix(graphql-model-transformer): iam role name does not exceed 64 characters (#8244)

--- packages/amplify-graphql-transformer-core/src/transformer-context/resource-helper.ts
+++ packages/amplify-graphql-transformer-core/src/transformer-context/resource-helper.ts
@@ -1,6 +1,7 @@
 import { GraphQLAPIProvider, TransformerResourceHelperProvider } from '@aws-amplify/graphql-transformer-interfaces';
 import { CfnParameter } from '@aws-cdk/core';
 import { StackManager } from './stack-manager';
+import md5 from 'md5';
 
 export class TransformerResourceHelper implements TransformerResourceHelperProvider {
   private api?: GraphQLAPIProvider;
@@ -8,12 +9,25 @@ export class TransformerResourceHelper implements TransformerResourceHelperProvi
   constructor(private stackManager: StackManager) {}
   public generateResourceName = (name: string): string => {
     if (!this.api) {
-      throw new Error('API not initalized');
+      throw new Error('API not initialized');
     }
     this.ensureEnv();
     const env = (this.stackManager.getParameter('env') as CfnParameter).valueAsString;
     const apiId = this.api!.apiId;
     return `${name}-${apiId}-${env}`;
+  };
+
+  public generateIAMRoleName = (name: string): string => {
+    if (!this.api) {
+      throw new Error('API not initialized');
+    }
+    this.ensureEnv();
+    const env = (this.stackManager.getParameter('env') as CfnParameter).valueAsString;
+    const apiId = this.api!.apiId;
+
+    // 38 = 26(apiId) + 10(env) + 2(-)
+    const shortName = name.slice(0, 64 - 38 - 6) + md5(name).slice(0, 6);
+    return `${shortName}-${apiId}-${env}`; // max of 64.
   };
 
   bind(api: GraphQLAPIProvider) {
