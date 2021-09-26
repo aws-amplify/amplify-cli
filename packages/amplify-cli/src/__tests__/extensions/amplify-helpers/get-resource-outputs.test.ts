ffdbb18ff4874de35b77aa3294d068180b04ce62
togami2864 <tuabobo123@gmail.com>
2021-09-26 23:10:47 +0900

    test: add the test for get-resource-output.ts

--- packages/amplify-cli/src/__tests__/extensions/amplify-helpers/get-resource-outputs.test.ts
+++ packages/amplify-cli/src/__tests__/extensions/amplify-helpers/get-resource-outputs.test.ts
@@ -1,22 +1,23 @@
 import { getResourceOutputs } from '../../../extensions/amplify-helpers/get-resource-outputs';
 import { stateManager } from 'amplify-cli-core';
-
-let metaDataMock = null;
+import mockAmplifyMetaJson from './testData/mockMetaData/mock-amplify-meta.json';
+import expectedResponse from './testData/mockMetaData/response-mock-amplify-meta.json';
 
 jest.mock('amplify-cli-core', () => ({
   stateManager: {
-    getMeta: jest.fn().mockImplementation(() => ({})),
+    getMeta: jest.fn().mockImplementation(() => mockAmplifyMetaJson),
   },
 }));
 
 describe('getResourceOutputs', () => {
   const stateManagerMock = stateManager as jest.Mocked<typeof stateManager>;
   it('should return the correct object', () => {
-    metaDataMock = stateManagerMock.getMeta();
-    const received = getResourceOutputs(metaDataMock);
+    const received = getResourceOutputs(mockAmplifyMetaJson);
+    expect(received).toStrictEqual(expectedResponse);
   });
   it('should return the correct object if amplifyMeta is null', () => {
-    const received = getResourceOutputs(metaDataMock);
+    const received = getResourceOutputs(null);
     expect(stateManagerMock.getMeta).toHaveBeenCalled();
+    expect(received).toStrictEqual(expectedResponse);
   });
 });
