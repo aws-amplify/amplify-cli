943b6eff8cbe73a9448afcb05b6fd22725a2ac6c
Marc VandenBerg <marc.vandenberg1@gmail.com>
2021-09-27 22:50:28 -0400

    Change: Get auto-migrator running
    Runs on any event that triggers a schema transformation

--- packages/amplify-graphql-transformer-core/src/migration/utils.ts
+++ packages/amplify-graphql-transformer-core/src/migration/utils.ts
@@ -1,12 +1,9 @@
 import * as fs from 'fs-extra';
+import { stateManager } from 'amplify-cli-core';
 
 export type SchemaDocument = {
   schema: string;
   filePath: string;
-}
-
-export function getResourceDir(): string {
-  return '';
 }
 
 /**
@@ -15,6 +12,19 @@ export function getResourceDir(): string {
  */
 export async function backupSchema(schemaPath: string): Promise<void> {
   await fs.copyFile(schemaPath, `${schemaPath}.bkp`);
+}
+
+export async function readSingleFileSchema(schemaPath: string): Promise<SchemaDocument[]> {
+  let schema: string = (await fs.readFile(schemaPath)).toString();
+  return [{ schema: schema, filePath: schemaPath }];
+}
+
+export async function replaceFile(newSchema: string, filePath: string): Promise<void> {
+  await fs.writeFile(filePath, newSchema, {encoding: 'utf-8', flag: 'w'});
+}
+
+export function removeBkpExtension(path: string): string {
+  return path.slice(0, path.length - 4);
 }
 
 export async function readSchemaDocuments(schemaDirectoryPath: string): Promise<SchemaDocument[]> {
@@ -36,4 +46,32 @@ export async function readSchemaDocuments(schemaDirectoryPath: string): Promise<
     }
   }
   return schemaDocuments;
+}
+
+export async function undoAllSchemaMigration(resourceDir: string): Promise<void> {
+  const files = await fs.readdir(resourceDir);
+  for (const fileName of files) {
+    if (!fileName.endsWith(".bkp")) {
+      continue;
+    }
+
+    const fullPath = `${resourceDir}/${fileName}`;
+    const stats = await fs.lstat(fullPath);
+    if (stats.isDirectory()) {
+      await undoAllSchemaMigration(fullPath);
+    } else if (stats.isFile()) {
+      fs.moveSync(fullPath, removeBkpExtension(fullPath), { overwrite: true });
+      fs.removeSync(fullPath);
+    }
+  }
+}
+
+export async function getDefaultAuthFromContext(): Promise<string> {
+  const backendConfig = stateManager.getBackendConfig();
+  if (Object.keys(backendConfig.api).length < 1) {
+    return "AMAZON_COGNITO_USER_POOLS"
+  }
+  // Only support one API, so grab the ID
+  const firstAPIID = Object.keys(backendConfig.api)[0];
+  return backendConfig.api[firstAPIID].output.authConfig.defaultAuthentication.authenticationType;
 }
