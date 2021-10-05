5466ec696843cc20844742fb175b11bac32b3c07
Kaustav Ghosh <kaustav.ghosh19@gmail.com>
2021-10-05 15:08:45 -0700

    chore: ddb walkthrough refactor and override tests (#8364)
    * chore: overrides ddb and walthrough refactor tests
    
    * chore: address pr comments
    
    Co-authored-by: Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>

---
+++ packages/amplify-category-storage/src/__tests__/commands/remove.test.ts
@@ -0,0 +1,46 @@
+import { $TSContext, $TSObject } from 'amplify-cli-core';
+import { run } from '../../commands/storage/remove';
+import * as providerController from '../../provider-utils/awscloudformation/index';
+
+jest.mock('../../provider-utils/awscloudformation/index');
+jest.mock('amplify-cli-core');
+
+const providerController_mock = providerController as jest.Mocked<typeof providerController>;
+providerController_mock.updateResource.mockImplementation = jest.fn().mockImplementation(async () => {
+  return 'mockResourceName';
+});
+
+describe('remove ddb command tests', () => {
+  const provider = 'awscloudformation';
+  let mockContext: $TSContext;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockContext = {
+      amplify: {},
+      parameters: {},
+    } as unknown as $TSContext;
+  });
+
+  it('update resource workflow is invoked for DDB with no params', async () => {
+    mockContext.amplify.removeResource = jest.fn().mockImplementation(async () => {
+      return;
+    });
+
+    await run(mockContext);
+
+    expect(mockContext.amplify.removeResource).toHaveBeenCalledWith(mockContext, 'storage', undefined);
+  });
+
+  it('update resource workflow is invoked for DDB with params as resourceName', async () => {
+    const mockResourceName = 'mockResourceName';
+    mockContext.parameters.first = mockResourceName;
+    mockContext.amplify.removeResource = jest.fn().mockImplementation(async () => {
+      return;
+    });
+
+    await run(mockContext);
+
+    expect(mockContext.amplify.removeResource).toHaveBeenCalledWith(mockContext, 'storage', mockResourceName);
+  });
+});
