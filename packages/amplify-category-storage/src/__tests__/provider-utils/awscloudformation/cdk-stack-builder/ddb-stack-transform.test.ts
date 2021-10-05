cf5e7f5709ba6e93d9b31f37fea3e84349b8730d
Kaustav Ghosh <kaustav.ghosh19@gmail.com>
2021-10-05 15:08:45 -0700

    chore: ddb walkthrough refactor and override tests (#8364)
    * chore: overrides ddb and walthrough refactor tests
    
    * chore: address pr comments
    
    Co-authored-by: Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>

---
+++ packages/amplify-category-storage/src/__tests__/provider-utils/awscloudformation/cdk-stack-builder/ddb-stack-transform.test.ts
@@ -0,0 +1,84 @@
+/* These tests, test the tranform and in rurn the cdk builder too which is used within this file */
+
+import { JSONUtilities, buildOverrideDir, pathManager } from 'amplify-cli-core';
+import * as fs from 'fs-extra';
+import { DDBStackTransform } from '../../../../provider-utils/awscloudformation/cdk-stack-builder/ddb-stack-transform';
+import {
+  DynamoDBCLIInputs,
+  FieldType,
+} from '../../../../provider-utils/awscloudformation/service-walkthrough-types/dynamoDB-user-input-types';
+import { DynamoDBInputState } from '../../../../provider-utils/awscloudformation/service-walkthroughs/dynamoDB-input-state';
+import path from 'path';
+
+jest.mock('amplify-cli-core', () => ({
+  buildOverrideDir: jest.fn().mockResolvedValue(false),
+  JSONUtilities: {
+    writeJson: jest.fn(),
+    readJson: jest.fn(),
+  },
+  pathManager: {
+    getBackendDirPath: jest.fn().mockReturnValue('mockbackendpath'),
+    getResourceDirectoryPath: jest.fn().mockReturnValue('mockresourcepath'),
+  },
+}));
+jest.mock('fs-extra', () => ({
+  readFileSync: () => '{ "Cognito": { "provider": "aws"}}',
+  existsSync: () => true,
+  ensureDirSync: jest.fn(),
+}));
+
+jest.mock('path', () => ({
+  join: jest.fn().mockReturnValue('mockjoinedpath'),
+  resolve: jest.fn().mockReturnValue('mockjoinedpath'),
+}));
+
+jest.mock('../../../../provider-utils/awscloudformation/service-walkthroughs/dynamoDB-input-state');
+
+describe('Test DDB transform generates correct CFN template', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Generated ddb template with all CLI configurations set with no overrides', async () => {
+    const resourceName = 'mockResource';
+    const cliInputsJSON: DynamoDBCLIInputs = {
+      resourceName: resourceName,
+      tableName: 'mocktablename',
+      partitionKey: {
+        fieldName: 'id',
+        fieldType: FieldType.string,
+      },
+      sortKey: {
+        fieldName: 'name',
+        fieldType: FieldType.number,
+      },
+      gsi: [
+        {
+          name: 'gsiname',
+          partitionKey: {
+            fieldName: 'name',
+            fieldType: FieldType.number,
+          },
+        },
+        {
+          name: 'updategsiname',
+          partitionKey: {
+            fieldName: 'col',
+            fieldType: FieldType.string,
+          },
+        },
+      ],
+      triggerFunctions: [],
+    };
+
+    jest.spyOn(DynamoDBInputState.prototype, 'getCliInputPayload').mockImplementation(() => cliInputsJSON);
+    const ddbTransform = new DDBStackTransform(resourceName);
+    await ddbTransform.transform();
+
+    console.log(ddbTransform._cfn);
+    console.log(ddbTransform._cfnInputParams);
+
+    expect(ddbTransform._cfn).toMatchSnapshot();
+    expect(ddbTransform._cfnInputParams).toMatchSnapshot();
+  });
+});
--- packages/amplify-category-storage/src/commands/storage/override.ts
+++ packages/amplify-category-storage/src/commands/storage/override.ts
@@ -2,10 +2,8 @@
     entry code for amplify override root
 */
 
-import { generateOverrideSkeleton, $TSContext, FeatureFlags, stateManager, pathManager } from 'amplify-cli-core';
-import { printer } from 'amplify-prompts';
-import * as fs from 'fs-extra';
-import inquirer from 'inquirer';
+import { generateOverrideSkeleton, $TSContext, stateManager, pathManager } from 'amplify-cli-core';
+import { printer, prompter } from 'amplify-prompts';
 import { DynamoDBInputState } from '../../provider-utils/awscloudformation/service-walkthroughs/dynamoDB-input-state';
 import { DDBStackTransform } from '../../provider-utils/awscloudformation/cdk-stack-builder/ddb-stack-transform';
 import * as path from 'path';
@@ -14,70 +12,55 @@ import { categoryName } from '../../constants';
 export const name = 'override';
 
 export const run = async (context: $TSContext) => {
-  if (FeatureFlags.getBoolean('overrides.project')) {
-    const { amplify } = context;
-    const amplifyMeta = stateManager.getMeta();
+  const amplifyMeta = stateManager.getMeta();
 
-    const storageResources: string[] = [];
+  const storageResources: string[] = [];
 
+  if (amplifyMeta[categoryName]) {
     Object.keys(amplifyMeta[categoryName]).forEach(resourceName => {
       storageResources.push(resourceName);
     });
-
-    if (storageResources.length === 0) {
-      const errMessage = 'No resources to override. You need to add a resource.';
-      printer.error(errMessage);
-      return;
-    }
-
-    let selectedResourceName: string = storageResources[0];
-
-    if (storageResources.length > 1) {
-      const resourceAnswer = await inquirer.prompt({
-        type: 'list',
-        name: 'resource',
-        message: 'Which resource would you like to add overrides for?',
-        choices: storageResources,
-      });
-      selectedResourceName = resourceAnswer.resource;
-    }
-
-    const destPath = pathManager.getResourceDirectoryPath(undefined, categoryName, selectedResourceName);
-    fs.ensureDirSync(destPath);
-
-    const srcPath = path.join(
-      __dirname,
-      '..',
-      '..',
-      '..',
-      'resources',
-      'overrides-resource',
-      amplifyMeta[categoryName][selectedResourceName].service,
-    );
-
-    // Make sure to migrate first
-    if (amplifyMeta[categoryName][selectedResourceName].service === 'DynamoDB') {
-      const resourceInputState = new DynamoDBInputState(selectedResourceName);
-      if (!resourceInputState.cliInputFileExists()) {
-        if (await amplify.confirmPrompt('File migration required to continue. Do you want to continue?', true)) {
-          resourceInputState.migrate();
-          const stackGenerator = new DDBStackTransform(selectedResourceName);
-          stackGenerator.transform();
-        } else {
-          return;
-        }
-      }
-    } else if (amplifyMeta[categoryName][selectedResourceName].service === 'S3') {
-      // S3 migration logic goes in here
-    }
-
-    await generateOverrideSkeleton(context, srcPath, destPath);
-  } else {
-    printer.info('Storage overrides is currently not turned on. In amplify/cli.json file please include the following:');
-    printer.info(`{
-      override: {
-         storage: true
-      }
-    }`);
   }
+
+  if (storageResources.length === 0) {
+    const errMessage = 'No resources to override. You need to add a resource.';
+    printer.error(errMessage);
+    return;
+  }
+
+  let selectedResourceName: string = storageResources[0];
+
+  if (storageResources.length > 1) {
+    selectedResourceName = await prompter.pick('Which resource would you like to add overrides for?', storageResources);
+  }
+
+  const destPath = pathManager.getResourceDirectoryPath(undefined, categoryName, selectedResourceName);
+
+  const srcPath = path.join(
+    __dirname,
+    '..',
+    '..',
+    '..',
+    'resources',
+    'overrides-resource',
+    amplifyMeta[categoryName][selectedResourceName].service,
+  );
+
+  // Make sure to migrate first
+  if (amplifyMeta[categoryName][selectedResourceName].service === 'DynamoDB') {
+    const resourceInputState = new DynamoDBInputState(selectedResourceName);
+    if (!resourceInputState.cliInputFileExists()) {
+      if (await prompter.yesOrNo('File migration required to continue. Do you want to continue?', true)) {
+        resourceInputState.migrate();
+        const stackGenerator = new DDBStackTransform(selectedResourceName);
+        stackGenerator.transform();
+      } else {
+        return;
+      }
+    }
+  } else if (amplifyMeta[categoryName][selectedResourceName].service === 'S3') {
+    // S3 migration logic goes in here
+  }
+
+  await generateOverrideSkeleton(context, srcPath, destPath);
 };
