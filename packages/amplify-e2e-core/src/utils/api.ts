efbd04873815be2f0268d2ba072a022c8e699a52
Pavel Lazar <85319655+lazpavel@users.noreply.github.com>
2021-10-09 13:31:31 -0400

    Revert custom override resolvers (#8409)
    * Revert "feat(amplify-provider-awscloudformation): enable custom resolvers for v2 transformer (#8332)"
    
    This reverts commit 1c730423085b09b4ba681193f020c30bc2a5e3cc.
    
    * Revert "feat(amplify-provider-awscloudformation): merge user config with transform generated resolvers (#8262)"
    
    This reverts commit f25abbf68f1e268f6e3dcb362685e7fe9ec760c8.

--- packages/amplify-e2e-core/src/utils/api.ts
+++ packages/amplify-e2e-core/src/utils/api.ts
@@ -11,13 +11,3 @@ export function updateConfig(projectDir: string, projectName: string, config: an
   const configPath = path.join(projectDir, 'amplify', 'backend', 'api', projectName, TRANSFORM_CONFIG_FILE_NAME);
   fs.writeFileSync(configPath, JSON.stringify(config, null, 4));
 }
-
-export function addCustomResolver(projectDir: string, projectName: string, resolverName: string, resolver: string) {
-  const resolverPath = path.join(projectDir, 'amplify', 'backend', 'api', projectName, 'resolvers', resolverName);
-  fs.writeFileSync(resolverPath, resolver);
-}
-
-export function addCustomResourcesJson(projectDir: string, projectName: string, json?: string) {
-  const jsonPath = path.join(projectDir, 'amplify', 'backend', 'api', projectName, 'stacks', 'CustomResources.json');
-  fs.writeFileSync(jsonPath, json || '{}');
-}
