6df4f685ed79e39f6a61c4c1d4e57f99eabadeb0
Marc VandenBerg <marc.vandenberg1@gmail.com>
2021-10-09 18:41:25 -0400

    Schema migration changes
    add in migration viability checks
    updated according to api review

--- packages/amplify-cli-core/src/feature-flags/featureFlags.ts
+++ packages/amplify-cli-core/src/feature-flags/featureFlags.ts
@@ -551,6 +551,12 @@ export class FeatureFlags {
         defaultValueForExistingProjects: false,
         defaultValueForNewProjects: true,
       },
+      {
+        name: 'suppressSchemaMigrationPrompt',
+        type: 'boolean',
+        defaultValueForExistingProjects: false,
+        defaultValueForNewProjects: false,
+      }
     ]);
 
     this.registerFlag('frontend-ios', [
--- packages/amplify-graphql-transformer-core/src/migration/migrate.ts
+++ packages/amplify-graphql-transformer-core/src/migration/migrate.ts
@@ -4,8 +4,8 @@ import { migrateKeys } from './migrators/key'
 import { migrateAuth } from './migrators/auth'
 import { migrateConnection } from './migrators/connection'
 import {
-  backupSchema,
-  getDefaultAuthFromContext,
+  backupSchema, combineSchemas, DiffDocument,
+  getDefaultAuthFromContext, getSchemaDiffs,
   readSchemaDocuments,
   readSingleFileSchema,
   replaceFile,
@@ -16,6 +16,7 @@ import { DocumentNode } from 'graphql/language';
 import { prompter, printer } from 'amplify-prompts';
 import path from 'path';
 import { $TSContext, exitOnNextTick, FeatureFlags, pathManager, stateManager } from 'amplify-cli-core';
+import { detectCustomResolvers, detectOverriddenResolvers, detectUnsupportedDirectives, graphQLUsingSQL } from './schema-inspector';
 
 const cliToMigratorAuthMap: Map<string, string> = new Map<string, string>([
   ['API_KEY', 'apiKey'],
@@ -23,6 +24,17 @@ const cliToMigratorAuthMap: Map<string, string> = new Map<string, string>([
   ['AMAZON_COGNITO_USER_POOLS', 'userPools'],
   ['OPENID_CONNECT', 'oidc']
 ]);
+
+const MIGRATION_URL = "<insert migration docs URL here>";
+
+async function showDiffs(diffDocs: DiffDocument[]): Promise<void> {
+  await printer.info("Changes made to your schemas:\n");
+  for (let doc of diffDocs) {
+    await printer.info(`File: ${doc.filePath}`);
+    await printer.info(doc.schemaDiff);
+    await printer.info("\n");
+  }
+}
 
 function migrateGraphQLSchema(schema: string, authMode: string, massSchema: DocumentNode): string {
   let output = parse(schema)
@@ -40,14 +52,25 @@ function migrateGraphQLSchema(schema: string, authMode: string, massSchema: Docu
   return print(output);
 }
 
-async function askMigration(singleFileExists: boolean, directoryExists: boolean): Promise<boolean> {
-  await printer.info(`About to begin schema migration, found valid target(s) for migration:\n${singleFileExists ? 'schema.graphql\n' : ''}${directoryExists ? 'schema directory\n' : ''}\n`);
-  await printer.info('If you confirm migration, you will receive no further prompts and all migration will be completed. The CLI will exit after migration and will not push your resources to give you time to review.');
-  await printer.warn('If you are using @auth, we strongly recommend spending extra time reviewing your auth rules after migration to ensure they\'re accurate');
-  return await prompter.confirmContinue("About to start migrating to V2 transformer, are you ready to continue?");
+async function getSchemaDocs(resourceDir: string): Promise<SchemaDocument[]> {
+  const schemaFilePath = path.join(resourceDir, 'schema.graphql');
+  const schemaDirectoryPath = path.join(resourceDir, 'schema');
+  const schemaFileExists = fs.existsSync(schemaFilePath);
+  const schemaDirectoryExists = fs.existsSync(schemaDirectoryPath);
+
+  if (!schemaFileExists && !schemaDirectoryExists) {
+    return [];
+  }
+  if (schemaFileExists) {
+    return await readSingleFileSchema(schemaFilePath);
+  }
+  else if (schemaDirectoryExists) {
+    return await readSchemaDocuments(schemaDirectoryPath);
+  }
+  return [];
 }
 
-async function runMigration(schemas: SchemaDocument[], authMode: string): Promise<void> {
+async function runMigration(schemas: SchemaDocument[], authMode: string): Promise<boolean> {
   let fullSchema: string;
   let schemaList: string[] = new Array(schemas.length);
   let backupSchemaPromiseArray: Promise<void>[] = [];
@@ -59,12 +82,41 @@ async function runMigration(schemas: SchemaDocument[], authMode: string): Promis
 
   fullSchema = schemaList.join('\n');
   let fullSchemaNode = parse(fullSchema);
-  let writeNewPromiseArray: Promise<void>[] = [];
-  schemas.forEach(doc => {
+  let newSchemaList: SchemaDocument[] = new Array(schemas.length);
+  schemas.forEach((doc, idx) => {
     const newSchema = migrateGraphQLSchema(doc.schema, authMode, fullSchemaNode);
-    writeNewPromiseArray.push(replaceFile(newSchema, doc.filePath));
+    newSchemaList[idx] = { schema: newSchema, filePath: doc.filePath };
+  });
+
+  const diffs = getSchemaDiffs(schemas, newSchemaList);
+  await showDiffs(diffs);
+
+  const migrationChoices: Array<string> = ["Yes", "Yes, but exit the CLI so I can review/edit my new schemas before compiling/pushing",
+    "No, continue my operation with my old schemas"];
+  const migrationChoice: string = await prompter.pick("Do you want to proceed with the auto-migration?", migrationChoices);
+  const migrationIndex: number = migrationChoices.findIndex((value) => {
+    return value === migrationChoice;
+  });
+  if (!migrationIndex || (migrationIndex === 2)) {
+    return false;
+  }
+
+  let writeNewPromiseArray: Promise<void>[] = [];
+  newSchemaList.forEach((doc) => {
+    writeNewPromiseArray.push(replaceFile(doc.schema, doc.filePath));
   });
   await Promise.all(writeNewPromiseArray);
+
+  if (migrationIndex === 1) {
+    // Customer has selected to review their new schemas before compiling or pushing
+    exitOnNextTick(0);
+  }
+  await printer.info(`Success! Auto-migration completed. Just in case, we've placed a back-up of your old 
+GraphQL schema file(s) at these location(s):`);
+  for(let schema of schemas) {
+    await printer.info(`- ${schema.filePath}.bkp`);
+  }
+  return true;
 }
 
 export async function updateTransformerVersion(env: string): Promise<void> {
@@ -90,6 +142,7 @@ export async function updateTransformerVersion(env: string): Promise<void> {
     cliJSON.features.graphqltransformer = {}
   }
   cliJSON.features.graphqltransformer.useexperimentalpipelinedtransformer = true;
+  cliJSON.features.graphqltransformer.suppressSchemaMigrationPrompt = true;
 
   if (envCLI) {
     stateManager.setCLIJSON(projectPath, cliJSON, env);
@@ -100,35 +153,17 @@ export async function updateTransformerVersion(env: string): Promise<void> {
   await FeatureFlags.reloadValues();
 }
 
-export async function migrateToV2Transformer(resourceDir: string, context: $TSContext): Promise<boolean> {
+export async function migrateToV2Transformer(resourceDir: string, context: $TSContext, schemaDocs: SchemaDocument[]): Promise<boolean> {
   const { envName } = context.amplify.getEnvInfo();
   const defaultAuth = await getDefaultAuthFromContext();
   const authMode = cliToMigratorAuthMap.get(defaultAuth);
   if (!authMode) {
     throw Error(`Unidentified authorization mode for API found: ${defaultAuth}`);
   }
-  const schemaFilePath = path.join(resourceDir, 'schema.graphql');
-  const schemaDirectoryPath = path.join(resourceDir, 'schema');
-  const schemaFileExists = fs.existsSync(schemaFilePath);
-  const schemaDirectoryExists = fs.existsSync(schemaDirectoryPath);
 
-  if (schemaFileExists || schemaDirectoryExists) {
-    let migrateCheck = await askMigration(schemaFileExists, schemaDirectoryExists);
-    if (!migrateCheck) {
-      await printer.info("Skipping schema migration, did not receive confirmation.");
-      return false;
-    }
-  }
   try {
-    if (schemaFileExists) {
-      await runMigration(await readSingleFileSchema(schemaFilePath), authMode);
-    }
-    if (schemaDirectoryExists) {
-      await runMigration(await readSchemaDocuments(schemaDirectoryPath), authMode);
-    }
+    await runMigration(schemaDocs, authMode);
     await updateTransformerVersion(envName);
-    await printer.info("Successfully migrated schema(s), please review them to ensure they've been properly migrated before running 'amplify push'. Pay extra care with any auth rules to ensure your data remains secured.");
-    exitOnNextTick(0);
   }
   catch (error) {
     await printer.error("Encountered an error while migrating schema to V2 transformer, reverting to old schemas");
@@ -136,4 +171,43 @@ export async function migrateToV2Transformer(resourceDir: string, context: $TSCo
     throw error;
   }
   return true;
+}
+
+
+export async function attemptV2TransformerMigration(resourceDir: string, apiName: string, context: $TSContext): Promise<boolean> {
+  if (graphQLUsingSQL(apiName)) {
+    return false;
+  }
+  await printer.warn(`Amplify CLI made new improvements to GraphQL APIs, such as pipeline 
+resolvers support, deny-by-default authorization, improved search and result 
+aggregations. Learn all about the new changes and the required migration process 
+here: ${MIGRATION_URL}`);
+  const schemaDocs = await getSchemaDocs(resourceDir);
+  const fullSchema = combineSchemas(schemaDocs);
+  const usingCustomResolvers = detectCustomResolvers(parse(fullSchema));
+  const usingOverriddenResolvers = detectOverriddenResolvers(apiName);
+  const unsupportedDirectives: Array<string> = detectUnsupportedDirectives(fullSchema);
+  if (usingCustomResolvers || unsupportedDirectives.length > 0) {
+    await printer.info(`We detected that your GraphQL schema can not be auto-migrated because:`);
+    if (usingCustomResolvers) {
+      await printer.info(`- You have configured custom resolvers for your GraphQL API`);
+    }
+    if (usingOverriddenResolvers) {
+      await printer.info(`- You have overwritten an Amplify generated resolver`);
+    }
+    if (unsupportedDirectives.length > 0) {
+      await printer.info(`- You are using the following directives not supported in the new transformer:\n\t ${unsupportedDirectives.join(', ')}`);
+    }
+    await printer.info(`To migrate to the new GraphQL API capabilities, follow the step-by-step instructions
+here: ${MIGRATION_URL}`);
+    return false;
+  }
+  await printer.warn(`If you have overridden any of the Amplify-generated resolvers, you will
+need to migrate manually. The GraphQL migrator does not detect this,
+so you should decline migration.`);
+  const chooseToRunMigration: string = await prompter.pick(`We detected that your GraphQL schema can be auto-migrated! In particular, we've made 
+a number of authorization rule changes. Let's review the changes we'll apply to your
+GraphQL schema:`, ['Continue', `No, I'll migrate later`]);
+  const runMigration: boolean = chooseToRunMigration === 'Continue';
+  return runMigration ? migrateToV2Transformer(resourceDir, context, schemaDocs) : runMigration;
 }
