c2fde74f3ee1ed34f521c5818fc3928f35fe65e7
jhockett <johocke@amazon.com>
2021-10-11 13:40:55 -0700

    test: improve unit tests for headless schema validation

--- packages/amplify-util-headless-input/src/__tests__/storage/update/index.test.ts
+++ packages/amplify-util-headless-input/src/__tests__/storage/update/index.test.ts
@@ -2,11 +2,18 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import { validateUpdateStorageRequest } from '../../../index';
 
-const assetRoot = path.resolve(path.join(__dirname, '..', '..', 'assets'));
+const topLevelMissingProperty: ExpectedFunction = (missingProperty: string) =>
+  `Data did not validate against the supplied schema. Underlying errors were [{\"keyword\":\"required\",\"dataPath\":\"\",\"schemaPath\":\"#/required\",\"params\":{\"missingProperty\":\"${missingProperty}\"},\"message\":\"should have required property '${missingProperty}'\"}]`;
+const serviceConfigurationMissingProperty: ExpectedFunction = (missingProperty: string) =>
+  `Data did not validate against the supplied schema. Underlying errors were [{\"keyword\":\"required\",\"dataPath\":\".serviceConfiguration\",\"schemaPath\":\"#/required\",\"params\":{\"missingProperty\":\"${missingProperty}\"},\"message\":\"should have required property '${missingProperty}'\"}]`;
+
+type ExpectedFunction = (missingProperty: string) => string;
+
+const updateStorageAssetsPath = path.resolve(path.join(__dirname, '..', '..', 'assets', 'storage', 'update'));
 
 describe('validate update storage request', () => {
   it('returns valid payload', async () => {
-    const rawRequest = fs.readFileSync(path.join(assetRoot, 'storage', 'update', 'validUpdateStorageRequest.json'), 'utf8');
+    const rawRequest = fs.readFileSync(path.join(updateStorageAssetsPath, 'validUpdateStorageRequest.json'), 'utf8');
     const result = await validateUpdateStorageRequest(rawRequest);
     expect(result).toMatchSnapshot();
   });
@@ -18,12 +25,28 @@ describe('rejects promise when invalid payload', () => {
     await expect(resultPromise).rejects.toBeTruthy();
   });
 
-  const missingFields = ['permissions', 'resourceName', 'serviceName', 'version'];
-  for (const field of missingFields) {
-    it(`rejects request with missing field ${field}`, async () => {
-      const rawRequest = fs.readFileSync(path.join(assetRoot, 'storage', 'update', `invalidRequest.missing.${field}.json`), 'utf8');
-      const resultPromise = validateUpdateStorageRequest(rawRequest);
-      await expect(resultPromise).rejects.toBeTruthy();
-    });
-  }
+  test.each([
+    [
+      'version',
+      'invalidRequest.missing.version.json',
+      (missingProperty: string) => `data does not have a top level \"${missingProperty}\" field`,
+    ],
+    [
+      'version',
+      'invalidRequest.string.version.json',
+      (missingProperty: string) => `data does not have a top level \"${missingProperty}\" field`,
+    ],
+    ['version', 'invalidRequest.invalid.version.json', (missingProperty: string) => `No schema found for ${missingProperty} 9999`],
+    ['serviceConfiguration', 'invalidRequest.missing.serviceConfiguration.json', topLevelMissingProperty],
+    ['serviceName', 'invalidRequest.missing.serviceName.json', serviceConfigurationMissingProperty],
+    ['resourceName', 'invalidRequest.missing.resourceName.json', serviceConfigurationMissingProperty],
+    ['permissions', 'invalidRequest.missing.permissions.json', serviceConfigurationMissingProperty],
+  ])(
+    'with a rejected promise when the payload is missing %s',
+    async (missingProperty: string, fileName: string, expectedFn: ExpectedFunction) => {
+      const rawRequest = fs.readFileSync(path.join(updateStorageAssetsPath, fileName), 'utf8');
+
+      await expect(validateUpdateStorageRequest(rawRequest)).rejects.toThrow(expectedFn(missingProperty));
+    },
+  );
 });
