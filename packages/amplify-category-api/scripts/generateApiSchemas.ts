1b6dc6e6a52fa8373f4a3986dd81757879d500b9
Akshay <akshayupadhyay3@gmail.com>
2021-10-15 15:55:27 -0700

    feat: api-input-state and JSON schema

---
+++ packages/amplify-category-api/scripts/generateApiSchemas.ts
@@ -0,0 +1,18 @@
+import * as SchemaGenerator from 'amplify-cli-core';
+
+type TypeDef = SchemaGenerator.TypeDef;
+
+const ApigwTypeDef: TypeDef = {
+  typeName: 'APIGatewayCLIInputs',
+  service: 'API Gateway',
+};
+const AppsyncApiTypeDef: TypeDef = {
+  typeName: 'AppsyncCLIInputs',
+  service: 'appsync',
+};
+
+// Defines the type names and the paths to the TS files that define them
+const apiCategoryTypeDefs: TypeDef[] = [ApigwTypeDef, AppsyncApiTypeDef];
+
+const schemaGenerator = new SchemaGenerator.CLIInputSchemaGenerator(apiCategoryTypeDefs);
+schemaGenerator.generateJSONSchemas(); // convert CLI input data into json schemas.
