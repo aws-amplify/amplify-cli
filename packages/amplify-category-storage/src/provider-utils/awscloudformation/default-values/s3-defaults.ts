295351c81bf5e6286e9cf3df4c09b763d9be8a77
Sachin Panemangalore <83682223+sachscode@users.noreply.github.com>
2021-10-18 10:16:05 -0700

    (feat) Override for S3 - ( includues refactor s3-walkthrough and migrate) (#8383)
    Co-authored-by: Sachin Panemangalore <sachinrp@amazon.com>

--- packages/amplify-category-storage/src/provider-utils/awscloudformation/default-values/s3-defaults.ts
+++ packages/amplify-category-storage/src/provider-utils/awscloudformation/default-values/s3-defaults.ts
@@ -1,28 +1,20 @@
+
+import { $TSAny } from 'amplify-cli-core';
 import { v4 as uuid } from 'uuid';
+import { S3AccessType, S3UserInputs } from '../service-walkthrough-types/s3-user-input-types';
 
-export const getAllDefaults = (project: Project) => {
+export const getAllDefaults = (project: $TSAny, shortId : string) : S3UserInputs  => {
   const name = project.projectConfig.projectName.toLowerCase();
-  const [shortId] = uuid().split('-');
-
-  const authRoleName = {
-    Ref: 'AuthRoleName',
-  };
-
-  const unauthRoleName = {
-    Ref: 'UnauthRoleName',
-  };
-
-  const defaults = {
+  const defaults : S3UserInputs = {
     resourceName: `s3${shortId}`,
+    policyUUID : shortId,
     bucketName: `${name}${uuid().replace(/-/g, '')}`.substr(0, 47), // 63(max) - 10 (envName max) - 4(stack name) - 2(separators)
-    authPolicyName: `s3_amplify_${shortId}`,
-    unauthPolicyName: `s3_amplify_${shortId}`,
-    authRoleName,
-    unauthRoleName,
-    storageAccess: 'auth',
-    selectedGuestPermissions: ['s3:GetObject', 's3:ListBucket'],
-    selectedAuthenticatedPermissions: ['s3:GetObject', 's3:ListBucket'],
-    triggerFunction: 'NONE',
+    storageAccess: S3AccessType.AUTH_ONLY,
+    guestAccess: [],
+    authAccess:  [],
+    triggerFunction : undefined,
+    groupAccess : {},
+    groupList : []
   };
 
   return defaults;
