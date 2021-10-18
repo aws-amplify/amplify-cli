106b58e08e05f9962dccd31262fa212a610a7a83
Sachin Panemangalore <83682223+sachscode@users.noreply.github.com>
2021-10-18 10:16:05 -0700

    (feat) Override for S3 - ( includues refactor s3-walkthrough and migrate) (#8383)
    Co-authored-by: Sachin Panemangalore <sachinrp@amazon.com>

--- packages/amplify-migration-tests/src/__tests__/update_tests/storage_migration_update.test.ts
+++ packages/amplify-migration-tests/src/__tests__/update_tests/storage_migration_update.test.ts
@@ -1,6 +1,8 @@
+import { $TSObject } from 'amplify-cli-core';
 import {
   addDDBWithTrigger,
   addSimpleDDB,
+  addS3,
   amplifyPushAuth,
   createNewProjectDir,
   deleteProject,
@@ -8,6 +10,9 @@ import {
   getDDBTable,
   getProjectMeta,
   updateDDBWithTrigger,
+  updateS3AddTrigger,
+  checkIfBucketExists,
+  addS3Storage
 } from 'amplify-e2e-core';
 import { initJSProjectWithProfile } from '../../migration-helpers';
 
@@ -55,5 +60,64 @@ describe('amplify add/update storage(DDB)', () => {
     expect(table2StreamArn).toBeDefined();
     const table2Configs = await getDDBTable(table2Name, table2Region);
     expect(table2Configs.Table.TableArn).toEqual(table2Arn);
+  });
+});
+
+
+function getPluginServiceMetaFromAmplifyMeta( amplifyMeta : $TSObject, pluginServiceName : string ):$TSObject {
+  for ( const resourceName of Object.keys(amplifyMeta.storage)){
+    if ( amplifyMeta.storage[resourceName].service === pluginServiceName ){
+      return amplifyMeta.storage[resourceName];
+    }
+  }
+  throw new Error(`${pluginServiceName} Resource not found in meta-file`);
+}
+
+function getPluginDependsOnFromResourceMeta(  resourceMeta : $TSObject, dependencyCategory : string ){
+  for ( const dependentResource of resourceMeta.dependsOn ){
+    if ( dependentResource.category === dependencyCategory){
+      return dependentResource;
+    }
+  }
+  throw new Error(`${resourceMeta.providerMetadata.logicalId} has no dependsOn of ${dependencyCategory}`);
+}
+
+describe('amplify add/update storage(S3)', () => {
+  let projRoot: string;
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('s3-add-update migration');
+  });
+
+  afterEach(async () => {
+    // await deleteProject(projRoot);
+    // deleteProjectDir(projRoot);
+  });
+
+  it('init a project and add s3 bucket & update with new trigger', async () => {
+    // init, add storage and push with local cli
+    await initJSProjectWithProfile(projRoot, {});
+    await addS3Storage(projRoot);
+    await amplifyPushAuth(projRoot);
+    // update and push with new codebase
+    await updateS3AddTrigger(projRoot, { testingWithLatestCodebase: true });
+    await amplifyPushAuth(projRoot, true /*latest codebase*/);
+
+    const meta = getProjectMeta(projRoot);
+    const s3Meta = getPluginServiceMetaFromAmplifyMeta( meta, 'S3');
+    const output = s3Meta.output;
+    for ( const dependentResource of s3Meta.dependsOn ){
+      if ( dependentResource.category === "function"){
+        return dependentResource;
+      }
+    }
+
+    expect(output.BucketName).toBeDefined();
+    expect(output.Region).toBeDefined();
+ 
+    const bucketExists  = await checkIfBucketExists(output.BucketName, output.Region);
+    expect(bucketExists).toMatchObject({}); 
+    const dependsOnFunctionMeta = getPluginDependsOnFromResourceMeta( s3Meta , 'function');
+    expect(dependsOnFunctionMeta.resourceName).toBeDefined();
+
   });
 });
