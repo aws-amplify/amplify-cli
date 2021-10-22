3b9b963d5ef235b90635ed19e3a2c609b7524a37
Sachin Panemangalore <83682223+sachscode@users.noreply.github.com>
2021-10-21 18:46:51 -0700

    (fix) s3 add with no-auth resource should succeed (#8520)
    Co-authored-by: Sachin Panemangalore <sachinrp@amazon.com>

--- packages/amplify-category-storage/src/provider-utils/awscloudformation/service-walkthroughs/s3-auth-api.ts
+++ packages/amplify-category-storage/src/provider-utils/awscloudformation/service-walkthroughs/s3-auth-api.ts
@@ -1,6 +1,6 @@
-import { $TSAny, $TSContext} from "amplify-cli-core";
-import { printer } from "amplify-prompts";
-import { AmplifyCategories } from "amplify-cli-core";
+import { $TSAny, $TSContext } from 'amplify-cli-core';
+import { printer } from 'amplify-prompts';
+import { AmplifyCategories } from 'amplify-cli-core';
 
 /* This file contains all functions interacting with AUTH category */
 
@@ -11,27 +11,35 @@ import { AmplifyCategories } from "amplify-cli-core";
  * @param context  used to fetch all auth resources used by storage(S3)
  * @returns Name of the auth resource used by S3
  */
-export  async function getAuthResourceARN( context : $TSContext ) : Promise<string> {
-    let authResources = (await context.amplify.getResourceStatus('auth')).allResources;
-    authResources = authResources.filter((resource: $TSAny) => resource.service === 'Cognito');
-    if (authResources.length === 0) {
-      throw new Error('No auth resource found. Please add it using amplify add auth');
-    }
-    return authResources[0].resourceName as string;
+export async function getAuthResourceARN(context: $TSContext): Promise<string> {
+  let authResources = (await context.amplify.getResourceStatus('auth')).allResources;
+  authResources = authResources.filter((resource: $TSAny) => resource.service === 'Cognito');
+  if (authResources.length === 0) {
+    throw new Error('No auth resource found. Please add it using amplify add auth');
+  }
+  return authResources[0].resourceName as string;
 }
 /**
  * Migrate all Auth resources used by Storage(S3) for Override feature.
  * @param context - used to fetch auth resources and to migrate auth resources for override-feature.
  */
-export async function migrateAuthDependencyResource( context : $TSContext ) {
-    const authResourceName = await getAuthResourceARN(context);
+export async function migrateAuthDependencyResource(context: $TSContext) {
+  let authResourceName = undefined;
+  try {
+    authResourceName = await getAuthResourceARN(context);
+  } catch (error) {
+    //No auth resources to migrate - new project
+    return;
+  }
+  if (authResourceName) {
     try {
-      await context.amplify.invokePluginMethod(context,
-                                               AmplifyCategories.AUTH, undefined,
-                                               'migrateAuthResource',
-                                               [context, authResourceName ]);
+      await context.amplify.invokePluginMethod(context, AmplifyCategories.AUTH, undefined, 'migrateAuthResource', [
+        context,
+        authResourceName,
+      ]);
     } catch (error) {
       printer.error(error as string);
       throw error;
     }
+  }
 }
