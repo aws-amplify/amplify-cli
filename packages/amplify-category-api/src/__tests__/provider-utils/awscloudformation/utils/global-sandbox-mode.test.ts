4bc499139cadf9930b4ab19b9c4af2e26b861200
Danielle Adams <damsnmd@amazon.com>
2021-10-26 17:19:53 -0400

    feat(amplify-provider-awscloudformation): change sandbox mode syntax in schema

--- packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/utils/global-sandbox-mode.test.ts
+++ packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/utils/global-sandbox-mode.test.ts
@@ -1,21 +1,10 @@
 import { defineGlobalSandboxMode } from '../../../../provider-utils/awscloudformation/utils/global-sandbox-mode';
-import { $TSContext } from 'amplify-cli-core';
 
 describe('global sandbox mode GraphQL directive', () => {
-  it('returns AMPLIFY_DIRECTIVE type with code comment, directive, and env name', () => {
-    const envName = 'envone';
-    const ctx = <$TSContext>{
-      amplify: {
-        getEnvInfo() {
-          return { envName };
-        },
-      },
-    };
-
-    expect(defineGlobalSandboxMode(ctx))
-      .toBe(`# This allows public create, read, update, and delete access for a limited time to all models via API Key.
-# To configure PRODUCTION-READY authorization rules, review: https://docs.amplify.aws/cli/graphql-transformer/auth
-type AMPLIFY_GLOBAL @allow_public_data_access_with_api_key(in: \"${envName}\") # FOR TESTING ONLY!\n
+  it('returns input AMPLIFY with code comment', () => {
+    expect(defineGlobalSandboxMode()).toEqual(`# This "input" configures a global authorization rule to enable public access to
+# all models in this schema. Learn more about authorization rules here: https://docs.amplify.aws/cli/graphql-transformer/auth
+input AMPLIFY { global_auth_rule: AuthorizationRule = { allow: public } } # FOR TESTING ONLY!\n
 `);
   });
 });
