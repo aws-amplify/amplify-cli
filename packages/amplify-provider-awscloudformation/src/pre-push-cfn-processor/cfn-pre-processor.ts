b0a17a24f3ef9d1ced20a6df28272d29fd6cc32f
Ammar <56042290+ammarkarachi@users.noreply.github.com>
2021-10-27 13:02:01 -0700

    fix: e2e test for generating custom policies (#8580)
    * fix: logical and instead of logical or
    
    * test: increased test coverage

--- packages/amplify-provider-awscloudformation/src/pre-push-cfn-processor/cfn-pre-processor.ts
+++ packages/amplify-provider-awscloudformation/src/pre-push-cfn-processor/cfn-pre-processor.ts
@@ -26,7 +26,7 @@ export async function preProcessCFNTemplate(filePath: string): Promise<string> {
 
 //get data from custom polcies file and write custom policies to CFN template
 export async function writeCustomPoliciesToCFNTemplate(resourceName: string, service: string, cfnFile: string, category: string) {
-  if ((category !== 'api' && service !== 'ElasticContainer') || (category !== 'function' && service !== 'Lambda')) {
+  if (!(category === 'api' && service === 'ElasticContainer') && !(category === 'function' && service === 'Lambda')) {
     return;
   }
 
