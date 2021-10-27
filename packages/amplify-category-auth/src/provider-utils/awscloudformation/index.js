0c0828dcbd13095bed3435969925139a77374bce
Akshay <akshayupadhyay3@gmail.com>
2021-10-27 03:43:44 -0700

    fix: rebase leftovers

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/index.js
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/index.js
@@ -5,10 +5,8 @@ const { getAuthResourceName } = require('../../utils/getAuthResourceName');
 const { copyCfnTemplate, saveResourceParameters } = require('./utils/synthesize-resources');
 const { ENV_SPECIFIC_PARAMS, AmplifyAdmin, UserPool, IdentityPool, BothPools, privateKeys } = require('./constants');
 const { getAddAuthHandler, getUpdateAuthHandler } = require('./handlers/resource-handlers');
-const { supportedServices } = require('../supported-services');
-const { importResource, importedAuthEnvInit } = require('./import');
-const { AuthInputState } = require('./auth-inputs-manager/auth-input-state');
 const { getSupportedServices } = require('../supported-services');
+const { importResource, importedAuthEnvInit } = require('./import');
 
 function serviceQuestions(context, defaultValuesFilename, stringMapsFilename, serviceWalkthroughFilename, serviceMetadata) {
   const serviceWalkthroughSrc = `${__dirname}/service-walkthroughs/${serviceWalkthroughFilename}`;
@@ -26,7 +24,7 @@ async function addResource(context, service, skipNextSteps = false) {
 }
 
 async function updateResource(context, { service }) {
-  const serviceMetadata = supportedServices[service];
+  const serviceMetadata = getSupportedServices()[service];
   const { defaultValuesFilename, stringMapsFilename, serviceWalkthroughFilename } = serviceMetadata;
   return getUpdateAuthHandler(context)(
     await serviceQuestions(context, defaultValuesFilename, stringMapsFilename, serviceWalkthroughFilename, serviceMetadata),
@@ -34,14 +32,12 @@ async function updateResource(context, { service }) {
 }
 
 async function updateConfigOnEnvInit(context, category, service) {
-  const srvcMetaData = supportedServices.Cognito;
+  const srvcMetaData = getSupportedServices().Cognito;
   const { defaultValuesFilename, stringMapsFilename, serviceWalkthroughFilename, provider } = srvcMetaData;
 
   const providerPlugin = context.amplify.getPluginInstance(context, provider);
   // previously selected answers
-  const currentAuthName = await getAuthResourceName(context);
-  const cliState = new AuthInputState(currentAuthName);
-  const resourceParams = await cliState.loadResourceParameters(context, cliState.getCLIInputPayload());
+  const resourceParams = providerPlugin.loadResourceParameters(context, 'auth', service);
   // ask only env specific questions
   let currentEnvSpecificValues = context.amplify.loadEnvResourceParameters(context, category, service);
 
@@ -185,7 +181,7 @@ async function migrate(context) {
   if (!Object.keys(existingAuth).length > 0) {
     return;
   }
-  const { provider, cfnFilename, defaultValuesFilename } = supportedServices.Cognito;
+  const { provider, cfnFilename, defaultValuesFilename } = getSupportedServices().Cognito;
   const defaultValuesSrc = `${__dirname}/assets/${defaultValuesFilename}`;
 
   const { roles } = require(defaultValuesSrc);
@@ -435,7 +431,7 @@ async function openIdentityPoolConsole(context, region, identityPoolId) {
 }
 
 function getPermissionPolicies(context, service, resourceName, crudOptions) {
-  const { serviceWalkthroughFilename } = supportedServices[service];
+  const { serviceWalkthroughFilename } = getSupportedServices()[service];
   const serviceWalkthroughSrc = `${__dirname}/service-walkthroughs/${serviceWalkthroughFilename}`;
   const { getIAMPolicies } = require(serviceWalkthroughSrc);
 
