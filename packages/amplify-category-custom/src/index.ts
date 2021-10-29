0cfc343abc435c7dedfa9bea508431534f8723ad
Kaustav Ghosh <kaustav.ghosh19@gmail.com>
2021-10-29 10:00:31 -0700

    feat: define custom resources with CDK or CFN (#8590)
    * feat: define custom resources with CDK or CFN
    
    * chore: fix lint errors
    
    * chore: address API review and PR feedback
    
    * chore: fix unit test
    
    * fix: update skeleton CDK sample app based on API-Review
    
    * chore: fix build errors
    
    * chore: fix unit tests
    
    * chore: addressed PR review comments
    
    * chore: update uuid package version
    
    Co-authored-by: Ghosh <kaustavg@3c22fb229ff6.ant.amazon.com>

---
+++ packages/amplify-category-custom/src/index.ts
@@ -0,0 +1,29 @@
+import { $TSAny, $TSContext, IAmplifyResource } from 'amplify-cli-core';
+import { printer } from 'amplify-prompts';
+import * as path from 'path';
+import { buildCustomResources } from './utils/build-custom-resources';
+import { categoryName } from './utils/constants';
+export { addCDKResourceDependency } from './utils/dependency-management-utils';
+
+export async function executeAmplifyCommand(context: $TSContext) {
+  let commandPath = path.normalize(path.join(__dirname, 'commands'));
+
+  if (context.input.command === 'help') {
+    commandPath = path.join(commandPath, categoryName);
+  } else {
+    commandPath = path.join(commandPath, categoryName, context.input.command);
+  }
+
+  const commandModule = await import(commandPath);
+
+  await commandModule.run(context);
+}
+
+export async function handleAmplifyEvent(context: $TSContext, args: $TSAny) {
+  printer.info(`${categoryName} handleAmplifyEvent to be implemented`);
+  printer.info(`Received event args ${args}`);
+}
+
+export async function transformCategoryStack(context: $TSContext, resource: IAmplifyResource) {
+  await buildCustomResources(context, resource.resourceName);
+}
