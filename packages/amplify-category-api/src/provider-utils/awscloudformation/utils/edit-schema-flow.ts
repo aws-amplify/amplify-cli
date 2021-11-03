9822117a092ca03695f0a3f12e627836ec53c5bb
John Hockett <jhockett@users.noreply.github.com>
2021-11-03 14:53:43 -0700

    feat: extensibility for REST APIs (#8598)

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/edit-schema-flow.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/edit-schema-flow.ts
@@ -1,18 +1,14 @@
-import inquirer, { ConfirmQuestion } from 'inquirer';
-import path from 'path';
+import { $TSContext, pathManager } from 'amplify-cli-core';
+import { prompter } from 'amplify-prompts';
+import * as path from 'path';
 import { category } from '../../../category-constants';
 import { gqlSchemaFilename } from '../aws-constants';
 
-export const editSchemaFlow = async (context: any, apiName: string) => {
-  const prompt: ConfirmQuestion = {
-    type: 'confirm',
-    name: 'editNow',
-    message: 'Do you want to edit the schema now?',
-    default: true,
-  };
+export const editSchemaFlow = async (context: $TSContext, apiName: string) => {
+  if (!(await prompter.yesOrNo('Do you want to edit the schema now?', true))) {
+    return;
+  }
 
-  if (!(await inquirer.prompt(prompt)).editNow) return;
-
-  const schemaPath = path.join(context.amplify.pathManager.getBackendDirPath(), category, apiName, gqlSchemaFilename);
+  const schemaPath = path.join(pathManager.getResourceDirectoryPath(undefined, category, apiName), gqlSchemaFilename);
   await context.amplify.openEditor(context, schemaPath, false);
 };
