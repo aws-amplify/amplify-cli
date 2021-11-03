cd8fb5ce64176b5015fea8246ddea85bf77a43ef
SwaySway <7465495+SwaySway@users.noreply.github.com>
2021-11-03 10:44:34 -0700

    fix: vtl list utils & update model test scenarios

--- packages/amplify-appsync-simulator/src/utils/graphql-runner/index.ts
+++ packages/amplify-appsync-simulator/src/utils/graphql-runner/index.ts
@@ -1,9 +1,10 @@
-import { JWTToken } from '../auth-helpers/helpers';
+import { JWTToken, IAMToken } from '../auth-helpers/helpers';
 import { AmplifyAppSyncSimulatorAuthenticationType } from '../../type-definition';
 
 export type AppSyncGraphQLExecutionContext = {
-  readonly jwt?: JWTToken;
   readonly sourceIp?: string;
+  readonly jwt?: JWTToken;
+  readonly iamToken?: IAMToken;
   headers: Record<string, string | string[]>;
   appsyncErrors?: Error[];
   requestAuthorizationMode: AmplifyAppSyncSimulatorAuthenticationType;
