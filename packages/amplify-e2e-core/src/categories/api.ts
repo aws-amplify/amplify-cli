48f8cfdb83927a4cea7dcc7931a49ac85a9fb18a
John Corser <johnpc@umich.edu>
2021-11-02 20:10:19 -0400

    ci: misc updates to circleci config
    * test: fix e2e amplify dir
    
    * test: update api default testingWithLatestCodebase
    
    * ci: shorten test suite names
    
    * fix test names in split-e2e-tests script
    
    * skip api_5 on windows

--- packages/amplify-e2e-core/src/categories/api.ts
+++ packages/amplify-e2e-core/src/categories/api.ts
@@ -32,7 +32,7 @@ interface AddApiOptions {
 
 const defaultOptions: AddApiOptions = {
   apiName: '\r',
-  testingWithLatestCodebase: true,
+  testingWithLatestCodebase: false,
 };
 
 export function addApiWithoutSchema(cwd: string, opts: Partial<AddApiOptions & { apiKeyExpirationDays: number }> = {}) {
