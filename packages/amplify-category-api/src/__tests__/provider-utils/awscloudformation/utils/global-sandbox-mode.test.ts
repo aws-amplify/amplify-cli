4c6db6a238cf1663b9b320ee605201c070c422be
Danielle Adams <damsnmd@amazon.com>
2021-11-04 15:54:18 -0400

    feat(amplify-provider-awscloudformation): change global_auth_rule to globalAuthRule for global auth

--- packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/utils/global-sandbox-mode.test.ts
+++ packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/utils/global-sandbox-mode.test.ts
@@ -4,7 +4,7 @@ describe('global sandbox mode GraphQL directive', () => {
   it('returns input AMPLIFY with code comment', () => {
     expect(defineGlobalSandboxMode()).toEqual(`# This "input" configures a global authorization rule to enable public access to
 # all models in this schema. Learn more about authorization rules here: https://docs.amplify.aws/cli/graphql-transformer/auth
-input AMPLIFY { global_auth_rule: AuthRule = { allow: public } } # FOR TESTING ONLY!\n
+input AMPLIFY { globalAuthRule: AuthRule = { allow: public } } # FOR TESTING ONLY!\n
 `);
   });
 });
