4d4d52ff548341936b7ddbfb1fb70b41028842c9
Sachin Panemangalore <83682223+sachscode@users.noreply.github.com>
2021-11-06 16:22:39 -0700

    Ext overrides3 fix auth import s3 add (#8704)
    * (fix) add missing logic to validate auth requirements in storage
    
    * (fix) check for import auth
    
    * (fix) auth unit-tests couldnt find getImportedAuthProperties
    
    Co-authored-by: Sachin Panemangalore <sachinrp@amazon.com>

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/check-for-auth-migration.ts
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/check-for-auth-migration.ts
@@ -5,17 +5,21 @@ import { generateAuthStackTemplate } from './generate-auth-stack-template';
 import { migrateResourceToSupportOverride } from './migrate-override-resource';
 
 export const checkAuthResourceMigration = async (context: $TSContext, authName: string) => {
-  const cliState = new AuthInputState(authName);
-  if (!cliState.cliInputFileExists()) {
-    printer.debug('Cli-inputs.json doesnt exist');
-    // put spinner here
-    const isMigrate = await prompter.yesOrNo(`Do you want to migrate this ${authName} to support overrides?`, true);
-    if (isMigrate) {
-      // generate cli-inputs for migration from parameters.json
-      await migrateResourceToSupportOverride(authName);
-      // fetch cli Inputs again
-      const cliInputs = cliState.getCLIInputPayload();
-      await generateAuthStackTemplate(context, cliInputs.cognitoConfig.resourceName);
+  // check if its imported auth
+  const { imported} = context.amplify.getImportedAuthProperties(context);
+  if(!imported){
+    const cliState = new AuthInputState(authName);
+    if (!cliState.cliInputFileExists()) {
+      printer.debug('Cli-inputs.json doesnt exist');
+      // put spinner here
+      const isMigrate = await prompter.yesOrNo(`Do you want to migrate this ${authName} to support overrides?`, true);
+      if (isMigrate) {
+        // generate cli-inputs for migration from parameters.json
+        await migrateResourceToSupportOverride(authName);
+        // fetch cli Inputs again
+        const cliInputs = cliState.getCLIInputPayload();
+        await generateAuthStackTemplate(context, cliInputs.cognitoConfig.resourceName);
+      }
     }
   }
 };
