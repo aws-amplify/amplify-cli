be984d3e424df978ad6747280ef25b057c16ffb1
akshbhu <39866697+akshbhu@users.noreply.github.com>
2021-11-07 09:40:38 -0800

    Consolidate types headless (#8698)
    * fix: consolidate types and auto migrate
    
    * fix: changed package name to extensiblity
    
    * fix: address comments

--- packages/amplify-provider-awscloudformation/src/root-stack-builder/root-stack-builder.ts
+++ packages/amplify-provider-awscloudformation/src/root-stack-builder/root-stack-builder.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as iam from '@aws-cdk/aws-iam';
-import { AmplifyRootStackTemplate } from './types';
+import { AmplifyRootStackTemplate } from '@aws-amplify/cli-extensibility-helper';
 import { IStackSynthesizer, ISynthesisSession } from '@aws-cdk/core';
 
 const CFN_TEMPLATE_FORMAT_VERSION = '2010-09-09';
