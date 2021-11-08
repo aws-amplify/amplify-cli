f2cc9467886ee1e80f11f7534e9f87375e80e039
akshbhu <39866697+akshbhu@users.noreply.github.com>
2021-11-07 16:32:34 -0800

    Root stack fix (#8717)
    * fix: disable root stack update check
    
    * fix: root stack hash and helper package versions

--- packages/amplify-cli/src/extensions/amplify-helpers/root-stack-status.ts
+++ packages/amplify-cli/src/extensions/amplify-helpers/root-stack-status.ts
@@ -2,6 +2,7 @@ import { pathManager } from 'amplify-cli-core';
 import { rootStackFileName } from 'amplify-provider-awscloudformation';
 import { hashElement, HashElementOptions } from 'folder-hash';
 import * as fs from 'fs-extra';
+import * as path from 'path';
 
 export function getHashForRootStack(dirPath, files?: string[]) {
   const options: HashElementOptions = {
@@ -19,10 +20,10 @@ export async function isRootStackModifiedSinceLastPush(hashFunction): Promise<bo
     const localBackendDir = pathManager.getRootStackBuildDirPath(projectPath!);
     const cloudBackendDir = pathManager.getCurrentCloudRootStackDirPath(projectPath!);
     if (fs.existsSync(localBackendDir)) {
-      const localDirHash = await hashFunction(localBackendDir, ['*.json']);
+      const localCfnBuffer = fs.readFileSync(path.join(localBackendDir, rootStackFileName));
       if (fs.existsSync(cloudBackendDir)) {
-        const cloudDirHash = await hashFunction(cloudBackendDir, ['*.json']);
-        return localDirHash !== cloudDirHash;
+        const cloudCfnBuffer = fs.readFileSync(path.join(cloudBackendDir, rootStackFileName));
+        return !localCfnBuffer.equals(cloudCfnBuffer);
       } else {
         return true;
       }
