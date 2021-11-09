ba24962a8b403bb5c5abd74a40878c1cfc323961
AmmarKarachi <ammkara@amazon.com>
2021-11-09 15:06:27 -0800

    Revert "Remove pipelineFunction references in favor of resolvers (#8729)"
    This reverts commit a0295aa940dd735b29f54be989f2b11dac47aacc.

--- packages/amplify-provider-awscloudformation/src/graphql-transformer/utils.ts
+++ packages/amplify-provider-awscloudformation/src/graphql-transformer/utils.ts
@@ -9,7 +9,6 @@ import { Diff, diff as getDiffs } from 'deep-diff';
 import { ResourceConstants } from 'graphql-transformer-common';
 import { pullAllBy, find } from 'lodash';
 import { isAmplifyAdminApp } from '../utils/admin-helpers';
-import { printer } from 'amplify-prompts';
 
 const ROOT_STACK_FILE_NAME = 'cloudformation-template.json';
 const PARAMETERS_FILE_NAME = 'parameters.json';
@@ -117,48 +116,23 @@ export function readFromPath(directory: string): any {
 export function mergeUserConfigWithTransformOutput(
   userConfig: TransformerProjectConfig,
   transformOutput: DeploymentResources,
-  opts?: any,
 ): DeploymentResources {
   const userFunctions = userConfig.functions || {};
   const userResolvers = userConfig.resolvers || {};
   const userPipelineFunctions = userConfig.pipelineFunctions || {};
   const functions = transformOutput.functions;
-  const resolvers = transformOutput.resolvers;
   const pipelineFunctions = transformOutput.pipelineFunctions;
 
-  if (!opts?.disableFunctionOverrides) {
-    for (const userFunction of Object.keys(userFunctions)) {
-      functions[userFunction] = userFunctions[userFunction];
-    }
-  }
-
-  if (!opts?.disablePipelineFunctionOverrides) {
-    const pipelineFunctionKeys = Object.keys(userPipelineFunctions);
-
-    if (pipelineFunctionKeys.length > 0) {
-      printer.warn(
-        ' You are using the "pipelineFunctions" directory for overridden and custom resolvers. ' +
-          'Please use the "resolvers" directory as "pipelineFunctions" will be deprecated.\n',
-      );
-    }
-
-    for (const userPipelineFunction of pipelineFunctionKeys) resolvers[userPipelineFunction] = userPipelineFunctions[userPipelineFunction];
-  }
-
-  if (!opts?.disableResolverOverrides) {
-    for (const userResolver of Object.keys(userResolvers)) {
-      if (userResolver !== 'README.md') {
-        resolvers[userResolver] = userResolvers[userResolver].toString();
-      }
-    }
-  }
+  for (const userFunction of Object.keys(userFunctions)) functions[userFunction] = userFunctions[userFunction];
+  for (const userPipelineFunction of Object.keys(userPipelineFunctions))
+    pipelineFunctions[userPipelineFunction] = userPipelineFunctions[userPipelineFunction];
+  for (const userResolver of Object.keys(userResolvers)) pipelineFunctions[userResolver] = userResolvers[userResolver];
 
   const stacks = overrideUserDefinedStacks(userConfig, transformOutput);
 
   return {
     ...transformOutput,
     functions,
-    resolvers,
     pipelineFunctions,
     stacks,
   };
@@ -320,6 +294,10 @@ function initStacksAndResolversDirectories(directory: string) {
   const resolverRootPath = resolverDirectoryPath(directory);
   if (!fs.existsSync(resolverRootPath)) {
     fs.mkdirSync(resolverRootPath);
+  }
+  const pipelineFunctionRootPath = pipelineFunctionDirectoryPath(directory);
+  if (!fs.existsSync(pipelineFunctionRootPath)) {
+    fs.mkdirSync(pipelineFunctionRootPath);
   }
   const stackRootPath = stacksDirectoryPath(directory);
   if (!fs.existsSync(stackRootPath)) {
