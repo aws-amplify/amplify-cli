e8c35a19ab3a479bae1430508fb3a89bc3996737
Akshay <akshayupadhyay3@gmail.com>
2021-11-11 11:07:58 -0800

    fix: auth update tests

--- packages/amplify-migration-tests/src/__tests__/update_tests/auth_migration_update.test.ts
+++ packages/amplify-migration-tests/src/__tests__/update_tests/auth_migration_update.test.ts
@@ -1,3 +1,4 @@
+import { $TSAny } from 'amplify-cli-core';
 import {
   addAuthWithCustomTrigger,
   addAuthWithDefault,
@@ -24,10 +25,10 @@ describe('amplify auth migration', () => {
 
   afterEach(async () => {
     const metaFilePath = join(projRoot, 'amplify', '#current-cloud-backend', 'amplify-meta.json');
-    if (fs.existsSync(metaFilePath)) {
-      await deleteProject(projRoot);
-    }
-    deleteProjectDir(projRoot);
+    // if (fs.existsSync(metaFilePath)) {
+    //   await deleteProject(projRoot);
+    // }
+    // deleteProjectDir(projRoot);
   });
   it('...should init a project and add auth with a custom trigger, and then update to remove the custom js while leaving the other js', async () => {
     // init, add and push auth with installed cli
@@ -53,7 +54,14 @@ describe('amplify auth migration', () => {
     expect(lambdaFunction.Configuration.Environment.Variables.MODULES).toEqual('email-filter-denylist,custom');
 
     // update and push with codebase
-    await updateAuthWithoutCustomTrigger(projRoot, { testingWithLatestCodebase: true });
+    const authResourceName = Object.keys(meta.auth).filter(resourceName => meta.auth[resourceName].service === 'Cognito')[0];
+    // update and push with codebase
+    const overridesObj: $TSAny = {
+      resourceName: authResourceName,
+      category: 'auth',
+      service: 'cognito',
+    };
+    await updateAuthWithoutCustomTrigger(projRoot, { testingWithLatestCodebase: true, overrides: overridesObj });
     await amplifyPushAuth(projRoot, true);
     const updatedFunction = await getLambdaFunction(functionName, meta.providers.awscloudformation.Region);
     const updatedDirContents = fs.readdirSync(`${projRoot}/amplify/backend/function/${Object.keys(meta.auth)[0]}PreSignup/src`);
@@ -67,8 +75,15 @@ describe('amplify auth migration', () => {
     await initJSProjectWithProfile(projRoot, { name: 'authMigration' });
     await addAuthWithDefault(projRoot, {});
     await amplifyPushAuth(projRoot);
+    const meta = getProjectMeta(projRoot);
+    const authResourceName = Object.keys(meta.auth).filter(resourceName => meta.auth[resourceName].service === 'Cognito')[0];
     // update and push with codebase
-    await updateAuthWithoutTrigger(projRoot, { testingWithLatestCodebase: true });
+    const overridesObj: $TSAny = {
+      resourceName: authResourceName,
+      category: 'auth',
+      service: 'cognito',
+    };
+    await updateAuthWithoutTrigger(projRoot, { testingWithLatestCodebase: true, overrides: overridesObj });
     await amplifyPushAuth(projRoot, true);
   });
 });
