dbb41c488a89dd33c8b0b88474d063217adf7595
Danielle Adams <6271256+danielleadams@users.noreply.github.com>
2021-11-11 14:51:26 -0500

    fix(graphql-transformer-core): remove the allow_public_global directive references (#8751)

--- packages/amplify-graphql-transformer-core/src/transformation/transform.ts
+++ packages/amplify-graphql-transformer-core/src/transformation/transform.ts
@@ -142,7 +142,6 @@ export class GraphQLTransform {
         aws_oidc: true,
         aws_lambda: true,
         aws_cognito_user_pools: true,
-        allow_public_data_access_with_api_key: true,
         deprecated: true,
       },
     );
