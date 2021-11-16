fdd9f422c23beedb76ec07c285417ae9c69c977a
Marc VandenBerg <marc.vandenberg1@gmail.com>
2021-11-16 14:28:54 -0500

    Test: Add test case for custom timestamps v1->v2 transform

--- packages/amplify-graphql-migration-tests/src/test-case-registry.ts
+++ packages/amplify-graphql-migration-tests/src/test-case-registry.ts
@@ -10,6 +10,7 @@ export const getTestCaseRegistry = (): TestEntry[] => [
   ['compound sort key', compoundSortKey],
   ['custom primary key', customPrimaryKey],
   ['connection on custom primary key', connectionOnCustomPrimaryKey],
+  ['support renaming timestamps', renameTimestampFields],
   /*
   [
     'add additional tests with a descriptive name',
@@ -130,5 +131,13 @@ const namedHasManyBelongsToConnection = /* GraphQL */ `
     id: ID!
     content: String!
     post: PostConnection @connection(name: "PostComments")
+  }
+`;
+
+const renameTimestampFields = /* GraphQL */ `
+  type Post @model(timestamps: { createdAt: "made", updatedAt: "updated"}) {
+    id: ID!
+    title: String!
+    contents: String
   }
 `;
