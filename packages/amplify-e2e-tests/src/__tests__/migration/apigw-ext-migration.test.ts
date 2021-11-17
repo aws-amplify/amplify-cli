9b2e2f1d3a066b2d138100717d4522dbdde50e5a
Edward Foyle <foyleef@amazon.com>
2021-11-17 11:14:50 -0800

    test: admin queries extensibility migration test

--- packages/amplify-e2e-tests/src/__tests__/migration/apigw-ext-migration.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/migration/apigw-ext-migration.test.ts
@@ -6,9 +6,12 @@ import {
   deleteProject,
   deleteProjectDir,
   getCLIInputs,
-  getCLIPath,
   initJSProjectWithProfile,
   updateRestApi,
+  addAuthWithDefault,
+  updateAuthAddAdminQueries,
+  updateAuthAdminQueriesWithExtMigration,
+  getProjectMeta,
 } from 'amplify-e2e-core';
 import { v4 as uuid } from 'uuid';
 
@@ -31,10 +34,30 @@ describe('API Gateway CDK migration', () => {
   it('migrates on api update', async () => {
     await addRestApi(projRoot, { existingLambda: false, apiName: 'restapimig' });
     await amplifyPushAuth(projRoot);
-    await cliVersionController.resetCliVersion();
+    cliVersionController.resetCliVersion();
     await updateRestApi(projRoot, { updateOperation: 'Add another path', newPath: '/foo', expectMigration: true });
     await amplifyPushAuth(projRoot);
     const cliInputs = getCLIInputs(projRoot, 'api', 'restapimig');
     expect(cliInputs).toBeDefined();
+  });
+
+  it('migrates auth with admin queries', async () => {
+    await addAuthWithDefault(projRoot);
+    await updateAuthAddAdminQueries(projRoot);
+    await amplifyPushAuth(projRoot);
+
+    cliVersionController.resetCliVersion();
+
+    await updateAuthAdminQueriesWithExtMigration(projRoot);
+    await amplifyPushAuth(projRoot);
+
+    const meta = getProjectMeta(projRoot);
+    const authName = Object.keys(meta.auth)[0];
+
+    const authCliInputs = getCLIInputs(projRoot, 'auth', authName);
+    expect(authCliInputs).toBeDefined();
+
+    const adminQueriesCliInputs = getCLIInputs(projRoot, 'api', 'AdminQueries');
+    expect(adminQueriesCliInputs).toBeDefined();
   });
 });
