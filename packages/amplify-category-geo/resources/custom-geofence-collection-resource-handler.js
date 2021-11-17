251ce3e80324b365b4ac98a1c0e48da2453de078
phani-srikar <phanisrikar93ume@gmail.com>
2021-11-17 15:10:36 -0800

    fix(amplify-category-geo): update lambda to delete separate send function

--- packages/amplify-category-geo/resources/custom-geofence-collection-resource-handler.js
+++ packages/amplify-category-geo/resources/custom-geofence-collection-resource-handler.js
@@ -1,6 +1,6 @@
 const response = require('cfn-response');
 const aws = require('aws-sdk');
-exports.handler = async (event, context) => {
+exports.handler = async function (event, context) {
   try {
     console.log('REQUEST RECEIVED:' + JSON.stringify(event));
     if (event.RequestType == 'Create') {
@@ -15,9 +15,9 @@ exports.handler = async (event, context) => {
       const res = await locationClient.createGeofenceCollection(params).promise();
       console.log('create resource response data' + JSON.stringify(res));
       if (res.CollectionName && res.CollectionArn) {
-        await send(event, context, response.SUCCESS, res, params.CollectionName);
+        await response.send(event, context, response.SUCCESS, res, params.CollectionName);
       } else {
-        await send(event, context, response.FAILED, res, params.CollectionName);
+        await response.send(event, context, response.FAILED, res, params.CollectionName);
       }
     }
     if (event.RequestType == 'Update') {
@@ -32,9 +32,9 @@ exports.handler = async (event, context) => {
       const res = await locationClient.updateGeofenceCollection(params).promise();
       console.log('update resource response data' + JSON.stringify(res));
       if (res.CollectionName) {
-        await send(event, context, response.SUCCESS, res, params.CollectionName);
+        await response.send(event, context, response.SUCCESS, res, params.CollectionName);
       } else {
-        await send(event, context, response.FAILED, res, params.CollectionName);
+        await response.send(event, context, response.FAILED, res, params.CollectionName);
       }
     }
     if (event.RequestType == 'Delete') {
@@ -44,18 +44,12 @@ exports.handler = async (event, context) => {
       const locationClient = new aws.Location({ apiVersion: '2020-11-19', region: event.ResourceProperties.region });
       const res = await locationClient.deleteGeofenceCollection(params).promise();
       console.log('delete resource response data' + JSON.stringify(res));
-      await send(event, context, response.SUCCESS, res, params.CollectionName);
+      await response.send(event, context, response.SUCCESS, res, params.CollectionName);
     }
   } catch (err) {
     console.log(err.stack);
     const res = { Error: err };
-    await send(event, context, response.FAILED, res, event.ResourceProperties.collectionName);
+    await response.send(event, context, response.FAILED, res, event.ResourceProperties.collectionName);
     throw err;
   }
 };
-
-function send(event, context, status, data, physicalResourceId) {
-  return new Promise(() => {
-    response.send(event, context, status, data, physicalResourceId);
-  });
-}
