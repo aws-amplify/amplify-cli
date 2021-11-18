e99adc05522efb8c5bb123dabc7088c71b22a71e
Sachin Panemangalore <83682223+sachscode@users.noreply.github.com>
2021-11-18 12:30:20 -0600

    fix: auth cloudformation generation error leading to push failure (#8941)
    Co-authored-by: Sachin Panemangalore <sachinrp@amazon.com>

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/auth-stack-builder/auth-stack-transform.ts
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/auth-stack-builder/auth-stack-transform.ts
@@ -213,7 +213,7 @@ export class AmplifyAuthTransform extends AmplifyCategoryTransform {
       cognitoStackFileName,
     );
     // write CFN template
-    writeCFNTemplate(template, cognitostackFilePath, {
+    await writeCFNTemplate(template, cognitostackFilePath, {
       templateFormat: CFNTemplateFormat.JSON,
     });
     // write parameters.json
