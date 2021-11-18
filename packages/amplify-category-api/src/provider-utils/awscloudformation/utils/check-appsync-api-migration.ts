4cd2819d8a4207b1cb545fd38d4ad4d870b4a442
Akshay <akshayupadhyay3@gmail.com>
2021-11-17 21:01:02 -0800

    fix: scoped package to public and minor fixes

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/check-appsync-api-migration.ts
+++ packages/amplify-category-api/src/provider-utils/awscloudformation/utils/check-appsync-api-migration.ts
@@ -5,29 +5,15 @@ import { migrateResourceToSupportOverride } from './migrate-api-override-resourc
 
 export const checkAppsyncApiResourceMigration = async (context: $TSContext, apiName: string, isUpdate: boolean): Promise<boolean> => {
   const cliState = new AppsyncApiInputState(apiName);
-  /**
-   * Below steps checks for TransformerV1 app and updates the FF { useexperimentalpipelinedtransformer , transformerversion}
-   */
-  const transformerVersion = await context.amplify.invokePluginMethod(context, 'awscloudformation', undefined, 'getTransformerVersion', [
-    context,
-  ]);
-  if (transformerVersion === 2) {
-    if (!cliState.cliInputFileExists()) {
-      printer.debug('cli-inputs.json doesnt exist');
-      const headlessMigrate = context.input.options?.yes || context.input.options?.forcePush || context.input.options?.headless;
-      if (
-        headlessMigrate ||
-        (await prompter.yesOrNo(getMigrateResourceMessageForOverride(AmplifyCategories.API, apiName, isUpdate), true))
-      ) {
-        // generate cli-inputs for migration from parameters.json
-        await migrateResourceToSupportOverride(apiName);
-        return true;
-      }
-      return false;
-    } else {
+  if (!cliState.cliInputFileExists()) {
+    printer.debug('cli-inputs.json doesnt exist');
+    const headlessMigrate = context.input.options?.yes || context.input.options?.forcePush || context.input.options?.headless;
+    if (headlessMigrate || (await prompter.yesOrNo(getMigrateResourceMessageForOverride(AmplifyCategories.API, apiName, isUpdate), true))) {
+      // generate cli-inputs for migration from parameters.json
+      await migrateResourceToSupportOverride(apiName);
       return true;
     }
-  } else {
     return false;
   }
+  return true;
 };
