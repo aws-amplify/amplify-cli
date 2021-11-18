51c9c2db3e1ee374a685d89e90505fdcaccf518a
Akshay <akshayupadhyay3@gmail.com>
2021-11-18 11:19:55 -0800

    fix: conflict resolution tests

--- packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/cfn-api-artifact-handler.test.ts
+++ packages/amplify-category-api/src/__tests__/provider-utils/awscloudformation/cfn-api-artifact-handler.test.ts
@@ -124,7 +124,7 @@ describe('create artifacts', () => {
 
   it('creates the transform.conf.json file', async () => {
     await cfnApiArtifactHandler.createArtifacts(addRequestStub);
-    expect(writeTransformerConfiguration_mock.mock.calls.length).toBe(1);
+    expect(writeTransformerConfiguration_mock.mock.calls.length).toBe(2);
     expect(writeTransformerConfiguration_mock.mock.calls[0]).toMatchSnapshot();
   });
 
