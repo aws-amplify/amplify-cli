5691af00164268e5160a809404082286dc095b18
Edward Foyle <foyleef@amazon.com>
2021-11-18 14:06:39 -0800

    fix: passthrough group auth rule config (#8928)

--- packages/amplify-graphql-transformer-migrator/src/__tests__/migration/auth-tests.ts
+++ packages/amplify-graphql-transformer-migrator/src/__tests__/migration/auth-tests.ts
@@ -26,6 +26,22 @@ describe('Schema migration tests for @auth', () => {
       migrateAndValidate(schema, API_KEY);
     });
 
+    it('migrates default api_key auth correctly', () => {
+      const schema = `
+        type Todo @model {
+          id: ID!
+          name: String!
+          description: String
+      }
+
+      type Comment @model @auth(rules: [{ allow: private }]) {
+          id: ID!
+          content: String!
+      }`;
+
+      migrateAndValidate(schema, API_KEY);
+    });
+
     it('migrates @auth to public @auth with api key', () => {
       const schema = `
         type Todo @model @auth(rules: [{ allow: public }]) {
@@ -72,6 +88,33 @@ describe('Schema migration tests for @auth', () => {
         }`;
 
       migrateAndValidate(schema, IAM);
+    });
+  });
+
+  describe('group auth', () => {
+    it('retains dynamic groups in auth rules', () => {
+      const schema = /* GraphQL */ `
+        type Todo @model @auth(rules: [{ allow: groups, groups: ["Admins"] }]) {
+          id: ID!
+          rating: Int
+          title: String
+        }
+      `;
+
+      migrateAndValidate(schema);
+    });
+
+    it('retains groupClaims in auth rules', () => {
+      const schema = /* GraphQL */ `
+        type Todo
+          @model
+          @auth(rules: [{ allow: groups, provider: oidc, groups: ["Admins"], groupClaim: "https://myapp.com/claims/groups" }]) {
+          id: ID!
+          title: String!
+        }
+      `;
+
+      migrateAndValidate(schema);
     });
   });
 });
