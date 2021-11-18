7c58671bba9ce81e6863b49ff6045533266293c4
Colin Ihrig <cjihrig@gmail.com>
2021-11-18 17:02:42 -0500

    test: add gql migrator @connection unit tests (#8902)
    Co-authored-by: Colin Ihrig <colihrig@amazon.com>

--- packages/amplify-graphql-transformer-migrator/src/migrators/connection/index.ts
+++ packages/amplify-graphql-transformer-migrator/src/migrators/connection/index.ts
@@ -1,27 +1,27 @@
 import { isListType } from 'graphql-transformer-common';
 
-const validConnectionDirectiveNames = new Set(["hasOne", "hasMany", "connection"]);
+const validConnectionDirectiveNames = new Set(['hasOne', 'hasMany', 'connection']);
 
 export function getFieldsWithConnection(fields: any) {
-  return fields.filter((field: any) => field.directives.find((d: any) => d.name.value === "connection"));
+  return fields.filter((field: any) => field.directives.find((d: any) => d.name.value === 'connection'));
 }
 
 export function getConnectionFieldsArg(connection: any) {
-  return connection.arguments.find((a: any) => a?.name?.value === "fields")?.value?.values?.map((v: any) => v.value);
+  return connection.arguments.find((a: any) => a?.name?.value === 'fields')?.value?.values?.map((v: any) => v.value);
 }
 
 export function isFieldIndex(field: any) {
-  return field.directives.some((dir: any) => dir.name.value === "index");
+  return field.directives.some((dir: any) => dir.name.value === 'index');
 }
 
 export function getConnectionDirective(field: any) {
-  return field.directives.find((d: any) => d.name.value === "connection" || d.name.value === 'hasMany' || d.name.value === 'hasOne' || d.name.value === 'belongsTo');
+  return field.directives.find(
+    (d: any) => d.name.value === 'connection' || d.name.value === 'hasMany' || d.name.value === 'hasOne' || d.name.value === 'belongsTo',
+  );
 }
 
 function getRelatedType(output: any, relatedTypeName: any) {
-  const relatedType = output.definitions.find(
-    (d: any) => d.kind === "ObjectTypeDefinition" && d.name.value === relatedTypeName,
-  );
+  const relatedType = output.definitions.find((d: any) => d.kind === 'ObjectTypeDefinition' && d.name.value === relatedTypeName);
 
   return relatedType;
 }
@@ -44,8 +44,12 @@ export function migrateConnection(node: any, ast: any) {
     const connectionDirective = getConnectionDirective(connectionField);
     let typeIsList = isListType(connectionField.type);
     if (typeIsList) {
-      connectionDirective.name.value = "hasMany";
-      connectionDirective.arguments.find((a: any) => a.name.value === "keyName").name.value = "indexName";
+      connectionDirective.name.value = 'hasMany';
+      const keyNameArg = connectionDirective.arguments.find((a: any) => a.name.value === 'keyName');
+
+      if (keyNameArg) {
+        keyNameArg.name.value = 'indexName';
+      }
     } else {
       const relatedType = getRelatedType(ast, getFieldType(connectionField));
       const biDirectionalRelation = relatedType.fields.find((relatedField: any) => {
@@ -64,10 +68,10 @@ export function migrateConnection(node: any, ast: any) {
       });
 
       if (biDirectionalRelation && isListType(biDirectionalRelation.type)) {
-        connectionDirective.name.value = "belongsTo";
+        connectionDirective.name.value = 'belongsTo';
       } else {
-        connectionDirective.name.value = "hasOne";
+        connectionDirective.name.value = 'hasOne';
       }
     }
-  })
+  });
 }
