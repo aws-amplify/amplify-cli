c902238817d8430592d0b124278ad726fbf4d1f3
Sachin Panemangalore <sachinrp@amazon.com>
2021-11-19 11:29:12 -0800

    fix(amplify-category-auth): auth cloudformation corruption fixed for outputs

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/auth-stack-builder/user-pool-group-stack-transform.ts
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/auth-stack-builder/user-pool-group-stack-transform.ts
@@ -222,10 +222,11 @@ export class AmplifyUserPoolGroupTransform extends AmplifyCategoryTransform {
   public synthesizeTemplates = async (): Promise<Template> => {
     this._app.synth();
     const templates = this._synthesizer.collectStacks();
-    const cfnTemplate = templates.get('AmplifyUserPoolGroupStack')!;
-    const cfnTemplateOutputs = templates.get('AmplifyUserPoolGroupStackOutputs')!;
-    Object.assign(cfnTemplate, cfnTemplateOutputs);
-    return cfnTemplate;
+    const cfnUserPoolGroupStack: Template = templates.get('AmplifyUserPoolGroupStack')!;
+    const templatesOutput = this._synthesizerOutputs.collectStacks();
+    const cfnUserPoolGroupOutputs: Template = templatesOutput.get('AmplifyUserPoolGroupStackOutputs')!;
+    cfnUserPoolGroupStack.Outputs = cfnUserPoolGroupOutputs.Outputs;
+    return cfnUserPoolGroupStack;
   };
 
   public saveBuildFiles = async (context: $TSContext, template: Template): Promise<void> => {
