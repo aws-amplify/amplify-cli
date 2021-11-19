3771237b36900a6fdadf1468627f7d1c204238d7
Kaustav Ghosh <kaustavg@amazon.com>
2021-11-18 18:23:04 -0800

    Revert "fix(graphql-transformer-migrator): disables field level hoisting of auth (#8946)"
    This reverts commit a21118ae7f45dd4938b6731d1e388f2c6107e7d7.

--- packages/amplify-graphql-transformer-migrator/src/migrators/auth/index.ts
+++ packages/amplify-graphql-transformer-migrator/src/migrators/auth/index.ts
@@ -1,5 +1,6 @@
 import { isModelType } from '../model';
 import { migrateDefaultAuthMode } from './defaultAuth';
+import { migrateFieldAuth } from './fieldAuth';
 import { migrateOwnerAuth } from './ownerAuth';
 import { createArgumentNode, createDirectiveNode, createListValueNode } from '../generators';
 
@@ -40,6 +41,8 @@ export function migrateAuth(node: any, defaultAuthMode: any) {
   if (!isModelType(node)) {
     return;
   }
+
+  migrateFieldAuth(node);
 
   if (hasAuthDirectives(node)) {
     migrateOwnerAuth(node, defaultAuthMode);
