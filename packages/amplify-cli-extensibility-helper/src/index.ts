d5262988e59ddb8d004e239358dd533fc05e9cc3
John Hockett <jhockett@users.noreply.github.com>
2021-11-20 03:21:47 -0800

    fix: rest iam policy roles array, bump ext helper types version (#8983)

--- packages/amplify-cli-extensibility-helper/src/index.ts
+++ packages/amplify-cli-extensibility-helper/src/index.ts
@@ -1,7 +1,7 @@
 import { addCDKResourceDependency, AmplifyResourceProps } from '@aws-amplify/amplify-category-custom';
 import { getProjectInfo } from './helpers/project-info';
 export { AmplifyApiGraphQlResourceStackTemplate } from './types/api/amplify-api-resource-stack-types';
-export { AmplifyApiRestResourceStackTemplate } from './types/api/types';
+export { AmplifyApiRestResourceStackTemplate, ApigwPathPolicy } from './types/api/types';
 export { AmplifyAuthCognitoStackTemplate, AmplifyUserPoolGroupStackTemplate } from './types/auth/types';
 export { AmplifyRootStackTemplate } from './types/project/types';
 export { AmplifyCDKL1, AmplifyDDBResourceTemplate, AmplifyS3ResourceTemplate } from './types/storage/types';
