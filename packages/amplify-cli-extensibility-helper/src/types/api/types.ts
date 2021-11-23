e59e9890c367ce505eadde71a4f4da3cde748c92
jhockett <johocke@amazon.com>
2021-11-22 22:23:00 -0800

    chore: export AmplifyCDKL1 type for REST overrides

--- packages/amplify-cli-extensibility-helper/src/types/api/types.ts
+++ packages/amplify-cli-extensibility-helper/src/types/api/types.ts
@@ -2,7 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as apigwCdk from '@aws-cdk/aws-apigateway';
 import * as iamCdk from '@aws-cdk/aws-iam';
 
-type AmplifyCDKL1 = {
+export type AmplifyCDKL1 = {
   addCfnCondition(props: cdk.CfnConditionProps, logicalId: string): void;
   addCfnMapping(props: cdk.CfnMappingProps, logicalId: string): void;
   addCfnOutput(props: cdk.CfnOutputProps, logicalId: string): void;
