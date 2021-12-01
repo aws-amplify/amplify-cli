dd769dd894d328773c9b66e9798fe1ff65e2c7c6
Zeyu Li <lizeyu@amazon.com>
2021-12-01 15:48:34 -0800

    feat(geo): add bulk geofence upload (#8822)
    * feat(geo): add bulk geofence upload
    
    * add validation for linear ring
    
    * add aws sdk call for bulk put geofence
    
    * add clockwise check
    
    * add unit test for validation
    
    * change command name to populate
    
    * add unit tests for validation
    
    * add resource and region check
    
    * fix lgtm alert
    
    * add comments for code
    
    * fix successCount
    
    * address CRs
    
    * address CRs
    
    * rm duplicate dependencies
    
    * fix lgtm
    
    * add line seperator

---
+++ packages/amplify-category-geo/src/service-utils/populateParams.ts
@@ -0,0 +1,40 @@
+export type FeatureCollection = {
+  type: "FeatureCollection";
+  features: Feature[];
+}
+
+export type Feature = {
+  type: "Feature";
+  properties: Record<string, any>;
+  geometry: Geometry;
+  id: string;
+}
+
+export type Geometry = {
+  type: "Polygon";
+  coordinates: Array<Array<Array<number>>>;
+}
+
+export type GeofenceCollectionParams = {
+  CollectionName: string;
+  Entries: GeofenceParams[];
+}
+
+export type GeofenceParams = {
+  GeofenceId: string;
+  Geometry: {
+    Polygon: Array<Array<Array<number>>>
+  }
+}
+
+export type PopulateParams = {
+  collectionToPopulate: string;
+  uniqueIdentifier: string;
+  identifierOption: IdentifierOption;
+  geoJSONObj: FeatureCollection;
+}
+
+export enum IdentifierOption {
+  RootLevelID = "RootLevelID",
+  CustomProperty = "CustomProperty"
+}
