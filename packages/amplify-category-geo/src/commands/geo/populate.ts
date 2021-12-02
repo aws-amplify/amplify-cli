54b6bb876f5e5a58c2a7a82d8eccf78a626441da
Zeyu Li <lizeyu@amazon.com>
2021-12-02 10:54:16 -0800

    feat(geo): add bulk geofence upload (#8822)
    * fix(amplify-category-geo): correct spell error
    
    * feat(geo): add bulk geofence upload (#8822)
    
    * feat(geo): add bulk geofence upload
    
    * add validation for linear ring
    
    * add aws sdk call for bulk put geofence
    
    * add clockwise check
    
    * add unit test for validation
    
    * change command name to populate
    
    * add unit tests for validation
    
    * add resource and region check
    
    * fix lgtm alert
    
    * add comments for code
    
    * fix successCount
    
    * address CRs
    
    * address CRs
    
    * rm duplicate dependencies
    
    * fix lgtm
    
    * add line seperator
    
    Co-authored-by: phani-srikar <phanisrikar93ume@gmail.com>

---
+++ packages/amplify-category-geo/src/commands/geo/populate.ts
@@ -0,0 +1,22 @@
+import { $TSAny, $TSContext } from 'amplify-cli-core';
+import { printer } from 'amplify-prompts';
+import { populateResource } from '../../provider-controllers/populate';
+
+export const name = 'populate';
+
+export const run = async (context: $TSContext) => {
+  try {
+    return await populateResource(context);
+  } catch (error: $TSAny) {
+    if (error.message) {
+      printer.error(error.message);
+    }
+    printer.blankLine();
+    if (error.stack) {
+      printer.info(error.stack);
+    }
+    printer.error('There was an error adding/updating the geofence collection');
+    context.usageData.emitError(error);
+    process.exitCode = 1;
+  }
+};
---
+++ packages/amplify-category-geo/src/service-utils/validateGeoJSONFile.ts
@@ -0,0 +1,88 @@
+import { readFileSync } from 'fs-extra';
+import Ajv from 'ajv';
+import { FeatureCollection, IdentifierOption } from './populateParams';
+import GeoJSONSchema from 'amplify-category-geo/schema/GeoJSONSchema.json';
+import { v4 as uuid } from 'uuid';
+import { printer } from 'amplify-prompts';
+
+const MAX_VERTICES_NUM_PER_POLYGON = 1000;
+
+export const validateGeoJSONFile = (geoJSONFilePath: string, uniqueIdentifier: string = 'id', identifierOption: IdentifierOption = IdentifierOption.RootLevelID) => {
+  const data = JSON.parse(readFileSync(geoJSONFilePath, 'utf-8')) as FeatureCollection;
+  //Validate against pre-defined schema
+  const ajv = new Ajv();
+  const validator = ajv.compile(GeoJSONSchema);
+  if (!validator(data) as boolean) {
+    throw new Error(`The input GeoJSON file failed JSON schema validation. Underlying errors were ${JSON.stringify(validator.errors)}`);
+  };
+  const { features } = data;
+  let identifierSet = new Set();
+  features.forEach((feature) => {
+    //Check for identifier uniqueness
+    let identifierField: string;
+    if (identifierOption === IdentifierOption.RootLevelID) {
+      if (!feature.id) {
+        feature.id = uuid();
+        printer.info(`No root level id found. Auto assigning feature with id ${feature.id}.`)
+      }
+      identifierField = feature.id
+    }
+    else {
+      identifierField = feature.properties[uniqueIdentifier];
+      //Check if custom identifier exists in property
+      if (!identifierField) {
+        throw new Error(`Identifier field ${uniqueIdentifier} is missing in the feature property`)
+      }
+    }
+    if (identifierSet.has(identifierField)) {
+      throw new Error(`Identifier field ${uniqueIdentifier} is not unique in GeoJSON.`)
+    }
+    identifierSet.add(identifierField);
+    //Additional validation for each linear ring
+    const { coordinates } = feature.geometry;
+    let vortexCount = 0;
+    coordinates.forEach((linearRing, index) => {
+      validateLinearRing(linearRing, index === 0, identifierField);
+      vortexCount += linearRing.length;
+    })
+    if (vortexCount > MAX_VERTICES_NUM_PER_POLYGON) {
+      throw new Error(`Polygon should have at most ${MAX_VERTICES_NUM_PER_POLYGON} vertices.`)
+    }
+  });
+  return data;
+}
+
+/**
+ * Validate linear ring for those not captured by JSON Schema, incluidng first and last identical position, winding direction
+ * @param linearRing Array of coordinates
+ * @param isFirstRing Whether it is first(exterior) ring
+ */
+const validateLinearRing = (linearRing: Array<Array<number>>, isFirstRing: boolean, featureIdentity: string) => {
+  const numPoint = linearRing.length;
+  //Check if first position is identical to last one
+  if (!(linearRing[0][0] === linearRing[numPoint-1][0] && linearRing[0][1] === linearRing[numPoint-1][1])) {
+    throw new Error(`Linear ring of feature "${featureIdentity}" should have the identical values for first and last position.`);
+  }
+  //Check polygon wind direction
+  const isClockWise: boolean = isClockWiseLinearRing(linearRing);
+  if (isFirstRing) {
+    //First Ring should be counter clockwise
+    if (isClockWise) {
+      throw new Error('The first linear ring is exterior ring and should be counter-clockwise.')
+    }
+  } else {
+    //Non-first should be clockwise
+    if (!isClockWise) {
+      throw new Error('The interior ring should be clockwise.')
+    }
+  }
+};
+
+//Check the wind direction of linear ring. Assume the linear ring is valid and closed
+const isClockWiseLinearRing = (linearRing: Array<Array<number>>): boolean => {
+  let result = 0;
+  for (let i = 1; i < linearRing.length; i++) {
+    result += (linearRing[i][0] - linearRing[i-1][0]) * (linearRing[i][1] + linearRing[i-1][1]);
+  }
+  return result > 0;
+}
