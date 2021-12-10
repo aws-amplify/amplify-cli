d1c88fbae9dacb622fd6f6169859e16f3d3c001d
John Corser <johnpc@umich.edu>
2021-12-10 14:39:23 -0500

    Update packages/amplify-util-uibuilder/src/commands/version.ts
    Co-authored-by: Edward Foyle <foyleef@amazon.com>

--- packages/amplify-util-uibuilder/src/commands/version.ts
+++ packages/amplify-util-uibuilder/src/commands/version.ts
@@ -2,5 +2,5 @@ import { $TSContext } from 'amplify-cli-core';
 import path from 'path';
 
 export async function run(context: $TSContext) {
-  context.print.info(require(path.join(__dirname, '..', '..', 'package.json')).version);
+  printer.info((await import(path.join(__dirname, '..', '..', 'package.json'))).version);
 }
