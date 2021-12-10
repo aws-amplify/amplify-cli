3de7e8010e934bef6d65760160dabca8fd79155a
jhockett <johocke@amazon.com>
2021-12-10 12:46:08 -0800

    fix: update migration message text

--- packages/amplify-cli-core/src/overrides-manager/migration-message.ts
+++ packages/amplify-cli-core/src/overrides-manager/migration-message.ts
@@ -10,7 +10,7 @@ export function getMigrateResourceMessageForOverride(categoryName: string, resou
       `A migration is needed to support latest updates on ${categoryName} resources.`,
       chalk.red(`Recommended to try in a non-production environment first. Run "amplify env add" to create or clone an environment.`),
       chalk.red(
-        `Custom cloudformation changes will NOT be preserved. Custom changes can be made with "amplify ${categoryName} override" after migration.`,
+        `Custom CloudFormation changes will NOT be preserved. Custom changes can be made with "amplify ${categoryName} override" after migration.`,
       ),
       `Learn more about this migration: ${docsLink}`,
       `Do you want to migrate ${categoryName} resource "${resourceName}"?`,
@@ -21,6 +21,7 @@ export function getMigrateResourceMessageForOverride(categoryName: string, resou
     '',
     `A migration is needed to override ${categoryName} resources.`,
     chalk.red(`Recommended to try in a non-production environment first. Run "amplify env add" to create or clone an environment.`),
+    chalk.red(`Custom CloudFormation changes will NOT be preserved, but they can be reintroduced in the override.ts file.`),
     `Learn more about this migration: ${docsLink}`,
     `Do you want to migrate ${categoryName} resource "${resourceName}" to support overrides?`,
   ].join(EOL);
