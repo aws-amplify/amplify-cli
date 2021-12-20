7f37a6efacbf8b6dfb2f43cf5c72e9fc68015ca2
John Corser <johnpc@umich.edu>
2021-12-20 12:51:25 -0500

    chore: improve `add env` error handling

--- packages/amplify-cli/src/commands/env/add.ts
+++ packages/amplify-cli/src/commands/env/add.ts
@@ -1,6 +1,13 @@
-import { $TSContext } from 'amplify-cli-core';
+import { $TSContext, pathManager } from 'amplify-cli-core';
 import { run as init } from '../init';
+import fs from 'fs-extra';
 
 export const run = async (context: $TSContext) => {
+  const amplifyMetaFilePath = pathManager.getAmplifyMetaFilePath();
+  if (!fs.existsSync(amplifyMetaFilePath)) {
+    throw new Error(
+      'Your workspace is not configured to modify the backend. If you wish to change this configuration, remove your `amplify` directory and pull the project again.',
+    );
+  }
   await init(context);
 };
