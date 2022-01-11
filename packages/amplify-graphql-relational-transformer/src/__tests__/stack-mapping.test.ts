d354e78dd1e253d9572da3b08a4d8883e2fe673e
Edward Foyle <foyleef@amazon.com>
2022-01-11 11:54:55 -0800

    fix: use StackMapping for V2 resolvers (#9238)
    * fix: use stack mapping for v2 resolvers
    
    * test: add tests
    
    * chore: address PR comments
    
    * fix: return existing stack if present

---
+++ packages/amplify-graphql-relational-transformer/src/__tests__/stack-mapping.test.ts
@@ -0,0 +1,34 @@
+import { ModelTransformer } from '@aws-amplify/graphql-model-transformer';
+import { GraphQLTransform } from '@aws-amplify/graphql-transformer-core';
+import { BelongsToTransformer, HasManyTransformer } from '..';
+
+describe('transformer stack mapping', () => {
+  it('maps relational resolvers to specified stack', async () => {
+    const inputSchema = /* GraphQL */ `
+      type Blog @model {
+        id: ID!
+        name: String!
+        posts: [Post] @hasMany
+      }
+
+      type Post @model {
+        id: ID!
+        title: String!
+        blog: Blog @belongsTo
+      }
+    `;
+    const transformer = new GraphQLTransform({
+      transformers: [new ModelTransformer(), new HasManyTransformer(), new BelongsToTransformer()],
+      stackMapping: {
+        BlogpostsResolver: 'myCustomStack1',
+        PostblogResolver: 'myCustomStack2',
+      },
+    });
+
+    const result = transformer.transform(inputSchema);
+    expect(Object.keys(result.stacks.myCustomStack1.Resources!).includes('BlogpostsResolver')).toBe(true);
+    expect(Object.keys(result.stacks.myCustomStack2.Resources!).includes('PostblogResolver')).toBe(true);
+
+    expect(result.stacks.ConnectionStack).toBeUndefined();
+  });
+});
