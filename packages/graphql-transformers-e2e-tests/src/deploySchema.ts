e0672e5ba4a9c72adf7ceb551532974d322b041c
Edward Foyle <foyleef@amazon.com>
2022-01-26 12:40:39 -0800

    test: add mapsTo e2e tests

--- packages/graphql-transformers-e2e-tests/src/deploySchema.ts
+++ packages/graphql-transformers-e2e-tests/src/deploySchema.ts
@@ -15,14 +15,24 @@ const cf = new CloudFormationClient('us-west-2');
 const customS3Client = new S3Client('us-west-2');
 const awsS3Client = new S3({ region: 'us-west-2' });
 
-export type DeploySchemaReturn = {
-  graphqlClient: GraphQLClient;
-  cleanUp: () => Promise<void>;
+/**
+ * Interface for object that can manage graphql api deployments and cleanup for e2e tests
+ */
+export type SchemaDeployer = {
+  /**
+   * Deploy the given schema and return a client to query the API
+   */
+  deploy: (schema: string) => Promise<GraphQLClient>;
+  /**
+   * Cleanup the API
+   */
+  cleanup: () => Promise<void>;
 };
 
 /**
- * Deploys an AppSync API using the given transformer and schema and returns a GraphQL client pointing to the deployed API.
- * Also returns a function that can be used to tear down the API after the test is finished.
+ * Returns an object that can be used to deploy and cleanup GraphQL APIs. The deploy function supports multiple deployments to the same API.
+ * Each call to deploy returns a GraphQL client that can be used to query the API.
+ * The cleanup function will remove all local and cloud resources related to the API.
  *
  * No other tests are refactored to use this function at this point,
  * but it would be nice to extend this function to handle spinning up and cleaning up all test GQL endpoints
@@ -32,48 +42,54 @@ export type DeploySchemaReturn = {
  * @param schema The schema to transform
  * @returns A GraphQL client pointing to an AppSync API with the provided schema deployed to it
  */
-export const deploySchema = async (testId: string, transformer: GraphQLTransform, schema: string): Promise<DeploySchemaReturn> => {
-  const buildTimestamp = moment().format('YYYYMMDDHHmmss');
-  const stackName = `${testId}-${buildTimestamp}`;
-  const testBucketName = `${testId}-bucket-${buildTimestamp}`.toLowerCase();
+export const getSchemaDeployer = async (testId: string, transformerFactory: () => GraphQLTransform): Promise<SchemaDeployer> => {
+  const initialTimestamp = moment().format('YYYYMMDDHHmmss');
+  const stackName = `${testId}-${initialTimestamp}`;
+  const testBucketName = `${testId}-bucket-${initialTimestamp}`.toLowerCase();
   const localBuildDir = path.join(os.tmpdir(), testId);
   const s3RootDirKey = 'deployments';
+  let initialDeployment = true;
 
+  // create deployment bucket
   try {
     await awsS3Client.createBucket({ Bucket: testBucketName }).promise();
   } catch (err) {
     console.error(`Failed to create bucket ${testBucketName}: ${err}`);
   }
 
-  const out = transformer.transform(schema);
-
-  try {
-    const finishedStack = await deploy(customS3Client, cf, stackName, out, {}, localBuildDir, testBucketName, s3RootDirKey, buildTimestamp);
-
-    // Arbitrary wait to make sure everything is ready.
-    await cf.wait(5, () => Promise.resolve());
-
-    expect(finishedStack).toBeDefined();
-
-    const getApiEndpoint = outputValueSelector(ResourceConstants.OUTPUTS.GraphQLAPIEndpointOutput);
-    const getApiKey = outputValueSelector(ResourceConstants.OUTPUTS.GraphQLAPIApiKeyOutput);
-    const endpoint = getApiEndpoint(finishedStack.Outputs);
-    const apiKey = getApiKey(finishedStack.Outputs);
-
-    expect(apiKey).toBeDefined();
-    expect(endpoint).toBeDefined();
-
-    return {
-      graphqlClient: new GraphQLClient(endpoint, { 'x-api-key': apiKey }),
-      cleanUp: async () => {
-        await cleanupStackAfterTest(testBucketName, stackName, cf);
-        await fs.remove(localBuildDir);
-      },
-    };
-  } catch (e) {
-    console.error(e);
-    expect(true).toEqual(false);
-  }
+  return {
+    deploy: async (schema: string) => {
+      const deployTimestamp = moment().format('YYYYMMDDHHmmss');
+      const out = transformerFactory().transform(schema);
+      const finishedStack = await deploy(
+        customS3Client,
+        cf,
+        stackName,
+        out,
+        {},
+        localBuildDir,
+        testBucketName,
+        s3RootDirKey,
+        deployTimestamp,
+        initialDeployment,
+      );
+      // Arbitrary wait to make sure everything is ready.
+      await cf.wait(10, () => Promise.resolve());
+      expect(finishedStack).toBeDefined();
+      const endpoint = getApiEndpoint(finishedStack.Outputs);
+      const apiKey = getApiKey(finishedStack.Outputs);
+      expect(apiKey).toBeDefined();
+      expect(endpoint).toBeDefined();
+      console.log(`endpoint is ${endpoint}`);
+      console.log(`api key is ${apiKey}`);
+      initialDeployment = false;
+      return new GraphQLClient(endpoint, { 'x-api-key': apiKey });
+    },
+    cleanup: async () => {
+      await cleanupStackAfterTest(testBucketName, initialDeployment ? undefined : stackName, cf);
+      await fs.remove(localBuildDir);
+    },
+  };
 };
 
 function outputValueSelector(key: string) {
@@ -82,3 +98,6 @@ function outputValueSelector(key: string) {
     return output ? output.OutputValue : null;
   };
 }
+
+const getApiEndpoint = outputValueSelector(ResourceConstants.OUTPUTS.GraphQLAPIEndpointOutput);
+const getApiKey = outputValueSelector(ResourceConstants.OUTPUTS.GraphQLAPIApiKeyOutput);
