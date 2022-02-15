8ae5da1529874a4ec53a773262d8fc310d0e7fe4
Owen Richards <owen@biglemon.co.uk>
2022-02-15 22:42:39 +0000

    fix(amplify-graphql-transformer-core): Add line break back in without being wrapped in dedent

--- packages/amplify-graphql-transformer-core/src/transformer-context/resolver.ts
+++ packages/amplify-graphql-transformer-core/src/transformer-context/resolver.ts
@@ -265,7 +265,8 @@ export class TransformerResolver implements TransformerResolverProvider {
     );
     if (authModes.includes(AuthorizationType.IAM)) {
       const authRoleParameter = (context.stackManager.getParameter(IAM_AUTH_ROLE_PARAMETER) as CfnParameter).valueAsString;
-      const unauthRoleParameter = (context.stackManager.getParameter(IAM_UNAUTH_ROLE_PARAMETER) as CfnParameter).valueAsString;
+			const unauthRoleParameter = (context.stackManager.getParameter(IAM_UNAUTH_ROLE_PARAMETER) as CfnParameter).valueAsString;
+			initResolver += '\n'
       initResolver += dedent`
       $util.qr($ctx.stash.put("authRole", "arn:aws:sts::${
         Stack.of(context.stackManager.rootStack).account
