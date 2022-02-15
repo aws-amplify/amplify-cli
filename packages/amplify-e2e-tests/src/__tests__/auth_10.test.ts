cf1a5944e1ff60b7b497c9ee8811032d354c12a4
lazpavel <85319655+lazpavel@users.noreply.github.com>
2022-02-15 12:57:56 -0500

    fix(amplify-category-auth,amplify-category-geo): update backend-config on auth/geo category update

---
+++ packages/amplify-e2e-tests/src/__tests__/auth_10.test.ts
@@ -0,0 +1,44 @@
+import { $TSAny } from 'amplify-cli-core';
+import {
+  addAuthWithDefault,
+  createNewProjectDir,
+  deleteProject,
+  deleteProjectDir,
+  getBackendConfig,
+  initJSProjectWithProfile,
+  updateAuthMFAConfiguration,
+} from 'amplify-e2e-core';
+
+const defaultsSettings = {
+  name: 'authTest',
+};
+
+describe('amplify update auth', () => {
+  let projRoot: string;
+
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('auth');
+  });
+
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
+
+  it('...should update backend-config.json on auth update', async () => {
+    await initJSProjectWithProfile(projRoot, defaultsSettings);
+    await addAuthWithDefault(projRoot, {});
+    const backendConfigBefore = await getBackendConfig(projRoot);
+
+    await updateAuthMFAConfiguration(projRoot, {});
+    const backendConfigAfter = await getBackendConfig(projRoot);
+
+    expect(backendConfigBefore).toBeDefined();
+    const beforeMFAConfiguration = (Object.values(backendConfigBefore.auth)[0] as any).frontendAuthConfig.mfaConfiguration;
+    expect(beforeMFAConfiguration).toEqual('OFF');
+
+    expect(backendConfigAfter).toBeDefined();
+    const afterMFAConfiguration = (Object.values(backendConfigAfter.auth)[0] as any).frontendAuthConfig.mfaConfiguration;
+    expect(afterMFAConfiguration).toEqual('OPTIONAL');
+  });
+});
