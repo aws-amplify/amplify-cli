50b8a473d45047af63ce0eefa04d1e521526ca91
Christopher Sundersingh <sundersc@amazon.com>
2022-02-16 10:00:15 -0800

    test(api): split api_5 test into two

--- packages/amplify-e2e-tests/src/__tests__/api_5.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/api_5.test.ts
@@ -1,25 +1,18 @@
 import {
-  addApiWithoutSchema,
-  addFeatureFlag,
   addFunction,
   addRestApi,
   addSimpleDDB,
-  amplifyPushGraphQlWithCognitoPrompt,
   amplifyPushUpdate,
   checkIfBucketExists,
   createNewProjectDir,
   deleteProject,
   deleteProjectDir,
-  getAppSyncApi,
   getProjectMeta,
   initJSProjectWithProfile,
   listAttachedRolePolicies,
   listRolePolicies,
-  updateApiSchema,
   updateAuthAddAdminQueries,
 } from 'amplify-e2e-core';
-import { readdirSync, readFileSync } from 'fs';
-import * as path from 'path';
 
 // to deal with bug in cognito-identity-js
 (global as any).fetch = require('node-fetch');
@@ -162,57 +155,4 @@ describe('amplify add api (REST)', () => {
     validateMeta(amplifyMeta);
   });
 
-  it('adds a rest api and then adds a path to the existing api', async () => {
-    await initJSProjectWithProfile(projRoot, {});
-    await addFunction(projRoot, { functionTemplate: 'Hello World' }, 'nodejs');
-    await addRestApi(projRoot, { existingLambda: true });
-    await addRestApi(projRoot, { isFirstRestApi: false, existingLambda: true, path: '/newpath' });
-    await amplifyPushUpdate(projRoot);
-    validateMeta();
-  });
-
-  it('migrates malformed project files during push', async () => {
-    await initJSProjectWithProfile(projRoot, {});
-    await addFunction(projRoot, { functionTemplate: 'Hello World' }, 'nodejs');
-    await addRestApi(projRoot, { existingLambda: true, restrictAccess: true });
-
-    const apisDirectory = path.join(projRoot, 'amplify', 'backend', 'api');
-    const apis = readdirSync(apisDirectory);
-    const apiName = apis[0];
-    const apiDirectory = path.join(apisDirectory, apiName);
-    const cfnTemplateFile = path.join(apiDirectory, 'build', `${apiName}-cloudformation-template.json`);
-    const cfnTemplate = JSON.parse(readFileSync(cfnTemplateFile, 'utf8'));
-
-    // The ApiId output is required
-    expect(cfnTemplate.Outputs.ApiId).toBeDefined();
-
-    await amplifyPushUpdate(projRoot);
-    validateMeta();
-  });
-
-  it('amplify push prompt for cognito configuration if auth mode is missing', async () => {
-    const envName = 'devtest';
-    const projName = 'lambdaauthmode';
-    await initJSProjectWithProfile(projRoot, { name: projName, envName });
-    await addFeatureFlag(projRoot, 'graphqltransformer', 'useexperimentalpipelinedtransformer', true);
-    await addFeatureFlag(projRoot, 'graphqltransformer', 'transformerversion', 2);
-    await addApiWithoutSchema(projRoot, { transformerVersion: 1 });
-    await addFunction(projRoot, { functionTemplate: 'Hello World' }, 'nodejs');
-    await updateApiSchema(projRoot, projName, 'cognito_simple_model.graphql');
-    await amplifyPushGraphQlWithCognitoPrompt(projRoot);
-
-    const meta = getProjectMeta(projRoot);
-    const region = meta.providers.awscloudformation.Region;
-    const { output } = meta.api.lambdaauthmode;
-    const { GraphQLAPIIdOutput, GraphQLAPIEndpointOutput, GraphQLAPIKeyOutput } = output;
-    const { graphqlApi } = await getAppSyncApi(GraphQLAPIIdOutput, region);
-
-    expect(GraphQLAPIIdOutput).toBeDefined();
-    expect(GraphQLAPIEndpointOutput).toBeDefined();
-    expect(GraphQLAPIKeyOutput).toBeDefined();
-
-    expect(graphqlApi).toBeDefined();
-    expect(graphqlApi.apiId).toEqual(GraphQLAPIIdOutput);
-    validateMeta(meta);
-  });
 });
