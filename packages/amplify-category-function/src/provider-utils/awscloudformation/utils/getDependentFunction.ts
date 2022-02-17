80117f4cab727cc61217c07c0a745f6ce5540aa7
Marc VandenBerg <marc.vandenberg1@gmail.com>
2022-02-17 16:56:00 -0500

    fix: Remove functions no longer in use

--- packages/amplify-category-function/src/provider-utils/awscloudformation/utils/getDependentFunction.ts
+++ packages/amplify-category-function/src/provider-utils/awscloudformation/utils/getDependentFunction.ts
@@ -4,43 +4,6 @@ import { loadFunctionParameters, loadFunctionStackAsJSON } from './loadFunctionP
 import { ServiceName } from './constants';
 import { categoryName } from '../../../constants';
 
-/**
- * @deprecated in favor of lambdasWithMissingApiDependency, using a list of existing models rather than deleted ones
- */
-export async function lambdasWithApiDependency(
-  context: $TSContext,
-  allResources: $TSObject[],
-  backendDir: string,
-  modelsDeleted: string[],
-) {
-  //get the List of functions dependent on deleted model
-  let dependentFunctions = [];
-  const lambdaFuncResources = allResources.filter(
-    resource =>
-      resource.service === ServiceName.LambdaFunction &&
-      resource.mobileHubMigrated !== true &&
-      resource.dependsOn !== undefined &&
-      resource.dependsOn.find(val => val.category === 'api'),
-  );
-
-  // initialize function parameters for update
-  for (const lambda of lambdaFuncResources) {
-    const resourceDirPath = path.join(backendDir, categoryName, lambda.resourceName);
-    const currentParameters = loadFunctionParameters(resourceDirPath);
-    const selectedCategories = currentParameters.permissions;
-    let deletedModelFound: boolean;
-
-    if (typeof selectedCategories === 'object' && selectedCategories !== null) {
-      for (const selectedResources of Object.values(selectedCategories)) {
-        deletedModelFound = Object.keys(selectedResources).some(r => modelsDeleted.includes(r));
-        if (deletedModelFound) {
-          dependentFunctions.push(lambda);
-        }
-      }
-    }
-  }
-  return dependentFunctions;
-}
 
 export async function lambdasWithMissingApiDependency(
   context: $TSContext,
