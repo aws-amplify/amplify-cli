e3912d1798a1cec7f784644ad941a65ebc265a7c
Akshay <akshayupadhyay3@gmail.com>
2022-02-24 08:30:36 -0800

    fix: build resource when adding new env

--- packages/amplify-e2e-core/src/utils/index.ts
+++ packages/amplify-e2e-core/src/utils/index.ts
@@ -24,6 +24,7 @@ export * from './sleep';
 export * from './transformConfig';
 export * from './admin-ui';
 export * from './hooks';
+export * from './transformCurrentProjectToGitPulledProject';
 
 // run dotenv config to update env variable
 config();
