97d932b1a19ae526d65090e0318593babda285ed
jhockett <johocke@amazon.com>
2022-02-25 17:15:37 -0800

    fix: error message when auth is already imported

--- packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/project-has-auth.ts
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/project-has-auth.ts
@@ -1,15 +1,25 @@
+import { $TSContext, $TSObject, stateManager } from 'amplify-cli-core';
+import { printer } from 'amplify-prompts';
 import { messages } from '../assets/string-maps';
 
 /**
  * Checks if auth already exists in the project and prints a warning if so.
  * Returns true if auth already exists, false otherwise
- * @param context The amplify context
  */
-export const projectHasAuth = (context: any) => {
-  const existingAuth = context.amplify.getProjectDetails().amplifyMeta.auth || {};
+export const projectHasAuth = (context: $TSContext) => {
+  const meta = stateManager.getMeta(undefined, { throwIfNotExist: false });
+  const existingAuth: [string, $TSObject][] = Object.entries(meta?.auth || {});
 
-  if (Object.keys(existingAuth).length > 0) {
-    context.print.warning(messages.authExists);
+  if (existingAuth.length > 0) {
+    if (existingAuth.filter(([_, resource]) => resource?.serviceType === 'imported').length > 0) {
+      const command = !context?.input?.command || context.input.command === 'update' ? 'import' : context.input.command;
+      printer.warn(
+        'Auth has already been imported to this project and cannot be modified from the CLI. ' +
+          `To modify, run "amplify remove auth" to unlink the imported auth resource. Then run "amplify ${command} auth".`,
+      );
+    } else {
+      printer.warn(messages.authExists);
+    }
     return true;
   }
   return false;
