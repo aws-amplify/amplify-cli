6a963627e62bb1bf96630b6aaac77150eea82ff4
Edward Foyle <foyleef@amazon.com>
2022-03-03 16:40:46 -0800

    chore: set base branch

--- .circleci/lint_pr.sh
+++ .circleci/lint_pr.sh
@@ -1,4 +1,6 @@
 # get the name of the remote repo (probably "origin")
 REMOTE_NAME=$(git remote -v | grep aws-amplify/amplify-cli | head -n1 | awk '{print $1;}')
+# CCI doesn't have an env var that exposes the base branch. Hardcoding for now
+BASE_BRANCH=master
 # get the files that have changed since diverging from the remote, filter only code files, filter removed files, then pass to eslint
-git diff --name-only HEAD $(git merge-base --fork-point $REMOTE_NAME/master) | grep -E '\.(js|jsx|ts|tsx)$' | xargs find 2> /dev/null | xargs yarn eslint
+git diff --name-only HEAD $(git merge-base --fork-point $REMOTE_NAME/$BASE_BRANCH) | grep -E '\.(js|jsx|ts|tsx)$' | xargs find 2> /dev/null | xargs yarn eslint
