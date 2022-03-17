ed8986aa70880a511ac6394936db221a1945e211
Edward Foyle <foyleef@amazon.com>
2022-03-17 15:46:13 -0700

    chore: fix merge issue

--- scripts/add-to-dict.js
+++ scripts/add-to-dict.js
@@ -3,23 +3,13 @@ const dictionary = require('../.eslint-dictionary.json');
 
 const main = async () => {
   const newWords = process.argv.slice(2);
-<<<<<<< HEAD
-  dictionary.push(...newWords);
-  dictionary.sort();
-  await fs.writeFile(require.resolve('../.eslint-dictionary.json'), JSON.stringify(dictionary, undefined, 2))
-=======
   const wordSet = new Set(dictionary);
   newWords.forEach(word => wordSet.add(word));
   const sortedDedupedWordList = Array.from(wordSet).sort();
   await fs.writeFile(require.resolve('../.eslint-dictionary.json'), JSON.stringify(sortedDedupedWordList, undefined, 2))
->>>>>>> upstream/master
 }
 
 main().catch(err => {
   console.log(err);
   process.exit(1);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> upstream/master
