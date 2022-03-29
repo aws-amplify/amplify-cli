08a39d82502fd8f604adfe84029616364154147f
jhockett <johocke@amazon.com>
2022-03-29 16:16:47 -0700

    style: allow for..of loops, allow increment and decrement

--- .eslintrc.js
+++ .eslintrc.js
@@ -153,6 +153,24 @@ module.exports = {
     'no-useless-constructor': 'off',
     'no-underscore-dangle': 'off',
     'no-template-curly-in-string': 'off',
+    'no-plusplus': 'off',
+
+    // same as air-bnb default with the exception of allowing for...of
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
 
     // function style
     'arrow-parens': ['error', 'as-needed'],
