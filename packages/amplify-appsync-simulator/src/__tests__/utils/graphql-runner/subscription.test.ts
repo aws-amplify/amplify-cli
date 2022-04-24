59a6f2cdaf059a13e01d1bc4c035ef09e0c60d1a
Vinod S R <smartvinu@gmail.com>
2022-04-24 16:17:05 +0530

    test: added test cases for subscription filter
    Subscription filter was not having any proper testcases

--- packages/amplify-appsync-simulator/src/__tests__/utils/graphql-runner/subscription.test.ts
+++ packages/amplify-appsync-simulator/src/__tests__/utils/graphql-runner/subscription.test.ts
@@ -4,22 +4,26 @@ import { AmplifyAppSyncSimulatorAuthenticationType } from '../../../type-definit
 import { AppSyncGraphQLExecutionContext } from '../../../utils/graphql-runner';
 import { runQueryOrMutation } from '../../../utils/graphql-runner/query-and-mutation';
 import { runSubscription, SubscriptionResult } from '../../../utils/graphql-runner/subscriptions';
+import { AmplifyAppSyncSimulator } from '../../..';
 
 jest.mock('../../../utils/graphql-runner/query-and-mutation');
 const mockRunQuery = runQueryOrMutation as jest.Mock;
 describe('runSubscription', () => {
   const schemaDoc = parse(/* GraphQL */ `
+    type Profile {
+      name: String
+    }
     type Query {
-      getName: String!
+      getProfile: Profile!
     }
     type Subscription {
-      onSetName: String
+      onChangeProfile(name: String): Profile
     }
   `);
   let schema: GraphQLSchema;
 
-  let subscriptionResolver = jest.fn();
-  let subscriptionSubscribe = jest.fn();
+  const subscriptionResolver = jest.fn();
+  const subscriptionSubscribe = jest.fn();
 
   const executionContext: AppSyncGraphQLExecutionContext = {
     headers: { 'x-api-key': 'da-fake-key' },
@@ -28,36 +32,31 @@ describe('runSubscription', () => {
   };
   const variables = {};
 
+  let simulator: AmplifyAppSyncSimulator;
+
   beforeEach(() => {
     jest.resetAllMocks();
-    subscriptionSubscribe.mockImplementation(() => {
-      return {
-        [Symbol.asyncIterator]: () => {
-          return {
-            async next() {
-              return { done: false, value: 42 };
-            },
-          };
-        },
-      };
-    });
+    simulator = new AmplifyAppSyncSimulator();
+    // Use the real async iterator to test the filter
+    subscriptionSubscribe.mockImplementation((rootValue, args, context, info) => simulator.asyncIterator('onChangeProfile', rootValue, args, context, info));
 
     const resolvers = {
       Subscription: {
-        onSetName: {
+        onChangeProfile: {
           resolve: subscriptionResolver,
           subscribe: subscriptionSubscribe,
         },
       },
     };
+    /*eslint-disable spellcheck/spell-checker*/
     schema = makeExecutableSchema({ typeDefs: schemaDoc, resolvers });
     mockRunQuery.mockReturnValue({ data: null, errors: [] });
   });
 
-  it('should call subscribe resolver', async () => {
+  it('should call subscribe resolver (without filter)', async () => {
     const doc = parse(/* GraphQL */ `
-      subscription onSetName {
-        onSetName
+      subscription onChangeProfile {
+        onChangeProfile
       }
     `);
 
@@ -65,12 +64,72 @@ describe('runSubscription', () => {
     expect(typeof (result as SubscriptionResult).asyncIterator[Symbol.asyncIterator]).toEqual('function');
     expect(subscriptionSubscribe).toHaveBeenCalled();
     expect(subscriptionSubscribe.mock.calls[0][2]).toEqual(executionContext);
+    // Trigger a mock mutation
+    setTimeout(async () => {
+      /*eslint-disable spellcheck/spell-checker*/
+      await simulator.pubsub.publish('onChangeProfile', { name: 'sam' });
+    }, 1000);
+    // Will resolve once the mutation executes
+    await (result as SubscriptionResult).asyncIterator.next();
+    expect(subscriptionResolver).toHaveBeenCalled();
+    expect(subscriptionResolver.mock.calls[0][0]).toEqual({ name: 'sam' });
+  });
+
+  it('should call subscribe resolver with filter', async () => {
+    const doc = parse(/* GraphQL */ `
+      subscription onChangeProfile {
+        onChangeProfile(name: "mac") {
+          name
+        }
+      }
+    `);
+
+    const result = await runSubscription(schema, doc, variables, undefined, executionContext);
+    expect(typeof (result as SubscriptionResult).asyncIterator[Symbol.asyncIterator]).toEqual('function');
+    expect(subscriptionSubscribe).toHaveBeenCalled();
+    expect(subscriptionSubscribe.mock.calls[0][2]).toEqual(executionContext);
+    // Trigger a mock mutation
+    setTimeout(async () => {
+      /*eslint-disable spellcheck/spell-checker*/
+      await simulator.pubsub.publish('onChangeProfile', { name: 'mac' });
+    }, 1000);
+    // Will resolve once the mutation executes
+    await (result as SubscriptionResult).asyncIterator.next();
+    expect(subscriptionResolver).toHaveBeenCalled();
+    expect(subscriptionResolver.mock.calls[0][0]).toEqual({ name: 'mac' });
+  });
+
+  /* eslint-disable jest/no-done-callback*/
+  it('should not call subscribe resolver when the params are not matching', done => {
+    const doc = parse(/* GraphQL */ `
+      subscription onSetName {
+        onChangeProfile(name: "mac")
+      }
+    `);
+
+    runSubscription(schema, doc, variables, undefined, executionContext).then(result => {
+      expect(typeof (result as SubscriptionResult).asyncIterator[Symbol.asyncIterator]).toEqual('function');
+      expect(subscriptionSubscribe).toHaveBeenCalled();
+      expect(subscriptionSubscribe.mock.calls[0][2]).toEqual(executionContext);
+      setTimeout(async () => {
+        /*eslint-disable spellcheck/spell-checker*/
+        await simulator.pubsub.publish('onChangeProfile', { name: 'James' });
+      }, 1000);
+      // This will not be resolved as the filter and the mutation payload are not matching.
+      // timeout will be triggered instead
+      setTimeout(() => {
+        done();
+      }, 1500);
+      (result as SubscriptionResult).asyncIterator.next().then(() => {
+        throw new Error('Should not trigger subscription if the filter is not matching');
+      });
+    });
   });
 
   it('should throw error when the subscription is not authorized', async () => {
     const doc = parse(/* GraphQL */ `
-      subscription onSetName {
-        onSetName
+      subscription onChangeProfile {
+        onChangeProfile
       }
     `);
 
