1a97fc11bb15c88c30a09562ae0c2ed2fa071c17
Sachin Panemangalore <sachinrp@amazon.com>
2022-04-25 08:37:34 -0700

    chore: remove extraneous lib folder and update gitignore

--- packages/amplify-cli-shared-interfaces/lib/amplify-cli-flow-reporter-types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=amplify-cli-flow-reporter-types.js.map
--- packages/amplify-cli-shared-interfaces/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './amplify-cli-flow-reporter-types';
-export * from './amplify-cli-interactions';
-//# sourceMappingURL=index.d.ts.map
