1f5aea85f5cd98f733a5a183cc5bc283bdd46a9b
John Corser <xss@amazon.com>
2022-04-25 17:19:53 +0000

    chore: upgrade node12.x to node14.x

--- packages/amplify-e2e-tests/src/__tests__/migration/node.function.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/migration/node.function.test.ts
@@ -57,7 +57,7 @@ describe('nodejs version migration tests', () => {
     );
     let authStackContent = fs.readFileSync(authStackFileName).toString();
 
-    authStackContent = authStackContent.replace('nodejs12.x', 'nodejs10.x');
+    authStackContent = authStackContent.replace('nodejs14.x', 'nodejs10.x');
 
     fs.writeFileSync(authStackFileName, authStackContent, 'utf-8');
 
@@ -84,8 +84,8 @@ describe('nodejs version migration tests', () => {
     functionStackContent = fs.readFileSync(functionStackFileName).toString();
 
     expect(projectConfigContent.indexOf('3.1')).toBeGreaterThan(0);
-    expect(authStackContent.indexOf('nodejs12.x')).toBeGreaterThan(0);
-    expect(functionStackContent.indexOf('nodejs12.x')).toBeGreaterThan(0);
+    expect(authStackContent.indexOf('nodejs14.x')).toBeGreaterThan(0);
+    expect(functionStackContent.indexOf('nodejs14.x')).toBeGreaterThan(0);
   });
 
   function amplifyNodeMigrationAndPush(cwd: string) {
