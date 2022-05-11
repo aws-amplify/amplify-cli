2612585b1f84cd39ec81f54917d93b06c9d129d5
Al Harris <91494052+alharris-at@users.noreply.github.com>
2022-05-11 13:16:57 -0700

    Revert "Revert "chore: wrap getTransformerFactory behind a single API (#10373)" (#10390)" (#10391)
    This reverts commit c2629f6ac6aba05d205e6a3622a8cfa9f8c2d0bf.

--- packages/amplify-provider-awscloudformation/src/graphql-transformer-factory/directive-definitions.ts
+++ packages/amplify-provider-awscloudformation/src/graphql-transformer-factory/directive-definitions.ts
@@ -5,22 +5,23 @@ import {
   $TSContext,
 } from 'amplify-cli-core';
 import { print } from 'graphql';
-import { getTransformerFactoryV1, getTransformerFactoryV2 } from './transformer-factory';
+import { getTransformerFactory } from './transformer-factory';
 import { getTransformerVersion } from './transformer-version';
 
 /**
  * Return the set of directive definitions for the project, includes both appsync and amplify supported directives.
  * This will return the relevant set determined by whether or not the customer is using GQL transformer v1 or 2 in their project.
  */
-export async function getDirectiveDefinitions(context: $TSContext, resourceDir: string): Promise<string> {
+export const getDirectiveDefinitions = async (context: $TSContext, resourceDir: string): Promise<string> => {
   const transformerVersion = await getTransformerVersion(context);
+  const transformer = await getTransformerFactory(context, resourceDir);
   const transformList = transformerVersion === 2
-    ? await getTransformerFactoryV2(resourceDir)({ addSearchableTransformer: true, authConfig: {} })
-    : await getTransformerFactoryV1(context, resourceDir)(true);
+    ? await transformer({ addSearchableTransformer: true, authConfig: {} })
+    : await transformer(true);
 
   const transformDirectives = transformList
     .map(transformPluginInst => [transformPluginInst.directive, ...transformPluginInst.typeDefinitions].map(node => print(node)).join('\n'))
     .join('\n');
 
   return [getAppSyncServiceExtraDirectives(), transformDirectives].join('\n');
-}
+};
