2e0e1932f7464afcaa55269ac5415e4a5bea6e19
jhockett <johocke@amazon.com>
2022-05-12 15:20:23 -0700

    build: verify checksum against computed hash

--- packages/amplify-dynamodb-simulator/scripts/update-ddb-simulator.js
+++ packages/amplify-dynamodb-simulator/scripts/update-ddb-simulator.js
@@ -2,6 +2,7 @@ const { ensureDir, readFile, remove, writeFile } = require('fs-extra');
 const extract = require('extract-zip');
 const nodefetch = require('node-fetch');
 const { join } = require('path');
+const hash = require('hash.js');
 
 const main = async () => {
   const ddbSimulatorUrl = 'https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.zip';
@@ -11,22 +12,29 @@ const main = async () => {
   const sha256FilePath = join(emulatorDirPath, 'sha256');
 
 
-  const sha256 = await nodefetch(sha256Url).then(res => res.text());
+  const latestSha256 = (await nodefetch(sha256Url).then(res => res.text())).split(' ')[0];
   await ensureDir(emulatorDirPath);
 
-  let previousHash;
+  let previousSha256;
   try {
-    previousHash = (await readFile(sha256FilePath)).toString();
+    previousSha256 = (await readFile(sha256FilePath)).toString();
   } catch {
-    previousHash = undefined;
+    previousSha256 = undefined;
   }
 
-  if (previousHash !== sha256) {
+  if (previousSha256 !== latestSha256) {
     const ddbSimulatorZip = await nodefetch(ddbSimulatorUrl).then(res => res.buffer());
     const ddbSimulatorZipPath = join(__dirname, '..', 'ddbSimulator.zip');
+
+    const computedSha256 = hash.sha256().update(ddbSimulatorZip).digest('hex');
+    if (latestSha256 !== computedSha256) {
+      throw Error(`SHA256 DID NOT MATCH CHECKSUM. EXPECTED: ${latestSha256} RECEIVED: ${computedSha256}`);
+    }
+
+    // TODO: find a way to extract the zip file directly from memory
     await writeFile(ddbSimulatorZipPath, ddbSimulatorZip);
     await extract(ddbSimulatorZipPath, { dir: emulatorDirPath });
-    return Promise.all([writeFile(sha256FilePath, sha256), remove(ddbSimulatorZipPath)]);
+    return Promise.all([writeFile(sha256FilePath, latestSha256), remove(ddbSimulatorZipPath)]);
   }
 }
 
