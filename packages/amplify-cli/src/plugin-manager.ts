b2998d75f83bd75788f1665c3b1f61a4766d13ea
John Hockett <jhockett@users.noreply.github.com>
2022-05-11 17:02:04 -0700

    Revert "refactor: plugin platform code cleanup (#10384)" (#10403)
    This reverts commit 7474ba67f1405a10acd82fb87c76022429b7ddb1.

--- packages/amplify-cli/src/plugin-manager.ts
+++ packages/amplify-cli/src/plugin-manager.ts
@@ -1,40 +1,37 @@
-import { printer, prompter } from 'amplify-prompts';
-import { AddPluginError, AddPluginResult } from './domain/add-plugin-result';
-import { AmplifyEvent } from './domain/amplify-event';
-import { constants } from './domain/constants';
-import { PluginInfo } from './domain/plugin-info';
+import * as inquirer from 'inquirer';
 import { PluginPlatform } from './domain/plugin-platform';
+import { PluginInfo } from './domain/plugin-info';
 import { readPluginsJsonFile, writePluginsJsonFile } from './plugin-helpers/access-plugins-file';
-import { twoPluginsAreTheSame } from './plugin-helpers/compare-plugins';
-import createNewPlugin from './plugin-helpers/create-new-plugin';
 import {
+  scanPluginPlatform,
   getCorePluginDirPath,
   getCorePluginVersion,
   isUnderScanCoverageSync,
-  scanPluginPlatform,
 } from './plugin-helpers/scan-plugin-platform';
 import { verifyPlugin } from './plugin-helpers/verify-plugin';
+import createNewPlugin from './plugin-helpers/create-new-plugin';
+import { AddPluginResult, AddPluginError } from './domain/add-plugin-result';
+import { twoPluginsAreTheSame } from './plugin-helpers/compare-plugins';
+import { AmplifyEvent } from './domain/amplify-event';
+import { constants } from './domain/constants';
+import { print } from './context-extensions';
 import { postInstallInitialization } from './utils/post-install-initialization';
 
-export { verifyPlugin };
-export { createNewPlugin };
-
-/**
- * This function is called at the beginning of each command execution
- * and performs the following actions:
- * 1. read the plugins.json file
- * 2. checks the last scan time stamp,
- * 3. re-scan if needed.
- * 4. write to update the plugins.json file if re-scan is performed
- * 5. return the pluginsInfo object
- */
-export const getPluginPlatform = async (): Promise<PluginPlatform> => {
+export async function getPluginPlatform(): Promise<PluginPlatform> {
+  // This function is called at the beginning of each command execution
+  // and performs the following actions:
+  // 1. read the plugins.json file
+  // 2. checks the last scan time stamp,
+  // 3. re-scan if needed.
+  // 4. write to update the plugins.json file if re-scan is performed
+  // 5. return the pluginsInfo object
   let pluginPlatform = readPluginsJsonFile();
 
   if (pluginPlatform) {
     if (isCoreMatching(pluginPlatform)) {
       const lastScanTime = new Date(pluginPlatform.lastScanTime);
       const currentTime = new Date();
+      // tslint:disable-next-line
       const timeDiffInSeconds = (currentTime.getTime() - lastScanTime.getTime()) / 1000;
       if (timeDiffInSeconds > pluginPlatform.maxScanIntervalInSeconds) {
         pluginPlatform = await scan();
@@ -51,9 +48,9 @@ export const getPluginPlatform = async (): Promise<PluginPlatform> => {
   }
 
   return pluginPlatform;
-};
+}
 
-const isCoreMatching = (pluginPlatform: PluginPlatform): boolean => {
+function isCoreMatching(pluginPlatform: PluginPlatform): boolean {
   try {
     const currentCorePluginDirPath = getCorePluginDirPath();
     const currentCorePluginVersion = getCorePluginVersion();
@@ -63,12 +60,9 @@ const isCoreMatching = (pluginPlatform: PluginPlatform): boolean => {
   } catch {
     return false;
   }
-};
+}
 
-/**
- * get plugins by name
- */
-export const getPluginsWithName = (pluginPlatform: PluginPlatform, nameOrAlias: string): Array<PluginInfo> => {
+export function getPluginsWithName(pluginPlatform: PluginPlatform, nameOrAlias: string): Array<PluginInfo> {
   let result = new Array<PluginInfo>();
 
   Object.keys(pluginPlatform.plugins).forEach(pluginName => {
@@ -84,19 +78,14 @@ export const getPluginsWithName = (pluginPlatform: PluginPlatform, nameOrAlias: 
   });
 
   return result;
-};
+}
 
-/**
- * get plugins by name and command
- */
-export const getPluginsWithNameAndCommand = (pluginPlatform: PluginPlatform, nameOrAlias: string, command: string): Array<PluginInfo> => {
+export function getPluginsWithNameAndCommand(pluginPlatform: PluginPlatform, nameOrAlias: string, command: string): Array<PluginInfo> {
   const result = new Array<PluginInfo>();
 
   Object.keys(pluginPlatform.plugins).forEach(pluginName => {
     pluginPlatform.plugins[pluginName].forEach(pluginInfo => {
-      const {
-        name, aliases, commands, commandAliases,
-      } = pluginInfo.manifest;
+      const { name, aliases, commands, commandAliases } = pluginInfo.manifest;
       const nameOrAliasMatching = name === nameOrAlias || (aliases && aliases!.includes(nameOrAlias));
 
       if (nameOrAliasMatching) {
@@ -108,12 +97,9 @@ export const getPluginsWithNameAndCommand = (pluginPlatform: PluginPlatform, nam
   });
 
   return result;
-};
+}
 
-/**
- * get plugins that support handling a passed in event
- */
-export const getPluginsWithEventHandler = (pluginPlatform: PluginPlatform, event: AmplifyEvent): Array<PluginInfo> => {
+export function getPluginsWithEventHandler(pluginPlatform: PluginPlatform, event: AmplifyEvent): Array<PluginInfo> {
   const result = new Array<PluginInfo>();
 
   Object.keys(pluginPlatform.plugins).forEach(pluginName => {
@@ -126,12 +112,9 @@ export const getPluginsWithEventHandler = (pluginPlatform: PluginPlatform, event
   });
 
   return result;
-};
+}
 
-/**
- * get the names of all plugins as a set
- */
-export const getAllPluginNames = (pluginPlatform: PluginPlatform): Set<string> => {
+export function getAllPluginNames(pluginPlatform: PluginPlatform): Set<string> {
   const result = new Set<string>();
 
   Object.keys(pluginPlatform.plugins).forEach(pluginName => {
@@ -147,52 +130,43 @@ export const getAllPluginNames = (pluginPlatform: PluginPlatform): Set<string> =
   });
 
   return result;
-};
+}
 
-/**
- * wrapper around scanPluginPlatform
- */
-export const scan = async (pluginPlatform?: PluginPlatform): Promise<PluginPlatform> => {
+export async function scan(pluginPlatform?: PluginPlatform): Promise<PluginPlatform> {
   try {
-    return scanPluginPlatform(pluginPlatform);
+    const result = await scanPluginPlatform(pluginPlatform);
+    return result;
   } catch (e) {
-    printer.error('Plugin scan failed.');
-    printer.info(e);
+    print.error('Plugin scan failed.');
+    print.info(e);
     throw new Error('Plugin scan failed.');
   }
-};
+}
 
-/**
- * Ask to scan
- */
-export const confirmAndScan = async (pluginPlatform: PluginPlatform): Promise<void> => {
-  const confirmed = await prompter.confirmContinue('Run a fresh scan for plugins on the Amplify CLI pluggable platform?');
+export { verifyPlugin };
+
+export { createNewPlugin };
+
+export async function confirmAndScan(pluginPlatform: PluginPlatform) {
+  const { confirmed } = await inquirer.prompt({
+    type: 'confirm',
+    name: 'confirmed',
+    message: 'Run a fresh scan for plugins on the Amplify CLI pluggable platform',
+    default: false,
+  });
   if (confirmed) {
     await scan(pluginPlatform);
   }
+}
+
+export const addUserPluginPackage = async (pluginPlatform: PluginPlatform, pluginDirPath: string): Promise<AddPluginResult> => {
+  return addPluginPackage(pluginPlatform, pluginDirPath);
 };
 
-/**
- * add user plugin
- */
-export const addUserPluginPackage = async (
-  pluginPlatform: PluginPlatform,
-  pluginDirPath: string,
-): Promise<AddPluginResult> => addPluginPackage(pluginPlatform, pluginDirPath);
+export const addExcludedPluginPackage = async (pluginPlatform: PluginPlatform, pluginInfo: PluginInfo): Promise<AddPluginResult> => {
+  return addPluginPackage(pluginPlatform, pluginInfo.packageLocation);
+};
 
-/**
- * add excluded plugin
- */
-export const addExcludedPluginPackage = async (
-  pluginPlatform: PluginPlatform,
-  pluginInfo: PluginInfo,
-): Promise<AddPluginResult> => addPluginPackage(pluginPlatform, pluginInfo.packageLocation);
-
-/* eslint-disable no-param-reassign */
-
-/**
- * add a plugin
- */
 export const addPluginPackage = async (pluginPlatform: PluginPlatform, pluginDirPath: string): Promise<AddPluginResult> => {
   const pluginVerificationResult = await verifyPlugin(pluginDirPath);
   const result = new AddPluginResult(false, pluginVerificationResult);
@@ -243,12 +217,10 @@ export const addPluginPackage = async (pluginPlatform: PluginPlatform, pluginDir
   return result;
 };
 
-/**
- * only remove from the plugins:
- *   if the location belongs to the scan directories, put the info inside the excluded
- *   if the location is in the userAddedLocations, remove it from the user added locations
- */
-export const removePluginPackage = (pluginPlatform: PluginPlatform, pluginInfo: PluginInfo): void => {
+// remove: select from the plugins only,
+// if the location belongs to the scan directories, put the info inside the excluded.
+// if the location is in the useraddedlocaitons, remove it from the user added locations.
+export function removePluginPackage(pluginPlatform: PluginPlatform, pluginInfo: PluginInfo): void {
   // remove from the plugins
   if (pluginPlatform.plugins[pluginInfo.manifest.name] && pluginPlatform.plugins[pluginInfo.manifest.name].length > 0) {
     const updatedPlugins = new Array<PluginInfo>();
@@ -282,6 +254,4 @@ export const removePluginPackage = (pluginPlatform: PluginPlatform, pluginInfo: 
   }
 
   writePluginsJsonFile(pluginPlatform);
-};
-
-/* eslint-enable no-param-reassign */
+}
