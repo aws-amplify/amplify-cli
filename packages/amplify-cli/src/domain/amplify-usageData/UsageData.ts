4959266463b913c5a9513dca4d24aac612337959
Ammar Karachi <ammkara@amazon.com>
2022-05-17 23:46:43 +0000

    feat: adding prompt times to usage-data

--- packages/amplify-cli/src/domain/amplify-usageData/UsageData.ts
+++ packages/amplify-cli/src/domain/amplify-usageData/UsageData.ts
@@ -178,7 +178,7 @@ export class UsageData implements IUsageData {
 
     // stop all currently running timers
     Array.from(this.codePathTimers.keys()).forEach(this.internalStopCodePathTimer);
-
+    
     const payload = new UsageDataPayload(
       this.sessionUuid,
       this.installationUuid,
@@ -192,6 +192,9 @@ export class UsageData implements IUsageData {
       Object.fromEntries(this.codePathDurations),
       this.flow.getFlowReport() as IFlowReport,
     );
+    
+    await this.send(payload);
+
     return payload;
   }
 
