d0719febca81a6a8a437d33412a8ff436392ee44
Edward Foyle <foyleef@amazon.com>
2022-05-26 19:45:40 -0700

    Revert "chore: removes console logging from auth events (#10467)"
    This reverts commit 07899189497f91f5934826203f1d01c923b71cbd.

--- packages/amplify-category-auth/src/index.js
+++ packages/amplify-category-auth/src/index.js
@@ -529,6 +529,7 @@ const executeAmplifyHeadlessCommand = async (context, headlessPayload) => {
  * entry point for amplify events
  */
 async function handleAmplifyEvent(context, args) {
+  context.print.info(`Received event args ${args}`);
   switch (args.event) {
     case 'PrePush':
       await prePushHandler(context);
