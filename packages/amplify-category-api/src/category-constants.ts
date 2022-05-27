4f831ee29a469877bdbc09e37a9439de82155780
Alexander Harris <alharris@amazon.com>
2022-05-27 10:02:01 -0700

    chore: refactor amplify-category-api out and into it's own repo

--- packages/amplify-category-api/src/category-constants.ts
+++
@@ -1,5 +0,0 @@
-import { AmplifyCategories } from 'amplify-cli-core';
-
-export const ADMIN_QUERIES_NAME = 'AdminQueries';
-export const NETWORK_STACK_LOGICAL_ID = 'NetworkStack';
-export const category = AmplifyCategories.API;
