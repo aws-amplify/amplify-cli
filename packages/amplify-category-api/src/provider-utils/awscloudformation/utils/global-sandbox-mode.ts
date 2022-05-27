decf612bed8107cc42ac5a815522c9db8d0dd829
Alexander Harris <alharris@amazon.com>
2022-05-27 15:41:49 -0700

    chore: removing api

--- packages/amplify-category-api/src/provider-utils/awscloudformation/utils/global-sandbox-mode.ts
+++
@@ -1,6 +0,0 @@
-export function defineGlobalSandboxMode(link: string): string {
-  return `# This "input" configures a global authorization rule to enable public access to
-# all models in this schema. Learn more about authorization rules here: ${link}
-input AMPLIFY { globalAuthRule: AuthRule = { allow: public } } # FOR TESTING ONLY!\n
-`;
-}
