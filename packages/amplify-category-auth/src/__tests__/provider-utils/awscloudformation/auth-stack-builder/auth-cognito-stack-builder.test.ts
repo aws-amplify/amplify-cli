41dcc057b96ac5d6debf2b705ed8a218469cca53
Akshay <akshayupadhyay3@gmail.com>
2022-06-13 16:54:02 -0700

    fix: updated unit tests

--- packages/amplify-category-auth/src/__tests__/provider-utils/awscloudformation/auth-stack-builder/auth-cognito-stack-builder.test.ts
+++ packages/amplify-category-auth/src/__tests__/provider-utils/awscloudformation/auth-stack-builder/auth-cognito-stack-builder.test.ts
@@ -1,10 +1,69 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable max-lines-per-function */
+import * as iam from '@aws-cdk/aws-iam';
+import * as cdk from '@aws-cdk/core';
 import { AmplifyAuthCognitoStack } from '../../../../provider-utils/awscloudformation/auth-stack-builder/auth-cognito-stack-builder';
 import { AuthStackSynthesizer } from '../../../../provider-utils/awscloudformation/auth-stack-builder/stack-synthesizer';
-import * as cdk from '@aws-cdk/core';
-import * as iam from '@aws-cdk/aws-iam';
+import { AttributeType } from '../../../../provider-utils/awscloudformation/service-walkthrough-types/awsCognito-user-input-types';
 import { CognitoStackOptions } from '../../../../provider-utils/awscloudformation/service-walkthrough-types/cognito-user-input-types';
 
 describe('generateCognitoStackResources', () => {
+  const props: CognitoStackOptions = {
+    identityPoolName: 'issue96802f106de3_identitypool_2f106de3',
+    allowUnauthenticatedIdentities: false,
+    resourceNameTruncated: 'issue92f106de3',
+    userPoolName: 'issue96802f106de3_userpool_2f106de3',
+    autoVerifiedAttributes: ['email'],
+    mfaConfiguration: 'OFF',
+    mfaTypes: ['SMS Text Message'],
+    smsAuthenticationMessage: 'Your authentication code is {####}',
+    smsVerificationMessage: 'Your verification code is {####}',
+    emailVerificationSubject: 'Your verification code',
+    emailVerificationMessage: 'Your verification code is {####}',
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [],
+    requiredAttributes: ['email'],
+    aliasAttributes: [],
+    userpoolClientGenerateSecret: false,
+    userpoolClientRefreshTokenValidity: 30,
+    userpoolClientWriteAttributes: ['email'],
+    userpoolClientReadAttributes: ['email'],
+    userpoolClientLambdaRole: 'issue92f106de3_userpoolclient_lambda_role',
+    userpoolClientSetAttributes: false,
+    sharedId: '2f106de3',
+    resourceName: 'issue96802f106de32f106de3',
+    authSelections: 'identityPoolAndUserPool',
+    useDefault: 'manual',
+    thirdPartyAuth: false,
+    userPoolGroups: false,
+    adminQueries: false,
+    triggers: {
+      PreSignup: ['custom'],
+    },
+    hostedUI: false,
+    userPoolGroupList: [],
+    serviceName: 'Cognito',
+    usernameCaseSensitive: false,
+    useEnabledMfas: true,
+    authRoleArn: {
+      'Fn::GetAtt': ['AuthRole', 'Arn'],
+    },
+    unauthRoleArn: {
+      'Fn::GetAtt': ['UnauthRole', 'Arn'],
+    },
+    breakCircularDependency: false,
+    dependsOn: [
+      {
+        category: 'function',
+        resourceName: 'issue96802f106de32f106de3PreSignup',
+        attributes: ['Arn', 'Name'],
+      },
+    ],
+    permissions: [],
+    authTriggerConnections: [{ triggerType: 'PreSignUp', lambdaFunctionName: 'issue96802f106de32f106de3PreSignup' }],
+    authProviders: [],
+  };
+
   it('adds correct custom oauth lambda dependencies', () => {
     const testApp = new cdk.App();
     const cognitoStack = new AmplifyAuthCognitoStack(testApp, 'testCognitoStack', { synthesizer: new AuthStackSynthesizer() });
@@ -30,86 +89,55 @@ describe('generateCognitoStackResources', () => {
   it('adds correct preSignUp  lambda config and permissions', () => {
     const testApp = new cdk.App();
     const cognitoStack = new AmplifyAuthCognitoStack(testApp, 'CognitoPreSignUpTriggerTest', { synthesizer: new AuthStackSynthesizer() });
-    const props : CognitoStackOptions = {
-        "identityPoolName": "issue96802f106de3_identitypool_2f106de3",
-        "allowUnauthenticatedIdentities": false,
-        "resourceNameTruncated": "issue92f106de3",
-        "userPoolName": "issue96802f106de3_userpool_2f106de3",
-        "autoVerifiedAttributes": [
-          "email"
-        ],
-        "mfaConfiguration": "OFF",
-        "mfaTypes": [
-          "SMS Text Message"
-        ],
-        "smsAuthenticationMessage": "Your authentication code is {####}",
-        "smsVerificationMessage": "Your verification code is {####}",
-        "emailVerificationSubject": "Your verification code",
-        "emailVerificationMessage": "Your verification code is {####}",
-        "passwordPolicyMinLength": 8,
-        "passwordPolicyCharacters": [],
-        "requiredAttributes": [
-          "email"
-        ],
-        "aliasAttributes": [],
-        "userpoolClientGenerateSecret": false,
-        "userpoolClientRefreshTokenValidity": 30,
-        "userpoolClientWriteAttributes": [
-          "email"
-        ],
-        "userpoolClientReadAttributes": [
-          "email"
-        ],
-        "userpoolClientLambdaRole": "issue92f106de3_userpoolclient_lambda_role",
-        "userpoolClientSetAttributes": false,
-        "sharedId": "2f106de3",
-        "resourceName": "issue96802f106de32f106de3",
-        "authSelections": "identityPoolAndUserPool",
-        "useDefault": "manual",
-        "thirdPartyAuth": false,
-        "userPoolGroups": false,
-        "adminQueries": false,
-        "triggers": {
-          "PreSignup": [
-            "custom"
-          ]
-        },
-        "hostedUI": false,
-        "userPoolGroupList": [],
-        "serviceName": "Cognito",
-        "usernameCaseSensitive": false,
-        "useEnabledMfas": true,
-        "authRoleArn": {
-          "Fn::GetAtt": [
-            "AuthRole",
-            "Arn"
-          ]
-        },
-        "unauthRoleArn": {
-          "Fn::GetAtt": [
-            "UnauthRole",
-            "Arn"
-          ]
-        },
-        "breakCircularDependency": false,
-        "dependsOn": [
-          {
-            "category": "function",
-            "resourceName": "issue96802f106de32f106de3PreSignup",
-            "attributes": [
-              "Arn",
-              "Name"
-            ]
-          }
-        ],
-        "permissions": [],
-        "authTriggerConnections": [
-          {triggerType: "PreSignUp",lambdaFunctionName: "issue96802f106de32f106de3PreSignup"}
-        ],
-        "authProviders": [],
-      }
     cognitoStack.generateCognitoStackResources(props);
-    expect(cognitoStack.userPool!.lambdaConfig).toHaveProperty('preSignUp');
+    expect(cognitoStack.userPool?.lambdaConfig).toHaveProperty('preSignUp');
     expect(cognitoStack.lambdaConfigPermissions).toHaveProperty('UserPoolPreSignupLambdaInvokePermission');
+  });
+
+  it('disables updateAttributeSetting when autoVerified attributes not present', () => {
+    const testApp = new cdk.App();
+    const cognitoStack = new AmplifyAuthCognitoStack(testApp, 'CognitoPreSignUpTriggerTest', { synthesizer: new AuthStackSynthesizer() });
+    const updatedProps = { ...props };
+    delete updatedProps.autoVerifiedAttributes;
+    cognitoStack.generateCognitoStackResources(updatedProps);
+    expect(cognitoStack.userPool?.userAttributeUpdateSettings).toBeUndefined();
+  });
+
+  it('correctly adds updateAttributeSetting when autoVerifiedAttributes attributes is TOTP', () => {
+    const testApp = new cdk.App();
+    const cognitoStack = new AmplifyAuthCognitoStack(testApp, 'CognitoUpdateAttributesettingTest', {
+      synthesizer: new AuthStackSynthesizer(),
+    });
+    const updatedProps: CognitoStackOptions = {
+      ...props,
+      userAutoVerifiedAttributeUpdateSettings: [AttributeType.PHONE_NUMBER],
+    };
+    cognitoStack.generateCognitoStackResources(updatedProps);
+    expect(cognitoStack.userPool?.userAttributeUpdateSettings).toMatchInlineSnapshot(`
+      Object {
+        "attributesRequireVerificationBeforeUpdate": Array [
+          "phone_number",
+        ],
+      }
+    `);
+  });
+
+  it('correctly adds updateAttributeSetting when autoVerifiedAttributes attributes is email', () => {
+    const testApp = new cdk.App();
+    const cognitoStack = new AmplifyAuthCognitoStack(testApp, 'CognitoUpdateAttributesettingTest1', {
+      synthesizer: new AuthStackSynthesizer(),
+    });
+    const updatedProps: CognitoStackOptions = {
+      ...props,
+      userAutoVerifiedAttributeUpdateSettings: [AttributeType.EMAIL],
+    };
+    cognitoStack.generateCognitoStackResources(updatedProps);
+    expect(cognitoStack.userPool?.userAttributeUpdateSettings).toMatchInlineSnapshot(`
+      Object {
+        "attributesRequireVerificationBeforeUpdate": Array [
+          "email",
+        ],
+      }
+    `);
   });
 });
