86440397aa87db163159067c357ac204d24b4731
Pavel Lazar <85319655+lazpavel@users.noreply.github.com>
2022-06-14 18:12:01 -0400

    Revert "chore: add FF for primary key connection field attributes (#10556)" (#10603)
    This reverts commit b4b297e4ead10dab8415af430aab53d88ef302ec.

--- packages/amplify-cli-core/src/feature-flags/featureFlags.ts
+++ packages/amplify-cli-core/src/feature-flags/featureFlags.ts
@@ -637,12 +637,6 @@ export class FeatureFlags {
         defaultValueForExistingProjects: false,
         defaultValueForNewProjects: false,
       },
-      {
-        name: 'respectPrimaryKeyAttributesOnConnectionField',
-        type: 'boolean',
-        defaultValueForExistingProjects: false,
-        defaultValueForNewProjects: false,
-      },
     ]);
 
     this.registerFlag('frontend-ios', [
