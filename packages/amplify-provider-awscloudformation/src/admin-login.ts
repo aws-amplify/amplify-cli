6fa6c59411a3736a5c53195734d448f9813f4219
John Corser <johnpc@umich.edu>
2022-06-23 13:32:06 -0400

    Fix safari login (#10606)
    * Revert "Revert "feat: allow login via admin ui on safari (#10407)" (#10602)"
    
    This reverts commit 16104ea72313382cea2c5c5a16231a22c5cba344.
    
    * fix: handle promise resolution to prevent hanging after Studio pull

--- packages/amplify-provider-awscloudformation/src/admin-login.ts
+++ packages/amplify-provider-awscloudformation/src/admin-login.ts
@@ -1,40 +1,125 @@
 import ora from 'ora';
+import util from 'util';
+import readline from 'readline';
+import { Writable } from 'stream';
 import { $TSContext, open } from 'amplify-cli-core';
-
-import { adminVerifyUrl, adminBackendMap, isAmplifyAdminApp } from './utils/admin-helpers';
+import { printer } from 'amplify-prompts';
+import { adminVerifyUrl, adminBackendMap, isAmplifyAdminApp } from './utils/admin-helpers';  // eslint-disable-line
 import { AdminLoginServer } from './utils/admin-login-server';
+import { AdminAuthPayload } from './utils/auth-types';
 
-export async function adminLoginFlow(context: $TSContext, appId: string, envName?: string, region?: string) {
-  envName = envName || context.amplify.getEnvInfo().envName;
+/**
+ * Launches browser and promises to receive authentication from browser
+ */
+export const adminLoginFlow = async (context: $TSContext, appId: string, envName?: string, region?: string): Promise<void> => {
+  envName = envName || context.amplify.getEnvInfo().envName;  // eslint-disable-line
   if (!region) {
     const { isAdminApp, region: _region } = await isAmplifyAdminApp(appId);
     if (!isAdminApp) {
       throw new Error(`Amplify Studio not enabled for appId: ${appId}`);
     }
-    region = _region;
+    region = _region; // eslint-disable-line
   }
 
   const url = adminVerifyUrl(appId, envName, region);
-  context.print.info(`Opening link: ${url}`);
+  printer.info(`Opening link: ${url}`);
   await open(url, { wait: false }).catch(e => {
-    context.print.error(`Failed to open web browser: ${e.message || e}`);
-    return;
+    printer.error(`Failed to open web browser: ${e.message || e}`);
   });
-  const spinner = ora('Continue in browser to log in…\n').start();
+  const spinner = ora('Confirm login in the browser or manually paste in your CLI login key:\n').start();
+
   try {
     // spawn express server locally to get credentials
     // environment variable AMPLIFY_CLI_ADMINUI_BASE_URL is used to set the login url to http://localhost:3000 when developing against beta/gamma endpoints
     const originUrl = process.env.AMPLIFY_CLI_ADMINUI_BASE_URL ?? adminBackendMap[region]?.amplifyAdminUrl;
-    const adminLoginServer = new AdminLoginServer(appId, originUrl, context.print);
-    await new Promise<void>(resolve =>
-      adminLoginServer.startServer(() => {
-        adminLoginServer.shutdown();
-        spinner.succeed('Successfully received Amplify Studio tokens.');
-        resolve();
-      }),
-    );
+    const adminLoginServer = new AdminLoginServer(appId, originUrl, printer);
+
+    const getTokenViaServer = (): [Promise<void>, () => void] => {
+      let finished = false;
+      let cancel = (): void => { finished = true; };
+      const promise = new Promise<void>((resolve, reject) => {
+        adminLoginServer.startServer(() => {
+          adminLoginServer.shutdown();
+          printer.success('Successfully received Amplify Studio tokens.');
+          finished = true;
+          resolve();
+        });
+        cancel = () => {
+          if (finished) {
+            return;
+          }
+          finished = true;
+          adminLoginServer.shutdown();
+          reject();
+        };
+      });
+
+      return [promise, cancel];
+    };
+
+    const getTokenViaPrompt = (): [Promise<void>, () => void] => {
+      let finished = false;
+      let cancel = (): void => { finished = true; };
+      const promise = new Promise<void>((resolve, reject) => {
+        // Input is hidden when the user pastes their authorization code into the CLI
+        const hiddenStdout: Writable | { muted: boolean} = new Writable({
+          write: (__, ___, callback) => {
+            callback();
+          },
+        });
+
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: hiddenStdout,
+          terminal: true,
+        });
+
+        const question = util.promisify(rl.question).bind(rl);
+        // No need to emit a question since the ora spinner has already printed a prompt
+        question('')
+          .then(async (tokenBase64: string) => {
+            if (finished) {
+              resolve();
+              return;
+            }
+            try {
+              const tokenJson = JSON.parse(Buffer.from(tokenBase64, 'base64').toString()) as unknown as AdminAuthPayload;
+              await adminLoginServer.storeTokens(tokenJson, appId);
+            } catch (e) {
+              printer.error('Provided token was invalid.');
+              rl.close();
+              reject();
+              return;
+            }
+            finished = true;
+            rl.close();
+            resolve();
+          });
+
+        cancel = () => {
+          if (finished) {
+            return;
+          }
+          finished = true;
+          rl.close();
+          reject();
+        };
+      });
+      return [promise, cancel];
+    };
+
+    const [promiseGetTokenViaPrompt, cancelGetTokenViaPrompt] = getTokenViaPrompt();
+    const [promiseGetTokenViaServer, cancelGetTokenViaServer] = getTokenViaServer();
+
+    // After the first promise completes, we need to manually clean up the unused promise.
+    await Promise.race([promiseGetTokenViaServer, promiseGetTokenViaPrompt])
+      .finally(() => {
+        cancelGetTokenViaServer();
+        cancelGetTokenViaPrompt();
+      });
+    spinner.succeed('Successfully received Amplify Studio tokens.');
   } catch (e) {
     spinner.stop();
-    context.print.error(`Failed to authenticate with Amplify Studio: ${e.message || e}`);
+    printer.error(`Failed to authenticate with Amplify Studio: ${e.message || e}`);
   }
-}
+};
