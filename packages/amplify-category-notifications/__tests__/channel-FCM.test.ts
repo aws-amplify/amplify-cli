a85993b6d4c788322a12c060ee328f0d3ff4bf6e
Sachin Panemangalore <sachinrp@amazon.com>
2022-06-22 17:49:06 -0700

    fix: channel-FCM tests

--- packages/amplify-category-notifications/__tests__/channel-FCM.test.ts
+++ packages/amplify-category-notifications/__tests__/channel-FCM.test.ts
@@ -1,39 +1,88 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable spellcheck/spell-checker */
-/* eslint-disable jest/no-conditional-expect */
-/* eslint-disable no-param-reassign */
-import { $TSContext, $TSAny } from 'amplify-cli-core';
+import {
+  $TSContext, $TSAny, AmplifyCategories, AmplifySupportedService,
+} from 'amplify-cli-core';
+import inquirer from 'inquirer';
 import * as channelFCM from '../src/channel-FCM';
-
-const inquirer = require('inquirer');
+import { ChannelAction, ChannelConfigDeploymentType, IChannelAPIResponse } from '../src/channel-types';
+import { ChannelCfg } from '../src/notifications-backend-cfg-channel-api';
 
 const channelName = 'FCM';
 
 const mockInquirer = (answers : $TSAny): $TSAny => {
-  inquirer.prompt = async (prompts:$TSAny):Promise<$TSAny> => {
-    [].concat(prompts).forEach((question:$TSAny) => {
-      if (!(question.name in answers) && typeof question.default !== 'undefined') {
-        answers[question.name] = question.default;
+  (inquirer as any).prompt = async (prompts:$TSAny):Promise<$TSAny> => {
+    [].concat(prompts).forEach(prompt => {
+      if (!((prompt as unknown as any).name in answers) && typeof (prompt as unknown as any).default !== 'undefined') {
+        // eslint-disable-next-line no-param-reassign
+        answers[(prompt as unknown as any).name] = (prompt as unknown as any).default;
       }
     });
-
     return answers;
   };
 };
 
+const mockPinpointResponseData = (status: boolean, action : ChannelAction): IChannelAPIResponse => ({
+  action,
+  channel: ChannelCfg.ChannelType.FCM,
+  deploymentType: ChannelConfigDeploymentType.INLINE,
+  output: undefined,
+  response: {
+    capability: AmplifyCategories.NOTIFICATIONS,
+    pluginName: AmplifyCategories.NOTIFICATIONS,
+    resourceProviderServiceName: AmplifySupportedService.PINPOINT,
+    status,
+    subCapability: ChannelCfg.ChannelType.FCM,
+  },
+});
+
+const mockContext = (output: $TSAny, client: $TSAny): $TSContext => ({
+  exeInfo: {
+    serviceMeta: {
+      output,
+    },
+    pinpointClient: client,
+  },
+  print: {
+    info: jest.fn(),
+    error: jest.fn(),
+  },
+}) as unknown as $TSContext;
+
+const mockContextReject = (output:$TSAny, clientReject:$TSAny):$TSAny => ({
+  exeInfo: {
+    serviceMeta: {
+      output,
+    },
+    pinpointClient: clientReject,
+  },
+  print: {
+    info: jest.fn(),
+    error: jest.fn(),
+  },
+});
+
+class NoErrorThrownError extends Error {}
+
+// wrapper to avoid conditional error checks
+const getError = async <TError>(call: () => unknown): Promise<TError> => {
+  try {
+    await call();
+    throw new NoErrorThrownError();
+  } catch (error: unknown) {
+    return error as TError;
+  }
+};
+
 describe('channel-FCM', () => {
   const mockServiceOutput : Record<string, unknown> = {};
-  const mockChannelOutput = { Enabled: true };
-  mockServiceOutput[channelName] = mockChannelOutput;
+  const mockChannelEnabledOutput = { Enabled: true };
+  mockServiceOutput[channelName] = mockChannelEnabledOutput;
 
   const mockPinpointResponseErr = new Error('channel-FCM.test.js error');
-  const mockPinpointResponseData = {
-    FCMChannelResponse: {},
-  };
 
   const mockPinpointClient = {
     updateGcmChannel: jest.fn((_, cb) => new Promise(() => {
-      cb(null, mockPinpointResponseData);
+      cb(null, (mockPinpointResponseData(true, ChannelAction.ENABLE)));
     })),
   };
 
@@ -43,94 +92,68 @@ describe('channel-FCM', () => {
     })),
   };
 
-  const mockContext = {
-    exeInfo: {
-      serviceMeta: {
-        output: mockServiceOutput,
-      },
-      pinpointClient: mockPinpointClient,
-    },
-    print: {
-      info: jest.fn(),
-      error: jest.fn(),
-    },
-  } as unknown as $TSContext;
-
-  const mockContextReject = {
-    exeInfo: {
-      serviceMeta: {
-        output: mockServiceOutput,
-      },
-      pinpointClient: mockPinpointClientReject,
-    },
-    print: {
-      info: jest.fn(),
-      error: jest.fn(),
-    },
-  };
-
   test('configure', async () => {
-    mockChannelOutput.Enabled = true;
+    mockChannelEnabledOutput.Enabled = true;
     mockInquirer({ disableChannel: true });
-
-    await channelFCM.configure(mockContext).then(() => {
+    const mockContextObj = mockContext(mockChannelEnabledOutput, mockPinpointClient);
+    await channelFCM.configure(mockContextObj).then(() => {
       expect(mockPinpointClient.updateGcmChannel).toBeCalled();
     });
 
-    mockChannelOutput.Enabled = true;
+    mockChannelEnabledOutput.Enabled = true;
     mockInquirer({ disableChannel: false });
-    await channelFCM.configure(mockContext).then(() => {
+    await channelFCM.configure(mockContext(mockChannelEnabledOutput, mockPinpointClient)).then(() => {
       expect(mockPinpointClient.updateGcmChannel).toBeCalled();
     });
 
-    mockChannelOutput.Enabled = false;
+    mockChannelEnabledOutput.Enabled = false;
     mockInquirer({ enableChannel: true });
-    await channelFCM.configure(mockContext).then(() => {
+    await channelFCM.configure(mockContext(mockChannelEnabledOutput, mockPinpointClient)).then(() => {
       expect(mockPinpointClient.updateGcmChannel).toBeCalled();
     });
   });
 
   test('enable', async () => {
     mockInquirer({ ApiKey: 'ApiKey-abc123' });
-    await channelFCM.enable(mockContext, 'successMessage').then(data => {
-      expect(mockPinpointClient.updateGcmChannel).toBeCalled();
-      expect(data).toEqual(mockPinpointResponseData);
-    });
+    const mockContextObj = mockContext(mockChannelEnabledOutput, mockPinpointClient);
+    const data = await channelFCM.enable(mockContextObj, 'successMessage');
+    expect(mockPinpointClient.updateGcmChannel).toBeCalled();
+    expect(data).toEqual(mockPinpointResponseData(true, ChannelAction.ENABLE));
   });
 
   test('enable with newline', async () => {
     mockInquirer({ ApiKey: 'ApiKey-abc123\n' });
-    await channelFCM.enable(mockContext as unknown as $TSContext, 'successMessage').then(data => {
-      expect(mockPinpointClient.updateGcmChannel).toBeCalledWith(
-        {
-          ApplicationId: undefined,
-          GCMChannelRequest: {
-            ApiKey: 'ApiKey-abc123',
-            Enabled: true,
-          },
+    const data = await channelFCM.enable(mockContext(mockChannelEnabledOutput, mockPinpointClient), 'successMessage');
+    expect(mockPinpointClient.updateGcmChannel).toBeCalledWith(
+      {
+        ApplicationId: undefined,
+        GCMChannelRequest: {
+          ApiKey: 'ApiKey-abc123',
+          Enabled: true,
         },
-        expect.anything(),
-      );
-      expect(data).toEqual(mockPinpointResponseData);
-    });
+      },
+      expect.anything(),
+    );
+    expect(data).toEqual(mockPinpointResponseData(true, ChannelAction.ENABLE));
   });
 
+  // eslint-disable-next-line jest/no-focused-tests
   test('enable unsuccessful', async () => {
     mockInquirer({ ApiKey: 'ApiKey-abc123' });
-    await channelFCM.enable(mockContextReject as unknown as $TSContext, 'successMessage').catch(err => {
-      expect(mockPinpointClient.updateGcmChannel).toBeCalled();
-      expect(err).toEqual(mockPinpointResponseErr);
-    });
+    const err: IChannelAPIResponse = await getError(async () => channelFCM.enable(mockContextReject(mockServiceOutput, mockPinpointClientReject), 'successMessage'));
+    expect(err.response.reasonMsg).toEqual(mockPinpointResponseErr.message);
+    expect(mockPinpointClient.updateGcmChannel).toBeCalled();
   });
 
   test('disable', async () => {
-    await channelFCM.disable(mockContext as unknown as $TSContext).then(data => {
-      expect(mockPinpointClient.updateGcmChannel).toBeCalled();
-      expect(data).toEqual(mockPinpointResponseData);
-    });
+    const data = await channelFCM.disable(mockContextReject(mockServiceOutput, mockPinpointClient));
+    expect(mockPinpointClient.updateGcmChannel).toBeCalled();
+    expect(data).toEqual(mockPinpointResponseData(true, ChannelAction.DISABLE));
   });
 
   test('disable unsuccessful', async () => {
-    await expect(channelFCM.disable(mockContextReject as unknown as $TSContext)).rejects.toThrow(mockPinpointResponseErr);
+    const err: IChannelAPIResponse = await getError(async () => channelFCM
+      .disable(mockContextReject(mockServiceOutput, mockPinpointClientReject)));
+    expect(err.response.reasonMsg).toEqual(mockPinpointResponseErr.message);
   });
 });
