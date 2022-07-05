343e7f276cdad0fc5dfd77dca81c0d7b2036f454
akshayupadhyay3@gmail.com <akz@88665a38169d.ant.amazon.com>
2022-07-05 10:56:48 -0700

    fix: upgrades updateNotifier package

--- packages/amplify-cli/src/version-notifier.ts
+++ packages/amplify-cli/src/version-notifier.ts
@@ -1,15 +1,20 @@
 import { JSONUtilities, $TSAny, isPackaged } from 'amplify-cli-core';
-import { default as updateNotifier } from 'update-notifier';
 import path from 'path';
 import chalk from 'chalk';
 
+import * as updateNotifier from 'update-notifier';
+
 const pkg = JSONUtilities.readJson<$TSAny>(path.join(__dirname, '..', 'package.json'));
-const notifier = updateNotifier({ pkg }); // defaults to 1 day interval
+const notifier = updateNotifier.default({ pkg }); // defaults to 1 day interval
 
 const defaultOpts: updateNotifier.NotifyOptions = {
   message: isPackaged ? `Update available:\nRun ${chalk.blueBright('amplify upgrade')} for the latest features and fixes!` : undefined,
 };
-export function notify(notifyOpts?: updateNotifier.NotifyOptions): void {
+/**
+ * notifies to upgrade pkg cli version
+ */
+export const notify = (notifyOpts?: updateNotifier.NotifyOptions): void => {
+  // eslint-disable-next-line no-param-reassign
   notifyOpts = { ...defaultOpts, ...notifyOpts };
   notifier.notify(notifyOpts);
-}
+};
