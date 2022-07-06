bb4affafe7d62c26431bc33c5ec96e2b8641432d
Danielle Adams <damsnmd@amazon.com>
2022-07-06 19:08:48 -0400

    chore: make fixes to ensure correct tag

--- scripts/tag-rc.sh
+++ scripts/tag-rc.sh
@@ -5,7 +5,7 @@ if [[ "$CIRCLE_BRANCH" != "dev" ]]; then
   exit 0
 fi
 
-rc_tag="v$(jq .version packages/amplify-cli/package.json | tr -d '"')"
+rc_tag="v$(jq -r .version packages/amplify-cli/package.json)"
 commit="$(git rev-parse HEAD~1)"
 git tag $rc_tag $commit
 git push $rc_tag
