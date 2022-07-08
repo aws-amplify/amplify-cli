1f9b0ef7a0e1d481b59e55e4f57e5fbc9d655c28
Danielle Adams <6271256+danielleadams@users.noreply.github.com>
2022-07-08 17:48:29 -0400

    chore: reset release branches (#10694)
    * chore: create a release script for promoting the rc
    
    * chore: add release commit to tag and push release branch to remote

--- scripts/github-release.ts
+++ scripts/github-release.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
  * This function expects a pre-release of 'version' to already exist
  * The 'pre-release' flag of the release is turned off (thus making it the latest release)
  */
-const publishRelease = async (version: string) => {
+const publishRelease = async (version: string, commit: string) => {
   const { id: releaseId } = await releasesRequest(join('tags', semverToGithubTag(version)));
   const releaseIdStr = (releaseId as number).toString();
   console.log('Publishing release');
@@ -13,13 +13,14 @@ const publishRelease = async (version: string) => {
     method: 'PATCH',
     body: JSON.stringify({
       prerelease: false,
+      target_commitish: commit,
     }),
   });
 };
 
 const main = async () => {
-  const { version } = getArgs();
-  await publishRelease(version);
+  const { version, commit } = getArgs();
+  await publishRelease(version, commit);
 };
 
 main();
