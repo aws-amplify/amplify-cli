c31f568237fb3fb558451cb7a489334737802e37
Edward Foyle <foyleef@amazon.com>
2022-07-12 12:38:06 -0700

    test: fix polling logic in studio e2e test (#10748)

--- packages/amplify-e2e-core/src/init/adminUI.ts
+++ packages/amplify-e2e-core/src/init/adminUI.ts
@@ -1,41 +1,24 @@
+// eslint-disable-next-line import/no-cycle
+import { retry } from '../utils';
 import { setupAmplifyAdminUI, getAmplifyBackendJobStatus } from '../utils/sdk-calls';
 
-export async function enableAdminUI(appId: string, envName: string, region: string) {
-  const setupAdminUIJobDetails = await setupAmplifyAdminUI(appId, region);
+/**
+ * Kick off Amplify backend provisioning and poll until provisioning complete (or failed)
+ */
+export const enableAdminUI = async (appId: string, __envName: string, region: string): Promise<void> => {
+  const { JobId, BackendEnvironmentName } = await setupAmplifyAdminUI(appId, region);
 
-  const jobCompletionDetails = await pollUntilDone(setupAdminUIJobDetails.JobId, appId, envName, region, 2 * 1000, 2000 * 1000);
-
-  if (jobCompletionDetails.Status === 'FAILED') {
+  try {
+    await retry(
+      () => getAmplifyBackendJobStatus(JobId, appId, BackendEnvironmentName, region),
+      jobDetails => jobDetails.Status === 'COMPLETED',
+      {
+        timeoutMS: 1000 * 60 * 5, // 5 minutes
+        stopOnError: false,
+      },
+      jobDetails => jobDetails.Status === 'FAILED',
+    );
+  } catch {
     throw new Error('Setting up Amplify Studio failed');
   }
-}
-
-// interval is how often to poll
-// timeout is how long to poll waiting for a result (0 means try forever)
-
-async function pollUntilDone(jobId: string, appId: string, envName: string, region: string, interval: number, timeout: number) {
-  const start = Date.now();
-  while (true) {
-    const jobDetails = await getAmplifyBackendJobStatus(jobId, appId, envName, region);
-
-    if (jobDetails.Status === 'FAILED' || jobDetails.Status === 'COMPLETED') {
-      // we know we're done here, return from here whatever you
-      // want the final resolved value of the promise to be
-      return jobDetails;
-    } else {
-      if (timeout !== 0 && Date.now() - start > timeout) {
-        throw new Error(`Job Timed out for ${jobId}`);
-      } else {
-        // run again with a short delay
-        await delay(interval);
-      }
-    }
-  }
-}
-
-// create a promise that resolves after a short delay
-function delay(t: number) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, t);
-  });
-}
+};
