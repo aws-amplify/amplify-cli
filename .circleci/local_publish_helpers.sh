f154b898c25f97175fae21e906f85a277a4dafd9
Josue Ruiz <7465495+SwaySway@users.noreply.github.com>
2022-07-28 12:15:12 -0700

    chore: include macos binary when running pkg local (#10809)

--- .circleci/local_publish_helpers.sh
+++ .circleci/local_publish_helpers.sh
@@ -91,7 +91,8 @@ function generatePkgCli {
   if [[ "$CIRCLE_BRANCH" == "release" ]] || [[ "$CIRCLE_BRANCH" =~ ^run-e2e-with-rc\/.* ]] || [[ "$CIRCLE_BRANCH" =~ ^release_rc\/.* ]] || [[ "$CIRCLE_BRANCH" =~ ^tagged-release ]]; then
     npx pkg -t node14-macos-x64,node14-linux-x64,node14-linux-arm64,node14-win-x64 ../build/node_modules --out-path ../out
   else
-    npx pkg -t node14-linux-x64,node14-win-x64 ../build/node_modules --out-path ../out
+    npx pkg -t node14-macos-x64,node14-linux-x64,node14-win-x64 ../build/node_modules --out-path ../out
+    mv ../out/amplify-pkg-macos ../out/amplify-pkg-macos-x64
     mv ../out/amplify-pkg-linux ../out/amplify-pkg-linux-x64
     mv ../out/amplify-pkg-win.exe ../out/amplify-pkg-win-x64.exe
   fi
