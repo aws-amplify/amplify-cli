437d39adcacfefa93ef1c4d915543b7c8ba01708
Christopher Sundersingh <sundersc@amazon.com>
2022-07-28 19:12:35 -0700

    remove ff from e2e tests

--- packages/amplify-e2e-tests/src/__tests__/schema-auth-14.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/schema-auth-14.test.ts
@@ -21,7 +21,6 @@ describe('api directives @auth batch 14', () => {
       projectDir = await createNewProjectDir('auth1');
       await initJSProjectWithProfile(projectDir, {});
       addFeatureFlag(projectDir, 'graphqlTransformer', 'useSubUsernameForDefaultIdentityClaim', true);
-      addFeatureFlag(projectDir, 'graphqlTransformer', 'showListQueryNotification', false);
     });
 
     it('auth customClaims2', async () => {
@@ -40,7 +39,6 @@ describe('api directives @auth batch 14', () => {
       projectDir = await createNewProjectDir('auth2');
       await initAndroidProjectWithProfile(projectDir, {});
       addFeatureFlag(projectDir, 'graphqlTransformer', 'useSubUsernameForDefaultIdentityClaim', true);
-      addFeatureFlag(projectDir, 'graphqlTransformer', 'showListQueryNotification', false);
     });
 
     it('auth owner9', async () => {
