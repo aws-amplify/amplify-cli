d351263e86022aeb50f98803fa577dc38b86daa6
Kethan Sai Yaram <ykethan@amazon.com>
2022-08-01 15:38:33 -0500

    added templates for Nodejs

--- packages/amplify-nodejs-function-template-provider/src/index.ts
+++ packages/amplify-nodejs-function-template-provider/src/index.ts
@@ -5,30 +5,32 @@ import { provideCrud } from './providers/crudProvider';
 import { provideServerless } from './providers/serverlessProvider';
 import { provideTrigger } from './providers/triggerProvider';
 import { provideLambdaAuth } from './providers/lambdaAuthProvider';
+import { provideAppSyncTodo } from './providers/appSyncTodoProvider';
 
-export const functionTemplateContributorFactory: FunctionTemplateContributorFactory = context => {
-  return {
-    contribute: request => {
-      switch (request.selection) {
-        case 'hello-world': {
-          return provideHelloWorld();
-        }
-        case 'crud': {
-          return provideCrud(context);
-        }
-        case 'serverless': {
-          return provideServerless();
-        }
-        case 'trigger': {
-          return provideTrigger(context);
-        }
-        case 'lambda-auth': {
-          return provideLambdaAuth();
-        }
-        default: {
-          throw new Error(`Unknown template selection [${request.selection}]`);
-        }
+export const functionTemplateContributorFactory: FunctionTemplateContributorFactory = context => ({
+  contribute: request => {
+    switch (request.selection) {
+      case 'hello-world': {
+        return provideHelloWorld();
       }
-    },
-  };
-};
+      case 'crud': {
+        return provideCrud(context);
+      }
+      case 'serverless': {
+        return provideServerless();
+      }
+      case 'trigger': {
+        return provideTrigger(context);
+      }
+      case 'lambda-auth': {
+        return provideLambdaAuth();
+      }
+      case 'appsync-todo': {
+        return provideAppSyncTodo();
+      }
+      default: {
+        throw new Error(`Unknown template selection [${request.selection}]`);
+      }
+    }
+  },
+});
