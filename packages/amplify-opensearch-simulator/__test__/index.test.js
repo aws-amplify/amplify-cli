0ce509ba71e70d178f03603ffd1ed8be37f9bf68
phani-srikar <phanisrikar93ume@gmail.com>
2022-08-27 16:34:53 -0700

    chore: add unit tests for opensearch simulator

--- packages/amplify-opensearch-simulator/__test__/index.test.js
+++ packages/amplify-opensearch-simulator/__test__/index.test.js
@@ -1,137 +1,135 @@
 const openSearchEmulator = require('../index');
 const fs = require('fs-extra');
+const { join } = require('path');
+const http = require("http");
 
-jest.mock('amplify-cli-core', () => ({
-  pathManager: {
-    getAmplifyPackageLibDirPath: jest.fn().mockReturnValue('./'),
-  },
-}));
 
 describe('emulator operations', () => {
-  const mockSearchableResourcePath = `${__dirname}/dynamodb-data/${process.pid}`;
-  // taken from dynamodb examples.
-  const dbParams = {
-    AttributeDefinitions: [
-      {
-        AttributeName: 'Artist',
-        AttributeType: 'S',
-      },
-      {
-        AttributeName: 'SongTitle',
-        AttributeType: 'S',
-      },
-    ],
-    KeySchema: [
-      {
-        AttributeName: 'Artist',
-        KeyType: 'HASH',
-      },
-      {
-        AttributeName: 'SongTitle',
-        KeyType: 'RANGE',
-      },
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 5,
-      WriteCapacityUnits: 5,
-    },
+  const mockSearchableResourcePath = join(__dirname, 'mock-api-resources', 'searchable');
+
+  const openSearchClusterOptions = {
+    clusterName: 'mock-opensearch-cluster',
+    nodeName: 'mock-opensearch-node-local',
+    port: 9600,
+    version: '1.3.0'
   };
 
-  const ensureNoDbPath = () => {
-    if (fs.existsSync(dbPath)) {
-      try {
-        fs.removeSync(dbPath);
-      }
-      catch(err) {
-        console.log(err);
-      }
-    }
+  const openSearchClusterDefaultOptions = {
+    clusterName: 'opensearch-cluster',
+    nodeName: 'opensearch-node-local',
+    port: 9200,
+    version: '1.3.0'
+  };
+
+  const ensureMockSearchableResourcePath = () => {
+    fs.ensureDirSync(mockSearchableResourcePath);
+    fs.emptyDirSync(mockSearchableResourcePath);
   };
 
   let emulators;
   beforeEach(async () => {
-    ensureNoDbPath();
+    ensureMockSearchableResourcePath();
     emulators = [];
-    jest.setTimeout(40 * 1000);
+    jest.setTimeout(5 * 60 * 1000);
   });
 
   afterEach(async () => {
     await Promise.all(emulators.map(emu => emu.terminate()));
-    ensureNoDbPath();
   });
 
-  it('should download the right version of emulator', async () => {
-    const emu = await ddbSimulator.launch();
-    emulators.push(emu);
-    const dynamo = ddbSimulator.getClient(emu);
-
-    const tables = await dynamo.listTables().promise();
-    expect(tables).toEqual({ TableNames: [] });
+  afterAll(async () => {
+    ensureMockSearchableResourcePath();
   });
 
-  it('should spin up a local opensearch cluster', async () => {
-    const emu = await ddbSimulator.launch();
-    emulators.push(emu);
-    const dynamo = ddbSimulator.getClient(emu);
-
-    const tables = await dynamo.listTables().promise();
-    expect(tables).toEqual({ TableNames: [] });
-  });
-
-  it('should spin up a local opensearch cluster', async () => {
-    const emu = await ddbSimulator.launch();
-    emulators.push(emu);
-    const dynamo = ddbSimulator.getClient(emu);
-
-    const tables = await dynamo.listTables().promise();
-    expect(tables).toEqual({ TableNames: [] });
-  });
-
-  it('should preserve state between restarts with dbPath', async () => {
-    const emuOne = await ddbSimulator.launch({ dbPath });
-    emulators.push(emuOne);
-    const dynamoOne = ddbSimulator.getClient(emuOne);
-    await dynamoOne
-      .createTable({
-        TableName: 'foo',
-        ...dbParams,
-      })
-      .promise();
-    await emuOne.terminate();
-    emulators = [];
-    const emuTwo = await ddbSimulator.launch({ dbPath });
-    emulators.push(emuTwo);
-    const dynamoTwo = await ddbSimulator.getClient(emuTwo);
-    const t = await dynamoTwo.listTables().promise();
-    expect(t).toEqual({
-      TableNames: ['foo'],
+  const fetch = async (url) => {
+    return new Promise((resolve, reject) => {
+      http
+        .get(url, (resp) => {
+          let data = "";
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
+          resp.on("end", () => {
+            resolve(data);
+          });
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
     });
-  });
+  };
 
-  it('should start on specific port', async () => {
-    const port = await require('get-port')();
-    const emu = await ddbSimulator.launch({ port });
-    emulators.push(emu);
-    expect(emu.port).toBe(port);
-  });
+  if (process.platform.startsWith('win')) {
+    it('should fail to launch on windows OS', async () => {
+        try {
+            await openSearchEmulator.launch(mockSearchableResourcePath);
+            fail('launching simulator is expected to throw but did not');
+        } catch (error) {
+            expect(error.message).toEqual('Cannot launch OpenSearch Simulator on windows OS');
+        }
+    });
+  }
+  else {
 
-  it('correctly resolves the path to local opensearch binary', async () => {
-    expect.assertions(1);
-    await expect(ddbSimulator.launch({ dbPath: 'dynamodb-data' })).rejects.toThrow('invalid directory for database creation');
-  });
+    it('should setup local instance of opensearch with default configuration', async () => {
+        const emu = await openSearchEmulator.launch(mockSearchableResourcePath);
+        emulators.push(emu);
+    
+        expect(emu.port).toEqual(openSearchClusterDefaultOptions.port);
+        expect(emu.url).toEqual(`http://localhost:${openSearchClusterDefaultOptions.port}/`);
+    
+        const openSearchInstanceDetails = JSON.parse(await fetch(emu.url));
+        expect(openSearchInstanceDetails.name).toEqual(openSearchClusterDefaultOptions.nodeName);
+        expect(openSearchInstanceDetails.cluster_name).toEqual(openSearchClusterDefaultOptions.clusterName);
+        expect(openSearchInstanceDetails.version.number).toEqual(openSearchClusterDefaultOptions.version);
+    });
 
-  it('skips downloading another opensearch binary when one is present', async () => {
-    expect.assertions(1);
-    await expect(ddbSimulator.launch({ dbPath: 'dynamodb-data' })).rejects.toThrow('invalid directory for database creation');
-  });
+    it('should setup local instance of opensearch with custom configuration', async () => {
+        const emu = await openSearchEmulator.launch(mockSearchableResourcePath, openSearchClusterOptions);
+        emulators.push(emu);
+    
+        expect(emu.port).toEqual(openSearchClusterOptions.port);
+        expect(emu.url).toEqual(`http://localhost:${openSearchClusterOptions.port}/`);
+    
+        const openSearchInstanceDetails = JSON.parse(await fetch(emu.url));
+        expect(openSearchInstanceDetails.name).toEqual(openSearchClusterOptions.nodeName);
+        expect(openSearchInstanceDetails.cluster_name).toEqual(openSearchClusterOptions.clusterName);
+        expect(openSearchInstanceDetails.version.number).toEqual(openSearchClusterOptions.version);
+    });
 
-  it('correctly generates opensearch args from given options', async () => {
-    expect.assertions(1);
-    await expect(ddbSimulator.launch({ dbPath: 'dynamodb-data' })).rejects.toThrow('invalid directory for database creation');
-  });
+    it('correctly resolves the path to local opensearch binary', async () => {
+        const relativePathFromMockSearchableResourceDir = await openSearchEmulator.getPathToOpenSearchBinary();
+        expect(relativePathFromMockSearchableResourceDir).toEqual(join('opensearchLib', 'bin', 'opensearch'));
+        
+        const fullPathToOpenSearchBinary = await openSearchEmulator.getPathToOpenSearchBinary(mockSearchableResourcePath);
+        expect(fullPathToOpenSearchBinary).toEqual(join(mockSearchableResourcePath, 'opensearchLib', 'bin', 'opensearch'));
+    });
+    
+    it('skips downloading another opensearch binary when one is locally available', async () => {
+        const openSearchExists = await openSearchEmulator.openSearchLocalExists(mockSearchableResourcePath);
+        // returns false when there is no local binary
+        expect(openSearchExists).toEqual(false);
 
-  it('throws error if max re-tries is exhausted', async () => {
-    expect.assertions(1);
-    await expect(ddbSimulator.launch({ dbPath: 'dynamodb-data' })).rejects.toThrow('invalid directory for database creation');
-  });
+        openSearchEmulator.openSearchLocalExists = jest.fn().mockReturnValueOnce(true);
+        const nodefetch = require('node-fetch');
+        jest.mock('node-fetch', ()=>jest.fn());
+        nodefetch.mockReturnValueOnce('');
+        expect(nodefetch).toBeCalledTimes(0);
+    });
+    
+    it('correctly generates opensearch args from given options', async () => {
+        const resolvedBuildArgs = openSearchEmulator.buildArgs(openSearchClusterOptions);
+        const expectedCall = `-Ecluster.name=${openSearchClusterOptions.clusterName} -Enode.name=${openSearchClusterOptions.nodeName} -Ehttp.port=${openSearchClusterOptions.port}`;
+        expect(resolvedBuildArgs.join(' ')).toEqual(expectedCall);
+    });
+    
+    it('throws error if max re-tries is breached', async () => {
+        try {
+            await openSearchEmulator.launch(mockSearchableResourcePath, {}, 5);
+            fail('launching simulator is expected to throw but did not');
+        } catch (error) {
+            expect(error.message).toEqual('Max retries hit for starting OpenSearch simulator');
+        }
+    });
+  }
 });
