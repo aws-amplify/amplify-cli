314e53871fc47c7f4085c4ddde986f56e9f55ab6
ykethan <ykethan@amazon.com>
2022-09-09 21:41:19 +0000

    added template tests and fixed template

---
+++ packages/amplify-e2e-tests/src/__tests__/function_11.test.ts
@@ -0,0 +1,190 @@
+import {
+  addFunction,
+  amplifyPush,
+  createNewProjectDir,
+  deleteProject,
+  deleteProjectDir,
+  getBackendConfig,
+  initJSProjectWithProfile,
+  getProjectMeta,
+  getFunction,
+  functionBuild,
+  invokeFunction,
+  addApiWithoutSchema,
+  updateApiSchema,
+  generateRandomShortId,
+  amplifyPushFunction,
+  addApi,
+
+} from '@aws-amplify/amplify-e2e-core';
+
+describe('Lambda AppSync nodejs:', () => {
+  let projRoot: string;
+  
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('lambda-appsync-nodejs');
+  });
+  
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
+  
+  it('API not present', async () => {
+    const projName = `iammodel${generateRandomShortId()}`;
+
+    await initJSProjectWithProfile(projRoot, { name: projName });
+    
+    let catchError;
+    
+    try{
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        additionalPermissions: {
+          permissions: ['api'],
+          choices: ['api'],
+          resources: ["Test_API"],
+          operations: ['Query'],
+        },
+      },
+      'nodejs',
+    )       
+    } catch(error){
+      catchError = error;
+    }
+    
+    expect(catchError.toString()).toBe(`Error: Process exited with non zero exit code 1`);
+  })
+  
+  it('IAM Auth not present', async () => {
+    const projName = `apikeymodel${generateRandomShortId()}`;
+
+    await initJSProjectWithProfile(projRoot, { name: projName });
+
+    await addApi(projRoot, {
+      'API key': {},
+    });
+
+    expect(getBackendConfig(projRoot)).toBeDefined();
+    const beforeMeta = getBackendConfig(projRoot);
+    const apiName = Object.keys(beforeMeta.api)[0];
+    
+    let catchError;
+    
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        expectFailure: true,
+      },
+      'nodejs',
+    )       
+    
+  })
+  
+  it('IAM as default auth', async () => {
+    const projName = `iammodel${generateRandomShortId()}`;
+
+    await initJSProjectWithProfile(projRoot, { name: projName });
+
+    await addApi(projRoot, {'IAM': {}, transformerVersion: 2 });
+    await updateApiSchema(projRoot, projName, 'iam_simple_model.graphql');
+
+    expect(getBackendConfig(projRoot)).toBeDefined();
+
+    const beforeMeta = getBackendConfig(projRoot);
+    const apiName = Object.keys(beforeMeta.api)[0];
+
+    expect(apiName).toBeDefined();
+
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        additionalPermissions: {
+          permissions: ['api'],
+          choices: ['api'],
+          resources: [apiName],
+          operations: ['Query'],
+        },
+      },
+      'nodejs',
+    );
+
+    await functionBuild(projRoot, {});
+    await amplifyPush(projRoot);
+
+    const meta = getProjectMeta(projRoot);
+    const { Arn: functionArn, Name: functionName, Region: region } = Object.keys(meta.function).map(key => meta.function[key])[0].output;
+
+    expect(functionArn).toBeDefined();
+    expect(functionName).toBeDefined();
+    expect(region).toBeDefined();
+
+    const cloudFunction = await getFunction(functionName, region);
+    expect(cloudFunction.Configuration.FunctionArn).toEqual(functionArn);
+
+    const payloadObj = { test: 'test' };
+    const fnResponse = await invokeFunction(functionName, JSON.stringify(payloadObj), region);
+
+    expect(fnResponse.StatusCode).toBe(200);
+    expect(fnResponse.Payload).toBeDefined();
+
+    const gqlResponse = JSON.parse(fnResponse.Payload as string);
+
+    expect(gqlResponse.body).toBeDefined();
+  });
+  
+    it('IAM auth as secondary auth type', async () => {
+      const projName = `iammodel${generateRandomShortId()}`;
+      await initJSProjectWithProfile(projRoot, { name: projName });
+      await addApi(projRoot, {transformerVersion: 2, 'API key': {}, IAM:{}});
+      await updateApiSchema(projRoot, projName, 'iam_simple_model.graphql');
+
+    expect(getBackendConfig(projRoot)).toBeDefined();
+
+    const beforeMeta = getBackendConfig(projRoot);
+    const apiName = Object.keys(beforeMeta.api)[0];
+
+    expect(apiName).toBeDefined();
+
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        additionalPermissions: {
+          permissions: ['api'],
+          choices: ['api'],
+          resources: [apiName],
+          operations: ['Query'],
+        },
+      },
+      'nodejs',
+    );
+
+    await functionBuild(projRoot, {});
+    await amplifyPush(projRoot);
+
+    const meta = getProjectMeta(projRoot);
+    const { Arn: functionArn, Name: functionName, Region: region } = Object.keys(meta.function).map(key => meta.function[key])[0].output;
+
+    expect(functionArn).toBeDefined();
+    expect(functionName).toBeDefined();
+    expect(region).toBeDefined();
+
+    const cloudFunction = await getFunction(functionName, region);
+    expect(cloudFunction.Configuration.FunctionArn).toEqual(functionArn);
+
+    const payloadObj = { test: 'test' };
+    const fnResponse = await invokeFunction(functionName, JSON.stringify(payloadObj), region);
+
+    expect(fnResponse.StatusCode).toBe(200);
+    expect(fnResponse.Payload).toBeDefined();
+
+    const gqlResponse = JSON.parse(fnResponse.Payload as string);
+
+    expect(gqlResponse.body).toBeDefined();
+  })
+});
--- packages/amplify-nodejs-function-template-provider/src/providers/graphqlRequestProvider.ts
+++ packages/amplify-nodejs-function-template-provider/src/providers/graphqlRequestProvider.ts
@@ -6,17 +6,18 @@ import {
 } from 'amplify-cli-core';
 import { getDstMap } from '../utils/destFileMapper';
 import { templateRoot } from '../utils/constants';
+import * as gql from 'graphql-transformer-core';
 
 const pathToTemplateFilesIAM = path.join(templateRoot, 'lambda/appsync-request');
 
 /**
- * Graphql request to an AppSync API using Node runtime Lambda function 
+ * Graphql request to an AppSync API using Node runtime Lambda function
  */
 export async function graphqlRequest(context: any): Promise<FunctionTemplateParameters> {
   const { allResources } = await context.amplify.getResourceStatus();
   const apiResource = allResources.filter((resource: { service: string }) => resource.service === AmplifySupportedService.APPSYNC);
   let apiResourceName: string;
-
+  
   if (apiResource.length > 0) {
     const resource = apiResource[0];
     apiResourceName = resource.resourceName;
@@ -24,13 +25,12 @@ export async function graphqlRequest(context: any): Promise<FunctionTemplatePara
     throw new Error(`${AmplifySupportedService.APPSYNC} API does not exist. To add an api, use "amplify add api".`);
   }
 
-  const meta = stateManager.getCurrentMeta(undefined, { throwIfNotExist: false });
-  const authConfigs = meta.api[apiResourceName].output.authConfig;
+  const authConfigs = allResources[1].output.authConfig;
 
   const additionalAuth = authConfigs.additionalAuthenticationProviders.filter((obj: { authenticationType: string; }) => obj.authenticationType === 'AWS_IAM');
 
   if (authConfigs.defaultAuthentication.authenticationType !== 'AWS_IAM' && Object.keys(additionalAuth).length === 0) {
-    context.print.error(`IAM Auth not found for ${AmplifySupportedService.APPSYNC} API. To update an api, use "amplify update api".`);
+    context.print.error(`IAM Auth not enabled for ${AmplifySupportedService.APPSYNC} API. To update an api, use "amplify update api".`);
     exitOnNextTick(0);
   }
 
