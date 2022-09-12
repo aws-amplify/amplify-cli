44e656247a46e5a3597e7679c3ea05ba04e2e717
phani-srikar <phanisrikar93ume@gmail.com>
2022-09-12 16:12:44 -0700

    test: add unit tests for searchable mocking

--- packages/amplify-util-mock/src/__tests__/api/lambda-invoke.test.ts
+++ packages/amplify-util-mock/src/__tests__/api/lambda-invoke.test.ts
@@ -1,5 +1,5 @@
-import { $TSContext } from 'amplify-cli-core';
-import { invokeLambda } from '../../api/lambda-invoke';
+import { $TSAny, $TSContext } from 'amplify-cli-core';
+import { invokeTrigger } from '../../api/lambda-invoke';
 import { ProcessedLambdaFunction } from '../../CFNParser/stack/types';
 import { loadLambdaConfig } from '../../utils/lambda/load-lambda-config';
 import { getInvoker, getBuilder } from 'amplify-category-function';
@@ -22,7 +22,6 @@ jest.mock('amplify-category-function', () => ({
 const getInvokerMock = getInvoker as jest.MockedFunction<typeof getInvoker>;
 const getBuilderMock = getBuilder as jest.MockedFunction<typeof getBuilder>;
 
-  
 jest.mock('../../func', () => ({
     timeConstrainedInvoker: jest.fn().mockResolvedValue({})
 }));
@@ -41,7 +40,7 @@ describe('Invoke local lambda function', () => {
     printer.error = jest.fn();
   });
 
-  it('invoke the local lambda with given data', async () => {
+  it('invoke the local lambda using name with given data', async () => {
     let isBuilt = false;
     getInvokerMock.mockResolvedValueOnce(() => new Promise(resolve => setTimeout(() => resolve('lambda value'), 11000)));
     getBuilderMock.mockReturnValueOnce(async () => {
@@ -49,10 +48,70 @@ describe('Invoke local lambda function', () => {
     });
     const echoInput = { key: 'value' };
     timeConstrainedInvokerMock.mockResolvedValue(echoInput);
-    await invokeLambda(mockContext, 'lambda1', echoInput);
+    await invokeTrigger(mockContext, { name: 'lambda1' }, echoInput);
     expect(loadLambdaConfigMock.mock.calls[0][1]).toEqual('lambda1');
     expect(printer.info).toBeCalledWith(JSON.stringify(echoInput, undefined, 2));
     expect(printer.error).toBeCalledTimes(0);
     expect(printer.info).toBeCalledWith('Finished execution.');
+  });
+
+  it('invoke the local lambda using trigger config with given data', async () => {
+    const mockInvoke = jest.fn().mockReturnValue({});
+    const mockBuild = jest.fn().mockReturnValue({});
+    const mockCheckDeps = jest.fn().mockReturnValue({hasRequiredDependencies: true});
+
+    mockContext['amplify'] = {
+      loadRuntimePlugin: jest.fn().mockReturnValue({
+        checkDependencies: mockCheckDeps,
+        package: jest.fn().mockReturnValue({}),
+        build: mockBuild,
+        invoke: mockInvoke
+      })
+    } as $TSAny;
+
+    let isBuilt = false;
+    getInvokerMock.mockResolvedValueOnce(() => new Promise(resolve => setTimeout(() => resolve('lambda value'), 11000)));
+    getBuilderMock.mockReturnValueOnce(async () => {
+        isBuilt = true;
+    });
+    const echoInput = { key: 'value' };
+    timeConstrainedInvokerMock.mockResolvedValue(echoInput);
+    const mockTriggerConfig = {
+      runtimePluginId: 'amplify-python-function-runtime-provider',
+      handler: 'index.handler',
+      runtime: 'python',
+      directory: 'mock-lambda-trigger',
+      reBuild: true,
+      envVars: { 'ENV_KEY': 'env_value' }
+    };
+    await invokeTrigger(mockContext, { config: mockTriggerConfig }, echoInput);
+    expect(mockContext.amplify.loadRuntimePlugin).toBeCalledTimes(2);
+    expect(mockContext.amplify.loadRuntimePlugin).toBeCalledWith(mockContext, mockTriggerConfig.runtimePluginId);
+    
+    // check for lambda dependencies
+    expect(mockCheckDeps).toBeCalledTimes(1);
+    expect(mockCheckDeps).toBeCalledWith(mockTriggerConfig.runtime);
+    
+    // ensure latest lambda changes are built
+    expect(mockBuild).toBeCalledTimes(1);
+    expect(mockBuild).toBeCalledWith({
+      buildType: 'DEV',
+      runtime: mockTriggerConfig.runtime,
+      srcRoot: mockTriggerConfig.directory
+    });
+    
+    // ensure lambda trigger is invoked with correct input
+    expect(mockInvoke).toBeCalledTimes(1);
+    expect(mockInvoke).toBeCalledWith({
+      handler: mockTriggerConfig.handler,
+      event: JSON.stringify(echoInput),
+      runtime: mockTriggerConfig.runtime,
+      srcRoot: mockTriggerConfig.directory,
+      envVars: mockTriggerConfig.envVars
+    });
+    expect(printer.info).toBeCalledWith('Finished execution.');
+    
+    // Below call is for triggers provisioned using functions category
+    expect(loadLambdaConfigMock).toBeCalledTimes(0);
   });
 });
