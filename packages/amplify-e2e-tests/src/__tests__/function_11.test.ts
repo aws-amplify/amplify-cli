d002c9077c5ee55f19d4c3fdff016c7e789a3b5a
ykethan <ykethan@amazon.com>
2022-09-18 05:18:15 +0000

    full e2e tests

--- packages/amplify-e2e-tests/src/__tests__/function_11.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/function_11.test.ts
@@ -28,46 +28,46 @@ describe('Lambda AppSync Python:', () => {
     deleteProjectDir(projRoot);
   });
 
-//   it('Test case for when API is not present', async () => {
-//     const projName = `iammodel${generateRandomShortId()}`;
+  it('Test case for when API is not present', async () => {
+    const projName = `iammodel${generateRandomShortId()}`;
 
-//     await initJSProjectWithProfile(projRoot, { name: projName });
-//     await addFunction(
-//       projRoot,
-//       {
-//         functionTemplate: 'AppSync - GraphQL API request (with IAM)',
-//         expectFailure: true,
-//         additionalPermissions: {
-//           permissions: ['api'],
-//           choices: ['api'],
-//           resources: ['Test_API'],
-//           operations: ['Query'],
-//         },
-//       },
-//       'python',
-//     );
-//   });
+    await initJSProjectWithProfile(projRoot, { name: projName });
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        expectFailure: true,
+        additionalPermissions: {
+          permissions: ['api'],
+          choices: ['api'],
+          resources: ['Test_API'],
+          operations: ['Query'],
+        },
+      },
+      'python',
+    );
+  });
 
-//   it('Test case for when IAM Auth is not present', async () => {
-//     const projName = `iammodel${generateRandomShortId()}`;
+  it('Test case for when IAM Auth is not present', async () => {
+    const projName = `iammodel${generateRandomShortId()}`;
 
-//     await initJSProjectWithProfile(projRoot, { name: projName });
+    await initJSProjectWithProfile(projRoot, { name: projName });
 
-//     await addApi(projRoot, {
-//       'API key': {},
-//     });
+    await addApi(projRoot, {
+      'API key': {},
+    });
 
-//     expect(getBackendConfig(projRoot)).toBeDefined();
+    expect(getBackendConfig(projRoot)).toBeDefined();
 
-//     await addFunction(
-//       projRoot,
-//       {
-//         functionTemplate: 'AppSync - GraphQL API request (with IAM)',
-//         expectFailure: true,
-//       },
-//       'python',
-//     );
-//   });
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        expectFailure: true,
+      },
+      'python',
+    );
+  });
 
   it('Test case when IAM is set as default auth', async () => {
     const projName = `iammodel${generateRandomShortId()}`;
@@ -75,6 +75,56 @@ describe('Lambda AppSync Python:', () => {
     await initJSProjectWithProfile(projRoot, { name: projName });
 
     await addApi(projRoot, { IAM: {}, transformerVersion: 2 });
+    await updateApiSchema(projRoot, projName, 'iam_simple_model.graphql');
+
+    expect(getBackendConfig(projRoot)).toBeDefined();
+
+    const beforeMeta = getBackendConfig(projRoot);
+    const apiName = Object.keys(beforeMeta.api)[0];
+
+    expect(apiName).toBeDefined();
+
+    await addFunction(
+      projRoot,
+      {
+        functionTemplate: 'AppSync - GraphQL API request (with IAM)',
+        additionalPermissions: {
+          permissions: ['api'],
+          choices: ['api'],
+          resources: [apiName],
+          operations: ['Query'],
+        },
+      },
+      'python',
+    );
+
+    await amplifyPush(projRoot);
+
+    const meta = getProjectMeta(projRoot);
+    const { Arn: functionArn, Name: functionName, Region: region } = Object.keys(meta.function).map(key => meta.function[key])[0].output;
+
+    expect(functionArn).toBeDefined();
+    expect(functionName).toBeDefined();
+    expect(region).toBeDefined();
+
+    const cloudFunction = await getFunction(functionName, region);
+    expect(cloudFunction.Configuration.FunctionArn).toEqual(functionArn);
+
+    const payloadObj = { test: 'test' };
+    const fnResponse = await invokeFunction(functionName, JSON.stringify(payloadObj), region);
+
+    expect(fnResponse.StatusCode).toBe(200);
+    expect(fnResponse.Payload).toBeDefined();
+
+    const gqlResponse = JSON.parse(fnResponse.Payload as string);
+
+    expect(gqlResponse.body).toBeDefined();
+  });
+
+  it('Test case for when IAM auth is set as secondary auth type', async () => {
+    const projName = `iammodel${generateRandomShortId()}`;
+    await initJSProjectWithProfile(projRoot, { name: projName });
+    await addApi(projRoot, { transformerVersion: 2, 'API key': {}, IAM: {} });
     await updateApiSchema(projRoot, projName, 'iam_simple_model.graphql');
 
     expect(getBackendConfig(projRoot)).toBeDefined();
@@ -121,55 +171,4 @@ describe('Lambda AppSync Python:', () => {
 
     expect(gqlResponse.body).toBeDefined();
   });
-
-//   it('Test case for when IAM auth is set as secondary auth type', async () => {
-//     const projName = `iammodel${generateRandomShortId()}`;
-//     await initJSProjectWithProfile(projRoot, { name: projName });
-//     await addApi(projRoot, { transformerVersion: 2, 'API key': {}, IAM: {} });
-//     await updateApiSchema(projRoot, projName, 'iam_simple_model.graphql');
-
-//     expect(getBackendConfig(projRoot)).toBeDefined();
-
-//     const beforeMeta = getBackendConfig(projRoot);
-//     const apiName = Object.keys(beforeMeta.api)[0];
-
-//     expect(apiName).toBeDefined();
-
-//     await addFunction(
-//       projRoot,
-//       {
-//         functionTemplate: 'AppSync - GraphQL API request (with IAM)',
-//         additionalPermissions: {
-//           permissions: ['api'],
-//           choices: ['api'],
-//           resources: [apiName],
-//           operations: ['Query'],
-//         },
-//       },
-//       'python',
-//     );
-
-//     await functionBuild(projRoot, {});
-//     await amplifyPush(projRoot);
-
-//     const meta = getProjectMeta(projRoot);
-//     const { Arn: functionArn, Name: functionName, Region: region } = Object.keys(meta.function).map(key => meta.function[key])[0].output;
-
-//     expect(functionArn).toBeDefined();
-//     expect(functionName).toBeDefined();
-//     expect(region).toBeDefined();
-
-//     const cloudFunction = await getFunction(functionName, region);
-//     expect(cloudFunction.Configuration.FunctionArn).toEqual(functionArn);
-
-//     const payloadObj = { test: 'test' };
-//     const fnResponse = await invokeFunction(functionName, JSON.stringify(payloadObj), region);
-
-//     expect(fnResponse.StatusCode).toBe(200);
-//     expect(fnResponse.Payload).toBeDefined();
-
-//     const gqlResponse = JSON.parse(fnResponse.Payload as string);
-
-//     expect(gqlResponse.body).toBeDefined();
-//   });
 });
