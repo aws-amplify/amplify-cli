6fedb5285f3a593e6efcaf267f716cd3bc056263
Edward Foyle <foyleef@amazon.com>
2022-09-27 13:41:40 -0700

    fix: getAllEnvs in new project returns empty array

--- packages/amplify-cli/src/extensions/amplify-helpers/get-all-envs.ts
+++ packages/amplify-cli/src/extensions/amplify-helpers/get-all-envs.ts
@@ -1,9 +1,17 @@
-import { stateManager } from 'amplify-cli-core';
+import { pathManager, stateManager } from 'amplify-cli-core';
 
 /**
  * Get all locally configured environments
  */
-export const getAllEnvs = (): string[] => Object.keys(stateManager.getLocalAWSInfo(undefined, {
-  throwIfNotExist: false,
-  default: {},
-}));
+export const getAllEnvs = (): string[] => {
+  try {
+    pathManager.getLocalAWSInfoFilePath();
+  } catch (err) {
+    // if the file doesn't exist or the project is not initialized
+    return [];
+  }
+  return Object.keys(stateManager.getLocalAWSInfo(undefined, {
+    throwIfNotExist: false,
+    default: {},
+  }));
+};
