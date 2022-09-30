e61e89e5b8a502bfa399ddc206717bfdc1ab9847
Armando Luja <aluja@amazon.com>
2022-09-29 20:37:56 -0400

    chore: split up the schema-auth-11 test

--- packages/amplify-e2e-tests/src/__tests__/schema-auth-11-b.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/schema-auth-11-b.test.ts
@@ -1,9 +1,12 @@
+/* eslint-disable spellcheck/spell-checker */
+/* eslint-disable import/no-extraneous-dependencies */
+
 import {
   initJSProjectWithProfile, deleteProject, createNewProjectDir, deleteProjectDir,
 } from '@aws-amplify/amplify-e2e-core';
 import { testSchema } from '../schema-api-directives';
 
-describe('api directives @auth batch 3', () => {
+describe('api directives @auth batch 3b', () => {
   let projectDir: string;
 
   beforeEach(async () => {
@@ -16,18 +19,9 @@ describe('api directives @auth batch 3', () => {
     deleteProjectDir(projectDir);
   });
 
-  it('auth dynamicGroup1', async () => {
-    const testresult = await testSchema(projectDir, 'auth', 'dynamicGroup1');
-    expect(testresult).toBeTruthy();
-  });
-
   it('auth dynamicGroup2', async () => {
     const testresult = await testSchema(projectDir, 'auth', 'dynamicGroup2');
     expect(testresult).toBeTruthy();
   });
 
-  it('auth dynamicGroup3', async () => {
-    const testresult = await testSchema(projectDir, 'auth', 'dynamicGroup3');
-    expect(testresult).toBeTruthy();
-  });
 });
