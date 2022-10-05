c454ae7d833397d231f896a6ddeac8224cad40c0
awsluja <110861985+awsluja@users.noreply.github.com>
2022-10-05 16:46:38 -0700

    Add parallelism to e2e suite (#11080)
    * chore: add parallelism to each test job
    
    * chore: re-organize longest running tests correctly
    
    * chore: test splitting with describe
    
    * chore: split top 4 longest running tests
    
    * chore: split auth_5 test into smaller tests
    
    * chore: split up auth migration test
    
    * chore: split api migration tests
    
    * chore: split api migration tests
    
    * chore: split global sandbox test
    
    * chore: split global sandbox test
    
    * chore: es lint fix
    
    * chore: split up geo add test
    
    * chore: linting
    
    * chore: fix test name
    
    * chore: cleanup geo tests in the split test script
    
    * chore: split env tests
    
    * chore: lint
    
    * chore: split export pull test
    
    * chore: lint
    
    * chore: split up the schema-auth-11 test
    
    * chore: lint
    
    * chore: make sure that newly split schema-auth-11 test is included
    
    * chore: split the schema-auth-9 test
    
    * chore: lint
    
    * chore: split the schema-model test
    
    * chore: fix test ordering
    
    * chore: increase job concurrency, experiment
    
    * chore: reduce timeout to reduce hang time
    
    * chore: fix bad promise chain and set timeout to 60m
    
    * build(deps): bump vm2 from 3.9.8 to 3.9.11 (#11077)
    
    Bumps [vm2](https://github.com/patriksimek/vm2) from 3.9.8 to 3.9.11.
    - [Release notes](https://github.com/patriksimek/vm2/releases)
    - [Changelog](https://github.com/patriksimek/vm2/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/patriksimek/vm2/compare/3.9.8...3.9.11)
    
    ---
    updated-dependencies:
    - dependency-name: vm2
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: Await Metadata file updates to avoid race conditions (#11074)
    
    * fix: fix bug where we are not awaiting file updates after push
    
    * chore: address memory limit on test suite
    
    * chore: migrate env to new error handling (#11053)
    
    * chore: migrate env to new error handling
    
    * chore: update to use troubleshooting link shorthand method
    
    * chore: throw amplify error on BackendDeleteFault
    
    * chore: update pr to use troubleshooting link instead
    
    * chore: fix unused import
    
    * chore: moved error message to details section
    
    * fix: updated test to expect correct error type
    
    * chore: update snapshot to match new test description
    
    * chore: rename snapshot
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ packages/amplify-e2e-tests/src/__tests__/geo-add-d.test.ts
@@ -0,0 +1,63 @@
+/* eslint-disable spellcheck/spell-checker */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import {
+  createNewProjectDir,
+  deleteProject,
+  deleteProjectDir,
+  initJSProjectWithProfile,
+  addAuthWithDefault,
+  addMapWithDefault,
+  getProjectMeta,
+  amplifyPushWithoutCodegen,
+  getMap,
+  generateRandomShortId,
+  getGeoJSConfiguration,
+} from '@aws-amplify/amplify-e2e-core';
+import { existsSync } from 'fs';
+import path from 'path';
+import { getAWSExports } from '../aws-exports/awsExports';
+
+describe('amplify geo add d', () => {
+  let projRoot: string;
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('geo-add-test');
+  });
+
+  afterEach(async () => {
+    const metaFilePath = path.join(projRoot, 'amplify', '#current-cloud-backend', 'amplify-meta.json');
+    if (existsSync(metaFilePath)) {
+      await deleteProject(projRoot);
+    }
+    deleteProjectDir(projRoot);
+  });
+
+  it('init a project with default auth config and add two map resources with the second set to default', async () => {
+    const map1Id = `map${generateRandomShortId()}`;
+    const map2Id = `map${generateRandomShortId()}`;
+    await initJSProjectWithProfile(projRoot, {});
+    await addAuthWithDefault(projRoot);
+    await addMapWithDefault(projRoot, { resourceName: map1Id, isFirstGeoResource: true });
+    await addMapWithDefault(projRoot, { resourceName: map2Id, isAdditional: true });
+    await amplifyPushWithoutCodegen(projRoot);
+
+    // check amplify meta file
+    const meta = getProjectMeta(projRoot);
+    expect(meta.geo[map1Id].isDefault).toBe(false);
+    expect(meta.geo[map2Id].isDefault).toBe(true);
+    // check if resource is provisioned in cloud
+    const region = meta.geo[map1Id].output.Region;
+    const map1Name = meta.geo[map1Id].output.Name;
+    const map2Name = meta.geo[map2Id].output.Name;
+    const map1 = await getMap(map1Name, region);
+    const map2 = await getMap(map2Name, region);
+    expect(map1.MapName).toBeDefined();
+    expect(map2.MapName).toBeDefined();
+    // check aws export file
+    const awsExport: any = getAWSExports(projRoot).default;
+    expect(getGeoJSConfiguration(awsExport).maps.items[map1Name]).toBeDefined();
+    expect(getGeoJSConfiguration(awsExport).maps.items[map2Name]).toBeDefined();
+    expect(getGeoJSConfiguration(awsExport).maps.default).toEqual(map2Name);
+    expect(getGeoJSConfiguration(awsExport).region).toEqual(region);
+  });
+});
---
+++ packages/amplify-e2e-tests/src/__tests__/global_sandbox-c.test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable spellcheck/spell-checker */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import {
+  initJSProjectWithProfile,
+  deleteProject,
+  addApiWithThreeModels,
+  apiGqlCompile,
+  amplifyPush,
+  generateModels,
+  createNewProjectDir, deleteProjectDir,
+} from '@aws-amplify/amplify-e2e-core';
+
+describe('global sandbox mode c', () => {
+  let projectDir: string;
+
+  beforeEach(async () => {
+    projectDir = await createNewProjectDir('sandbox');
+    await initJSProjectWithProfile(projectDir);
+  });
+
+  afterEach(async () => {
+    await deleteProject(projectDir);
+    deleteProjectDir(projectDir);
+  });
+
+  // TODO: need a reason why we're skipping this or we should remove this test
+  it.skip('compiles schema with three models and pushes to cloud', async () => {
+    await addApiWithThreeModels(projectDir);
+    await apiGqlCompile(projectDir, true);
+    await generateModels(projectDir);
+    await amplifyPush(projectDir, true);
+  });
+});
