a9840d58c53bcb6a6b35e0a83d39d2102627874c
Edward Foyle <foyleef@amazon.com>
2022-10-18 15:18:23 -0700

    test: add analytics test

---
+++ packages/amplify-category-analytics/src/__tests__/commands/analytics/remove.test.ts
@@ -0,0 +1,25 @@
+import { $TSContext } from 'amplify-cli-core';
+import { run as runRemove } from '../../../commands/analytics/remove';
+import { checkResourceInUseByNotifications } from '../../../plugin-client-api-notifications';
+
+jest.mock('../../../plugin-client-api-notifications');
+
+const checkResourceInUseByNotificationsMock = checkResourceInUseByNotifications as jest.MockedFunction<
+  typeof checkResourceInUseByNotifications
+>;
+
+checkResourceInUseByNotificationsMock.mockResolvedValue(true);
+
+describe('remove analytics handler', () => {
+  it('throws error if notifications exists', async () => {
+    const stubContext = ({
+      amplify: {},
+      parameters: {
+        first: 'testing',
+      },
+    } as unknown) as $TSContext;
+    await expect(() => runRemove(stubContext)).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Analytics resource testing is being used by the notifications category and cannot be removed"`,
+    );
+  });
+});
