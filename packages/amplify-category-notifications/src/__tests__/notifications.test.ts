49462669687f0eeacd854ff30bf17e00533b3273
lazpavel <85319655+lazpavel@users.noreply.github.com>
2022-10-28 14:48:24 -0400

    fix: trigger analytics migration on update/configure notifications

---
+++ packages/amplify-category-notifications/src/__tests__/notifications.test.ts
@@ -0,0 +1,37 @@
+import { $TSContext } from 'amplify-cli-core';
+import { migrationCheck } from '../migrations/index';
+import * as apiAnalyticsClient from '../plugin-client-api-analytics';
+
+const mockContext = {
+  input: { command: undefined },
+} as unknown as $TSContext;
+
+jest.mock('../plugin-client-api-analytics', () => ({ invokeAnalyticsMigrations: jest.fn() }));
+
+describe('notifications tests', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('notification migrations calls analytics migration', async () => {
+    mockContext.input.command = 'add';
+    await migrationCheck(mockContext);
+    expect(apiAnalyticsClient.invokeAnalyticsMigrations).toBeCalled();
+    jest.resetAllMocks();
+
+    mockContext.input.command = 'configure';
+    await migrationCheck(mockContext);
+    expect(apiAnalyticsClient.invokeAnalyticsMigrations).toBeCalled();
+    jest.resetAllMocks();
+
+    mockContext.input.command = 'push';
+    await migrationCheck(mockContext);
+    expect(apiAnalyticsClient.invokeAnalyticsMigrations).toBeCalled();
+    jest.resetAllMocks();
+
+    mockContext.input.command = 'remove';
+    await migrationCheck(mockContext);
+    expect(apiAnalyticsClient.invokeAnalyticsMigrations).not.toBeCalled();
+    jest.resetAllMocks();
+  });
+});
