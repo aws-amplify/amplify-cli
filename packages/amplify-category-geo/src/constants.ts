ae44fb417e74ffb2f402147286fe4c779cdb3837
Sreeram Sama <12038441+sreeramsama@users.noreply.github.com>
2022-11-04 10:32:21 -0700

    fix(geo): add missing learnmore url

--- packages/amplify-category-geo/src/constants.ts
+++ packages/amplify-category-geo/src/constants.ts
@@ -1,2 +1,4 @@
 export const category = 'geo';
 export const authCategoryName = 'auth';
+
+export const learnMoreKMSLink = 'https://docs.aws.amazon.com/location/latest/developerguide/encryption-at-rest.html';
