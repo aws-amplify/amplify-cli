734111b05f6998c5421c5947fc6e90af57f517f8
Kamil Sobol <sobkamil@amazon.com>
2022-11-07 15:33:28 -0800

    chore: add api extractor

---
+++ scripts/extract-api.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import {
+  Extractor,
+  ExtractorConfig,
+  ExtractorResult
+} from '@microsoft/api-extractor';
+
+const packagesDir = path.join(__dirname, '..', 'packages');
+
+const packages = fs.readdirSync(packagesDir)
+const configTemplatePath = path.join(__dirname, 'api-extractor.json');
+
+for (let pkg of packages) {
+  const hasTypeScript = fs.pathExistsSync(path.join(packagesDir, pkg, 'tsconfig.json'));
+  const hasEntryPoint = fs.pathExistsSync(path.join(packagesDir, pkg, 'lib', 'index.js'));
+  if (!hasTypeScript || !hasEntryPoint) {
+    console.log('Skipping ' + pkg);
+    continue;
+  } else {
+    console.log('Extracting ' + pkg)
+  }
+
+  const pkgConfigPath = path.join(packagesDir, pkg, 'api-extractor.json');
+  fs.copySync(configTemplatePath, pkgConfigPath);
+  try {
+    const extractorConfig = ExtractorConfig.loadFileAndPrepare(pkgConfigPath);
+    const extractorResult: ExtractorResult = Extractor.invoke(
+      extractorConfig,
+      {
+        localBuild: true,
+        showVerboseMessages: false
+      });
+  } finally {
+    const tmpPath = path.join(packagesDir, pkg, 'temp');
+    if (fs.pathExistsSync(tmpPath)) {
+      fs.removeSync(tmpPath);
+    }
+    fs.removeSync(pkgConfigPath);
+  }
+}
