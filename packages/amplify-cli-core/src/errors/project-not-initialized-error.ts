dbdc861d710ef1c8d04fd375b163fbf016992491
Spencer Stolworthy <sstol@amazon.com>
2022-11-09 12:42:45 -0700

    fix: change reference to Amplify Admin UI to Amplify Studio

--- packages/amplify-cli-core/src/errors/project-not-initialized-error.ts
+++ packages/amplify-cli-core/src/errors/project-not-initialized-error.ts
@@ -8,6 +8,6 @@ export const projectNotInitializedError = (): AmplifyError => new AmplifyError('
   resolution: `
 Either initialize a new Amplify project or pull an existing project.
 - "amplify init" to initialize a new Amplify project
-- "amplify pull <app-id>" to pull your existing Amplify project. Find the <app-id> in the AWS Console or Amplify Admin UI.
+- "amplify pull <app-id>" to pull your existing Amplify project. Find the <app-id> in the AWS Console or Amplify Studio.
 `,
 });
