3adf756346326088bb74f82214ee6071e392aaef
Edward Foyle <foyleef@amazon.com>
2022-11-09 08:28:56 -0800

    test: fix gitInit function

--- packages/amplify-e2e-core/src/utils/git-operations.ts
+++ packages/amplify-e2e-core/src/utils/git-operations.ts
@@ -1,10 +1,12 @@
 import execa from 'execa';
 
 /**
- * Initializes a git repo in the specified directory
+ * Initializes a git repo in the specified directory and configures a test name and email for commits
  */
 export const gitInit = async (cwd: string): Promise<void> => {
   await execa('git', ['init'], { cwd });
+  await execa('git', ['config', 'user.email', 'e2e-test@test.com'], { cwd });
+  await execa('git', ['config', 'user.name', 'E2E Test'], { cwd });
 };
 
 /**
