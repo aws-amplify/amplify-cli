92e24fcdb92f998956d36833778f8d14224aaf21
thaddmt <68032955+thaddmt@users.noreply.github.com>
2022-11-11 11:19:02 -0800

    feat(geo): Added walkthrough for Update tracker and Remove tracker (#11314)
    * chore: added update walkthrough for device tracking
    
    * chore: added e2e test for update flow
    
    * Update packages/amplify-e2e-tests/src/__tests__/geo-update-2.test.ts
    
    Co-authored-by: John Hockett <jhockett@users.noreply.github.com>
    
    * fix: remove debug statement
    
    * fix: separate geo update e2e tests
    
    * feat(geo): added remove device location tracker workflow
    
    * fix: udate geo imports in e2e tests
    
    * fix: update amplify-frontend for ios and android
    
    * fix: update delete logic for customLambda resource
    
    * fix: fix imports from pr feedback
    
    Co-authored-by: John Hockett <jhockett@users.noreply.github.com>

--- packages/amplify-e2e-tests/src/__tests__/geo-update-3.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/geo-update-3.test.ts
@@ -1,6 +1,3 @@
-/* eslint-disable spellcheck/spell-checker */
-/* eslint-disable import/no-extraneous-dependencies */
-
 import {
   createNewProjectDir,
   deleteProject,
@@ -9,20 +6,21 @@ import {
   addAuthWithDefault,
   getProjectMeta,
   amplifyPushWithoutCodegen,
-  generateRandomShortId,
+  generateResourceIdsInOrder,
   getGeoJSConfiguration,
   updateAuthAddUserGroups,
   addDeviceTrackerWithDefault,
   getDeviceLocationTracker,
+  updateDeviceTrackerWithDefault,
 } from '@aws-amplify/amplify-e2e-core';
 import { existsSync } from 'fs';
 import path from 'path';
 import { getAWSExports } from '../aws-exports/awsExports';
 
-describe('amplify geo add g', () => {
+describe('amplify geo update', () => {
   let projRoot: string;
   beforeEach(async () => {
-    projRoot = await createNewProjectDir('geo-add-test');
+    projRoot = await createNewProjectDir('geo-update-test');
   });
 
   afterEach(async () => {
@@ -33,21 +31,21 @@ describe('amplify geo add g', () => {
     deleteProjectDir(projRoot);
   });
 
-  it('init a project with default auth config and add two device location tracker resources with the second set to default', async () => {
-    const deviceTracker1Id = `deviceTracker${generateRandomShortId()}`;
-    const deviceTracker2Id = `deviceTracker${generateRandomShortId()}`;
+  it('init a project with default auth config and add two device location tracker resources and update the first to be default', async () => {
+    const [deviceTracker1Id, deviceTracker2Id] = generateResourceIdsInOrder(3);
     await initJSProjectWithProfile(projRoot, {});
     await addAuthWithDefault(projRoot);
     const cognitoGroups = ['admin', 'admin1'];
     await updateAuthAddUserGroups(projRoot, cognitoGroups);
     await addDeviceTrackerWithDefault(projRoot, cognitoGroups, { resourceName: deviceTracker1Id });
     await addDeviceTrackerWithDefault(projRoot, cognitoGroups, { resourceName: deviceTracker2Id, isAdditional: true });
+    await updateDeviceTrackerWithDefault(projRoot, cognitoGroups);
     await amplifyPushWithoutCodegen(projRoot);
 
     // check amplify meta file
     const meta = getProjectMeta(projRoot);
-    expect(meta.geo[deviceTracker1Id].isDefault).toBe(false);
-    expect(meta.geo[deviceTracker2Id].isDefault).toBe(true);
+    expect(meta.geo[deviceTracker1Id].isDefault).toBe(true);
+    expect(meta.geo[deviceTracker2Id].isDefault).toBe(false);
     // check if resource is provisioned in cloud
     const region = meta.geo[deviceTracker1Id].output.Region;
     const deviceTracker1Name = meta.geo[deviceTracker1Id].output.Name;
@@ -60,7 +58,7 @@ describe('amplify geo add g', () => {
     const awsExport: any = getAWSExports(projRoot).default;
     expect(getGeoJSConfiguration(awsExport).trackers.items).toContain(deviceTracker1Name);
     expect(getGeoJSConfiguration(awsExport).trackers.items).toContain(deviceTracker2Name);
-    expect(getGeoJSConfiguration(awsExport).trackers.default).toEqual(deviceTracker2Name);
+    expect(getGeoJSConfiguration(awsExport).trackers.default).toEqual(deviceTracker1Name);
     expect(getGeoJSConfiguration(awsExport).region).toEqual(region);
   });
 });
