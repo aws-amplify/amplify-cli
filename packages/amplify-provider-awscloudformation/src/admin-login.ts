32b37681742a03f8c54b8963292e02691374e0c1
Danielle Adams <damsnmd@amazon.com>
2022-11-15 16:26:20 -0500

    chore: replace ora with amplify spinner from prompts module

--- packages/amplify-provider-awscloudformation/src/admin-login.ts
+++ packages/amplify-provider-awscloudformation/src/admin-login.ts
@@ -1,11 +1,10 @@
-import ora from 'ora';
 import util from 'util';
 import readline from 'readline';
 import { Writable } from 'stream';
 import {
   $TSContext, AmplifyError, AMPLIFY_DOCS_URL, open,
 } from 'amplify-cli-core';
-import { printer } from 'amplify-prompts';
+import { printer, AmplifySpinner } from 'amplify-prompts';
 import { adminVerifyUrl, adminBackendMap, isAmplifyAdminApp } from './utils/admin-helpers';  // eslint-disable-line
 import { AdminLoginServer } from './utils/admin-login-server';
 import { AdminAuthPayload } from './utils/auth-types';
@@ -27,15 +26,13 @@ export const adminLoginFlow = async (context: $TSContext, appId: string, envName
   }
 
   const url = adminVerifyUrl(appId, envName, region);
-  let spinner: ora.Ora;
+  const spinner = new AmplifySpinner();
 
   await open(url, { wait: false }).then(() => {
     printer.info(`Opening link: ${url}`);
-    spinner = ora('Confirm login in the browser or manually paste in your CLI login key:\n');
-  }).catch(e => {
-    spinner = ora('Attempt to open browser failed, please manually paste in your CLI login key:\n');
-  }).finally(() => {
-    spinner.start();
+    spinner.start('Confirm login in the browser or manually paste in your CLI login key:\n');
+  }).catch(() => {
+    spinner.start('Attempt to open browser failed, please manually paste in your CLI login key:\n');
   });
 
   try {
@@ -127,7 +124,8 @@ export const adminLoginFlow = async (context: $TSContext, appId: string, envName
         cancelGetTokenViaServer();
         cancelGetTokenViaPrompt();
       });
-    spinner.succeed('Successfully received Amplify Studio tokens.');
+
+    spinner.stop('Successfully received Amplify Studio tokens.');
   } catch (e) {
     spinner.stop();
     printer.error(`Failed to authenticate with Amplify Studio: ${e?.message || e}`);
