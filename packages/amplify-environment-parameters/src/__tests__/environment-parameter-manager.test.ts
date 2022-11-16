100b46c1cc700903db79e90297d5d44e783f1108
Edward Foyle <foyleef@amazon.com>
2022-11-16 14:41:08 -0800

    chore: lint fix

--- packages/amplify-environment-parameters/src/__tests__/environment-parameter-manager.test.ts
+++ packages/amplify-environment-parameters/src/__tests__/environment-parameter-manager.test.ts
@@ -1,77 +1,77 @@
-import { pathManager, stateManager } from "amplify-cli-core";
-import { IEnvironmentParameterManager, saveAll } from "../environment-parameter-manager";
+import { pathManager, stateManager } from 'amplify-cli-core';
+import { IEnvironmentParameterManager, saveAll } from '../environment-parameter-manager';
 
-jest.mock("amplify-cli-core");
+jest.mock('amplify-cli-core');
 const stateManagerMock = stateManager as jest.Mocked<typeof stateManager>;
-const testEnv = "testEnv";
+const testEnv = 'testEnv';
 stateManagerMock.getLocalEnvInfo.mockReturnValue({ envName: testEnv });
 const stubTPI = {
   testEnv: {
     categories: {
       function: {
         funcName: {
-          envVar1: "testValue1",
-          envVar2: "testValue2"
-        }
-      }
-    }
-  }
+          envVar1: 'testValue1',
+          envVar2: 'testValue2',
+        },
+      },
+    },
+  },
 };
 stateManagerMock.getTeamProviderInfo.mockReturnValue(stubTPI);
 stateManagerMock.getBackendConfig.mockReturnValue({});
 stateManagerMock.backendConfigFileExists.mockReturnValue(true);
 
 const pathManagerMock = pathManager as jest.Mocked<typeof pathManager>;
-pathManagerMock.findProjectRoot.mockReturnValue("test/project/root");
+pathManagerMock.findProjectRoot.mockReturnValue('test/project/root');
 
 let ensureEnvParamManager: () => Promise<{ instance: IEnvironmentParameterManager }>;
 
 beforeEach(() => {
   jest.clearAllMocks();
   jest.isolateModules(async () => {
-    ({ ensureEnvParamManager } = await import("../environment-parameter-manager"));
+    ({ ensureEnvParamManager } = await import('../environment-parameter-manager'));
   });
 });
 
-describe("init", () => {
-  it("loads params and registers save on exit listener", async () => {
+describe('init', () => {
+  it('loads params and registers save on exit listener', async () => {
     await ensureEnvParamManager();
     await saveAll();
     expect(stateManagerMock.setTeamProviderInfo).toHaveBeenCalledWith(undefined, stubTPI);
   });
 });
 
-describe("save", () => {
-  it("stores resources with no params as empty object", async () => {
+describe('save', () => {
+  it('stores resources with no params as empty object', async () => {
     const envParamManager = (await ensureEnvParamManager()).instance;
-    const funcParamManager = envParamManager.getResourceParamManager("function", "funcName");
-    funcParamManager.deleteParam("envVar1");
-    funcParamManager.deleteParam("envVar2");
+    const funcParamManager = envParamManager.getResourceParamManager('function', 'funcName');
+    funcParamManager.deleteParam('envVar1');
+    funcParamManager.deleteParam('envVar2');
     envParamManager.save();
     expect(stateManagerMock.setTeamProviderInfo).toHaveBeenCalledWith(undefined, {
       testEnv: {
         categories: {
           function: {
-            funcName: {}
-          }
-        }
-      }
+            funcName: {},
+          },
+        },
+      },
     });
   });
 
-  it("does not store empty categories", async () => {
+  it('does not store empty categories', async () => {
     const envParamManager = (await ensureEnvParamManager()).instance;
-    envParamManager.removeResourceParamManager("function", "funcName");
+    envParamManager.removeResourceParamManager('function', 'funcName');
     envParamManager.save();
     expect(stateManagerMock.setTeamProviderInfo).toHaveBeenCalledWith(undefined, {
-      testEnv: {}
+      testEnv: {},
     });
   });
 
-  it("calls IParameterMapController.save if in the current environment", async () => {
+  it('calls IParameterMapController.save if in the current environment', async () => {
     const envParamManager = (await ensureEnvParamManager()).instance;
-    const resourceParamManager = envParamManager.getResourceParamManager("function", "funcName");
-    resourceParamManager.setParam("testParam", "testValue");
+    const resourceParamManager = envParamManager.getResourceParamManager('function', 'funcName');
+    resourceParamManager.setParam('testParam', 'testValue');
     envParamManager.save();
     expect(stateManagerMock.setBackendConfig.mock.calls[0][1]).toMatchInlineSnapshot(`
       Object {
