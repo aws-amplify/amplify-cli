2632687b7c269a3bc620f776b55b68ab05416936
Kamil Sobol <sobol.k.r@gmail.com>
2022-11-17 16:17:44 -0800

    Update .circleci/validate_cdk_version.ts
    Co-authored-by: John Hockett <jhockett@users.noreply.github.com>

--- .circleci/validate_cdk_version.ts
+++ .circleci/validate_cdk_version.ts
@@ -3,7 +3,7 @@ import { exec } from 'child_process';
 // @aws-cdk/core indicates CDK v1.
 exec('yarn why @aws-cdk/core', (err, stdout, stderr) => {
 
-  const cdkV1AbsenceIndicator = 'We couldn\'t find a match'
+  const cdkV1AbsenceIndicator = "We couldn't find a match";
   if (stdout.toString().includes(cdkV1AbsenceIndicator) || stderr.toString().includes(cdkV1AbsenceIndicator)) {
     console.log('Success! CDK V1 not found');
   } else {
