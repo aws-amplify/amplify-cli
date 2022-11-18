ab580e494225c04a2ec4c6187bcee42b7e2da020
Armando Luja <aluja@amazon.com>
2022-11-18 14:05:49 -0500

    chore: remove legacy logic

--- scripts/split-e2e-tests.ts
+++ scripts/split-e2e-tests.ts
@@ -130,100 +130,6 @@ const USE_PARENT_ACCOUNT = [
   'storage',
 ];
 
-// This array needs to be update periodically when new tests suites get added
-// or when a test suite changes drastically
-
-const KNOWN_SUITES_SORTED_ACCORDING_TO_RUNTIME = [
-  //<10m
-  'src/__tests__/plugin.test.ts',
-  'src/__tests__/init-special-case.test.ts',
-  'src/__tests__/datastore-modelgen.test.ts',
-  'src/__tests__/amplify-configure.test.ts',
-  'src/__tests__/tags.test.ts',
-  'src/__tests__/notifications.test.ts',
-  'src/__tests__/geo-headless.test.ts',
-  //<15m
-  'src/__tests__/schema-versioned.test.ts',
-  'src/__tests__/schema-data-access-patterns.test.ts',
-  'src/__tests__/interactions.test.ts',
-  'src/__tests__/schema-predictions.test.ts',
-  'src/__tests__/amplify-app.test.ts',
-  'src/__tests__/hosting.test.ts',
-  'src/__tests__/analytics.test.ts',
-  'src/__tests__/schema-iterative-update-2.test.ts',
-  'src/__tests__/containers-api.test.ts',
-  //<20m
-  'src/__tests__/predictions.test.ts',
-  'src/__tests__/hostingPROD.test.ts',
-  'src/__tests__/geo-update.test.ts',
-  'src/__tests__/geo-remove.test.ts',
-  'src/__tests__/geo-multi-env.test.ts',
-  //<25m
-  'src/__tests__/schema-key.test.ts',
-  'src/__tests__/function_3.test.ts',
-  'src/__tests__/schema-iterative-update-1.test.ts',
-  //<30m
-  'src/__tests__/schema-auth-3.test.ts',
-  'src/__tests__/function_2.test.ts',
-  'src/__tests__/auth_3.test.ts',
-  //<35m
-  'src/__tests__/migration/api.key.migration1.test.ts',
-  'src/__tests__/auth_4.test.ts',
-  'src/__tests__/auth_5e.test.ts',
-  'src/__tests__/schema-auth-7.test.ts',
-  'src/__tests__/api_3.test.ts',
-  'src/__tests__/import_auth_1.test.ts',
-  'src/__tests__/import_auth_2.test.ts',
-  'src/__tests__/import_s3_1.test.ts',
-  'src/__tests__/import_dynamodb_1.test.ts',
-  'src/__tests__/schema-iterative-rollback-1.test.ts',
-  //<40m
-  'src/__tests__/geo-import.test.ts',
-  'src/__tests__/schema-iterative-rollback-2.test.ts',
-  'src/__tests__/auth_2.test.ts',
-  'src/__tests__/migration/api.key.migration2.test.ts',
-  'src/__tests__/migration/api.key.migration3.test.ts',
-  'src/__tests__/function_1.test.ts',
-  'src/__tests__/function_4.test.ts',
-  //<45m
-  'src/__tests__/schema-function.test.ts',
-  'src/__tests__/migration/api.connection.migration.test.ts',
-  'src/__tests__/schema-connection.test.ts',
-  'src/__tests__/schema-iterative-update-3.test.ts',
-  'src/__tests__/schema-auth-1.test.ts',
-  //<50m
-  'src/__tests__/schema-auth-2.test.ts',
-  'src/__tests__/api_1.test.ts',
-  //<55m
-  'src/__tests__/storage.test.ts',
-  'src/__tests__/api_2.test.ts',
-  'src/__tests__/api_5.test.ts',
-  'src/__tests__/schema-iterative-update-4.test.ts',
-  'src/__tests__/schema-auth-10.test.ts',
-  //<2h
-  'src/__tests__/function_9.test.ts',
-  'src/__tests__/api_9.test.ts',
-  'src/__tests__/auth_6.test.ts',
-  'src/__tests__/delete.test.ts',
-  'src/__tests__/feature-flags.test.ts',
-  'src/__tests__/layer.test.ts',
-  'src/__tests__/schema-searchable.test.ts',
-  'src/__tests__/api_migration_update.test.ts',
-];
-
-/**
- * Sorts the test suite in ascending order. If the test is not included in known
- * tests it would be inserted at the begining o the array
- * @param tesSuites an array of test suites
- */
-function sortTestsBasedOnTime(tesSuites: string[]): string[] {
-  return tesSuites.sort((a, b) => {
-    const aIndx = KNOWN_SUITES_SORTED_ACCORDING_TO_RUNTIME.indexOf(a);
-    const bIndx = KNOWN_SUITES_SORTED_ACCORDING_TO_RUNTIME.indexOf(b);
-    return aIndx - bIndx;
-  });
-}
-
 export type WorkflowJob =
   | {
       [name: string]: {
@@ -250,7 +156,7 @@ export type CircleCIConfig = {
 const repoRoot = join(__dirname, '..');
 
 function getTestFiles(dir: string, pattern = 'src/**/*.test.ts'): string[] {
-  return sortTestsBasedOnTime(glob.sync(pattern, { cwd: dir })).reverse();
+  return glob.sync(pattern, { cwd: dir });
 }
 
 function generateJobName(baseName: string, testSuitePath: string): string {
@@ -346,7 +252,6 @@ function splitTests(
       Object.values(jobByRegion).forEach(regionJobs => {
         const newJobNames = Object.keys(regionJobs as object);
         const jobs = newJobNames.map((newJobName, index) => {
-          const requires = getRequiredJob(newJobNames, index, concurrency);
           if (typeof workflowJob === 'string') {
             return newJobName;
           } else {
@@ -355,7 +260,7 @@ function splitTests(
             return {
               [newJobName]: {
                 ...Object.values(workflowJob)[0],
-                requires: [...(requires ? [requires] : workflowJob[jobName].requires || [])],
+                requires: workflowJob[jobName].requires || [],
                 matrix: {
                   parameters: {
                     os: WINDOWS_TEST_ALLOWLIST.includes(newJobName) ? [linuxVMSize, 'w_medium'] : [linuxVMSize],
@@ -429,21 +334,6 @@ function replaceWorkflowDependency(jobs: WorkflowJob[], jobName: string, jobsToR
       [currentJobName]: jobObj,
     };
   });
-}
-
-/**
- * Helper function that creates requires block for jobs to limit the concurrency of jobs in circle ci
- * @param jobNames - An array of jobs
- * @param index - current index of the job
- * @param concurrency - number of parallel jobs allowed
- */
-function getRequiredJob(jobNames: string[], index: number, concurrency: number = 4): string | void {
-  const mod = index % concurrency;
-  const mult = Math.floor(index / concurrency);
-  if (mult > 0) {
-    const prevIndex = (mult - 1) * concurrency + mod;
-    return jobNames[prevIndex];
-  }
 }
 
 function loadConfig(): CircleCIConfig {
