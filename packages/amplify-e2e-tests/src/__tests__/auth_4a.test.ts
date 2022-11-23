d245010c6877b28faf166a28685096a299d667ce
Armando Luja <aluja@amazon.com>
2022-11-23 01:51:15 -0500

    chore: split auth-4 tests

--- packages/amplify-e2e-tests/src/__tests__/auth_4a.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/auth_4a.test.ts
@@ -82,48 +82,4 @@ describe('amplify updating auth...', () => {
     expect(updatedFunction.Configuration.Environment.Variables.MODULES).toEqual('email-filter-denylist');
     expect(lambdaFunction.Configuration.Environment.Variables.DOMAINDENYLIST).toEqual('amazon.com');
   });
-
-  it('...should init a project and add auth with a custom trigger using legacy language', async () => {
-    await initJSProjectWithProfile(projRoot, defaultsSettings);
-    addFeatureFlag(projRoot, 'auth', 'useinclusiveterminology', false);
-    await addAuthWithCustomTrigger(projRoot, { useInclusiveTerminology: false });
-    await amplifyPushAuth(projRoot);
-    const meta = getProjectMeta(projRoot);
-    const authName = Object.keys(meta.auth)[0];
-    const functionName = `${authName}PreSignup-integtest`;
-    const lambdaFunction = await getFunction(functionName, meta.providers.awscloudformation.Region);
-    const dirContents = fs.readdirSync(`${projRoot}/amplify/backend/function/${authName}PreSignup/src`);
-    expect(lambdaFunction).toBeDefined();
-    expect(lambdaFunction.Configuration.Environment.Variables.MODULES).toEqual('email-filter-denylist-legacy,custom');
-    expect(lambdaFunction.Configuration.Environment.Variables.DOMAINBLACKLIST).toEqual('amazon.com');
-    expect(dirContents.includes('email-filter-denylist-legacy.js')).toBeTruthy();
-  });
-
-  it('...should init an android project and add customAuth flag, and remove flag when custom auth triggers are removed upon update', async () => {
-    await initAndroidProjectWithProfile(projRoot, defaultsSettings);
-    await addAuthWithRecaptchaTrigger(projRoot, {});
-    await amplifyPushAuth(projRoot);
-    let meta = getAwsAndroidConfig(projRoot);
-    expect(meta.Auth.Default.authenticationFlowType).toBeDefined();
-    expect(meta.Auth.Default.authenticationFlowType).toEqual('CUSTOM_AUTH');
-    await updateAuthRemoveRecaptchaTrigger(projRoot, {});
-    await amplifyPushAuth(projRoot);
-    meta = getAwsAndroidConfig(projRoot);
-    expect(meta.Auth.Default.authenticationFlowType).toBeDefined();
-    expect(meta.Auth.Default.authenticationFlowType).toEqual('USER_SRP_AUTH');
-  });
-
-  it('...should init an ios project and add customAuth flag, and remove the flag when custom auth triggers are removed upon update', async () => {
-    await initIosProjectWithProfile(projRoot, defaultsSettings);
-    await addAuthWithRecaptchaTrigger(projRoot, {});
-    await amplifyPushAuth(projRoot);
-    let meta = getAwsIOSConfig(projRoot);
-    expect(meta.Auth.Default.authenticationFlowType).toBeDefined();
-    expect(meta.Auth.Default.authenticationFlowType).toEqual('CUSTOM_AUTH');
-    await updateAuthRemoveRecaptchaTrigger(projRoot, {});
-    await amplifyPushAuth(projRoot);
-    meta = getAwsIOSConfig(projRoot);
-    expect(meta.Auth.Default.authenticationFlowType).toBeDefined();
-    expect(meta.Auth.Default.authenticationFlowType).toEqual('USER_SRP_AUTH');
-  });
 });
