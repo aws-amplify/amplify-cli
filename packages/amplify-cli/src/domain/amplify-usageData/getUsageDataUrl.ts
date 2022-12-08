bd7db868f39fa7b2619f41a991b89203147f6477
lazpavel <85319655+lazpavel@users.noreply.github.com>
2022-12-08 10:11:46 -0500

    chore: addressed PR feedback

--- packages/amplify-cli/src/domain/amplify-usageData/getUsageDataUrl.ts
+++ packages/amplify-cli/src/domain/amplify-usageData/getUsageDataUrl.ts
@@ -1,12 +1,23 @@
 import url, { UrlWithStringQuery } from 'url';
 import { getLatestApiVersion } from './VersionManager';
 
+let parsedUrl: UrlWithStringQuery;
+
 const version = getLatestApiVersion();
 export const prodUrl = `https://api.cli.amplify.aws/${version}/metrics`;
+
 /**
  *  Usage data tracking service URL
  */
 export const getUrl = (): UrlWithStringQuery => {
+  if (!parsedUrl) {
+    parsedUrl = getParsedUrl();
+  }
+
+  return parsedUrl;
+};
+
+const getParsedUrl = (): UrlWithStringQuery => {
   if (isProduction() && !useBetaUrl()) {
     return url.parse(prodUrl);
   }
