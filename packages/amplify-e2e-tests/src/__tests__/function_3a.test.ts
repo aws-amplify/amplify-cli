c61bfb8aa9336617a845b411074ca7b00a0f1be5
Kamil Sobol <sobkamil@amazon.com>
2022-12-08 13:16:56 -0800

    fix: dotnet lambda templates compilation issues (#11562)
    * fix: fix dotnet lambda templates
    
    * fix: fix naming
    
    * fix: split function_3
    
    * fix: cover triggers
    
    * fix: fix tests
    
    * fix: fix tests
    
    * fix: add kinesis test
    
    * chore: pr feedback

--- packages/amplify-e2e-tests/src/__tests__/function_3a.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/function_3a.test.ts
@@ -105,53 +105,6 @@ describe('python function tests', () => {
   });
 });
 
-describe('dotnet function tests', () => {
-  const helloWorldSuccessObj = {
-    key1: 'VALUE1',
-    key2: 'VALUE2',
-    key3: 'VALUE3',
-  };
-  const helloWorldSuccessString = '  "key3": "VALUE3"';
-  let projRoot: string;
-  let funcName: string;
-
-  beforeEach(async () => {
-    projRoot = await createNewProjectDir('dotnet-functions');
-    await initJSProjectWithProfile(projRoot, {});
-
-    funcName = `dotnettestfn${generateRandomShortId()}`;
-
-    await addFunction(
-      projRoot,
-      {
-        name: funcName,
-        functionTemplate: 'Hello World',
-      },
-      'dotnetCore31',
-    );
-  });
-
-  afterEach(async () => {
-    await deleteProject(projRoot);
-    deleteProjectDir(projRoot);
-  });
-
-  it('add dotnet hello world function and mock locally', async () => {
-    await functionMockAssert(projRoot, {
-      funcName,
-      successString: helloWorldSuccessString,
-      eventFile: 'src/event.json',
-    }); // will throw if successString is not in output
-  });
-
-  it('add dotnet hello world function and invoke in the cloud', async () => {
-    const payload = '{"key1":"value1","key2":"value2","key3":"value3"}';
-    await amplifyPushAuth(projRoot);
-    const response = await functionCloudInvoke(projRoot, { funcName, payload });
-    expect(JSON.parse(response.Payload.toString())).toEqual(helloWorldSuccessObj);
-  });
-});
-
 describe('nodejs function tests', () => {
   const helloWorldSuccessString = 'Hello from Lambda!';
   const helloWorldSuccessObj = {
