536cee36c4a7b2c1a4b3352376b9e3e86499fc76
Spencer Stolworthy <sdstolworthy@gmail.com>
2022-12-12 12:07:21 -0800

    fix: only warn in test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const dictionary = require('./.eslint-dictionary.json');
+const dictionary = require("./.eslint-dictionary.json");
 /**
  * README if you have come here because you are sick and tired of some rule being on your case all the time:
  * If you are trying to modify a rule for normal code, see the docs for each of the lint plugins we are using in the "rules" section.
@@ -9,155 +9,151 @@ const dictionary = require('./.eslint-dictionary.json');
  */
 module.exports = {
   root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   env: {
     es6: true,
     node: true,
-    jest: true,
+    jest: true
   },
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
       arrowFunctions: true,
       modules: true,
-      module: true,
-    },
+      module: true
+    }
   },
-  plugins: ['@typescript-eslint', 'spellcheck', 'import', 'jsdoc', 'prefer-arrow'],
+  plugins: ["@typescript-eslint", "spellcheck", "import", "jsdoc", "prefer-arrow"],
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
-    'import/resolver': {
-      typescript: {},
-    },
+    "import/resolver": {
+      typescript: {}
+    }
   },
   rules: {
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/no-floating-promises": "error",
     "@typescript-eslint/no-misused-promises": "error",
     "no-bitwise": "warn",
-    'spellcheck/spell-checker': ['error', {
-      lang: 'en_US',
-      skipWords: dictionary,
-      skipIfMatch: [
-        'http://[^s]*',
-        '^[-\\w]+/[-\\w\\.]+$', //For MIME Types
-      ],
-      minLength: 4,
-    }],
-  },
-  "parserOptions": {
-    "project": [
-      "tsconfig.base.json",
-      "packages/amplify-cli/tsconfig.json"
+    "spellcheck/spell-checker": [
+      "error",
+      {
+        lang: "en_US",
+        skipWords: dictionary,
+        skipIfMatch: [
+          "http://[^s]*",
+          "^[-\\w]+/[-\\w\\.]+$" //For MIME Types
+        ],
+        minLength: 4
+      }
     ]
+  },
+  parserOptions: {
+    project: ["tsconfig.base.json", "packages/amplify-cli/tsconfig.json"]
   },
   overrides: [
     {
       // Add files to this list that shouldn't be spellchecked
-      files: ['.eslintrc.js'],
+      files: [".eslintrc.js"],
       rules: {
-        'spellcheck/spell-checker': 'off',
-      },
+        "spellcheck/spell-checker": "off"
+      }
     },
     {
       // edit rules here to modify test linting
-      files: ['__tests__/**', '*.test.ts'],
-      plugins: ['jest'],
-      extends: ['plugin:jest/recommended'],
+      files: ["__tests__/**", "*.test.ts"],
+      plugins: ["jest", "only-warn"],
+      extends: ["plugin:jest/recommended"],
       rules: {
-        '@typescript-eslint/unbound-method': 'off',
-        'jest/unbound-method': 'error',
-        '@typescript-eslint/no-explicit-any': 'off',
+        "@typescript-eslint/unbound-method": "off",
+        "jest/unbound-method": "error",
+        "@typescript-eslint/no-explicit-any": "off"
       }
     }
   ],
   // Files / paths / globs that shouldn't be linted at all
   // (note that only .js, .jsx, .ts, and .tsx files are linted in the first place)
   ignorePatterns: [
-    '**/__tests__/**',
-    '**.test.**',
-    '.eslintrc.js',
-    'scripts/',
-    'node_modules',
-    'dist',
-    'build',
-    '__mocks__',
-    'coverage',
+    "**/__tests__/**",
+    "**.test.**",
+    ".eslintrc.js",
+    "scripts/",
+    "node_modules",
+    "dist",
+    "build",
+    "__mocks__",
+    "coverage",
 
     // Forked package
-    'amplify-velocity-template',
+    "amplify-velocity-template",
 
     // Project specific excludes
-    '/cypress',
-    '/packages/amplify-appsync-simulator/public',
-    '/packages/amplify-cli/scripts/post-install.js',
+    "/cypress",
+    "/packages/amplify-appsync-simulator/public",
+    "/packages/amplify-cli/scripts/post-install.js",
 
     // Ignore project/file templates
-    'function-template-dir',
-    '/packages/graphql-predictions-transformer/lambdaFunction',
+    "function-template-dir",
+    "/packages/graphql-predictions-transformer/lambdaFunction",
 
     // Ignore override resource test files
-    '/packages/amplify-e2e-tests/overrides',
+    "/packages/amplify-e2e-tests/overrides",
 
     // Ignore lib directory of typescript packages until all packages are migrated to typescript
-    '/packages/amplify-*-function-*/lib',
-    '/packages/amplify-appsync-simulator/lib',
-    '/packages/amplify-category-api/lib',
-    '/packages/amplify-category-auth/lib',
-    '/packages/amplify-category-function/lib',
-    '/packages/amplify-category-geo/lib',
-    '/packages/amplify-category-storage/lib',
-    '/packages/amplify-cli-npm/lib',
-    '/packages/amplify-cli-core/lib',
-    '/packages/amplify-cli/lib',
-    '/packages/amplify-cli-logger/lib',
-    '/packages/amplify-e2e-core/lib',
-    '/packages/amplify-e2e-tests/lib',
-    '/packages/amplify-function-plugin-interface/lib',
-    '/packages/amplify-graphql-schema-test-library/lib',
-    '/packages/amplify-headless-interface/lib',
-    '/packages/amplify-migration-tests/lib',
-    '/packages/amplify-prompts/lib',
-    '/packages/amplify-provider-awscloudformation/lib',
-    '/packages/amplify-storage-simulator/lib',
-    '/packages/amplify-ui-tests/lib',
-    '/packages/amplify-util-headless-input/lib',
-    '/packages/amplify-util-mock/lib',
-    '/packages/graphql-*-transformer/lib',
-    '/packages/graphql-mapping-template/lib',
-    '/packages/graphql-transformer-*/lib',
-    '/packages/amplify-headless-interface/lib',
-    '/packages/amplify-util-headless-input/lib',
-    '/packages/amplify-util-uibuilder/lib',
-    '/packages/amplify-graphql-docs-generator/lib',
-    '/packages/amplify-graphql-*transformer*/lib',
-    '/packages/amplify-graphql-types-generator/lib',
-    '/packages/amplify-provider-awscloudformation/lib',
-    '/packages/amplify-console-integration-tests/lib',
-    '/packages/amplify-cli-extensibility-helper/lib',
-    '/packages/amplify-category-auth/resources/auth-custom-resource',
-    '/packages/amplify-category-custom/lib',
-    '/packages/amplify-category-custom/resources',
-    '/packages/amplify-console-hosting/lib/',
-    '/packages/amplify-container-hosting/lib/',
-    '/packages/amplify-category-predictions/lib',
-    '/packages/amplify-category-analytics/lib',
-    '/packages/amplify-category-notifications/lib',
-    '/amplify-category-interactions/lib',
-    '/packages/amplify-category-custom/src/utils/generate-cfn-from-cdk.ts',
-    '/packages/amplify-environment-parameters/lib',
-    '/packages/amplify-opensearch-simulator/lib',
+    "/packages/amplify-*-function-*/lib",
+    "/packages/amplify-appsync-simulator/lib",
+    "/packages/amplify-category-api/lib",
+    "/packages/amplify-category-auth/lib",
+    "/packages/amplify-category-function/lib",
+    "/packages/amplify-category-geo/lib",
+    "/packages/amplify-category-storage/lib",
+    "/packages/amplify-cli-npm/lib",
+    "/packages/amplify-cli-core/lib",
+    "/packages/amplify-cli/lib",
+    "/packages/amplify-cli-logger/lib",
+    "/packages/amplify-e2e-core/lib",
+    "/packages/amplify-e2e-tests/lib",
+    "/packages/amplify-function-plugin-interface/lib",
+    "/packages/amplify-graphql-schema-test-library/lib",
+    "/packages/amplify-headless-interface/lib",
+    "/packages/amplify-migration-tests/lib",
+    "/packages/amplify-prompts/lib",
+    "/packages/amplify-provider-awscloudformation/lib",
+    "/packages/amplify-storage-simulator/lib",
+    "/packages/amplify-ui-tests/lib",
+    "/packages/amplify-util-headless-input/lib",
+    "/packages/amplify-util-mock/lib",
+    "/packages/graphql-*-transformer/lib",
+    "/packages/graphql-mapping-template/lib",
+    "/packages/graphql-transformer-*/lib",
+    "/packages/amplify-headless-interface/lib",
+    "/packages/amplify-util-headless-input/lib",
+    "/packages/amplify-util-uibuilder/lib",
+    "/packages/amplify-graphql-docs-generator/lib",
+    "/packages/amplify-graphql-*transformer*/lib",
+    "/packages/amplify-graphql-types-generator/lib",
+    "/packages/amplify-provider-awscloudformation/lib",
+    "/packages/amplify-console-integration-tests/lib",
+    "/packages/amplify-cli-extensibility-helper/lib",
+    "/packages/amplify-category-auth/resources/auth-custom-resource",
+    "/packages/amplify-category-custom/lib",
+    "/packages/amplify-category-custom/resources",
+    "/packages/amplify-console-hosting/lib/",
+    "/packages/amplify-container-hosting/lib/",
+    "/packages/amplify-category-predictions/lib",
+    "/packages/amplify-category-analytics/lib",
+    "/packages/amplify-category-notifications/lib",
+    "/amplify-category-interactions/lib",
+    "/packages/amplify-category-custom/src/utils/generate-cfn-from-cdk.ts",
+    "/packages/amplify-environment-parameters/lib",
+    "/packages/amplify-opensearch-simulator/lib",
 
     // Ignore CHANGELOG.md files
-    '/packages/*/CHANGELOG.md',
-  ],
+    "/packages/*/CHANGELOG.md"
+  ]
 };
