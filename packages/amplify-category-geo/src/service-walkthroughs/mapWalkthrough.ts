dc60f7d1368b3bb4721f51f2404bf7a8f84075c0
Yasunori Kirimoto <yasunorikirimoto@gmail.com>
2022-12-20 06:20:34 +0900

    feat(category-geo): add new map style 2022.12 (#11621)
    * Map Style Add
    
    * Update API.md

--- packages/amplify-category-geo/src/service-walkthroughs/mapWalkthrough.ts
+++ packages/amplify-category-geo/src/service-walkthroughs/mapWalkthrough.ts
@@ -92,7 +92,8 @@ export const mapStyleWalkthrough = async (parameters: Partial<MapParameters>): P
         { name: 'Navigation (data provided by Esri)', value: MapStyle.VectorEsriNavigation },
         { name: 'LightGrayCanvas (data provided by Esri)', value: MapStyle.VectorEsriLightGrayCanvas },
         { name: 'DarkGrayCanvas (data provided by Esri)', value: MapStyle.VectorEsriDarkGrayCanvas },
-        { name: 'Imagery (data provided by Esri)', value: MapStyle.RasterEsriImagery }
+        { name: 'Imagery (data provided by Esri)', value: MapStyle.RasterEsriImagery },
+        { name: 'StandardLight (data provided by OpenStreetMap)', value: MapStyle.VectorOpenDataStandardLight }
     ];
     const mapStyleDefault = parameters.dataProvider && parameters.mapStyleType ?
         getGeoMapStyle(parameters.dataProvider, parameters.mapStyleType) : 'VectorEsriStreets';
