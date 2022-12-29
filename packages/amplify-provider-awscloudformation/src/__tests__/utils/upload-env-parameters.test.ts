69ad1a0b12653e0ae8ff3704176d0b2e3c755a53
John Hockett <hi@johnhockett.com>
2022-12-29 12:59:50 -0800

    fix: unit test and remove unused import

--- packages/amplify-provider-awscloudformation/src/__tests__/utils/upload-env-parameters.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/utils/upload-env-parameters.test.ts
@@ -3,19 +3,22 @@ import { SSM } from '../../aws-utils/aws-ssm';
 import { getEnvParametersUploadHandler } from '../../utils/upload-env-parameters';
 
 jest.mock('amplify-cli-core');
+jest.mock('../../aws-utils/aws-ssm');
 
 const stateManagerMock = stateManager as jest.Mocked<typeof stateManager>;
 const mockSSM = SSM as jest.Mocked<typeof SSM>;
 
 stateManagerMock.metaFileExists = jest.fn().mockReturnValueOnce(true);
 stateManagerMock.getMeta = jest.fn().mockReturnValueOnce({ 'providers': { 'awscloudformation': { 'AmplifyAppId': 'mockedAppId' } } });
-stateManagerMock.getCurrentEnvName = jest.fn().mockResolvedValueOnce('mocked');
+stateManagerMock.getCurrentEnvName = jest.fn().mockReturnValueOnce('mocked');
 
-const putParameterPromiseMock = jest.fn();
+const putParameterPromiseMock = jest.fn().mockImplementation(() => Promise.resolve());
 
 mockSSM.getInstance = jest.fn().mockResolvedValue({
-  putParameter: {
-    promise: putParameterPromiseMock,
+  client: {
+    putParameter: jest.fn().mockImplementation(() => ({
+      promise: putParameterPromiseMock,
+    })),
   },
 });
 
@@ -23,7 +26,7 @@ const contextMock = {} as unknown as $TSContext;
 
 
 describe('uploading environment parameters', () => {
-  it('returns an function and ', async () => {
+  it('returns an async function which can invoke the SSM client', async () => {
     const returnedFn = await getEnvParametersUploadHandler(contextMock);
     expect(returnedFn).toBeDefined();
     await returnedFn('key', 'value');
