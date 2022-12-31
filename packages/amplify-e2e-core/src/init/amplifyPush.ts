304c345389172d46bf93583904b73de7a098295f
Armando Luja <aluja@amazon.com>
2022-12-31 02:31:45 -0500

    chore: increase push timeout

--- packages/amplify-e2e-core/src/init/amplifyPush.ts
+++ packages/amplify-e2e-core/src/init/amplifyPush.ts
@@ -17,7 +17,7 @@
 
 import { getCLIPath, nspawn as spawn } from '..';
 
-const pushTimeoutMS = 1000 * 60 * 20; // 20 minutes;
+const pushTimeoutMS = 1000 * 60 * 45; // 45 minutes;
 
 /**
  * Data structure defined for Layer Push
