8fb8461fccf5a0c0e40eb920a90617d3fd016a27
John Hockett <hi@johnhockett.com>
2023-01-06 10:28:32 -0800

    fix: return a no-op function if an AppId cannot be resolved

--- packages/amplify-provider-awscloudformation/src/utils/upload-env-parameters.ts
+++ packages/amplify-provider-awscloudformation/src/utils/upload-env-parameters.ts
@@ -1,4 +1,5 @@
 import { $TSContext, AmplifyFault, stateManager } from 'amplify-cli-core';
+import { printer } from 'amplify-prompts';
 import type { SSM as SSMType } from 'aws-sdk';
 import { SSM } from '../aws-utils/aws-ssm';
 import { resolveAppId } from './resolve-appId';
@@ -7,7 +8,13 @@ import { resolveAppId } from './resolve-appId';
  * Higher order function for uploading CloudFormation parameters to the service
  */
 export const getEnvParametersUploadHandler = async (context: $TSContext): Promise<((key: string, value: string) => Promise<void>)> => {
-  const appId = resolveAppId(context);
+  let appId: string;
+  try {
+    appId = resolveAppId(context)
+  } catch {
+    printer.warn('Failed to resolve AppId, skipping parameter upload');
+    return async (key, value) => { /* no-op */ };
+  }
   const envName = stateManager.getCurrentEnvName();
   const { client } = await SSM.getInstance(context);
   return uploadParameterToParameterStore(appId, envName, client);
