07ffabbf2ade66e4507f4a760bb9c378b17016f0
Zachary Goldberg <goldbez@amazon.com>
2023-01-10 16:32:35 -0800

    feat: update amplify delete to also delete all PS values for the project (improving tests)

--- packages/amplify-provider-awscloudformation/src/__tests__/utils/delete-from-parameter-store.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/utils/delete-from-parameter-store.test.ts
@@ -1,33 +1,33 @@
-import { $TSContext } from 'amplify-cli-core';
+import { $TSContext, AmplifyFault } from 'amplify-cli-core';
 import { getEnvParametersDeleteHandler } from '../../utils/delete-from-service';
 import { SSM } from '../../aws-utils/aws-ssm';
 import { getSsmSdkParametersDeleteMultiKeys } from '../../utils/get-ssm-sdk-parameters';
 
 jest.mock('../../aws-utils/aws-ssm');
-const mockSSM = SSM as jest.Mocked<typeof SSM>;
-const deleteParametersPromiseMock = jest.fn().mockImplementation(() => Promise.resolve());
-const deleteParameters = jest.fn().mockImplementation(() => ({ promise: deleteParametersPromiseMock }));
 
-mockSSM.getInstance = jest.fn().mockResolvedValue({
-  client: {
-    deleteParameters: deleteParameters,
-  },
-});
 const fakeAppId = 'fakeAppId';
 const keys: Array<string> = ['one', 'two'];
 const envName: string = 'dev';
+const contextStub = {
+  exeInfo: {
+    inputParams: {
+      amplify: {
+        appId: fakeAppId,
+      },
+    },
+  },
+};
 
 describe('parameters-delete-handler', () => {
-  it('check if returned function is defined', async () => {
-    const contextStub = {
-      exeInfo: {
-        inputParams: {
-          amplify: {
-            appId: fakeAppId,
-          },
-        },
+  it('check if returned function is called once with correct paramater', async () => {
+    const deleteParametersPromiseMock = jest.fn().mockImplementation(() => Promise.resolve());
+    const deleteParameters = jest.fn().mockImplementation(() => ({ promise: deleteParametersPromiseMock }));
+    const mockSSM = SSM as jest.Mocked<typeof SSM>;
+    mockSSM.getInstance = jest.fn().mockResolvedValue({
+      client: {
+        deleteParameters: deleteParameters,
       },
-    };
+    });
 
     const deleteParametersFromService = await getEnvParametersDeleteHandler((contextStub as unknown) as $TSContext, envName);
     await deleteParametersFromService(keys);
