69d109ae9fe15598bc3b60e79e9c7910d70fd659
Zachary Goldberg <goldbez@amazon.com>
2023-01-11 16:34:43 -0800

    feat: update amplify delete to also delete all PS values for the project (chunking)

--- packages/amplify-provider-awscloudformation/src/utils/delete-from-service.ts
+++ packages/amplify-provider-awscloudformation/src/utils/delete-from-service.ts
@@ -20,8 +20,10 @@ const deleteParametersFromParameterStore = (
 ): ((keys: Array<string>) => Promise<void>) => {
   return async (keys: Array<string>): Promise<void> => {
     try {
-      const sdkParameters = getSsmSdkParametersDeleteMultiKeys(appId, envName, keys);
-      await ssmClient.deleteParameters(sdkParameters).promise();
+      const chunkedKeys = chunkForParameterStore(keys);
+      await Promise.all(
+        chunkedKeys.map(chunk => ssmClient.deleteParameters(getSsmSdkParametersDeleteMultiKeys(appId, envName, chunk)).promise())
+      );
     } catch (e) {
       throw new AmplifyFault(
         'ParametersDeleteFault',
@@ -33,3 +35,19 @@ const deleteParametersFromParameterStore = (
     }
   };
 };
+
+// SSMS deleteParameters can only take up to 10 paramaters
+function chunkForParameterStore(keys: Array<string>) {
+  const maxLength = 10;
+  const chunkedKeys: Array<Array<string>> = [];
+  let lastChunk: Array<string> = [];
+  chunkedKeys.push(lastChunk);
+  keys.forEach(key => {
+    if (lastChunk.length == maxLength) {
+      lastChunk = [];
+      chunkedKeys.push(lastChunk);
+    }
+    lastChunk.push(key);
+  });
+  return chunkedKeys;
+}
