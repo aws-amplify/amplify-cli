87df9ef7f00ea052c7538374eab2b5fe73743f1a
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-01-18 14:53:30 -0800

    fix: remove unused import

--- scripts/coverage.ts
+++ scripts/coverage.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs';
-import { execSync } from 'child_process';
 import * as path from 'path';
 import * as istanbul from 'istanbul-lib-coverage';
 import * as libReport from 'istanbul-lib-report';
