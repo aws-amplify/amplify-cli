e11baf9fd76fc33ffe883df2da1fd6265543edff
Zachary Goldberg <goldbez@amazon.com>
2023-02-01 14:23:40 -0800

    feat: add option to clone env params from src env (updating unit test)

--- packages/amplify-provider-awscloudformation/src/__tests__/utils/clone-env-param-manager.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/utils/clone-env-param-manager.test.ts
@@ -6,20 +6,19 @@ jest.mock('@aws-amplify/amplify-environment-parameters');
 
 describe('clone env params test', () => {
   const mockEnvParamManagerCloneFn = jest.fn().mockReturnValue(Promise.resolve());
+  const envPeramManagerA = {
+    instance: {
+      cloneEnvParamsToNewEnvParamManager: mockEnvParamManagerCloneFn,
+      init: jest.fn(),
+      removeResourceParamManager: jest.fn(),
+      hasResourceParamManager: jest.fn(),
+      getResourceParamManagerResourceKeys: jest.fn(),
+      getResourceParamManager: jest.fn(),
+      save: jest.fn(),
+    } as IEnvironmentParameterManager,
+  };
 
-  jest.spyOn(amplifyEnvironmentParameters, 'ensureEnvParamManager').mockReturnValue(
-    Promise.resolve({
-      instance: {
-        cloneEnvParamsToNewEnvParamManager: mockEnvParamManagerCloneFn,
-        init: jest.fn(),
-        removeResourceParamManager: jest.fn(),
-        hasResourceParamManager: jest.fn(),
-        getResourceParamManagerResourceKeys: jest.fn(),
-        getResourceParamManager: jest.fn(),
-        save: jest.fn(),
-      } as IEnvironmentParameterManager,
-    }),
-  );
+  jest.spyOn(amplifyEnvironmentParameters, 'ensureEnvParamManager').mockReturnValue(Promise.resolve(envPeramManagerA));
 
   it('check if func is called', async () => {
     await cloneEnvParamManager('envA', 'envB');
