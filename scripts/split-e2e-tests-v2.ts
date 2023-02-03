a9c6e0724299e44e52269eca1893c01bf3d70520
Kamil Sobol <sobol.k.r@gmail.com>
2023-02-03 11:05:15 -0800

    chore: include v10 migration tests in solo runs

--- scripts/split-e2e-tests-v2.ts
+++ scripts/split-e2e-tests-v2.ts
@@ -238,7 +238,7 @@ export const splitTestsV2 = function splitTests(
       const US_WEST_2 = FORCE_US_WEST_2.find(t => test.startsWith(t));
       const USE_PARENT = USE_PARENT_ACCOUNT.some(usesParent => test.startsWith(usesParent));
 
-      const isMigrationTest = test.includes("/migration_tests/");
+      const isMigrationTest = test.includes("/migration_tests");
       if (isMigrationTest || RUN_SOLO.find(solo => test === solo)) {
         const newSoloJob = createRandomJob(os);
         newSoloJob.tests.push(test);
