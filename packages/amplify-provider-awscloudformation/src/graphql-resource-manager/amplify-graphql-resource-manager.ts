91e45888c13793ee764c271fada0e41d690efc9d
Zeyu Li <lizeyu@amazon.com>
2023-02-08 11:05:47 -0800

    fix git bot alert

--- packages/amplify-provider-awscloudformation/src/graphql-resource-manager/amplify-graphql-resource-manager.ts
+++ packages/amplify-provider-awscloudformation/src/graphql-resource-manager/amplify-graphql-resource-manager.ts
@@ -457,7 +457,7 @@ export class GraphQLResourceManager {
     template.Resources = {};
     template.Resources[OpenSearchDomainLogicalID] = searchDomain;
     template.Outputs = {};
-  }
+  };
 
   private clearTemplateState = (stackName: string) => {
     while (this.templateState.has(stackName)) {
