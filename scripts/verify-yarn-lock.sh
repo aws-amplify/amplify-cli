432176756c6d68baf83174724817b374524368ef
Kamil Sobol <sobkamil@amazon.com>
2023-02-08 08:51:05 -0800

    chore: be more verbose

--- scripts/verify-yarn-lock.sh
+++ scripts/verify-yarn-lock.sh
@@ -5,6 +5,8 @@ yarn install
 yarn_lock_changed=$(git status | grep -F yarn.lock | wc -l)
 
 if [[ yarn_lock_changed -gt 0 ]]; then
-  echo "Detected change in yarn.lock file. Please run 'yarn install' and add yarn.lock file changes to the change set."
+  echo "Fail! Detected change in yarn.lock file. Please run 'yarn install' and add yarn.lock file changes to the change set."
   exit 1;
+else
+  echo "Success! No drift detected in yarn.lock file."
 fi
