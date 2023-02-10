76a161def403343f417b5bb8bcdfd462eeecac9f
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-02-10 10:15:27 -0800

    fix: unused import

--- scripts/extract-formatting-changes.ts
+++ scripts/extract-formatting-changes.ts
@@ -1,4 +1,4 @@
-import { exec, execSync, spawn } from 'child_process';
+import { exec, execSync } from 'child_process';
 import yargs from 'yargs';
 import * as fs from 'fs';
 import * as path from 'path';
