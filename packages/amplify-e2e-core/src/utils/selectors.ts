a2bb2f27b623ff5cd59643d8092f7a68e73ef123
Kamil Sobol <sobkamil@amazon.com>
2023-02-14 14:46:57 -0800

    spike: multi select testing pt2.

--- packages/amplify-e2e-core/src/utils/selectors.ts
+++ packages/amplify-e2e-core/src/utils/selectors.ts
@@ -30,9 +30,11 @@ export const multiSelect = <T>(chain: ExecutionContext, items: T[] = [], allChoi
     // calculate the diff with the latest, since items are sorted, always positive
     // represents the numbers of moves down we need to make to selection
     .reduce((diffs, move) => (diffs.length > 0 ? [...diffs, move - diffs[diffs.length - 1]] : [move]), [] as number[])
-    .reduce((chain, move) => moveDown(chain, move).send(' '), chain);
+    .reduce((chain, move) => moveDown(chain, move), chain);
 
-  // This seems to be a key, we have to wait until enquirer flushes stdout before sending carriage return.
+  // Looks that we have to synchronously wait for transitions in other process and not sent space prematurely.
+  chain.wait('❯○ storage');
+  chain.send(' ');
   chain.wait('❯● storage');
   chain.sendCarriageReturn();
   return chain;
