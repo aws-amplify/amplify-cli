71ccfe805ad716a9f917155dbd6c8ac0232586d9
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-02-20 14:54:57 -0800

    fix: add barrel file to ./mixins

---
+++ packages/amplify-testing/src/mixins/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @see https://www.typescriptlang.org/docs/handbook/mixins.html
+ */
+export type Constructor<T = object> = new (...args: any[]) => T;
+export type MixinResult<MixinInterface, TBase> = {
+  new (...args: any[]): MixinInterface;
+  prototype: MixinInterface;
+} & TBase;
+export { default as WithStorage, StorageMixin } from './storage';
