84800dcac1aa584c869e31d2ee252262a93e2185
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-02-20 14:39:01 -0800

    feat: add amplify testing package

---
+++ packages/amplify-testing/src/amplify_tester.ts
@@ -0,0 +1,29 @@
+export type TestResult<T> = {
+  data: T;
+  inputs: Record<string, unknown>;
+  outputs: Record<string, unknown>;
+};
+
+export type AmplifyTesterOptions = Record<string, unknown>;
+export type AmplifyTestConfiguration = Record<string, unknown>;
+export type TestParameterCreator = (options: AmplifyTesterOptions) => TestParameters;
+export type TestParameters = Record<string, unknown>;
+export type TestResultProcessor = (result: Record<string, unknown>) => Record<string, unknown>;
+
+export class AmplifyTester {
+  private resultProcessors: Array<TestResultProcessor> = [];
+  protected addResultProcessor = (processor: TestResultProcessor) => {
+    this.resultProcessors.push(processor);
+  };
+  private testParameterCreators: Array<TestParameterCreator> = [];
+  protected addTestParameterCreator = (creator: TestParameterCreator) => {
+    this.testParameterCreators.push(creator);
+  };
+
+  public runTest = async <T>(runner: (parameters: TestParameters) => Promise<T>): Promise<TestResult<T>> => {
+    const parameters = this.testParameterCreators.reduce((parameters, creator) => ({ ...parameters, ...creator(parameters) }), {});
+    const data = await runner(parameters);
+    const outputs = this.resultProcessors.reduce((result, processor) => ({ ...result, ...processor(result) }), {});
+    return { outputs, data, inputs: parameters };
+  };
+}
---
+++ packages/amplify-testing/src/index.ts
@@ -0,0 +1,6 @@
+import { AmplifyTester } from './amplify_tester';
+
+export function createAmplifyTest() {
+  const tester = new AmplifyTester();
+  return tester;
+}
