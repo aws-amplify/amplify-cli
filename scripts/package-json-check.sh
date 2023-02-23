89fa6c63b6b558a46f6583d61a6c0fe80ec16c6d
Eddy Varela <eddvar@amazon.com>
2023-02-23 13:18:20 -0500

    chore: improving error messages and sorting packages

--- scripts/package-json-check.sh
+++ scripts/package-json-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -z "git diff --diff-filter=MRA --name-only | grep package.json" ]; then
   yarn sort-packages
-  echo "changes made to package.json files, please stage these changes before pushing"
+  echo "Changes made to package.json files. Please stage these changes before pushing."
 fi
