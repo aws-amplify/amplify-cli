0f555f9d02bb85ef408db8c73e40a501b59728ef
Eddy Varela <eddvar@amazon.com>
2023-02-23 13:16:56 -0500

    chore: improving reporting

--- scripts/package-json-check.sh
+++ scripts/package-json-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -z "git diff --diff-filter=MRA --name-only | grep package.json" ]; then
-  echo "changes made to package.json files"
   yarn sort-packages
+  echo "changes made to package.json files, please stage these changes before pushing"
 fi
