ae338b15e2882fe6a6dd5cd9e0577c6056ecf372
Kamil Sobol <sobkamil@amazon.com>
2023-02-24 09:26:16 -0800

    chore: revert "chore: do other violation"
    This reverts commit a5722e1e7e87ffe189eb9ff323311499e89b6a58.

--- packages/amplify-category-geo/src/service-utils/geofenceCollectionParams.ts
+++ packages/amplify-category-geo/src/service-utils/geofenceCollectionParams.ts
@@ -1,7 +1,5 @@
 import { ResourceParameters } from './resourceParams';
 import _ from 'lodash';
-import os from 'os';
-import fs from 'fs-extra';
 
 /**
  * Data structure that represents a Geofence Collection Resource
@@ -25,6 +23,5 @@ export const convertToCompleteGeofenceCollectionParams = (partial: Partial<Geofe
   if (isCompleteGeofenceCollectionParams(partial)) {
     return partial as GeofenceCollectionParameters;
   }
-  fs.writeFileSync(os.tmpdir() + '/foo.txt', 'foo');
   throw new Error('Partial<GeofenceCollectionParameters> does not satisfy GeofenceCollectionParameters');
 };
