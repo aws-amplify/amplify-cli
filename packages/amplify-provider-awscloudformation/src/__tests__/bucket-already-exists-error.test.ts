b1e41c0e0b768aa76d89b4506bcf908f0f68c7a4
Zachary Goldberg <goldbez@amazon.com>
2023-02-27 15:58:12 -0800

    fix: move error logic to handleCloudFormationError (add tests)

--- packages/amplify-provider-awscloudformation/src/__tests__/bucket-already-exists-error.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/bucket-already-exists-error.test.ts
@@ -2,17 +2,23 @@ import { handleCloudFormationError } from '../handle-cloud-formation-error';
 import { AmplifyException } from 'amplify-cli-core';
 
 describe('bucket already exists error', () => {
+  const bucketName = 'bucket-name';
   const mockError = {
     name: 'DeploymentFault',
     message: 'Resource is not in the state stackUpdateComplete',
-    details: 'Name: S3Bucket (AWS::S3::Bucket), Event Type: create, Reason: bucket-name already exists',
+    details: `Name: S3Bucket (AWS::S3::Bucket), Event Type: create, Reason: ${bucketName} already exists`,
   };
 
   it('bucket already exists error contents', async () => {
-    const testThrowError = () => {
+    expect(() => {
       handleCloudFormationError(mockError);
-    };
-    expect(testThrowError).toThrow('The S3 bucket bucket-name already exists.');
+    }).toThrowError(
+      expect.objectContaining({
+        classification: 'ERROR',
+        name: 'ResourceAlreadyExistsError',
+        message: `The S3 bucket ${bucketName} already exists.`,
+      }),
+    );
   });
 
   it('bucket already exists error type', async () => {
