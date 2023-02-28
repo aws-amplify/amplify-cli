ccfacad5e199b6094563a44deb7b8918bf65b6ac
Zachary Goldberg <goldbez@amazon.com>
2023-02-28 12:18:15 -0800

    fix: account for multi line details

--- packages/amplify-provider-awscloudformation/src/__tests__/bucket-already-exists-error.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/bucket-already-exists-error.test.ts
@@ -9,6 +9,12 @@ describe('bucket already exists error', () => {
     details: `Name: S3Bucket (AWS::S3::Bucket), Event Type: create, Reason: ${bucketName} already exists`,
   };
 
+  const mockErrorMultiLineDetails = {
+    name: 'DeploymentFault',
+    message: 'Resource is not in the state stackUpdateComplete',
+    details: `Name: <stackName> (AWS::CloudFormation::Stack), Event Type: update, Reason: Parameters: [a1] must have values\nName: S3Bucket (AWS::S3::Bucket), Event Type: create, Reason: ${bucketName} already exists`,
+  };
+
   it('bucket already exists error contents', async () => {
     expect(() => {
       handleCloudFormationError(mockError);
@@ -24,6 +30,26 @@ describe('bucket already exists error', () => {
   it('bucket already exists error type', async () => {
     try {
       handleCloudFormationError(mockError);
+    } catch (error) {
+      expect(error).toBeInstanceOf(AmplifyException);
+    }
+  });
+
+  it('bucket already exists error contents multi-line details', async () => {
+    expect(() => {
+      handleCloudFormationError(mockErrorMultiLineDetails);
+    }).toThrowError(
+      expect.objectContaining({
+        classification: 'ERROR',
+        name: 'ResourceAlreadyExistsError',
+        message: `The S3 bucket ${bucketName} already exists.`,
+      }),
+    );
+  });
+
+  it('bucket already exists error type multi-line details', async () => {
+    try {
+      handleCloudFormationError(mockErrorMultiLineDetails);
     } catch (error) {
       expect(error).toBeInstanceOf(AmplifyException);
     }
--- packages/amplify-provider-awscloudformation/src/cloud-formation-error-handler.ts
+++ packages/amplify-provider-awscloudformation/src/cloud-formation-error-handler.ts
@@ -1,12 +1,17 @@
 import { $TSAny, AmplifyError } from 'amplify-cli-core';
 
+const s3Indicator = '(AWS::S3::Bucket)';
+
 const handleS3Error = (err: Error & { details?: string }): void => {
   const alreadyExistsSuffix = 'already exists';
-  if (err?.details?.includes(alreadyExistsSuffix)) {
+  const errorDetailLines = err.details.split('\n');
+  const bucketExistsLines = errorDetailLines.filter((line) => line.includes(s3Indicator) && line.includes(alreadyExistsSuffix));
+  if (bucketExistsLines.length) {
+    const lineWithError = bucketExistsLines[0];
     const prefix = `Name: ${
-      err.details.includes('CustomMessageConfirmationBucket') ? 'CustomMessageConfirmationBucket' : 'S3Bucket'
+      lineWithError.includes('CustomMessageConfirmationBucket') ? 'CustomMessageConfirmationBucket' : 'S3Bucket'
     } (AWS::S3::Bucket), Event Type: create, Reason:`;
-    const bucketName = err.details.slice(prefix.length + 1, err.details.length - alreadyExistsSuffix.length - 1);
+    const bucketName = lineWithError.slice(prefix.length + 1, lineWithError.length - alreadyExistsSuffix.length - 1);
     throw new AmplifyError('ResourceAlreadyExistsError', {
       message: `The S3 bucket ${bucketName} already exists.`,
       resolution: `Please delete this bucket in the AWS S3 console and try again. The bucket can be found at: https://s3.console.aws.amazon.com/s3/buckets/${bucketName}.`,
@@ -31,7 +36,7 @@ export const handleCloudFormationError = (err: Error & { details?: string }): vo
     );
   }
 
-  if (err?.details?.includes('(AWS::S3::Bucket)')) {
+  if (err?.details?.includes(s3Indicator)) {
     handleS3Error(err);
   }
 
