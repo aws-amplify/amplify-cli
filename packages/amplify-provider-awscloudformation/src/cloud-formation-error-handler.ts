371eb7ab0767a17b396a84e4d65e634794165938
Zachary Goldberg <goldbez@amazon.com>
2023-02-28 11:35:55 -0800

    fix: move error logic to seperate function

--- packages/amplify-provider-awscloudformation/src/cloud-formation-error-handler.ts
+++ packages/amplify-provider-awscloudformation/src/cloud-formation-error-handler.ts
@@ -1,5 +1,19 @@
 import { $TSAny, AmplifyError } from 'amplify-cli-core';
 
+const handleS3Error = (err: Error & { details?: string }): void => {
+  const alreadyExistsSuffix = 'already exists';
+  if (err?.details?.includes(alreadyExistsSuffix)) {
+    const prefix = `Name: ${
+      err.details.includes('CustomMessageConfirmationBucket') ? 'CustomMessageConfirmationBucket' : 'S3Bucket'
+    } (AWS::S3::Bucket), Event Type: create, Reason:`;
+    const bucketName = err.details.slice(prefix.length + 1, err.details.length - alreadyExistsSuffix.length - 1);
+    throw new AmplifyError('ResourceAlreadyExistsError', {
+      message: `The S3 bucket ${bucketName} already exists.`,
+      resolution: `Please delete this bucket in the AWS S3 console and try again. The bucket can be found at: https://s3.console.aws.amazon.com/s3/buckets/${bucketName}.`,
+    });
+  }
+};
+
 export const handleCloudFormationError = (err: Error & { details?: string }): void => {
   if (err?.name === 'ValidationError' && err?.message === 'No updates are to be performed.') {
     return;
@@ -18,16 +32,8 @@ export const handleCloudFormationError = (err: Error & { details?: string }): vo
   }
 
   const s3Indicator = '(AWS::S3::Bucket)';
-  const alreadyExistsSuffix = 'already exists';
-  if (err?.details?.includes(s3Indicator) && err?.details?.includes(alreadyExistsSuffix)) {
-    const prefix = `Name: ${
-      err.details.includes('CustomMessageConfirmationBucket') ? 'CustomMessageConfirmationBucket' : 'S3Bucket'
-    } (AWS::S3::Bucket), Event Type: create, Reason:`;
-    const bucketName = err.details.slice(prefix.length + 1, err.details.length - alreadyExistsSuffix.length - 1);
-    throw new AmplifyError('ResourceAlreadyExistsError', {
-      message: `The S3 bucket ${bucketName} already exists.`,
-      resolution: `Please delete this bucket in the AWS S3 console and try again. The bucket can be found at: https://s3.console.aws.amazon.com/s3/buckets/${bucketName}.`,
-    });
+  if (err?.details?.includes(s3Indicator)) {
+    handleS3Error(err);
   }
 
   throw err;
