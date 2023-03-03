f1b97fcd005a4e05df33076a4cb406a2aff9663a
Kamil Sobol <sobol.k.r@gmail.com>
2023-03-02 17:21:44 -0800

    Update split-e2e-tests-v2.ts

--- scripts/split-e2e-tests-v2.ts
+++ scripts/split-e2e-tests-v2.ts
@@ -14,6 +14,7 @@ const RUN_SOLO = [
   'src/__tests__/geo-remove-3.test.ts',
   'src/__tests__/geo-update-1.test.ts',
   'src/__tests__/geo-update-2.test.ts',
+  'src/__tests__/hostingPROD.test.ts',
   'src/__tests__/import_auth_1a.test.ts',
   'src/__tests__/import_auth_1b.test.ts',
   'src/__tests__/import_auth_2a.test.ts',
