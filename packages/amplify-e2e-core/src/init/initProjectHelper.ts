9b1748bc81f9e9cd4bd49499c6d9e972c8a19ea0
Zachary Goldberg <goldbez@amazon.com>
2023-03-03 10:03:21 -0800

    fix: headless init helper

--- packages/amplify-e2e-core/src/init/initProjectHelper.ts
+++ packages/amplify-e2e-core/src/init/initProjectHelper.ts
@@ -492,3 +492,21 @@ export function amplifyStatus(cwd: string, expectedStatus: string, testingWithLa
       });
   });
 }
+
+export function initHeadless(cwd: string, appId: string, envName: string) {
+  return new Promise<void>((resolve, reject) => {
+    spawn(getCLIPath(), ['init', '--appId', appId, '--envName', envName, '--yes'], {
+      cwd,
+      stripColors: true,
+    })
+      .wait(/Try "amplify add api" to create a backend API and then "amplify (push|publish)" to deploy everything/)
+      .run((err: Error) => {
+        if (!err) {
+          addCircleCITags(cwd);
+          resolve();
+        } else {
+          reject(err);
+        }
+      });
+  });
+}
