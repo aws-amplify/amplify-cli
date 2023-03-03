189c7268002cdd74b8b4b86a15a11f61533e0f74
Zachary Goldberg <goldbez@amazon.com>
2023-03-03 09:00:37 -0800

    fix: export storage types bug e2e test (not working)

---
+++ packages/amplify-e2e-tests/custom-resources/custom-cdk-stack-with-storage.ts
@@ -0,0 +1,136 @@
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+/**
+ * Base amplify stack class
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export class cdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+    super(scope, id, props);
+
+    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+    // eslint-disable-next-line no-new
+    new cdk.CfnParameter(this, 'env', {
+      type: 'String',
+      description: 'Current Amplify CLI env name',
+    });
+
+    /* AWS CDK Code goes here - Learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
+
+    /* Example 1: Set up an SQS queue with an SNS topic */
+
+    const queue = new sqs.Queue(this, 'sqs-queue', {
+      queueName: cdk.Fn.join('-', ['custom-cdk-generated-sqs-queue-test', cdk.Fn.ref('env')]),
+    }); // For name uniqueness
+
+    // 👇 create sns topic
+    const topic = new sns.Topic(this, 'sns-topic', {
+      topicName: cdk.Fn.join('-', ['custom-cdk-generated-sns-topic-test', cdk.Fn.ref('env')]),
+    }); // For name uniqueness
+
+    // 👇 subscribe queue to topic
+    topic.addSubscription(new subs.SqsSubscription(queue));
+
+    // eslint-disable-next-line no-new
+    new cdk.CfnOutput(this, 'snsTopicArn', {
+      value: topic.topicArn,
+      description: 'The arn of the SNS topic',
+    });
+
+    /* Example 2: Adding IAM role to the custom stack */
+    const role = new iam.Role(this, 'CustomRole', {
+      roleName: cdk.Fn.join('-', ['custom-cdk-generated-custom-role-test', cdk.Fn.ref('env')]), // For name uniqueness
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+
+    /* Example 3: Adding policy to the IAM role*/
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['*'],
+        resources: [topic.topicArn],
+      }),
+    );
+
+    AmplifyHelpers.addResourceDependency(this, amplifyResourceProps.category, amplifyResourceProps.resourceName, [
+      { category: 'storage', resourceName: 'ddb' },
+    ]);
+
+    // const amplifyResources: AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(
+    //         this,
+    //         amplifyResourceProps.category,
+    //         amplifyResourceProps.resourceName,
+    //         [{ category: 'storage', resourceName: 'epigram' }],
+    //       );
+  }
+}
+
+// import * as cdk from 'aws-cdk-lib';
+// import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+// import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
+// import * as iam from 'aws-cdk-lib/aws-iam';
+// import * as sns from 'aws-cdk-lib/aws-sns';
+// import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import { Construct } from 'constructs';
+
+// export class cdkStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+//     super(scope, id, props);
+//     /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+//     new cdk.CfnParameter(this, 'env', {
+//       type: 'String',
+//       description: 'Current Amplify CLI env name',
+//     });
+//     /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
+
+//     // Example 1: Set up an SQS queue with an SNS topic
+
+//     const amplifyProjectInfo = AmplifyHelpers.getProjectInfo();
+//     const sqsQueueResourceNamePrefix = `sqs-queue-${amplifyProjectInfo.projectName}`;
+//     const queue = new sqs.Queue(this, 'sqs-queue', {
+//       queueName: `${sqsQueueResourceNamePrefix}-${cdk.Fn.ref('env')}`,
+//     });
+//     // 👇create sns topic
+
+//     const snsTopicResourceNamePrefix = `sns-topic-${amplifyProjectInfo.projectName}`;
+//     const topic = new sns.Topic(this, 'sns-topic', {
+//       topicName: `${snsTopicResourceNamePrefix}-${cdk.Fn.ref('env')}`,
+//     });
+//     // 👇 subscribe queue to topic
+//     topic.addSubscription(new subs.SqsSubscription(queue));
+//     new cdk.CfnOutput(this, 'snsTopicArn', {
+//       value: topic.topicArn,
+//       description: 'The arn of the SNS topic',
+//     });
+
+//     // Example 2: Adding IAM role to the custom stack
+//     const roleResourceNamePrefix = `CustomRole-${amplifyProjectInfo.projectName}`;
+
+//     const role = new iam.Role(this, 'CustomRole', {
+//       assumedBy: new iam.AccountRootPrincipal(),
+//       roleName: `${roleResourceNamePrefix}-${cdk.Fn.ref('env')}`,
+//     });
+
+//     // Example 3: Adding policy to the IAM role
+
+//     role.addToPolicy(
+//       new iam.PolicyStatement({
+//         actions: ['*'],
+//         resources: [topic.topicArn],
+//       }),
+//     );
+
+//     const amplifyResources: AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(
+//       this,
+//       amplifyResourceProps.category,
+//       amplifyResourceProps.resourceName,
+//       [{ category: 'storage', resourceName: 'epigram' }],
+//     );
+//   }
+// }
---
+++ packages/amplify-e2e-tests/src/__tests__/custom-resource-with-storage.test.ts
@@ -0,0 +1,64 @@
+import {
+  addCDKCustomResource,
+  addCFNCustomResource,
+  addSimpleDDB,
+  amplifyPushAuth,
+  buildCustomResources,
+  createNewProjectDir,
+  deleteProject,
+  deleteProjectDir,
+  getProjectMeta,
+  initJSProjectWithProfile,
+  useLatestExtensibilityHelper,
+  gitInit,
+  gitCleanFdx,
+  initHeadless,
+  getAppId,
+  addAuthWithDefault,
+  addS3WithGuestAccess,
+} from '@aws-amplify/amplify-e2e-core';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { v4 as uuid } from 'uuid';
+import { JSONUtilities } from 'amplify-cli-core';
+
+describe('adding custom resources test', () => {
+  let projRoot: string;
+  const envName = 'dev';
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('custom-resources');
+    await initJSProjectWithProfile(projRoot, { envName });
+    await gitInit(projRoot);
+  });
+
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
+
+  it('export custom storage types', async () => {
+    await addAuthWithDefault(projRoot, {});
+    await addS3WithGuestAccess(projRoot, {});
+    await amplifyPushAuth(projRoot);
+
+    console.log('LINE 44');
+
+    const appId = getAppId(projRoot);
+    const cdkResourceName = `custom${uuid().split('-')[0]}`;
+    await addCDKCustomResource(projRoot, { name: cdkResourceName });
+    const srcCustomResourceFilePath = path.join(__dirname, '..', '..', 'custom-resources', 'custom-cdk-stack-with-storage.ts');
+    const destCustomResourceFilePath = path.join(projRoot, 'amplify', 'backend', 'custom', cdkResourceName, 'cdk-stack.ts');
+    fs.copyFileSync(srcCustomResourceFilePath, destCustomResourceFilePath);
+    await buildCustomResources(projRoot);
+    await amplifyPushAuth(projRoot);
+
+    console.log('LINE 52');
+
+    await gitCleanFdx(projRoot);
+    await initHeadless(projRoot, appId, envName);
+    const typesPath = path.join(projRoot, 'amplify', 'backend', 'types', 'amplify-dependent-resources-ref.d.ts');
+    const typesFileJSON = JSONUtilities.readJson(typesPath);
+    const keys = Object.keys(typesFileJSON);
+    expect('auth' in keys && 'custom' in keys && 'storage' in keys).toBeTruthy();
+  });
+});
