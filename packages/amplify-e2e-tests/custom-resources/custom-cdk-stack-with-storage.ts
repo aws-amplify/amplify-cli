7bae35e86ad00ded1e565f0f269c43ef3eaaa2f0
Zachary Goldberg <goldbez@amazon.com>
2023-03-06 14:51:34 -0800

    fix: remove other custom resources (e2e not working)

--- packages/amplify-e2e-tests/custom-resources/custom-cdk-stack-with-storage.ts
+++ packages/amplify-e2e-tests/custom-resources/custom-cdk-stack-with-storage.ts
@@ -5,6 +5,7 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
 
 /**
  * Base amplify stack class
@@ -25,37 +26,37 @@ export class cdkStack extends cdk.Stack {
 
     /* Example 1: Set up an SQS queue with an SNS topic */
 
-    const queue = new sqs.Queue(this, 'sqs-queue', {
-      queueName: cdk.Fn.join('-', ['custom-cdk-generated-sqs-queue-test', cdk.Fn.ref('env')]),
-    }); // For name uniqueness
+    // const queue = new sqs.Queue(this, 'sqs-queue', {
+    //   queueName: cdk.Fn.join('-', ['custom-cdk-generated-sqs-queue-test-2', cdk.Fn.ref('env')]),
+    // }); // For name uniqueness
 
-    // 👇 create sns topic
-    const topic = new sns.Topic(this, 'sns-topic', {
-      topicName: cdk.Fn.join('-', ['custom-cdk-generated-sns-topic-test', cdk.Fn.ref('env')]),
-    }); // For name uniqueness
+    // // 👇 create sns topic
+    // const topic = new sns.Topic(this, 'sns-topic', {
+    //   topicName: cdk.Fn.join('-', ['custom-cdk-generated-sns-topic-test-2', cdk.Fn.ref('env')]),
+    // }); // For name uniqueness
 
-    // 👇 subscribe queue to topic
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // // 👇 subscribe queue to topic
+    // topic.addSubscription(new subs.SqsSubscription(queue));
 
-    // eslint-disable-next-line no-new
-    new cdk.CfnOutput(this, 'snsTopicArn', {
-      value: topic.topicArn,
-      description: 'The arn of the SNS topic',
-    });
+    // // eslint-disable-next-line no-new
+    // new cdk.CfnOutput(this, 'snsTopicArn', {
+    //   value: topic.topicArn,
+    //   description: 'The arn of the SNS topic',
+    // });
 
-    /* Example 2: Adding IAM role to the custom stack */
-    const role = new iam.Role(this, 'CustomRole', {
-      roleName: cdk.Fn.join('-', ['custom-cdk-generated-custom-role-test', cdk.Fn.ref('env')]), // For name uniqueness
-      assumedBy: new iam.AccountRootPrincipal(),
-    });
+    // /* Example 2: Adding IAM role to the custom stack */
+    // const role = new iam.Role(this, 'CustomRole', {
+    //   roleName: cdk.Fn.join('-', ['custom-cdk-generated-custom-role-test-3', cdk.Fn.ref('env')]), // For name uniqueness
+    //   assumedBy: new iam.AccountRootPrincipal(),
+    // });
 
-    /* Example 3: Adding policy to the IAM role*/
-    role.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ['*'],
-        resources: [topic.topicArn],
-      }),
-    );
+    // /* Example 3: Adding policy to the IAM role*/
+    // role.addToPolicy(
+    //   new iam.PolicyStatement({
+    //     actions: ['*'],
+    //     resources: [topic.topicArn],
+    //   }),
+    // );
 
     const amplifyResources: AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(
       this,
