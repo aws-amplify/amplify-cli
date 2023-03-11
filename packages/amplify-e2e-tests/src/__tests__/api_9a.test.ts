97332f949debc74785692c503f52d14a75257af0
Manaswi Manthena <manamaha@amazon.com>
2023-03-10 17:14:43 -0800

    improve e2e on mock api + auth

--- packages/amplify-e2e-tests/src/__tests__/api_9a.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/api_9a.test.ts
@@ -12,6 +12,8 @@ import {
   getTransformConfig,
   createRandomName,
   generateRandomShortId,
+  amplifyMockApi,
+  cancelAmplifyMockApi,
 } from '@aws-amplify/amplify-e2e-core';
 import path from 'path';
 import { existsSync } from 'fs';
@@ -77,6 +79,56 @@ describe('amplify add api (GraphQL)', () => {
     expect(graphqlApi.apiId).toEqual(GraphQLAPIIdOutput);
   });
 
+  it('init a project and add the simple_model api with multiple authorization providers and mock', async () => {
+    const appName = createRandomName();
+    await initJSProjectWithProfile(projRoot, { name: appName });
+    await addApiWithoutSchema(projRoot, { transformerVersion: 1 });
+    await updateApiSchema(projRoot, appName, 'simple_model.graphql');
+    await updateApiWithMultiAuth(projRoot, {});
+    await amplifyPush(projRoot);
+    
+    amplifyMockApi(projRoot);
+
+    const meta = getProjectMeta(projRoot);
+    const { output } = meta.api[appName];
+    const { GraphQLAPIIdOutput, GraphQLAPIEndpointOutput, GraphQLAPIKeyOutput } = output;
+    const { graphqlApi } = await getAppSyncApi(GraphQLAPIIdOutput, meta.providers.awscloudformation.Region);
+
+    expect(graphqlApi).toBeDefined();
+    expect(graphqlApi.authenticationType).toEqual('API_KEY');
+    expect(graphqlApi.additionalAuthenticationProviders).toHaveLength(3);
+    expect(graphqlApi.additionalAuthenticationProviders).toHaveLength(3);
+
+    const cognito = graphqlApi.additionalAuthenticationProviders.filter((a) => a.authenticationType === 'AMAZON_COGNITO_USER_POOLS')[0];
+
+    expect(cognito).toBeDefined();
+    expect(cognito.userPoolConfig).toBeDefined();
+
+    const iam = graphqlApi.additionalAuthenticationProviders.filter((a) => a.authenticationType === 'AWS_IAM')[0];
+
+    expect(iam).toBeDefined();
+
+    const oidc = graphqlApi.additionalAuthenticationProviders.filter((a) => a.authenticationType === 'OPENID_CONNECT')[0];
+
+    expect(oidc).toBeDefined();
+    expect(oidc.openIDConnectConfig).toBeDefined();
+    expect(oidc.openIDConnectConfig.issuer).toEqual('https://facebook.com/');
+    expect(oidc.openIDConnectConfig.clientId).toEqual('clientId');
+    expect(oidc.openIDConnectConfig.iatTTL).toEqual(1000);
+    expect(oidc.openIDConnectConfig.authTTL).toEqual(2000);
+
+    expect(GraphQLAPIIdOutput).toBeDefined();
+    expect(GraphQLAPIEndpointOutput).toBeDefined();
+    expect(GraphQLAPIKeyOutput).toBeDefined();
+
+    expect(graphqlApi).toBeDefined();
+    expect(graphqlApi.apiId).toEqual(GraphQLAPIIdOutput);
+
+    const transformConfig = getTransformConfig(projRoot, appName); 
+    expect(transformConfig.Version).toEqual(TRANSFORM_CURRENT_VERSION);
+
+    await cancelAmplifyMockApi(projRoot);
+  });
   it('init a project and add the simple_model api, match transformer version to current version', async () => {
     const name = `simplemodel${generateRandomShortId()}`;
     await initJSProjectWithProfile(projRoot, { name });
