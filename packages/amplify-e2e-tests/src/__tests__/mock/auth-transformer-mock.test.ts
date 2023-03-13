30d9da130b612279b8584a97d29f7b7e9416a857
Marc VandenBerg <marc.vandenberg1@gmail.com>
2023-03-13 17:00:43 -0400

    add E2E test for mock

--- packages/amplify-e2e-tests/src/__tests__/mock/auth-transformer-mock.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/mock/auth-transformer-mock.test.ts
@@ -5,17 +5,23 @@ import {
   initJSProjectWithProfile,
   updateApiSchema,
 } from '@aws-amplify/amplify-e2e-core';
+import {GraphQLClient} from '../../../../amplify-util-mock/src/__e2e__/utils';
+
 
 describe('Test auth transformer with mock', () => {
   const projName = 'authmock'
+  const apiKey = 'da-fake-api-key';
+  const endpoint = 'http://localhost:20002';
   let projRoot: string;
   let executionContext;
+  let graphqlClient;
   beforeEach(async () => {
     projRoot = await createNewProjectDir(projName);
     await initJSProjectWithProfile(projRoot, { name: projName });
     await addApiWithAllAuthModes(projRoot, { transformerVersion: 2 });
-    await updateApiSchema(projRoot, projName, 'simple_model.graphql');
+    await updateApiSchema(projRoot, projName, 'simple_model_api_key.graphql');
     executionContext = amplifyMock(projRoot);
+    graphqlClient = new GraphQLClient(endpoint, { 'x-api-key': apiKey });
   });
 
   afterEach(async () => {
@@ -25,6 +31,17 @@ describe('Test auth transformer with mock', () => {
   });
 
   it('Should execute a simple list query against mock server', async () => {
-
+    const response = await graphqlClient.query(
+      `query {
+        listTodos(input: {title: "Test Todo"}) {
+          items {
+            id
+            name
+          }
+        }
+      }`,
+      {},
+    );
+    expect(response?.data?.listTodos).toBeDefined();
   });
 });
