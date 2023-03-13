31d8fd23b63efd93d4d9354e2a83d3622a480566
Zachary Goldberg <goldbez@amazon.com>
2023-03-13 12:24:25 -0700

    fix: remove unused variables in cdk stack

--- packages/amplify-e2e-tests/custom-resources/custom-cdk-stack-with-storage.ts
+++ packages/amplify-e2e-tests/custom-resources/custom-cdk-stack-with-storage.ts
@@ -1,8 +1,6 @@
 import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
 import * as cdk from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
 
 /**
  * Base amplify stack class
@@ -12,26 +10,9 @@ export class cdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
     super(scope, id, props);
 
-    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
-    // eslint-disable-next-line no-new
     new cdk.CfnParameter(this, 'env', {
       type: 'String',
       description: 'Current Amplify CLI env name',
     });
-
-    /* AWS CDK Code goes here - Learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
-
-    /* Example 2: Adding IAM role to the custom stack */
-    const role = new iam.Role(this, 'CustomRole', {
-      roleName: cdk.Fn.join('-', ['custom-cdk-generated-custom-role-test', cdk.Fn.ref('env')]), // For name uniqueness
-      assumedBy: new iam.AccountRootPrincipal(),
-    });
-
-    const amplifyResources: AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(
-      this,
-      amplifyResourceProps.category,
-      amplifyResourceProps.resourceName,
-      [{ category: 'storage', resourceName: 'customStorage' }],
-    );
   }
 }
