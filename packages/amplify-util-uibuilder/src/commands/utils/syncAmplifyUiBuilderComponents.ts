e46e227705a0bad4693ca0aa46509f41d24d77e3
Joe Buono <joebuono@amazon.com>
2023-03-14 14:08:39 -0500

    pass formFeatureFlags to checkIsSupportedAsForm

--- packages/amplify-util-uibuilder/src/commands/utils/syncAmplifyUiBuilderComponents.ts
+++ packages/amplify-util-uibuilder/src/commands/utils/syncAmplifyUiBuilderComponents.ts
@@ -108,7 +108,7 @@ export const generateUiBuilderForms = (
   const modelMap: { [model: string]: Set<'create' | 'update'> } = {};
   if (dataSchema?.dataSourceType === 'DataStore' && autoGenerateForms) {
     Object.entries(dataSchema.models).forEach(([name, model]) => {
-      if (checkIsSupportedAsForm(model) && !model.isJoinTable) {
+      if (checkIsSupportedAsForm(model, formFeatureFlags) && !model.isJoinTable) {
         modelMap[name] = new Set(['create', 'update']);
       }
     });
