a87818ff18fbb3cc58e40396a1860e829cc9c6fd
Eddy Varela <eddvar@amazon.com>
2023-03-14 22:19:22 -0400

    chore: storing cache capability

--- codebuild_specs/cache-load.sh
+++ codebuild_specs/cache-load.sh
@@ -5,16 +5,16 @@ cachedir = $HOME/.s3buildcache
 # creates directory only if it doesn't exist already
 mkdir -p $cachedir
 
-if [[ "${S3_BUILD_CACHE:-}" = "" ]]; then
+if [[ "/root/myCache/" = "" ]]; then
     exit 0
 fi
 
-echo "🧳 Build cache enabled: ${S3_BUILD_CACHE}"
-if ! aws s3 ls ${S3_BUILD_CACHE} > /dev/null; then
+echo "🧳 Build cache enabled: /root/myCache/"
+if ! aws s3 ls /root/myCache/ > /dev/null; then
     echo "🧳⚠️  Cache not found."
     exit 0
 fi
 
-if ! (cd $cachedir && aws s3 cp ${S3_BUILD_CACHE} - | tar xzv); then
+if ! (cd $cachedir && aws s3 cp /root/myCache/ - | tar xzv); then
     echo "🧳⚠️  Something went wrong fetching the cache. Continuing anyway."
 fi
---
+++ codebuild_specs/cache-store.sh
@@ -0,0 +1,16 @@
+set -eu
+
+cachedir=$HOME/.s3buildcache
+mkdir -p $cachedir
+
+if [[ "${S3_BUILD_CACHE:-}" = "" ]]; then
+    exit 0
+fi
+
+echo "🧳 Storing build cache at: /root/myCache/}"
+
+if ! (cd $cachedir && tar czv . | aws s3 cp - /root/myCache/}); then
+    echo "🧳⚠️  Something went wrong storing the cache."
+fi
+
+echo "🧳 Finished."
