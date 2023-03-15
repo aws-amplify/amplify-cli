e68201fbc9ca224fe8a3c9659c01d60b5a413455
Danielle Adams <damsnmd@amazon.com>
2023-03-14 21:22:24 -0400

    test: add e2e test to test identity role mappings

---
+++ packages/amplify-e2e-tests/src/__tests__/auth/user-groups.test.ts
@@ -0,0 +1,60 @@
+import {
+  addAuthWithDefault,
+  amplifyPushAuth,
+  createNewProjectDir,
+  deleteProject,
+  deleteProjectDir,
+  getIdentityPoolRoles,
+  getProjectMeta,
+  getUserPool,
+  initJSProjectWithProfile,
+  updateAuthAddUserGroups,
+} from '@aws-amplify/amplify-e2e-core';
+
+const defaultsSettings = {
+  name: 'authTest',
+};
+
+describe('user group tests', () => {
+  let projRoot: string;
+
+  beforeEach(async () => {
+    projRoot = await createNewProjectDir('auth');
+  });
+
+  afterEach(async () => {
+    await deleteProject(projRoot);
+    deleteProjectDir(projRoot);
+  });
+
+  describe('amplify add auth', () => {
+    describe('adding a user pool group', () => {
+      it('assigns authenticated roles for users added to user group', async () => {
+        await initJSProjectWithProfile(projRoot, defaultsSettings);
+        await addAuthWithDefault(projRoot);
+        await updateAuthAddUserGroups(projRoot, ['mygroup']);
+        await amplifyPushAuth(projRoot);
+
+        const meta = getProjectMeta(projRoot);
+        const region = meta.providers.awscloudformation.Region;
+        const {
+          AppClientID,
+          AppClientIDWeb,
+          IdentityPoolId,
+          UserPoolId,
+        } = Object.keys(meta.auth).map((key) => meta.auth[key])[0].output;
+
+        const userPool = await getUserPool(UserPoolId, region); // test fails without user pool call
+        const identityPoolRoles = await getIdentityPoolRoles(IdentityPoolId);
+
+        const roleMapKeyClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientID}`;
+        const roleMapKeyWebClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientIDWeb}`;
+
+        expect(identityPoolRoles.RoleMappings[roleMapKeyClientId].AmbiguousRoleResolution).toEqual('AuthenticatedRole');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyClientId].Type).toEqual('Token');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].AmbiguousRoleResolution).toEqual('AuthenticatedRole');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].Type).toEqual('Token');
+      });
+    });
+  });
+});
