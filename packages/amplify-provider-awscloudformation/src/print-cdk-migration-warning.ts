6a3d08cc368383a14da9b261e1b9d2be444c44a9
akshbhu <39866697+akshbhu@users.noreply.github.com>
2023-03-16 10:34:44 -0400

    fix: stops cdk v1 warning for non cdk custom resource (#12241)
    * fix: stops cdk warning for non cdk custom resource
    
    * fix: added unit test
    
    ---------
    
    Co-authored-by: Akshay Upadhyay <akz@amazon.com>

--- packages/amplify-provider-awscloudformation/src/print-cdk-migration-warning.ts
+++ packages/amplify-provider-awscloudformation/src/print-cdk-migration-warning.ts
@@ -65,7 +65,9 @@ const getOverridesWarning = (resourcesToBuild: IAmplifyResource[], dependencyToS
 const getCustomResourcesWarning = (resourcesToBuild: IAmplifyResource[], dependencyToSearch: string): AmplifyWarning | undefined => {
   let customResourcesWarningObject;
   const customResourceImpactedFiles = [];
-  const customCategoryResources = resourcesToBuild.filter((resource) => resource.category === AmplifyCategories.CUSTOM);
+  const customCategoryResources = resourcesToBuild.filter(
+    (resource) => resource.category === AmplifyCategories.CUSTOM && resource.service !== 'customCloudformation',
+  );
   customCategoryResources.forEach((resource) => {
     const targetDir = path.join(pathManager.getBackendDirPath(), resource.category, resource.resourceName);
     const amplifyDetectorProps: AmplifyNodePkgDetectorProps = {
