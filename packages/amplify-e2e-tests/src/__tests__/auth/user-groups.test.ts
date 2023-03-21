61be25a04e0c348c7b06daa843fb475daf637e49
Danielle Adams <damsnmd@amazon.com>
2023-03-21 08:39:37 -0400

    test: fix e2e test and add additional test

--- packages/amplify-e2e-tests/src/__tests__/auth/user-groups.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/auth/user-groups.test.ts
@@ -1,5 +1,6 @@
 import {
   addAuthWithDefault,
+  addAuthWithGroups,
   amplifyPushAuth,
   createNewProjectDir,
   deleteProject,
@@ -26,9 +27,30 @@ describe('user group tests', () => {
     deleteProjectDir(projRoot);
   });
 
-  describe('... amplify add auth', () => {
-    describe('... adding a user pool group', () => {
-      it('... assigns authenticated roles for users added to user group', async () => {
+  describe('...amplify add auth', () => {
+    describe('...creating with a user pool group', () => {
+      it('...assigns authenticated roles for users added to user group', async () => {
+        await initJSProjectWithProfile(projRoot, defaultsSettings);
+        await addAuthWithGroups(projRoot);
+        await amplifyPushAuth(projRoot);
+
+        const meta = getProjectMeta(projRoot);
+        const region = meta.providers.awscloudformation.Region;
+        const { AppClientID, AppClientIDWeb, IdentityPoolId, UserPoolId } = Object.keys(meta.auth).map((key) => meta.auth[key]).find((auth) => auth.service === 'Cognito').output;
+
+        const identityPoolRoles = await getIdentityPoolRoles(IdentityPoolId, region);
+        const roleMapKeyClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientID}`;
+        const roleMapKeyWebClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientIDWeb}`;
+
+        expect(identityPoolRoles.RoleMappings[roleMapKeyClientId].AmbiguousRoleResolution).toEqual('AuthenticatedRole');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyClientId].Type).toEqual('Token');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].AmbiguousRoleResolution).toEqual('AuthenticatedRole');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].Type).toEqual('Token');
+      });
+    });
+
+    describe('...updating to add a user pool group', () => {
+      it('...assigns authenticated roles for users added to user group', async () => {
         await initJSProjectWithProfile(projRoot, defaultsSettings);
         await addAuthWithDefault(projRoot);
         await updateAuthAddUserGroups(projRoot, ['mygroup']);
@@ -36,7 +58,7 @@ describe('user group tests', () => {
 
         const meta = getProjectMeta(projRoot);
         const region = meta.providers.awscloudformation.Region;
-        const { AppClientID, AppClientIDWeb, IdentityPoolId, UserPoolId } = Object.keys(meta.auth).map((key) => meta.auth[key])[0].output;
+        const { AppClientID, AppClientIDWeb, IdentityPoolId, UserPoolId } = Object.keys(meta.auth).map((key) => meta.auth[key]).find((auth) => auth.service === 'Cognito').output;
 
         const identityPoolRoles = await getIdentityPoolRoles(IdentityPoolId, region);
         const roleMapKeyClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientID}`;
