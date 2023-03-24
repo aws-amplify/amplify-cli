616e21cad6ccd5d64226ef8537bb8343affbdbb3
Danielle Adams <damsnmd@amazon.com>
2023-03-24 11:26:43 -0400

    test: add additional e2e test for update

--- packages/amplify-e2e-tests/src/__tests__/auth/user-groups.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/auth/user-groups.test.ts
@@ -50,8 +50,36 @@ describe('user group tests', () => {
         expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].Type).toEqual('Token');
       });
     });
+  });
 
+  describe('...amplify update auth', () => {
     describe('...updating to add a user pool group', () => {
+      it('...assigns authenticated roles for users added to user group', async () => {
+        await initJSProjectWithProfile(projRoot, defaultsSettings);
+        await addAuthWithDefault(projRoot);
+        await amplifyPushAuth(projRoot);
+
+        await updateAuthAddUserGroups(projRoot, ['mygroup']);
+        await amplifyPushAuth(projRoot);
+
+        const meta = getProjectMeta(projRoot);
+        const region = meta.providers.awscloudformation.Region;
+        const { AppClientID, AppClientIDWeb, IdentityPoolId, UserPoolId } = Object.keys(meta.auth)
+          .map((key) => meta.auth[key])
+          .find((auth) => auth.service === 'Cognito').output;
+
+        const identityPoolRoles = await getIdentityPoolRoles(IdentityPoolId, region);
+        const roleMapKeyClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientID}`;
+        const roleMapKeyWebClientId = `cognito-idp.${region}.amazonaws.com/${UserPoolId}:${AppClientIDWeb}`;
+
+        expect(identityPoolRoles.RoleMappings[roleMapKeyClientId].AmbiguousRoleResolution).toEqual('AuthenticatedRole');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyClientId].Type).toEqual('Token');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].AmbiguousRoleResolution).toEqual('AuthenticatedRole');
+        expect(identityPoolRoles.RoleMappings[roleMapKeyWebClientId].Type).toEqual('Token');
+      });
+    });
+
+    describe('...updating to add a user pool group after create', () => {
       it('...assigns authenticated roles for users added to user group', async () => {
         await initJSProjectWithProfile(projRoot, defaultsSettings);
         await addAuthWithDefault(projRoot);
