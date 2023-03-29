d4874b0c8f632c97178b3d0d61427fba8975373c
akshbhu <39866697+akshbhu@users.noreply.github.com>
2023-03-29 15:46:55 -0400

    fix:removing auth from local state if imported userpool is deleted (#12286)
    * fix: cleanup auth resource when userpool not found
    
    * fix: lint fixes
    
    * fix: added e2e test for the scenario
    
    * chore: address comments
    
    * fix: added some refractoring
    
    * fix: unit tests
    
    * fix: lint fixes
    
    * fix: fixing lint in auth files
    
    * chore: removed unused imports
    
    * chore: adjust warning
    
    ---------
    
    Co-authored-by: Akshay Upadhyay <akz@amazon.com>

---
+++ packages/amplify-category-auth/src/__tests__/provider-utils/awscloudformation/utils/project-has-auth.test.ts
@@ -0,0 +1,30 @@
+import { stateManager } from '@aws-amplify/amplify-cli-core';
+import { projectHasAuth } from '../../../../provider-utils/awscloudformation/utils/project-has-auth';
+
+jest.mock('amplify-cli-core');
+
+describe('auth category dependency check', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('returns true if auth category exists', () => {
+    stateManager.getMeta = jest.fn().mockReturnValue({
+      auth: {
+        myapp12345: {
+          service: 'Cognito',
+        },
+      },
+    });
+    expect(projectHasAuth()).toEqual(true);
+  });
+
+  it('returns false if auth category does not exist', () => {
+    stateManager.getMeta = jest.fn().mockReturnValue({
+      geo: {
+        map12345: {},
+      },
+    });
+    expect(projectHasAuth()).toEqual(false);
+  });
+});
