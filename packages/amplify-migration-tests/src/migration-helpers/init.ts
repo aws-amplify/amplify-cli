f83dbd61a4015a2b775a162cc87d8fe1432eda39
Pavel Lazar <lazar.pavel@gmail.com>
2023-04-07 12:26:05 -0400

    chore: address PR feedback

--- packages/amplify-migration-tests/src/migration-helpers/init.ts
+++ packages/amplify-migration-tests/src/migration-helpers/init.ts
@@ -165,3 +165,46 @@ export function initAndroidProjectWithProfile(cwd: string, settings: Record<stri
       });
   });
 }
+
+export function initAndroidProjectWithProfileInquirer(cwd: string, settings: Record<string, unknown>): Promise<void> {
+  const s = { ...defaultSettings, ...settings };
+
+  addCircleCITags(cwd);
+
+  return new Promise((resolve, reject) => {
+    spawn(getCLIPath(), ['init'], {
+      cwd,
+      stripColors: true,
+      env: {
+        CLI_DEV_INTERNAL_DISABLE_AMPLIFY_APP_CREATION: '1',
+      },
+    })
+      .wait('Enter a name for the project')
+      .sendLine(s.name)
+      .wait('Initialize the project with the above configuration?')
+      .sendConfirmNo()
+      .wait('Enter a name for the environment')
+      .sendLine(s.envName)
+      .wait('Choose your default editor:')
+      .sendLine(s.editor)
+      .wait("Choose the type of app that you're building")
+      .send('j')
+      .sendCarriageReturn()
+      .wait('Where is your Res directory')
+      .sendCarriageReturn()
+      .wait('Select the authentication method you want to use:')
+      .sendCarriageReturn()
+      .wait('Please choose the profile you want to use')
+      .sendLine(s.profileName)
+      .wait(/Try "amplify add api" to create a backend API and then "amplify (push|publish)" to deploy everything/)
+      .run((err: Error) => {
+        if (!err) {
+          addCircleCITags(cwd);
+
+          resolve();
+        } else {
+          reject(err);
+        }
+      });
+  });
+}
