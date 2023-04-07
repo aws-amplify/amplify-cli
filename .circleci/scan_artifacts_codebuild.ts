e27297f92c64541af2fce4bc28fbbd7121d0944b
awsluja <110861985+awsluja@users.noreply.github.com>
2023-04-07 15:33:44 -0400

    ci: add artifact scanning, windows test and check in e2e support changes (#12403)
    * ci: test with mock values
    
    * ci: test with mock values
    
    * ci: fix typo
    
    * ci: cleanup apple mock key
    
    * ci: cleanup and add artifact scanning
    
    * ci: add artifact scanning using codebuild src dir
    
    * ci: rerun
    
    * ci: do a full run
    
    * ci: test artifacts and multi-core
    
    * ci: test artifacts
    
    * ci: add windows and cleanup
    
    * ci: rename codebuild specific files
    
    * ci: fix build issue
    
    * ci: try without image declaration
    
    * ci: update image pull role for windows
    
    * ci: retry without specifying image
    
    * ci: try without depends on linux job
    
    * ci: specify codebuild role in buildspec files
    
    * ci: try without specifying role
    
    * ci: try with windows 19 latest
    
    * ci: try just windows build
    
    * ci: fix version
    
    * ci: try again after updates to cb project
    
    * ci: remove invalid flag, cleanup
    
    * ci: extract node and npm versions
    
    * ci: install node 18 and yarn
    
    * ci: install yarn
    
    * ci: cleanup

--- .circleci/scan_artifacts_codebuild.ts
+++ .circleci/scan_artifacts_codebuild.ts
@@ -2,11 +2,9 @@ import * as execa from 'execa';
 import * as path from 'path';
 
 // This list contains a platform agnostic list of paths where artifacts are stored after each test.
-import { ARTIFACT_STORAGE_PATH_ALLOW_LIST } from '../scripts/artifact-storage-path-allow-list';
+import { ARTIFACT_STORAGE_PATH_ALLOW_LIST_CODEBUILD } from '../scripts/artifact-storage-path-allow-list-codebuild';
 
-// ROOT_FOLDER_ABSOLUTE_PATH resolves to the absolute value of ~
-// This is because of the location of this file in ~/repo/.circleci/scan_artifacts
-const ROOT_FOLDER_ABSOLUTE_PATH = path.normalize(path.join(__dirname, '..', '..'));
+const ROOT_FOLDER_ABSOLUTE_PATH = process.env.CODEBUILD_SRC_DIR as string;
 
 export const hasMatchingContentInFolder = (patterns: string[], folder: string, excludeFolder = '{node_modules,.cache,.git}'): boolean => {
   console.log('Scanning folder:', folder);
@@ -29,19 +27,20 @@ export const hasMatchingContentInFolder = (patterns: string[], folder: string, e
 };
 
 const main = () => {
+  console.log(process.env.CODEBUILD_SRC_DIR);
   const envVarNameWithCredentialValues = (process.env.ENV_VAR_WITH_SECRETS || '').split(',').map((v) => v.trim());
   const values = envVarNameWithCredentialValues.map((v) => process.env[v]).filter(Boolean);
   if (values.length) {
-    for (let folder of ARTIFACT_STORAGE_PATH_ALLOW_LIST) {
-      if (folder.startsWith('~/')) {
-        const normalizedFolder = path.normalize(folder.replace('~', ROOT_FOLDER_ABSOLUTE_PATH));
-        const hasContent = hasMatchingContentInFolder(values, normalizedFolder);
+    for (let folder of ARTIFACT_STORAGE_PATH_ALLOW_LIST_CODEBUILD) {
+      if (folder.startsWith('$CODEBUILD_SRC_DIR')) {
+        const normalizedFolder = path.normalize(folder.replace('$CODEBUILD_SRC_DIR', ROOT_FOLDER_ABSOLUTE_PATH));
+        const hasContent = hasMatchingContentInFolder(values as string[], normalizedFolder);
         if (hasContent) {
           console.log('Scanning artifact has found secret value. Failing the build: ', normalizedFolder);
           process.exit(1);
         }
       } else {
-        console.log('Paths in ARTIFACT_STORAGE_PATH_ALLOW_LIST must start with ~/');
+        console.log('Paths in ARTIFACT_STORAGE_PATH_ALLOW_LIST_CODEBUILD must start with ~/');
         console.log('Update the path to use ~/ and make sure to do the same in the config.yaml/config.base.yaml files');
         process.exit(1);
       }
