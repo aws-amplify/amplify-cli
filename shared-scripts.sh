e493a56e64cfc9dc1f38f89c924beecc80cd267b
awsluja <aluja@amazon.com>
2023-04-13 15:39:42 -0400

    ci: try again

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -13,7 +13,7 @@ function storeCache {
     s3Path="s3://$CACHE_BUCKET_NAME/$CODEBUILD_SOURCE_VERSION/$alias"
     echo writing cache to $s3Path
     # zip contents and upload to s3
-    if ! (cd $localPath && tar -c -z . | aws s3 cp - $s3Path); then
+    if ! (cd $localPath && tar -c -z -f . | aws s3 cp - $s3Path); then
         echo Something went wrong storing the cache.
     fi
     echo done writing cache
