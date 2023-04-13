974518212106fd1563353e38b12b79e7e4625449
awsluja <aluja@amazon.com>
2023-04-13 16:59:48 -0400

    ci: split linux and windows scripts

--- shared-scripts-windows.sh
+++ shared-scripts-windows.sh
@@ -13,7 +13,7 @@ function storeCache {
     s3Path="s3://$CACHE_BUCKET_NAME/$CODEBUILD_SOURCE_VERSION/$alias"
     echo writing cache to $s3Path
     # zip contents and upload to s3
-    if ! (cd $localPath && tar cz . | aws s3 cp - $s3Path); then
+    if ! (cd $localPath && tar -cvzf cache.tar . && aws s3 cp cache.tar $s3Path); then
         echo Something went wrong storing the cache.
     fi
     echo done writing cache
@@ -45,7 +45,7 @@ function loadCache {
         exit 0
     fi
     # load cache and unzip it
-    if ! (cd $localPath && aws s3 cp $s3Path - | tar xz); then
+    if ! (cd $localPath && aws s3 cp $s3Path - | tar -x -z); then
         echo "Something went wrong fetching the cache. Continuing anyway."
     fi
     echo done loading cache
@@ -100,8 +100,8 @@ function _buildLinux {
 function _buildWindows {
     _setShell
     echo Windows Build
-    yarn run production-build
-    yarn build-tests
+    # yarn run production-build
+    # yarn build-tests
     storeCache $CODEBUILD_SRC_DIR repo-windows
     storeCache $HOME/.cache .cache-windows
 }
