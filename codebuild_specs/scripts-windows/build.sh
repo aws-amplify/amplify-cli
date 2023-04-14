eaea60f2698af0a5dcffba341c0fe4d04284c007
awsluja <aluja@amazon.com>
2023-04-14 19:23:01 -0400

    ci: try fixing pathing

--- codebuild_specs/scripts-windows/build.sh
+++ codebuild_specs/scripts-windows/build.sh
@@ -6,26 +6,23 @@ set -e
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
+export AMPLIFY_DIR=$CODEBUILD_SRC_DIR/out
+export AMPLIFY_PATH=$CODEBUILD_SRC_DIR/out/amplify.exe
+
 echo $CODEBUILD_SRC_DIR
-echo $APPLE_KEY_ID
+echo $AMPLIFY_DIR
 
 node -v
 npm -v
 yarn -v
 npm i -g yarn
 
-export AMPLIFY_DIR=$CODEBUILD_SRC_DIR/out
-export AMPLIFY_PATH=$CODEBUILD_SRC_DIR/out/amplify.exe
-
-source ./shared-scripts-windows.sh
+source ./scripts-windows/shared-scripts-windows.sh
 
 _setShell
 
 yarn run production-build
 yarn build-tests
-ls $CODEBUILD_SRC_DIR
-ls $HOME
-ls $CODEBUILD_SRC_DIR/..
 
 storeCache $CODEBUILD_SRC_DIR repo-windows
 storeCache $HOME/.cache .cache-windows
