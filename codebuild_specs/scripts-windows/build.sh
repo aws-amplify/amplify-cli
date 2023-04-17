65e3e29b99b84130199a3444138e2c46a5d7b34e
awsluja <aluja@amazon.com>
2023-04-17 17:35:31 -0400

    ci: test reading secrets

--- codebuild_specs/scripts-windows/build.sh
+++ codebuild_specs/scripts-windows/build.sh
@@ -6,15 +6,22 @@ set -e
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
-export AMPLIFY_DIR=$CODEBUILD_SRC_DIR/out
-export AMPLIFY_PATH=$CODEBUILD_SRC_DIR/out/amplify.exe
+export AMPLIFY_DIR=$CODEBUILD_SRC_DIR\\out
+export AMPLIFY_PATH=$CODEBUILD_SRC_DIR\\out\\amplify.exe
 
 echo $CODEBUILD_SRC_DIR
 echo $AMPLIFY_DIR
 
+aws sts get-caller-identity
+
 node -v
 npm -v
 yarn -v
+
+ls ..
+ls ~
+ls $HOME
+ls ..\\$HOME
 
 source ./codebuild_specs/scripts-windows/shared-scripts-windows.sh
 
@@ -23,6 +30,9 @@ _setShell
 yarn run production-build
 yarn build-tests
 
+ls ..
+ls ~
+ls ~/.cache/
 
 source ./codebuild_specs/scripts-windows/shared-scripts-windows.sh
 storeCache $CODEBUILD_SRC_DIR repo-windows
