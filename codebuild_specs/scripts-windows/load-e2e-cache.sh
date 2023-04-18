e1dce3fd6083503048f8d6eec3c09cf1b5d8a007
awsluja <aluja@amazon.com>
2023-04-18 16:38:57 -0400

    ci: cleanup and full run

--- codebuild_specs/scripts-windows/load-e2e-cache.sh
+++ codebuild_specs/scripts-windows/load-e2e-cache.sh
@@ -2,11 +2,11 @@
 
 source ./codebuild_specs/scripts-windows/shared-scripts-windows.sh
 
-export CODEBUILD_SOURCE_VERSION=f421b4b1d41cd1d38fbb53bd34da7f6e67c302f6
+# export CODEBUILD_SOURCE_VERSION=b0e2753b3e8c8f520c3a57d74d75cf293ee7b978
 loadCache repo-windows $CODEBUILD_SRC_DIR
 loadCache .cache-windows $HOME/AppData/Local/Yarn/Cache/v6
-loadCache verdaccio-cache $CODEBUILD_SRC_DIR/../verdaccio-cache
 
+loadCache verdaccio-cache $CODEBUILD_SRC_DIR/../verdaccio-cache
 loadCache all-binaries $CODEBUILD_SRC_DIR/out
 loadCacheFile .amplify-pkg-version $CODEBUILD_SRC_DIR/.amplify-pkg-version
 loadCacheFile UNIFIED_CHANGELOG.md $CODEBUILD_SRC_DIR/UNIFIED_CHANGELOG.md
