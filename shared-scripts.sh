9616a52ae2ced2391b94c9a69890ca95bb3509f9
Eddy Varela <eddvar@amazon.com>
2023-04-24 14:38:57 -0400

    run full e2e jobs

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -286,5 +286,5 @@ function _cleanupResources {
     cd packages/amplify-e2e-tests
     yarn clean-e2e-resources
 
-    storeCache $CODEBUILD_SRC_DIR/packages/amplify-e2e-tests/amplify-e2e-reports repo
+    storeCache $CODEBUILD_SRC_DIR/packages/amplify-e2e-tests/amplify-e2e-reports repo/packages/amplify-e2e-tests/amplify-e2e-reports
 }
