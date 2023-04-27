7fdc3f672159ad6fe3c19dee962df7ba0709454f
Zachary Goldberg <goldbez@amazon.com>
2023-04-27 10:53:59 -0700

    chore: noop trigger debug session build

--- codebuild_specs/aws-configure-credentials.py
+++ codebuild_specs/aws-configure-credentials.py
@@ -4,7 +4,7 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_SESSION_TOKEN = os.environ['AWS_SESSION_TOKEN']
 
-credentials_file_contents = '''[default]
+credentials_file_contents = f'''[default]
 aws_access_key_id={AWS_ACCESS_KEY_ID}
 aws_secret_access_key={AWS_SECRET_ACCESS_KEY}
 aws_session_token={AWS_SESSION_TOKEN}
