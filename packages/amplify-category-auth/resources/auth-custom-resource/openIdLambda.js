eab91c5c2f7ee02add65a74db016db6c68e2ae1c
Danielle Adams <adamzdanielle@gmail.com>
2023-05-02 18:06:43 -0400

    fix: add catch error to lambdas to prevent hanging CFN and return success on notfound exceptions

--- packages/amplify-category-auth/resources/auth-custom-resource/openIdLambda.js
+++ packages/amplify-category-auth/resources/auth-custom-resource/openIdLambda.js
@@ -1,22 +1,35 @@
+const response = require('cfn-response');
 const aws = require('aws-sdk');
 const iam = new aws.IAM();
 
 exports.handler = (event) => {
-  if (event.RequestType == 'Update' || event.RequestType == 'Create') {
-    iam
-      .listOpenIDConnectProviders({})
-      .promise()
-      .then(async (data) => {
-        let providerArn;
+  iam
+    .listOpenIDConnectProviders({})
+    .promise()
+    .then(async (data) => {
+      let providerArn;
 
-        if (data.OpenIDConnectProviderList && data.OpenIDConnectProviderList.length > 0) {
-          const providerArns = data.OpenIDConnectProviderList.map((x) => x.Arn);
-          providerArn = providerArns.find((i) => i.split('/')[1] === params.Url.split('//')[1]);
-        }
+      if (data.OpenIDConnectProviderList && data.OpenIDConnectProviderList.length > 0) {
+        const providerArns = data.OpenIDConnectProviderList.map((x) => x.Arn);
+        providerArn = providerArns.find((i) => i.split('/')[1] === params.Url.split('//')[1]);
+      }
 
-        if (providerArn) {
-          await iam.deleteOpenIDConnectProvider({ OpenIDConnectProviderArn: providerArn }).promise();
-        }
-      });
-  }
+      if (providerArn) {
+        await iam.deleteOpenIDConnectProvider({ OpenIDConnectProviderArn: providerArn })
+          .promise()
+          .catch((err) => {
+            console.log(err);
+
+            if (err.name === 'NotFoundException') {
+              return response.send(event, context, response.SUCCESS);
+            }
+
+            response.send(event, context, response.FAILED, { err });
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      response.send(event, context, response.FAILED, { err });
+    });
 };
