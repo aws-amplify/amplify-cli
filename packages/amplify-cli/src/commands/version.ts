3bdeaef7eeb675ff23af5db40ad07ba2c6c8f72c
Pavel Lazar <pavel@alazara.com>
2023-05-10 15:53:48 -0400

    feat: lambda yarn 2 support

--- packages/amplify-cli/src/commands/version.ts
+++ packages/amplify-cli/src/commands/version.ts
@@ -3,6 +3,6 @@ import { printer } from '@aws-amplify/amplify-prompts';
 import { getAmplifyVersion } from '../extensions/amplify-helpers/get-amplify-version';
 
 export const run = async () => {
-  printer.info(await execAsStringPromise('yarn --version'));
   printer.info(getAmplifyVersion());
+  printer.debug(await execAsStringPromise('yarn --version'));
 };
