3c2e0dfe3e3d2548a0e9d999c969636e3be01564
awsluja <aluja@amazon.com>
2023-05-16 18:55:00 -0400

    ci: refactor

--- scripts/cloud-e2e.sh
+++ scripts/cloud-e2e.sh
@@ -5,20 +5,35 @@ set -e
 # load .env
 set -o allexport
 source ./scripts/.env set
-PROFILE=AmplifyCLIE2E
-
-ACCOUNT=$E2E_ACCOUNT_PROD
-echo Submitting CodeBuild Request to AWS Account: $ACCOUNT
-CURR_BRANCH=$(git branch --show-current)
-UPSTREAM_BRANCH=run-e2e/$USER/$CURR_BRANCH
-git push $(git remote -v | grep aws-amplify/amplify-cli | head -n1 | awk '{print $1;}') $CURR_BRANCH:$UPSTREAM_BRANCH --no-verify --force-with-lease
-aws configure set region us-east-1 --profile $PROFILE
-echo Current branch is: $CURR_BRANCH
-echo Upstream branch is: $UPSTREAM_BRANCH
-echo Authenticating terminal...
-mwinit
-echo Loading E2E account credentials...
-ada cred update --profile="${PROFILE}" --account="${ACCOUNT}" --role=CodeBuildE2E --provider=isengard --once
-echo Submitting Build Request
-RESULT=$(aws codebuild start-build-batch --profile="${PROFILE}" --project-name AmplifyCLI-E2E-Testing --source-version=$UPSTREAM_BRANCH --query 'buildBatch.id' --output text)
-echo "https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$ACCOUNT/projects/AmplifyCLI-E2E-Testing/batch/$RESULT?region=us-east-1"
+export CURR_BRANCH=$(git branch --show-current)
+function authenticate {
+    echo Authenticating terminal...
+    mwinit
+    echo Loading E2E account credentials...
+    ada cred update --profile="${CLOUD_E2E_PROFILE}" --account="${CLOUD_E2E_ACCOUNT}" --role=CodeBuildE2E --provider=isengard --once
+    aws configure set region us-east-1 --profile $CLOUD_E2E_PROFILE
+}
+function triggerBuild {
+    echo Submitting CodeBuild Request to AWS Account: $CLOUD_E2E_ACCOUNT
+    echo Current branch is: $CURR_BRANCH
+    echo E2E Target branch is: $TARGET_BRANCH
+    RESULT=$(aws codebuild start-build-batch --profile="${CLOUD_E2E_PROFILE}" --project-name AmplifyCLI-E2E-Testing --source-version=$TARGET_BRANCH --query 'buildBatch.id' --output text)
+    echo "https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$CLOUD_E2E_ACCOUNT/projects/AmplifyCLI-E2E-Testing/batch/$RESULT?region=us-east-1"
+}
+function cloudE2EBeta {
+    echo Running Beta E2E Test Suite
+    export CLOUD_E2E_PROFILE=AmplifyCLIE2EBeta
+    export CLOUD_E2E_ACCOUNT=$E2E_ACCOUNT_BETA
+    export TARGET_BRANCH=$CURR_BRANCH
+    authenticate
+    triggerBuild
+}
+function cloudE2E {
+    echo Running Prod E2E Test Suite
+    export CLOUD_E2E_PROFILE=AmplifyCLIE2E
+    export CLOUD_E2E_ACCOUNT=$E2E_ACCOUNT_PROD
+    export TARGET_BRANCH=run-e2e/$USER/$CURR_BRANCH
+    git push $(git remote -v | grep aws-amplify/amplify-cli | head -n1 | awk '{print $1;}') $CURR_BRANCH:$TARGET_BRANCH --no-verify --force-with-lease
+    authenticate
+    triggerBuild
+}
