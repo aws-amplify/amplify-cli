0d7297f8307ee69ea87fed2b91232fba03522cac
John Hockett <hi@johnhockett.com>
2023-05-16 12:38:15 -0700

    test: update test infra to run new migration tests

--- scripts/split-e2e-test-filters.ts
+++ scripts/split-e2e-test-filters.ts
@@ -53,4 +53,5 @@ export const migrationFromV10Tests = [
 export const migrationFromV11Tests = [
   'src/__tests__/migration_tests_v11/auth-app-client-secret-migration.test.ts',
   'src/__tests__/migration_tests_v11/auth-oauth-lambda-migration.test.ts',
+  'src/__tests__/migration_tests_v11/auth-lambda-callout-migration.test.ts',
 ];
