5115c1ebbef1719f6818c5ee246eaa9eac4f1ec2
Pavel Lazar <pavel@alazara.com>
2023-05-16 18:37:36 -0400

    chore: fix yarn 2 smoketest

--- packages/amplify-e2e-tests/src/setup-tests.ts
+++ packages/amplify-e2e-tests/src/setup-tests.ts
@@ -12,8 +12,9 @@ expect.extend({ toBeIAMRoleWithArn });
 expect.extend({ toHaveValidPolicyConditionMatchingIdpId });
 expect.extend({ toBeAS3Bucket });
 
-const JEST_TIMEOUT = 1000 * 60 * 60; // 1 hour
+process.env.YARN_ENABLE_INLINE_BUILDS = undefined;
 
+const JEST_TIMEOUT = 1000 * 60 * 60; // 1 hour
 jest.setTimeout(JEST_TIMEOUT);
 if (process.env.CIRCLECI) {
   jest.retryTimes(1);
--- packages/amplify-migration-tests/src/setup-tests.ts
+++ packages/amplify-migration-tests/src/setup-tests.ts
@@ -5,10 +5,10 @@ const removeYarnPaths = () => {
 };
 
 removeYarnPaths();
+process.env.YARN_ENABLE_INLINE_BUILDS = undefined;
 
 // tslint:disable-next-line: no-magic-numbers
 const JEST_TIMEOUT = 1000 * 60 * 60; // 1 hour
-
 jest.setTimeout(JEST_TIMEOUT);
 if (process.env.CIRCLECI) {
   jest.retryTimes(1);
