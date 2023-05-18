647a9092a582a1fed902632390b82a2f181d24eb
Pavel Lazar <pavel@alazara.com>
2023-05-18 10:06:06 -0400

    chore: fix datastore model gen test

--- packages/amplify-e2e-tests/src/amplify-app-helpers/amplify-app-setup.ts
+++ packages/amplify-e2e-tests/src/amplify-app-helpers/amplify-app-setup.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 
 const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-const amplifyAppBinPath = path.join(__dirname, '..', '..', '..', 'amplify-app', 'bin', 'amplify-app');
-const spawnCommand = isCI() ? 'amplify-app' : amplifyAppBinPath;
+const amplifyAppBinPath = path.join(__dirname, '..', '..', '..', 'node_modules', '@aws-amplify/amplify-app/', 'bin', 'amplify-app');
+const spawnCommand = amplifyAppBinPath;
 
 function amplifyAppAndroid(projRoot: string): Promise<void> {
   return new Promise((resolve, reject) => {
