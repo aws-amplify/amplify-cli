01ba5b243feaf9f6617b787c4a13b05237a71e31
phani-srikar <phanisrikar93ume@gmail.com>
2023-05-23 11:49:52 -0700

    fix: warn about multiple schema files in admin modelgen task

--- packages/amplify-provider-awscloudformation/src/admin-modelgen.ts
+++ packages/amplify-provider-awscloudformation/src/admin-modelgen.ts
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import * as path from 'path';
 import { S3 } from './aws-utils/aws-s3';
 import { ProviderName as providerName } from './constants';
+import { printer } from '@aws-amplify/amplify-prompts';
 
 /**
  * Generates DataStore Models for Admin UI CMS to consume
@@ -23,6 +24,13 @@ export const adminModelgen = async (context: $TSContext, resources: $TSAny[]): P
   const appId = amplifyMeta?.providers?.[providerName]?.AmplifyAppId;
 
   if (!appId) {
+    return;
+  }
+
+  const localSchemaPath = path.join(pathManager.getResourceDirectoryPath(undefined, 'api', resourceName), 'schema.graphql');
+  // Early return with useful warning if the schema file does not exist
+  if (!fs.existsSync(localSchemaPath)) {
+    printer.warn(`Could not find the GraphQL schema file at "${localSchemaPath}". Amplify Studio's schema editor might not work as intended if you're using multiple schema files.`);
     return;
   }
 
@@ -65,7 +73,6 @@ export const adminModelgen = async (context: $TSContext, resources: $TSAny[]): P
     // invokes https://github.com/aws-amplify/amplify-codegen/blob/main/packages/amplify-codegen/src/commands/model-intropection.js#L8
     await context.amplify.invokePluginMethod(context, 'codegen', undefined, 'generateModelIntrospection', [context]);
 
-    const localSchemaPath = path.join(pathManager.getResourceDirectoryPath(undefined, 'api', resourceName), 'schema.graphql');
     const localSchemaJsPath = path.join(absoluteTempOutputDir, 'models', 'schema.js');
     const localModelIntrospectionPath = path.join(absoluteTempOutputDir, 'model-introspection.json');
 
