a71e6587081e36e1d8d2bfdbaf25c2494788260f
awsluja <aluja@amazon.com>
2023-05-23 15:42:42 -0400

    ci: cleanup

--- scripts/cloud-release.sh
+++ scripts/cloud-release.sh
@@ -2,10 +2,10 @@
 source ./scripts/cloud-cli-utils.sh
 export RELEASE_ROLE_NAME=CodebuildRelease
 export RELEASE_PROFILE_NAME=AmplifyCLIRelease
-export RELEASE_PROJECT_NAME=Release
+export RC_PROJECT_NAME=RC
 
 function releaseRCLocal {
-    echo Running Local Release
+    echo Running Local RC
     if [[ -z ${1+x} ]]; then
         echo "Include the release candidate commit ref you wish to release as the first argument"
         exit 1
@@ -15,10 +15,10 @@ function releaseRCLocal {
     git checkout -B "$branch_name" "$rc_sha"
     git push "origin" "$branch_name"
 
-    triggerProjectBatch $RELEASE_ACCOUNT_LOCAL $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Local" $RELEASE_PROJECT_NAME $branch_name
+    triggerProjectBatch $RELEASE_ACCOUNT_LOCAL $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Local" $RC_PROJECT_NAME $branch_name
 }
 function releaseRCBeta {
-    echo Running Beta Release
+    echo Running Beta RC
     echo You must be on the Beta repository to perform this action, or build will fail.
     if [[ -z ${1+x} ]]; then
         echo "Include the release candidate commit ref you wish to release as the first argument"
@@ -28,15 +28,15 @@ function releaseRCBeta {
     branch_name="release_rc/$rc_sha"
     git checkout -B "$branch_name" "$rc_sha"
     git push "origin" "$branch_name"
-    triggerProjectBatch $RELEASE_ACCOUNT_BETA $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Beta" $RELEASE_PROJECT_NAME $branch_name
+    triggerProjectBatch $RELEASE_ACCOUNT_BETA $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Beta" $RC_PROJECT_NAME $branch_name
 }
 function releaseRCProd {
-    echo Running Prod Release
+    echo Running Prod RC
     if [[ -z ${1+x} ]]; then
         echo "Include the release candidate commit ref you wish to release as the first argument"
         exit 1
     fi
     source ./scripts/release-rc.sh $1
     branch_name=$(git branch --show-current)
-    triggerProjectBatch $RELEASE_ACCOUNT_PROD $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Prod" $RELEASE_PROJECT_NAME $branch_name
+    triggerProjectBatch $RELEASE_ACCOUNT_PROD $RELEASE_ROLE_NAME "${RELEASE_PROFILE_NAME}Prod" $RC_PROJECT_NAME $branch_name
 }
