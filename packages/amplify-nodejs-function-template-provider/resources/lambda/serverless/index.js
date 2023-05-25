e8246014ffc84d68873c302c3497a6e4dc4bf6f5
ykethan <ykethan@amazon.com>
2023-05-25 12:54:28 -0400

    feat: update express template

--- packages/amplify-nodejs-function-template-provider/resources/lambda/serverless/index.js
+++ packages/amplify-nodejs-function-template-provider/resources/lambda/serverless/index.js
@@ -1,15 +1,10 @@
-const awsServerlessExpress = require('aws-serverless-express');
+const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./app');
-
-/**
- * @type {import('http').Server}
- */
-const server = awsServerlessExpress.createServer(app);
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+  return serverlessExpress({ app })(event, context);
 };
