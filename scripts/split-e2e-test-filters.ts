051b06cc4ae646ec0e5096fca5a95df9a3846dc3
Edward Foyle <foyleef@amazon.com>
2023-05-30 09:44:12 -0700

    chore: cleanup e2e file filter

--- scripts/split-e2e-test-filters.ts
+++ scripts/split-e2e-test-filters.ts
@@ -3,23 +3,9 @@ export const migrationFromV8Tests = [
   'src/__tests__/migration_tests/notifications-migration/notifications-migration-3.test.ts',
   'src/__tests__/migration_tests/notifications-migration/notifications-migration-4.test.ts',
   'src/__tests__/migration_tests/notifications-migration/notifications-migration.test.ts',
-  'src/__tests__/migration_tests/overrides/api-migration-a.test.ts',
-  'src/__tests__/migration_tests/overrides/api-migration-b.test.ts',
-  'src/__tests__/migration_tests/overrides/api-migration-c.test.ts',
-  'src/__tests__/migration_tests/overrides/api-migration-d.test.ts',
-  'src/__tests__/migration_tests/overrides/apigw-ext-migration.test.ts',
-  'src/__tests__/migration_tests/overrides/auth-migration-a.test.ts',
-  'src/__tests__/migration_tests/overrides/auth-migration-b.test.ts',
-  'src/__tests__/migration_tests/overrides/auth-migration-c.test.ts',
-  'src/__tests__/migration_tests/overrides/auth-migration-d.test.ts',
-  'src/__tests__/migration_tests/overrides/auth-migration-e.test.ts',
-  'src/__tests__/migration_tests/overrides/init-migration.test.ts',
   'src/__tests__/migration_tests/transformer_migration/api.key.migration-2.test.ts',
   'src/__tests__/migration_tests/transformer_migration/api.key.migration.test.ts',
-  'src/__tests__/update_tests/api_migration_update.test.ts',
-  'src/__tests__/update_tests/auth_migration_update.test.ts',
   'src/__tests__/update_tests/function_migration_update.test.ts',
-  'src/__tests__/update_tests/storage_migration_update.test.ts',
 ];
 
 export const migrationFromV10Tests = [
