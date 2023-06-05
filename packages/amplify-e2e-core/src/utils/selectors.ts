f4161a7c84af66bf4df1fc3b567c49c3051f7506
Kamil Sobol <sobkamil@amazon.com>
2023-06-05 12:24:18 -0700

    fix: e2e test on Windows

--- packages/amplify-e2e-core/src/utils/selectors.ts
+++ packages/amplify-e2e-core/src/utils/selectors.ts
@@ -1,10 +1,10 @@
 import { ExecutionContext } from '..';
 
 export const moveDown = (chain: ExecutionContext, nMoves: number) =>
-  Array.from(Array(nMoves).keys()).reduce((chain) => chain.send('j'), chain);
+  Array.from(Array(nMoves).keys()).reduce((chain) => chain.send('j').delay(500), chain);
 
 export const moveUp = (chain: ExecutionContext, nMoves: number) =>
-  Array.from(Array(nMoves).keys()).reduce((chain) => chain.send('k'), chain);
+  Array.from(Array(nMoves).keys()).reduce((chain) => chain.send('k').delay(500), chain);
 
 export const singleSelect = <T>(chain: ExecutionContext, item: T, allChoices: T[]) => multiSelect(chain, [item], allChoices);
 
