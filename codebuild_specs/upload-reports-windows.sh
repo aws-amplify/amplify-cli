35b61593724762dff629e84782df8afcff5e4897
Zachary Goldberg <goldbez@amazon.com>
2023-06-13 15:43:11 -0700

    chore: add reports for windows tests

--- codebuild_specs/upload-reports-windows.sh
+++ codebuild_specs/upload-reports-windows.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-# set exit on error to true
 set -e
-set +x
-
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
-
-echo "CODEBUILD_SOURCE_VERSION $CODEBUILD_SOURCE_VERSION"
-echo "CODEBUILD_BATCH_BUILD_IDENTIFIER $CODEBUILD_BATCH_BUILD_IDENTIFIER"
-
 source ./shared-scripts.sh
-
-echo "uploading 12"
 _uploadReportsToS3 $CODEBUILD_SOURCE_VERSION $CODEBUILD_BATCH_BUILD_IDENTIFIER amplify-e2e-tests
