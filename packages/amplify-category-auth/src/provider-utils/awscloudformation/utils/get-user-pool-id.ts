568186089bc8862b6711a5ab58e79041389ea08a
Danielle Adams <adamzdanielle@gmail.com>
2023-06-13 18:28:20 -0400

    chore: add helper method for getting user pool id from amplify meta

---
+++ packages/amplify-category-auth/src/provider-utils/awscloudformation/utils/get-user-pool-id.ts
@@ -0,0 +1,8 @@
+import { stateManager } from '@aws-amplify/amplify-cli-core';
+
+export const getUserPoolId = (resourceName: string): string | undefined => {
+  const amplifyMeta = stateManager.getMeta();
+  const resource = (amplifyMeta?.auth || {})[resourceName];
+
+  return resource?.output?.UserPoolId;
+}
