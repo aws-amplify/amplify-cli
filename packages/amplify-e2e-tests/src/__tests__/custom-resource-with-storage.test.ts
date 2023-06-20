61e783db9b625c445a8e6eda38e25ebee216f2de
Zachary Goldberg <goldbez@amazon.com>
2023-06-20 10:40:48 -0700

    chore: noop trigger cb

--- packages/amplify-e2e-tests/src/__tests__/custom-resource-with-storage.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/custom-resource-with-storage.test.ts
@@ -49,6 +49,7 @@ describe('adding custom resources test', () => {
     await amplifyPushAuth(projRoot);
     await gitCleanFdX(projRoot);
     console.log('doing init line 50');
+    console.log('REGION:', process.env.AWS_DEFAULT_REGION);
     await initHeadless(projRoot, envName, appId);
     const typesPath = path.join(projRoot, 'amplify', 'backend', 'types', 'amplify-dependent-resources-ref.d.ts');
     const typesFileContents = await fs.readFile(typesPath, 'utf-8');
