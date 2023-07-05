658b3c6aa7571cf27b80bb39d1d77988f58a66a1
Zachary Goldberg <goldbez@amazon.com>
2023-07-05 15:37:39 -0700

    chore: add amplify general config tests

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -549,6 +549,6 @@ function _amplifyGeneralConfigTests {
 
     source .circleci/local_publish_helpers.sh
     cd packages/amplify-e2e-tests
-    alias amplify="amplify-dev"
+    # alias amplify="amplify-dev"
     retry yarn general-config-e2e --no-cache --maxWorkers=3 --forceExit $TEST_SUITE
 }
