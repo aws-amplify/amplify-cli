c66cf2811de08e4bef9989fd53b3ddb9852b80af
Kamil Sobol <sobkamil@amazon.com>
2023-07-07 13:39:11 -0700

    chore: easy to port changes

--- scripts/split-e2e-test-filters.ts
+++ scripts/split-e2e-test-filters.ts
@@ -22,3 +22,9 @@ export const migrationFromV10Tests = [
   'src/__tests__/migration_tests_v10/storage.migration.test.ts',
   'src/__tests__/migration_tests_v10/pinpoint-region.migration.test.ts',
 ];
+export const migrationFromV12Tests = [
+  'src/__tests__/migration_tests_v12/auth-app-client-secret-migration.test.ts',
+  'src/__tests__/migration_tests_v12/auth-lambda-callout-migration.test.ts',
+  'src/__tests__/migration_tests_v12/auth-oauth-lambda-migration.test.ts',
+  'src/__tests__/migration_tests_v12/auth-resource-deletion-migration.test.ts',
+];
