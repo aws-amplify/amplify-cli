c6b7e0d4b9575fba2d72370050344fad9952b9d6
Zachary Goldberg <goldbez@amazon.com>
2023-07-20 13:46:01 -0700

    chore: codebuild cleanup script

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -626,12 +626,12 @@ function _amplifyGeneralConfigTests {
 }
 
 function _cleanUpResources {
-    loadCache repo $CODEBUILD_SRC_DIR
-    loadCache .cache $HOME/.cache
+    # loadCache repo $CODEBUILD_SRC_DIR
+    # loadCache .cache $HOME/.cache
     cd scripts
     yarn install
     _loadTestAccountCredentials
     echo "Executing resource cleanup"
-    yarn ts-node cleanup-codebuild-resources.ts
+    ts-node cleanup-codebuild-resources.ts
     _unassumeTestAccountCredentials
 }
