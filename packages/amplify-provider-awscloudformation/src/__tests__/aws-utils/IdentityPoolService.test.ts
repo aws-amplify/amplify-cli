93d1e3d6d05d7272e6c0dd253fa9f9ed9b11496d
John Hockett <hi@johnhockett.com>
2023-07-25 19:23:48 -0700

    test: add and update unit tests

--- packages/amplify-provider-awscloudformation/src/__tests__/aws-utils/IdentityPoolService.test.ts
+++ packages/amplify-provider-awscloudformation/src/__tests__/aws-utils/IdentityPoolService.test.ts
@@ -2,6 +2,11 @@ import { $TSContext } from '@aws-amplify/amplify-cli-core';
 import { createIdentityPoolService } from '../../aws-utils/IdentityPoolService';
 import { loadConfiguration } from '../../configuration-manager';
 
+let mockCognitoIdentityRoles = {
+  authenticated: 'arn:aws:iam::123456789012:role/service-role/my-auth-role',
+  unauthenticated: 'arn:aws:iam::123456789012:role/service-role/my-unauth-role',
+};
+
 jest.mock('aws-sdk', () => {
   return {
     CognitoIdentity: jest.fn(() => {
@@ -10,10 +15,7 @@ jest.mock('aws-sdk', () => {
         getIdentityPoolRoles: jest.fn().mockImplementation(() => ({
           promise: async () => {
             return {
-              Roles: {
-                authenticated: 'arn:aws:iam::123456789012:role/service-role/my-auth-role',
-                unauthenticated: 'arn:aws:iam::123456789012:role/service-role/my-unauth-role',
-              },
+              Roles: mockCognitoIdentityRoles,
             };
           },
         })),
@@ -33,9 +35,34 @@ describe('IdentityPoolService', () => {
     const idpService = await createIdentityPoolService({} as unknown as $TSContext, {});
     expect(await idpService.getIdentityPoolRoles('mockIdpId')).toEqual({
       authRoleArn: 'arn:aws:iam::123456789012:role/service-role/my-auth-role',
-      authRoleName: 'my-auth-role',
+      authRoleName: 'service-role/my-auth-role',
       unauthRoleArn: 'arn:aws:iam::123456789012:role/service-role/my-unauth-role',
+      unauthRoleName: 'service-role/my-unauth-role',
+    });
+  });
+
+  it('should correctly parse arn if it contains a single forward slash', async () => {
+    const idpService = await createIdentityPoolService({} as unknown as $TSContext, {});
+    mockCognitoIdentityRoles = {
+      authenticated: 'arn:aws:iam::123456789012:role/my-auth-role',
+      unauthenticated: 'arn:aws:iam::123456789012:role/my-unauth-role',
+    };
+
+    expect(await idpService.getIdentityPoolRoles('mockIdpId')).toEqual({
+      authRoleArn: 'arn:aws:iam::123456789012:role/my-auth-role',
+      authRoleName: 'my-auth-role',
+      unauthRoleArn: 'arn:aws:iam::123456789012:role/my-unauth-role',
       unauthRoleName: 'my-unauth-role',
     });
+  });
+
+  it('should fail to parse arn if it contains no forward slash', async () => {
+    const idpService = await createIdentityPoolService({} as unknown as $TSContext, {});
+    mockCognitoIdentityRoles = {
+      authenticated: 'arn:aws:iam::123456789012:my-auth-role',
+      unauthenticated: 'arn:aws:iam::123456789012:my-unauth-role',
+    };
+
+    await expect(idpService.getIdentityPoolRoles('mockIdpId')).rejects.toBeDefined();
   });
 });
