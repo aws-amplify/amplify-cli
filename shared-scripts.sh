c5bdf9cc705c59cf4aa172499bca44787963560a
Kamil Sobol <sobkamil@amazon.com>
2023-07-28 20:16:23 -0700

    chore: try that again

--- shared-scripts.sh
+++ shared-scripts.sh
@@ -151,12 +151,12 @@ function _mockE2ETests {
     # download [repo, .cache from s3]
     loadCache repo $CODEBUILD_SRC_DIR
     loadCache .cache $HOME/.cache
-    #useradd -u 8877 osuser
-    #chown -R osuser .
-    chmod -R a+r /root/
-    #source .circleci/local_publish_helpers.sh
-    #cd packages/amplify-util-mock/
-    #sudo -u osuser 'export NODE_OPTIONS=--max-old-space-size=4096; yarn e2e;'
+    useradd -u 8877 osuser
+    chown -R osuser .
+    #chmod -R a+r /root/
+    source .circleci/local_publish_helpers.sh
+    cd packages/amplify-util-mock/
+    sudo -u osuser bash -c 'export NODE_OPTIONS=--max-old-space-size=4096; yarn e2e;'
     # yarn e2e
 }
 function _mockE2ETestsStep2 {
