5bf53b8eaf3a18f935646808cdafe5be87f4e64e
Kamil Sobol <sobkamil@amazon.com>
2023-07-31 15:17:29 -0700

    build: upfront checkout (#13040)
    * chore: lets see
    
    * chore: lets see
    
    * chore: don't purge open search please
    
    * chore: try that
    
    * chore: try that
    
    * chore: try that
    
    * chore: undo hacks
    
    * chore: try this
    
    * chore: try that
    
    * chore: try that too
    
    * chore: try that too
    
    * chore: try that again
    
    * chore: do this
    
    * chore: check if build fails
    
    * Revert "chore: check if build fails"
    
    This reverts commit d5d418c277aa7c9c33902acf5b5931dfeea36c5a.
    
    * chore: comments
    
    * chore: try upfront checkout
    
    * chore: try upfront checkout
    
    * chore: print stuff
    
    * chore: print stuff
    
    * chore: print stuff
    
    * chore: print stuff
    
    * chore: print stuff
    
    * chore: grrrr
    
    * chore: try this
    
    * chore: comment
    
    * chore: grrrr
    
    * chore: fetch all tags
    
    * chore: fetch all tags
    
    * chore: this may contain pr
    
    * chore: comment
    
    * chore: add branch here

--- scripts/cloud-e2e.sh
+++ scripts/cloud-e2e.sh
@@ -19,7 +19,7 @@ function triggerBuild {
     echo Submitting CodeBuild Request to AWS Account: $CLOUD_E2E_ACCOUNT
     echo Current branch is: $CURR_BRANCH
     echo E2E Target branch is: $TARGET_BRANCH
-    RESULT=$(aws codebuild start-build-batch --profile="${CLOUD_E2E_PROFILE}" --project-name AmplifyCLI-E2E-Testing --source-version=$TARGET_BRANCH --query 'buildBatch.id' --output text)
+    RESULT=$(aws codebuild start-build-batch --profile="${CLOUD_E2E_PROFILE}" --project-name AmplifyCLI-E2E-Testing --source-version=$TARGET_BRANCH --environment-variables-override name=BRANCH_NAME,value=$TARGET_BRANCH,type=PLAINTEXT --query 'buildBatch.id' --output text)
     echo "https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$CLOUD_E2E_ACCOUNT/projects/AmplifyCLI-E2E-Testing/batch/$RESULT?region=us-east-1"
 }
 function cloudE2ELocal {
