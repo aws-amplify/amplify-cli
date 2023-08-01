412f982d0014cb7ceb8bd66e30ca5b04ff37ab9f
Kamil Sobol <sobkamil@amazon.com>
2023-08-01 15:25:19 -0700

    chore: try that

--- packages/amplify-e2e-core/src/utils/credentials-rotator.ts
+++ packages/amplify-e2e-core/src/utils/credentials-rotator.ts
@@ -1,4 +1,5 @@
 import { AssumeRoleCommand, STSClient } from '@aws-sdk/client-sts';
+import { fromContainerMetadata } from '@aws-sdk/credential-providers';
 import { generateRandomShortId, TEST_PROFILE_NAME } from './index';
 import * as ini from 'ini';
 import * as fs from 'fs-extra';
@@ -6,7 +7,10 @@ import { pathManager } from '@aws-amplify/amplify-cli-core';
 
 const refreshCredentials = async () => {
   try {
-    const client = new STSClient({});
+    const client = new STSClient({
+      // Use CodeBuild role to assume test account role. I.e. don't read credentials from process.env
+      credentials: fromContainerMetadata(),
+    });
     const roleArn = process.env.TEST_ACCOUNT_ROLE;
     const sessionName = `testSession${generateRandomShortId()}`;
     const command = new AssumeRoleCommand({
@@ -22,8 +26,11 @@ const refreshCredentials = async () => {
     credentialsContents[profileName].aws_access_key_id = response.Credentials.AccessKeyId;
     credentialsContents[profileName].aws_secret_access_key = response.Credentials.SecretAccessKey;
     credentialsContents[profileName].aws_session_token = response.Credentials.SessionToken;
+    process.env.AWS_ACCESS_KEY_ID = response.Credentials.AccessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = response.Credentials.SecretAccessKey;
+    process.env.AWS_SESSION_TOKEN = response.Credentials.SessionToken;
     await fs.writeFile(pathManager.getAWSCredentialsFilePath(), ini.stringify(credentialsContents));
-    console.log('Test profile credentials refreshed')
+    console.log('Test profile credentials refreshed');
   } catch (e) {
     console.error('Test profile credentials request failed');
     console.error(e);
