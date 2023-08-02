fe4c2b8b916f9d1f1d0b30face339f7301069856
Kamil Sobol <sobkamil@amazon.com>
2023-08-02 09:02:11 -0700

    chore: massaging code

--- packages/amplify-e2e-core/src/utils/credentials-rotator.ts
+++ packages/amplify-e2e-core/src/utils/credentials-rotator.ts
@@ -6,29 +6,33 @@ import * as fs from 'fs-extra';
 import { pathManager } from '@aws-amplify/amplify-cli-core';
 
 const refreshCredentials = async (roleArn: string) => {
-  try {
-    const client = new STSClient({
-      // Use CodeBuild role to assume test account role. I.e. don't read credentials from process.env
-      credentials: fromContainerMetadata(),
-    });
-    const sessionName = `testSession${generateRandomShortId()}`;
-    const command = new AssumeRoleCommand({
-      RoleArn: roleArn,
-      RoleSessionName: sessionName,
-      DurationSeconds: 3600,
-    });
-    const response = await client.send(command);
+  const client = new STSClient({
+    // Use CodeBuild role to assume test account role. I.e. don't read credentials from process.env
+    credentials: fromContainerMetadata(),
+  });
+  const sessionName = `testSession${generateRandomShortId()}`;
+  const command = new AssumeRoleCommand({
+    RoleArn: roleArn,
+    RoleSessionName: sessionName,
+    DurationSeconds: 3600,
+  });
+  const response = await client.send(command);
 
-    const profileName = TEST_PROFILE_NAME;
-    const credentialsContents = ini.parse((await fs.readFile(pathManager.getAWSCredentialsFilePath())).toString());
-    credentialsContents[profileName] = credentialsContents[profileName] || {};
-    credentialsContents[profileName].aws_access_key_id = response.Credentials.AccessKeyId;
-    credentialsContents[profileName].aws_secret_access_key = response.Credentials.SecretAccessKey;
-    credentialsContents[profileName].aws_session_token = response.Credentials.SessionToken;
-    process.env.AWS_ACCESS_KEY_ID = response.Credentials.AccessKeyId;
-    process.env.AWS_SECRET_ACCESS_KEY = response.Credentials.SecretAccessKey;
-    process.env.AWS_SESSION_TOKEN = response.Credentials.SessionToken;
-    await fs.writeFile(pathManager.getAWSCredentialsFilePath(), ini.stringify(credentialsContents));
+  const profileName = TEST_PROFILE_NAME;
+  const credentialsContents = ini.parse((await fs.readFile(pathManager.getAWSCredentialsFilePath())).toString());
+  credentialsContents[profileName] = credentialsContents[profileName] || {};
+  credentialsContents[profileName].aws_access_key_id = response.Credentials.AccessKeyId;
+  credentialsContents[profileName].aws_secret_access_key = response.Credentials.SecretAccessKey;
+  credentialsContents[profileName].aws_session_token = response.Credentials.SessionToken;
+  process.env.AWS_ACCESS_KEY_ID = response.Credentials.AccessKeyId;
+  process.env.AWS_SECRET_ACCESS_KEY = response.Credentials.SecretAccessKey;
+  process.env.AWS_SESSION_TOKEN = response.Credentials.SessionToken;
+  await fs.writeFile(pathManager.getAWSCredentialsFilePath(), ini.stringify(credentialsContents));
+};
+
+const tryRefreshCredentials = async (roleArn: string) => {
+  try {
+    await refreshCredentials(roleArn);
     console.log('Test profile credentials refreshed');
   } catch (e) {
     console.error('Test profile credentials request failed');
@@ -36,10 +40,18 @@ const refreshCredentials = async (roleArn: string) => {
   }
 };
 
-let isRotationScheduled = false;
+let isRotationBackgroundTaskIsAlreadyScheduled = false;
 
+/**
+ * Schedules a background task that attempts to refresh test account credentials
+ * on given interval.
+ *
+ * No-op outside Amplify CI environment.
+ *
+ * No-op if a background task has already been scheduled.
+ */
 export const tryScheduleCredentialRefresh = () => {
-  if (!process.env.IS_AMPLIFY_CI || !process.env.TEST_ACCOUNT_ROLE || isRotationScheduled) {
+  if (!process.env.IS_AMPLIFY_CI || !process.env.TEST_ACCOUNT_ROLE || isRotationBackgroundTaskIsAlreadyScheduled) {
     return;
   }
 
@@ -47,11 +59,12 @@ export const tryScheduleCredentialRefresh = () => {
     throw new Error('Credentials rotator supports only tests running in parent account at this time');
   }
 
+  // Attempts to refresh credentials in background every 15 minutes.
   setInterval(() => {
-    void refreshCredentials(process.env.TEST_ACCOUNT_ROLE);
+    void tryRefreshCredentials(process.env.TEST_ACCOUNT_ROLE);
   }, 15 * 60 * 1000);
 
-  isRotationScheduled = true;
+  isRotationBackgroundTaskIsAlreadyScheduled = true;
 
   console.log('Test profile credentials refresh was scheduled');
 };
