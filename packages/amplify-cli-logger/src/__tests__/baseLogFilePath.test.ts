1cf233e94cf861a232b9fabe4b11a96dc8c90078
Mikołaj Żarnawski <mzarnawski@gmail.com>
2023-08-07 00:36:39 +0200

    refactor(amplify-cli-logger): simplify baseLogFilePath test

--- packages/amplify-cli-logger/src/__tests__/baseLogFilePath.test.ts
+++ packages/amplify-cli-logger/src/__tests__/baseLogFilePath.test.ts
@@ -4,18 +4,16 @@ import { constants } from '../constants';
 import { getLogDirectory, getLocalLogFileDirectory } from '../baseLogFilePath';
 
 describe('test base path creation', () => {
-  const homedirectory = 'home';
-  const localPath = 'myProj';
   const slash = path.sep;
 
-  jest.spyOn(os, 'homedir').mockReturnValue(homedirectory);
+  jest.spyOn(os, 'homedir').mockReturnValue('home');
 
   afterAll(() => {
     jest.clearAllMocks();
   });
 
   it('should create path for logs folder in a specified directory', () => {
-    const result = getLocalLogFileDirectory(localPath);
+    const result = getLocalLogFileDirectory('myProj');
     expect(result).toBe('myProj' + slash + constants.LOG_DIRECTORY);
   });
 
