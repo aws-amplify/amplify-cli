717f145ab48e2812ec50e8688cea251fcf8fb606
Kamil Sobol <sobkamil@amazon.com>
2023-08-18 13:11:01 -0700

    ci: add sanity check to cb checkout script

--- .circleci/codebuild-checkout.sh
+++ .circleci/codebuild-checkout.sh
@@ -2,6 +2,9 @@
 
 # This script checks out a branch & loads git tags.
 
+# Get the hash that CodeBuild used to start workflow and use it later to validate that we didn't change it after transformations below.
+INITIAL_HEAD_HASH=$(git rev-parse HEAD)
+
 git status
 
 echo "CODEBUILD_SOURCE_VERSION=$CODEBUILD_SOURCE_VERSION"
@@ -37,3 +40,11 @@ git show --summary
 
 echo "Fetching tags"
 git fetch --all --tags
+
+# A sanity check that we haven't altered commit we're building from. This must be last section in this script
+HEAD_HASH=$(git rev-parse HEAD)
+if [[ "$INITIAL_HEAD_HASH" != "$HEAD_HASH" ]]
+  echo "Fail! Detected a drift of commit we attempt to build!"
+  echo "INITIAL_HEAD_HASH=$INITIAL_HEAD_HASH"
+  echo "HEAD_HASH=$HEAD_HASH"
+fi
