5212e90fd2594d8f9988e0a00b533dcad4c09caa
Kamil Sobol <sobkamil@amazon.com>
2023-08-24 10:06:38 -0700

    ci: fix test

--- packages/amplify-frontend-ios/tests/xcode.integration.test.js
+++ packages/amplify-frontend-ios/tests/xcode.integration.test.js
@@ -1,5 +1,7 @@
 const { importConfig, importModels } = require('../lib/amplify-xcode');
 const path = require('path');
+const fs = require('fs/promises');
+const { existsSync } = require('fs');
 
 jest.mock('@aws-amplify/amplify-cli-core', () => {
   const path = require('path');
@@ -13,14 +15,30 @@ jest.mock('@aws-amplify/amplify-cli-core', () => {
 
 describe('Should call amplify-xcode binary', () => {
   if (process.platform === 'darwin') {
+    const pathsToTest = [];
+    beforeAll(async () => {
+      const source = path.resolve(__dirname, 'sample-xcode-project');
+      pathsToTest.push(source);
+      const destination = path.resolve(__dirname, 'sample xcode project');
+      pathsToTest.push(destination);
+      if (!existsSync(destination)) {
+        await fs.mkdir(destination);
+        await fs.cp(source, destination, {
+          recursive: true,
+        });
+      }
+    });
+
     it('should importConfig', async () => {
-      const projPath = path.resolve(__dirname, 'sample xcode project');
-      await importConfig({ path: projPath });
+      for (const projPath of pathsToTest) {
+        await importConfig({ path: projPath });
+      }
     });
 
     it('should importModels', async () => {
-      const projPath = path.resolve(__dirname, 'sample xcode project');
-      await importModels({ path: projPath });
+      for (const projPath of pathsToTest) {
+        await importModels({ path: projPath });
+      }
     });
   } else {
     it('dummy test', () => {
