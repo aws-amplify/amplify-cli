e90235fa8a5898672a2f6f740eb9c388c22c866d
Kamil Sobol <sobkamil@amazon.com>
2023-08-24 13:23:38 -0700

    Revert "ci: whyyy"
    This reverts commit 302f9da81b922aa128ea5b7fb2966bf10f282314.

--- packages/amplify-cli/src/__tests__/serializable-error.test.ts
+++ packages/amplify-cli/src/__tests__/serializable-error.test.ts
@@ -3,12 +3,11 @@ import * as os from 'os';
 import { SerializableError } from '../domain/amplify-usageData/SerializableError';
 
 describe('test serializabe error', () => {
-  it.only('test SerializableError with regular stack trace', () => {
+  it('test SerializableError with regular stack trace', () => {
     const error = new Error('test error');
     const serializableError = new SerializableError(error);
     expect(serializableError.name).toBe('Error');
     expect(serializableError.trace).toBeDefined();
-    console.log(`serializableError.trace ${JSON.stringify(serializableError.trace)}`);
     serializableError.trace?.forEach((trace) => {
       expect(trace.file).not.toContain(os.homedir());
     });
