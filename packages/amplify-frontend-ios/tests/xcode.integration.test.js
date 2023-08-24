ceeb33ca892bc4ccf452c2fe34e944d372a575cb
Kamil Sobol <sobkamil@amazon.com>
2023-08-24 09:56:40 -0700

    ci: fix test

--- packages/amplify-frontend-ios/tests/xcode.integration.test.js
+++ packages/amplify-frontend-ios/tests/xcode.integration.test.js
@@ -1,18 +1,18 @@
 const { importConfig, importModels } = require('../lib/amplify-xcode');
 const path = require('path');
 
-if (process.platform === 'darwin') {
-  jest.mock('@aws-amplify/amplify-cli-core', () => {
-    const path = require('path');
-    const amplifyIosResourcePath = path.resolve(__dirname, '..');
-    return {
-      pathManager: {
-        getAmplifyPackageLibDirPath: jest.fn().mockReturnValue(amplifyIosResourcePath),
-      },
-    };
-  });
+jest.mock('@aws-amplify/amplify-cli-core', () => {
+  const path = require('path');
+  const amplifyIosResourcePath = path.resolve(__dirname, '..');
+  return {
+    pathManager: {
+      getAmplifyPackageLibDirPath: jest.fn().mockReturnValue(amplifyIosResourcePath),
+    },
+  };
+});
 
-  describe('Should call amplify-xcode binary', () => {
+describe('Should call amplify-xcode binary', () => {
+  if (process.platform === 'darwin') {
     it('should importConfig', async () => {
       const projPath = path.resolve(__dirname, 'sample xcode project');
       await importConfig({ path: projPath });
@@ -22,7 +22,9 @@ if (process.platform === 'darwin') {
       const projPath = path.resolve(__dirname, 'sample xcode project');
       await importModels({ path: projPath });
     });
-  });
-} else {
-  console.log('Xcode integration test can only run on macOS');
-}
+  } else {
+    it('dummy test', () => {
+      console.log('Xcode integration test can only run on macOS');
+    });
+  }
+});
