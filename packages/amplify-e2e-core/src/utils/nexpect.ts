f7573eee7675e2f26963b88f5349a1fe1b07af87
Kamil Sobol <sobkamil@amazon.com>
2023-08-25 14:32:27 -0700

    try this

--- packages/amplify-e2e-core/src/utils/nexpect.ts
+++ packages/amplify-e2e-core/src/utils/nexpect.ts
@@ -742,7 +742,7 @@ export function nspawn(command: string | string[], params: string[] = [], option
   }
 
   const testingWithLatestCodebase = isTestingWithLatestCodebase(command);
-  if (testingWithLatestCodebase || (process.platform === 'win32' && !command.endsWith('.exe'))) {
+  if (testingWithLatestCodebase) {
     params.unshift(command);
     command = getScriptRunnerPath(testingWithLatestCodebase);
   }
