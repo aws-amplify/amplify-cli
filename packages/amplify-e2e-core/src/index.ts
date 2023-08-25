4d9b8261c8b97d0964f3e100389ad75f38ea2d7e
Kamil Sobol <sobkamil@amazon.com>
2023-08-25 09:16:26 -0700

    chore: undo logs

--- packages/amplify-e2e-core/src/index.ts
+++ packages/amplify-e2e-core/src/index.ts
@@ -34,18 +34,14 @@ declare global {
 const amplifyTestsDir = 'amplify-e2e-tests';
 
 export function getCLIPath(testingWithLatestCodebase = false) {
-  console.log(`getCLIPath PATH ${process.env.PATH}`);
   if (!testingWithLatestCodebase) {
     if (process.env.AMPLIFY_PATH && fs.existsSync(process.env.AMPLIFY_PATH)) {
-      console.log(`getCLIPath using process.env.AMPLIFY_PATH ${process.env.AMPLIFY_PATH}`);
       return process.env.AMPLIFY_PATH;
     }
-    console.log(`getCLIPath using amplify or amplify.exe`);
     return process.platform === 'win32' ? 'amplify.exe' : 'amplify';
   }
 
   const amplifyScriptPath = path.join(__dirname, '..', '..', 'amplify-cli', 'bin', 'amplify');
-  console.log(`getCLIPath using amplifyScriptPath ${amplifyScriptPath}`);
   return amplifyScriptPath;
 }
 
