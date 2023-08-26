eee56612215618c7d23ab350caab3963af4add70
Kamil Sobol <sobkamil@amazon.com>
2023-08-26 10:06:37 -0700

    try this

---
+++ .github/actions/run-smoke-tests-windows/README.md
@@ -0,0 +1,2 @@
+The Run Smoke Tests contains lightweight version of smoke tests for Windows.
+GitHub Action's Windows workers are small and using our E2E testing framework for smoke testing overwhelms them.
--- .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
+++ .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
@@ -12,3 +12,23 @@ mkdir $projDir
 cd $projDir
 
 $AMPLIFY_PATH init --yes
+
+$AMPLIFY_PATH status
+
+ADD_AUTH_REQUEST=$(cat ../.github/actions/run-smoke-tests-windows/add_auth_request.json)
+$AMPLIFY_PATH add auth --headless <<< $ADD_AUTH_REQUEST
+
+$AMPLIFY_PATH status
+
+$AMPLIFY_PATH push --yes
+
+$AMPLIFY_PATH status
+
+ADD_API_REQUEST=$(cat ../.github/actions/run-smoke-tests-windows/add_api_request.json)
+$AMPLIFY_PATH add api --headless <<< $ADD_API_REQUEST
+
+$AMPLIFY_PATH status
+
+$AMPLIFY_PATH push --yes
+
+$AMPLIFY_PATH status
