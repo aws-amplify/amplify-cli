1b396ed497e0db53d38eaabbab4826b507db4c69
Kamil Sobol <sobkamil@amazon.com>
2023-08-26 08:55:12 -0700

    try this

---
+++ .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$AMPLIFY_PATH version
+
+random_guid=$(powershell -Command "[guid]::NewGuid().ToString()")
+random_guid=${random_guid//-/}
+random_guid=${random_guid:0:6}
+projDir="smoke-test-${random_guid}"
+echo "AMPLIFY_PROJ_DIR=$projDir" >> $GITHUB_ENV
+
+mkdir $projDir
+cd $projDir
+
