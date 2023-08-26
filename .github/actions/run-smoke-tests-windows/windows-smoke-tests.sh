cfdacaf4d6eba616e6abe668f9dd089485612d91
Kamil Sobol <sobkamil@amazon.com>
2023-08-26 10:27:30 -0700

    try this

--- .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
+++ .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
@@ -16,6 +16,7 @@ $AMPLIFY_PATH init --yes
 $AMPLIFY_PATH status
 
 ADD_AUTH_REQUEST=$(cat ../.github/actions/run-smoke-tests-windows/add_auth_request.json)
+ADD_AUTH_REQUEST=${ADD_AUTH_REQUEST//[$'\r\n']}
 echo $ADD_AUTH_REQUEST
 $AMPLIFY_PATH add auth --headless <<< $ADD_AUTH_REQUEST
 
@@ -26,6 +27,7 @@ $AMPLIFY_PATH push --yes
 $AMPLIFY_PATH status
 
 ADD_API_REQUEST=$(cat ../.github/actions/run-smoke-tests-windows/add_api_request.json)
+ADD_API_REQUEST=${ADD_API_REQUEST//[$'\r\n']}
 echo $ADD_API_REQUEST
 $AMPLIFY_PATH add api --headless <<< $ADD_API_REQUEST
 
