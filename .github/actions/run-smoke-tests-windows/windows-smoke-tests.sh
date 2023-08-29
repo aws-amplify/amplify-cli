8afc86deb14ebdcf225ab248a10d190db7bad580
Kamil Sobol <sobkamil@amazon.com>
2023-08-29 11:00:42 -0700

    chore: pr feedback

--- .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
+++ .github/actions/run-smoke-tests-windows/windows-smoke-tests.sh
@@ -15,7 +15,8 @@ $AMPLIFY_PATH init --yes
 
 $AMPLIFY_PATH status
 
-ADD_AUTH_REQUEST=$(cat ../.github/actions/run-smoke-tests-windows/add_auth_request.json)
+THIS_SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+ADD_AUTH_REQUEST=$(cat $THIS_SCRIPT_PATH/add_auth_request.json)
 ADD_AUTH_REQUEST=${ADD_AUTH_REQUEST//[$'\r\n']}
 echo $ADD_AUTH_REQUEST
 $AMPLIFY_PATH add auth --headless <<< $ADD_AUTH_REQUEST
@@ -26,7 +27,7 @@ $AMPLIFY_PATH push --yes
 
 $AMPLIFY_PATH status
 
-ADD_API_REQUEST=$(cat ../.github/actions/run-smoke-tests-windows/add_api_request.json)
+ADD_API_REQUEST=$(cat $THIS_SCRIPT_PATH/add_api_request.json)
 ADD_API_REQUEST=${ADD_API_REQUEST//[$'\r\n']}
 echo $ADD_API_REQUEST
 $AMPLIFY_PATH add api --headless <<< $ADD_API_REQUEST
