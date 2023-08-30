80d48613b0eb698430a1552213567b52557695cc
Kamil Sobol <sobkamil@amazon.com>
2023-08-29 19:10:12 -0700

    fix: include missing regions

--- packages/amplify-e2e-tests/src/cleanup-codebuild-resources.ts
+++ packages/amplify-e2e-tests/src/cleanup-codebuild-resources.ts
@@ -19,8 +19,10 @@ const AWS_REGIONS_TO_RUN_TESTS = [
   'us-east-2',
   'us-west-2',
   'eu-west-2',
+  'eu-west-3',
   'eu-central-1',
   'ap-northeast-1',
+  'ap-northeast-2',
   'ap-southeast-1',
   'ap-southeast-2',
 ];
