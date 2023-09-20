4fc910806e1cd6d5ef57faed7daa49dc625e4ffd
Kamil Sobol <sobkamil@amazon.com>
2023-09-20 10:34:08 -0700

    fix: test

--- packages/amplify-e2e-tests/src/__tests__/aws-exports/js-frontend-config.test.ts
+++ packages/amplify-e2e-tests/src/__tests__/aws-exports/js-frontend-config.test.ts
@@ -10,24 +10,26 @@ import {
   getNpxPath,
   initJSProjectWithProfile,
 } from '@aws-amplify/amplify-e2e-core';
-import {} from '@aws-amplify/amplify-e2e-core';
+import * as babel from '@babel/core';
+import * as babelTransformEsmToCjs from '@babel/plugin-transform-modules-commonjs';
+import Module from 'module';
 
 describe('JS frontend config tests', () => {
   let projRoot: string;
   let appRoot: string;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     projRoot = await createNewProjectDir('aws-exports-test');
+    appRoot = await createReactApp(projRoot, 'reactapp');
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
+    // extra delete in case test fails, delete is noop if project was already deleted
+    await deleteProject(appRoot);
     deleteProjectDir(projRoot);
   });
 
-  test('both aws-exports.js & amplifyconfiguration.json is generated on init', async () => {
-    // init react app using create-react-app
-    appRoot = await createReactApp(projRoot, 'reactapp');
-
+  test('both aws-exports.js & amplifyconfiguration.json is generated', async () => {
     // amplify init, this will generate aws-exports in the project
     await initJSProjectWithProfile(appRoot, { name: 'reactapp' });
 
@@ -35,31 +37,30 @@ describe('JS frontend config tests', () => {
     expect(fs.existsSync(`${appRoot}/src/aws-exports.js`)).toBeTruthy();
     expect(fs.existsSync(`${appRoot}/src/amplifyconfiguration.json`)).toBeTruthy();
 
-    const awsExportsModule = await import(`${appRoot}/src/aws-exports.js`);
-    const amplifyconfiguration = fs.readJSONSync(`${appRoot}/src/amplifyconfiguration.json`);
+    const initialAwsExportsModule = readAwsExports(appRoot, `${appRoot}/src/aws-exports.js`);
+    const initialAmplifyConfiguration = fs.readJSONSync(`${appRoot}/src/amplifyconfiguration.json`);
 
     // contents should match
-    expect(amplifyconfiguration).toMatchObject(awsExportsModule.default);
-  });
+    expect(initialAwsExportsModule).toMatchObject(initialAmplifyConfiguration);
 
-  test('should update both both aws-exports.js & amplifyconfiguration.json', async () => {
-    // add an category
+    // should update both aws-exports.js & amplifyconfiguration.json
+    // add a category
     await addAuthWithDefault(appRoot);
     await amplifyPushAuth(appRoot);
 
     // when aws-exports updates, both files should still exist
-    const awsExportsModule = await import(`${appRoot}/src/aws-exports.js`);
-    const amplifyconfiguration = fs.readJSONSync(`${appRoot}/src/amplifyconfiguration.json`);
+    const awsExportsModule = readAwsExports(appRoot, `${appRoot}/src/aws-exports.js`);
+    const amplifyConfiguration = fs.readJSONSync(`${appRoot}/src/amplifyconfiguration.json`);
 
     // config should have updated
-    expect(amplifyconfiguration.aws_user_pools_id).toBeDefined();
+    expect(amplifyConfiguration.aws_user_pools_id).toBeDefined();
 
     // contents should match
-    expect(amplifyconfiguration).toMatchObject(awsExportsModule.default);
-  });
+    expect(amplifyConfiguration).toMatchObject(awsExportsModule);
+    expect(amplifyConfiguration).not.toStrictEqual(initialAmplifyConfiguration);
 
-  test('both aws-exports.js & amplifyconfiguraion.json should be removed on delete', async () => {
-    await deleteProject(projRoot);
+    // both aws-exports.js & amplifyconfiguration.json should be removed on delete
+    await deleteProject(appRoot);
 
     // when amplify app gets deleted, all config should be removed
     expect(fs.existsSync(`${appRoot}/src/aws-exports.js`)).toBeFalsy();
@@ -71,7 +72,27 @@ describe('JS frontend config tests', () => {
 async function createReactApp(cwd: string, projectName: string) {
   const projectRoot = `${cwd}/${projectName}`;
 
-  execa.sync(getNpxPath(), ['create-react-app', projectName, '--use-npm'], { cwd, encoding: 'utf-8' });
+  execa.sync(getNpxPath(), ['create-react-app', '--scripts-version', '5.0.1', projectName, '--use-npm'], { cwd, encoding: 'utf-8' });
 
   return projectRoot;
+}
+
+/**
+ * Reads and transpiles aws exports. Jest doesn't like ES6 modules.
+ */
+function readAwsExports(projectPath: string, targetFilePath: string) {
+  const fileContents = fs.readFileSync(targetFilePath, 'utf-8');
+  // transpile the file contents to CommonJS
+  const { code } = babel.transformSync(fileContents, {
+    plugins: [babelTransformEsmToCjs],
+    configFile: false,
+    babelrc: false,
+  });
+  const mod = new Module('test_module');
+  // @ts-ignore
+  mod._compile(code, 'aws-exports.js');
+  // add paths to the module to account for node_module imports in aws-exports.js (should there be any)
+  mod.paths = [projectPath];
+  // the transpiled result will contain `exports.default`
+  return mod.exports?.default || mod.exports;
 }
