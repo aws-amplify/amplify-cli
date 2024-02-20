173d769adaef58aa2f1fcbf5a97c0907eaa2527b
Kamil Sobol <sobkamil@amazon.com>
2024-02-20 14:01:51 -0800

    chore: remove some v8 migration tests

--- scripts/split-e2e-test-filters.ts
+++ scripts/split-e2e-test-filters.ts
@@ -1,11 +1,6 @@
 export const migrationFromV8Tests = [
-  'src/__tests__/migration_tests/notifications-migration/notifications-migration-2.test.ts',
-  'src/__tests__/migration_tests/notifications-migration/notifications-migration-3.test.ts',
-  'src/__tests__/migration_tests/notifications-migration/notifications-migration-4.test.ts',
-  'src/__tests__/migration_tests/notifications-migration/notifications-migration.test.ts',
   'src/__tests__/migration_tests/transformer_migration/api.key.migration-2.test.ts',
   'src/__tests__/migration_tests/transformer_migration/api.key.migration.test.ts',
-  'src/__tests__/update_tests/function_migration_update.test.ts',
 ];
 
 export const migrationFromV12Tests = [
