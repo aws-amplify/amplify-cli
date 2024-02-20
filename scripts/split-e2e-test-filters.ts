abe24b7c0d2656989b993b5ce27e7ded8d6112d5
Kamil Sobol <sobkamil@amazon.com>
2024-02-20 12:44:43 -0800

    chore: remove v10 migration tests

--- scripts/split-e2e-test-filters.ts
+++ scripts/split-e2e-test-filters.ts
@@ -8,20 +8,6 @@ export const migrationFromV8Tests = [
   'src/__tests__/update_tests/function_migration_update.test.ts',
 ];
 
-export const migrationFromV10Tests = [
-  'src/__tests__/migration_tests_v10/api-graphql-v2.migration.test.ts',
-  'src/__tests__/migration_tests_v10/api-rest-basic.migration.test.ts',
-  'src/__tests__/migration_tests_v10/api-rest-lambda.migration.test.ts',
-  'src/__tests__/migration_tests_v10/api-rest-serverless.migration.test.ts',
-  'src/__tests__/migration_tests_v10/auth-add-all.migration.test.ts',
-  'src/__tests__/migration_tests_v10/auth-override.migration.test.ts',
-  'src/__tests__/migration_tests_v10/custom-stack.migration.test.ts',
-  'src/__tests__/migration_tests_v10/git-clone-migration-tests.test.ts',
-  'src/__tests__/migration_tests_v10/geo.migration.test.ts',
-  'src/__tests__/migration_tests_v10/scaffold.test.ts',
-  'src/__tests__/migration_tests_v10/storage.migration.test.ts',
-  'src/__tests__/migration_tests_v10/pinpoint-region.migration.test.ts',
-];
 export const migrationFromV12Tests = [
   'src/__tests__/migration_tests_v12/auth.migration.test.ts',
   'src/__tests__/migration_tests_v12/auth-app-client-secret-migration.test.ts',
