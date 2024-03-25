9e0a625d56d636d8f7cfb9f3475de99fff5994ce
0.618 <o.6180339@gmail.com>
2024-03-25 15:55:15 -0700

    fix: e2e l_diagnose_hooks_a_mock_api

--- packages/amplify-e2e-core/src/diagnose/index.ts
+++ packages/amplify-e2e-core/src/diagnose/index.ts
@@ -13,8 +13,6 @@ export const diagnoseSendReport = async (cwd: string): Promise<string> => {
   };
   await spawn(getCLIPath(), ['diagnose', '--send-report'], { cwd, stripColors: true })
     .wait(/Report saved/, callback)
-    .wait(/Done/)
-    .sendEof()
     .runAsync();
   return path;
 };
