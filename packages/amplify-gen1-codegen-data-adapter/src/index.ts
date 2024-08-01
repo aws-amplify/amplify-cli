256d23f621f9928b2c8853da4ecdcc60ff2d0f23
Spencer Dean Stolworthy <sstol@amazon.com>
2024-08-01 13:30:33 -0700

    fix: add tests for data adapter, fix lints

--- packages/amplify-gen1-codegen-data-adapter/src/index.ts
+++ packages/amplify-gen1-codegen-data-adapter/src/index.ts
@@ -1,12 +1,1 @@
-import assert from 'node:assert';
-import { DataDefinition } from '@aws-amplify/amplify-gen2-codegen';
-import { Stack } from '@aws-sdk/client-cloudformation';
-
-const tableMappingKey = 'TableMapping';
-
-export const getDataDefinition = (dataStack: Stack): DataDefinition => {
-  const rawTableMapping = dataStack.Outputs?.find((o) => o.OutputKey === tableMappingKey)?.OutputValue;
-  assert(rawTableMapping);
-  const tableMapping = JSON.parse(rawTableMapping);
-  return { tableMapping };
-};
+export { getDataDefinition } from './get_data_definition';
