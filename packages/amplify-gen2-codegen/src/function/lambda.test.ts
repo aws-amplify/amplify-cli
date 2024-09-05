8b679a64f20d30f7399302c17599538589381a4d
Surbhi Bansal <bzsurbhi@amazon.com>
2024-09-05 16:39:20 -0700

    feat: added functions codegen

--- packages/amplify-gen2-codegen/src/function/lambda.test.ts
+++ packages/amplify-gen2-codegen/src/function/lambda.test.ts
@@ -1,7 +1,8 @@
 import ts, { Identifier } from 'typescript';
 import assert from 'node:assert';
-import { createDefineFunctionCall, DefineFunctionParameter } from './lambda';
+import { createDefineFunctionCall } from './lambda';
 import { printNode } from '../test_utils/ts_node_printer';
+import { FunctionDefinition } from './source_builder';
 
 describe('createDefineFunctionCall', () => {
   it('creates a call expression to defineFunction', () => {
@@ -11,24 +12,25 @@ describe('createDefineFunctionCall', () => {
     assert.equal(id.escapedText, 'defineFunction');
     assert.equal(fn.arguments.at(0)?.kind, ts.SyntaxKind.ObjectLiteralExpression);
   });
-  const parameter: DefineFunctionParameter = {
-    runtime: 18,
+  const parameter: FunctionDefinition = {
     memoryMB: 1024,
-    timeoutSeconds: '35',
+    timeoutSeconds: 35,
     name: 'my-hello-world',
     entry: './hello-world',
   };
   describe('function parameters', () => {
     it('renders the environment object', () => {
       const environment = {
-        hello: 'world',
-        foo: 'bar',
+        Variables: {
+          hello: 'world',
+          foo: 'bar',
+        },
       };
 
       const fn = createDefineFunctionCall({ environment });
       const output = printNode(fn);
 
-      for (const [envKey, envValue] of Object.entries(environment)) {
+      for (const [envKey, envValue] of Object.entries(environment.Variables)) {
         assert.match(output, new RegExp(`${envKey}: "${envValue}"`));
       }
       assert.match(output, /environment: \{/);
@@ -37,7 +39,6 @@ describe('createDefineFunctionCall', () => {
       it(`${key} renders expected value ${value}`, () => {
         const fn = createDefineFunctionCall(parameter);
         const output = printNode(fn);
-
         assert.match(output, new RegExp(`${key}: "?${value}"?`));
       });
     }
