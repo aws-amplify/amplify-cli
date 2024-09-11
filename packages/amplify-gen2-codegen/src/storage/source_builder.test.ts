c2c5969f083abc4d3701c03403b9873e0fe4e717
Surbhi Bansal <bzsurbhi@amazon.com>
2024-09-10 22:30:31 -0700

    feat: updated functions codegen

--- packages/amplify-gen2-codegen/src/storage/source_builder.test.ts
+++ packages/amplify-gen2-codegen/src/storage/source_builder.test.ts
@@ -1,81 +1,81 @@
-// import assert from 'node:assert';
-// import { AccessPatterns, Permission, renderStorage } from './source_builder';
-// import { printNodeArray } from '../test_utils/ts_node_printer';
-// import { getImportRegex } from '../test_utils/import_regex';
+import assert from 'node:assert';
+import { AccessPatterns, Permission, renderStorage } from './source_builder';
+import { printNodeArray } from '../test_utils/ts_node_printer';
+import { getImportRegex } from '../test_utils/import_regex';
 
-// describe('Storage source generation', () => {
-//   describe('imports', () => {
-//     it('renders the defineStorage import', () => {
-//       const rendered = renderStorage();
-//       const output = printNodeArray(rendered);
-//       const regex = getImportRegex('defineStorage', '@aws-amplify/backend');
-//       assert.match(output, regex);
-//     });
-//   });
-//   describe('defineStorage', () => {
-//     describe('parameters', () => {
-//       it('does not render `name` if `storageIdentifier` is undefined', () => {
-//         const rendered = renderStorage();
-//         const output = printNodeArray(rendered);
+describe('Storage source generation', () => {
+  describe('imports', () => {
+    it('renders the defineStorage import', () => {
+      const rendered = renderStorage();
+      const output = printNodeArray(rendered);
+      const regex = getImportRegex('defineStorage', '@aws-amplify/backend');
+      assert.match(output, regex);
+    });
+  });
+  describe('defineStorage', () => {
+    describe('parameters', () => {
+      it('does not render `name` if `storageIdentifier` is undefined', () => {
+        const rendered = renderStorage();
+        const output = printNodeArray(rendered);
 
-//         assert(!output.includes(`name:`));
-//       });
-//       it('renders `name` if the `storageIdentifier` is passed', () => {
-//         const storageIdentifier = 'my-cool-bucket';
-//         const rendered = renderStorage({ storageIdentifier });
-//         const output = printNodeArray(rendered);
+        assert(!output.includes(`name:`));
+      });
+      it('renders `name` if the `storageIdentifier` is passed', () => {
+        const storageIdentifier = 'my-cool-bucket';
+        const rendered = renderStorage({ storageIdentifier });
+        const output = printNodeArray(rendered);
 
-//         assert(output.includes(`name: "${storageIdentifier}"`));
-//       });
-//       const permissions: Permission[] = ['read', 'write', 'delete'];
+        assert(output.includes(`name: "${storageIdentifier}"`));
+      });
+      const permissions: Permission[] = ['read', 'write', 'delete'];
 
-//       describe('access parameters', () => {
-//         describe('groups', () => {
-//           it(`renders a comment when group permissions are present in Gen 1`, () => {
-//             const groupName = 'manager';
-//             const accessPatterns: AccessPatterns = {
-//               groups: { [groupName]: ['read'] },
-//             };
-//             const rendered = renderStorage({ accessPatterns });
-//             const output = printNodeArray(rendered);
-//             assert.match(output, new RegExp(`Your project uses group permissions.`));
-//           });
-//           it(`does not render a comment when group permissions are not present in Gen 1`, () => {
-//             const accessPatterns: AccessPatterns = {};
-//             const rendered = renderStorage({ accessPatterns });
-//             const output = printNodeArray(rendered);
-//             assert.doesNotMatch(output, new RegExp(`Your project uses group permissions.`));
-//           });
-//         });
-//         const accessLevels = ['private', 'public', 'protected'];
-//         for (const accessLevel of accessLevels) {
-//           describe('authenticated', () => {
-//             for (const permission of permissions) {
-//               it(`grants ${permission} to ${accessLevel} path for authenticated user`, () => {
-//                 const accessPatterns: AccessPatterns = {
-//                   auth: [permission],
-//                 };
-//                 const rendered = renderStorage({ accessPatterns });
-//                 const output = printNodeArray(rendered);
-//                 assert.match(output, new RegExp(`${accessLevel}.*?allow.authenticated.to\\(\\["${permission}"\\]\\)`));
-//               });
-//             }
-//           });
-//         }
-//         describe('guest', () => {
-//           for (const permission of permissions) {
-//             it(`grants ${permission} to public path to guest`, async () => {
-//               const accessPatterns: AccessPatterns = {
-//                 guest: [permission],
-//               };
-//               const rendered = renderStorage({ accessPatterns });
-//               const output = printNodeArray(rendered);
+      describe('access parameters', () => {
+        describe('groups', () => {
+          it(`renders a comment when group permissions are present in Gen 1`, () => {
+            const groupName = 'manager';
+            const accessPatterns: AccessPatterns = {
+              groups: { [groupName]: ['read'] },
+            };
+            const rendered = renderStorage({ accessPatterns });
+            const output = printNodeArray(rendered);
+            assert.match(output, new RegExp(`Your project uses group permissions.`));
+          });
+          it(`does not render a comment when group permissions are not present in Gen 1`, () => {
+            const accessPatterns: AccessPatterns = {};
+            const rendered = renderStorage({ accessPatterns });
+            const output = printNodeArray(rendered);
+            assert.doesNotMatch(output, new RegExp(`Your project uses group permissions.`));
+          });
+        });
+        const accessLevels = ['private', 'public', 'protected'];
+        for (const accessLevel of accessLevels) {
+          describe('authenticated', () => {
+            for (const permission of permissions) {
+              it(`grants ${permission} to ${accessLevel} path for authenticated user`, () => {
+                const accessPatterns: AccessPatterns = {
+                  auth: [permission],
+                };
+                const rendered = renderStorage({ accessPatterns });
+                const output = printNodeArray(rendered);
+                assert.match(output, new RegExp(`${accessLevel}.*?allow.authenticated.to\\(\\["${permission}"\\]\\)`));
+              });
+            }
+          });
+        }
+        describe('guest', () => {
+          for (const permission of permissions) {
+            it(`grants ${permission} to public path to guest`, async () => {
+              const accessPatterns: AccessPatterns = {
+                guest: [permission],
+              };
+              const rendered = renderStorage({ accessPatterns });
+              const output = printNodeArray(rendered);
 
-//               assert.match(output, new RegExp(`public.*?allow.guest.to\\(\\["${permission}"\\]\\)`));
-//             });
-//           }
-//         });
-//       });
-//     });
-//   });
-// });
+              assert.match(output, new RegExp(`public.*?allow.guest.to\\(\\["${permission}"\\]\\)`));
+            });
+          }
+        });
+      });
+    });
+  });
+});
