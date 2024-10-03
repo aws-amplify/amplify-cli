0ecad3ff993fa3af67633f674b9abc064845d2e0
Sanay Yogesh Shah <sanayyog@amazon.com>
2024-10-03 11:22:29 -0700

    feat: storage and functions codegen integration tests

--- packages/amplify-migration-codegen-e2e/src/__tests__/migration_codegen_e2e.test.ts
+++ packages/amplify-migration-codegen-e2e/src/__tests__/migration_codegen_e2e.test.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import assert from 'node:assert';
-import { createNewProjectDir } from '@aws-amplify/amplify-e2e-core';
+import { createNewProjectDir, sleep } from '@aws-amplify/amplify-e2e-core';
 import {
   cleanupProjects,
   setupAndPushGen1Project,
@@ -8,6 +8,9 @@ import {
   runCodegenCommand,
   runGen2SandboxCommand,
   assertUserPoolResource,
+  assertStorageResource,
+  assertFunctionResource,
+  copyTsFile,
 } from '../helpers';
 
 void describe('Migration Codegen E2E tests', () => {
@@ -21,11 +24,27 @@ void describe('Migration Codegen E2E tests', () => {
     await cleanupProjects(projRoot);
   });
 
-  void it('performs full migration codegen flow with Auth backend', async () => {
+  void it('performs full migration codegen flow with backend', async () => {
     await setupAndPushGen1Project(projRoot, 'CodegenTest');
-    const { gen1UserPoolId, gen1Region } = await assertGen1Setup(projRoot);
+    const { gen1UserPoolId, gen1FunctionName, gen1BucketName, gen1Region } = await assertGen1Setup(projRoot);
     await assert.doesNotReject(runCodegenCommand(projRoot), 'Codegen failed');
+    await sleep(10000); // 10 seconds
+    const gen1FunctionPath = path.join(
+      projRoot,
+      '.amplify',
+      'migration',
+      'amplify',
+      'backend',
+      'function',
+      gen1FunctionName.split('-')[0],
+      'src',
+      'index.js',
+    );
+    const gen2FunctionPath = path.join(projRoot, 'amplify', 'function', gen1FunctionName.split('-')[0], 'handler.ts');
+    await copyTsFile(gen1FunctionPath, gen2FunctionPath);
     await assert.doesNotReject(runGen2SandboxCommand(projRoot), 'Gen2 CDK deployment failed');
     await assertUserPoolResource(projRoot, gen1UserPoolId, gen1Region);
+    await assertStorageResource(projRoot, gen1BucketName, gen1Region);
+    await assertFunctionResource(projRoot, gen1FunctionName, gen1Region);
   });
 });
--- packages/amplify-migration-codegen-e2e/src/helpers.ts
+++ packages/amplify-migration-codegen-e2e/src/helpers.ts
@@ -9,14 +9,41 @@ import {
   npmInstall,
   getNpxPath,
   nspawn as spawn,
+  addS3WithGuestAccess,
+  checkIfBucketExists,
+  addFunction,
+  functionBuild,
+  getFunction,
 } from '@aws-amplify/amplify-e2e-core';
 import * as fs from 'fs-extra';
 import { $TSAny } from '@aws-amplify/amplify-cli-core';
 import path from 'node:path';
-import { CloudControlClient, GetResourceCommand } from '@aws-sdk/client-cloudcontrol';
+import { CloudControlClient, GetResourceCommand, ListResourcesCommand, ListResourcesCommandOutput } from '@aws-sdk/client-cloudcontrol';
 import { unset } from 'lodash';
 
 const pushTimeoutMS = 1000 * 60 * 20; // 20 minutes;
+
+export async function copyTsFile(sourcePath: string, destinationPath: string): Promise<void> {
+  const content = await fs.readFile(sourcePath, 'utf8');
+
+  // Replace the first occurrence of 'event' with 'event: any'
+  const modifiedContent = content.replace(/(exports\.handler\s*=\s*async\s*\(\s*)event(\s*\))/, '$1event: any$2');
+
+  await fs.writeFile(destinationPath, modifiedContent, 'utf8');
+}
+
+async function listResourcesByType(typeName: string, region: string) {
+  const client = new CloudControlClient({ region });
+  const resources = [];
+  const command = new ListResourcesCommand({
+    TypeName: typeName,
+  });
+  const response: ListResourcesCommandOutput = await client.send(command);
+  if (response.ResourceDescriptions) {
+    resources.push(...response.ResourceDescriptions.map((rd) => JSON.parse(rd.Properties ?? '')));
+  }
+  return resources;
+}
 
 async function getResourceDetails(typeName: string, identifier: string, region: string) {
   const client = new CloudControlClient({ region });
@@ -61,18 +88,35 @@ function deleteGen2Sandbox(cwd: string) {
 }
 
 export async function setupAndPushGen1Project(projRoot: string, projectName: string) {
-  await initJSProjectWithProfile(projRoot, { name: projectName, disableAmplifyAppCreation: false });
+  await initJSProjectWithProfile(projRoot, { name: projectName, disableAmplifyAppCreation: false, includeGen2RecommendationPrompt: false });
   await addAuthWithDefault(projRoot);
+  await addFunction(projRoot, { functionTemplate: 'Hello World' }, 'nodejs');
+  await functionBuild(projRoot);
+  await addS3WithGuestAccess(projRoot);
   await amplifyPushAuth(projRoot);
 }
 
 export async function assertGen1Setup(projRoot: string) {
   const gen1Meta = getProjectMeta(projRoot);
-  const gen1UserPoolId = Object.keys(gen1Meta.auth).map((key) => gen1Meta.auth[key])[0].output.UserPoolId;
   const gen1Region = gen1Meta.providers.awscloudformation.Region;
-  const userPool = await getUserPool(gen1UserPoolId, gen1Region);
-  expect(userPool.UserPool).toBeDefined();
-  return { gen1UserPoolId, gen1Region };
+  const { UserPoolId: gen1UserPoolId } = Object.keys(gen1Meta.auth).map((key) => gen1Meta.auth[key])[0].output;
+  const { Arn: gen1FunctionArn, Name: gen1FunctionName } = Object.keys(gen1Meta.function).map((key) => gen1Meta.function[key])[0].output;
+  const { BucketName: gen1BucketName } = Object.keys(gen1Meta.storage).map((key) => gen1Meta.storage[key])[0].output;
+
+  expect(gen1Region).toBeDefined();
+
+  const cloudUserPool = await getUserPool(gen1UserPoolId, gen1Region);
+  expect(cloudUserPool.UserPool).toBeDefined();
+
+  expect(gen1FunctionArn).toBeDefined();
+  expect(gen1FunctionName).toBeDefined();
+  const cloudFunction = await getFunction(gen1FunctionName, gen1Region);
+  expect(cloudFunction.Configuration?.FunctionArn).toEqual(gen1FunctionArn);
+
+  expect(gen1BucketName).toBeDefined();
+  const bucketExists = await checkIfBucketExists(gen1BucketName, gen1Region);
+  expect(bucketExists).toMatchObject({});
+  return { gen1UserPoolId, gen1FunctionName, gen1BucketName, gen1Region };
 }
 
 export async function assertUserPoolResource(projRoot: string, gen1UserPoolId: string, gen1Region: string) {
@@ -103,6 +147,43 @@ export async function assertUserPoolResource(projRoot: string, gen1UserPoolId: s
     'AccountRecoverySetting',
     'EmailVerificationMessage',
   ]);
+  expect(gen2Resource).toEqual(gen1Resource);
+}
+
+export async function assertStorageResource(projRoot: string, gen1BucketName: string, gen1Region: string) {
+  const gen1Resource = await getResourceDetails('AWS::S3::Bucket', gen1BucketName, gen1Region);
+  removeProperties(gen1Resource, ['DualStackDomainName', 'DomainName', 'BucketName', 'Arn', 'RegionalDomainName', 'Tags', 'WebsiteURL']);
+  // TODO: remove below line after CorsConfiguration.CorsRules[0].Id inconsistency is fixed
+  removeProperties(gen1Resource, ['CorsConfiguration.CorsRules[0].Id']);
+
+  const gen2Meta = getProjectOutputs(projRoot);
+  const gen2BucketName = gen2Meta.storage.bucket_name;
+  const gen2Region = gen2Meta.storage.aws_region;
+  const gen2Resource = await getResourceDetails('AWS::S3::Bucket', gen2BucketName, gen2Region);
+  removeProperties(gen2Resource, ['DualStackDomainName', 'DomainName', 'BucketName', 'Arn', 'RegionalDomainName', 'Tags', 'WebsiteURL']);
+
+  expect(gen2Resource).toEqual(gen1Resource);
+}
+
+export async function assertFunctionResource(projRoot: string, gen1FunctionName: string, gen1Region: string) {
+  const gen1Resource = await getResourceDetails('AWS::Lambda::Function', gen1FunctionName, gen1Region);
+  removeProperties(gen1Resource, ['Arn', 'FunctionName', 'LoggingConfig.LogGroup', 'Role']);
+  // TODO: remove below line after RecursiveLoop, RuntimeManagementConfig, Tags is fixed
+  removeProperties(gen1Resource, ['RecursiveLoop', 'RuntimeManagementConfig', 'Tags']);
+
+  const gen2Meta = getProjectOutputs(projRoot);
+  const gen2Region = gen2Meta.auth.aws_region;
+
+  // Workaround as amplify_outputs.json file doesn't have function metadata
+  const gen2Resources = await listResourcesByType('AWS::Lambda::Function', gen2Region);
+  const gen2Resource = gen2Resources.find((resource) => {
+    const functionName = resource.FunctionName;
+    return functionName.includes('amplify-mygen2app') && functionName.includes(gen1FunctionName.split('-')[0]);
+  });
+  removeProperties(gen2Resource, ['Arn', 'FunctionName', 'LoggingConfig.LogGroup', 'Role']);
+  // TODO: remove below line after Environment.Variables.AMPLIFY_SSM_ENV_CONFIG inconsistency is fixed
+  removeProperties(gen2Resource, ['Environment.Variables.AMPLIFY_SSM_ENV_CONFIG']);
+
   expect(gen2Resource).toEqual(gen1Resource);
 }
 
