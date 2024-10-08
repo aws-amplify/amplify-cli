cf325ba9d0efcd45b8812db08c6476734617c914
Sanay Yogesh Shah <sanayyog@amazon.com>
2024-10-08 01:51:27 -0700

    feat: add data category integration test

--- packages/amplify-migration-codegen-e2e/src/__tests__/migration_codegen_e2e.test.ts
+++ packages/amplify-migration-codegen-e2e/src/__tests__/migration_codegen_e2e.test.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import assert from 'node:assert';
-import { createNewProjectDir, sleep } from '@aws-amplify/amplify-e2e-core';
+import { createNewProjectDir } from '@aws-amplify/amplify-e2e-core';
 import {
   cleanupProjects,
   setupAndPushGen1Project,
@@ -10,7 +10,9 @@ import {
   assertUserPoolResource,
   assertStorageResource,
   assertFunctionResource,
-  copyTsFile,
+  assertDataResource,
+  copyFunctionFile,
+  copyGen1Schema,
 } from '../helpers';
 
 void describe('Migration Codegen E2E tests', () => {
@@ -26,25 +28,14 @@ void describe('Migration Codegen E2E tests', () => {
 
   void it('performs full migration codegen flow with backend', async () => {
     await setupAndPushGen1Project(projRoot, 'CodegenTest');
-    const { gen1UserPoolId, gen1FunctionName, gen1BucketName, gen1Region } = await assertGen1Setup(projRoot);
+    const { gen1UserPoolId, gen1FunctionName, gen1BucketName, gen1GraphQLAPIId, gen1Region } = await assertGen1Setup(projRoot);
     await assert.doesNotReject(runCodegenCommand(projRoot), 'Codegen failed');
-    await sleep(10000); // 10 seconds
-    const gen1FunctionPath = path.join(
-      projRoot,
-      '.amplify',
-      'migration',
-      'amplify',
-      'backend',
-      'function',
-      gen1FunctionName.split('-')[0],
-      'src',
-      'index.js',
-    );
-    const gen2FunctionPath = path.join(projRoot, 'amplify', 'function', gen1FunctionName.split('-')[0], 'handler.ts');
-    await copyTsFile(gen1FunctionPath, gen2FunctionPath);
+    await copyFunctionFile(projRoot, gen1FunctionName);
+    await copyGen1Schema(projRoot);
     await assert.doesNotReject(runGen2SandboxCommand(projRoot), 'Gen2 CDK deployment failed');
     await assertUserPoolResource(projRoot, gen1UserPoolId, gen1Region);
     await assertStorageResource(projRoot, gen1BucketName, gen1Region);
     await assertFunctionResource(projRoot, gen1FunctionName, gen1Region);
+    await assertDataResource(projRoot, gen1GraphQLAPIId, gen1Region);
   });
 });
--- packages/amplify-migration-codegen-e2e/src/helpers.ts
+++ packages/amplify-migration-codegen-e2e/src/helpers.ts
@@ -3,7 +3,7 @@ import {
   deleteProjectDir,
   initJSProjectWithProfile,
   addAuthWithDefault,
-  amplifyPushAuth,
+  amplifyPush,
   getProjectMeta,
   getUserPool,
   npmInstall,
@@ -14,16 +14,85 @@ import {
   addFunction,
   functionBuild,
   getFunction,
+  addApiWithoutSchema,
+  updateApiSchema,
+  getAppSyncApi,
+  amplifyPushForce,
+  describeCloudFormationStack,
 } from '@aws-amplify/amplify-e2e-core';
 import * as fs from 'fs-extra';
 import { $TSAny } from '@aws-amplify/amplify-cli-core';
 import path from 'node:path';
-import { CloudControlClient, GetResourceCommand, ListResourcesCommand, ListResourcesCommandOutput } from '@aws-sdk/client-cloudcontrol';
+import { CloudControlClient, GetResourceCommand } from '@aws-sdk/client-cloudcontrol';
+import { AppSyncClient, GetDataSourceCommand } from '@aws-sdk/client-appsync';
 import { unset } from 'lodash';
+import { createHash } from 'crypto';
+import { userInfo } from 'os';
 
+type AppId = string;
+type ProjectName = string;
+type BranchName = string;
+type SandboxName = string;
+
+type BackendIdentifier =
+  | {
+      namespace: Readonly<AppId>;
+      name: Readonly<BranchName>;
+      type: Readonly<'branch'>;
+      hash?: Readonly<string>;
+    }
+  | {
+      namespace: Readonly<ProjectName>;
+      name: Readonly<SandboxName>;
+      type: Readonly<'sandbox'>;
+      hash?: Readonly<string>;
+    };
+
+const STACK_NAME_LENGTH_LIMIT = 128;
+const AMPLIFY_PREFIX = 'amplify';
+const HASH_LENGTH = 10;
+const NUM_DASHES = 4;
 const pushTimeoutMS = 1000 * 60 * 20; // 20 minutes;
 
-export async function copyTsFile(sourcePath: string, destinationPath: string): Promise<void> {
+function toStackName(backendId: BackendIdentifier): string {
+  const hash = getHash(backendId);
+
+  // only take the first 50 chars here to make sure there is room in the stack name for the namespace as well
+  const name = sanitizeChars(backendId.name).slice(0, 50);
+
+  const namespaceMaxLength =
+    STACK_NAME_LENGTH_LIMIT - AMPLIFY_PREFIX.length - backendId.type.length - name.length - NUM_DASHES - HASH_LENGTH;
+
+  const namespace = sanitizeChars(backendId.namespace).slice(0, namespaceMaxLength - 1);
+
+  return ['amplify', namespace, name, backendId.type, hash].join('-');
+}
+
+const getHash = (backendId: BackendIdentifier): string =>
+  backendId.hash ??
+  // md5 would be sufficient here because this hash does not need to be cryptographically secure, but this ensures that we don't get unnecessarily flagged by some security scanner
+  createHash('sha512').update(backendId.namespace).update(backendId.name).digest('hex').slice(0, HASH_LENGTH);
+
+/**
+ * Remove all non-alphanumeric characters from the input string
+ */
+const sanitizeChars = (str: string): string => {
+  return str.replace(/[^A-Za-z0-9]/g, '');
+};
+
+export async function copyFunctionFile(projRoot: string, gen1FunctionName: string): Promise<void> {
+  const sourcePath = path.join(
+    projRoot,
+    '.amplify',
+    'migration',
+    'amplify',
+    'backend',
+    'function',
+    gen1FunctionName.split('-')[0],
+    'src',
+    'index.js',
+  );
+  const destinationPath = path.join(projRoot, 'amplify', 'function', gen1FunctionName.split('-')[0], 'handler.ts');
   const content = await fs.readFile(sourcePath, 'utf8');
 
   // Replace the first occurrence of 'event' with 'event: any'
@@ -32,17 +101,64 @@ export async function copyTsFile(sourcePath: string, destinationPath: string): P
   await fs.writeFile(destinationPath, modifiedContent, 'utf8');
 }
 
-async function listResourcesByType(typeName: string, region: string) {
-  const client = new CloudControlClient({ region });
-  const resources = [];
-  const command = new ListResourcesCommand({
-    TypeName: typeName,
-  });
-  const response: ListResourcesCommandOutput = await client.send(command);
-  if (response.ResourceDescriptions) {
-    resources.push(...response.ResourceDescriptions.map((rd) => JSON.parse(rd.Properties ?? '')));
+export async function copyGen1Schema(projRoot: string): Promise<void> {
+  const gen1SchemaPath = path.join(projRoot, '.amplify', 'migration', 'amplify', 'backend', 'api', 'codegentest', 'schema.graphql');
+  const gen1Schema = await fs.readFile(gen1SchemaPath, 'utf-8');
+  const dataResourcePath = path.join(projRoot, 'amplify', 'data', 'resource.ts');
+  const dataResourceContent = await fs.readFile(dataResourcePath, 'utf-8');
+  const backendPath = path.join(projRoot, 'amplify', 'backend.ts');
+  let backendContent = await fs.readFile(backendPath, 'utf-8');
+
+  const schemaRegex = /"TODO: Add your existing graphql schema here"/;
+  const updatedContent = dataResourceContent.replace(schemaRegex, `\`${gen1Schema.trim()}\``);
+
+  const errorRegex = /throw new Error\("TODO: Add Gen 1 GraphQL schema"\);?\s*/;
+  const finalContent = updatedContent.replace(errorRegex, '');
+
+  await fs.writeFile(dataResourcePath, finalContent, 'utf-8');
+
+  const linesToAdd = `
+  const todoTable = backend.data.resources.cfnResources.additionalCfnResources['Todo'];
+  todoTable.addOverride('Properties.sseSpecification', { sseEnabled: false });
+  `;
+
+  backendContent += linesToAdd;
+  await fs.writeFile(backendPath, backendContent, 'utf-8');
+}
+
+async function setEnableGen2MigrationFeatureFlag(projectRoot: string): Promise<void> {
+  const cliJsonPath = path.join(projectRoot, 'amplify', 'cli.json');
+  const cliJson = await fs.readJSON(cliJsonPath);
+  if (!cliJson.features) {
+    cliJson.features = {};
   }
-  return resources;
+  if (!cliJson.features.graphqltransformer) {
+    cliJson.features.graphqltransformer = {};
+  }
+  cliJson.features.graphqltransformer.enablegen2migration = true;
+  await fs.writeJSON(cliJsonPath, cliJson, { spaces: 2 });
+}
+
+async function updatePackageJsonDependency(cwd: string, dependencyName: string, version: string): Promise<void> {
+  const packageJsonPath = path.join(cwd, 'package.json');
+  const packageJsonContent = await fs.readFile(packageJsonPath, 'utf-8');
+  const packageJson = JSON.parse(packageJsonContent);
+
+  packageJson.devDependencies = packageJson.devDependencies || {};
+  packageJson.devDependencies[dependencyName] = version;
+
+  const updatedContent = JSON.stringify(packageJson, null, 2);
+  await fs.writeFile(packageJsonPath, updatedContent, 'utf-8');
+}
+
+async function getAppSyncDataSource(apiId: string, dataSourceName: string, region: string) {
+  const client = new AppSyncClient({ region });
+  const command = new GetDataSourceCommand({
+    apiId: apiId,
+    name: dataSourceName,
+  });
+  const response = await client.send(command);
+  return response.dataSource;
 }
 
 async function getResourceDetails(typeName: string, identifier: string, region: string) {
@@ -55,7 +171,8 @@ async function getResourceDetails(typeName: string, identifier: string, region: 
   return JSON.parse(response.ResourceDescription.Properties);
 }
 
-export function runGen2SandboxCommand(cwd: string) {
+export async function runGen2SandboxCommand(cwd: string) {
+  await updatePackageJsonDependency(cwd, '@aws-amplify/backend', '0.0.0-test-20241003180022');
   npmInstall(cwd);
   return spawn(getNpxPath(), ['ampx', 'sandbox', '--once'], {
     cwd,
@@ -88,12 +205,16 @@ function deleteGen2Sandbox(cwd: string) {
 }
 
 export async function setupAndPushGen1Project(projRoot: string, projectName: string) {
-  await initJSProjectWithProfile(projRoot, { name: projectName, disableAmplifyAppCreation: false, includeGen2RecommendationPrompt: false });
+  await initJSProjectWithProfile(projRoot, { name: projectName, disableAmplifyAppCreation: false });
   await addAuthWithDefault(projRoot);
   await addFunction(projRoot, { functionTemplate: 'Hello World' }, 'nodejs');
   await functionBuild(projRoot);
   await addS3WithGuestAccess(projRoot);
-  await amplifyPushAuth(projRoot);
+  await addApiWithoutSchema(projRoot, { transformerVersion: 2 });
+  await updateApiSchema(projRoot, projectName, 'simple_model.graphql');
+  await amplifyPush(projRoot);
+  await setEnableGen2MigrationFeatureFlag(projRoot);
+  await amplifyPushForce(projRoot);
 }
 
 export async function assertGen1Setup(projRoot: string) {
@@ -102,6 +223,12 @@ export async function assertGen1Setup(projRoot: string) {
   const { UserPoolId: gen1UserPoolId } = Object.keys(gen1Meta.auth).map((key) => gen1Meta.auth[key])[0].output;
   const { Arn: gen1FunctionArn, Name: gen1FunctionName } = Object.keys(gen1Meta.function).map((key) => gen1Meta.function[key])[0].output;
   const { BucketName: gen1BucketName } = Object.keys(gen1Meta.storage).map((key) => gen1Meta.storage[key])[0].output;
+  const {
+    GraphQLAPIIdOutput: gen1GraphQLAPIId,
+    GraphQLAPIEndpointOutput,
+    GraphQLAPIKeyOutput,
+  } = Object.keys(gen1Meta.api).map((key) => gen1Meta.api[key])[0].output;
+  const { graphqlApi } = await getAppSyncApi(gen1GraphQLAPIId, gen1Region);
 
   expect(gen1Region).toBeDefined();
 
@@ -116,7 +243,14 @@ export async function assertGen1Setup(projRoot: string) {
   expect(gen1BucketName).toBeDefined();
   const bucketExists = await checkIfBucketExists(gen1BucketName, gen1Region);
   expect(bucketExists).toMatchObject({});
-  return { gen1UserPoolId, gen1FunctionName, gen1BucketName, gen1Region };
+
+  expect(gen1GraphQLAPIId).toBeDefined();
+  expect(GraphQLAPIEndpointOutput).toBeDefined();
+  expect(GraphQLAPIKeyOutput).toBeDefined();
+
+  expect(graphqlApi).toBeDefined();
+  expect(graphqlApi?.apiId).toEqual(gen1GraphQLAPIId);
+  return { gen1UserPoolId, gen1FunctionName, gen1BucketName, gen1GraphQLAPIId, gen1Region };
 }
 
 export async function assertUserPoolResource(projRoot: string, gen1UserPoolId: string, gen1Region: string) {
@@ -168,23 +302,61 @@ export async function assertStorageResource(projRoot: string, gen1BucketName: st
 export async function assertFunctionResource(projRoot: string, gen1FunctionName: string, gen1Region: string) {
   const gen1Resource = await getResourceDetails('AWS::Lambda::Function', gen1FunctionName, gen1Region);
   removeProperties(gen1Resource, ['Arn', 'FunctionName', 'LoggingConfig.LogGroup', 'Role']);
-  // TODO: remove below line after RecursiveLoop, RuntimeManagementConfig, Tags is fixed
-  removeProperties(gen1Resource, ['RecursiveLoop', 'RuntimeManagementConfig', 'Tags']);
+  // TODO: remove below line after Tags inconsistency is fixed
+  removeProperties(gen1Resource, ['Tags']);
 
   const gen2Meta = getProjectOutputs(projRoot);
   const gen2Region = gen2Meta.auth.aws_region;
-
-  // Workaround as amplify_outputs.json file doesn't have function metadata
-  const gen2Resources = await listResourcesByType('AWS::Lambda::Function', gen2Region);
-  const gen2Resource = gen2Resources.find((resource) => {
-    const functionName = resource.FunctionName;
-    return functionName.includes('amplify-mygen2app') && functionName.includes(gen1FunctionName.split('-')[0]);
-  });
+  const gen2StackName = toStackName({ name: userInfo().username, namespace: 'my-gen2-app', type: 'sandbox' });
+  const outputs = (await describeCloudFormationStack(gen2StackName, gen2Region)).Outputs;
+  const gen2FunctionName = JSON.parse(
+    outputs?.find((output: { OutputKey: string }) => output.OutputKey === 'definedFunctions')?.OutputValue ?? '[]',
+  )[0];
+  const gen2Resource = await getResourceDetails('AWS::Lambda::Function', gen2FunctionName, gen2Region);
   removeProperties(gen2Resource, ['Arn', 'FunctionName', 'LoggingConfig.LogGroup', 'Role']);
-  // TODO: remove below line after Environment.Variables.AMPLIFY_SSM_ENV_CONFIG inconsistency is fixed
-  removeProperties(gen2Resource, ['Environment.Variables.AMPLIFY_SSM_ENV_CONFIG']);
+  // TODO: remove below line after Environment.Variables.AMPLIFY_SSM_ENV_CONFIG, Tags inconsistency is fixed
+  removeProperties(gen2Resource, ['Environment.Variables.AMPLIFY_SSM_ENV_CONFIG', 'Tags']);
 
   expect(gen2Resource).toEqual(gen1Resource);
+}
+
+export async function assertDataResource(projRoot: string, gen1GraphQLAPIId: string, gen1Region: string) {
+  const gen1Resource = await getAppSyncApi(gen1GraphQLAPIId, gen1Region);
+  const gen1DataSource = (await getAppSyncDataSource(gen1GraphQLAPIId, 'TodoTable', gen1Region)) as Record<string, unknown>;
+  removeProperties(gen1DataSource, ['dataSourceArn', 'serviceRoleArn']);
+  removeProperties(gen1Resource, [
+    'graphqlApi.name',
+    'graphqlApi.apiId',
+    'graphqlApi.arn',
+    'graphqlApi.uris',
+    'graphqlApi.tags',
+    'graphqlApi.dns',
+  ]);
+  // TODO: remove below line after authenticationType inconsistency is fixed
+  removeProperties(gen1Resource, ['graphqlApi.authenticationType']);
+
+  const gen2Meta = getProjectOutputs(projRoot);
+  const gen2Region = gen2Meta.data.aws_region;
+  const gen2StackName = toStackName({ name: userInfo().username, namespace: 'my-gen2-app', type: 'sandbox' });
+  const outputs = (await describeCloudFormationStack(gen2StackName, gen2Region)).Outputs;
+  const gen2GraphQLAPIId = outputs?.find((output: { OutputKey: string }) => output.OutputKey === 'awsAppsyncApiId')?.OutputValue ?? '';
+  const gen2Resource = await getAppSyncApi(gen2GraphQLAPIId, gen2Region);
+  const gen2DataSource = (await getAppSyncDataSource(gen2GraphQLAPIId, 'TodoTable', gen1Region)) as Record<string, unknown>;
+  removeProperties(gen2DataSource, ['dataSourceArn', 'serviceRoleArn']);
+  removeProperties(gen2Resource, [
+    'graphqlApi.name',
+    'graphqlApi.apiId',
+    'graphqlApi.arn',
+    'graphqlApi.uris',
+    'graphqlApi.tags',
+    'graphqlApi.additionalAuthenticationProviders',
+    'graphqlApi.dns',
+  ]);
+  // TODO: remove below line after authenticationType, userPoolConfig inconsistency is fixed
+  removeProperties(gen2Resource, ['graphqlApi.authenticationType', 'graphqlApi.userPoolConfig']);
+
+  expect(gen2DataSource).toEqual(gen1DataSource);
+  expect(gen2Resource).toEqual(gen2Resource);
 }
 
 function removeProperties(obj: Record<string, unknown>, propertiesToRemove: string[]) {
