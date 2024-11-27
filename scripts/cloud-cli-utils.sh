9dd069bfb62976d6531f2ca36301e592230c4c27
Armando Luja <aluja@amazon.com>
2024-11-27 13:02:48 -0800

    chore: install libgm1 instead of dev version

--- scripts/cloud-cli-utils.sh
+++ scripts/cloud-cli-utils.sh
@@ -16,6 +16,48 @@ function authenticate {
     ada cred update --profile="${profile_name}" --account="${account_number}" --role=${role_name} --provider=isengard --once
     aws configure set region us-east-1 --profile $profile_name
 }
+# function triggerProjectBatch {
+#     account_number=$1
+#     role_name=$2
+#     profile_name=$3
+#     project_name=$4
+#     target_branch=$5
+#     npm_tag=$6
+#     authenticate $account_number $role_name $profile_name
+#     echo AWS Account: $account_number
+#     echo Project: $project_name
+#     echo Target Branch: $target_branch
+#     if [[ "$npm_tag" != "" ]]; then
+#       echo NPM tag: $npm_tag
+#       npm_variable_override="name=NPM_TAG,value=$npm_tag,type=PLAINTEXT"
+#     fi
+#     RESULT=$(aws codebuild start-build-batch --profile="${profile_name}" --project-name $project_name --source-version=$target_branch \
+#      --environment-variables-override name=BRANCH_NAME,value=$target_branch,type=PLAINTEXT $npm_variable_override \
+#      --query 'buildBatch.id' --output text)
+#     echo "https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$account_number/projects/$project_name/batch/$RESULT?region=us-east-1"
+# }
+
+# function triggerProjectBatch {
+#     account_number=$1
+#     role_name=$2
+#     profile_name=$3
+#     project_name=$4
+#     target_branch=$5
+#     npm_tag=$6
+#     authenticate $account_number $role_name $profile_name
+#     echo AWS Account: $account_number
+#     echo Project: $project_name
+#     echo Target Branch: $target_branch
+#     if [[ "$npm_tag" != "" ]]; then
+#       echo NPM tag: $npm_tag
+#       npm_variable_override="name=NPM_TAG,value=$npm_tag,type=PLAINTEXT"
+#     fi
+#     RESULT=$(aws codebuild start-build-batch --profile="${profile_name}" --project-name $project_name --source-version=$target_branch \
+#      --image-override '671107461633.dkr.ecr.us-east-1.amazonaws.com/amplify-cli-codebuild-testing-stack-prod-amplifyclie2eecrc5fd2bba-ke8oxaimjhf2:dotnet8' \
+#      --environment-variables-override name=BRANCH_NAME,value=$target_branch,type=PLAINTEXT name=WINDOWS_IMAGE_2019,value=671107461633.dkr.ecr.us-east-1.amazonaws.com/amplify-cli-codebuild-testing-stack-prod-amplifyclie2eecrc5fd2bba-ke8oxaimjhf2:windows2019-1.0.3-1,type=PLAINTEXT $npm_variable_override \
+#      --query 'buildBatch.id' --output text)
+#     echo "https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$account_number/projects/$project_name/batch/$RESULT?region=us-east-1"
+# }
 function triggerProjectBatch {
     account_number=$1
     role_name=$2
@@ -31,8 +73,10 @@ function triggerProjectBatch {
       echo NPM tag: $npm_tag
       npm_variable_override="name=NPM_TAG,value=$npm_tag,type=PLAINTEXT"
     fi
+    
     RESULT=$(aws codebuild start-build-batch --profile="${profile_name}" --project-name $project_name --source-version=$target_branch \
-     --environment-variables-override name=BRANCH_NAME,value=$target_branch,type=PLAINTEXT $npm_variable_override \
+     --image-override 'aws/codebuild/amazonlinux-x86_64-standard:5.0' \
+     --environment-variables-override name=BRANCH_NAME,value=$target_branch,type=PLAINTEXT name=WINDOWS_IMAGE_2019,value=aws/codebuild/windows-base:2019-3.0,type=PLAINTEXT $npm_variable_override \
      --query 'buildBatch.id' --output text)
     echo "https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$account_number/projects/$project_name/batch/$RESULT?region=us-east-1"
 }
