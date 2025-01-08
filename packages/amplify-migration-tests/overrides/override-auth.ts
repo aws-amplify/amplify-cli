900f709c55f09b06c26d863b7c9406219c6cba22
Vieltojarvi <lvielto@amazon.com>
2025-01-08 10:21:36 -0800

    fix: removed all migration tests

--- packages/amplify-migration-tests/overrides/override-auth.ts
+++
@@ -1,9 +0,0 @@
-export function override(props: Record<string, unknown>): Record<string, unknown> {
-  props.userPool['deviceConfiguration'] = {
-    challengeRequiredOnNewDevice: true,
-  };
-  props.userPool['userAttributeUpdateSettings'] = {
-    attributesRequireVerificationBeforeUpdate: ['email'],
-  };
-  return props;
-}
--- packages/amplify-migration-tests/src/migration-helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './init';
-export * from './api';
-export * from './check-version';
-export * from './legacy-lambda-layer';
