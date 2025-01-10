58856c8fb908ed21f3aea850d4dcc0d4a03f4114
Leena <81589006+ShadowCat567@users.noreply.github.com>
2025-01-09 16:59:23 -0800

    fix: removed migration tests between versions of Gen 1 (#14062)
    * fix: removed migration tests
    
    * fix: removed v12 migration test helpers
    
    * chore: remove v12 migration tests
    
    * fix: removed some more tests
    
    * fix: hopefully updated generated file
    
    * fix: more tests removed from generated file
    
    ---------
    
    Co-authored-by: Vieltojarvi <lvielto@amazon.com>

--- scripts/split-e2e-tests-codebuild.ts
+++ scripts/split-e2e-tests-codebuild.ts
@@ -4,7 +4,6 @@ import { join } from 'path';
 import * as yaml from 'js-yaml';
 import { REPO_ROOT } from './cci-utils';
 import { FORCE_REGION_MAP, getOldJobNameWithoutSuffixes, loadTestTimings, USE_PARENT_ACCOUNT } from './cci-utils';
-import { migrationFromV12Tests, migrationFromV8Tests } from './split-e2e-test-filters';
 const CODEBUILD_CONFIG_BASE_PATH = join(REPO_ROOT, 'codebuild_specs', 'e2e_workflow_base.yml');
 const CODEBUILD_GENERATE_CONFIG_PATH = join(REPO_ROOT, 'codebuild_specs', 'e2e_workflow_generated');
 const RUN_SOLO = [
@@ -329,36 +328,8 @@ function main(): void {
     false,
     undefined,
   );
-  const splitMigrationV8Tests = splitTestsV3(
-    {
-      identifier: 'migration_tests_v8',
-      buildspec: 'codebuild_specs/migration_tests_v8.yml',
-      env: {},
-      'depend-on': ['upb'],
-    },
-    undefined,
-    join(REPO_ROOT, 'packages', 'amplify-migration-tests'),
-    true,
-    (tests: string[]) => {
-      return tests.filter((testName) => migrationFromV8Tests.find((t: string) => t === testName));
-    },
-  );
-  const splitMigrationV12Tests = splitTestsV3(
-    {
-      identifier: 'migration_tests_v12',
-      buildspec: 'codebuild_specs/migration_tests_v12.yml',
-      env: {},
-      'depend-on': ['upb'],
-    },
-    undefined,
-    join(REPO_ROOT, 'packages', 'amplify-migration-tests'),
-    true,
-    (tests: string[]) => {
-      return tests.filter((testName) => migrationFromV12Tests.find((t) => t === testName));
-    },
-  );
 
-  let allBuilds = [...splitE2ETests, ...splitMigrationV8Tests, ...splitMigrationV12Tests];
+  let allBuilds = [...splitE2ETests];
   const dependeeIdentifiers: string[] = allBuilds.map((buildObject) => buildObject.identifier).sort();
   const dependeeIdentifiersFileContents = `${JSON.stringify(dependeeIdentifiers, null, 2)}\n`;
   const waitForIdsFilePath = './codebuild_specs/wait_for_ids.json';
