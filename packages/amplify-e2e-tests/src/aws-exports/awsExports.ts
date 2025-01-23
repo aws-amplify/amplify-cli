9aaea50170849aac7ed2ad8f87d4105f238874dc
Kamil Sobol <5849952+sobolk@users.noreply.github.com>
2025-01-23 06:44:39 -0800

    try this

--- packages/amplify-e2e-tests/src/aws-exports/awsExports.ts
+++ packages/amplify-e2e-tests/src/aws-exports/awsExports.ts
@@ -1,5 +1,8 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import { transformSync } from '@babel/core';
+import babelTransformEsmToCjs from '@babel/plugin-transform-modules-commonjs';
+import { Module } from 'module';
 
 export function getAWSExportsPath(projRoot: string): string {
   return path.join(projRoot, 'src', 'aws-exports.js');
@@ -7,9 +10,16 @@ export function getAWSExportsPath(projRoot: string): string {
 
 export function getAWSExports(projectRoot: string) {
   const awsExportsPath = getAWSExportsPath(projectRoot);
-  // From Jest 25, ESM modules can only be loaded with mjs extension and Jest takes over
-  // require, that's why we need to copy the file.
-  const awsExportsMJSPath = awsExportsPath.replace('.js', '.mjs');
-  fs.copySync(awsExportsPath, awsExportsMJSPath, { overwrite: true });
-  return require(awsExportsMJSPath);
+  const fileContents = fs.readFileSync(awsExportsPath, 'utf-8');
+  // transpile the file contents to CommonJS
+  const { code } = transformSync(fileContents, {
+    plugins: [babelTransformEsmToCjs],
+    configFile: false,
+    babelrc: false,
+  });
+  const mod = new Module('aws-exports.js');
+  // @ts-expect-error This is private api.
+  mod._compile(code, 'aws-exports.js');
+  mod.paths = [projectRoot];
+  return mod.exports;
 }
