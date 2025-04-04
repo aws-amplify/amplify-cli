2d72d5109aaccff31025813b14ac14e529d85b21
rjabhi <rjabhi@amazon.com>
2025-04-04 13:22:14 -0700

    feat(migrate): remove gen1 config files in prepare command

--- packages/amplify-migration/src/command-handler.test.ts
+++ packages/amplify-migration/src/command-handler.test.ts
@@ -1,12 +1,13 @@
 import fs from 'node:fs/promises';
 import { AmplifyClient, GetAppCommand } from '@aws-sdk/client-amplify';
-import { updateAmplifyYmlFile } from './command-handlers';
+import { updateAmplifyYmlFile, removeGen1ConfigurationFiles, GEN1_CONFIGURATION_FILES } from './command-handlers';
 import { pathManager } from '@aws-amplify/amplify-cli-core';
 
 jest.mock('node:fs/promises', () => ({
   access: jest.fn(),
   readFile: jest.fn(),
   writeFile: jest.fn(),
+  rm: jest.fn(),
 }));
 
 jest.mock('@aws-amplify/amplify-cli-core');
@@ -94,5 +95,58 @@ frontend:
     (fs.readFile as jest.Mock).mockRejectedValue(error);
 
     await expect(updateAmplifyYmlFile(amplifyClient, mockAppId)).rejects.toThrow(error);
+  });
+});
+
+describe('removeGen1ConfigurationFiles', () => {
+  const sourceDir = 'src';
+  const mockProjectConfig = {
+    whyContinueWithGen1: 'Prefer not to answer',
+    projectName: 'testgen1',
+    version: '3.1',
+    frontend: 'javascript',
+    javascript: {
+      framework: 'none',
+      config: {
+        SourceDir: sourceDir,
+        DistributionDir: 'dist',
+        BuildCommand: 'npm run-script build',
+        StartCommand: 'npm run-script start',
+      },
+    },
+    providers: ['awscloudformation'],
+  };
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should remove all gen1 configuration files', async () => {
+    jest.mocked(fs.readFile).mockResolvedValue(JSON.stringify(mockProjectConfig));
+
+    await removeGen1ConfigurationFiles();
+
+    GEN1_CONFIGURATION_FILES.forEach((file) => {
+      expect(fs.rm).toHaveBeenCalledWith(`${sourceDir}/${file}`);
+    });
+  });
+
+  it('should not throw an error when project-config json is not found', async () => {
+    jest.mocked(fs.readFile).mockRejectedValue({ code: 'ENOENT' });
+
+    await expect(removeGen1ConfigurationFiles()).resolves.not.toThrow();
+    GEN1_CONFIGURATION_FILES.forEach((file) => {
+      expect(fs.rm).not.toHaveBeenCalledWith(`${sourceDir}/${file}`);
+    });
+  });
+
+  it('should not throw an error when a configuration file is not found', async () => {
+    jest.mocked(fs.readFile).mockResolvedValue(JSON.stringify(mockProjectConfig));
+    jest.mocked(fs.rm).mockRejectedValueOnce({ code: 'ENOENT' });
+
+    await expect(removeGen1ConfigurationFiles()).resolves.not.toThrow();
+    GEN1_CONFIGURATION_FILES.forEach((file) => {
+      // making some other files are removed even if one errors out
+      expect(fs.rm).toHaveBeenCalledWith(`${sourceDir}/${file}`);
+    });
   });
 });
