5b8c70cb6d31de452e78a899eb7990143437ad06
Vieltojarvi <lvielto@amazon.com>
2025-06-03 14:38:08 -0700

    chore: adjust messaging

--- packages/amplify-category-interactions/src/commands/interactions/add.js
+++ packages/amplify-category-interactions/src/commands/interactions/add.js
@@ -7,7 +7,7 @@ module.exports = {
 
     return context.print.info(
       `
-      Amazon Lex V1 is reaching end of life on September 15, 2025 and is no longer allowing the creating of new bots as of March 31, 2025. 
+      Amazon Lex V1 is reaching end of life on September 15, 2025 and is no longer allowing the creation of new bots as of March 31, 2025. 
       The interactions category will not migrate to Lex V2.
       If you wish to create a new bot with Lex in your Amplify application, we recommend you follow the steps outlined here to create a bot with Lex V2: https://docs.amplify.aws/gen1/react/build-a-backend/more-features/interactions/set-up-interactions/#setup-aws-lexv2-bot
       `,
