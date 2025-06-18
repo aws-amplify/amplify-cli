484c81b3f26fc98b65072023196bf60d3e14c408
Kamil Sobol <5849952+sobolk@users.noreply.github.com>
2025-06-18 08:47:23 -0700

    chore: remove example from velocity

--- packages/amplify-velocity-template/examples/app.js
+++
@@ -1,9 +0,0 @@
-import { parse, Compile } from 'velocityjs';
-
-const asts = parse(document.querySelector('#tmpl').innerHTML);
-const data = {
-  items: [{ a: '1' }, { a: 'successed' }],
-};
-const s = new Compile(asts).render(data);
-
-document.querySelector('.foo').innerHTML = s;
