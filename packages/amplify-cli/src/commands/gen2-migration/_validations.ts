669f6fd01bf47cead6ff6f3fd7cfc9e7a1678dfe
Eli Polonsky <epolon@amazon.com>
2025-10-07 11:28:26 -0400

    chore: scaffolding migration

---
+++ packages/amplify-cli/src/commands/gen2-migration/_validations.ts
@@ -0,0 +1,36 @@
+import { AmplifyDriftDetector } from '../drift';
+import { $TSContext } from '@aws-amplify/amplify-cli-core';
+import { printer } from '@aws-amplify/amplify-prompts';
+
+export class AmplifyGen2MigrationValidations {
+  constructor(private readonly context: $TSContext) {}
+
+  public async validateDrift(): Promise<void> {
+    return new AmplifyDriftDetector(this.context).detect();
+  }
+
+  public async validateWorkingDirectory(): Promise<void> {
+    printer.warn('Not implemented');
+  }
+
+  public async validateDeploymentStatus(): Promise<void> {
+    printer.warn('Not implemented');
+  }
+
+  public async validateDeploymentVersion(): Promise<void> {
+    printer.warn('Not implemented');
+  }
+
+  public async validateIsolatedEnvironment(): Promise<void> {
+    printer.warn('Not implemented');
+  }
+
+  // eslint-disable-next-line spellcheck/spell-checker
+  public async validateStatefulResources(): Promise<void> {
+    printer.warn('Not implemented');
+  }
+
+  public async validateIngressTraffic(): Promise<void> {
+    printer.warn('Not implemented');
+  }
+}
