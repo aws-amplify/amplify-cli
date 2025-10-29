ad8350ad26ea6293eb5c5e9e5f64da7a4b8b0c3a
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-10-29 10:58:53 -0400

    fix: resolver denormalizes dependsOn array

--- packages/amplify-cli/src/commands/gen2-migration/refactor/resolvers/cfn-dependency-resolver.ts
+++ packages/amplify-cli/src/commands/gen2-migration/refactor/resolvers/cfn-dependency-resolver.ts
@@ -14,11 +14,11 @@ class CfnDependencyResolver {
         // If resource is not part of refactor, it should not depend on any resource being moved as part of refactor.
         if (depsInRefactor.length > 0 && !resourcesToRefactor.includes(logicalResourceId)) {
           const filteredDeps = deps.filter((dep: string) => !resourcesToRefactor.includes(dep));
-          resource.DependsOn = filteredDeps.length === 1 ? filteredDeps[0] : filteredDeps;
+          resource.DependsOn = filteredDeps;
         }
         // If resource is part of refactor, it should only depend on resources being moved as part of refactor.
         else if (resourcesToRefactor.includes(logicalResourceId) && deps.length > depsInRefactor.length) {
-          resource.DependsOn = depsInRefactor.length === 1 ? depsInRefactor[0] : depsInRefactor;
+          resource.DependsOn = depsInRefactor;
         }
       }
     });
