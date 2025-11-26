816b9832b07435d0fc92ab4eef8eec670ef87be5
dgandhi62 <gandhidhyan@gmail.com>
2025-11-26 10:27:47 -0500

    feat: add storage codegen naming

--- packages/amplify-cli/src/commands/gen2-migration/codegen-generate/src/generators/storage/index.ts
+++ packages/amplify-cli/src/commands/gen2-migration/codegen-generate/src/generators/storage/index.ts
@@ -55,7 +55,23 @@ export const renderStorage = (storageParams: StorageRenderParameters = {}) => {
 
   const postImportStatements = [];
 
-  // Remove name parameter - let Gen 2 auto-generate storage names
+  const amplifyGen1EnvStatement = createVariableStatement(
+    factory.createVariableDeclaration(
+      gen2BranchNameVariableName,
+      undefined,
+      undefined,
+      factory.createIdentifier('process.env.AWS_BRANCH ?? "sandbox"'),
+    ),
+  );
+  postImportStatements.push(amplifyGen1EnvStatement);
+
+  if (storageParams.storageIdentifier) {
+    const splitStorageIdentifier = storageParams.storageIdentifier.split('-');
+    const storageNameWithoutBackendEnvName = splitStorageIdentifier.slice(0, -1).join('-');
+
+    const storageNameAssignment = createTemplateLiteral(`${storageNameWithoutBackendEnvName}-`, gen2BranchNameVariableName, '');
+    propertyAssignments.push(factory.createPropertyAssignment(factory.createIdentifier('name'), storageNameAssignment));
+  }
   if (storageParams.accessPatterns) {
     propertyAssignments.push(getAccessPatterns(storageParams.accessPatterns));
   }
