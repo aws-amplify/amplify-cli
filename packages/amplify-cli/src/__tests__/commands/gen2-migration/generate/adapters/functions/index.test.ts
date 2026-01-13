4cc1250b47951e864ef608a72db0d86013ca7ac8
Eli Polonsky <Eli.polonsky@gmail.com>
2026-01-13 07:29:17 -0500

    fix(gen2-migration): functions are generated with incorrect storage access env variables (#14457)
    chore: mid work

--- packages/amplify-cli/src/__tests__/commands/gen2-migration/generate/adapters/functions/index.test.ts
+++ packages/amplify-cli/src/__tests__/commands/gen2-migration/generate/adapters/functions/index.test.ts
@@ -1,6 +1,107 @@
 import { getFunctionDefinition } from '../../../../../../commands/gen2-migration/generate/adapters/functions/index';
 
 describe('getFunctionDefinition', () => {
+  test('auth env variables are removed', () => {
+    const definition = getFunctionDefinition(
+      [
+        {
+          Handler: undefined,
+          FunctionName: 'MyFunc',
+          Environment: {
+            Variables: {
+              SOME_ENV: 'some-value',
+              AUTH_MEDIAVAULT_USERPOOLID: 'authUserPoolId',
+            },
+          },
+        },
+      ],
+      [],
+      new Map(),
+      {
+        function: {
+          MyFunc: {
+            service: 'Lambda',
+            providerPlugin: 'awscloudformation',
+            output: {
+              Name: 'MyFunc',
+            },
+          },
+        },
+      },
+    );
+
+    expect(definition.length).toEqual(1);
+    expect(definition[0].environment?.Variables).toEqual({ SOME_ENV: 'some-value' });
+  });
+
+  test('storage dynamo env variables are removed', () => {
+    const definition = getFunctionDefinition(
+      [
+        {
+          Handler: undefined,
+          FunctionName: 'MyFunc',
+          Environment: {
+            Variables: {
+              SOME_ENV: 'some-value',
+              STORAGE_ACTIVITY_ARN: 'storageArn',
+              STORAGE_ACTIVITY_NAME: 'storageName',
+              STORAGE_ACTIVITY_STREAMARN: 'storageStreamName',
+            },
+          },
+        },
+      ],
+      [],
+      new Map(),
+      {
+        function: {
+          MyFunc: {
+            service: 'Lambda',
+            providerPlugin: 'awscloudformation',
+            output: {
+              Name: 'MyFunc',
+            },
+          },
+        },
+      },
+    );
+
+    expect(definition.length).toEqual(1);
+    expect(definition[0].environment?.Variables).toEqual({ SOME_ENV: 'some-value' });
+  });
+
+  test('storage s3 env variables are removed', () => {
+    const definition = getFunctionDefinition(
+      [
+        {
+          Handler: undefined,
+          FunctionName: 'MyFunc',
+          Environment: {
+            Variables: {
+              SOME_ENV: 'some-value',
+              STORAGE_MEDIAVAULT_BUCKETNAME: 'storageBucketName',
+            },
+          },
+        },
+      ],
+      [],
+      new Map(),
+      {
+        function: {
+          MyFunc: {
+            service: 'Lambda',
+            providerPlugin: 'awscloudformation',
+            output: {
+              Name: 'MyFunc',
+            },
+          },
+        },
+      },
+    );
+
+    expect(definition.length).toEqual(1);
+    expect(definition[0].environment?.Variables).toEqual({ SOME_ENV: 'some-value' });
+  });
+
   test('graphql env variables are removed', () => {
     const definition = getFunctionDefinition(
       [
