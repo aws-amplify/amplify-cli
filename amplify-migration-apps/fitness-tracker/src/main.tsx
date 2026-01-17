dbe663086495dfebcca1b730254cd94084838fac
Eli Polonsky <Eli.polonsky@gmail.com>
2026-01-16 19:54:11 -0500

    Apply suggestion from @iliapolo

--- amplify-migration-apps/fitness-tracker/src/main.tsx
+++ amplify-migration-apps/fitness-tracker/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 
 import { Amplify } from 'aws-amplify';
-import awsExports from './amplifyconfiguration.json';
+import awsExports from './aws-exports.js';
 Amplify.configure(awsExports);
 
 createRoot(document.getElementById('root')!).render(
