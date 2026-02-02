dee7445e5cdd4569247ffde14b3fe51981daf65f
Ian Hou <45278651+iankhou@users.noreply.github.com>
2026-02-02 14:42:24 -0500

    review changes

--- packages/amplify-migration-e2e-system/src/core/index.ts
+++
@@ -1,5 +0,0 @@
-export { AppSelector } from './app-selector';
-export { EnvironmentDetector } from './environment-detector';
-export { ConfigurationLoader } from './configuration-loader';
-export { AmplifyInitializer } from './amplify-initializer';
-export { CDKAtmosphereIntegration } from './cdk-atmosphere-integration';
