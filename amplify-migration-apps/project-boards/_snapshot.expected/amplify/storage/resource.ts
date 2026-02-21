eb48a435ec4776977f142677b1e769030a039034
Eli Polonsky <epolon@amazon.com>
2026-02-21 18:24:54 -0500

    chore: mid work

--- amplify-migration-apps/project-boards/_snapshot.expected/amplify/storage/resource.ts
+++ amplify-migration-apps/project-boards/_snapshot.expected/amplify/storage/resource.ts
@@ -6,14 +6,14 @@ export const storage = defineStorage({
   name: `projectboardsfe06adf5e87344b59ef17edd21ee3b3c1e851-${branchName}`,
   access: (allow) => ({
     'public/*': [
-      allow.guest.to(['read']), 
-      allow.authenticated.to(['write', 'read', 'delete'])
+      allow.guest.to(['read']),
+      allow.authenticated.to(['write', 'read', 'delete']),
     ],
     'protected/{entity_id}/*': [
-      allow.authenticated.to(['write', 'read', 'delete'])
+      allow.authenticated.to(['write', 'read', 'delete']),
     ],
     'private/{entity_id}/*': [
-      allow.authenticated.to(['write', 'read', 'delete'])
+      allow.authenticated.to(['write', 'read', 'delete']),
     ],
   }),
 });
