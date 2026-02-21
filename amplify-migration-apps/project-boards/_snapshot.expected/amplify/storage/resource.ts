13d6ad84b9ec6088a3716920d6adb366ea2f9ee2
Eli Polonsky <epolon@amazon.com>
2026-02-21 18:22:08 -0500

    chore: mid work

--- amplify-migration-apps/project-boards/_snapshot.expected/amplify/storage/resource.ts
+++ amplify-migration-apps/project-boards/_snapshot.expected/amplify/storage/resource.ts
@@ -5,8 +5,15 @@ const branchName = process.env.AWS_BRANCH ?? 'sandbox';
 export const storage = defineStorage({
   name: `projectboardsfe06adf5e87344b59ef17edd21ee3b3c1e851-${branchName}`,
   access: (allow) => ({
-    'public/*': [allow.guest.to(['read']), allow.authenticated.to(['write', 'read', 'delete'])],
-    'protected/{entity_id}/*': [allow.authenticated.to(['write', 'read', 'delete'])],
-    'private/{entity_id}/*': [allow.authenticated.to(['write', 'read', 'delete'])],
+    'public/*': [
+      allow.guest.to(['read']), 
+      allow.authenticated.to(['write', 'read', 'delete'])
+    ],
+    'protected/{entity_id}/*': [
+      allow.authenticated.to(['write', 'read', 'delete'])
+    ],
+    'private/{entity_id}/*': [
+      allow.authenticated.to(['write', 'read', 'delete'])
+    ],
   }),
 });
