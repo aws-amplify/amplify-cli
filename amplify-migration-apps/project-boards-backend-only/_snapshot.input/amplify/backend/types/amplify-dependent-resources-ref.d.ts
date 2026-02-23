8d0e16576f9ec48d4bc5d4b7df76bdef671f9ed3
Eli Polonsky <epolon@amazon.com>
2026-02-23 16:37:13 -0500

    chore: add backend-only-app

---
+++ amplify-migration-apps/project-boards-backend-only/_snapshot.input/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "backendonly": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "backendonly1de78d1b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "quotegenerator": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s397fd7f08": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
