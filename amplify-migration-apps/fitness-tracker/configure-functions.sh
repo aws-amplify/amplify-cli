3a8ceab7d81cd68f71391a92093827350b1beb52
dgandhi62 <gandhidhyan@gmail.com>
2026-03-18 14:41:36 -0400

    chore: remove fitness-tracker not part of this PR

--- amplify-migration-apps/fitness-tracker/configure-functions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-cp -f restapi.js ./amplify/backend/function/lognutrition/src/app.js
-cp -f adminapi.js ./amplify/backend/function/admin/src/app.js
-cp -f adminapi.package.json ./amplify/backend/function/admin/src/package.json
--- amplify-migration-apps/fitness-tracker/configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-./configure-schema.sh
-./configure-functions.sh
