dc36b63ba2e46703c1366e367940b00869ca6ad5
Eli Polonsky <epolon@amazon.com>
2026-04-02 13:20:38 -0400

    chore: revert pipefail

--- amplify-migration-apps/imported-resources/configure.sh
+++ amplify-migration-apps/imported-resources/configure.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -euxo pipefail
-
 cp -f schema.graphql ./amplify/backend/api/importedresources/schema.graphql
 cp -f quotegenerator.js ./amplify/backend/function/importedresourcequotegenerator/src/index.js
--- amplify-migration-apps/project-boards/configure.sh
+++ amplify-migration-apps/project-boards/configure.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -euxo pipefail
-
-#cp -f schema.graphql ./amplify/backend/api/projectboards/schema.graphql
+cp -f schema.graphql ./amplify/backend/api/projectboards/schema.graphql
 cp -f quotegenerator.js ./amplify/backend/function/quotegenerator/src/index.js
