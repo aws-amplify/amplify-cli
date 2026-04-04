0d162a684e9b3c8a583bc76476d28416df2d1fff
Eli Polonsky <epolon@amazon.com>
2026-04-03 20:41:43 -0400

    chore: remove export files

--- packages/amplify-gen2-migration-e2e-system/src/core/index.ts
+++
@@ -1,2 +0,0 @@
-export { AmplifyInitializer } from './amplify-initializer';
-export { Gen2MigrationExecutor } from './gen2-migration-executor';
--- packages/amplify-gen2-migration-e2e-system/src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Main entry point for the Amplify Migration System
- */
-
-export { App } from './core/app';
-export { Logger } from './core/logger';
