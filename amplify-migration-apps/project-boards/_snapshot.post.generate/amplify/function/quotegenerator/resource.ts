254d15b8b8d588b6dc9a549f2bdb9c7ec0af6129
Eli Polonsky <epolon@amazon.com>
2026-04-13 10:35:57 -0400

    chore: restore snapshots

--- amplify-migration-apps/project-boards/_snapshot.post.generate/amplify/function/quotegenerator/resource.ts
+++ amplify-migration-apps/project-boards/_snapshot.post.generate/amplify/function/quotegenerator/resource.ts
@@ -1,12 +1,12 @@
 import { defineFunction } from '@aws-amplify/backend';
 
-const branchName = process.env.AWS_BRANCH ?? 'sandbox';
+const dc72c1d108.deploymentTypeName = process.env.AWS_BRANCH ?? 'sandbox';
 
 export const quotegenerator = defineFunction({
   entry: './index.js',
-  name: `quotegenerator-${branchName}`,
+  name: `quotegenerator-${dc72c1d108.deploymentTypeName}`,
   timeoutSeconds: 25,
   memoryMB: 128,
-  environment: { ENV: `${branchName}`, REGION: 'us-east-1' },
+  environment: { ENV: `${dc72c1d108.deploymentTypeName}`, REGION: 'us-east-1' },
   runtime: 22,
 });
