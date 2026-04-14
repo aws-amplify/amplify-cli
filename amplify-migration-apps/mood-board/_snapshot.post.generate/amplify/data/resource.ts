8daaaddc2ffb623de052ce569c6af37b4da96b2f
Eli Polonsky <epolon@amazon.com>
2026-04-13 22:24:31 -0400

    chore: regen snapshots

--- amplify-migration-apps/mood-board/_snapshot.post.generate/amplify/data/resource.ts
+++ amplify-migration-apps/mood-board/_snapshot.post.generate/amplify/data/resource.ts
@@ -26,10 +26,10 @@ export const data = defineData({
   migratedAmplifyGen1DynamoDbTableMappings: [
     {
       //The "branchname" variable needs to be the same as your deployment branch if you want to reuse your Gen1 app tables
-      branchName: 'main',
+      branchName: 'x',
       modelNameToTableNameMapping: {
-        MoodItem: 'MoodItem-vsozkn3hbzdjppzyf7xtf2f4sy-main',
-        Board: 'Board-vsozkn3hbzdjppzyf7xtf2f4sy-main',
+        MoodItem: 'af391f9f22.GetAttMoodItemTableName',
+        Board: '1156e2eacd.GetAttBoardTableName',
       },
     },
   ],
