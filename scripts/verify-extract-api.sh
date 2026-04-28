f7225178a2a6a45812062e6f84eb0714385b62a7
Ian Hou <45278651+iankhou@users.noreply.github.com>
2026-04-28 11:57:17 -0400

    debug api-extract

--- scripts/verify-extract-api.sh
+++ scripts/verify-extract-api.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-changed_api_extracts=$(git status | grep -F API.md | wc -l)
+
+gs_output=$(git status)
+
+echo git status output follows
+echo $gs_output
+
+gd_output=$(git diff)
+
+echo git diff output follows
+echo $gd_output
+
+changed_api_extracts=$(gs_output | grep -F API.md | wc -l)
 
 if [[ changed_api_extracts -gt 0 ]]; then
   echo "Fail! Detected api change. Please run 'yarn extract-api' and add API.md file changes to the change set."
