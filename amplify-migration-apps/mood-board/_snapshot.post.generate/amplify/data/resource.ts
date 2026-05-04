95ec86f1ebd44a9d6a75ed98fe3f75040458c3df
dgandhi62 <gandhidhyan@gmail.com>
2026-05-04 15:56:42 -0400

    feat: new moodboard snapshots

--- amplify-migration-apps/mood-board/_snapshot.post.generate/amplify/data/resource.ts
+++ amplify-migration-apps/mood-board/_snapshot.post.generate/amplify/data/resource.ts
@@ -1,5 +1,11 @@
 import { defineData } from '@aws-amplify/backend';
 import type { Backend } from '../backend';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readdirSync } from 'fs';
+import * as assets from 'aws-cdk-lib/aws-s3-assets';
+import { aws_appsync } from 'aws-cdk-lib';
+import { CfnResolver } from 'aws-cdk-lib/aws-appsync';
 
 const branchName = process.env.AWS_BRANCH ?? 'sandbox';
 const schema = `type MoodItem @model @auth(rules: [{ allow: public }]) {
@@ -34,9 +40,10 @@ export const data = defineData({
       //The "branchName" variable needs to be the same as your deployment branch if you want to reuse your Gen1 app tables
       branchName: 'x',
       modelNameToTableNameMapping: {
-        MoodItem: 'MoodItem-vyw4apkp65cjxjp6ojxlou7twi-x',
-        Board: 'Board-vyw4apkp65cjxjp6ojxlou7twi-x',
-        KinesisEventCount: 'KinesisEventCount-vyw4apkp65cjxjp6ojxlou7twi-x',
+        MoodItem: 'MoodItem-tw6yns43nvct3fzlwlouod43x4-x',
+        Board: 'Board-tw6yns43nvct3fzlwlouod43x4-x',
+        KinesisEventCount:
+          'KinesisEventCount-tw6yns43nvct3fzlwlouod43x4-x',
       },
     },
   ],
@@ -61,4 +68,77 @@ export function applyEscapeHatches(backend: Backend) {
       },
     },
   ];
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const resolversDir = join(__dirname, 'resolvers');
+  const resolverFiles = readdirSync(resolversDir).filter(
+    (f) =>
+      (f.endsWith('.req.vtl') || f.endsWith('.res.vtl')) &&
+      f.split('.').length === 4
+  );
+  for (const file of resolverFiles) {
+    const [typeName, fieldName, templateType] = file.split('.');
+    const capitalizedFieldName =
+      fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
+    const functionId = `${typeName}${capitalizedFieldName}DataResolverFn`;
+    const fn =
+      backend.data.resources.cfnResources.cfnFunctionConfigurations[functionId];
+    const vtlTemplate = new assets.Asset(backend.data, `VTLTemplate-${file}`, {
+      path: join(resolversDir, file),
+    });
+    if (templateType === 'req') {
+      fn.requestMappingTemplateS3Location = vtlTemplate.s3ObjectUrl;
+    } else {
+      fn.responseMappingTemplateS3Location = vtlTemplate.s3ObjectUrl;
+    }
+  }
+  const noneDataSource =
+    backend.data.resources.graphqlApi.addNoneDataSource('none');
+  const MutationcreateBoardinit2 = new aws_appsync.AppsyncFunction(
+    backend.data.stack,
+    'MutationcreateBoardinit2',
+    {
+      name: 'MutationcreateBoardinit2',
+      api: backend.data.resources.graphqlApi,
+      dataSource: noneDataSource,
+      requestMappingTemplate: aws_appsync.MappingTemplate.fromFile(
+        join(resolversDir, 'Mutation.createBoard.init.2.req.vtl')
+      ),
+      responseMappingTemplate: aws_appsync.MappingTemplate.fromString(
+        '$util.toJson($ctx.prev.result)'
+      ),
+    }
+  );
+  const MutationcreateBoardfinish1 = new aws_appsync.AppsyncFunction(
+    backend.data.stack,
+    'MutationcreateBoardfinish1',
+    {
+      name: 'MutationcreateBoardfinish1',
+      api: backend.data.resources.graphqlApi,
+      dataSource: noneDataSource,
+      requestMappingTemplate:
+        aws_appsync.MappingTemplate.fromString('$util.toJson({})'),
+      responseMappingTemplate: aws_appsync.MappingTemplate.fromFile(
+        join(resolversDir, 'Mutation.createBoard.finish.1.res.vtl')
+      ),
+    }
+  );
+  const mutationCreateBoardResolver = backend.data.resources.cfnResources
+    .cfnResolvers['Mutation.createBoard'] as CfnResolver;
+  const mutationCreateBoardPipelineFunctions =
+    (
+      mutationCreateBoardResolver.pipelineConfig as CfnResolver.PipelineConfigProperty
+    ).functions || [];
+  mutationCreateBoardPipelineFunctions.splice(
+    1,
+    0,
+    MutationcreateBoardinit2.functionId
+  );
+  mutationCreateBoardPipelineFunctions.splice(
+    5,
+    0,
+    MutationcreateBoardfinish1.functionId
+  );
+  mutationCreateBoardResolver.pipelineConfig = {
+    functions: mutationCreateBoardPipelineFunctions,
+  };
 }
