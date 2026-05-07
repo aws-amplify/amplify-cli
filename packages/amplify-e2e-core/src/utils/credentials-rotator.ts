be69d6e6c015a25237b9e5d1389c5e52288f72bd
Eli Polonsky <epolon@amazon.com>
2026-05-07 15:02:34 -0400

    chore: revert a bunch

--- packages/amplify-e2e-core/src/utils/credentials-rotator.ts
+++ packages/amplify-e2e-core/src/utils/credentials-rotator.ts
@@ -1,28 +1,24 @@
 import { AssumeRoleCommand, STSClient } from '@aws-sdk/client-sts';
-// import { fromContainerMetadata } from '@aws-sdk/credential-providers';
+import { fromContainerMetadata } from '@aws-sdk/credential-providers';
 import { generateRandomShortId, TEST_PROFILE_NAME } from './index';
 import * as ini from 'ini';
 import * as fs from 'fs-extra';
 import { pathManager } from '@aws-amplify/amplify-cli-core';
 
-export const refreshCredentials = async (roleArn: string, profile?: string) => {
+const refreshCredentials = async (roleArn: string) => {
   const client = new STSClient({
     // Use CodeBuild role to assume test account role. I.e. don't read credentials from process.env
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID_ORIG,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ORIG,
-      sessionToken: process.env.AWS_SESSION_TOKEN_ORIG,
-    },
+    credentials: fromContainerMetadata(),
   });
   const sessionName = `testSession${generateRandomShortId()}`;
   const command = new AssumeRoleCommand({
     RoleArn: roleArn,
     RoleSessionName: sessionName,
-    DurationSeconds: 900,
+    DurationSeconds: 3600,
   });
   const response = await client.send(command);
 
-  const profileName = profile ?? TEST_PROFILE_NAME;
+  const profileName = TEST_PROFILE_NAME;
   const credentialsContents = ini.parse((await fs.readFile(pathManager.getAWSCredentialsFilePath())).toString());
   credentialsContents[profileName] = credentialsContents[profileName] || {};
   credentialsContents[profileName].aws_access_key_id = response.Credentials.AccessKeyId;
@@ -34,9 +30,9 @@ export const refreshCredentials = async (roleArn: string, profile?: string) => {
   await fs.writeFile(pathManager.getAWSCredentialsFilePath(), ini.stringify(credentialsContents));
 };
 
-export const tryRefreshCredentials = async (roleArn: string, profile?: string) => {
+const tryRefreshCredentials = async (roleArn: string) => {
   try {
-    await refreshCredentials(roleArn, profile);
+    await refreshCredentials(roleArn);
     console.log('Test profile credentials refreshed');
   } catch (e) {
     console.error('Test profile credentials request failed');
